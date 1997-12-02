b3e9cce89a07963e0ed3e3bcaf25f6ccc047480f
lijewski <lijewski>
1997-12-02 19:12:17 +0000

    ErrorType and ErrorFunc are now member of ErrorRec.

--- Src/C_AMRLib/ErrorList.cpp
+++ Src/C_AMRLib/ErrorList.cpp
@@ -1,82 +1,69 @@
 //BL_COPYRIGHT_NOTICE
 
 //
-// $Id: ErrorList.cpp,v 1.3 1997-11-26 20:41:43 lijewski Exp $
+// $Id: ErrorList.cpp,v 1.4 1997-12-02 19:12:17 lijewski Exp $
 //
 
 #include <ErrorList.H>
 
-// ------------------------------------------------------------------
-ErrorRec::ErrorRec(const aString &nm, int ng, ErrorType etyp, ErrorFunc f)
-    : ngrow(ng), err_type(etyp), err_func(f), derive_name(nm)
-{
-}
-
-// ------------------------------------------------------------------
-ErrorRec::~ErrorRec()
-{
-}
-
-// ------------------------------------------------------------------
-ErrorList::ErrorList()
-    : lst()
-{}
-
-// ------------------------------------------------------------------
-ErrorList::~ErrorList()
+ErrorList::~ErrorList ()
 {
    clear();
 }
 
-// ------------------------------------------------------------------
 void
-ErrorList::clear()
+ErrorList::clear ()
 {
-    ListIterator<ErrorRec*> li(lst);
-    while(li) {
+    for (ListIterator<ErrorRec*> li(lst); li; ++li)
+    {
 	delete lst[li];
 	lst[li] = 0;
-	++li;
     }
 }
 
-// ------------------------------------------------------------------
 const ErrorRec*
 ErrorList::operator[] (int k) const
 {
-    assert( k < length() );
+    assert(k < length());
+
     ListIterator<ErrorRec*> li(lst);
-    while(li && k > 0) {
+    
+    while (li && k > 0)
+    {
 	++li;
 	--k;
     }
     return lst[li];
 }
 
-// ------------------------------------------------------------------
 void
-ErrorList::add(const aString &name, int nextra, ErrorType typ,
-	       ErrorFunc func)
+ErrorList::add (const aString&      name,
+                int                 nextra,
+                ErrorRec::ErrorType typ,
+                ErrorRec::ErrorFunc func)
 {
     ErrorRec* er = new ErrorRec(name,nextra,typ,func);
     if (er == 0)
         BoxLib::OutOfMemory(__FILE__, __LINE__);
+    //
+    // Keep list in order of definition, append().
+    //
     lst.append(er);
-      // keep list in order of definition, append
 }
 
-static aString err_name[] = {"Richardson", "Special"};
+static const char* err_name[] = { "Richardson", "Special" };
 
-// ------------------------------------------------------------------
 ostream&
 operator << (ostream &os, const ErrorList& elst)
 {
     for (ListIterator<ErrorRec*> li(elst.lst); li; ++li)
     {
-	os << li()->derive_name;
-	os << ' ' << li()->ngrow << ' ' << err_name[li()->err_type];
-	os << '\n';
-	++li;
+	os << li()->name()
+           << ' '
+           << li()->nGrow()
+           << ' '
+           << err_name[li()->errType()]
+           << '\n';
     }
     return os;
 }
