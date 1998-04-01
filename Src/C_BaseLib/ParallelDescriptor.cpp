09f6fbb6b238095dc66b235ad74eaf7b888be8e0
lijewski <lijewski>
1998-04-01 16:55:04 +0000

    Cleaned up code. Implemented Synchronize() for MPI using MPI_Barrier().

--- Src/C_BaseLib/ParallelDescriptor.cpp
+++ Src/C_BaseLib/ParallelDescriptor.cpp
@@ -1,100 +1,121 @@
 //BL_COPYRIGHT_NOTICE
 
 //
-// $Id: ParallelDescriptor.cpp,v 1.18 1998-04-01 00:29:49 car Exp $
+// $Id: ParallelDescriptor.cpp,v 1.19 1998-04-01 16:55:04 lijewski Exp $
 //
+
 #include <Utility.H>
 #include <ParallelDescriptor.H>
 
-
 #if defined(BL_USE_BSP)
+
 #include "bsp.h"
-
-#ifdef FIXBSPLIBLEVEL1HEADER
-extern "C"
-{
-  extern void bsp_fold(void (*)(void *, void *, void *, int *),
-                       void*,void*,int);
-  extern void bsp_fold_cpp(void (*)(void *, void *, void *, int *),
-                           void*,void* , int, int, char *);
-}
-#endif /*FIXBSPLIBLEVEL1HEADER*/
-
-#include "bsp_level1.h"
-
-
-void ParallelDescriptor::StartParallel(int nprocs, int* argc, char*** argv)
-{
-    bsp_begin(nprocs);
-}
-
-void ParallelDescriptor::EndParallel()            
-{
-    bsp_end();
-}
 
 //
 // Type of function pointer required by bsp_fold().
 //
 typedef void (*VFVVVI)(void*,void*,void*,int*);
 
-int ParallelDescriptor::MyProc ()                 
+#ifdef FIXBSPLIBLEVEL1HEADER
+extern "C"
+{
+  extern void bsp_fold (VFVVVI,void*,void*,int);
+  extern void bsp_fold_cpp (VFVVVI,void*,void*,int,int,char*);
+}
+#endif /*FIXBSPLIBLEVEL1HEADER*/
+
+#include "bsp_level1.h"
+
+void
+ParallelDescriptor::StartParallel (int nprocs, int*, char***)
+{
+    bsp_begin(nprocs);
+}
+
+void
+ParallelDescriptor::EndParallel ()
+{
+    bsp_end();
+}
+
+int
+ParallelDescriptor::MyProc ()
 { 
     return bsp_pid();                
 }
-int ParallelDescriptor::NProcs ()                 
+
+int
+ParallelDescriptor::NProcs ()
 { 
     return bsp_nprocs();             
 }
-bool ParallelDescriptor::IOProcessor ()           
+
+bool
+ParallelDescriptor::IOProcessor ()
 { 
     return bsp_pid() == ioProcessor; 
 }
-int  ParallelDescriptor::IOProcessorNumber ()     
+
+int
+ParallelDescriptor::IOProcessorNumber ()
 { 
     return ioProcessor;              
 }
-void ParallelDescriptor::Abort (const char* msg)  
+
+void
+ParallelDescriptor::Abort (const char* msg)  
 { 
     bsp_abort((char*)msg);           
 }
-double ParallelDescriptor::second ()              
+
+double
+ParallelDescriptor::second ()              
 { 
     return bsp_time();               
 }
 
-void ParallelDescriptor::ShareVar (const void* var,
-				   int         bytes)
+void
+ParallelDescriptor::ShareVar (const void* var,
+                              int         bytes)
 {
     bsp_pushregister(var, bytes);
 }
 
-void ParallelDescriptor::UnshareVar (const void* var)
+void
+ParallelDescriptor::UnshareVar (const void* var)
 {
     bsp_popregister(var);
 }
-void ParallelDescriptor::WriteData (int         procnum,
-				    const void* src,
-				    void*       dest,
-				    int         offset,
-				    int         bytes)
+
+void
+ParallelDescriptor::WriteData (int         procnum,
+                               const void* src,
+                               void*       dest,
+                               int         offset,
+                               int         bytes)
 {
     bsp_hpput(procnum, src, dest, offset, bytes);
 }
-void ParallelDescriptor::ReadData (int         procnum,
-				   const void* src,
-				   int         offset,
-				   void*       dest,
-				   int         bytes)
+
+void
+ParallelDescriptor::ReadData (int         procnum,
+                              const void* src,
+                              int         offset,
+                              void*       dest,
+                              int         bytes)
 {
     bsp_hpget(procnum, src, offset, dest, bytes);
 }
-void ParallelDescriptor::SetMessageHeaderSize (int messageHeaderSize)
+
+void
+ParallelDescriptor::SetMessageHeaderSize (int messageHeaderSize)
 {
     bsp_set_tag_size(&messageHeaderSize);
 } 
-bool ParallelDescriptor::GetMessageHeader (int& dataSize,
-					   void* messageHeader)
+
+bool
+ParallelDescriptor::GetMessageHeader (int&  dataSize,
+                                      void* messageHeader)
 {
     bsp_get_tag(&dataSize, messageHeader);
     
@@ -103,15 +124,18 @@ bool ParallelDescriptor::GetMessageHeader (int& dataSize,
 	: true;  // A message is waiting
 } 
 
-void ParallelDescriptor::SendData (int         toproc,
-				   const void* messageHeader,
-				   const void* data,
-				   int         datasizeinbytes)
+void
+ParallelDescriptor::SendData (int         toproc,
+                              const void* messageHeader,
+                              const void* data,
+                              int         datasizeinbytes)
 {
     bsp_send(toproc, messageHeader, data, datasizeinbytes);
 }
-void ParallelDescriptor::ReceiveData (void* data,
-				      int   datasizeinbytes)
+
+void
+ParallelDescriptor::ReceiveData (void* data,
+                                 int   datasizeinbytes)
 {
     bsp_move(data, datasizeinbytes);
 }
@@ -123,7 +147,7 @@ ParallelDescriptor::Synchronize ()
 }
 
 void
-ParallelDescriptor::ReduceBoolAnd (bool &rvar)
+ParallelDescriptor::ReduceBoolAnd (bool& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(bool));
     ParallelDescriptor::Synchronize();
@@ -132,7 +156,7 @@ ParallelDescriptor::ReduceBoolAnd (bool &rvar)
 }
 
 void
-ParallelDescriptor::ReduceBoolOr  (bool &rvar)
+ParallelDescriptor::ReduceBoolOr  (bool& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(bool));
     ParallelDescriptor::Synchronize();
@@ -141,7 +165,7 @@ ParallelDescriptor::ReduceBoolOr  (bool &rvar)
 }
 
 void
-ParallelDescriptor::ReduceRealSum (Real &rvar)
+ParallelDescriptor::ReduceRealSum (Real& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(Real));
     ParallelDescriptor::Synchronize();
@@ -150,7 +174,7 @@ ParallelDescriptor::ReduceRealSum (Real &rvar)
 }
 
 void
-ParallelDescriptor::ReduceRealMax (Real &rvar)
+ParallelDescriptor::ReduceRealMax (Real& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(Real));
     ParallelDescriptor::Synchronize();
@@ -159,7 +183,7 @@ ParallelDescriptor::ReduceRealMax (Real &rvar)
 }
 
 void
-ParallelDescriptor::ReduceRealMin (Real &rvar)
+ParallelDescriptor::ReduceRealMin (Real& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(Real));
     ParallelDescriptor::Synchronize();
@@ -168,7 +192,7 @@ ParallelDescriptor::ReduceRealMin (Real &rvar)
 }
 
 void
-ParallelDescriptor::ReduceIntSum (int &rvar)
+ParallelDescriptor::ReduceIntSum (int& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(int));
     ParallelDescriptor::Synchronize();
@@ -177,7 +201,7 @@ ParallelDescriptor::ReduceIntSum (int &rvar)
 }
 
 void
-ParallelDescriptor::ReduceIntMax (int &rvar)
+ParallelDescriptor::ReduceIntMax (int& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(int));
     ParallelDescriptor::Synchronize();
@@ -186,7 +210,7 @@ ParallelDescriptor::ReduceIntMax (int &rvar)
 }
 
 void
-ParallelDescriptor::ReduceIntMin (int &rvar)
+ParallelDescriptor::ReduceIntMin (int& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(int));
     ParallelDescriptor::Synchronize();
@@ -195,7 +219,7 @@ ParallelDescriptor::ReduceIntMin (int &rvar)
 }
 
 void
-ParallelDescriptor::ReduceLongSum (long &rvar)
+ParallelDescriptor::ReduceLongSum (long& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(long));
     ParallelDescriptor::Synchronize();
@@ -204,7 +228,7 @@ ParallelDescriptor::ReduceLongSum (long &rvar)
 }
 
 void
-ParallelDescriptor::ReduceLongMax (long &rvar)
+ParallelDescriptor::ReduceLongMax (long& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(long));
     ParallelDescriptor::Synchronize();
@@ -213,7 +237,7 @@ ParallelDescriptor::ReduceLongMax (long &rvar)
 }
 
 void
-ParallelDescriptor::ReduceLongMin (long &rvar)
+ParallelDescriptor::ReduceLongMin (long& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(long));
     ParallelDescriptor::Synchronize();
@@ -222,7 +246,7 @@ ParallelDescriptor::ReduceLongMin (long &rvar)
 }
 
 void
-ParallelDescriptor::ReduceLongAnd (long &rvar)
+ParallelDescriptor::ReduceLongAnd (long& rvar)
 {
     ParallelDescriptor::ShareVar(&rvar, sizeof(long));
     ParallelDescriptor::Synchronize();
@@ -239,9 +263,13 @@ ParallelDescriptor::MessageQueueEmpty ()
     return ParallelDescriptor::GetMessageHeader(dataWaitingSize,&fabComTag);
 }
 
-void ParallelDescriptor::Broadcast (int fromproc, void*  src, void*  dest, int nbytes)
+void
+ParallelDescriptor::Broadcast (int    fromproc,
+                               void*  src,
+                               void*  dest,
+                               int    nbytes)
 {
-    bsp_bcast(fromproc, (void *) src, (void *) dest, nbytes);
+    bsp_bcast(fromproc, (void*) src, (void*) dest, nbytes);
 }
 
 #elif defined(BL_USE_MPI)
@@ -249,163 +277,184 @@ void ParallelDescriptor::Broadcast (int fromproc, void*  src, void*  dest, int n
 #include "mpi.h"
 
 static int numprocs = -1;
-static int myid = -1;
+static int myid     = -1;
 
-void ParallelDescriptor::StartParallel(int, int* argc, char*** argv)
+void
+ParallelDescriptor::StartParallel (int,
+                                   int*    argc,
+                                   char*** argv)
 {
     MPI_Init(argc, argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 }
-void ParallelDescriptor::EndParallel()
+
+void
+ParallelDescriptor::EndParallel ()
 {
     MPI_Finalize();
 }
 
-int ParallelDescriptor::MyProc()
+int
+ParallelDescriptor::MyProc ()
 {
     assert(myid != -1);
     return myid;
 }
-int ParallelDescriptor::NProcs()
+
+int
+ParallelDescriptor::NProcs ()
 {
     assert(numprocs != -1);
     return numprocs;
 }
 
-bool ParallelDescriptor::IOProcessor()
+bool
+ParallelDescriptor::IOProcessor ()
 {
     assert(myid == -1);
     return myid == ioProcessor;
 }
-int ParallelDescriptor::IOProcessorNumber()
+
+int
+ParallelDescriptor::IOProcessorNumber ()
 {
     return ioProcessor;
 }
 
-double ParallelDescriptor::second()
+double
+ParallelDescriptor::second ()
 {
     return MPI_Wtime();
 }
 
-void ParallelDescriptor::ReduceRealMax(Real& r)
+void
+ParallelDescriptor::Synchronize ()
+{
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+void
+ParallelDescriptor::ReduceRealMax (Real& r)
 {
     Real recv;
     MPI_Allreduce(&r, &recv, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
     r = recv;
 }
 
-void ParallelDescriptor::ReduceRealMin(Real& r)
+void
+ParallelDescriptor::ReduceRealMin (Real& r)
 {
     Real recv;
     MPI_Allreduce(&r, &recv, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
     r = recv;
 }
 
-void ParallelDescriptor::ReduceRealSum(Real& r)
+void
+ParallelDescriptor::ReduceRealSum (Real& r)
 {
     Real recv;
     MPI_Allreduce(&r, &recv, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     r = recv;
 }
 
-void ParallelDescriptor::ReduceLongAnd(long& r)
+void
+ParallelDescriptor::ReduceLongAnd (long& r)
 {
     long recv;
     MPI_Allreduce(&r, &recv, 1, MPI_LONG, MPI_LAND, MPI_COMM_WORLD);
     r = recv;
 }
 
-void ParallelDescriptor::ReduceLongSum(long& r)
+void
+ParallelDescriptor::ReduceLongSum (long& r)
 {
     long recv;
     MPI_Allreduce(&r, &recv, 1, MPI_LONG, MPI_SUM, MPI_COMM_WORLD);
     r = recv;
 }
 
-void ParallelDescriptor::ReduceIntSum(int& r)
+void
+ParallelDescriptor::ReduceIntSum (int& r)
 {
     int recv;
     MPI_Allreduce(&r, &recv, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
     r = recv;
 }
 
-void ParallelDescriptor::Abort(const char*)
+void
+ParallelDescriptor::Abort (const char* msg)
 {
+    BoxLib::Warning(msg);
     MPI_Abort(MPI_COMM_WORLD, -1);
 }
 
 #else
 
-
 void ParallelDescriptor::StartParallel(int, int*, char***) {}
 void ParallelDescriptor::EndParallel() {}
 
-void ParallelDescriptor::Abort (const char* str)
-{
-    BoxLib::Abort(str);
-}
+void ParallelDescriptor::Abort (const char* str) { BoxLib::Abort(str); }
 int ParallelDescriptor::MyProc () { return 0; }
 int ParallelDescriptor::NProcs () { return 1; }
 void ParallelDescriptor::Synchronize () {}
 bool ParallelDescriptor::IOProcessor () { return true; }
 int  ParallelDescriptor::IOProcessorNumber () { return 0; }
 
-// reduction operations
-//template<class T> static void ReduceMin (T &rvar) {}
-
-// bool
 void ParallelDescriptor::ReduceBoolAnd (bool& rvar) {}
 void ParallelDescriptor::ReduceBoolOr  (bool& rvar) {}
-// Real
+
 void ParallelDescriptor::ReduceRealSum (Real& rvar) {}
 void ParallelDescriptor::ReduceRealMax (Real& rvar) {}
 void ParallelDescriptor::ReduceRealMin (Real& rvar) {}
-// int
+
 void ParallelDescriptor::ReduceIntSum (int& rvar) {}
 void ParallelDescriptor::ReduceIntMax (int& rvar) {}
 void ParallelDescriptor::ReduceIntMin (int& rvar) {}
-// long
+
 void ParallelDescriptor::ReduceLongSum (long& rvar) {}
 void ParallelDescriptor::ReduceLongMax (long& rvar) {}
 void ParallelDescriptor::ReduceLongMin (long& rvar) {}
 void ParallelDescriptor::ReduceLongAnd (long& rvar) {}
 
-// data transfer functions
-void ParallelDescriptor::ShareVar (const void* var, int         bytes) {}
+
+void ParallelDescriptor::ShareVar (const void* var, int bytes) {}
 void ParallelDescriptor::UnshareVar (const void* var) {}
 void ParallelDescriptor::WriteData (int         procnum,
-		       const void* src,
-		       void*       dest,
-		       int         offset,
-		       int         bytes) {}
+                                    const void* src,
+                                    void*       dest,
+                                    int         offset,
+                                    int         bytes) {}
 
 void ParallelDescriptor::ReadData (int         procnum,
-	       const void* src,
-	       int         offset,
-	       void*       dest,
-	       int         bytes) {}
+                                   const void* src,
+                                   int         offset,
+                                   void*       dest,
+                                   int         bytes) {}
 
 void ParallelDescriptor::SetMessageHeaderSize (int messageHeaderSize) {} 
 
 bool ParallelDescriptor::GetMessageHeader (int&  dataSize,
-		       void* messageHeader)
+                                           void* messageHeader)
 {
-    return false;  // no messages waiting
-} 
+    return false;  // No messages waiting.
+}
 bool ParallelDescriptor::MessageQueueEmpty ()
 {
-    return true;  // no messages waiting
+    return true;  // No messages waiting.
 } 
 void ParallelDescriptor::SendData (int         toproc,
-	       const void* messageHeader,
-	       const void* data,
-	       int         datasizeinbytes) {}
+                                   const void* messageHeader,
+                                   const void* data,
+                                   int         datasizeinbytes) {}
 
 void ParallelDescriptor::ReceiveData (void* data,
-		  int   datasizeinbytes) {}
+                                      int   datasizeinbytes) {}
 
-void ParallelDescriptor::Broadcast (int fromproc, void*  src, void*  dest, int nbytes) {}
+void ParallelDescriptor::Broadcast (int    fromproc,
+                                    void*  src,
+                                    void*  dest,
+                                    int    nbytes) {}
 
 //
 // Here so we don't need to include <Utility.H> in <ParallelDescriptor.H>.
@@ -417,4 +466,4 @@ ParallelDescriptor::second ()
     return Utility::second();
 }
 
-#endif /*BL_USE_BSP*/
+#endif
