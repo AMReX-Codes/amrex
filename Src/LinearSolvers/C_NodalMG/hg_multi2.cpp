aa61f39ebe8767f9af24acd7bcceb8401dd0ae14
car <car>
1998-11-18 01:30:51 +0000

    *** empty log message ***

--- Src/LinearSolvers/C_NodalMG/hg_multi2.cpp
+++ Src/LinearSolvers/C_NodalMG/hg_multi2.cpp
@@ -384,7 +384,6 @@ void holy_grail_amr_multigrid::interface_residual(int mglev, int lev)
 	const Box& cbox = fres_cbox[lev][iface];
 	const Box& sigmacbox = fres_scbox[lev][iface];
 	const Box& sigmafbox = fres_sfbox[lev][iface];
-	assert(sigmafbox == sigma[mglev][igrid].box());
 	task_fab* sigmac = new task_fill_patch(resid[mglev], igrid, sigmacbox, sigma[mglev], lev_interface[mglevc], bndry, -1, -1);
 	task_fab* cdst   = new task_fill_patch(resid[mglev], igrid, cbox, dest[lev-1], lev_interface[mglevc], boundary.pressure(), -1, -1);
 	const Box& creg = fres_creg[lev][iface];
@@ -412,7 +411,6 @@ void holy_grail_amr_multigrid::interface_residual(int mglev, int lev)
 	}
     }
     tl.execute();
-
     if (m_hg_cross_stencil || m_hg_terrain)
     {
 	
