2371e215d251dba7897c75e6f1eb444ad67fb0c8
lijewski <lijewski>
1998-11-28 21:58:43 +0000

    More work bring up-to-date with amrlib structures.

--- Src/C_AMRLib/StationData.cpp
+++ Src/C_AMRLib/StationData.cpp
@@ -1,7 +1,7 @@
 //BL_COPYRIGHT_NOTICE
 
 //
-// $Id: StationData.cpp,v 1.2 1998-11-25 23:58:42 lijewski Exp $
+// $Id: StationData.cpp,v 1.3 1998-11-28 21:58:43 lijewski Exp $
 //
 #ifdef BL_USE_NEW_HFILES
 #include <cstring>
@@ -9,6 +9,7 @@
 #include <string.h>
 #endif
 
+#include "AmrLevel.H"
 #include "ParmParse.H"
 #include "StationData.H"
 
@@ -26,9 +27,7 @@ AddCoord (const Array<Real>& coord,
     for (int n = 0; n < BL_SPACEDIM; n++)
         rec.pos[n] = coord[n];
 
-    rec.id    = id_count++;
-    rec.level = -1;
-    rec.grd   = 0;
+    rec.id = id_count++;
 }
 
 void
@@ -48,10 +47,20 @@ StationData::init ()
     if (pp.contains("vars"))
     {
         m_vars.resize(pp.countval("vars"));
+        m_typ.resize(m_vars.length());
+        m_ncomp.resize(m_vars.length());
 
         for (int i = 0; i < m_vars.length(); i++)
         {
             pp.get("vars", m_vars[i], i);
+
+            if (!AmrLevel::isStateVariable(m_vars[i],m_typ[i],m_ncomp[i]))
+            {
+                cout << "StationData::init(): `"
+                     << m_vars[i]
+                     << "' is not a state variable\n";
+                BoxLib::Abort();
+            }
         }
     }
 
@@ -116,6 +125,8 @@ StationData::openFile (int timestep)
    filename += buf;
 
    m_ofile.open(filename.c_str(), ios::out|ios::app);
+
+   m_ofile.precision(15);
 }
 
 void
@@ -140,42 +151,62 @@ StationData::listStations () const
 }
 
 void
-StationData::report (Real time) const
+StationData::report (Real time,
+                     int  level) const
 {
-    static Real* data      = 0;
-    static int   data_size = 0;
+    if (m_stn.length() <= 0)
+        return;
 
-    if (nvar > data_size)
-    {
-        data      = new Real[nvar];
-        data_size = nvar;
-    }
+    static Array<Real> data;
+
+    const int N      = m_vars.length();
+    const int MyProc = ParallelDescriptor::MyProc();
+
+    data.resize(N);
+
+    const DescriptorList& desc_lst = AmrLevel::get_desc_lst();
+
     for (int i = 0; i < m_stn.length(); i++)
     {
-        m_stn[i].grd->getData(m_stn[i].ix,data);
-        //
-        // Write data to output stream.
-        //
-        m_ofile << m_stn[i].id << ' ' << time << ' ';
+        if (m_stn[i].own && m_stn[i].level == level)
+        {
+            //
+            // Fill in the `data' vector.
+            //
+            for (int j = 0; j < N; j++)
+            {
+                const MultiFab& mf = desc_lst[m_typ[j]].newData();
 
-        for (int k = 0; k < BL_SPACEDIM; k++)
-        {
-            m_ofile << m_stn[i].pos[k] << ' ';
+                assert(mf.DistributionMap()[m_stn[i].grd] == MyProc);
+
+                assert(mf.nComp() > m_ncomp[j]);
+
+                data[j] = mf[m_stn[i].grd](m_stn[i].ix,m_ncomp[j]);
+            }
+            //
+            // Write data to output stream.
+            //
+            m_ofile << m_stn[i].id << ' ' << time << ' ';
+
+            for (int k = 0; k < BL_SPACEDIM; k++)
+            {
+                m_ofile << m_stn[i].pos[k] << ' ';
+            }
+            for (int k = 0; k < N; k++)
+            {
+                m_ofile << data[k] << ' ';
+            }
+            m_ofile << '\n';
         }
-        for (int k = 0; k < nvar; k++)
-        {
-            m_ofile << data[k] << ' ';
-        }
-        m_ofile << '\n';
     }
 }
 
 void
-StationData::findGrid (const GridList* gl,
-                       int             hi_lev,
-                       Real            dx_lev[MAX_LEV][BL_SPACEDIM],
-                       Real            prob_lo[BL_SPACEDIM])
+StationData::findGrid (const PArray<AmrLevel>& levels,
+                       const Array<Geometry>&  geoms)
 {
+    assert(geom.length() == levels.length());
+
     if (m_stn.length() <= 0)
         return;
     //
@@ -184,33 +215,38 @@ StationData::findGrid (const GridList* gl,
     for (int i = 0; i < m_stn.length(); i++)
     {
         m_stn[i].level = -1;
-        m_stn[i].grd   = 0;
     }
     //
-    // Walk list of grids from highest level down.
+    // Find level and grid owning the data.
     //
-    for (int level = hi_lev; level >= 0; level--)
-    {
-        for (Grid* g = gl[level].first(); g != 0; g = gl[level].next(g))
-        {
-            //
-            // Find all station points in this grid.
-            //
-            const Box& gbx = g->box();
+    const int MyProc = ParallelDescriptor::MyProc();
 
-            for (int i = 0; i < m_stn.length(); i++)
+    for (int level = levels.length(); level >= 0; level--)
+    {
+        const BoxArray& ba          = levels[level].boxArray();
+        const Array<RealBox>& boxes = levels[level].gridLocations();
+
+        for (int i = 0; i < m_stn.length(); i++)
+        {
+            if (m_stn[i].level < 0)
             {
-                if (m_stn[i].level < 0)
+                const Real* pos = &m_stn[i].pos[0];
+
+                for (int j = 0; j < boxes.length(); j++)
                 {
-                    for (int n = 0; n < BL_SPACEDIM; n++)
-                    {
-                        int ipos = int((m_stn[i].pos[n]-prob_lo[n])/dx_lev[level][n]);
-                        m_stn[i].ix.setVal(n,ipos);
-                    }
-                    if (gbx.contains(m_stn[i].ix))
+                    if (boxes[j].contains(pos))
                     {
                         m_stn[i].level = level;
-                        m_stn[i].grd   = g;
+                        m_stn[i].grd   = j;
+                        m_stn[i].ix    = geoms[level].CellIndex(pos);
+                        //
+                        // Does this CPU own the data?
+                        //
+                        MultiFab mf(ba,1,0,Fab_noallocate);
+
+                        m_stn[i].own = (mf.DistributionMap()[j] == MyProc);
+
+                        break;
                     }
                 }
             }
