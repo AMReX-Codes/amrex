b7627e94ead81e6f642b19fa8c9a1bdf22cbe194
lijewski <lijewski>
1999-03-12 02:55:43 +0000

    Beefed up the test case a bit.

--- Src/C_BaseLib/test/tFB.cpp
+++ Src/C_BaseLib/test/tFB.cpp
@@ -1,63 +1,119 @@
 //BL_COPYRIGHT_NOTICE
 
 //
-// $Id: tFB.cpp,v 1.1 1998-09-30 17:07:39 lijewski Exp $
+// $Id: tFB.cpp,v 1.2 1999-03-12 02:55:43 lijewski Exp $
 //
 // A test program for FillBoundary().
 //
 
 #include <Utility.H>
 #include <MultiFab.H>
+
+#ifdef BL_USE_NEW_HFILES
+#include <new>
+using std::setprecision;
+#ifndef WIN32
+using std::set_new_handler;
+#endif
+#else
 #include <new.h>
+#endif
 
 static
 void
-dumpMF (const MultiFab& mf,
-        const char*     str)
+DumpFirstFab (const MultiFab& mf)
 {
+    assert(mf.length() > 1);
+
     if (ParallelDescriptor::IOProcessor())
     {
-	cout << str << endl;
-	ConstMultiFabIterator it(mf);
-	for (;it.isValid(); ++it)
-	{
-	    cout << it() << endl;
-	}
+        assert(mf.DistributionMap()[0] == ParallelDescriptor::MyProc());
+
+        cout << mf[0] << endl;
     }
-    if (!ParallelDescriptor::IOProcessor())
+}
+
+static
+void
+DoIt (MultiFab& mf)
+{
+    assert(mf.nComp() >= 2);
+
+    mf.setVal(0);
+    //
+    // Do loop just filling first component.
+    //
+    for (int i = 0; i < 10; i++)
     {
-	ConstMultiFabIterator it(mf);
-	for (;it.isValid(); ++it)
-	{
-	    cout << it() << endl;
-	}
+        for(MultiFabIterator mfi(mf); mfi.isValid(); ++mfi)
+        {
+            mfi().setVal(mfi.index()+1,0);
+        }
+
+        mf.FillBoundary(0,1);
+
+        DumpFirstFab(mf);
     }
-    ParallelDescriptor::Barrier();
+    //
+    // Do loop just filling second component.
+    //
+    for (int i = 0; i < 10; i++)
+    {
+        for(MultiFabIterator mfi(mf); mfi.isValid(); ++mfi)
+        {
+            mfi().setVal(mfi.index()+10,1);
+        }
+
+        mf.FillBoundary(1,1);
+
+        DumpFirstFab(mf);
+    }
+    //
+    // Do loop filling both components.
+    //
+    for (int i = 0; i < 10; i++)
+    {
+        for(MultiFabIterator mfi(mf); mfi.isValid(); ++mfi)
+        {
+            mfi().setVal(mfi.index()+100);
+        }
+
+        mf.FillBoundary();
+
+        DumpFirstFab(mf);
+    }
 }
 
 int
 main (int argc, char** argv)
 {
-    int nprocs = 1;
+#ifndef WIN32
+    set_new_handler(Utility::OutOfMemory);
+#endif
 
-    ParallelDescriptor::StartParallel(nprocs,&argc,&argv);
+    ParallelDescriptor::StartParallel(&argc,&argv);
 
     BoxList bl;
-    bl.append(Box(IntVect(0,0),IntVect(1,1)));
-    bl.append(Box(IntVect(0,2),IntVect(1,3)));
-    BoxArray bs(bl);
-    
-    MultiFab junk(bs, 1, 1, Fab_allocate);
 
-    for(MultiFabIterator mfi(junk); mfi.isValid(); ++mfi)
-	mfi().setVal(mfi.index()+1);
-    junk.FillBoundary();
-    dumpMF(junk,"after 1 time...");
+    Box bx(IntVect::TheZeroVector(),IntVect::TheUnitVector());
+
+    bl.append(bx);
+
+    bx.shift(0,2);
+
+    bl.append(bx);
+
+    BoxArray ba(bl);
     
-    for(MultiFabIterator mfi(junk); mfi.isValid(); ++mfi)
-	mfi().setVal(mfi.index()+5);
-    junk.FillBoundary();
-    dumpMF(junk,"after 2 times...");
+    MultiFab junk(ba,2,1);
+
+    DoIt(junk);
+
+    MultiFab junky(ba,2,1);
+
+    DoIt(junky);
     
     ParallelDescriptor::EndParallel();
+
+    return 0;
 }
