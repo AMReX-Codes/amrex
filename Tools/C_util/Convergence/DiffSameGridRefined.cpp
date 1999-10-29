084127e37bfcf2cf2e1065f216514807d5763669
sstanley <sstanley>
1999-10-29 16:36:03 +0000

    Small modifications to DiffSameGrid.cpp (all documentation) and added the routine DiffSameGridRefined.cpp which does the same thing as DiffSameGrid.cpp but allows the two plotfiles to have different levels of refinement, as long as they have the same physical region refined.

--- Tools/C_util/Convergence/DiffSameGridRefined.cpp
+++ Tools/C_util/Convergence/DiffSameGridRefined.cpp
@@ -1,7 +1,7 @@
 //BL_COPYRIGHT_NOTICE
 
 //
-// $Id: DiffSameGrid.cpp,v 1.8 1999-10-29 16:36:03 sstanley Exp $
+// $Id: DiffSameGridRefined.cpp,v 1.1 1999-10-29 16:36:03 sstanley Exp $
 //
 
 #ifdef BL_USE_NEW_HFILES
@@ -31,6 +31,7 @@ using std::set_new_handler;
 #include "DataServices.H"
 #include "Utility.H"
 #include "VisMF.H"
+#include "AVGDOWN_F.H"
 
 #ifndef NDEBUG
 #include "TV_TempWrite.H"
@@ -49,13 +50,24 @@ static
 void
 PrintUsage (const char* progName)
 {
-    cout << 'This utility performs a diff operation between two'     << endl
-         << 'plotfiles which have the exact same grids.'             << endl;
+    cout << "This utility performs a diff operation between two"     << endl
+         << "plotfiles which have the same geometrical grid"         << endl
+         << "configurations but a factor of refinement between"      << endl
+         << "the grids from each plotfile at the same level."        << endl
+         << "For instance, it works to diff two plotfiles having"    << endl
+         << "  Plotfile 1: 25x25 base grid, Ref_Ratio = 2"           << endl
+         << "  Plotfile 2: 50x50 base grid, Ref_Ratio = 2"           << endl
+         << "Should also work for,"                                  << endl
+         << "  Plotfile 1: 25x25 base grid, Ref_Ratio = 2"           << endl
+         << "  Plotfile 2: 25x25 base grid, Ref_Ratio = 4"           << endl
+         << "In both cases, the geometrical region which is refined" << endl
+         << "must be the same.  So, this is generally good for"      << endl
+         << "comparing cases ran using a fixed grid file."           << endl;
     cout << '\n';
     cout << "Usage:" << '\n';
     cout << progName << '\n';
     cout << "    infile1 = inputFileName1" << '\n';
-    cout << "    infile2 = inputFileName2" << '\n';
+    cout << "    reffile = refinedPlotFile" << '\n';
     cout << "    diffile = differenceFileName" << '\n';
     cout << "              (If not specified no file is written)" << '\n';
     cout << "       norm = integer norm (Ie. default is 2 for L2 norm)" << '\n';
@@ -68,6 +80,12 @@ PrintUsage (const char* progName)
 bool
 amrDatasHaveSameDerives(const AmrData& amrd1,
 			const AmrData& amrd2);
+
+IntVect
+getRefRatio(const Box& crse,
+            const Box& fine);
+
+
 int
 main (int   argc,
       char* argv[])
@@ -96,15 +114,15 @@ main (int   argc,
     if (pp.contains("verbose"))
     {
         verbose = true;
-        AmrData::SetVerbose(true);
+//      AmrData::SetVerbose(true);
     }
     pp.query("infile1", iFile1);
     if (iFile1.isNull())
         BoxLib::Abort("You must specify `infile1'");
 
-    pp.query("infile2", iFile2);
+    pp.query("reffile", iFile2);
     if (iFile2.isNull())
-        BoxLib::Abort("You must specify `infile2'");
+        BoxLib::Abort("You must specify `reffile'");
 
     pp.query("diffile", difFile);
 
@@ -114,31 +132,31 @@ main (int   argc,
     DataServices::SetBatchMode();
     FileType fileType(NEWPLT);
     
-    DataServices dataServicesC(iFile1, fileType);
-    DataServices dataServicesF(iFile2, fileType);
+    DataServices dataServices1(iFile1, fileType);
+    DataServices dataServices2(iFile2, fileType);
 
-    if (!dataServicesC.AmrDataOk() || !dataServicesF.AmrDataOk())
+    if (!dataServices1.AmrDataOk() || !dataServices2.AmrDataOk())
         BoxLib::Abort("ERROR: Dataservices not OK");
 
 
     //
     // Generate AmrData Objects 
     //
-    AmrData& amrDataI = dataServicesC.AmrDataRef();
-    AmrData& amrDataE = dataServicesF.AmrDataRef();
+    AmrData& amrData1 = dataServices1.AmrDataRef();
+    AmrData& amrData2 = dataServices2.AmrDataRef();
 
     //
     // Initial Tests 
     //
-    if (!amrDatasHaveSameDerives(amrDataI,amrDataE))
+    if (!amrDatasHaveSameDerives(amrData1,amrData2))
         BoxLib::Abort("ERROR: Plotfiles do not have the same state variables");
 
-    if (amrDataI.FinestLevel() != amrDataE.FinestLevel())
+    if (amrData1.FinestLevel() != amrData2.FinestLevel())
         BoxLib::Abort("ERROR: Finest level is not the same in the two plotfiles");
 
-    int nComp       = amrDataI.NComp();
-    int finestLevel = amrDataI.FinestLevel();
-    const Array<aString>& derives = amrDataI.PlotVarNames();
+    int nComp       = amrData1.NComp();
+    int finestLevel = amrData1.FinestLevel();
+    const Array<aString>& derives = amrData1.PlotVarNames();
     Array<int> destComps(nComp);
     for (int i = 0; i < nComp; i++) 
         destComps[i] = i;
@@ -155,44 +173,90 @@ main (int   argc,
 
     for (int iLevel = 0; iLevel <= finestLevel; ++iLevel)
     {
-        const BoxArray& baI = amrDataI.boxArray(iLevel);
-        const BoxArray& baE = amrDataE.boxArray(iLevel);
+        //
+        // Construct level box array and check that the lengths are the same
+        //
+        const BoxArray& ba1 = amrData1.boxArray(iLevel);
+        const BoxArray& ba2 = amrData2.boxArray(iLevel);
 
-        if (baI.length() != baE.length())
+        if (ba1.length() != ba2.length())
         {
             cout << "ERROR: BoxArray lengths are not the same at level " 
                  << iLevel << endl;
             ParallelDescriptor::Abort();
         }
 
-	error[iLevel] = new MultiFab(baI, nComp, 0);
+        //
+        // Construct MultiFab for errors
+        //
+	error[iLevel] = new MultiFab(ba1, nComp, 0);
 	error[iLevel]->setVal(GARBAGE);
 
-        MultiFab dataI(baI, nComp, 0);
-        MultiFab dataE(baE, nComp, 0);
-
-        amrDataI.FillVar(dataI, iLevel, derives, destComps);
-        amrDataE.FillVar(dataE, iLevel, derives, destComps);
-
-        (*error[iLevel]).copy(dataI);
-        (*error[iLevel]).minus(dataE, 0, nComp, 0);
-
-   
         //
-        // Output Statistics
+        // Construct refinement ratio, build the coarsened boxarray
+        // (amrData2 is the refined plotfile)
         //
-        if (ParallelDescriptor::IOProcessor())
-            cout << "  " << iLevel << "    ";
+        const Box& domain1     = amrData1.ProbDomain()[iLevel];
+        const Box& domain2     = amrData2.ProbDomain()[iLevel];
+        IntVect refine_ratio   = getRefRatio(domain1, domain2);
+        if (refine_ratio == IntVect())
+            BoxLib::Error("Cannot find refinement ratio from data to exact");
 
+        if (verbose)
+            cerr << "level = " << iLevel << "  Ref_Ratio = " << refine_ratio
+                                                             << endl;
+
+        BoxArray ba2Coarse(ba2);
+        ba2Coarse.coarsen(refine_ratio);
+
+        //
+        // For each component, average the fine fields down and calculate
+        // the errors
+        //
         Array<Real> norms(nComp);
         for (int iComp = 0; iComp < nComp; iComp++)
             norms[iComp] = 0.0;
 
-        for (MultiFabIterator mfi(*error[iLevel]); mfi.isValid(); ++mfi)
+        for (int iComp = 0; iComp < nComp; ++iComp)
         {
-            for (int iComp = 0; iComp < nComp; iComp++)
+            MultiFab& data1 = amrData1.GetGrids(iLevel, iComp);
+            MultiFab& data2Fine = amrData2.GetGrids(iLevel, iComp);
+
+
+            //
+            // Calculate the errors  for each FAB in the MultiFab
+            //
+            for (MultiFabIterator mfi1(data1); mfi1.isValid(); ++mfi1)
             {
-                Real grdL2 = mfi().norm(norm, iComp, 1);
+                DependentMultiFabIterator mfi2Fine(mfi1, data2Fine);
+                DependentMultiFabIterator errMfi(mfi1, *error[iLevel]);
+    
+                //
+                // Create the Coarsened version of data2
+                //
+                FArrayBox data2Coarse(ba2Coarse[mfi1.index()], 1);
+                int ncCoarse = data2Coarse.nComp();
+
+                FORT_CV_AVGDOWN(data2Coarse.dataPtr(),
+                                  ARLIM(data2Coarse.loVect()),
+                                  ARLIM(data2Coarse.hiVect()),
+                                &ncCoarse,
+                                mfi2Fine().dataPtr(),
+                                  ARLIM(mfi2Fine().loVect()),
+                                  ARLIM(mfi2Fine().hiVect()), 
+                                ba2Coarse[mfi1.index()].loVect(), 
+                                ba2Coarse[mfi1.index()].hiVect(),
+                                refine_ratio.getVect());
+
+
+                //
+                // Calculate the errors on this FAB for this component
+                //
+                errMfi().copy(mfi1(), 0, iComp, 1);
+                errMfi().minus(data2Coarse, 0, iComp, 1);
+
+   
+                Real grdL2 = errMfi().norm(norm, iComp, 1);
 
                 if (norm != 0)
                 {
@@ -202,9 +266,15 @@ main (int   argc,
                 {
                     norms[iComp] = Max(norms[iComp], grdL2);
                 }
-                
             }
         }
+
+
+        //
+        // Output Statistics
+        //
+        if (ParallelDescriptor::IOProcessor())
+            cout << "  " << iLevel << "    ";
 
 
 #ifdef BL_USE_MPI
@@ -249,7 +319,7 @@ main (int   argc,
 
         Real vol = 1.0;
         for (int dir = 0; dir < BL_SPACEDIM; dir++)
-            vol *= amrDataI.DxLevel()[iLevel][dir];
+            vol *= amrData1.DxLevel()[iLevel][dir];
 
         if (ParallelDescriptor::IOProcessor())
         {
@@ -269,7 +339,7 @@ main (int   argc,
 
 
     if (!difFile.isNull())
-        WritePlotFile(error, amrDataI, difFile, verbose);
+        WritePlotFile(error, amrData1, difFile, verbose);
     
     for (int iLevel = 0; iLevel <= finestLevel; ++iLevel)
 	delete error[iLevel];
@@ -294,5 +364,34 @@ amrDatasHaveSameDerives(const AmrData& amrd1,
 	if (derives1[i] != derives2[i])
 	    return false;
     return true;
+}
+
+
+IntVect
+getRefRatio(const Box& crse,
+            const Box& fine)
+{
+    // Compute refinement ratio between crse and fine boxes, return invalid
+    // IntVect if there is none suitable
+    ParmParse pp("");
+    Array<int> rr_in(BL_SPACEDIM,-1);
+    int Nrr = pp.countval("ref_ratio",Nrr);
+    BL_ASSERT(Nrr==0 || Nrr==BL_SPACEDIM || Nrr==1);
+    if (Nrr>0)
+    {
+        pp.queryarr("ref_ratio",rr_in,0,Nrr);
+        return IntVect(rr_in);
+    }
+
+    IntVect ref_ratio;
+    for (int i=0; i<BL_SPACEDIM; ++i)
+        ref_ratio[i] = fine.length(i) / crse.length(i);
+
+    // Check results
+    Box test1 = ::Box(fine).coarsen(ref_ratio);
+    Box test2 = ::Box(test1).refine(ref_ratio);
+    if (test1 != crse  ||  test2 != fine)
+        ref_ratio = IntVect();
+    return ref_ratio;
 }
 
