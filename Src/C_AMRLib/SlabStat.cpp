eeca8b87a3fd2380b45beed2cc359ac129ee17f1
lijewski <lijewski>
2000-03-29 21:42:38 +0000

    Now can specify boxes via ParmParse.

--- Src/C_AMRLib/SlabStat.cpp
+++ Src/C_AMRLib/SlabStat.cpp
@@ -1,10 +1,11 @@
 //BL_COPYRIGHT_NOTICE
 
 //
-// $Id: SlabStat.cpp,v 1.4 2000-03-01 21:58:51 lijewski Exp $
+// $Id: SlabStat.cpp,v 1.5 2000-03-29 21:42:38 lijewski Exp $
 //
 
 #include <AmrLevel.H>
+#include <ParmParse.H>
 #include <SlabStat.H>
 
 SlabStatRec::SlabStatRec (const aString&  name,
@@ -26,6 +27,115 @@ SlabStatRec::SlabStatRec (const aString&  name,
     m_tmp_mf(m_boxes,m_vars.length(),m_ngrow),
     m_interval(0)
 {
+    m_mf.setVal(0);
+}
+
+//
+// This is a helper function for the following constructor.
+//
+// It opens "file" and looks for the Boxes on which the statistic
+// "name" is to be calculated.
+//
+// The structure of the file:
+//
+//   #
+//   # Lines beginning with a pound sign are comments.
+//   #
+//   default:
+//     N
+//     b1
+//     b2
+//     ...
+//     bN
+//   name:
+//     M
+//     b1
+//     b2
+//     ...
+//     bM
+//
+
+static
+BoxArray
+Boxes (const aString& file,
+       const aString& name)
+{
+    const aString TheDflt = "default:";
+    const aString TheName = name + ":";
+
+    ifstream is(file.c_str(),ios::in);
+
+    if (!is.good())
+        Utility::FileOpenFailed(file);
+
+#define STRIP while( is.get() != '\n' )
+
+    BoxArray ba_dflt;
+    BoxArray ba_name;
+
+    aString line;
+
+    while (line.getline(is))
+    {
+        if (line.isNull() || line[0] == '#') continue;
+
+        if (line == TheDflt || line == TheName)
+        {
+            bool dflt = (line == TheDflt) ? true : false;
+
+            int N; Box bx; BoxList bl;
+
+            is >> N; STRIP;
+
+            BL_ASSERT(N > 0);
+
+            for (int i = 0; i < N; i++)
+            {
+                is >> bx; STRIP; bl.append(bx);
+            }
+
+            (dflt ? ba_dflt : ba_name) = BoxArray(bl);
+        }
+    }
+
+    is.close();
+
+    if (!ba_dflt.ready() && !ba_name.ready())
+        BoxLib::Abort("slabstats.boxes doesn't have appropriate structure");
+
+    return ba_name.ready() ? ba_name : ba_dflt;
+
+#undef STRIP
+}
+
+SlabStatRec::SlabStatRec (const aString&  name,
+                          int             ncomp,
+                          Array<aString>& vars,
+                          int             ngrow,
+                          int             level,
+                          SlabStatFunc    func)
+    :
+    m_name(name),
+    m_ncomp(ncomp),
+    m_vars(vars),
+    m_ngrow(ngrow),
+    m_level(level),
+    m_func(func),
+    m_interval(0)
+{
+    ParmParse pp("slabstat");
+
+    aString file;
+
+    if (!pp.query("boxes", file))
+        BoxLib::Abort("SlabStatRec: slabstats.boxes isn't defined");
+
+    m_boxes = Boxes(file, m_name);
+
+    m_mf.define(m_boxes, m_ncomp, 0, Fab_allocate);
+
+    m_tmp_mf.define(m_boxes, m_vars.length(), m_ngrow, Fab_allocate);
+
     m_mf.setVal(0);
 }
 
