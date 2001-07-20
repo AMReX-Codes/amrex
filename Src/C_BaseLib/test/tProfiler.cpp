960659929b14cfe5657d5f95f90e1a2f05198709
car <car>
2001-07-20 19:37:36 +0000

    *** empty log message ***

---
+++ Src/C_BaseLib/test/tProfiler.cpp
@@ -0,0 +1,43 @@
+#include <Thread.H>
+#include <Profiler.H>
+
+namespace BoxLib3
+{
+namespace testing
+{
+void profiler_main(int& argc, char**& argv);
+}
+}
+
+extern "C"
+void* test_profiler_routine(void*)
+{
+    BL_PROFILE("tp_routine");
+    // BoxLib3::Thread::sleep(1.0);
+    return 0;
+}
+
+namespace
+{
+void
+thread_timing()
+{
+    BL_PROFILE("a_thread_timing()");
+    FunctionThread ft(test_profiler_routine);
+    FunctionThread ft1(test_profiler_routine);
+    test_profiler_routine(0);
+    ft.join();
+    ft1.join();
+}
+}
+
+void
+BoxLib3::testing::profiler_main(int&, char**& argv)
+{
+    BL_PROFILE("BoxLib3::testing::profiler_main()");
+    BoxLib::WallTimer wt;
+    wt.start();
+    wt.stop();
+    //  std::cout << "Wall timer reports = " << wt << std::endl;
+    thread_timing();
+}
