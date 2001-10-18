b37f4962c6a3de9a7a5a570f93eebe3d05e0bfd0
lijewski <lijewski>
2001-10-18 17:55:31 +0000

    some thread fixes

---
+++ Src/C_BaseLib/BaseFab.cpp
@@ -0,0 +1,43 @@
+
+#include <winstd.H>
+
+#include <BaseFab.H>
+#include <BArena.H>
+#include <CArena.H>
+#include <Thread.H>
+
+int BoxLib::BF_init::m_cnt = 0;
+
+static ThreadSpecificData<Arena>* arena = 0;
+
+BoxLib::BF_init::BF_init ()
+{
+    if (m_cnt++ == 0)
+        arena = new ThreadSpecificData<Arena>;
+}
+
+BoxLib::BF_init::~BF_init ()
+{
+    if (--m_cnt == 0)
+        delete arena;
+}
+
+Arena*
+BoxLib::The_Arena ()
+{
+    BL_ASSERT(arena != 0);
+
+    Arena* a = arena->get();
+
+    if (a == 0)
+    {
+#if defined(BL_THREADS) || defined(BL_COALESCE_FABS)
+        arena->set(a = new CArena);
+#else
+        arena->set(a = new BArena);
+#endif
+    }
+
+    return a;
+}
+
