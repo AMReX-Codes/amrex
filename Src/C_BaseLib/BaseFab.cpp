16813e44d01096e385aed62a4052662111b1bd1d
lijewski <lijewski>
2001-10-23 17:01:02 +0000

    added BoxLib::ResetArena()

--- Src/C_BaseLib/BaseFab.cpp
+++ Src/C_BaseLib/BaseFab.cpp
@@ -23,6 +23,20 @@ BoxLib::BF_init::~BF_init ()
 }
 
 Arena*
+BoxLib::ResetArena (Arena* newarena)
+{
+    BL_ASSERT(newarena != 0);
+
+    Arena* oldarena = arena->get();
+
+    BL_ASSERT(oldarena != 0);
+
+    arena->set(newarena);
+
+    return oldarena;
+}
+
+Arena*
 BoxLib::The_Arena ()
 {
     BL_ASSERT(arena != 0);
