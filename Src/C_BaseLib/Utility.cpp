df06e6982db379987476cd6ae951536e062aba12
car <car>
2002-02-06 18:14:46 +0000

    Fixed random number seeding.

--- Src/C_BaseLib/Utility.cpp
+++ Src/C_BaseLib/Utility.cpp
@@ -1,5 +1,5 @@
 //
-// $Id: Utility.cpp,v 1.57 2001-11-26 18:14:26 car Exp $
+// $Id: Utility.cpp,v 1.58 2002-02-06 18:14:46 car Exp $
 //
 
 #include <cstdlib>
@@ -615,6 +615,7 @@ inline unsigned long TEMPERING_SHIFT_L(unsigned long y) { return y >> 18L; }
 void
 BoxLib::mt19937::sgenrand(unsigned long seed)
 {
+#ifdef BL_MERSENNE_ORIGINAL_INIT
     for ( int i = 0; i < N; ++i )
     {
 	mt[i] = seed & 0xFFFF0000UL;
@@ -622,7 +623,48 @@ BoxLib::mt19937::sgenrand(unsigned long seed)
 	mt[i] |= (seed& 0xFFFF0000UL) >> 16;
 	seed = 69069U*seed + 1;
     }
+#else
+    mt[0]= seed & 0xffffffffUL;
+    for ( mti=1; mti<N; mti++ ) 
+    {
+        mt[mti] = (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30L)) + mti); 
+        /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+        /* In the previous versions, MSBs of the seed affect   */
+        /* only MSBs of the array mt[].                        */
+        /* 2002/01/09 modified by Makoto Matsumoto             */
+        mt[mti] &= 0xffffffffUL;       /* for >32 bit machines */
+    }
+#endif
     mti = N;
+}
+
+/* initialize by an array with array-length */
+/* init_key is the array for initializing keys */
+/* key_length is its length */
+void 
+BoxLib::mt19937::sgenrand(unsigned long init_key[], int key_length)
+{
+    int i, j, k;
+    sgenrand(19650218UL);
+    i=1; j=0;
+    k = (N>key_length ? N : key_length);
+    for ( ; k; k-- ) 
+    {
+        mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL)) + init_key[j] + j; /* non linear */
+        mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+        i++; j++;
+        if (i>=N) { mt[0] = mt[N-1]; i=1; }
+        if (j>=key_length) j=0;
+    }
+    for ( k=N-1; k; k-- ) 
+    {
+        mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL)) - i; /* non linear */
+        mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+        i++;
+        if (i>=N) { mt[0] = mt[N-1]; i=1; }
+    }
+
+    mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
 }
 
 void
@@ -635,15 +677,15 @@ BoxLib::mt19937::reload()
     for ( kk=0; kk<N-M; kk++ )
     {
 	y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-	mt[kk] = mt[kk+M] ^ (y >> 1L) ^ mag01[y & 0x1];
+	mt[kk] = mt[kk+M] ^ (y >> 1L) ^ mag01[y & 0x1UL];
     }
     for ( ; kk<N-1; kk++ )
     {
 	y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-	mt[kk] = mt[kk+(M-N)] ^ (y >> 1L) ^ mag01[y & 0x1];
+	mt[kk] = mt[kk+(M-N)] ^ (y >> 1L) ^ mag01[y & 0x1UL];
     }
     y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-    mt[N-1] = mt[M-1] ^ (y >> 1L) ^ mag01[y & 0x1];
+    mt[N-1] = mt[M-1] ^ (y >> 1L) ^ mag01[y & 0x1UL];
 
     mti = 0;
 }
@@ -667,6 +709,11 @@ BoxLib::mt19937::mt19937(unsigned long seed)
     : init_seed(seed), mti(N+1)
 {
     sgenrand(seed);
+}
+
+BoxLib::mt19937::mt19937 (unsigned long seed_array[], int len)
+{
+    sgenrand(seed_array, len);
 }
 
 void
