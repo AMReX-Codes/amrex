1104c57200cc834c4b5681371ca0785690a8ca3f
car <car>
2002-06-17 17:00:40 +0000

    *** empty log message ***

---
+++ Src/C_BaseLib/BLParmParse_F.f
@@ -0,0 +1,81 @@
+c-----------------------------------------------------------------------
+      SUBROUTINE bl_pp_new(ipp, str)
+      INTEGER ipp
+	CHARACTER*(*) str
+	INTEGER NSTR
+	PARAMETER (NSTR = 128)
+	CALL blstr2int(istr, NSTR, str)
+	CALL bl_pp_new_cpp(ipp, istr, NSTR)
+	END
+c-----------------------------------------------------------------------
+      SUBROUTINE bl_pp_get_int(ierr, ipp, str, ival)
+      INTEGER ierr, ipp
+	INTEGER ival
+      CHARACTER*(*) str
+      INTEGER NSTR
+      PARAMETER (NSTR = 128)
+      INTEGER istr(NSTR)
+C
+      CALL blstr2int(istr, NSTR, str)
+      CALL bl_pp_get_int_cpp(ierr, ipp, istr, NSTR, ival)
+      END
+c-----------------------------------------------------------------------
+      SUBROUTINE bl_pp_get_logical(ierr, ipp, str, lval)
+      INTEGER ierr, ipp
+	LOGICAL lval
+      CHARACTER*(*) str
+      INTEGER NSTR
+      PARAMETER (NSTR = 128)
+      INTEGER istr(NSTR)
+	INTEGER ival
+C
+      CALL blstr2int(istr, NSTR, str)
+      CALL bl_pp_get_logical_cpp(ierr, ipp, istr, NSTR, ival)
+	IF ( IERR .NE. 0 ) THEN
+	    IF ( ival .NE. 0 ) THEN
+	        lval = .TRUE.
+	    ELSE
+	        lval = .FALSE.
+	    END IF
+	END IF
+      END
+c-----------------------------------------------------------------------
+      SUBROUTINE bl_pp_get_real(ierr, ipp, str, rval)
+      INTEGER ierr, ipp
+	REAL rval
+      CHARACTER*(*) str
+      INTEGER NSTR
+      PARAMETER (NSTR = 128)
+      INTEGER istr(NSTR)
+C
+      CALL blstr2int(istr, NSTR, str)
+      CALL bl_pp_get_real_cpp(ierr, ipp, istr, NSTR, rval)
+      END
+c-----------------------------------------------------------------------
+      SUBROUTINE bl_pp_get_double(ierr, ipp, str, dval)
+      INTEGER ierr, ipp
+	DOUBLE PRECISION dval
+      CHARACTER*(*) str
+      INTEGER NSTR
+      PARAMETER (NSTR = 128)
+      INTEGER istr(NSTR)
+C
+      CALL blstr2int(istr, NSTR, str)
+      CALL bl_pp_get_double_cpp(ierr, ipp, istr, NSTR, dval)
+      END
+c-----------------------------------------------------------------------
+      SUBROUTINE bl_pp_get_string(ierr, ipp, str, ostr)
+      INTEGER ierr, ipp
+	CHARACTER*(*) ostr
+      CHARACTER*(*) str
+      INTEGER NSTR
+      PARAMETER (NSTR = 128)
+      INTEGER istr(NSTR)
+	INTEGER iostr(NSTR)
+C
+      CALL blstr2int(istr, NSTR, str)
+	CALL bl_pp_get_string_cpp(ierr, ipp, istr, NSTR, ostr, NSTR)
+	IF ( ierr ) THEN
+	    CALL blint2str(ostr, iostr, NSTR)
+      END IF
+      END
---
+++ Src/C_BaseLib/BLutil_F.f
@@ -0,0 +1,51 @@
+C=======================================================================
+C
+C     String handling routines:
+C     Strings are handled differently in C++ and in FORTRAN.  In order
+C     to simplify the framework strings are passed from FORTRAN to C++
+C     as arrays of integer characters, terminated by the EOS symbol
+C     which we set to -1
+C     blstr2int converts a FORTRAN string to an integer array,
+C     blint2str converts an integer array to a FORTRAN string.
+C      
+C-----------------------------------------------------------------------
+      SUBROUTINE blstr2int(iarr, n, str)
+      CHARACTER*(*) str
+      INTEGER n, i, j
+      INTEGER iarr(n)
+      INTEGER EOS
+      PARAMETER (EOS=-1)
+C
+      IF ( n .LE. len(str) ) THEN
+         STOP "blstr2int: str to large for iarr"
+      END IF
+C     Make sure that IARR is empty
+      DO J = 1, N
+         iarr(J) = 0
+      END DO
+      j = 1
+      DO i = 1, len(str)
+         iarr(j) = ichar(str(i:i))
+         j = j + 1
+      END DO
+C     EOS
+      iarr(j) = EOS
+C
+      END
+C-----------------------------------------------------------------------
+      SUBROUTINE blint2str(str, iarr, n)
+      CHARACTER*(*) str
+      INTEGER n
+      INTEGER iarr(n)
+      INTEGER EOS
+      PARAMETER (EOS=-1)
+      INTEGER i
+C
+      DO i = 1, n
+	   IF ( i .GT. LEN(str) ) STOP "blint2str: iarr to large for str"
+         IF ( iarr(i) .EQ. EOS ) GO TO 100
+         str(i:i) = char(iarr(i))
+      END DO
+ 100  CONTINUE
+C
+      END
