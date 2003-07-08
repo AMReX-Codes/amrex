8df5e30497bb73be236b1e1239f8ca2ed30c4edb
car <car>
2003-07-08 20:48:23 +0000

    cleanup

--- Src/LinearSolvers/C_TensorMG/visc2d.m
+++ Src/LinearSolvers/C_TensorMG/visc2d.m
@@ -1,33 +1,23 @@
 (* definition of tau *)
 
 tauxxdef = tauxx[i_ + half, j_] -> 2 mu[i + half, j] dudx[i + half, j]
-
 tauyydef = tauyy[i_, j_ + half] -> 2 mu[i, j + half] dvdy[i, j + half]
-
 tauxydef = tauxy[i_, j_] -> mu[i, j] (dudy[i, j] + dvdx[i, j])
 
 (* definitions of derivatives *)
 
 dudydef1 = dudy[i_, j_ + half] -> (u[i, j + 1] - u[i, j])/hy
-
 dvdxdef1 = dvdx[i_ + half, j_] -> (v[i + 1, j] - v[i, j])/hx
-
 dudydef2 = dudy[i_ + half, j_] -> (u[i, j + 1] - u[i, j - 1] + u[i + 1, j + 1] - u[i + 1, j - 1])/(4 hy)
-
 dvdxdef2 = dvdx[i_, j_ + half] -> (v[i + 1, j + 1] - v[i - 1, j + 1] + v[i + 1, j] - v[i - 1, j])/(4 hx)
-
 dudxdef  = dudx[i_ + half, j_] -> (u[i + 1, j] - u[i, j])/hx
-
 dvdydef  = dvdy[i_, j_ + half] -> (v[i, j + 1] - v[i, j])/hy
 
 (* definitions used for fortran output *)
 
 murepl1 = mu[i_, j_ + half] -> muY[i, j + 1]
-
 murepl2 = mu[i_ + half, j_] -> muX[i + 1, j]
-
 urepl = u[i_, j_] -> U[i, j, 1]
-
 vrepl = v[i_, j_] -> U[i, j, 2]
 
 (* 
@@ -45,7 +35,7 @@ the mask array.
 
 Clear[dependentCellsNotCovered]
 dependentCellsNotCovered[exp_, logfunc_, maskfun_] := 
-  Module[{cond, lexp}, cond = False; lexp = exp; 
+  Module[{cond = False, lexp = exp, il, jl, kl}, 
     For[il = -2, il <= +2, il++, 
       For[jl = -2, jl <= +2, jl++, 
         For[kl = 1, kl <= 2, kl++, 
@@ -63,26 +53,13 @@ one or more of the cells were not covered.
 
 Clear[dependentCellsCovered]
 dependentCellsCovered[exp_, logfunc_, maskfun_] := 
-  Module[{cond, lexp}, cond = True; lexp = exp; 
+  Module[{cond = True, lexp = exp, il, jl, kl}, 
     For[il = -2, il <= +2, il++, 
       For[jl = -2, jl <= +2, jl++, 
         For[kl = 1, kl <= 2, kl++, 
           If[logfunc[il, jl] && 
               Coefficient[Expand[exp], U[i + il, j + jl, kl]] =!= 0, 
             cond = cond && maskfun[i + il, j + jl] == 0]]]]; cond]
-(*
-this is an alternate definition which treats cells with mask .eq. 1 as
-untrusted, rather than mask .gt. 0
-*)
-
-dependentCellsNotCoveredalt[exp_, logfunc_, maskfun_] := 
-  Module[{cond, lexp}, cond = False; lexp = exp; 
-    For[il = -2, il <= +2, il++, 
-      For[jl = -2, jl <= +2, jl++, 
-        For[kl = 1, kl <= 2, kl++, 
-          If[logfunc[il, jl] && 
-              Coefficient[Expand[exp], U[i + il, j + jl, kl]] =!= 0, 
-            cond = cond || maskfun[i + il, j + jl] == 1]]]]; cond]
 
 (* 
 definitions for Do One-sided Derivative in X direction.  if sign is
@@ -91,11 +68,8 @@ if negative, extend in other direction
 *)
 
 DOneX[u_, i_, j_, sign_] := (-u[i + 2, j] + 4*u[i + 1, j] - 3*u[i, j])/(2*hx) /; sign == 1
-
 DOneX[u_, i_, j_, sign_] := (u[i - 2, j] - 4*u[i - 1, j] + 3*u[i, j])/(2*hx) /; sign == -1
-
 DOneX[u_, i_, j_, k_, sign_] := (-u[i + 2, j, k] + 4*u[i + 1, j, k] - 3*u[i, j, k])/(2*hx) /; sign == 1
-
 DOneX[u_, i_, j_, k_, sign_] := (u[i - 2, j, k] - 4*u[i - 1, j, k] + 3*u[i, j, k])/(2*hx) /; sign == -1
 
 (*
