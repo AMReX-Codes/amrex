098db076c129dc9f0bb60e5157721550b029d93e
car <car>
2004-05-29 16:05:34 +0000

    *** empty log message ***

--- Src/F_BaseLib/test/main.f90
+++ Src/F_BaseLib/test/main.f90
@@ -8,10 +8,10 @@ program main
 ! call t_plotfile
 ! call t_ba
 ! call t_cluster
-call t_mf_fabio
+! call t_mf_fabio
 ! call t_mf
 ! call t_fabio
-! call t_domain
+call t_domain
 ! call t_box_chop
 ! call t_box_mod
 ! call t_boxassoc
