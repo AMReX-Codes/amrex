05598a4b5e3ddf378d2818d3ba31b4c1849ac841
car <car>
2004-06-22 16:42:05 +0000

    *** empty log message ***

--- Tools/F_scripts/coco/coco.pl
+++ Tools/F_scripts/coco/coco.pl
@@ -5,8 +5,10 @@ use Getopt::Long;
 
 my $noglob;
 BEGIN {
-    eval "use File::Glob";
-    if ($@) { $noglob =1 }
+  eval "use File::Glob";
+  if ($@) {
+    $noglob =1;
+  }
 }
 
 my $dirsep = ":";
@@ -23,37 +25,38 @@ my $opt_help;
 my $the_outfile = "-";
 
 GetOptions(
-   "help"     => \$opt_help,
-   "debug"    => \$debug,
-   "output=s" => \$the_outfile,
-) or die $usage;
+	   "help"     => \$opt_help,
+	   "debug"    => \$debug,
+	   "output=s" => \$the_outfile,
+	  ) or die $usage;
 
-open( OUT, ">$the_outfile")
-  || die "Couldn't write to \"$the_outfile\": $!.\n";
+open( OUT, ">$the_outfile") || die "Couldn't write to \"$the_outfile\": $!.\n";
 
 unless ( $noglob && ! $is_win32 ) {
-    @ARGV = map {
-	    my @g = File::Glob::glob($_) if /[*?]/;
-	    @g ? @g : $_;
-	} @ARGV;
+  @ARGV = map {
+    my @g = File::Glob::glob($_) if /[*?]/;
+    @g ? @g : $_;
+  } @ARGV;
 }
 
-push @ARGV, "-";
+if ( @ARGV == 0 ) {
+  push @ARGV, "-";
+}
 
 foreach my $file (@ARGV) {
-    open(FILE, $file) || die "Can't open $file: $!\n";
-    my $line;
-    while ( defined($line=<FILE>) ) {
-      chomp $line;
-      if ( $line =~ /^??/ ) {
-	$line =~ s/!.*$/ /;
-	$line =~ s/ *$//;
-	if ( $line =~ s/&$/ / ) {
-	  $line .= <FILE>;
-	  redo unless eof(FILE);
-	}
-	print OUT "line = \"$line\"\n";
+  open(FILE, $file) || die "Can't open $file: $!\n";
+  my $line;
+  while ( defined($line=<FILE>) ) {
+    chomp $line;
+    if ( $line =~ /^\?\?/ ) {
+      $line =~ s/!.*$/ /;
+      $line =~ s/ *$//;
+      if ( $line =~ s/&$/ / ) {
+	$line .= <FILE>;
+	redo unless eof(FILE);
       }
+      print OUT "line = \"$line\"\n";
     }
-    close(FILE);
+  }
+  close(FILE);
 }
