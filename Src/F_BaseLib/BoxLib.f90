dcdceecd14df0b1c0e4d3561ae4a78df5cad8c4c
car <car>
2004-06-28 16:00:30 +0000

    minor fixes

--- Src/F_BaseLib/BoxLib.f90
+++ Src/F_BaseLib/BoxLib.f90
@@ -5,15 +5,14 @@
 !! some basic types and parameter.
 module BoxLib
 
-  use bl_error_module
-  use bl_space
-  use bl_types
   use parallel
 
   implicit none
 
-  !! The default dump unint
-  integer, private :: dunit = -1
+  private
+
+  public :: boxlib_initialize
+  public :: boxlib_finalize
 
 contains
 
@@ -28,32 +27,5 @@ contains
   subroutine boxlib_finalize()
     call parallel_finalize
   end subroutine boxlib_finalize
-
-  !! Returns the dump unit.
-  function boxlib_dunit() result(r)
-    integer :: r
-    r = dunit
-  end function boxlib_dunit
-
-  !! Sets the dump unit.
-  subroutine boxlib_set_dunit(unit)
-    integer, intent(in) :: unit
-    dunit = unit
-  end subroutine boxlib_set_dunit
-
-  !! Sets the Dump outfile.  Without setting this file
-  subroutine boxlib_open_dfile(str)
-    use bl_IO_module
-    character(len=*) str
-    dunit = unit_new()
-    open(unit=dunit, file = str, status='replace', action = 'write')
-  end subroutine boxlib_open_dfile
-
-  !! Closes the dump unit.  Will no longer be available for IO.
-  subroutine boxlib_close_dfile()
-    use bl_IO_module
-    close(unit=dunit)
-    dunit = -1
-  end subroutine boxlib_close_dfile
 
 end module BoxLib
