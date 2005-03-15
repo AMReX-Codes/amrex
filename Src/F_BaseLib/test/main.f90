347af29371a229892b5b99ba22644c7e0f837b00
car <car>
2005-03-15 19:14:00 +0000

    think i've fixed ann's checkpoints

--- Src/F_BaseLib/test/main.f90
+++ Src/F_BaseLib/test/main.f90
@@ -8,7 +8,7 @@ program main
 ! call t_pingpong
 ! call t_plotfile
 ! call t_ba
-  call t_bx
+! call t_bx
 ! call t_cluster
 ! call t_mf_fabio
 ! call t_mf
@@ -20,7 +20,7 @@ program main
 ! call t_mt_random_numbers
 ! call t_box_conn
 ! call t_boxarray
-! call t_nodal_mf_fabio
+call t_nodal_mf_fabio
 ! call t_timer
 ! call t_knap
 ! call t_ml_mf_read()
