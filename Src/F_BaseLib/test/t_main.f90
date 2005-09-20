8318ae5aa8cfe06c31934e475358b9d62c1d78a2
car <car>
2005-09-20 17:22:56 +0000

    *** empty log message ***

--- Src/F_BaseLib/test/t_main.f90
+++ Src/F_BaseLib/test/t_main.f90
@@ -94,53 +94,6 @@ subroutine t_fabio
   
 end subroutine t_fabio
 
-subroutine t_boxassoc
-
-  use layout_module
-  type(boxassoc) :: bxasc
-  type(boxarray) :: ba
-  integer :: dm
-  integer :: ng
-  integer :: pd_xyz(MAX_SPACEDIM)
-  integer :: ba_maxsize
-  type(layout) :: la
-  logical :: nodal(MAX_SPACEDIM)
-  type(box) :: bxs(4)
-  logical pmask(MAX_SPACEDIM)
-  type(box) :: pd
-
-  dm = 2
-  ng = 1
-  pd_xyz     = 32
-  ba_maxsize = 16
-
-  nodal = .true.
-  nodal = .false.
-  pmask = .true.
-
-  if ( .false. ) then
-     bxs(1) = make_box((/0,0/),(/3,3/))
-     bxs(2) = make_box((/4,4/),(/7,7/))
-     bxs(3) = make_box((/0,8/),(/7,16/))
-     call build(ba, bxs(1:2))
-  else
-     call build(ba, make_box((/(0,i=1,dm)/), pd_xyz(1:dm)-1))
-     call boxarray_maxsize(ba, ba_maxsize)
-  end if
-
-  pd = bbox(ba)
-  call build(la, ba, pd = pd, pmask = pmask(1:dm))
-
-  call boxassoc_build(bxasc, la%lap, ng, nodal = nodal(1:dm))
-
-  call boxassoc_print(bxasc)
-
-  call boxassoc_destroy(bxasc)
-  call destroy(la)
-  call boxarray_destroy(ba)
-
-end subroutine t_boxassoc
-
 subroutine t_box_conn
   use ml_boxarray_module
   use box_util_module
