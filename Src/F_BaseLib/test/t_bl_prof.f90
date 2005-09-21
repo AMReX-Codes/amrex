02aa4c03911162aed4a5d58b732264569903ecd5
car <car>
2005-09-21 18:59:20 +0000

    *** empty log message ***

---
+++ Src/F_BaseLib/test/t_bl_prof.f90
@@ -0,0 +1,52 @@
+subroutine t_bl_prof
+  use bl_prof_module
+  implicit none
+  type(bl_prof_timer), save :: bpt
+  real :: a(100), b(100)
+  integer :: i
+
+  call bl_prof_initialize(on = .true.)
+  call build(bpt, "t_bl_prof")
+  b = (/(i,i=1,100)/)
+  call t()
+  a = sin(b)
+  call t1()
+  call destroy(bpt)
+  call bl_prof_glean("bl_prof_res")
+  call bl_prof_finalize
+
+contains
+
+  subroutine t
+    type(bl_prof_timer), save :: bpt
+    integer i
+    call build(bpt, "t", no_start = .true.)
+    call start(bpt)
+    do i = 1, 100
+       a = sin(b)
+       a = sin(b)
+    end do
+    call destroy(bpt)
+  end subroutine t
+
+  subroutine t1
+    type(bl_prof_timer), save :: bpt
+    integer i
+    call build(bpt, "t1", no_start = .true.)
+    call start(bpt)
+    do i = 1, 100*parallel_myproc()
+       a = sin(b)
+       a = sin(b)
+    end do
+    call destroy(bpt)
+  end subroutine t1
+
+  subroutine g
+    type(bl_prof_timer), save :: bpt
+    call build(bpt, "t")        ! (sic)
+    call start(bpt)
+    call stop(bpt)
+    call destroy(bpt)
+  end subroutine g
+
+end subroutine t_bl_prof
