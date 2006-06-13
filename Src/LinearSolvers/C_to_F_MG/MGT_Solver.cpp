00195651789346dda381e36b779fb53f5e88313e
car <car>
2006-06-13 16:46:20 +0000

    changes for c++/fortran

--- Src/LinearSolvers/C_to_F_MG/MGT_Solver.cpp
+++ Src/LinearSolvers/C_to_F_MG/MGT_Solver.cpp
@@ -23,6 +23,59 @@ mgt_get mgt_get_rh = mgt_get_rh_3d;
 mgt_set mgt_set_rh = mgt_set_rh_3d;
 #endif
   
+MGT_Solver::MGT_Solver(const BndryData& bd, const double* dx, 
+		       const std::vector<BoxArray>& grids,
+		       const std::vector<DistributionMapping>& dmap,
+		       bool nodal)
+  :
+  m_bd(bd), m_dmap(dmap), m_grids(grids), m_nodal(nodal)
+{
+  BL_ASSERT( m_grids.size() == dmap.size() );
+  m_nlevel = grids.size();
+  int dm = BL_SPACEDIM;
+  int i_nodal = (m_nodal)?1:0;
+  mgt_alloc(&m_mgt, &dm, &m_nlevel, &i_nodal);
+  for ( int i = 0; i < BL_SPACEDIM; ++i )
+    {
+      m_dx[i] = dx[i];
+    }
+  int nb = grids[0].size();
+  std::vector<int> lo(nb*dm);
+  std::vector<int> hi(nb*dm);
+  for ( int i = 0; i < nb; ++i )
+    {
+      for ( int j = 0; j < dm; ++j )
+	{
+	  lo[i + j*nb] = m_grids[0][i].smallEnd(j);
+	  hi[i + j*nb] = m_grids[0][i].bigEnd(j);
+	}
+    }
+  int pm[dm];
+  int bc[dm*2];
+  const Geometry& geom = bd.getGeom();
+  for ( int i = 0; i < dm; ++i ) 
+    {
+      pm[i] = geom.isPeriodic(i)? 1 : 0;
+      if ( pm[i] )
+	{
+	  bc[i*2 + 0] = 0;
+	  bc[i*2 + 1] = 0;
+	}
+      else
+	{
+	  // bc[i*2 + 0] = phys_bc.lo(i)==Outflow? MGT_BC_DIR : MGT_BC_NEU;
+	  // bc[i*2 + 1] = phys_bc.hi(i)==Outflow? MGT_BC_DIR : MGT_BC_NEU;
+	}
+    }
+  int lev = 0;
+  const Array<int>& pmap = dmap[0].ProcessorMap();
+  Box domain = bd.getDomain();
+  mgt_set_level(&m_mgt, &lev, &nb, &dm, &lo[0], &hi[0], 
+		domain.loVect(), domain.hiVect(), &bc[0], pm, &pmap[0]);
+  mgt_finalize(&m_mgt);
+}
+
+#if 0
 MGT_Solver::MGT_Solver(int nlevel, const BoxArray& ba, const Box& domain, const BCRec& phys_bc, const double* dx,
 		       const DistributionMapping& dmap, const Geometry& geom)
   : m_dm(BL_SPACEDIM),
@@ -72,26 +125,28 @@ MGT_Solver::MGT_Solver(int nlevel, const BoxArray& ba, const Box& domain, const 
       m_dx[i] = dx[i];
     }
 }
+#endif
 
 void 
-MGT_Solver::solve(MultiFab& uu, MultiFab& rh)
+MGT_Solver::solve(MultiFab* uu[], MultiFab* rh[])
 {
-  int lev = 1;
-  for (MFIter umfi(uu); umfi.isValid(); ++umfi)
+  for ( int i = 0; i < m_nlevel; ++i )
     {
-      const FArrayBox& rhs = rh[umfi];
-      const FArrayBox& sol = uu[umfi];
-      const Real* rd = rhs.dataPtr();
-      const Real* sd = sol.dataPtr();
-      int n = umfi.index() + 1; // Fortran Index Correction
-      const int* lo = umfi.validbox().loVect();
-      const int* hi = umfi.validbox().hiVect();
-      const int* plo = rhs.box().loVect();
-      const int* phi = rhs.box().hiVect();
-      const int* splo = sol.box().loVect();
-      const int* sphi = sol.box().hiVect();
-      mgt_set_rh(&m_mgt, &lev, &n, rd, plo, phi, lo, hi);
-      mgt_set_uu(&m_mgt, &lev, &n, sd, plo, phi, lo, hi);
+      int lev = i + 1;
+      for (MFIter umfi(*(uu[i])); umfi.isValid(); ++umfi)
+	{
+	  const FArrayBox& rhs = (*(rh[i]))[umfi];
+	  const FArrayBox& sol = (*(uu[i]))[umfi];
+	  const Real* rd = rhs.dataPtr();
+	  const Real* sd = sol.dataPtr();
+	  int n = umfi.index() + 1; // Fortran Index Correction
+	  const int* lo = umfi.validbox().loVect();
+	  const int* hi = umfi.validbox().hiVect();
+	  const int* plo = rhs.box().loVect();
+	  const int* phi = rhs.box().hiVect();
+	  mgt_set_rh(&m_mgt, &lev, &n, rd, plo, phi, lo, hi);
+	  mgt_set_uu(&m_mgt, &lev, &n, sd, plo, phi, lo, hi);
+	}
     }
   mgt_finalize(&m_mgt);
   double xa[BL_SPACEDIM], xb[BL_SPACEDIM];
@@ -103,16 +158,20 @@ MGT_Solver::solve(MultiFab& uu, MultiFab& rh)
     }
   mgt_finalize_stencil(&m_mgt, xa, xb, pxa, pxb);
   mgt_solve_cc(&m_mgt);
-  for (MFIter umfi(uu); umfi.isValid(); ++umfi)
+  for ( int i = 0; i < m_nlevel; ++i )
     {
-      FArrayBox& sol = uu[umfi];
-      Real* sd = sol.dataPtr();
-      int n = umfi.index() + 1; // Fortran Index Correction
-      const int* lo = umfi.validbox().loVect();
-      const int* hi = umfi.validbox().hiVect();
-      const int* plo = sol.box().loVect();
-      const int* phi = sol.box().hiVect();
-      mgt_get_uu(&m_mgt, &lev, &n, sd, plo, phi, lo, hi);
+      int lev = i + 1;
+      for (MFIter umfi(*(uu[i])); umfi.isValid(); ++umfi)
+	{
+	  FArrayBox& sol = (*(uu[i]))[umfi];
+	  Real* sd = sol.dataPtr();
+	  int n = umfi.index() + 1; // Fortran Index Correction
+	  const int* lo = umfi.validbox().loVect();
+	  const int* hi = umfi.validbox().hiVect();
+	  const int* plo = sol.box().loVect();
+	  const int* phi = sol.box().hiVect();
+	  mgt_get_uu(&m_mgt, &lev, &n, sd, plo, phi, lo, hi);
+	}
     }
 }
 
