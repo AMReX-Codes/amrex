4a41e4035435623d65a2f282599de27e3652a84c
lijewski <lijewski>
2006-09-12 20:01:16 +0000

    refinement to copy caching

--- Src/C_BaseLib/FabArray.cpp
+++ Src/C_BaseLib/FabArray.cpp
@@ -1,5 +1,7 @@
 #include <winstd.H>
 
+#include <map>
+
 #include <FabArray.H>
 
 FabArrayBase::FabArrayBase ()
@@ -139,7 +141,19 @@ CommDataCache::operator= (const Array<CommData>& rhs)
 // Stuff used for copy() caching.
 //
 
-CPC::CPC () {}
+CopyComTag::CopyComTag () {}
+
+CopyComTag::CopyComTag (const CopyComTag& cct)
+    :
+    box(cct.box),
+    fabIndex(cct.fabIndex),
+    srcIndex(cct.srcIndex)
+{}
+
+CPC::CPC ()
+    :
+    m_reused(false)
+{}
 
 CPC::CPC (const BoxArray&            dstba,
           const BoxArray&            srcba,
@@ -149,7 +163,8 @@ CPC::CPC (const BoxArray&            dstba,
     m_dstba(dstba),
     m_srcba(srcba),
     m_dstdm(dstdm),
-    m_srcdm(srcdm)
+    m_srcdm(srcdm),
+    m_reused(false)
 {}
 
 CPC::CPC (const CPC& rhs)
@@ -157,7 +172,8 @@ CPC::CPC (const CPC& rhs)
     m_dstba(rhs.m_dstba),
     m_srcba(rhs.m_srcba),
     m_dstdm(rhs.m_dstdm),
-    m_srcdm(rhs.m_srcdm)
+    m_srcdm(rhs.m_srcdm),
+    m_reused(rhs.m_reused)
 {}
 
 CPC::~CPC () {}
@@ -176,32 +192,77 @@ CPC::operator!= (const CPC& rhs) const
 {
     return !operator==(rhs);
 }
-    
-//
-// A useful typedef.
-//
-typedef std::list<CPC> CPCList;
 
-static CPCList TheCopyCache;
+typedef std::multimap<int,CPC> CPCCache;
+
+typedef CPCCache::iterator CPCCacheIter;
+
+static CPCCache TheCopyCache;
 
 CPC&
 CPC::TheCPC (const CPC& cpc, bool& got_from_cache)
 {
-    got_from_cache = false;
+    static bool first          = false;
+    static bool use_copy_cache = true;
 
-    for (CPCList::iterator it = TheCopyCache.begin(); it != TheCopyCache.end(); ++it)
+    if (!first)
     {
-        if (*it == cpc)
-        {
-            got_from_cache = true;
-
-            return *it;
-        }
+        first = true;
+        ParmParse pp("fabarray");
+        pp.query("use_copy_cache", use_copy_cache);
     }
 
-    TheCopyCache.push_front(cpc);
+    got_from_cache = false;
 
-    return TheCopyCache.front();
+    const int key = cpc.m_dstba.size() + cpc.m_srcba.size();
+
+    if (use_copy_cache)
+    {
+        std::pair<CPCCacheIter,CPCCacheIter> er_it = TheCopyCache.equal_range(key);
+
+        for (CPCCacheIter it = er_it.first; it != er_it.second; ++it)
+        {
+            if (it->second == cpc)
+            {
+                it->second.m_reused = true;
+                got_from_cache = true;
+                return it->second;
+            }
+        }
+        //
+        // Don't let the size of the cache get too big.
+        //
+        const int N = 50;
+
+        if (TheCopyCache.size() >= N)
+        {
+            for (CPCCacheIter it = TheCopyCache.begin(); it != TheCopyCache.end(); )
+            {
+                if (!it->second.m_reused)
+                {
+                    TheCopyCache.erase(it++);
+                }
+                else
+                {
+                    ++it;
+                }
+            }
+
+            if (TheCopyCache.size() >= N)
+                //
+                // Get rid of first entry which is the one with the smallest key.
+                //
+                TheCopyCache.erase(TheCopyCache.begin());
+        }
+    }
+    else
+    {
+        TheCopyCache.clear();
+    }
+
+    CPCCacheIter it = TheCopyCache.insert(std::make_pair(key,cpc));
+
+    return it->second;
 }
 
 void
