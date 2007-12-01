725b1ba43522f365e15331e18d1bfc8e373870b9
lijewski <lijewski>
2007-12-01 00:21:44 +0000

    little cleanup

--- Src/F_BaseLib/fillpatch.f90
+++ Src/F_BaseLib/fillpatch.f90
@@ -18,23 +18,21 @@ contains
 
     type(multifab), intent(inout) :: fine
     type(multifab), intent(inout) :: crse
-    type(box     ), intent(in   ) :: fine_domain  ! TODO - this is not used.  Remove from call sequence.
+    type(box     ), intent(in   ) :: fine_domain
     integer       , intent(in   ) :: ng
     integer       , intent(in   ) :: ir(:)
     type(bc_level), intent(in   ) :: bc_crse, bc_fine
-    integer       , intent(in   ) :: icomp,bcomp,nc
+    integer       , intent(in   ) :: icomp, bcomp, nc
 
-    integer          :: i, j, dm, local_bc(crse%dim,2,nc)
+    integer         :: i, j, dm, local_bc(fine%dim,2,nc), gv(fine%dim)
     integer         :: lo(4), lo_f(3), lo_c(3), hi_f(3), hi_c(3), cslope_lo(2), cslope_hi(2)
     type(layout)    :: la, tmpla
     type(multifab)  :: cfine, tmpcrse
-    type(box)       :: bx, fbx, cbx, fine_box, periodic_domain_box, crse_domain
+    type(box)       :: bx, fbx, cbx, fine_box, pdomain, crse_domain
     type(list_box)  :: bl
     type(boxarray)  :: ba, tmpba
     real(kind=dp_t) :: dx(3)
-    logical         :: lim_slope, lin_limit
-    logical         :: pmask(fine%dim)
-    integer         :: grow_vect(fine%dim)
+    logical         :: lim_slope, lin_limit, pmask(fine%dim)
 
     real(kind=dp_t), allocatable :: fvcx(:), fvcy(:), fvcz(:)
     real(kind=dp_t), allocatable :: cvcx(:), cvcy(:), cvcz(:)
@@ -47,8 +45,6 @@ contains
     if ( .not. cell_centered_q(fine) ) call bl_error('fillpatch: fine is NOT cell centered')
     if ( .not. cell_centered_q(crse) ) call bl_error('fillpatch: crse is NOT cell centered')
 
-    crse_domain = box_coarsen_v(fine_domain,ir)
-
     dx        = ONE
     dm        = crse%dim
     lim_slope = .true.
@@ -59,25 +55,22 @@ contains
     call fill_boundary(crse, icomp, nc, ng)
 
     call multifab_physbc(crse,icomp,bcomp,nc,dx,bc_crse)
-
     !
     ! Build coarsened version of fine such that the fabs @ i are owned by the same CPUs.
     !
-    periodic_domain_box = fine_domain
+    pdomain = fine_domain
     pmask(1:dm) = layout_get_pmask(fine%la)
-    grow_vect(:) = 0
+    gv(:) = 0
     do i = 1, dm
-       if (pmask(i)) grow_vect(i) = ng
+       if (pmask(i)) gv(i) = ng
     end do
-    periodic_domain_box = grow(periodic_domain_box,grow_vect)
+    pdomain = grow(pdomain,gv)
 
     do i = 1, nboxes(fine)
        !
        ! We don't use get_pbox here as we only want to fill ng ghost cells of fine & it may have more ghost cells than that.
        !
-       bx =  grow(get_ibox(fine,i),ng)
-       bx =  box_intersection(bx,periodic_domain_box)
-       call push_back(bl,bx)
+       call push_back(bl,box_intersection(grow(get_ibox(fine,i),ng),pdomain))
     end do
 
     call build(ba, bl, sort = .false.)
@@ -86,7 +79,7 @@ contains
 
     call boxarray_coarsen(ba, ir)
 
-    call boxarray_grow(ba, 1)      ! Grow by one for stencil in lin_cc_interp
+    call boxarray_grow(ba, 1) ! Grow by one for stencil in lin_cc_interp
 
     call build(la, ba, explicit_mapping = get_proc(fine%la))
 
@@ -97,8 +90,7 @@ contains
     ! Fill cfine from crse.  Got to do it in stages as parallel copy only goes from valid -> valid.
     !
     do i = 1, nboxes(crse)
-       bx = get_pbox(crse,i)
-       call push_back(bl, bx)
+       call push_back(bl, get_pbox(crse,i))
     end do
 
     call build(tmpba, bl, sort = .false.)
@@ -124,44 +116,42 @@ contains
 
     call destroy(tmpla)
 
+    crse_domain = coarsen(fine_domain,ir)
+
     do i = 1, nboxes(cfine)
        if ( remote(cfine, i) ) cycle
 
-       cbx = get_ibox(cfine,i)
+       cbx      = get_ibox(cfine,i)
        fine_box = get_ibox(fine,i)
-       fbx = grow(fine_box,ng)
-       fbx =  box_intersection(fbx,periodic_domain_box)
+       fbx      =  box_intersection(grow(fine_box,ng),pdomain)
 
        cslope_lo(1:dm) = lwb(grow(cbx, -1))
        cslope_hi(1:dm) = upb(grow(cbx, -1))
 
-!      local_bc(:,:,1:nc) = bc_fine%adv_bc_level_array(i,:,:,bcomp:bcomp+nc-1)
-
        local_bc(:,:,1:nc) = INTERIOR
 
-                if (cslope_lo(1) == crse_domain%lo(1)) then
-                   local_bc(1,1,1:nc) = bc_crse%adv_bc_level_array(0,1,1,bcomp:bcomp+nc-1)
-                end if
-                if (cslope_hi(1) == crse_domain%hi(1)) then
-                   local_bc(1,2,1:nc) = bc_crse%adv_bc_level_array(0,1,2,bcomp:bcomp+nc-1)
-                end if
-                if (dm > 1) then
-                   if (cslope_lo(2) == crse_domain%lo(2)) then
-                      local_bc(2,1,1:nc) = bc_crse%adv_bc_level_array(0,2,1,bcomp:bcomp+nc-1)
-                   end if
-                   if (cslope_hi(2) == crse_domain%hi(2)) then
-                      local_bc(2,2,1:nc) = bc_crse%adv_bc_level_array(0,2,2,bcomp:bcomp+nc-1)
-                   end if
-                end if
-                if (dm > 2) then
-                   if (cslope_lo(dm) == crse_domain%lo(dm)) then
-                      local_bc(dm,1,1:nc) = bc_crse%adv_bc_level_array(0,dm,1,bcomp:bcomp+nc-1)
-                   end if
-                   if (cslope_hi(dm) == crse_domain%hi(dm)) then
-                      local_bc(dm,2,1:nc) = bc_crse%adv_bc_level_array(0,dm,2,bcomp:bcomp+nc-1)
-                   end if
-                end if
-
+       if (cslope_lo(1) == crse_domain%lo(1)) then
+          local_bc(1,1,1:nc) = bc_crse%adv_bc_level_array(0,1,1,bcomp:bcomp+nc-1)
+       end if
+       if (cslope_hi(1) == crse_domain%hi(1)) then
+          local_bc(1,2,1:nc) = bc_crse%adv_bc_level_array(0,1,2,bcomp:bcomp+nc-1)
+       end if
+       if (dm > 1) then
+          if (cslope_lo(2) == crse_domain%lo(2)) then
+             local_bc(2,1,1:nc) = bc_crse%adv_bc_level_array(0,2,1,bcomp:bcomp+nc-1)
+          end if
+          if (cslope_hi(2) == crse_domain%hi(2)) then
+             local_bc(2,2,1:nc) = bc_crse%adv_bc_level_array(0,2,2,bcomp:bcomp+nc-1)
+          end if
+       end if
+       if (dm > 2) then
+          if (cslope_lo(dm) == crse_domain%lo(dm)) then
+             local_bc(dm,1,1:nc) = bc_crse%adv_bc_level_array(0,dm,1,bcomp:bcomp+nc-1)
+          end if
+          if (cslope_hi(dm) == crse_domain%hi(dm)) then
+             local_bc(dm,2,1:nc) = bc_crse%adv_bc_level_array(0,dm,2,bcomp:bcomp+nc-1)
+          end if
+       end if
 
        lo_c(1:dm) = lwb(cbx)
        hi_c(1:dm) = upb(cbx)
