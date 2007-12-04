6f228ca90ca2e1fecc5b0b801de0d03206b5539e
lijewski <lijewski>
2007-12-04 00:29:26 +0000

    start on more efficient parallel version -- more to do ...

--- Src/F_BaseLib/multifab_fill_ghost_cells.f90
+++ Src/F_BaseLib/multifab_fill_ghost_cells.f90
@@ -13,6 +13,135 @@ module multifab_fill_ghost_module
 
 contains
 
+  subroutine multifab_fill_ghost_cells_new(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
+!  subroutine multifab_fill_ghost_cells(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
+
+    type(multifab), intent(inout) :: fine
+    type(multifab), intent(inout) :: crse
+    type(box     ), intent(in   ) :: fine_domain
+    integer       , intent(in   ) :: ng
+    integer       , intent(in   ) :: ir(:)
+    type(bc_level), intent(in   ) :: bc_crse,bc_fine
+    integer       , intent(in   ) :: icomp,bcomp,nc
+
+    integer        :: i, j
+    type(multifab) :: ghost, tmpfine
+    type(box)      :: bx
+    type(boxarray) :: ba
+    type(list_box) :: bl, pieces, leftover
+    type(layout)   :: la, tmpla
+
+    real(kind=dp_t), pointer :: src(:,:,:,:), dst(:,:,:,:)
+    real(kind=dp_t) :: dx(3)
+
+    type(box_intersector), pointer :: bi(:)
+
+    if (ng == 0) return
+
+    if ( nghost(fine) <  ng          ) call bl_error('fillpatch: fine does NOT have enough ghost cells')
+
+    if ( .not. cell_centered_q(fine) ) call bl_error('fillpatch: fine is NOT cell centered')
+    !
+    ! Build list of all ghost cells not covered by valid region.
+    !
+    do i = 1, nboxes(fine)
+       bx = get_ibox(fine,i)
+       call boxarray_box_diff(ba, grow(bx,ng), bx)
+       do j = 1, nboxes(ba)
+          call push_back(bl, get_box(ba,j))
+       end do
+       call destroy(ba)
+    end do
+
+    call build(ba, bl, sort = .false.)
+
+    call destroy(bl)
+
+    do i = 1, nboxes(ba)
+       bx = get_box(ba,i)
+       bi => layout_get_box_intersector(fine%la, bx)
+       do j = 1, size(bi)
+          call push_back(pieces, bi(j)%bx)
+       end do
+       deallocate(bi)
+       leftover = boxlist_boxlist_diff(bx, pieces)
+       call splice(bl, leftover)
+       call destroy(pieces)
+    end do
+
+    call destroy(ba)
+    !
+    ! Now fillpatch a temporary multifab on those ghost cells.
+    !
+    ! We use an extra grow cell so we get wide enough strips to enable HOEXTRAP.
+    !
+    call build(ba, bl, sort = .false.)
+
+    call boxarray_simplify(ba)
+
+    call destroy(bl)
+
+    call build(la, ba, get_pd(fine%la), get_pmask(fine%la))
+
+    call destroy(ba)
+
+    call build(ghost, la, nc, ng = 1)
+
+    call fillpatch(ghost, crse, fine_domain, 1, ir, bc_crse, bc_fine, 1, bcomp, nc)
+    !
+    ! Copy fillpatch()d ghost cells to fine.
+    !
+    ! Got to do it in two stages since copy()s only go from valid -> valid.
+    !
+    do i = 1, nboxes(fine)
+       call push_back(bl, grow(get_ibox(fine,i),ng))
+    end do
+
+    call build(ba, bl, sort = .false.)
+
+    call destroy(bl)
+
+    call build(tmpla, ba, explicit_mapping = get_proc(fine%la))
+
+    call destroy(ba)
+
+    call build(tmpfine, tmpla, nc = nc, ng = 0)
+
+    do i = 1, nboxes(fine)
+       if ( remote(fine, i) ) cycle
+       bx  =  get_ibox(tmpfine,i)
+       src => dataptr(fine,    i, bx, icomp, nc)
+       dst => dataptr(tmpfine, i, bx, 1    , nc)
+       dst =  src
+    end do
+
+    call copy(tmpfine, 1, ghost, 1, nc)
+
+    do i = 1, nboxes(fine)
+       if ( remote(fine, i) ) cycle
+       bx  =  get_ibox(tmpfine,i)
+       dst => dataptr(fine,    i, bx, icomp, nc)
+       src => dataptr(tmpfine, i, bx, 1    , nc)
+       dst =  src
+    end do
+    !
+    ! Finish up.
+    !
+    call fill_boundary(fine, icomp, nc, ng)
+
+    dx = ONE
+
+    call multifab_physbc(fine, icomp, bcomp, nc, dx, bc_fine)
+
+    call destroy(ghost)
+    call destroy(tmpfine)
+
+    call destroy(la)
+    call destroy(tmpla)
+
+  end subroutine
+
+!  subroutine multifab_fill_ghost_cells_old(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
   subroutine multifab_fill_ghost_cells(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
 
     type(multifab), intent(inout) :: fine
@@ -38,33 +167,27 @@ contains
     ! Copy ghost cells in tfine to fine.
     !
     do i = 1, nboxes(fine)
-
        if ( remote(fine, i) ) cycle
-
        call boxarray_box_diff(ba, get_pbox(tfine,i), get_ibox(tfine,i))
-
        do j = 1, nboxes(ba)
           bx  =  get_box(ba, j)
           src => dataptr(tfine, i, bx, 1,     nc)
           dst => dataptr(fine,  i, bx, icomp, nc)
           dst =  src
        end do
-
        call destroy(ba)
-
     end do
 
-    ! We need this call here in addition to the one in fillpatch because
-    !   this will extrapolate from the correct fine data, not from interpolated
-    !   crse data.
-    dx(:) = ONE
+    dx = ONE
+
     call multifab_physbc(fine,icomp,bcomp,nc,dx,bc_fine)
 
     call destroy(tfine)
 
   end subroutine
 
-  subroutine multifab_fill_ghost_cells_old(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
+  subroutine multifab_fill_ghost_cells_oold(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
+!  subroutine multifab_fill_ghost_cells(fine,crse,fine_domain,ng,ir,bc_crse,bc_fine,icomp,bcomp,nc)
 
     type(multifab), intent(inout) :: fine
     type(multifab), intent(inout) :: crse
