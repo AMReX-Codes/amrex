992ba8b51a24677dd6f62d7f6dfc4d6fc7cb402d
lijewski <lijewski>
2007-12-26 01:05:31 +0000

    now calculates min/max cnt & min/max alloc across CPUs

--- Src/F_BaseLib/bl_mem_stat.f90
+++ Src/F_BaseLib/bl_mem_stat.f90
@@ -5,9 +5,9 @@ module bl_mem_stat_module
   implicit none
 
   type mem_stats
-     integer(kind =ll_t) :: num_alloc = 0_ll_t
+     integer(kind =ll_t) :: num_alloc   = 0_ll_t
      integer(kind =ll_t) :: num_dealloc = 0_ll_t
-     integer(kind =ll_t) :: cnt_alloc = 0_ll_t
+     integer(kind =ll_t) :: cnt_alloc   = 0_ll_t
      integer(kind =ll_t) :: cnt_dealloc = 0_ll_t
   end type mem_stats
 
@@ -29,7 +29,7 @@ module bl_mem_stat_module
 
 contains
 
-  subroutine mem_stats_print(ms, str, unit, advance, total)
+  subroutine mem_stats_print(ms, str, unit, advance)
     use parallel
     use bl_IO_module
     use bl_string_module
@@ -37,28 +37,26 @@ contains
     character(len=*), intent(in), optional :: str
     character(len=*), intent(in), optional :: advance
     integer, intent(in), optional :: unit
-    logical, intent(in), optional :: total
-    integer :: un
+    integer :: un, ioproc
     character(len=3) :: adv
-    logical :: ltotal
-    ltotal = .true.; if ( present(total) ) ltotal = total
+    real(dp_t) :: lov, hiv, loc, hic
+
+    ioproc = parallel_IOProcessorNode()
+ 
+    call parallel_reduce(lov, real(ms%num_alloc-ms%num_dealloc,dp_t), MPI_MIN, proc = ioproc)
+    call parallel_reduce(hiv, real(ms%num_alloc-ms%num_dealloc,dp_t), MPI_MAX, proc = ioproc)
+ 
+    call parallel_reduce(loc, real(ms%cnt_alloc-ms%cnt_dealloc,dp_t), MPI_MIN, proc = ioproc)
+    call parallel_reduce(hic, real(ms%cnt_alloc-ms%cnt_dealloc,dp_t), MPI_MAX, proc = ioproc)
+
     if ( parallel_IOProcessor() ) then
        un = unit_stdout(unit)
        adv = unit_advance(advance)
        if ( present(str) ) then
           write(unit = un, fmt = '(A,": ")', advance = 'no') str
        end if
-       if ( ltotal ) then
-          write(unit=un, fmt='(4i15)', advance = 'no') &
-               ms%num_alloc,                           &
-               ms%num_alloc-ms%num_dealloc,            &
-               ms%cnt_alloc,                           &
-               ms%cnt_alloc-ms%cnt_dealloc
-       else
-          write(unit=un, fmt='(2i15)', advance = 'no') &
-               ms%num_alloc-ms%num_dealloc,            &
-               ms%cnt_alloc-ms%cnt_dealloc
-       end if
+       write(unit=un, fmt='(4i15)', advance = 'no') &
+            int(lov,ll_t), int(hiv,ll_t), int(loc,ll_t), int(hic,ll_t)
        if ( eq_i(adv, "YES") ) write(unit=un, fmt='()')
     end if
   end subroutine mem_stats_print
