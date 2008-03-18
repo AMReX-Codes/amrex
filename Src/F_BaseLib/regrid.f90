21027fb55efcd7cdc083eba2b073dbfe3961d5f4
almgren <almgren>
2008-03-18 19:54:58 +0000

    Candace's changes to regrid -- buffer subroutine not complete, but this does multiple levels.

--- Src/F_BaseLib/regrid.f90
+++ Src/F_BaseLib/regrid.f90
@@ -13,56 +13,94 @@ module regrid_module
   use cluster_module
   use ml_layout_module
 
+
+  implicit none 
+
   contains
 
-    subroutine make_new_grids(mla,mla_new,mf,dx_crse,buf_wid)
+    subroutine make_new_grids(mla,mla_new,mf,dx_crse,buf_wid,&
+         rr,lev,new_grid)
 
        type(ml_layout), intent(in   ) :: mla
-       type(ml_layout), intent(  out) :: mla_new
+       type(ml_layout), intent(inout) :: mla_new
        type(multifab) , intent(in   ) :: mf
        real(dp_t)     , intent(in   ) :: dx_crse
        integer        , intent(in   ) :: buf_wid
+! need to came back and change ref_ratio to vary
+       integer        , intent(in   ) :: rr(:,:)
+       integer, optional, intent(in   ) :: lev
+       logical, optional, intent(  out) :: new_grid
 
        type(ml_boxarray) :: mba_new
-       type(boxarray) :: ba_new
-       type(boxarray) :: ba_loc
-       type(box     ) :: bx_loc
+       type(boxarray)    :: ba_new
+       integer           :: llev,dim,n,i
 
-       integer :: dim
        logical :: pmask(mf%dim)
 
-       pmask = layout_get_pmask(mf%la)
-
-       call make_boxes(mf,ba_new,dx_crse,buf_wid)
-       call boxarray_refine(ba_new,mla%mba%rr(1,:))
-
-       call copy(mba_new,mla%mba)
-       mba_new%bas(2) = ba_new
 
        dim = mf%dim
+       pmask = mla%pmask
+       llev = 1; if (present(lev)) llev = lev
+
+       call make_boxes(mf,ba_new,dx_crse,buf_wid, llev)
+
+       if (empty(ba_new)) then 
+          new_grid = .false.
+          call build(mba_new,llev,dim)
+          do n = 1, llev-1
+             mba_new%pd(n) = mla%mba%pd(n)
+             mba_new%rr(n,:) = rr(n,:)
+             call copy(mba_new%bas(n),mla%mba%bas(n))
+          enddo
+          mba_new%pd(llev) = mla%mba%pd(llev)
+          call copy(mba_new%bas(llev),mla%mba%bas(llev))
+          call  ml_layout_build(mla_new,mba_new,pmask)
+          call destroy(mba_new)
+          return
+       endif
+
+       new_grid = .true.
+       
+       call build(mba_new,llev+1,dim)
+       do n = 1, llev
+          mba_new%pd(n) = mla%mba%pd(n)
+          mba_new%rr(n,:) = rr(n,:)
+          call copy(mba_new%bas(n),mla%mba%bas(n))
+       enddo
+       call boxarray_refine(ba_new,rr(llev,:))
+       mba_new%bas(llev+1) = ba_new
+       mba_new%pd(llev+1) = refine(mba_new%pd(llev),rr(llev,:))
+
        call ml_layout_build(mla_new,mba_new,pmask)
+
+       call destroy(mba_new)
+!       call destroy(ba_new)
 
     end subroutine make_new_grids
 
-    subroutine make_boxes(mf,ba_new,dx_crse,buf_wid)
+    subroutine make_boxes(mf,ba_new,dx_crse,buf_wid,lev)
 
       type(multifab), intent(in   ) :: mf
       type(boxarray), intent(  out) :: ba_new
       real(dp_t)    , intent(in   ) :: dx_crse
       integer       , intent(in   ) :: buf_wid
+      integer, optional, intent(in   ) :: lev
 
       type(list_box_node), pointer :: bn
       type(lmultifab) :: tagboxes
 
       real(kind = dp_t), pointer :: sp(:,:,:,:)
       logical          , pointer :: tp(:,:,:,:)
-
       real(kind = dp_t) :: min_eff
       integer           :: i, j, k, dm
       integer, allocatable  :: lo(:)
       integer           :: minwidth
+      integer           :: llev, ng_cell
 
+
+      llev = 1; if (present(lev)) llev = lev
       dm = mf%dim
+      ng_cell = mf%ng
       allocate(lo(dm))
 
       call lmultifab_build(tagboxes,mf%la,1,0)
@@ -72,10 +110,11 @@ module regrid_module
         sp => dataptr(mf, i)
         tp => dataptr(tagboxes, i)
         lo =  lwb(get_box(tagboxes, i))
+
         select case (dm)
           case (2)
-             call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,ng_cell,dx_crse)
-          case (3)
+             call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,ng_cell,dx_crse,llev)
+          case  (3)
              call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,ng_cell,dx_crse)
           end select
       end do
@@ -86,31 +125,44 @@ module regrid_module
       call cluster(ba_new, tagboxes, minwidth, buf_wid, min_eff)
 
       call destroy(tagboxes)
-
+  
     end subroutine make_boxes
 
-    subroutine tag_boxes_2d(tagbox,mf,lo,ng,dx_crse)
+    subroutine tag_boxes_2d(tagbox,mf,lo,ng,dx_crse,lev)
 
       integer          , intent(in   ) :: lo(:),ng
       logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
       real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
       real(kind = dp_t), intent(in   ) :: dx_crse
+      integer, optional, intent(in   ) :: lev
+      integer :: i,j,nx,ny, llev
 
-      integer :: i,j,nx,ny
-
+      llev = 1; if (present(lev)) llev = lev
       nx = size(tagbox,dim=1)
       ny = size(tagbox,dim=2)
 
       tagbox = .false.
 
-      ! NOTE: THIS IS JUST A PLACEHOLDER LOOP -- CRITERION SHOULD REALLY BE USER-SET
-      do j = lo(2),lo(2)+ny-1
-         do i = lo(1),lo(1)+nx-1
-            if (mf(i,j) .gt. 0.0) then
-               tagbox(i,j) = .true.
-            end if
-         end do
-      end do
+      select case(llev)
+         case (1)
+            ! tag all boxes with a density >= 1.1
+            do j = lo(2),lo(2)+ny-1
+               do i = lo(1),lo(1)+nx-1
+                  if (mf(i,j) .gt. 1.1d0) then
+                     tagbox(i,j) = .true.
+                  end if
+               end do
+            enddo
+         case (2)
+            ! for level 2 tag all boxes with a density >= 2.5
+            do j = lo(2),lo(2)+ny-1
+               do i = lo(1),lo(1)+nx-1
+                  if (mf(i,j) .gt. 1.5d0) then
+                     tagbox(i,j) = .true.
+                  end if
+               end do
+            end do
+         end select
 
     end subroutine tag_boxes_2d
 
@@ -141,5 +193,23 @@ module regrid_module
       end do
 
     end subroutine tag_boxes_3d
+
+    subroutine buffer(mf,ba_new,buf_wid)
+
+      type(multifab), intent(in   ) :: mf
+      type(boxarray), intent(inout) :: ba_new
+      integer,        intent(in   ) :: buf_wid
+
+      type(lmultifab) :: boxes
+      integer         :: minwidth
+      real(kind = dp_t) :: min_eff
+
+      call lmultifab_build(boxes,mf%la,1,0)
+      call setval(boxes, .true., all=.true.)
+
+
+      call cluster(ba_new,boxes, minwidth,buf_wid, min_eff)
+
+    end subroutine buffer
 
 end module regrid_module
