35ecf29d2b844be0aa262f988e5fa22bbd23eaf3
almgren <almgren>
2008-07-07 18:05:26 +0000

    This takes max_grid_size as an argument to make_new_grids, and some other changes..

--- Src/F_BaseLib/regrid.f90
+++ Src/F_BaseLib/regrid.f90
@@ -13,81 +13,56 @@ module regrid_module
   use cluster_module
   use ml_layout_module
 
-
   implicit none 
 
   contains
 
-    subroutine make_new_grids(mla,mla_new,mf,dx_crse,buf_wid,rr,lev,max_grid_size,new_grid)
+    subroutine make_new_grids(la_crse,la_fine,mf,dx_crse,buf_wid,ref_ratio,lev_in,max_grid_size,new_grid)
 
-       type(ml_layout), intent(in   ) :: mla
-       type(ml_layout), intent(inout) :: mla_new
-       type(multifab) , intent(in   ) :: mf
-       real(dp_t)     , intent(in   ) :: dx_crse
-       integer        , intent(in   ) :: buf_wid
-       integer        , intent(in   ) :: max_grid_size
-
-       ! ref_ratio now fixed across levels and dimensions -- fix me?
-       integer        , intent(in   ) :: rr(:,:)
-
-       integer, optional, intent(in   ) :: lev
+       type(layout)     , intent(in   ) :: la_crse
+       type(layout)     , intent(inout) :: la_fine
+       type(multifab)   , intent(in   ) :: mf
+       real(dp_t)       , intent(in   ) :: dx_crse
+       integer          , intent(in   ) :: buf_wid
+       integer          , intent(in   ) :: max_grid_size
+       integer          , intent(in   ) :: ref_ratio
+       integer, optional, intent(in   ) :: lev_in
        logical, optional, intent(  out) :: new_grid
 
-       type(ml_boxarray) :: mba_new
+       type(box)         :: pd_fine
        type(boxarray)    :: ba_new
-       integer           :: llev,dim,n,i
-       integer           :: rr_max
+       integer           :: llev,dm,n,i
 
        logical :: pmask(mf%dim)
 
-       dim = mf%dim
-       pmask = mla%pmask
-       llev = 1; if (present(lev)) llev = lev
-
-       rr_max = rr(llev,1)
-       if (mf%dim > 1) then
-         do i = 2,mf%dim
-           rr_max = max(rr_max, rr(llev,i))
-         end do
-       end if
+       dm = mf%dim
+       pmask = layout_get_pmask(la_crse)
+       llev = 1; if (present(lev_in)) llev = lev_in
 
        call make_boxes(mf,ba_new,dx_crse,buf_wid,llev)
 
-       ! Need to divide max_grid_size by rr_max since we are creating
-       !  the grids at the coarser resolution but want max_grid_size to
-       !  apply at the fine resolution.
-       call boxarray_maxsize(ba_new,max_grid_size/rr_max)
-
        if (empty(ba_new)) then 
+
           new_grid = .false.
-          call build(mba_new,llev,dim)
-          do n = 1, llev-1
-             mba_new%pd(n) = mla%mba%pd(n)
-             mba_new%rr(n,:) = rr(n,:)
-             call copy(mba_new%bas(n),mla%mba%bas(n))
-          enddo
-          mba_new%pd(llev) = mla%mba%pd(llev)
-          call copy(mba_new%bas(llev),mla%mba%bas(llev))
-          call  ml_layout_build(mla_new,mba_new,pmask)
-          call destroy(mba_new)
-          return
-       endif
 
-       new_grid = .true.
+       else
+
+          new_grid = .true.
+
+          ! Need to divide max_grid_size by ref_ratio since we are creating
+          !  the grids at the coarser resolution but want max_grid_size to
+          !  apply at the fine resolution.
+          call boxarray_maxsize(ba_new,max_grid_size/ref_ratio)
        
-       call build(mba_new,llev+1,dim)
-       do n = 1, llev
-          mba_new%pd(n) = mla%mba%pd(n)
-          mba_new%rr(n,:) = rr(n,:)
-          call copy(mba_new%bas(n),mla%mba%bas(n))
-       enddo
-       call boxarray_refine(ba_new,rr(llev,:))
-       mba_new%bas(llev+1) = ba_new
-       mba_new%pd(llev+1) = refine(mba_new%pd(llev),rr(llev,:))
+          call boxarray_refine(ba_new,ref_ratio)
+ 
+          pd_fine = refine(layout_get_pd(la_crse),ref_ratio)
+   
+          call layout_build_ba(la_fine,ba_new,pd_fine,pmask)
+   
+          call destroy(ba_new)
 
-       call ml_layout_build(mla_new,mba_new,pmask)
-
-       call destroy(mba_new)
+       endif
 
     end subroutine make_new_grids
 
@@ -170,9 +145,9 @@ module regrid_module
             ! for level 2 tag all boxes with a density >= 2.5
             do j = lo(2),lo(2)+ny-1
                do i = lo(1),lo(1)+nx-1
-                  if (mf(i,j) .gt. 1.5d0) then
+!                 if (mf(i,j) .gt. 1.0d0) then
                      tagbox(i,j) = .true.
-                  end if
+!                 end if
                end do
             end do
          case (3)
