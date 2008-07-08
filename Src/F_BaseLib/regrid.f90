a19cba9792fa81be52d72d66fa8271ee338b9c21
almgren <almgren>
2008-07-08 23:53:01 +0000

    Separated the tag_boxes functionality out of regrid -- new files tag_boxes.f90

--- Src/F_BaseLib/regrid.f90
+++ Src/F_BaseLib/regrid.f90
@@ -68,6 +68,8 @@ module regrid_module
 
     subroutine make_boxes(mf,ba_new,dx_crse,buf_wid,lev)
 
+      use tag_boxes_module
+
       type(multifab), intent(in   ) :: mf
       type(boxarray), intent(  out) :: ba_new
       real(dp_t)    , intent(in   ) :: dx_crse
@@ -77,35 +79,18 @@ module regrid_module
       type(list_box_node), pointer :: bn
       type(lmultifab) :: tagboxes
 
-      real(kind = dp_t), pointer :: sp(:,:,:,:)
-      logical          , pointer :: tp(:,:,:,:)
       real(kind = dp_t) :: min_eff
       integer           :: i, j, k, dm
-      integer, allocatable  :: lo(:)
       integer           :: minwidth
       integer           :: llev, ng_cell
-
 
       llev = 1; if (present(lev)) llev = lev
       dm = mf%dim
       ng_cell = mf%ng
-      allocate(lo(dm))
 
       call lmultifab_build(tagboxes,mf%la,1,0)
 
-      do i = 1, mf%nboxes
-        if ( multifab_remote(mf, i) ) cycle
-        sp => dataptr(mf, i)
-        tp => dataptr(tagboxes, i)
-        lo =  lwb(get_box(tagboxes, i))
-
-        select case (dm)
-          case (2)
-             call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,ng_cell,dx_crse,llev)
-          case  (3)
-             call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,ng_cell,dx_crse)
-          end select
-      end do
+      call tag_boxes(mf,tagboxes,llev)
 
       minwidth = 2
       min_eff = .7
@@ -116,119 +101,27 @@ module regrid_module
   
     end subroutine make_boxes
 
-    subroutine tag_boxes_2d(tagbox,mf,lo,ng,dx_crse,lev)
+    subroutine buffer(lev,la_fine,ba_crse,ref_ratio,buff)
 
-      integer          , intent(in   ) :: lo(:),ng
-      logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
-      real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
-      real(kind = dp_t), intent(in   ) :: dx_crse
-      integer, optional, intent(in   ) :: lev
-      integer :: i,j,nx,ny, llev
+      integer          , intent(in   ) :: lev
+      type(layout)     , intent(in   ) :: la_fine
+      type(boxarray)   , intent(inout) :: ba_crse
+      integer          , intent(in   ) :: buff
+      integer          , intent(in   ) :: ref_ratio
 
-      llev = 1; if (present(lev)) llev = lev
-      nx = size(tagbox,dim=1)
-      ny = size(tagbox,dim=2)
-
-      tagbox = .false.
-
-      select case(llev)
-         case (1)
-            ! tag all boxes with a density >= 1.1
-            do j = lo(2),lo(2)+ny-1
-               do i = lo(1),lo(1)+nx-1
-                  if (mf(i,j) .gt. 1.1d0) then
-                     tagbox(i,j) = .true.
-                  end if
-               end do
-            enddo
-         case (2)
-            ! for level 2 tag all boxes with a density >= 2.5
-            do j = lo(2),lo(2)+ny-1
-               do i = lo(1),lo(1)+nx-1
-!                 if (mf(i,j) .gt. 1.0d0) then
-                     tagbox(i,j) = .true.
-!                 end if
-               end do
-            end do
-         case (3)
-            ! for level 3 tag all boxes with a density >= 2.5
-            do j = lo(2),lo(2)+ny-1
-               do i = lo(1),lo(1)+nx-1
-                  if (mf(i,j) .gt. 1.5d0) then
-                     tagbox(i,j) = .true.
-                  end if
-               end do
-            end do
-         end select
-
-    end subroutine tag_boxes_2d
-
-    subroutine tag_boxes_3d(tagbox,mf,lo,ng,dx_crse)
-
-      integer          , intent(in   ) :: lo(:),ng
-      logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
-      real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
-      real(kind = dp_t), intent(in   ) :: dx_crse
-
-      integer :: i,j,k,nx,ny,nz
-
-      nx = size(tagbox,dim=1)
-      ny = size(tagbox,dim=2)
-      nz = size(tagbox,dim=3)
-
-      tagbox = .false.
-
-      ! NOTE: THIS IS JUST A PLACEHOLDER ARRAY -- CRITERION SHOULD REALLY BE USER-SET
-      do k = lo(3),lo(3)+nz-1
-      do j = lo(2),lo(2)+ny-1
-      do i = lo(1),lo(1)+nx-1
-         if (mf(i,j,k) .gt. 0.0) then
-            tagbox(i,j,k) = .true.
-         end if
-      end do
-      end do
-      end do
-
-    end subroutine tag_boxes_3d
-
-    subroutine buffer(lev,mla,buff)
-
-      integer,         intent(in   ) :: lev
-      type(ml_layout), intent(inout) :: mla
-      integer,         intent(in   ) :: buff
-
-      type(lmultifab) :: boxes, boxes2
-      type(boxarray)  :: ba_new, ba_lev
+      type(lmultifab) :: boxes
       integer         :: minwidth, n
       real(kind = dp_t) :: min_eff
-      type(ml_boxarray) :: mba_new
-      type(ml_layout)   :: mla_new
-      type(layout)      :: la
       
-      call lmultifab_build(boxes,mla%la(lev),1,0)
+      call lmultifab_build(boxes,la_fine,1,0)
       call setval(boxes,.true.,all=.true.)
      
       minwidth = 2
       min_eff = .7
 
-      call cluster(ba_new,boxes,minwidth,buff,min_eff)
+      call cluster(ba_crse,boxes,minwidth,buff,min_eff)
 
-      call destroy(boxes)
-      call build(mba_new,lev,mla%dim)
-      do n = 1, lev-1
-         mba_new%pd(n) = mla%mba%pd(n)
-         mba_new%rr(n,:) = mla%mba%rr(n,:)
-         call copy(mba_new%bas(n),mla%mba%bas(n))
-      enddo
-      mba_new%pd(lev) = mla%mba%pd(lev)
-      mba_new%bas(lev) = ba_new
-
-      call ml_layout_build(mla_new,mba_new,mla%pmask)
-      call destroy(mla)
-      call ml_layout_build(mla,mba_new,mla_new%pmask)
-      call destroy(mla_new)
-
-      call destroy(mba_new)
+      call boxarray_coarsen(ba_crse, ref_ratio)
 
     end subroutine buffer
 
---
+++ Src/F_BaseLib/tag_boxes.f90
@@ -0,0 +1,122 @@
+module tag_boxes_module
+
+  use BoxLib
+  use omp_module
+  use f2kcli
+  use list_box_module
+  use boxarray_module
+  use ml_boxarray_module
+  use layout_module
+  use multifab_module
+  use box_util_module
+  use bl_IO_module
+  use cluster_module
+  use ml_layout_module
+
+  implicit none 
+
+  contains
+
+    subroutine tag_boxes(mf,tagboxes,lev)
+
+      use probin_module, only : ng_cell
+
+      type( multifab), intent(in   ) :: mf
+      type(lmultifab), intent(inout) :: tagboxes
+      integer        , intent(in   ) :: lev
+
+      real(kind = dp_t), pointer :: sp(:,:,:,:)
+      logical          , pointer :: tp(:,:,:,:)
+      integer           :: i, lo(mf%dim)
+
+      do i = 1, mf%nboxes
+        if ( multifab_remote(mf, i) ) cycle
+        sp => dataptr(mf, i)
+        tp => dataptr(tagboxes, i)
+        lo =  lwb(get_box(tagboxes, i))
+
+        select case (mf%dim)
+          case (2)
+             call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,ng_cell,lev)
+          case  (3)
+             call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,ng_cell)
+          end select
+      end do
+
+    end subroutine tag_boxes
+
+    subroutine tag_boxes_2d(tagbox,mf,lo,ng,lev)
+
+      integer          , intent(in   ) :: lo(:),ng
+      logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
+      real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
+      integer, optional, intent(in   ) :: lev
+      integer :: i,j,nx,ny, llev
+
+      llev = 1; if (present(lev)) llev = lev
+      nx = size(tagbox,dim=1)
+      ny = size(tagbox,dim=2)
+
+      tagbox = .false.
+
+      select case(llev)
+         case (1)
+            ! tag all boxes with a density >= 1.1
+            do j = lo(2),lo(2)+ny-1
+               do i = lo(1),lo(1)+nx-1
+                  if (mf(i,j) .gt. 1.1d0) then
+                     tagbox(i,j) = .true.
+                  end if
+               end do
+            enddo
+         case (2)
+            ! for level 2 tag all boxes with a density >= 2.5
+            do j = lo(2),lo(2)+ny-1
+               do i = lo(1),lo(1)+nx-1
+                  if (mf(i,j) .gt. 1.5d0) then
+                     tagbox(i,j) = .true.
+                  end if
+               end do
+            end do
+         case (3)
+            ! for level 3 tag all boxes with a density >= 2.5
+            do j = lo(2),lo(2)+ny-1
+               do i = lo(1),lo(1)+nx-1
+                  if (mf(i,j) .gt. 1.5d0) then
+                     tagbox(i,j) = .true.
+                  end if
+               end do
+            end do
+         end select
+
+    end subroutine tag_boxes_2d
+
+    subroutine tag_boxes_3d(tagbox,mf,lo,ng)
+
+      integer          , intent(in   ) :: lo(:),ng
+      logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
+      real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+
+      integer :: i,j,k,nx,ny,nz
+
+      nx = size(tagbox,dim=1)
+      ny = size(tagbox,dim=2)
+      nz = size(tagbox,dim=3)
+
+      tagbox = .false.
+
+      ! NOTE: THIS IS JUST A PLACEHOLDER ARRAY -- CRITERION SHOULD REALLY BE USER-SET
+      do k = lo(3),lo(3)+nz-1
+      do j = lo(2),lo(2)+ny-1
+      do i = lo(1),lo(1)+nx-1
+         if (mf(i,j,k) .gt. 0.0) then
+            tagbox(i,j,k) = .true.
+         end if
+      end do
+      end do
+      end do
+
+    end subroutine tag_boxes_3d
+
+
+end module tag_boxes_module
