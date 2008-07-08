edca25d116ddb9cd6a8fb743eebed5cfca386819
almgren <almgren>
2008-07-08 23:39:52 +0000

    Remove commented line.

--- Src/F_BaseLib/boxarray.f90
+++ Src/F_BaseLib/boxarray.f90
@@ -127,7 +127,6 @@ module boxarray_module
   private :: boxlist_simplify
   private :: boxarray_maxsize_l
   private :: boxlist_box_diff
-!  private :: boxlist_boxlist_diff
   private :: boxlist_build_a
   private :: boxlist_nboxes
   private :: boxlist_verify_dim
