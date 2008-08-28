2fb4421f21fb70072fc0f7ceb5ae47b1fd020fa1
ajnonaka <ajnonaka>
2008-08-28 17:41:48 +0000

    made 2d consistent with 3d

--- Src/F_BaseLib/tag_boxes.f90
+++ Src/F_BaseLib/tag_boxes.f90
@@ -15,107 +15,134 @@ module tag_boxes_module
 
   implicit none 
 
-  contains
+contains
 
-    subroutine tag_boxes(mf,tagboxes,lev)
+  subroutine tag_boxes(mf,tagboxes,lev)
 
-      use probin_module, only : ng_cell
+    use probin_module, only : ng_cell
 
-      type( multifab), intent(in   ) :: mf
-      type(lmultifab), intent(inout) :: tagboxes
-      integer        , intent(in   ) :: lev
+    type( multifab), intent(in   ) :: mf
+    type(lmultifab), intent(inout) :: tagboxes
+    integer        , intent(in   ) :: lev
 
-      real(kind = dp_t), pointer :: sp(:,:,:,:)
-      logical          , pointer :: tp(:,:,:,:)
-      integer           :: i, lo(mf%dim)
+    real(kind = dp_t), pointer :: sp(:,:,:,:)
+    logical          , pointer :: tp(:,:,:,:)
+    integer           :: i, lo(mf%dim)
 
-      do i = 1, mf%nboxes
-        if ( multifab_remote(mf, i) ) cycle
-        sp => dataptr(mf, i)
-        tp => dataptr(tagboxes, i)
-        lo =  lwb(get_box(tagboxes, i))
+    do i = 1, mf%nboxes
+       if ( multifab_remote(mf, i) ) cycle
+       sp => dataptr(mf, i)
+       tp => dataptr(tagboxes, i)
+       lo =  lwb(get_box(tagboxes, i))
 
-        select case (mf%dim)
-          case (2)
-             call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,ng_cell,lev)
-          case  (3)
-             call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,ng_cell)
-          end select
-      end do
+       select case (mf%dim)
+       case (2)
+          call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,ng_cell,lev)
+       case  (3)
+          call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,ng_cell,lev)
+       end select
+    end do
 
-    end subroutine tag_boxes
+  end subroutine tag_boxes
 
-    subroutine tag_boxes_2d(tagbox,mf,lo,ng,lev)
+  subroutine tag_boxes_2d(tagbox,mf,lo,ng,lev)
 
-      integer          , intent(in   ) :: lo(:),ng
-      logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
-      real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
-      integer, optional, intent(in   ) :: lev
-      integer :: i,j,nx,ny, llev
+    integer          , intent(in   ) :: lo(:),ng
+    logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
+    real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
+    integer, optional, intent(in   ) :: lev
+    integer :: i,j,nx,ny,llev
 
-      llev = 1; if (present(lev)) llev = lev
-      nx = size(tagbox,dim=1)
-      ny = size(tagbox,dim=2)
+    llev = 1; if (present(lev)) llev = lev
+    nx = size(tagbox,dim=1)
+    ny = size(tagbox,dim=2)
 
-      tagbox = .false.
+    tagbox = .false.
 
-      select case(llev)
-         case (1)
-            ! tag all boxes with a density >= 1.01
-            do j = lo(2),lo(2)+ny-1
-               do i = lo(1),lo(1)+nx-1
-                  if (mf(i,j) .gt. 1.01d0) then
-                     tagbox(i,j) = .true.
-                  end if
-               end do
-            enddo
-         case (2)
-            ! for level 2 tag all boxes with a density >= 1.1
-            do j = lo(2),lo(2)+ny-1
-               do i = lo(1),lo(1)+nx-1
-                  if (mf(i,j) .gt. 1.1d0) then
-                     tagbox(i,j) = .true.
-                  end if
-               end do
-            end do
-         case default
-            ! for level 3 or greater tag all boxes with a density >= 1.5
-            do j = lo(2),lo(2)+ny-1
-               do i = lo(1),lo(1)+nx-1
-                  if (mf(i,j) .gt. 1.5d0) then
-                     tagbox(i,j) = .true.
-                  end if
-               end do
-            end do
-         end select
+    select case(llev)
+    case (1)
+       ! tag all boxes with a density >= 1.01
+       do j = lo(2),lo(2)+ny-1
+          do i = lo(1),lo(1)+nx-1
+             if (mf(i,j) .gt. 1.01d0) then
+                tagbox(i,j) = .true.
+             end if
+          end do
+       enddo
+    case (2)
+       ! for level 2 tag all boxes with a density >= 1.1
+       do j = lo(2),lo(2)+ny-1
+          do i = lo(1),lo(1)+nx-1
+             if (mf(i,j) .gt. 1.1d0) then
+                tagbox(i,j) = .true.
+             end if
+          end do
+       end do
+    case default
+       ! for level 3 or greater tag all boxes with a density >= 1.5
+       do j = lo(2),lo(2)+ny-1
+          do i = lo(1),lo(1)+nx-1
+             if (mf(i,j) .gt. 1.5d0) then
+                tagbox(i,j) = .true.
+             end if
+          end do
+       end do
+    end select
 
-    end subroutine tag_boxes_2d
+  end subroutine tag_boxes_2d
 
-    subroutine tag_boxes_3d(tagbox,mf,lo,ng)
+  subroutine tag_boxes_3d(tagbox,mf,lo,ng,lev)
 
-      integer          , intent(in   ) :: lo(:),ng
-      logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
-      real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    integer          , intent(in   ) :: lo(:),ng
+    logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
+    real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    integer, optional, intent(in   ) :: lev
 
-      integer :: i,j,k,nx,ny,nz
+    integer :: i,j,k,nx,ny,nz,llev
 
-      nx = size(tagbox,dim=1)
-      ny = size(tagbox,dim=2)
-      nz = size(tagbox,dim=3)
+    llev = 1; if (present(lev)) llev = lev
+    nx = size(tagbox,dim=1)
+    ny = size(tagbox,dim=2)
+    nz = size(tagbox,dim=3)
 
-      tagbox = .false.
+    tagbox = .false.
 
-      ! NOTE: THIS IS JUST A PLACEHOLDER ARRAY -- CRITERION SHOULD REALLY BE USER-SET
-      do k = lo(3),lo(3)+nz-1
-      do j = lo(2),lo(2)+ny-1
-      do i = lo(1),lo(1)+nx-1
-         if (mf(i,j,k) .gt. 0.0) then
-            tagbox(i,j,k) = .true.
-         end if
-      end do
-      end do
-      end do
+    select case(llev)
+    case (1)
+       ! tag all boxes with a density >= 1.01
+       do k = lo(3),lo(3)+nz-1
+          do j = lo(2),lo(2)+ny-1
+             do i = lo(1),lo(1)+nx-1
+                if (mf(i,j,k) .gt. 1.01d0) then
+                   tagbox(i,j,k) = .true.
+                end if
+             end do
+          enddo
+       end do
+    case (2)
+       ! for level 2 tag all boxes with a density >= 1.1
+       do k = lo(3),lo(3)+nz-1
+          do j = lo(2),lo(2)+ny-1
+             do i = lo(1),lo(1)+nx-1
+                if (mf(i,j,k) .gt. 1.1d0) then
+                   tagbox(i,j,k) = .true.
+                end if
+             end do
+          end do
+       end do
+    case default
+       ! for level 3 or greater tag all boxes with a density >= 1.5
+       do k = lo(3),lo(3)+nz-1
+          do j = lo(2),lo(2)+ny-1
+             do i = lo(1),lo(1)+nx-1
+                if (mf(i,j,k) .gt. 1.5d0) then
+                   tagbox(i,j,k) = .true.
+                end if
+             end do
+          end do
+       end do
+    end select
 
-    end subroutine tag_boxes_3d
+  end subroutine tag_boxes_3d
 
 end module tag_boxes_module
