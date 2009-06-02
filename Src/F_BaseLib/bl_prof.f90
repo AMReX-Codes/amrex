0e8d7f58ab3597b8b6752c686cc8bb8732e63eb3
ajnonaka <ajnonaka>
2009-06-02 21:38:51 +0000

    made timer output much more readable added additional timers in put_1d_array_...

--- Src/F_BaseLib/bl_prof.f90
+++ Src/F_BaseLib/bl_prof.f90
@@ -360,17 +360,17 @@ contains
        write(unit = un, fmt = '("* GLOBAL")')
        write(unit = un, fmt = '("  NPROCS = ", i5,/)') parallel_nprocs()
        call sort(sm(:,2), ism, greater_d)
-       write(unit = un, fmt = '("REGION",TR20,"COUNT", TR8,"TOTAL", TR22, "SELF",TR23,"MAX",TR10,"MIN")')
+       write(unit = un, fmt = '("REGION",TR40,"COUNT",TR8,"TOTAL",TR22,"SELF",TR23,"MAX",TR10,"MIN")')
        do i = 1, size(ism)
           ii = ism(i)
-          write(unit = un, fmt = '(A20,1x,I10,F13.3,TR13,F13.3,TR13,2F13.3)') trim(timers(ii)%name), int(sm(ii,0)), sm(ii,1:)
+          write(unit = un, fmt = '(A40,1x,I10,F13.3,TR13,F13.3,TR13,2F13.3)') trim(timers(ii)%name), int(sm(ii,0)), sm(ii,1:)
        end do
     end if
 
     if ( parallel_ioprocessor() ) then
        write(unit = un, fmt = '()')
        write(unit = un, fmt = &
-            '("REGION",TR40,"COUNT",TR8,"TOTAL", TR8, "CHILD", TR9, "SELF", TR10, "AVG",TR10,"MAX",TR10,"MIN")')
+            '("REGION",TR40,"COUNT",TR8,"TOTAL",TR8,"CHILD",TR9,"SELF",TR10,"AVG",TR10,"MAX",TR10,"MIN")')
     end if
 
     call p_activation(the_call_tree, un, 0, local = .false.)
@@ -394,15 +394,15 @@ contains
              call s_activation(the_call_tree, sm, local = .true.)
              !! Print the summary information
              call sort(sm(:,2), ism, greater_d)
-             write(unit = un, fmt = '("REGION",TR20,"COUNT", TR8,"TOTAL", TR22, "SELF",TR23,"MAX",TR10,"MIN")')
+             write(unit = un, fmt = '("REGION",TR40,"COUNT",TR8,"TOTAL",TR22,"SELF",TR23,"MAX",TR10,"MIN")')
              do j = 1, size(ism)
                 ii = ism(j)
-                write(unit = un, fmt = '(A20,1x,I10,F13.3,TR13,F13.3,TR13,2F13.3)') &
+                write(unit = un, fmt = '(A40,1x,I10,F13.3,TR13,F13.3,TR13,2F13.3)') &
                      trim(timers(ii)%name), int(sm(ii,0)), sm(ii,1:)
              end do
              write(unit = un, fmt = '()')
              write(unit = un, fmt = &
-                  '("REGION",TR40,"COUNT",TR8,"TOTAL", TR8, "CHILD", TR9, "SELF", TR10, "AVG",TR10,"MAX",TR10,"MIN")')
+                  '("REGION",TR40,"COUNT",TR8,"TOTAL",TR8,"CHILD",TR9,"SELF",TR10,"AVG",TR10,"MAX",TR10,"MIN")')
              call p_activation(the_call_tree, un, 0, local = .true.)
              close(unit = un)
           end if
