1bf20c60cf3a86f5d3279c38d67607865ce8c76c
gpau <gpau>
2009-10-07 16:35:31 +0000

    added some functionalities

--- Tools/C_util/Statistics/ComputeAmrDataStat.cpp
+++ Tools/C_util/Statistics/ComputeAmrDataStat.cpp
@@ -115,17 +115,10 @@ ComputeAmrDataMeanVar  (AmrData&     amrData,
 		vwFabSqrd.mult(fab,0,0,nComp);
 		vwFabSqrd.mult(refMult[iLevel]);
 		
-		Real* tmp = 0;
-		int   tmplen = 0;
-		
-
 		for (int iComp=0; iComp<nComp+sComp; ++iComp)
 		{
 		  n1[iComp] += vwFab.norm(fabbox, 1, iComp, 1);
 		  n2[iComp] += vwFabSqrd.norm(fabbox, 1, iComp, 1);
-
-		  delete [] tmp;
-
 		}
 	    }
 	    
@@ -141,10 +134,12 @@ ComputeAmrDataMeanVar  (AmrData&     amrData,
 	    }
 	}
     }
-    for (int iComp=0; iComp<nComp+sComp; ++iComp)
-    {
+    if (ParallelDescriptor::IOProcessor()) {
+      for (int iComp=0; iComp<nComp+sComp; ++iComp)
+      {
 	mean[iComp] /= total_volume;
 	variance[iComp] = variance[iComp]/total_volume - mean[iComp]*mean[iComp];
+      }
     }
 
     if (ParallelDescriptor::IOProcessor() && verbose)
@@ -159,7 +154,9 @@ ComputeAmrDataMeanVar  (AmrData&     amrData,
 }
 
 
+
 // take a list of files and find their mean and variance.
+// this is not really correct.
 void
 ComputeAmrDataList  (AmrData&         amrData,
 		     Array<MultiFab*> mean,
@@ -538,6 +535,327 @@ ComputeAmrDataPDF (AmrData&     amrData,
 // 	std::cout << "Writing zeroed state to " << oFile << '\n';
 // 	WritePlotFile(error, amrData, oFile, verbose);
 //     }
+}
+
+// Compute the correlation in space
+void
+ComputeAmrDataVAR (AmrData&           amrData,
+		   int                nBin,
+		   Array<std::string> cNames,
+		   Array<Real>        barr,
+		   std::string        oFile)
+{
+
+    bool do_normalize = false;
+
+    int finestLevel = amrData.FinestLevel();
+    int nComp = cNames.size();
+
+    Box domain = amrData.ProbDomain()[0];
+    vector<Real> bbll,bbur;
+    BL_ASSERT(barr.size()==2*BL_SPACEDIM);
+    bbll.resize(BL_SPACEDIM);
+    bbur.resize(BL_SPACEDIM);
+    for (int i=0; i<BL_SPACEDIM; ++i)
+    {
+      bbll[i] = barr[i];
+      bbur[i] = barr[BL_SPACEDIM+i];
+    }
+
+    // Find coarse-grid coordinates of bounding box, round outwardly
+    for (int i=0; i<BL_SPACEDIM; ++i) {
+      const Real dx = amrData.ProbSize()[i] / amrData.ProbDomain()[0].length(i);            
+      domain.setSmall(i,std::max(domain.smallEnd()[i], 
+				 (int)((bbll[i]-amrData.ProbLo()[i]+.0001*dx)/dx)));
+      domain.setBig(i,std::min(domain.bigEnd()[i], 
+			       (int)((bbur[i]-amrData.ProbLo()[i]-.0001*dx)/dx)));
+    }
+    
+    for (int i=1; i<=finestLevel; i++)
+      domain.refine(amrData.RefRatio()[i]);
+
+    IntVect sm = domain.smallEnd();
+    IntVect bg = domain.bigEnd();
+
+    // Initialize covariance, correlation and variogram matrices.
+    Real* covx[nComp], * covy[nComp];
+    Real* corx[nComp], * cory[nComp];
+    Real* varx[nComp], * vary[nComp]; 
+    for (int ic=0; ic<nComp; ic++) {
+      covx[ic]=new Real[nBin+1];
+      corx[ic]=new Real[nBin+1];
+      varx[ic]=new Real[nBin+1];
+      covy[ic]=new Real[nBin+1];
+      cory[ic]=new Real[nBin+1];
+      vary[ic]=new Real[nBin+1];
+      
+      for (int iBin=0; iBin<=nBin; iBin++) { 
+	covx[ic][iBin] = 0.0;
+	corx[ic][iBin] = 0.0;
+	varx[ic][iBin] = 0.0;
+	covy[ic][iBin] = 0.0;
+	cory[ic][iBin] = 0.0;
+	vary[ic][iBin] = 0.0;
+      }
+    }
+
+    // for 2D only for now
+    // In x-direction
+    int sz_in_y = bg[1]-sm[1]+1;
+    BoxArray bax(sz_in_y);
+    IntVect smx = sm;
+    IntVect bgx = bg;
+    for (int i=0; i<sz_in_y; i++) {
+      smx[1] = sm[1]+i;
+      bgx[1] = sm[1]+i;
+      Box bx(sm,bg);
+      bax.set(i,bx);
+    }
+
+    Array<int> destFillComps(nComp);
+    for (int i=0; i<nComp; ++i)
+      destFillComps[i] = i;
+    MultiFab tmpx(bax,nComp,0);
+    amrData.FillVar(tmpx,amrData.FinestLevel(),cNames,destFillComps);
+
+    int  Nh[nBin+1];
+    Real m0[nComp][nBin+1], mh[nComp][nBin+1];
+    Real s0[nComp][nBin+1], sh[nComp][nBin+1];
+
+    for (int iBin=0; iBin<=nBin; iBin++) { 
+      Nh[iBin] = 0;
+      for (int iComp=0; iComp<nComp; iComp++) {
+	m0[iComp][iBin] = 0.0;
+	mh[iComp][iBin] = 0.0;
+	s0[iComp][iBin] = 0.0;
+	sh[iComp][iBin] = 0.0;
+      }
+    }
+    Array<Real> mean(nComp,0.0), variance(nComp,0.0);
+    Real volume = 0.0;
+    for (MFIter mfi(tmpx); mfi.isValid(); ++mfi) {
+
+      const int* lo = tmpx[mfi].loVect();
+      const int* hi = tmpx[mfi].hiVect();
+
+      FArrayBox& fab = (tmpx)[mfi];
+      const Box& fabbox = mfi.validbox();
+
+      FArrayBox vwFab(fabbox,nComp);
+      FArrayBox vwFabSqrd(fabbox,nComp);
+
+      // sum and sum-squared
+      vwFab.copy(fab,0,0,nComp);
+      vwFabSqrd.copy(fab,0,0,nComp);
+      
+      for (int iComp=0; iComp<nComp; ++iComp)
+      {
+	mean[iComp] += vwFab.norm(fabbox, 1, iComp, 1);
+	variance[iComp] += vwFabSqrd.norm(fabbox, 1, iComp, 1);
+      }
+
+      volume += fabbox.numPts();
+      
+      for (int i=0; i<=nBin; i++) {
+
+	Real tmp, tmpp1;
+
+	for (int iy=lo[1]; iy<=hi[1]; iy++) {
+	  for (int ix=lo[0]; ix<hi[0]-i; ix++) {
+
+	    Nh[i] = Nh[i] + 1;
+	    for (int ic=0; ic<nComp; ic++) {
+	      tmp = tmpx[mfi](IntVect(ix,iy),ic);
+	      tmpp1 = tmpx[mfi](IntVect(ix+i,iy),ic);
+	      
+	      covx[ic][i] = covx[ic][i]+ tmp*tmpp1;
+	      varx[ic][i] = varx[ic][i]+ (tmpp1 - tmp)*(tmpp1 - tmp);
+	      m0[ic][i]   = m0[ic][i] + tmp;
+	      mh[ic][i]   = mh[ic][i] + tmpp1;
+	      s0[ic][i]   = s0[ic][i] + tmp*tmp;
+	      sh[ic][i]   = sh[ic][i] + tmpp1*tmpp1;
+
+	    }
+	  }
+	}
+      }
+    }
+
+    const int IOProc = ParallelDescriptor::IOProcessorNumber();
+
+    ParallelDescriptor::ReduceIntSum(Nh,nBin+1,IOProc);
+    ParallelDescriptor::ReduceRealSum(volume);
+    for (int iComp=0; iComp<nComp; iComp++) {
+      ParallelDescriptor::ReduceRealSum(covx[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(m0[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(mh[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(s0[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(sh[iComp],nBin+1,IOProc);
+
+      ParallelDescriptor::ReduceRealSum(mean[iComp]);
+      ParallelDescriptor::ReduceRealSum(variance[iComp]);
+
+    }
 
 
+
+    if (ParallelDescriptor::IOProcessor()) {
+      for (int iComp=0; iComp<nComp; iComp++) {
+	
+	mean[iComp] /= volume;
+	variance[iComp] = variance[iComp]/volume - mean[iComp]*mean[iComp];
+
+	for (int iBin=0; iBin<=nBin; iBin++) { 
+	  m0[iComp][iBin] /= Nh[iBin];
+	  mh[iComp][iBin] /= Nh[iBin];
+	  s0[iComp][iBin] = s0[iComp][iBin]/Nh[iBin] - m0[iComp][iBin];
+	  sh[iComp][iBin] = sh[iComp][iBin]/Nh[iBin] - mh[iComp][iBin];
+
+	  covx[iComp][iBin] = covx[iComp][iBin]/Nh[iBin] -
+	    m0[iComp][iBin]*mh[iComp][iBin];
+	  corx[iComp][iBin] = covx[iComp][iBin]/sqrt(s0[iComp][iBin]*sh[iComp][iBin]);
+	  varx[iComp][iBin] = 0.5*varx[iComp][iBin]/Nh[iBin];
+
+	  // normalize with mean
+	  if (do_normalize) {
+	    covx[iComp][iBin] /= mean[iComp];
+	    corx[iComp][iBin] /= mean[iComp];
+	    varx[iComp][iBin] /= mean[iComp];
+	  }
+	}
+      }
+    }
+
+    bax.clear();
+    tmpx.clear();
+
+    // in y direction
+    int sz_in_x = bg[0]-sm[0]+1;
+    BoxArray bay(sz_in_x);
+    smx = sm;
+    bgx = bg;
+    for (int i=0; i<sz_in_x; i++) {
+      smx[0] = sm[0]+i;
+      bgx[0] = sm[0]+i;
+      Box bx(sm,bg);
+      bay.set(i,bx);
+    }
+
+    MultiFab tmpy(bay,nComp,0);
+    amrData.FillVar(tmpy,amrData.FinestLevel(),cNames,destFillComps);
+
+    for (int iBin=0; iBin<=nBin; iBin++) { 
+      Nh[iBin] = 0;
+      for (int iComp=0; iComp<nComp; iComp++) {
+	m0[iComp][iBin] = 0.0;
+	mh[iComp][iBin] = 0.0;
+	s0[iComp][iBin] = 0.0;
+	sh[iComp][iBin] = 0.0;
+      }
+    }
+
+    for (MFIter mfi(tmpy); mfi.isValid(); ++mfi) {
+
+      const int* lo = tmpy[mfi].loVect();
+      const int* hi = tmpy[mfi].hiVect();
+
+      for (int i=0; i<=nBin; i++) {
+
+	Real tmp, tmpp1;
+
+	for (int ix=lo[0]; ix<=hi[0]; ix++) {
+	  for (int iy=lo[1]; iy<hi[1]-i; iy++) {
+
+	    Nh[i] = Nh[i] + 1;
+	    for (int ic=0; ic<nComp; ic++) {
+	      tmp = tmpy[mfi](IntVect(ix,iy),ic);
+	      tmpp1 = tmpy[mfi](IntVect(ix+i,iy),ic);
+	      
+	      covy[ic][i] = covy[ic][i]+ tmp*tmpp1;
+	      vary[ic][i] = vary[ic][i]+ (tmpp1 - tmp)*(tmpp1 - tmp);
+	      m0[ic][i]   = m0[ic][i] + tmp;
+	      mh[ic][i]   = mh[ic][i] + tmpp1;
+	      s0[ic][i]   = s0[ic][i] + tmp*tmp;
+	      sh[ic][i]   = sh[ic][i] + tmpp1*tmpp1;
+
+	    }
+	  }
+	}
+      }
+    }
+
+    bay.clear();
+    tmpy.clear();
+
+    ParallelDescriptor::ReduceIntSum(Nh,nBin+1,IOProc);
+    for (int iComp=0; iComp<nComp; iComp++) {
+      ParallelDescriptor::ReduceRealSum(covy[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(m0[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(mh[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(s0[iComp],nBin+1,IOProc);
+      ParallelDescriptor::ReduceRealSum(sh[iComp],nBin+1,IOProc);
+    }
+
+    if (ParallelDescriptor::IOProcessor()) {
+      for (int iComp=0; iComp<nComp; iComp++) {
+	for (int iBin=0; iBin<=nBin; iBin++) { 
+	  m0[iComp][iBin] /= Nh[iBin];
+	  mh[iComp][iBin] /= Nh[iBin];
+	  s0[iComp][iBin] = s0[iComp][iBin]/Nh[iBin] - m0[iComp][iBin];
+	  sh[iComp][iBin] = sh[iComp][iBin]/Nh[iBin] - mh[iComp][iBin];
+
+	  covy[iComp][iBin] = covy[iComp][iBin]/Nh[iBin] -
+	    m0[iComp][iBin]*mh[iComp][iBin];
+	  cory[iComp][iBin] = covy[iComp][iBin]/sqrt(s0[iComp][iBin]*sh[iComp][iBin]);
+	  vary[iComp][iBin] = 0.5*vary[iComp][iBin]/Nh[iBin];
+	  
+	  // normalize with mean
+	  if (do_normalize) {
+	    covy[iComp][iBin] /= mean[iComp];
+	    cory[iComp][iBin] /= mean[iComp];
+	    vary[iComp][iBin] /= mean[iComp];
+	  }
+	}
+      }
+    }
+
+
+    if (ParallelDescriptor::IOProcessor()) {
+      
+      std::ofstream outputFile;
+      outputFile.open(oFile.c_str(),std::ios::out);
+      if (outputFile.fail()) BoxLib::Abort("Output file cannot be opened");
+      for (int ic=0; ic<nComp; ic++) {
+	for (int ib=0; ib<=nBin; ib++) 		
+	  outputFile << covx[ic][ib] << " ";
+	outputFile << "\n";
+	for (int ib=0; ib<=nBin; ib++) 		
+	  outputFile << corx[ic][ib] << " ";
+	outputFile << "\n";
+	for (int ib=0; ib<=nBin; ib++) 		
+	  outputFile << varx[ic][ib] << " ";
+	outputFile << "\n";
+	for (int ib=0; ib<=nBin; ib++) 		
+	  outputFile << covy[ic][ib] << " ";
+	outputFile << "\n";
+	for (int ib=0; ib<=nBin; ib++) 		
+	  outputFile << cory[ic][ib] << " ";
+	outputFile << "\n";
+	for (int ib=0; ib<=nBin; ib++) 		
+	  outputFile << vary[ic][ib] << " ";
+	outputFile << "\n";
+
+
+      }
+      outputFile.close();
+    }
+
+    for (int ic=0;ic<nComp;ic++) {
+      delete [] covx[ic];
+      delete [] corx[ic];
+      delete [] varx[ic];
+      delete [] covy[ic];
+      delete [] cory[ic];
+      delete [] vary[ic];
+    }
 }
