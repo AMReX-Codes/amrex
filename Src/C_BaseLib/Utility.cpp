c95f9bb47fab0e23e1f2b0ab210e724e6318eb38
lijewski <lijewski>
2009-12-11 01:06:49 +0000

    Added InvNormDistBest(). This is used instead of the previous "best" option to InvNormDist(). The algorithm is from Applied Statistics Algorithm 241 by Michael Wichura and the C++ implementation by John Burkardt.  It provides machine precision and is much faster than the previous "best" option. It's only about 30% slower than InvNormDist().

--- Src/C_BaseLib/Utility.cpp
+++ Src/C_BaseLib/Utility.cpp
@@ -1,5 +1,5 @@
 //
-// $Id: Utility.cpp,v 1.81 2009-12-09 17:41:57 lijewski Exp $
+// $Id: Utility.cpp,v 1.82 2009-12-11 01:06:49 lijewski Exp $
 //
 
 #include <cstdlib>
@@ -914,7 +914,7 @@ stdnormal_cdf (double u)
 //
 
 double
-BoxLib::InvNormDist (double p, bool best)
+BoxLib::InvNormDist (double p)
 {
     if (p <= 0 || p >= 1)
         BoxLib::Error("BoxLib::InvNormDist(): p MUST be in (0,1)");
@@ -991,27 +991,9 @@ BoxLib::InvNormDist (double p, bool best)
         x = (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r+a[5])*q /
             (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r+1);
     }
-    //
-    // The relative error of the approximation has absolute value less
-    // than 1.15e-9.  One iteration of Halley's rational method (third
-    // order) gives full machine precision.
-    //
-    if (best)
-    {
-        static const double Sqrt_2Pi = 2.5066282746310002;  // sqrt(2*Pi)
-
-        double e = stdnormal_cdf(x) - p;
-        double u = e*Sqrt_2Pi*std::exp(x*x/2);
-
-        x -= u/(1 + x*u/2);
-    }
 
     return x;
 }
-
-//
-// Fortran entry points for BoxLib::InvNormDist().
-//
 
 BL_FORT_PROC_DECL(BLINVNORMDIST,blinvnormdist)(Real* result)
 {
@@ -1020,7 +1002,164 @@ BL_FORT_PROC_DECL(BLINVNORMDIST,blinvnormdist)(Real* result)
     //
     double val = the_generator.d2_value();
 
-    *result = BoxLib::InvNormDist(val,false);
+    *result = BoxLib::InvNormDist(val);
+}
+
+//
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    InvNormDistBest inverts the standard normal CDF.
+//
+//  Discussion:
+//
+//    The result is accurate to about 1 part in 10**16.
+//
+//  Modified:
+//
+//    27 December 2004
+//
+//  Author:
+//
+//    Original FORTRAN77 version by Michael Wichura.
+//    C++ version by John Burkardt.
+//
+//  Reference:
+//
+//    Michael Wichura,
+//    The Percentage Points of the Normal Distribution,
+//    Algorithm AS 241,
+//    Applied Statistics,
+//    Volume 37, Number 3, pages 477-484, 1988.
+//
+//  Parameters:
+//
+//    Input, double P, the value of the cumulative probability 
+//    densitity function.  0 < P < 1.  Fails if P is outside this range.
+//
+//    Output, the normal deviate value with the property that the
+//    probability of a standard normal deviate being less than or equal
+//    to this value is P.
+//
+
+double
+BoxLib::InvNormDistBest (double p)
+
+{
+  static const double a[8] = { 
+    3.3871328727963666080,     1.3314166789178437745e+2,
+    1.9715909503065514427e+3,  1.3731693765509461125e+4,
+    4.5921953931549871457e+4,  6.7265770927008700853e+4,
+    3.3430575583588128105e+4,  2.5090809287301226727e+3 };
+
+  static const double b[8] =
+      {
+    1.0,                       4.2313330701600911252e+1,
+    6.8718700749205790830e+2,  5.3941960214247511077e+3,
+    2.1213794301586595867e+4,  3.9307895800092710610e+4,
+    2.8729085735721942674e+4,  5.2264952788528545610e+3 };
+
+  static const double c[8] = {
+    1.42343711074968357734,     4.63033784615654529590,
+    5.76949722146069140550,     3.64784832476320460504,
+    1.27045825245236838258,     2.41780725177450611770e-1,
+    2.27238449892691845833e-2,  7.74545014278341407640e-4 };
+
+  static const double const1 = 0.180625;
+  static const double const2 = 1.6;
+
+  static const double d[8] = {
+    1.0,                        2.05319162663775882187,
+    1.67638483018380384940,     6.89767334985100004550e-1,
+    1.48103976427480074590e-1,  1.51986665636164571966e-2,
+    5.47593808499534494600e-4,  1.05075007164441684324e-9 };
+
+  static const double e[8] = {
+    6.65790464350110377720,     5.46378491116411436990,
+    1.78482653991729133580,     2.96560571828504891230e-1,
+    2.65321895265761230930e-2,  1.24266094738807843860e-3,
+    2.71155556874348757815e-5,  2.01033439929228813265e-7 };
+
+  static const double f[8] = {
+    1.0,                        5.99832206555887937690e-1,
+    1.36929880922735805310e-1,  1.48753612908506148525e-2,
+    7.86869131145613259100e-4,  1.84631831751005468180e-5,
+    1.42151175831644588870e-7,  2.04426310338993978564e-15 };
+
+  static const double split1 = 0.425;
+  static const double split2 = 5.0;
+
+  double r, value;
+
+  if (p <= 0 || p >= 1)
+      BoxLib::Error("InvNormDistBest(): p MUST be in (0,1)");
+
+  double q = p - 0.5;
+
+  if ( std::fabs ( q ) <= split1 )
+  {
+      r = const1 - q * q;
+
+      double numer = 0.0;
+      for (int i = 7; 0 <= i; i-- )
+          numer = numer * r + a[i];
+
+      double denom = 0.0;
+      for (int i = 7; 0 <= i; i-- )
+          denom = denom * r + b[i];
+
+      value = q * numer / denom;
+  }
+  else
+  {
+      if ( q < 0.0 )
+      {
+          r = p;
+      }
+      else
+      {
+          r = 1.0 - p;
+      }
+
+      r = std::sqrt ( -log ( r ) );
+
+      if ( r <= split2 )
+      {
+          r = r - const2;
+
+          double numer = 0.0;
+          for (int i = 7; 0 <= i; i-- )
+              numer = numer * r + c[i];
+
+          double denom = 0.0;
+          for (int i = 7; 0 <= i; i-- )
+              denom = denom * r + d[i];
+
+          value = numer / denom;
+      }
+      else
+      {
+          r = r - split2;
+
+          double numer = 0.0;
+          for (int i = 7; 0 <= i; i-- )
+              numer = numer * r + e[i];
+
+          double denom = 0.0;
+          for (int i = 7; 0 <= i; i-- )
+              denom = denom * r + f[i];
+
+          value = numer / denom;
+      }
+
+      if ( q < 0.0 )
+      {
+          value = -value;
+      }
+  }
+
+  return value;
 }
 
 BL_FORT_PROC_DECL(BLINVNORMDISTBEST,blinvnormdistbest)(Real* result)
@@ -1030,7 +1169,7 @@ BL_FORT_PROC_DECL(BLINVNORMDISTBEST,blinvnormdistbest)(Real* result)
     //
     double val = the_generator.d2_value();
 
-    *result = BoxLib::InvNormDist(val,true);
+    *result = BoxLib::InvNormDistBest(val);
 }
 
 
