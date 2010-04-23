9918c8846b8f7769edd3949bbf3d2b8ead6d6871
ajnonaka <ajnonaka>
2010-04-23 22:29:28 +0000

    pass in dx to tag_boxes so you can refine based on location

--- Src/F_BaseLib/tag_boxes.f90
+++ Src/F_BaseLib/tag_boxes.f90
@@ -17,10 +17,11 @@ module tag_boxes_module
 
 contains
 
-  subroutine tag_boxes(mf,tagboxes,lev)
+  subroutine tag_boxes(mf,tagboxes,dx,lev)
 
     type( multifab), intent(in   ) :: mf
     type(lmultifab), intent(inout) :: tagboxes
+    real(dp_t)     , intent(in   ) :: dx
     integer        , intent(in   ) :: lev
 
     real(kind = dp_t), pointer :: sp(:,:,:,:)
@@ -37,19 +38,20 @@ contains
        hi =  upb(get_box(tagboxes, i))
        select case (mf%dim)
        case (2)
-          call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,hi,ng,lev)
+          call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,hi,ng,dx,lev)
        case  (3)
-          call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,hi,ng,lev)
+          call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,hi,ng,ex,lev)
        end select
     end do
 
   end subroutine tag_boxes
 
-  subroutine tag_boxes_2d(tagbox,mf,lo,hi,ng,lev)
+  subroutine tag_boxes_2d(tagbox,mf,lo,hi,ng,dx,lev)
 
     integer          , intent(in   ) :: lo(:),hi(:),ng
     logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
     real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
+    real(dp_t)       , intent(in   ) :: dx
     integer, optional, intent(in   ) :: lev
     integer :: i,j,llev
 
@@ -89,11 +91,12 @@ contains
 
   end subroutine tag_boxes_2d
 
-  subroutine tag_boxes_3d(tagbox,mf,lo,hi,ng,lev)
+  subroutine tag_boxes_3d(tagbox,mf,lo,hi,ng,dx,lev)
 
     integer          , intent(in   ) :: lo(:),hi(:),ng
     logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
     real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    real(dp_t)       , intent(in   ) :: dx
     integer, optional, intent(in   ) :: lev
 
     integer :: i,j,k,llev
