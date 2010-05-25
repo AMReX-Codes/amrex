e2d9b789aa3ac9ebbd2b9b55f3a56c9f0f07a6fc
almgren <almgren>
2010-05-25 19:09:30 +0000

    Fix countval arguments.

--- Tools/C_util/Convergence/DiffSameGridRefined.cpp
+++ Tools/C_util/Convergence/DiffSameGridRefined.cpp
@@ -1,6 +1,6 @@
 
 //
-// $Id: DiffSameGridRefined.cpp,v 1.8 2010-02-19 23:40:10 almgren Exp $
+// $Id: DiffSameGridRefined.cpp,v 1.9 2010-05-25 19:09:30 almgren Exp $
 //
 
 #include <new>
@@ -349,7 +349,7 @@ getRefRatio(const Box& crse,
     // IntVect if there is none suitable
     ParmParse pp("");
     Array<int> rr_in(BL_SPACEDIM,-1);
-    int Nrr = pp.countval("ref_ratio",Nrr);
+    int Nrr = pp.countval("ref_ratio");
     BL_ASSERT(Nrr==0 || Nrr==BL_SPACEDIM || Nrr==1);
     if (Nrr>0)
     {
