3db71d39d3a3da01daefa3945ce091e0e418608c
almgren <almgren>
2010-05-25 21:24:14 +0000

    Allow different number of state variables.

--- Tools/C_util/Convergence/DiffSameDomainRefined.cpp
+++ Tools/C_util/Convergence/DiffSameDomainRefined.cpp
@@ -1,6 +1,6 @@
 
 //
-// $Id: DiffSameDomainRefined.cpp,v 1.2 2010-05-25 21:12:18 almgren Exp $
+// $Id: DiffSameDomainRefined.cpp,v 1.3 2010-05-25 21:24:14 almgren Exp $
 //
 
 #include <new>
@@ -126,11 +126,14 @@ main (int   argc,
     //
     // Initial Tests 
     //
-    if (!amrDatasHaveSameDerives(amrData1,amrData2))
-        BoxLib::Abort("ERROR: Plotfiles do not have the same state variables");
-
     if (amrData1.FinestLevel() != amrData2.FinestLevel())
         BoxLib::Abort("ERROR: Finest level is not the same in the two plotfiles");
+
+    if (!amrDatasHaveSameDerives(amrData1,amrData2))
+    {
+        std::cout << "Warning: Plotfiles do not have the same state variables" << std::endl;
+        std::cout << "         Setting nComp to be the minimum number of variables " << std::endl;
+    }
 
     int nComp1      = amrData1.NComp();
     int nComp2      = amrData2.NComp();
