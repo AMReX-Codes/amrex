d311f9fdd6c8bacfafd15b44d2aec9ae30040db1
gpau <gpau>
2010-08-12 00:12:50 +0000

    *** empty log message ***

--- Tools/C_util/Convergence/ComputeAmrDataNorms.cpp
+++ Tools/C_util/Convergence/ComputeAmrDataNorms.cpp
@@ -151,3 +151,117 @@ ComputeAmrDataNorms (AmrData&     amrData,
     for (int iLevel = 0; iLevel <= finestLevel; ++iLevel)
 	delete error[iLevel];
 }
+
+
+void
+ComputeAmrDataInt (AmrData&     amrData,
+		   Array<Real>& norm1,
+		   bool         verbose)
+{
+    std::string oFile, iFileDir, oFileDir;
+    
+    if (verbose)
+    {
+	ParmParse pp;
+	pp.query("outfile", oFile);
+	if (oFile.empty())
+	    BoxLib::Abort("You must specify `outfile' if run in verbose mode");
+    }
+    
+    int finestLevel = amrData.FinestLevel();
+    int nComp       = amrData.NComp();
+
+    norm1.clear(); norm1.resize(nComp,0.0);
+    
+    Array<int> refMult(finestLevel + 1, 1);
+    for (int iLevel=finestLevel-1; iLevel>=0; --iLevel)
+    {
+	int ref_ratio = amrData.RefRatio()[iLevel];
+	int vol = 1;
+	for (int i=0; i<BL_SPACEDIM; ++i)
+	    vol *= ref_ratio;
+	for (int jLevel=0; jLevel<=iLevel; ++jLevel)
+	    refMult[jLevel] *= vol;
+    }
+
+    //
+    // Compute the norms
+    //
+    long total_volume = 0;
+    Array<MultiFab*> error(finestLevel+1);
+    
+    for (int iLevel = finestLevel; iLevel>=0; --iLevel)
+    {
+        const BoxArray& ba = amrData.boxArray(iLevel);
+
+	error[iLevel] = new MultiFab(ba, nComp, 0);
+	for (int iComp=0; iComp<nComp; ++iComp)
+	{
+	    MultiFab& data = amrData.GetGrids(iLevel,iComp);
+	    error[iLevel]->copy(data,0,iComp,1);
+	}
+
+	// Zero out the error covered by fine grid
+	long covered_volume = 0;
+	if (iLevel != finestLevel)
+	{
+	    int ref_ratio = amrData.RefRatio()[iLevel];	    
+	    BoxArray baF = ::BoxArray(amrData.boxArray(iLevel+1)).coarsen(ref_ratio);
+	    for (MFIter mfi(*error[iLevel]); mfi.isValid(); ++mfi)
+	    {
+		for (int iGrid=0; iGrid<baF.size(); ++iGrid)
+		{
+		    Box ovlp = baF[iGrid] & mfi.validbox();
+		    if (ovlp.ok())
+		    {
+                        (*error[iLevel])[mfi].setVal(0.0, ovlp, 0, nComp);
+			covered_volume += ovlp.numPts()*refMult[iLevel];
+		    }
+		}
+	    }
+	    ParallelDescriptor::ReduceLongSum(covered_volume);
+	}
+
+	// Compute volume at this level
+	Real level_volume = 0.0;
+	for (int iGrid=0; iGrid<ba.size(); ++iGrid)
+	    level_volume += ba[iGrid].numPts()*refMult[iLevel];
+	level_volume -= covered_volume;
+
+	if (level_volume > 0.0)
+	{
+	    // Convert volume in numPts to volume in number of fine cells
+	    total_volume += long(level_volume);
+	    
+	    // Get norms at this level
+	    Array<Real> n1(nComp,0.0);
+	    for (MFIter mfi(*error[iLevel]); mfi.isValid(); ++mfi)
+	    {
+		FArrayBox& fab = (*error[iLevel])[mfi];
+		const Box& fabbox = mfi.validbox();
+		FArrayBox vwFab(fabbox,nComp);
+			
+		// compute volume-weighted error
+		vwFab.copy(fab,0,0,nComp);
+		for (int iComp=0; iComp<nComp; ++iComp)
+		{
+		    n1[iComp] += vwFab.sum(fabbox, iComp, 1);
+		}
+	    }
+
+	    // Do necessary communication, then blend this level's norms
+	    //  in with the running global values
+	    const Real *dx = amrData.DxLevel()[iLevel].dataPtr();
+	    for (int iComp=0; iComp<nComp; ++iComp)
+	    {
+		ParallelDescriptor::ReduceRealSum(n1[iComp]);
+		norm1[iComp] += n1[iComp]*dx[0]*dx[1];
+	    }
+	}
+    }
+    
+    // Clean up memory
+    for (int iLevel = 0; iLevel <= finestLevel; ++iLevel)
+	delete error[iLevel];
+}
+
