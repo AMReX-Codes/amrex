8574335147d76ba18c1a5845333ff8de12ad1389
marc <marc>
2011-04-08 01:34:01 +0000

    *** empty log message ***

---
+++ Tools/C_util/ViewMF/viewMFcol.cpp
@@ -0,0 +1,89 @@
+#ifdef BL_ARCH_CRAY
+#ifdef BL_USE_DOUBLE
+#error "DOUBLE PRECISION NOT ALLOWED ON CRAY"
+#endif
+#endif
+
+#ifndef        WIN32
+#include <unistd.h>
+#endif
+
+#include "MultiFab.H"
+//#include "ArrayView.H"
+#include "ParmParse.H"
+#include "Utility.H"
+#include "ParallelDescriptor.H"
+#include "TV_TempWrite.H"
+
+#include <new>
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::cerr;
+
+#ifndef WIN32
+using std::set_new_handler;
+#endif
+
+static
+void 
+PrintUsage(int argc, char *argv[])
+{
+    cout << "Usage: " << endl;
+    cout << argv[0] << " infile [options] \n\tOptions:" << endl;
+    cout << "\t   -ascii   \t[if set, dump ascii mfab to stdout]" << endl;
+    cout << "\t   ngrow=<#>\t[number of grow cells to include in output]" << endl;
+    cout << endl;
+    exit(0);
+}
+
+
+int
+main (int   argc,
+      char* argv[])
+{
+    BoxLib::Initialize(argc,argv);
+//
+//  Parse the command line
+//
+    if (argc < 2)
+        PrintUsage(argc,argv);
+
+    ParmParse pp;
+    
+    std::string iFile; pp.get("iFile", iFile);
+//
+//  Read multifab
+//
+    MultiFab mf;
+    VisMF::Read(mf,iFile);
+
+    
+    int sComp = 0; pp.query("sComp",sComp); sComp=std::min(sComp,mf.nComp()-1);
+    int nComp = mf.nComp(); pp.query("nComp",nComp);
+    if (nComp==-1) nComp = mf.nComp();
+    nComp = std::min(mf.nComp()-sComp,nComp);
+    
+    Box domain = mf.boxArray().minimalBox();
+    int dir = 1; pp.query("dir",dir);
+
+    int idx = (int) (0.5*(domain.smallEnd()[dir] + domain.bigEnd()[dir])); pp.query("idx",idx);
+    
+    for (int d=0; d<BL_SPACEDIM; ++d) {
+        if (d!=dir) {            
+            domain.setSmall(dir,idx);
+            domain.setBig(dir,idx);
+        }
+    }
+
+    FArrayBox fab(domain,nComp);
+    mf.copy(fab,sComp,0,nComp);
+
+    cout << "Components: " << sComp << " : " << sComp + nComp - 1 << endl;
+    cout << fab << endl;
+
+    BoxLib::Finalize();
+    
+    return 0;
+}
