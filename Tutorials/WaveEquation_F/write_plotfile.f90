80e66de4936530af228b896a580d015f4d641708
ajnonaka <AJNonaka@lbl.gov>
2011-11-18 17:33:20 -0800

    lots of comments in code, wrote small section in User's guide.  This could actually be released now

--- Tutorials/WaveEquation_F/write_plotfile.f90
+++ Tutorials/WaveEquation_F/write_plotfile.f90
@@ -16,6 +16,7 @@ contains
     real(dp_t)     , intent(in   ) :: dx(mla%nlevel,mla%dim), time
     real(dp_t)     , intent(in   ) :: prob_lo(mla%dim), prob_hi(mla%dim)
 
+    ! local variables
     type(multifab) :: plotdata(mla%dim)
 
     integer           :: n, nlevs
@@ -28,16 +29,21 @@ contains
     plot_names(2) = "Variable 2"
 
     do n=1,nlevs
+       ! build plotdata with 2 components and 0 ghost cells
        call multifab_build(plotdata(n),mla%la(n),2,0)
+       ! copy the state into plotdata
        call multifab_copy_c(plotdata(n),1,data(n),1,2)
     end do
 
+    ! define the name of the plotfile that will be written
     write(unit=sd_name,fmt='("plt",i5.5)') istep_to_write
 
+    ! write the plotfile
     call fabio_ml_multifab_write_d(plotdata, mla%mba%rr(:,1), sd_name, plot_names, &
                                    mla%mba%pd(1), prob_lo, prob_hi, time, dx(1,:))
 
-    do n = 1,nlevs
+    do n=1,nlevs
+       ! make sure to destroy the multifab or you'll leak memory
        call multifab_destroy(plotdata(n))
     end do
 
