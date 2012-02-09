c275e7e0515eb79b23595186214cf569aa4d0cd3
Michael Zingale <mzingale@mail.astro.sunysb.edu>
2012-02-08 20:00:43 -0500

    make the problem names more unique.  Eliminate docs

--- Tools/C_util/regtests/gen_compile_test.sh
+++ Tools/C_util/regtests/gen_compile_test.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+# script to automatically generate the test parameters for compile tests
+# for MAESTRO problems.  Run this in the MAESTRO/ directory.
+
 for i in `find . -name GNUmakefile -print`
 do 
-   temp=`dirname $i`
-   echo [`basename $temp`]
-   echo buildDir = fParallel/MAESTRO/`dirname $i`
-   echo compileTest = 1
-   echo " "
+   buildDir=`dirname $i`
+   topDir=`dirname $buildDir`
+
+   category=`basename $topDir`
+   problem=`basename $buildDir`
+
+   if [ "$category" != "docs" ]; then
+       if [ "$problem" != "docs" ]; then
+	   echo [$category-$problem]
+	   echo buildDir = fParallel/MAESTRO/$buildDir
+	   echo compileTest = 1
+	   echo " "
+       fi
+   fi
 done
 
