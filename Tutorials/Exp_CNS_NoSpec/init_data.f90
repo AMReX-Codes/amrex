d84057616f447f2a41196ea68783ac817b866c1a
lijewski <mjlijewski@lbl.gov>
2012-02-14 11:12:04 -0800

    OMP init_data

--- Tutorials/Exp_CNS_NoSpec/init_data.f90
+++ Tutorials/Exp_CNS_NoSpec/init_data.f90
@@ -54,16 +54,17 @@ contains
 
     scale = 0.02d0
 
-    do k=lo(3)-ng,hi(3)+ng
+    !$OMP PARALLEL DO PRIVATE(i,j,k,zloc,yloc,xloc,uvel,vvel,wvel,rholoc,eloc)
+    do k=lo(3),hi(3)
        zloc = dfloat(k)*dx(3)
-       do j=lo(2)-ng,hi(2)+ng
+       do j=lo(2),hi(2)
           yloc = dfloat(j)*dx(2)
-          do i=lo(1)-ng,hi(1)+ng
+          do i=lo(1),hi(1)
              xloc = dfloat(i)*dx(1)
 
-             uvel   = sin(xloc/scale)*sin(2*yloc/scale)*sin(3.d0*zloc/scale)
-             vvel   = sin(2*xloc/scale)*sin(4.d0*yloc/scale)*sin(1.d0*zloc/scale)
-             wvel   = sin(3.d0*xloc/scale)*cos(2*yloc/scale)*sin(2*zloc/scale)
+             uvel   = sin(xloc/scale)*sin(2*yloc/scale)*sin(3*zloc/scale)
+             vvel   = sin(2*xloc/scale)*sin(4*yloc/scale)*sin(zloc/scale)
+             wvel   = sin(3*xloc/scale)*cos(2*yloc/scale)*sin(2*zloc/scale)
              rholoc = 1.0d-3+1.0d-5*sin(xloc/scale)*cos(2*yloc/scale)*cos(3*zloc/scale)
              eloc   = 2.5d9+1.0d-3*sin(2*xloc/scale)*cos(2*yloc/scale)*sin(2*zloc/scale)
 
@@ -71,11 +72,12 @@ contains
              cons(i,j,k,imx)  = rholoc*uvel
              cons(i,j,k,imy)  = rholoc*vvel
              cons(i,j,k,imz)  = rholoc*wvel
-             cons(i,j,k,iene) = rholoc*(eloc+(uvel**2+vvel**2+wvel**2)/2)
+             cons(i,j,k,iene) = rholoc*(eloc + (uvel**2+vvel**2+wvel**2)/2)
 
           enddo
        enddo
     enddo
+    !$OMP END PARALLEL DO
 
   end subroutine init_data_3d
 
