b3dd826a12e437b68f052c78e516c6c2da3dac4b
lijewski <mjlijewski@lbl.gov>
2012-02-14 15:52:12 -0800

    Made write_plotfile() more generic.

--- Tutorials/Exp_CNS_NoSpec/write_plotfile.f90
+++ Tutorials/Exp_CNS_NoSpec/write_plotfile.f90
@@ -8,45 +8,46 @@ module write_plotfile_module
 
 contains
   
-  subroutine write_plotfile(la,data,istep,dx,time,prob_lo,prob_hi)
+  subroutine write_plotfile(data,istep,dx,time,prob_lo,prob_hi)
 
-    type(layout)   , intent(in   ) :: la
-    type(multifab) , intent(in   ) :: data
-    integer        , intent(in   ) :: istep
-    real(dp_t)     , intent(in   ) :: dx(data%dim),time
-    real(dp_t)     , intent(in   ) :: prob_lo(data%dim), prob_hi(data%dim)
+    type(multifab), intent(in   ) :: data
+    integer       , intent(in   ) :: istep
+    real(dp_t)    , intent(in   ) :: dx(data%dim),time
+    real(dp_t)    , intent(in   ) :: prob_lo(data%dim), prob_hi(data%dim)
 
-    ! local variables
-    character(len=8)  :: plotfile_name
-    character(len=20) :: variable_names(5)
+    character(len=2)               :: varname
+    character(len=8)               :: plotfile_name
+    character(len=20), allocatable :: variable_names(:)
+    type(layout)                   :: la
 
     ! dimensioned as an array of size 1 for fabio_ml_multifab_write_d
     type(multifab) :: plotdata(1)
 
     ! dimensioned as an array of size 0 for fabio_ml_multifab_write_d
-    integer :: rr(0)
+    integer :: rr(0), i, nc
 
-    variable_names(1) = "Variable 1"
-    variable_names(2) = "Variable 2"
-    variable_names(3) = "Variable 3"
-    variable_names(4) = "Variable 4"
-    variable_names(5) = "Variable 5"
+    la = get_layout(data)
 
-    ! build plotdata with 5 components and 0 ghost cells
-    call multifab_build(plotdata(1),la,5,0)
-    ! copy the state into plotdata
+    nc = ncomp(data)
+
+    allocate(variable_names(nc))
+
+    do i = 1, nc
+       write(unit=varname,fmt='(i2.2)') i
+       variable_names(i) = 'Variable_' // varname
+    end do
+
+    call multifab_build(plotdata(1),la,nc,0)
+
     call multifab_copy_c(plotdata(1),1,data,1,5)
 
-    ! define the name of the plotfile that will be written
     write(unit=plotfile_name,fmt='("plt",i5.5)') istep
 
     if ( parallel_IOProcessor() ) print*, 'Writing ' // plotfile_name // ' ...'
 
-    ! write the plotfile
     call fabio_ml_multifab_write_d(plotdata, rr, plotfile_name, variable_names, &
                                    la%lap%pd, prob_lo, prob_hi, time, dx)
 
-    ! make sure to destroy the multifab or you'll leak memory
     call multifab_destroy(plotdata(1))
 
   end subroutine write_plotfile
