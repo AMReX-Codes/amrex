efda4046347062a43653a3a125ce618d841200e6
Weiqun Zhang <weiqunzhang@lbl.gov>
2012-02-14 16:01:50 -0800

    Fix bugs for 2D inflow boundary.  3D still needs to be fixed.

--- Src/LinearSolvers/F_MG/nodal_divu.f90
+++ Src/LinearSolvers/F_MG/nodal_divu.f90
@@ -275,10 +275,10 @@ contains
       do i = 1,dm
          call ml_crse_divu_contrib(temp_rhs_crse, brs_flx%bmf(i,0), u(n_crse), &
                                    mgt(n_fine)%mm(mglev_fine), mgt(n_crse)%dh(:,mglev_crse), &
-                                   pdc,ref_ratio, -i)
+                                   pdc,ref_ratio, -i, lo_inflow, hi_inflow)
          call ml_crse_divu_contrib(temp_rhs_crse, brs_flx%bmf(i,1), u(n_crse), &
                                    mgt(n_fine)%mm(mglev_fine), mgt(n_crse)%dh(:,mglev_crse), &
-                                   pdc,ref_ratio, +i)
+                                   pdc,ref_ratio, +i, lo_inflow, hi_inflow)
       end do
 
       call multifab_plus_plus(rh_crse,temp_rhs_crse)
@@ -457,7 +457,8 @@ contains
 
 !   ********************************************************************************************* !
 
-    subroutine ml_crse_divu_contrib(rh, flux, u, mm, dx, crse_domain, ir, side)
+    subroutine ml_crse_divu_contrib(rh, flux, u, mm, dx, crse_domain, ir, side, &
+         lo_inflow, hi_inflow)
      type(multifab), intent(inout) :: rh
      type(multifab), intent(inout) :: flux
      type(multifab), intent(in   ) :: u
@@ -466,6 +467,7 @@ contains
      type(box)      ,intent(in   ) :: crse_domain
      integer        ,intent(in   ) :: ir(:)
      integer        ,intent(in   ) :: side
+     integer        ,intent(in   ) :: lo_inflow(:), hi_inflow(:)
 
      type(box) :: fbox, ubox, mbox, isect
 
@@ -530,15 +532,18 @@ contains
              case (1)
                 call ml_interface_1d_divu(rp(:,1,1,1), lor, &
                      fp(:,1,1,1), lof, &
-                     up(:,1,1,1), lou, mp(:,1,1,1), lom, lo, ir, side, dx)
+                     up(:,1,1,1), lou, mp(:,1,1,1), lom, lo, ir, side, dx, &
+                     lo_inflow, hi_inflow)
              case (2)
                 call ml_interface_2d_divu(rp(:,:,1,1), lor, &
                      fp(:,:,1,1), lof, lof, hif, &
-                     up(:,:,1,:), lou, mp(:,:,1,1), lom, lo, hi, ir, side, dx)
+                     up(:,:,1,:), lou, mp(:,:,1,1), lom, lo, hi, ir, side, dx, &
+                     lo_inflow, hi_inflow)
              case (3)
                 call ml_interface_3d_divu(rp(:,:,:,1), lor, &
                      fp(:,:,:,1), lof, lof, hif,  &
-                     up(:,:,:,:), lou, mp(:,:,:,1), lom, lo, hi, ir, side, dx)
+                     up(:,:,:,:), lou, mp(:,:,:,1), lom, lo, hi, ir, side, dx, &
+                     lo_inflow, hi_inflow)
              end select
 
           else if ( local(flux,i) ) then
@@ -571,15 +576,18 @@ contains
              case (1)
                 call ml_interface_1d_divu(rp(:,1,1,1), lor, &
                      fp(:,1,1,1), lo, &
-                     up(:,1,1,1), lou, mp(:,1,1,1), lom, lo, ir, side, dx)
+                     up(:,1,1,1), lou, mp(:,1,1,1), lom, lo, ir, side, dx, &
+                     lo_inflow, hi_inflow)
              case (2)
                 call ml_interface_2d_divu(rp(:,:,1,1), lor, &
                      fp(:,:,1,1), lo, lof, hif, &
-                     up(:,:,1,:), lou, mp(:,:,1,1), lom, lo, hi, ir, side, dx)
+                     up(:,:,1,:), lou, mp(:,:,1,1), lom, lo, hi, ir, side, dx, &
+                     lo_inflow, hi_inflow)
              case (3)
                 call ml_interface_3d_divu(rp(:,:,:,1), lor, &
                      fp(:,:,:,1), lo, lof, hif, &
-                     up(:,:,:,:), lou, mp(:,:,:,1), lom, lo, hi, ir, side, dx)
+                     up(:,:,:,:), lou, mp(:,:,:,1), lom, lo, hi, ir, side, dx, &
+                     lo_inflow, hi_inflow)
              end select
              deallocate(fp,mp)
           end if
@@ -593,7 +601,7 @@ contains
 !   ********************************************************************************************* !
 
     subroutine ml_interface_1d_divu(rh, lor, fine_flux, lof, uc, loc, &
-                                    mm, lom, lo, ir, side, dx)
+                                    mm, lom, lo, ir, side, dx, lo_inflow, hi_inflow)
     integer, intent(in) :: lor(:)
     integer, intent(in) :: loc(:)
     integer, intent(in) :: lom(:)
@@ -606,6 +614,7 @@ contains
     integer           , intent(in   ) :: ir(:)
     integer           , intent(in   ) :: side
     real(kind=dp_t)   , intent(in   ) :: dx(:)
+    integer, intent(in) :: lo_inflow(:), hi_inflow(:)
 
     integer :: i
     real (kind = dp_t) :: crse_flux,fac
@@ -645,7 +654,7 @@ contains
 !   ********************************************************************************************* !
 
     subroutine ml_interface_2d_divu(rh, lor, fine_flux, lof, loflx, hiflx, uc, loc, &
-                                    mm, lom, lo, hi, ir, side, dx)
+                                    mm, lom, lo, hi, ir, side, dx, lo_inflow, hi_inflow)
     integer, intent(in) :: lor(:)
     integer, intent(in) :: loc(:)
     integer, intent(in) :: lom(:)
@@ -659,6 +668,7 @@ contains
     integer           , intent(in   ) :: ir(:)
     integer           , intent(in   ) :: side
     real(kind=dp_t)   , intent(in   ) :: dx(:)
+    integer, intent(in) :: lo_inflow(:), hi_inflow(:)
 
     integer :: i, j
     real (kind = dp_t) :: crse_flux,fac
@@ -680,7 +690,11 @@ contains
           if (bc_dirichlet(mm(ir(1)*i,ir(2)*j),1,0)) then
              if (j == loflx(2)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),2,-1)) then
-                   crse_flux =        (uc(i,j,1)/dx(1) + uc(i,j,2)/dx(2))
+                   if (lo_inflow(2) .eq. 1) then
+                      crse_flux = uc(i,j,1)/dx(1) + (uc(i,j,2) - uc(i,j-1,2))/dx(2)
+                   else
+                      crse_flux =        (uc(i,j,1)/dx(1) + uc(i,j,2)/dx(2))
+                   end if
                 else 
                    ! We have FOURTH rather than HALF here because
                    ! point (i,j) will be touched again when side == -2.
@@ -690,7 +704,11 @@ contains
                 end if
              else if (j == hiflx(2)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),2,+1)) then
-                   crse_flux =        (uc(i,j-1,1)/dx(1) - uc(i,j-1,2)/dx(2))
+                   if (hi_inflow(2) .eq. 1) then
+                      crse_flux = uc(i,j-1,1)/dx(1) + (uc(i,j,2) - uc(i,j-1,2))/dx(2)
+                   else
+                      crse_flux =        (uc(i,j-1,1)/dx(1) - uc(i,j-1,2)/dx(2))
+                   end if
                 else 
                    crse_flux = FOURTH*(uc(i,j-1,1)/dx(1) - uc(i,j-1,2)/dx(2))
                 end if
@@ -713,13 +731,21 @@ contains
           if (bc_dirichlet(mm(ir(1)*i,ir(2)*j),1,0)) then
              if (j == loflx(2)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),2,-1)) then
-                   crse_flux =        (-uc(i-1,j,1)/dx(1) + uc(i-1,j,2)/dx(2))
+                   if (lo_inflow(2) .eq. 1) then
+                      crse_flux = -uc(i-1,j,1)/dx(1) + (uc(i-1,j,2)-uc(i-1,j-1,2))/dx(2)
+                   else
+                      crse_flux =        (-uc(i-1,j,1)/dx(1) + uc(i-1,j,2)/dx(2))
+                   end if
                 else
                    crse_flux = FOURTH*(-uc(i-1,j,1)/dx(1) + uc(i-1,j,2)/dx(2))
                 end if 
              else if (j == hiflx(2)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),2,+1)) then
-                   crse_flux =        (-uc(i-1,j-1,1)/dx(1) - uc(i-1,j-1,2)/dx(2))
+                   if (hi_inflow(2) .eq. 1) then
+                      crse_flux = -uc(i-1,j-1,1)/dx(1) + (uc(i-1,j,2)-uc(i-1,j-1,2))/dx(2)
+                   else
+                      crse_flux =        (-uc(i-1,j-1,1)/dx(1) - uc(i-1,j-1,2)/dx(2))
+                   end if
                 else 
                    crse_flux = FOURTH*(-uc(i-1,j-1,1)/dx(1) - uc(i-1,j-1,2)/dx(2)) 
                 end if
@@ -742,13 +768,21 @@ contains
           if (bc_dirichlet(mm(ir(1)*i,ir(2)*j),1,0)) then
              if (i == loflx(1)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),1,-1)) then
-                   crse_flux =        (uc(i,j,1)/dx(1) + uc(i,j,2)/dx(2))
+                   if (lo_inflow(1) .eq. 1) then
+                      crse_flux = (uc(i,j,1)-uc(i-1,j,1))/dx(1) + uc(i,j,2)/dx(2)
+                   else
+                      crse_flux =        (uc(i,j,1)/dx(1) + uc(i,j,2)/dx(2))
+                   end if
                 else 
                    crse_flux = FOURTH*(uc(i,j,1)/dx(1) + uc(i,j,2)/dx(2))
                 end if
              else if (i == hiflx(1)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),1,+1)) then
-                   crse_flux =        (-uc(i-1,j,1)/dx(1) + uc(i-1,j,2)/dx(2))
+                   if (hi_inflow(1) .eq. 1) then
+                      crse_flux = (uc(i,j,1)-uc(i-1,j,1))/dx(1) + uc(i-1,j,2)/dx(2)
+                   else
+                      crse_flux =        (-uc(i-1,j,1)/dx(1) + uc(i-1,j,2)/dx(2))
+                   end if
                 else 
                    crse_flux = FOURTH*(-uc(i-1,j,1)/dx(1) + uc(i-1,j,2)/dx(2))
                 end if
@@ -771,14 +805,22 @@ contains
 
              if (i == loflx(1)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),1,-1)) then
-                   crse_flux =        (uc(i,j-1,1)/dx(1) - uc(i,j-1,2)/dx(2))
+                   if (lo_inflow(1) .eq. 1) then
+                      crse_flux = (uc(i,j-1,1)-uc(i-1,j-1,1))/dx(1) - uc(i,j-1,2)/dx(2)
+                   else
+                      crse_flux =        (uc(i,j-1,1)/dx(1) - uc(i,j-1,2)/dx(2))
+                   end if
                 else
                    crse_flux = FOURTH*(uc(i,j-1,1)/dx(1) - uc(i,j-1,2)/dx(2))
                 end if
 
              else if (i == hiflx(1)) then
                 if (bc_neumann(mm(ir(1)*i,ir(2)*j),1,+1)) then
-                   crse_flux =        (-uc(i-1,j-1,1)/dx(1) - uc(i-1,j-1,2)/dx(2))
+                   if (hi_inflow(1) .eq. 1) then
+                      crse_flux = (uc(i,j-1,1)-uc(i-1,j-1,1))/dx(1) - uc(i-1,j-1,2)/dx(2)
+                   else
+                      crse_flux =        (-uc(i-1,j-1,1)/dx(1) - uc(i-1,j-1,2)/dx(2))
+                   end if
                 else 
                    crse_flux = FOURTH*(-uc(i-1,j-1,1)/dx(1) - uc(i-1,j-1,2)/dx(2))
                 end if
@@ -799,7 +841,7 @@ contains
 !   ********************************************************************************************* !
 
     subroutine ml_interface_3d_divu(rh, lor, fine_flux, lof, loflx, hiflx, uc, loc, &
-                                    mm, lom, lo, hi, ir, side, dx)
+                                    mm, lom, lo, hi, ir, side, dx, lo_inflow, hi_inflow)
     integer, intent(in) :: lor(:)
     integer, intent(in) :: loc(:)
     integer, intent(in) :: lom(:)
@@ -813,6 +855,7 @@ contains
     integer           , intent(in   ) :: ir(:)
     integer           , intent(in   ) :: side
     real(kind=dp_t)   , intent(in   ) :: dx(:)
+    integer, intent(in) :: lo_inflow(:), hi_inflow(:)
 
     integer :: i, j, k, ii, jj, kk, ifac
     logical :: lo_i_neu,lo_j_neu,lo_k_neu,hi_i_neu,hi_j_neu,hi_k_neu
