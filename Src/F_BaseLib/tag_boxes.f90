2747e8f95a043705b620bf22eb57cf17b6ec383c
ajnonaka <AJNonaka@lbl.gov>
2012-04-18 09:01:08 -0700

    clean up tag_boxes, add comments

--- Src/F_BaseLib/tag_boxes.f90
+++ Src/F_BaseLib/tag_boxes.f90
@@ -1,17 +1,7 @@
 module tag_boxes_module
 
-  use BoxLib
-  use f2kcli
-  use list_box_module
-  use boxarray_module
-  use ml_boxarray_module
-  use layout_module
   use multifab_module
-  use box_util_module
-  use bl_IO_module
-  use cluster_module
-  use ml_layout_module
-  use bl_error_module, only: bl_error
+  use bl_error_module
 
   implicit none 
 
@@ -26,26 +16,28 @@ contains
     type(multifab), optional, intent(in   ) :: aux_tag_mf
     ! aux_tag_mf allows user to pass in additional multifabs for tagging logic
     
-    real(kind = dp_t), pointer :: sp(:,:,:,:)
+    ! local variables
+    real(kind = dp_t), pointer :: mfp(:,:,:,:)
     logical          , pointer :: tp(:,:,:,:)
     integer           :: i, lo(get_dim(mf)), hi(get_dim(mf)), ng
 
-    if (present(aux_tag_mf)) &
-         call bl_error("aux_tag_mf passed to tag_boxes without implementation")
+    if (present(aux_tag_mf)) then
+       call bl_error("tag_boxes.f90: aux_tag_mf passed to tag_boxes without implementation")
+    end if
 
     ng = nghost(mf)
 
     do i = 1, nboxes(mf)
        if ( multifab_remote(mf, i) ) cycle
-       sp => dataptr(mf, i)
-       tp => dataptr(tagboxes, i)
+       mfp => dataptr(mf, i)
+       tp  => dataptr(tagboxes, i)
        lo =  lwb(get_box(tagboxes, i))
        hi =  upb(get_box(tagboxes, i))
        select case (get_dim(mf))
        case (2)
-          call tag_boxes_2d(tp(:,:,1,1),sp(:,:,1,1),lo,hi,ng,dx,lev)
+          call tag_boxes_2d(tp(:,:,1,1),mfp(:,:,1,1),lo,hi,ng,dx,lev)
        case  (3)
-          call tag_boxes_3d(tp(:,:,:,1),sp(:,:,:,1),lo,hi,ng,dx,lev)
+          call tag_boxes_3d(tp(:,:,:,1),mfp(:,:,:,1),lo,hi,ng,dx,lev)
        end select
     end do
 
@@ -54,17 +46,18 @@ contains
   subroutine tag_boxes_2d(tagbox,mf,lo,hi,ng,dx,lev)
 
     integer          , intent(in   ) :: lo(:),hi(:),ng
-    logical          , intent(  out) :: tagbox(lo(1):,lo(2):)
-    real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:)
+    logical          , intent(  out) :: tagbox(lo(1)   :,lo(2)   :)
+    real(kind = dp_t), intent(in   ) ::     mf(lo(1)-ng:,lo(2)-ng:)
     real(dp_t)       , intent(in   ) :: dx
-    integer, optional, intent(in   ) :: lev
-    integer :: i,j,llev
+    integer          , intent(in   ) :: lev
 
-    llev = 1; if (present(lev)) llev = lev
+    ! local variables
+    integer :: i,j
 
+    ! initially say that we do not want to tag any cells for refinement
     tagbox = .false.
 
-    select case(llev)
+    select case(lev)
     case (1)
        ! tag all boxes with a density >= 1.01
        do j = lo(2),hi(2)
@@ -99,18 +92,18 @@ contains
   subroutine tag_boxes_3d(tagbox,mf,lo,hi,ng,dx,lev)
 
     integer          , intent(in   ) :: lo(:),hi(:),ng
-    logical          , intent(  out) :: tagbox(lo(1):,lo(2):,lo(3):)
-    real(kind = dp_t), intent(in   ) :: mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
+    logical          , intent(  out) :: tagbox(lo(1)   :,lo(2)   :,lo(3)   :)
+    real(kind = dp_t), intent(in   ) ::     mf(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
     real(dp_t)       , intent(in   ) :: dx
-    integer, optional, intent(in   ) :: lev
+    integer          , intent(in   ) :: lev
 
-    integer :: i,j,k,llev
+    ! local variables
+    integer :: i,j,k
 
-    llev = 1; if (present(lev)) llev = lev
-
+    ! initially say that we do not want to tag any cells for refinement
     tagbox = .false.
 
-    select case(llev)
+    select case(lev)
     case (1)
        ! tag all boxes with a density >= 1.01
        do k = lo(3),hi(3)
