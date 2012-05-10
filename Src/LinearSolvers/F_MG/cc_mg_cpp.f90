32cd0c49001aaf03668e3d1a5d212cc5bf4099cd
Marc Day <MSDay@lbl.gov>
2012-05-09 19:26:23 -0700

    Not sure what happened, but the last version ended up broken....put it back

--- Src/LinearSolvers/F_MG/cc_mg_cpp.f90
+++ Src/LinearSolvers/F_MG/cc_mg_cpp.f90
@@ -257,7 +257,6 @@ subroutine mgt_finalize(dx,bc)
           min_width         = mgts%min_width, &
           verbose           = mgts%verbose, &
           cg_verbose        = mgts%cg_verbose, &
-          max_L0_growth     = mgts%max_L0_growth, &
           nodal             = nodal &
           )
 
@@ -352,7 +351,6 @@ subroutine mgt_finalize_n(dx,bc,nc_in,ns_in)
           min_width         = mgts%min_width, &
           verbose           = mgts%verbose, &
           cg_verbose        = mgts%cg_verbose, &
-          max_L0_growth     = mgts%max_L0_growth, &
           nodal             = nodal &
           )
   end do
