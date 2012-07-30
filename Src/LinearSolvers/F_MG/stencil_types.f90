e53f151825e9815f36834806e837742ac0238bdb
Weiqun Zhang <weiqunzhang@lbl.gov>
2012-07-30 16:02:31 -0700

    Add #ifndef _STENCIL_TYPES_H_ in stencil_types.H, and some comments

--- Src/LinearSolvers/F_MG/stencil_types.f90
+++ Src/LinearSolvers/F_MG/stencil_types.f90
@@ -1,5 +1,7 @@
 module stencil_types_module
 
+  ! If this file is updated, don't forget to update C_to_F_MG/stencil_types.H.
+
    integer, parameter :: CC_CROSS_STENCIL = 11
    integer, parameter :: ND_CROSS_STENCIL = 21
    integer, parameter :: ND_DENSE_STENCIL = 22 
