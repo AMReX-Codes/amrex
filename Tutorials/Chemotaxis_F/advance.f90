2475496cab253d18e4252b2f3efa624bdb5c6223
Matthew Emmett <memmett@gmail.com>
2012-07-31 15:16:09 -0400

    Chemotaxis: Minimal model working.

--- Tutorials/Chemotaxis_F/advance.f90
+++ Tutorials/Chemotaxis_F/advance.f90
@@ -1,31 +1,12 @@
 module advance_module
   
   use multifab_module
+  use chemotaxis_kernels
   use dtypes_module
   use sdcquad_module
   use write_plotfile_module
 
   implicit none
-
-  ! Throughout this module we use the fourth-order stencils from
-  ! Kadioglu, Klein, and Minion 2008.
-  !
-  ! The edge-average of a derivative normal to the edge is given by,
-  ! eg,
-  !
-  !   (\phi_x)_{i+1/2,j} = [ 1, -15, 15, -1 ] / 12 h
-  !
-  ! for offsets [ -1, 0, 1, 2 ] so that
-  !  
-  !   (\phi_x)_{i+1/2,j} - (\phi_x)_{i-1/2,j} = [ 1, -14, 0, 14, -1 ] / 12 h
-  !
-  ! for centered offsets.
-
-  double precision, parameter :: dxm2 =   1.0d0/12
-  double precision, parameter :: dxm1 = -14.0d0/12
-  double precision, parameter :: dxp1 =  14.0d0/12
-  double precision, parameter :: dxp2 =  -1.0d0/12
-
 
 contains
   
@@ -35,16 +16,21 @@ contains
     real(dp_t),      intent(in)    :: dt
     type(cht_ctx_t), intent(inout) :: ctx
 
-    integer        :: n, lo(2), hi(2)
+    integer        :: n, nc, ng, lo(2), hi(2)
     type(multifab) :: f
+    type(layout)   :: la
 
     double precision, pointer, dimension(:,:,:,:) :: qp, fp 
+
+    nc = ncomp(q)
+    ng = nghost(q)
+    la = get_layout(q)
 
     ! sync q and fill ghost cells
     call fill_boundary(q)
 
     ! build flux multifab
-    call build(f, ctx%la, ctx%nc, 0)
+    call build(f, la, nc, 0)
 
     do n=1, nboxes(q)
        if ( remote(q,n) ) cycle
@@ -55,8 +41,15 @@ contains
        lo = lwb(get_box(q,n))
        hi = upb(get_box(q,n))
 
-       call cell_diffusive_flux_2d(qp, lo, hi, ctx%ng, ctx, fp)
+       fp = 0.0d0
 
+       ! XXX: pass invdx here instead of dx
+
+       call cell_motility           (fp(:,:,1,iu), qp(:,:,1,iu),               lo, hi, ng, ctx%dx, ctx%diff)
+       call chemotactic_sensitivity (fp(:,:,1,iu), qp(:,:,1,iu), qp(:,:,1,iv), lo, hi, ng, ctx%dx, ctx%chi)
+       call signal_diffusion        (fp(:,:,1,iv), qp(:,:,1,iv),               lo, hi, ng, ctx%dx)
+       call signal_production       (fp(:,:,1,iv), qp(:,:,1,iu), qp(:,:,1,iv), lo, hi, ng, ctx%dx)
+       call signal_degradation      (fp(:,:,1,iv), qp(:,:,1,iu), qp(:,:,1,iv), lo, hi, ng, ctx%dx)
     end do
 
     call saxpy(q, dt, f)
@@ -76,33 +69,6 @@ contains
 
     
   end subroutine advance_sdc
-
-
-  subroutine cell_diffusive_flux_2d(qp, lo, hi, ng, ctx, fp)
-    integer,          intent(in)    :: lo(2), hi(2), ng
-    type(cht_ctx_t),  intent(inout) :: ctx
-    double precision, intent(in)    :: qp(-ng+lo(1):hi(1)+ng,-ng+lo(2):hi(2)+ng,2)
-    double precision, intent(out)   :: fp(lo(1):hi(1),lo(2):hi(2),2)
-
-    integer :: i, j 
-
-    ! diffusive flux: edge difference of: grad u - chi u grad v
-
-    do j = lo(2), hi(2)
-       do i = lo(1), hi(1)
-
-          ! \nabla u
-          fp(i,j,1) = ( &
-               + dxm2 * (qp(i-2,j,1) + qp(i,j-2,1)) &
-               + dxm1 * (qp(i-1,j,1) + qp(i,j-1,1)) &
-               + dxp1 * (qp(i+1,j,1) + qp(i,j+1,1)) &
-               + dxp2 * (qp(i+2,j,1) + qp(i,j+2,1)) &
-               ) / ctx%dx
-          
-       end do
-    end do
-  end subroutine cell_diffusive_flux_2d
-
 
 end module advance_module
 
