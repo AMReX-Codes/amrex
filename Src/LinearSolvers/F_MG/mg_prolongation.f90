7941eeee7db12f3030a70e8db48d670cbe943cf6
lijewski <mjlijewski@lbl.gov>
2013-07-26 16:10:30 -0700

    More prep work for tricubic.

--- Src/LinearSolvers/F_MG/mg_prolongation.f90
+++ Src/LinearSolvers/F_MG/mg_prolongation.f90
@@ -564,7 +564,7 @@ contains
     real (dp_t), intent(in   ) :: cc(loc(1):,loc(2):)
     integer,     intent(in   ) :: ir(:), ptype
 
-    integer     :: i, j, ic, jc, l, m, ng, clo(2), chi(2)
+    integer     :: i, j, ic, jc, l, m, ng, clo(2), chi(2), istart, jstart
     real (dp_t) :: fac_left, fac_rght, coeffs(0:15)
     real (dp_t) :: temp(lo(1):hi(1),lo(2):hi(2))
 
@@ -582,45 +582,35 @@ contains
           ! Bicubic was requested and we have one ghost cell.
           !
           do jc = clo(2),chi(2)
-             j = 2*jc
+             j      = 2*jc
+             jstart = jc-1
+             if ( j == hi(2) ) jstart = jc-2
+
              do ic = clo(1),chi(1)
-                i = 2*ic
+                i      = 2*ic
+                istart = ic-1
+                if ( i == hi(1) ) istart = ic-2
                 !
                 ! Direct injection for fine points overlaying coarse ones.
                 !
                 ff(i,j) = ff(i,j) + cc(ic,jc)
 
+                if ( i < hi(1) .or. j < hi(2) ) then
+                   coeffs( 0 :  3) = (/ (cc(m,jstart+0), m=istart,istart+3) /)
+                   coeffs( 4 :  7) = (/ (cc(m,jstart+1), m=istart,istart+3) /)
+                   coeffs( 8 : 11) = (/ (cc(m,jstart+2), m=istart,istart+3) /)
+                   coeffs(12 : 15) = (/ (cc(m,jstart+3), m=istart,istart+3) /)
+                end if
+
                 if ( i < hi(1) ) then
-
-                   if ( j == hi(2) ) then
-                      coeffs( 0 :  3) = (/ (cc(m,jc-2), m=ic-1,ic+2) /)
-                      coeffs( 4 :  7) = (/ (cc(m,jc-1), m=ic-1,ic+2) /)
-                      coeffs( 8 : 11) = (/ (cc(m,jc+0), m=ic-1,ic+2) /)
-                      coeffs(12 : 15) = (/ (cc(m,jc+1), m=ic-1,ic+2) /)
-                   else
-                      coeffs( 0 :  3) = (/ (cc(m,jc-1), m=ic-1,ic+2) /)
-                      coeffs( 4 :  7) = (/ (cc(m,jc+0), m=ic-1,ic+2) /)
-                      coeffs( 8 : 11) = (/ (cc(m,jc+1), m=ic-1,ic+2) /)
-                      coeffs(12 : 15) = (/ (cc(m,jc+2), m=ic-1,ic+2) /)
-                   end if
-
                    ff(i+1,j) = ff(i+1,j) + bicubicInterpolate(coeffs, 0.5d0, 0.0d0) 
 
                    if ( j < hi(2) ) then
-
                       ff(i+1,j+1) = ff(i+1,j+1) + bicubicInterpolate(coeffs, 0.5d0, 0.5d0) 
                    end if
                 end if
 
                 if ( j < hi(2) ) then
-
-                   if ( i == hi(1) ) then
-                      coeffs( 0 :  3) = (/ (cc(m,jc-1), m=ic-2,ic+1) /)
-                      coeffs( 4 :  7) = (/ (cc(m,jc+0), m=ic-2,ic+1) /)
-                      coeffs( 8 : 11) = (/ (cc(m,jc+1), m=ic-2,ic+1) /)
-                      coeffs(12 : 15) = (/ (cc(m,jc+2), m=ic-2,ic+1) /)
-                   end if
-
                    ff(i,j+1) = ff(i,j+1) + bicubicInterpolate(coeffs, 0.0d0, 0.5d0)
                 end if
 
@@ -714,15 +704,13 @@ contains
     integer,     intent(in   ) :: ir(:), ptype
 
     integer               :: i, j, k, ic, jc, kc, l, m, n, ng, clo(3), chi(3)
-    real (dp_t)           :: fac_left, fac_rght
+    real (dp_t)           :: fac_left, fac_rght, coeffs(0:63)
     real(dp_t), parameter :: ONE = 1.0_dp_t
 
     real (dp_t), allocatable :: temp(:,:,:)
 
     if ( ir(1) == 2 .and. ir(2) == 2 .and. ir(3) == 2 ) then
-       !
-       ! Do fast unrolled linear interp.
-       !
+
        clo = lo / 2
        chi = hi / 2
 
