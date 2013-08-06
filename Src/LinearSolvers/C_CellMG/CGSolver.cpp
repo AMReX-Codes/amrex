8902719bb281aba84ae1ea0ccb7c7c6afc0abdd1
lijewski <mjlijewski@lbl.gov>
2013-08-06 12:31:13 -0700

    Some work on adding a version of CABiCGStab that uses the quad precision routines from XBLAS.  It's disabled at the moment so it shouldn't affect anyone.  My plan is to see if a more accurate bottom solver can solve some problems with fewer iterations.  It may also be the case that with a more accurate bottom solver we can up the max "S" in the CABiCGStab from 4 -> 8 or somesuch.

--- Src/LinearSolvers/C_CellMG/CGSolver.cpp
+++ Src/LinearSolvers/C_CellMG/CGSolver.cpp
@@ -74,9 +74,10 @@ CGSolver::Initialize ()
     {
         switch (ii)
         {
-        case 0: def_cg_solver = CG;         break;
-        case 1: def_cg_solver = BiCGStab;   break;
-        case 2: def_cg_solver = CABiCGStab; break;
+        case 0: def_cg_solver = CG;             break;
+        case 1: def_cg_solver = BiCGStab;       break;
+        case 2: def_cg_solver = CABiCGStab;     break;
+        case 3: def_cg_solver = CABiCGStabQuad; break;
         default:
             BoxLib::Error("CGSolver::Initialize(): bad cg_solver");
         }
@@ -170,12 +171,16 @@ CGSolver::solve (MultiFab&       sol,
 {
     switch (def_cg_solver)
     {
-    case 0:
+    case CG:
         return solve_cg(sol, rhs, eps_rel, eps_abs, bc_mode);
-    case 1:
+    case BiCGStab:
         return solve_bicgstab(sol, rhs, eps_rel, eps_abs, bc_mode);
-    case 2:
+    case CABiCGStab:
         return solve_cabicgstab(sol, rhs, eps_rel, eps_abs, bc_mode);
+#ifdef XBLAS
+    case CABiCGStabQuad:
+        return solve_cabicgstab_quad(sol, rhs, eps_rel, eps_abs, bc_mode);
+#endif
     default:
         BoxLib::Error("CGSolver::solve(): unknown solver");
     }
@@ -288,12 +293,11 @@ dotxy (const MultiFab& r,
 //
 inline
 void
-gemv (Real* z,
-      Real  A[((4*SSS_MAX)+1)][((4*SSS_MAX)+1)],
-      Real* x,
-      Real* y,
-      int   rows,
-      int   cols)
+gemv (Real*       z,
+      Real        A[((4*SSS_MAX)+1)][((4*SSS_MAX)+1)],
+      const Real* x,
+      int         rows,
+      int         cols)
 {
     for (int r = 0; r < rows; r++)
     {
@@ -311,11 +315,11 @@ gemv (Real* z,
 //
 inline
 void
-axpy (Real* z,
-      Real* x,
-      Real  beta,
-      Real* y,
-      int   n)
+axpy (Real*       z,
+      const Real* x,
+      Real        beta,
+      const Real* y,
+      int         n)
 {
     for (int nn = 0; nn < n; nn++)
     {
@@ -328,7 +332,9 @@ axpy (Real* z,
 //
 inline
 Real
-dot (Real* x, Real* y, int n)
+dot (const Real* x,
+     const Real* y,
+     int         n)
 {
     Real sum = 0;
     for (int nn = 0; nn < n; nn++)
@@ -647,9 +653,9 @@ CGSolver::solve_cabicgstab (MultiFab&       sol,
 
         for (int nit = 0; nit < SSS; nit++)
         {
-            gemv( Tpaj,  Tp, aj,  Tpaj, 4*SSS+1, 4*SSS+1);
-            gemv( Tpcj,  Tp, cj,  Tpcj, 4*SSS+1, 4*SSS+1);
-            gemv(Tppaj, Tpp, aj, Tppaj, 4*SSS+1, 4*SSS+1);
+            gemv( Tpaj,  Tp, aj, 4*SSS+1, 4*SSS+1);
+            gemv( Tpcj,  Tp, cj, 4*SSS+1, 4*SSS+1);
+            gemv(Tppaj, Tpp, aj, 4*SSS+1, 4*SSS+1);
 
             const Real g_dot_Tpaj = dot(g, Tpaj, 4*SSS+1);
 
@@ -671,7 +677,7 @@ CGSolver::solve_cabicgstab (MultiFab&       sol,
 
             axpy(temp1, Tpcj, -alpha, Tppaj, 4*SSS+1);
 
-            gemv(temp2, G, temp1, temp2, 4*SSS+1, 4*SSS+1);
+            gemv(temp2, G, temp1, 4*SSS+1, 4*SSS+1);
 
             axpy(temp3,   cj, -alpha,  Tpaj, 4*SSS+1);
 
@@ -696,7 +702,7 @@ CGSolver::solve_cabicgstab (MultiFab&       sol,
             //
             axpy(temp1, cj,-alpha,  Tpaj, 4*SSS+1);
 
-            gemv(temp2, G, temp1, temp2, 4*SSS+1, 4*SSS+1);
+            gemv(temp2, G, temp1, 4*SSS+1, 4*SSS+1);
 
             const Real L2_norm_of_s = dot(temp1,temp2,4*SSS+1);
 
@@ -737,7 +743,7 @@ CGSolver::solve_cabicgstab (MultiFab&       sol,
             //
             // Do an early check of the residual to determine convergence.
             //
-            gemv(temp1, G, cj, temp1, 4*SSS+1, 4*SSS+1);
+            gemv(temp1, G, cj, 4*SSS+1, 4*SSS+1);
             //
             // sqrt( (cj,Gcj) ) == L2 norm of the intermediate residual in exact arithmetic.
             // However, finite precision can lead to the norm^2 being < 0 (Jim Demmel).
@@ -852,6 +858,445 @@ CGSolver::solve_cabicgstab (MultiFab&       sol,
 
     return ret;
 }
+
+#ifdef XBLAS
+
+#include <blas_enum.h>
+
+extern "C"
+{
+#include <blas_extended_proto.h>
+}
+
+inline
+Real
+qdot (const Real* x,
+      const Real* y,
+      int         n)
+{
+    Real sum = 0;
+
+    BLAS_ddot_x(blas_no_conj, n, 1.0, x, 1, 0.0, y, 1, &sum, blas_prec_extra);
+
+    return sum;
+}
+
+//
+// z[m] = A[m][n]*x[n]   [row][col]
+//
+static
+void
+qgemv (Real*       z,
+       Real        A[((4*SSS_MAX)+1)][((4*SSS_MAX)+1)],
+       const Real* x,
+       int         rows,
+       int         cols)
+{
+    Real tmp[(4*SSS_MAX+1)*(4*SSS_MAX+1)];
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            tmp[(r*rows)+c] = A[r][c];
+        }
+    }
+
+    BLAS_dgemv_x(blas_rowmajor, blas_no_trans,
+                 rows, cols, 1.0, tmp, rows,
+                 x, 1, 0.0, z, 1, blas_prec_extra);
+}
+
+//
+// z[n] = x[n]+beta*y[n]
+//
+inline
+void
+qaxpy (Real*       z,
+       const Real* x,
+       Real        beta,
+       const Real* y,
+       int         n)
+{
+    for (int i = 0; i < n; i++) z[i] = y[i];
+
+    BLAS_daxpby_x(n, 1.0, x, 1, beta, z, 1, blas_prec_extra);
+}
+
+int
+CGSolver::solve_cabicgstab_quad (MultiFab&       sol,
+                                 const MultiFab& rhs,
+                                 Real            eps_rel,
+                                 Real            eps_abs,
+                                 LinOp::BC_Mode  bc_mode)
+{
+    BL_PROFILE("CGSolver::solve_cabicgstab_quad()");
+
+    BL_ASSERT(sol.nComp() == 1);
+    BL_ASSERT(sol.boxArray() == Lp.boxArray(lev));
+    BL_ASSERT(rhs.boxArray() == Lp.boxArray(lev));
+
+    Real  temp1[4*SSS_MAX+1];
+    Real  temp2[4*SSS_MAX+1];
+    Real  temp3[4*SSS_MAX+1];
+    Real     Tp[4*SSS_MAX+1][4*SSS_MAX+1];
+    Real    Tpp[4*SSS_MAX+1][4*SSS_MAX+1];
+    Real     aj[4*SSS_MAX+1];
+    Real     cj[4*SSS_MAX+1];
+    Real     ej[4*SSS_MAX+1];
+    Real   Tpaj[4*SSS_MAX+1];
+    Real   Tpcj[4*SSS_MAX+1];
+    Real  Tppaj[4*SSS_MAX+1];
+    Real      G[4*SSS_MAX+1][4*SSS_MAX+1];    // Extracted from first 4*SSS+1 columns of Gg[][].  indexed as [row][col]
+    Real      g[4*SSS_MAX+1];                 // Extracted from last [4*SSS+1] column of Gg[][].
+    Real     Gg[(4*SSS_MAX+1)*(4*SSS_MAX+2)]; // Buffer to hold the Gram-like matrix produced by matmul().  indexed as [row*(4*SSS+2) + col]
+    //
+    // If variable_SSS we "telescope" SSS.
+    // We start with 1 and increase it up to SSS_MAX on the outer iterations.
+    //
+    if (variable_SSS) SSS = 1;
+
+    zero(   aj, 4*SSS_MAX+1);
+    zero(   cj, 4*SSS_MAX+1);
+    zero(   ej, 4*SSS_MAX+1);
+    zero( Tpaj, 4*SSS_MAX+1);
+    zero( Tpcj, 4*SSS_MAX+1);
+    zero(Tppaj, 4*SSS_MAX+1);
+    zero(temp1, 4*SSS_MAX+1);
+    zero(temp2, 4*SSS_MAX+1);
+    zero(temp3, 4*SSS_MAX+1);
+
+    SetMonomialBasis(Tp,Tpp,SSS);
+
+    const int ncomp = 1, nghost = 1;
+    //
+    // Contains the matrix powers of p[] and r[].
+    //
+    // First 2*SSS+1 components are powers of p[].
+    // Next  2*SSS   components are powers of r[].
+    //
+    MultiFab PR(sol.boxArray(), 4*SSS_MAX+1, 0);
+
+    MultiFab  p(sol.boxArray(), ncomp, 0);
+    MultiFab  r(sol.boxArray(), ncomp, 0);
+    MultiFab rt(sol.boxArray(), ncomp, 0);
+    
+    MultiFab tmp(sol.boxArray(), 4, nghost);
+
+    Lp.residual(r, rhs, sol, lev, bc_mode);
+
+    if ((verbose > 1) && r.contains_nan()) std::cout << "*** r contains NANs\n";
+
+    MultiFab::Copy(rt,r,0,0,1,0);
+    MultiFab::Copy( p,r,0,0,1,0);
+
+    const Real           rnorm0        = norm_inf(r);
+    Real                 delta         = dotxy(r,rt);
+    const Real           L2_norm_of_rt = sqrt(delta);
+    const LinOp::BC_Mode temp_bc_mode  = LinOp::Homogeneous_BC;
+
+    if ( verbose > 0 && ParallelDescriptor::IOProcessor() )
+    {
+        Spacer(std::cout, lev);
+        std::cout << "CGSolver_CABiCGStab: Initial error (error0) =        " << rnorm0 << '\n';
+    }
+
+    if ( rnorm0 == 0 || delta == 0 || rnorm0 < eps_abs )
+    {
+        if ( verbose > 0 && ParallelDescriptor::IOProcessor() )
+	{
+            Spacer(std::cout, lev);
+            std::cout << "CGSolver_CABiCGStab: niter = 0,"
+                      << ", rnorm = "   << rnorm0
+                      << ", delta = "   << delta
+                      << ", eps_abs = " << eps_abs << '\n';
+	}
+        return 0;
+    }
+
+    int niters = 0, ret = 0;
+
+    Real L2_norm_of_resid = 0, atime = 0, gtime = 0;
+
+    bool BiCGStabFailed = false, BiCGStabConverged = false;
+
+    for (int m = 0; m < maxiter && !BiCGStabFailed && !BiCGStabConverged; )
+    {
+        const Real time1 = ParallelDescriptor::second();
+        //
+        // Compute the matrix powers on p[] & r[] (monomial basis).
+        // The 2*SSS+1 powers of p[] followed by the 2*SSS powers of r[].
+        //
+        MultiFab::Copy(PR,p,0,0,1,0);
+        MultiFab::Copy(PR,r,0,2*SSS+1,1,0);
+
+        if ((verbose > 1) && PR.contains_nan(0,      1)) std::cout << "*** PR contains NANs @ p\n";
+        if ((verbose > 1) && PR.contains_nan(2*SSS+1,1)) std::cout << "*** PR contains NANs @ r\n";
+        //
+        // We use "tmp" to minimize the number of Lp.apply()s.
+        // We do this by doing p & r together in a single call.
+        //
+        MultiFab::Copy(tmp,p,0,0,1,0);
+        MultiFab::Copy(tmp,r,0,1,1,0);
+
+        for (int n = 1; n < 2*SSS; n++)
+        {
+            Lp.apply(tmp, tmp, lev, temp_bc_mode, false, 0, 2, 2);
+
+            MultiFab::Copy(tmp,tmp,2,0,2,0);
+
+            MultiFab::Copy(PR,tmp,0,        n,1,0);
+            MultiFab::Copy(PR,tmp,1,2*SSS+n+1,1,0);
+
+            if ((verbose > 1) && PR.contains_nan(n,        1)) std::cout << "*** PR contains NANs @ p: " << n         << '\n';
+            if ((verbose > 1) && PR.contains_nan(2*SSS+n+1,1)) std::cout << "*** PR contains NANs @ r: " << 2*SSS+n+1 << '\n';
+        }
+
+        MultiFab::Copy(tmp,PR,2*SSS-1,0,1,0);
+        Lp.apply(tmp, tmp, lev, temp_bc_mode, false, 0, 1, 1);
+        MultiFab::Copy(PR,tmp,1,2*SSS,1,0);
+
+        if ((verbose > 1) && PR.contains_nan(2*SSS-1,1)) std::cout << "*** PR contains NANs @ 2*SSS-1\n";
+        if ((verbose > 1) && PR.contains_nan(2*SSS,  1)) std::cout << "*** PR contains NANs @ 2*SSS\n";
+
+        Real time2 = ParallelDescriptor::second();
+
+        atime += (time2-time1);
+
+        BuildGramMatrix(Gg, PR, rt, SSS);
+
+        const Real time3 = ParallelDescriptor::second();
+
+        gtime += (time3-time2);
+        //
+        // Form G[][] and g[] from Gg.
+        //
+        for (int i = 0, k = 0; i < 4*SSS+1; i++)
+        {
+            for (int j = 0; j < 4*SSS+1; j++)
+                //
+                // First 4*SSS+1 elements in each row go to G[][].
+                //
+                G[i][j] = Gg[k++];
+            //
+            // Last element in row goes to g[].
+            //
+            g[i] = Gg[k++];
+        }
+
+        zero(aj, 4*SSS+1); aj[0]       = 1;
+        zero(cj, 4*SSS+1); cj[2*SSS+1] = 1;
+        zero(ej, 4*SSS+1);
+
+        for (int nit = 0; nit < SSS; nit++)
+        {
+            qgemv( Tpaj,  Tp, aj, 4*SSS+1, 4*SSS+1);
+            qgemv( Tpcj,  Tp, cj, 4*SSS+1, 4*SSS+1);
+            qgemv(Tppaj, Tpp, aj, 4*SSS+1, 4*SSS+1);
+
+            const Real g_dot_Tpaj = qdot(g, Tpaj, 4*SSS+1);
+
+            if ( g_dot_Tpaj == 0 )
+            {
+                if ( verbose > 1 && ParallelDescriptor::IOProcessor() )
+                    std::cout << "CGSolver_CABiCGStab: g_dot_Tpaj == 0, nit = " << nit << '\n';
+                BiCGStabFailed = true; ret = 1; break;
+            }
+
+            const Real alpha = delta / g_dot_Tpaj;
+
+            if ( isinf(alpha) )
+            {
+                if ( verbose > 1 && ParallelDescriptor::IOProcessor() )
+                    std::cout << "CGSolver_CABiCGStab: alpha == inf, nit = " << nit << '\n';
+                BiCGStabFailed = true; ret = 2; break;
+            }
+
+            qaxpy(temp1, Tpcj, -alpha, Tppaj, 4*SSS+1);
+
+            qgemv(temp2, G, temp1, 4*SSS+1, 4*SSS+1);
+
+            qaxpy(temp3,   cj, -alpha,  Tpaj, 4*SSS+1);
+
+            const Real omega_numerator   = qdot(temp3, temp2, 4*SSS+1);
+            const Real omega_denominator = qdot(temp1, temp2, 4*SSS+1);
+            //
+            // NOTE: omega_numerator/omega_denominator can be 0/x or 0/0, but should never be x/0.
+            //
+            // If omega_numerator==0, and ||s||==0, then convergence, x=x+alpha*aj.
+            // If omega_numerator==0, and ||s||!=0, then stabilization breakdown.
+            //
+            // Partial update of ej must happen before the check on omega to ensure forward progress !!!
+            //
+            qaxpy(ej, ej, alpha, aj, 4*SSS+1);
+            //
+            // ej has been updated so consider that we've done an iteration since
+            // even if we break out of the loop we'll be able to update both sol.
+            //
+            niters++;
+            //
+            // Calculate the norm of Saad's vector 's' to check intra s-step convergence.
+            //
+            qaxpy(temp1, cj,-alpha,  Tpaj, 4*SSS+1);
+
+            qgemv(temp2, G, temp1, 4*SSS+1, 4*SSS+1);
+
+            const Real L2_norm_of_s = qdot(temp1,temp2,4*SSS+1);
+
+            L2_norm_of_resid = (L2_norm_of_s < 0 ? 0 : sqrt(L2_norm_of_s));
+
+            if ( L2_norm_of_resid < eps_rel*L2_norm_of_rt )
+            {
+                if ( verbose > 1 && L2_norm_of_resid == 0 && ParallelDescriptor::IOProcessor() )
+                    std::cout << "CGSolver_CABiCGStab: L2 norm of s: " << L2_norm_of_s << '\n';
+                BiCGStabConverged = true; break;
+            }
+
+            if ( omega_denominator == 0 )
+            {
+                if ( verbose > 1 && ParallelDescriptor::IOProcessor() )
+                    std::cout << "CGSolver_CABiCGStab: omega_denominator == 0, nit = " << nit << '\n';
+                BiCGStabFailed = true; ret = 3; break;
+            }
+
+            const Real omega = omega_numerator / omega_denominator;
+
+            if ( verbose > 1 && ParallelDescriptor::IOProcessor() )
+            {
+                if ( omega == 0   ) std::cout << "CGSolver_CABiCGStab: omega == 0, nit = " << nit << '\n';
+                if ( isinf(omega) ) std::cout << "CGSolver_CABiCGStab: omega == inf, nit = " << nit << '\n';
+            }
+
+            if ( omega == 0   ) { BiCGStabFailed = true; ret = 4; break; }
+            if ( isinf(omega) ) { BiCGStabFailed = true; ret = 4; break; }
+            //
+            // Complete the update of ej & cj now that omega is known to be ok.
+            //
+            qaxpy(ej, ej,       omega,    cj, 4*SSS+1);
+            qaxpy(ej, ej,-omega*alpha,  Tpaj, 4*SSS+1);
+            qaxpy(cj, cj,      -omega,  Tpcj, 4*SSS+1);
+            qaxpy(cj, cj,      -alpha,  Tpaj, 4*SSS+1);
+            qaxpy(cj, cj, omega*alpha, Tppaj, 4*SSS+1);
+            //
+            // Do an early check of the residual to determine convergence.
+            //
+            qgemv(temp1, G, cj, 4*SSS+1, 4*SSS+1);
+            //
+            // sqrt( (cj,Gcj) ) == L2 norm of the intermediate residual in exact arithmetic.
+            // However, finite precision can lead to the norm^2 being < 0 (Jim Demmel).
+            // If cj_dot_Gcj < 0 we flush to zero and consider ourselves converged.
+            //
+            const Real L2_norm_of_r = qdot(cj, temp1, 4*SSS+1);
+
+            L2_norm_of_resid = (L2_norm_of_r > 0 ? sqrt(L2_norm_of_r) : 0);
+
+            if ( L2_norm_of_resid < eps_rel*L2_norm_of_rt )
+            {
+                if ( verbose > 1 && L2_norm_of_resid == 0 && ParallelDescriptor::IOProcessor() )
+                    std::cout << "CGSolver_CABiCGStab: L2_norm_of_r: " << L2_norm_of_r << '\n';
+                BiCGStabConverged = true; break;
+            }
+
+            const Real delta_next = qdot(g, cj, 4*SSS+1);
+
+            if ( verbose > 1 && ParallelDescriptor::IOProcessor() )
+            {
+                if ( delta_next == 0   ) std::cout << "CGSolver_CABiCGStab: delta == 0, nit = " << nit << '\n';
+                if ( isinf(delta_next) ) std::cout << "CGSolver_CABiCGStab: delta == inf, nit = " << nit << '\n';
+            }
+
+            if ( isinf(delta_next) ) { BiCGStabFailed = true; ret = 5; break; } // delta = inf?
+            if ( delta_next  == 0  ) { BiCGStabFailed = true; ret = 5; break; } // Lanczos breakdown...
+
+            const Real beta = (delta_next/delta)*(alpha/omega);
+
+            if ( verbose > 1 && ParallelDescriptor::IOProcessor() )
+            {
+                if ( beta == 0   ) std::cout << "CGSolver_CABiCGStab: beta == 0, nit = " << nit << '\n';
+                if ( isinf(beta) ) std::cout << "CGSolver_CABiCGStab: beta == inf, nit = " << nit << '\n';
+            }
+
+            if ( isinf(beta) ) { BiCGStabFailed = true; ret = 6; break; } // beta = inf?
+            if ( beta == 0   ) { BiCGStabFailed = true; ret = 6; break; } // beta = 0?  can't make further progress(?)
+
+            qaxpy(aj, cj,        beta,   aj, 4*SSS+1);
+            qaxpy(aj, aj, -omega*beta, Tpaj, 4*SSS+1);
+
+            delta = delta_next;
+        }
+        //
+        // Update iterates.
+        //
+        for (int i = 0; i < 4*SSS+1; i++)
+            sxay(sol,sol,ej[i],PR,i);
+
+        MultiFab::Copy(p,PR,0,0,1,0);
+        p.mult(aj[0],0,1);
+        for (int i = 1; i < 4*SSS+1; i++)
+            sxay(p,p,aj[i],PR,i);
+
+        MultiFab::Copy(r,PR,0,0,1,0);
+        r.mult(cj[0],0,1);
+        for (int i = 1; i < 4*SSS+1; i++)
+            sxay(r,r,cj[i],PR,i);
+
+        if ( !BiCGStabFailed && !BiCGStabConverged )
+        {
+            m += SSS;
+
+            if ( variable_SSS && SSS < SSS_MAX ) { SSS++; SetMonomialBasis(Tp,Tpp,SSS); }
+        }
+    }
+
+    if ( verbose > 0 )
+    {
+        if ( ParallelDescriptor::IOProcessor() )
+        {
+            Spacer(std::cout, lev);
+            std::cout << "CGSolver_CABiCGStab: Final: Iteration "
+                      << std::setw(4) << niters
+                      << " rel. err. "
+                      << L2_norm_of_resid << '\n';
+        }
+
+        if ( verbose > 1 )
+        {
+            Real tmp[2] = { atime, gtime };
+
+            ParallelDescriptor::ReduceRealMax(tmp,2,ParallelDescriptor::IOProcessorNumber());
+
+            if ( ParallelDescriptor::IOProcessor() )
+            {
+                Spacer(std::cout, lev);
+                std::cout << "CGSolver_CABiCGStab apply time: " << tmp[0] << ", gram time: " << tmp[1] << '\n';
+            }
+        }
+    }
+
+    if ( niters == maxiter && !BiCGStabFailed && !BiCGStabConverged)
+    {
+        if ( L2_norm_of_resid > L2_norm_of_rt )
+        {
+            if ( ParallelDescriptor::IOProcessor() )
+                BoxLib::Warning("CGSolver_CABiCGStab: failed to converge!");
+            //
+            // Return code 8 tells the MultiGrid driver to zero out the solution!
+            //
+            ret = 8;
+        }
+        else
+        {
+            //
+            // Return codes 1-7 tells the MultiGrid driver to smooth the solution!
+            //
+            ret = 7;
+        }
+    }
+
+    return ret;
+}
+
+#endif /*XBLAS*/
 
 int
 CGSolver::solve_bicgstab (MultiFab&       sol,
