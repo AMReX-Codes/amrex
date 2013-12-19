14eb3879f19277fab2592173455083d2e5804244
lijewski <mjlijewski@lbl.gov>
2013-12-19 14:02:02 -0800

    Decreased the default sizes of the Fill_Boundary() and copy() caches to 25.  This seems to get a majority of the reuse cases without leading to excessive metadata size.

--- Src/C_AMRLib/AuxBoundaryData.cpp
+++ Src/C_AMRLib/AuxBoundaryData.cpp
@@ -132,7 +132,7 @@ AuxBoundaryData::copyTo (MultiFab& mf,
 
     if (!m_empty && mf.size() > 0)
     {
-        mf.copy(m_fabs,src_comp,dst_comp,num_comp,FabArrayBase::COPY,false);
+        mf.copy(m_fabs,src_comp,dst_comp,num_comp);
     }
 }
 
@@ -146,6 +146,6 @@ AuxBoundaryData::copyFrom (const MultiFab& mf,
 
     if (!m_empty && mf.size() > 0)
     {
-         m_fabs.copy(mf,src_comp,dst_comp,num_comp,FabArrayBase::COPY,false);
+         m_fabs.copy(mf,src_comp,dst_comp,num_comp);
     }
 }
