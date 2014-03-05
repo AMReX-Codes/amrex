afbb1e2da6d15cf78185c64acfc6c861faed3ea4
vince <vebeckner@lbl.gov>
2014-03-05 11:27:13 -0800

    added other run sizes.

--- MiniApps/FillBoundary/qsub.ipm.bat
+++ MiniApps/FillBoundary/qsub.ipm.bat
@@ -4,7 +4,11 @@
 #PBS -j eo
 
 ### total cores
-#PBS -l mppwidth=64
+#PBS -l mppwidth=125
+
+###PBS -l mppwidth=1000
+###PBS -l mppwidth=10648
+###PBS -l mppwidth=110592
 
 cd $PBS_O_WORKDIR
 
@@ -14,5 +18,8 @@ setenv IPM_REPORT full
 setenv IPM_LOG full
 setenv IPM_HPM PAPI_L1_DCM,PAPI_TOT_INS,PAPI_L1_DCA,PAPI_FP_OPS
 
-aprun -n 64 ./fbtest3d.Linux.g++.gfortran.MPI.IPM.ex
+aprun -n 125 ./fbtest3d.Linux.g++.gfortran.MPI.IPM.ex
+#aprun -n 1000 ./fbtest3d.Linux.g++.gfortran.MPI.IPM.ex
+#aprun -n 10648 ./fbtest3d.Linux.g++.gfortran.MPI.IPM.ex
+#aprun -n 110592 ./fbtest3d.Linux.g++.gfortran.MPI.IPM.ex
 
