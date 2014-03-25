5ac8dc6b22848ea2d5dc427477b670701c997e91
ajnonaka <AJNonaka@lbl.gov>
2014-03-25 08:02:16 -0700

    move mac_applyop into cc_applyop (delete mac_applyop.f90) and renname the function cc_applyop to be consistent with the rest of the mg naming convention.

--- Src/LinearSolvers/F_MG/cc_applyop.f90
+++ Src/LinearSolvers/F_MG/cc_applyop.f90
@@ -4,10 +4,113 @@ module cc_applyop_module
   use mg_module
   use ml_layout_module
   use bndry_reg_module
+  use define_bc_module
 
   implicit none
 
+  private
+
+  public :: cc_applyop
+
 contains
+
+  subroutine cc_applyop(mla,res,phi,alpha,beta,dx,the_bc_tower,bc_comp,stencil_order_in)
+
+    use mg_module             , only: mg_tower, mg_tower_build, mg_tower_destroy
+    use cc_stencil_fill_module, only: stencil_fill_cc
+    use stencil_types_module
+
+    type(ml_layout), intent(in   ) :: mla
+    type(multifab) , intent(inout) :: res(:), phi(:)
+    type(multifab) , intent(in   ) :: alpha(:), beta(:,:)
+    real(dp_t)     , intent(in   ) :: dx(:,:)
+    type(bc_tower) , intent(in   ) :: the_bc_tower
+    integer        , intent(in   ) :: bc_comp
+    integer, intent(in), optional :: stencil_order_in
+
+    type(layout  ) :: la
+    type(box     ) :: pd
+
+    type(multifab) :: cell_coeffs
+    type(multifab) :: edge_coeffs(mla%dim)
+
+    type(mg_tower)  :: mgt(mla%nlevel)
+    integer         :: d, dm, nlevs
+    integer         :: stencil_order
+
+    ! MG solver defaults
+    integer    :: n
+    real(dp_t) ::  xa(mla%dim),  xb(mla%dim)
+    real(dp_t) :: pxa(mla%dim), pxb(mla%dim)
+
+    type(bl_prof_timer), save :: bpt
+
+    call build(bpt, "mac_applyop")
+
+    dm = mla%dim
+    nlevs = mla%nlevel
+
+    stencil_order = 2
+    if (present(stencil_order_in)) stencil_order = stencil_order_in
+
+    do n = nlevs, 1, -1
+
+       pd = layout_get_pd(mla%la(n))
+
+       call mg_tower_build(mgt(n), mla%la(n), pd, &
+                           the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp),&
+                           stencil_type = CC_CROSS_STENCIL, &
+                           dh = dx(n,:), &
+                           nodal = nodal_flags(res(nlevs)))
+
+    end do
+
+    !! Fill coefficient array
+
+    do n = nlevs,1,-1
+
+       la = mla%la(n)
+
+       call multifab_build(cell_coeffs, la,          nc=1,ng=nghost(alpha(n)))
+       call multifab_copy_c(cell_coeffs,1,alpha(n),1,nc=1,ng=nghost(alpha(n)))
+
+       do d = 1,dm
+          call multifab_build_edge(edge_coeffs(d), la,      nc=1,ng=nghost(beta(n,d)),dir=d)
+          call multifab_copy_c(edge_coeffs(d),1,beta(n,d),1,nc=1,ng=nghost(beta(n,d)))
+       end do
+
+       if (n > 1) then
+          xa = HALF*mla%mba%rr(n-1,:)*mgt(n)%dh(:,mgt(n)%nlevels)
+          xb = HALF*mla%mba%rr(n-1,:)*mgt(n)%dh(:,mgt(n)%nlevels)
+       else
+          xa = ZERO
+          xb = ZERO
+       end if
+
+       pxa = ZERO
+       pxb = ZERO
+
+       call stencil_fill_cc(mgt(n)%ss(mgt(n)%nlevels), cell_coeffs, edge_coeffs, mgt(n)%dh(:,mgt(n)%nlevels), &
+                            mgt(n)%mm(mgt(n)%nlevels), xa, xb, pxa, pxb, stencil_order, &
+                            the_bc_tower%bc_tower_array(n)%ell_bc_level_array(0,:,:,bc_comp))
+
+       call destroy(cell_coeffs)
+
+       do d = 1,dm
+          call destroy(edge_coeffs(d))
+       end do
+
+    end do
+
+    call ml_cc_applyop(mla, mgt, res, phi, mla%mba%rr)
+
+    do n = 1, nlevs
+       call mg_tower_destroy(mgt(n))
+    end do
+
+    call destroy(bpt)
+
+  end subroutine cc_applyop
 
   subroutine ml_cc_applyop(mla, mgt, res, full_soln, ref_ratio)
 
@@ -38,8 +141,6 @@ contains
     integer :: mglev, mglev_crse
 
     type(bl_prof_timer), save :: bpt
-    integer                   :: lo(get_dim(res(1))),hi(get_dim(res(1))),ng
-    real(kind=dp_t),  pointer :: resp(:,:,:,:)
 
     call build(bpt, "ml_cc_applyop")
 
@@ -152,6 +253,7 @@ contains
 !
 ! ******************************************************************************************
 !
+
  subroutine ml_cc_n_applyop(mla, mgt, res, full_soln, ref_ratio)
 
     use bl_prof_module
@@ -181,10 +283,8 @@ contains
     integer :: mglev, mglev_crse
 
     type(bl_prof_timer), save :: bpt
-    integer                   :: lo(get_dim(res(1))),hi(get_dim(res(1))),ng
-    real(kind=dp_t),  pointer :: resp(:,:,:,:)
 
-    call build(bpt, "ml_cc_applyop")
+    call build(bpt, "ml_cc_n_applyop")
 
     nlevs = mla%nlevel
 
