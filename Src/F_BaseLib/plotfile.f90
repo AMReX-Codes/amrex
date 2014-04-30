aef0fba9a94273c344435e581db2acf32729382d
Marc Day <MSDay@lbl.gov>
2014-04-30 12:21:12 -0700

    Increase max name length to 50 (from 20) for plotvars...probably should code this more robustly someday

--- Src/F_BaseLib/plotfile.f90
+++ Src/F_BaseLib/plotfile.f90
@@ -7,7 +7,7 @@ module plotfile_module
   implicit none
 
   integer, private, parameter :: MAX_PATH_NAME = 128
-  integer, private, parameter :: MAX_VAR_NAME  = 20
+  integer, private, parameter :: MAX_VAR_NAME  = 50
 
   interface destroy
      module procedure plotfile_destroy
