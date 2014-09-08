5a1c4fa295fcde2f0e0f42cb2dcc497093f2f84e
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-09-08 16:09:28 -0700

    added one moer argument to ml_restrict_and_fill so that n components can share one type of boundaries

--- Src/F_BaseLib/ml_restrict_fill.f90
+++ Src/F_BaseLib/ml_restrict_fill.f90
@@ -17,23 +17,26 @@ contains
   ! boundary filling.
 
   subroutine ml_restrict_and_fill(nlevs, mf, rr, bc, icomp, bcomp, nc, ng, &
-       time_in, dx_in, prob_lo_in, prob_hi_in, stencil_width_input,fourth_order_input)
+       time_in, dx_in, prob_lo_in, prob_hi_in, stencil_width_input,fourth_order_input, &
+       same_boundary)
 
     integer, intent(in) :: nlevs
     integer, intent(in) :: rr(:,:)
     type(bc_level), intent(in) :: bc(:)
     type(multifab), intent(inout) :: mf(:)
     integer, intent(in), optional :: icomp, bcomp, nc, ng, stencil_width_input
-    logical, intent(in), optional :: fourth_order_input
+    logical, intent(in), optional :: fourth_order_input, same_boundary
     real(kind=dp_t), intent(in), optional :: time_in, dx_in(:), prob_lo_in(:), prob_hi_in(:)
     
     integer :: licomp, lbcomp, lnc, lng
-    integer :: n
-
+    logical :: lsameb
+    integer :: n, i
+    
     licomp = 1;              if (present(icomp)) licomp = icomp
     lbcomp = 1;              if (present(bcomp)) lbcomp = bcomp
     lnc    = ncomp (mf(1));  if (present(nc   )) lnc    = nc
     lng    = nghost(mf(1));  if (present(ng   )) lng    = ng
+    lsameb = .false.;        if (present(same_boundary)) lsameb = same_boundary
 
     do n=nlevs,2,-1
        call ml_cc_restriction_c(mf(n-1), licomp, mf(n), licomp, rr(n-1,:), lnc)
@@ -41,17 +44,37 @@ contains
 
     n = 1
     call multifab_fill_boundary_c(mf(n), licomp, lnc, lng)
-    call multifab_physbc(mf(n), licomp, lbcomp, lnc, bc(n), &
-         time_in,dx_in,prob_lo_in,prob_hi_in)
+    
+    if (lsameb) then
+       do i=1, lnc
+          call multifab_physbc(mf(n), licomp+i-1, lbcomp, 1, bc(n), &
+               time_in,dx_in,prob_lo_in,prob_hi_in)
+       end do
+    else
+       call multifab_physbc(mf(n), licomp, lbcomp, lnc, bc(n), &
+            time_in,dx_in,prob_lo_in,prob_hi_in)
+    end if
     
     do n=2,nlevs
-       call multifab_fill_ghost_cells(mf(n), mf(n-1), lng, rr(n-1,:), bc(n-1), bc(n), &
-                                      licomp, lbcomp, lnc, &
-                                      stencil_width_input      = stencil_width_input, &
-                                      fourth_order_input       = fourth_order_input, &
-                                      fill_crse_boundary_input = .false., &
-                                      fill_crse_physbc_input   = .false.)
+       if (lsameb) then
+          do i=1, lnc
+             call multifab_fill_ghost_cells(mf(n), mf(n-1), lng, rr(n-1,:), bc(n-1), bc(n), &
+                                            licomp+i-1, lbcomp, 1, &
+                                            stencil_width_input      = stencil_width_input, &
+                                            fourth_order_input       = fourth_order_input, &
+                                            fill_crse_boundary_input = .false., &
+                                            fill_crse_physbc_input   = .false.)
+          end do
+       else
+          call multifab_fill_ghost_cells(mf(n), mf(n-1), lng, rr(n-1,:), bc(n-1), bc(n), &
+                                         licomp, lbcomp, lnc, &
+                                         stencil_width_input      = stencil_width_input, &
+                                         fourth_order_input       = fourth_order_input, &
+                                         fill_crse_boundary_input = .false., &
+                                         fill_crse_physbc_input   = .false.)
+       end if
     end do
+
   end subroutine ml_restrict_and_fill
 
 end module ml_restrict_fill_module
