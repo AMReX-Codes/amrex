88c485fe1969cf2ee99978ccc9bf1bf296b94ca1
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-09-09 21:21:58 -0700

    Only if there are no periodic boundaries, we can return immediately from multifab_copy when ng=0

--- Src/F_BaseLib/multifab_f.f90
+++ Src/F_BaseLib/multifab_f.f90
@@ -2545,7 +2545,15 @@ contains
     if ( present(idim) ) then
        if (idim > 0) lcross = .true. 
     end if
-    if ( lng < 1          ) return
+
+    ! multifab_fill_ghost_cells calls fillpatch with a multifab built
+    ! from a boxarray of ghost boxes of the fine multifab to be filled. 
+    ! And the multifab has ng=0.  So there are "valid" cells that are outside the domain.
+    ! For non-periodic boundaries, these cells outside the domain will be filled
+    ! by multifab_physbc later in multifab_fill_ghost_cells.  However, for
+    ! periodic boundaries, mf_fb_fancy_double must be called for these to be filled.
+    if ( lng < 1 .and. .not.any(mf%la%lap%pmask) ) return
+
     call build(bpt, "mf_fill_boundary_c")
     call mf_fb_fancy_double(mf, c, nc, lng, lcross, idim)
     call destroy(bpt)
@@ -2574,7 +2582,15 @@ contains
     if ( present(idim) ) then
        if (idim > 0) lcross = .true. 
     end if
-    if ( lng < 1          ) return
+
+    ! multifab_fill_ghost_cells calls fillpatch with a multifab built
+    ! from a boxarray of ghost boxes of the fine multifab to be filled. 
+    ! And the multifab has ng=0.  So there are "valid" cells that are outside the domain.
+    ! For non-periodic boundaries, these cells outside the domain will be filled
+    ! by multifab_physbc later in multifab_fill_ghost_cells.  However, for
+    ! periodic boundaries, mf_fb_fancy_double must be called for these to be filled.
+    if ( lng < 1 .and. .not.any(mf%la%lap%pmask) ) return
+
     call build(bpt, "mf_fill_boundary_nowait_c")
     call mf_fb_fancy_double_nowait(mf, fb_data, c, nc, lng, lcross, idim)
     call destroy(bpt)
@@ -2662,7 +2678,15 @@ contains
     if ( present(idim) ) then
        if (idim > 0) lcross = .true. 
     end if
-    if ( lng < 1          ) return
+
+    ! multifab_fill_ghost_cells calls fillpatch with a multifab built
+    ! from a boxarray of ghost boxes of the fine multifab to be filled. 
+    ! And the multifab has ng=0.  So there are "valid" cells that are outside the domain.
+    ! For non-periodic boundaries, these cells outside the domain will be filled
+    ! by multifab_physbc later in multifab_fill_ghost_cells.  However, for
+    ! periodic boundaries, mf_fb_fancy_double must be called for these to be filled.
+    if ( lng < 1 .and. .not.any(mf%la%lap%pmask) ) return
+
     call build(bpt, "mf_fill_boundary_test_c")
     call mf_fb_fancy_double_test(mf, fb_data, c, nc, lng, lcross, idim)
     call destroy(bpt)
