9ba4c92e1f2cba33886151aaf23e3bbd8926ebdd
Ann Almgren <asalmgren@lbl.gov>
2014-10-06 11:22:01 -0700

    Add test on plot_int < 0 so we can turn off plotfiles by setting plot_int negative in the inputs file

--- MiniApps/AMR_Adv_Diff_F90/main.f90
+++ MiniApps/AMR_Adv_Diff_F90/main.f90
@@ -370,8 +370,10 @@ program main
      time = time + dt(1)
 
      ! Write plotfile after every plot_int time steps, or if the simulation is done.
-     if (mod(istep,plot_int) .eq. 0 .or. istep .eq. nsteps) then
-        call write_plotfile(mla,phi_new,istep,dx,time,prob_lo,prob_hi)
+     if (plot_int .gt. 0) then
+        if (mod(istep,plot_int) .eq. 0 .or. istep .eq. nsteps) then
+           call write_plotfile(mla,phi_new,istep,dx,time,prob_lo,prob_hi)
+        end if
      end if
 
   end do
@@ -422,6 +424,9 @@ program main
   if ( parallel_IOProcessor() ) then
      print*,"Run time (s) =",run_time_IOproc
   end if
+
+  ! We put this here for debugging to make sure our print statements get seen.
+  call flush(6)
 
   call boxlib_finalize()
 
