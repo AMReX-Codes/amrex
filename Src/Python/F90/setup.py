e1afb4a0e4579669e1140df71317562fc202094b
Matthew Emmett <mwemmett@lbl.gov>
2014-11-14 00:05:59 +0300

    PyBoxLib: Update README files and add test script.

--- Src/Python/F90/setup.py
+++ Src/Python/F90/setup.py
@@ -1,37 +1,20 @@
 '''PyBoxLib setup script.'''
 
 import os
-import sys
 
 from distutils.core import setup
-from distutils.util import get_platform
-from distutils.extension import Extension
 from distutils.command.install import install
 from distutils.command.build import build
-from distutils.spawn import find_executable
-
-from subprocess import call
-
-import numpy as np
-
 
 class build_boxlib(build):
     def run(self):
         build.run(self)
         print 'running build_boxlib'
         self.mkpath(self.build_lib)
-        target_files = [ 'libboxlib.so' ]
+        target_files = [ 'fcboxlib.so' ]
         if not self.dry_run:
-            for target in target_files:
-                self.copy_file('build/lib/libboxlib.so',
-                               os.path.join(self.build_lib, 'fboxlib'))
-
-# build_temp = 'build/temp.%s-%s' % (get_platform(), sys.version[0:3])
-blnpy = Extension('fboxlib.blnpy',
-                  sources = ['src/boxlib_numpy_c.c'],
-                  include_dirs = [np.get_include()],
-                  library_dirs = ['build/lib'],
-                  libraries = ['boxlib'])
+            self.copy_file('build/fcboxlib.so',
+                           os.path.join(self.build_lib, 'fboxlib'))
 
 setup(
     name         = "PyBoxLib",
@@ -42,8 +25,6 @@ setup(
     license      = "XXX",
     keywords     = "BoxLib",
     url          = "https://ccse.lbl.gov/BoxLib/",
-    ext_modules  = [ blnpy ],
-
     cmdclass     = {
         'build': build_boxlib,
         }
