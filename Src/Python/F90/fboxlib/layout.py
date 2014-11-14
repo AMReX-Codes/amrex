30bff5876cc34e213e1302a304e9f2193e127f9a
Matthew Emmett <mwemmett@lbl.gov>
2014-11-14 03:54:34 +0300

    PyBoxLib: Flesh out a few more things.

--- Src/Python/F90/fboxlib/layout.py
+++ Src/Python/F90/fboxlib/layout.py
@@ -18,5 +18,18 @@ class layout():
     """Return number of boxes."""
     return fcboxlib.layout_nboxes(self.cptr)
 
+  @property
+  def local_boxes(self):
+    """List of local boxes."""
+    return [ n for n in range(1, self.nboxes+1) if self.is_local(n) ]
+
+  def is_local(self, n):
+    return fcboxlib.layout_local(self.cptr, n)
+
+  def get_box(self, n):
+    if 1 <= n <= self.nboxes:
+      return fcboxlib.layout_get_box(self.cptr, n)
+    return None
+
   def echo(self):
     fcboxlib.layout_print(self.cptr)
