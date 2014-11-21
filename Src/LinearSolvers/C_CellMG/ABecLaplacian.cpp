dadd93badd192f60e32630d974100a06ffabba3a
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-11-21 13:16:57 -0800

    Revert "tiled GSRB in C_CellMG" because of thread safety.  The problem is C++ std::map::find is not thread safe.
    This reverts commit 57e3776b305e98dfe8a6a858315721c88c6ca5de.

--- Src/LinearSolvers/C_CellMG/ABecLaplacian.cpp
+++ Src/LinearSolvers/C_CellMG/ABecLaplacian.cpp
@@ -381,14 +381,9 @@ ABecLaplacian::Fsmooth (MultiFab&       solnL,
     //const int nc = solnL.nComp(); // FIXME: This LinOp only really supports single-component
     const int nc = 1;
 
-    const bool tiling = true;
-
-#ifdef _OPENMP
-#pragma omp parallel
-#endif
-    for (MFIter solnLmfi(solnL,tiling); solnLmfi.isValid(); ++solnLmfi)
+    for (MFIter solnLmfi(solnL); solnLmfi.isValid(); ++solnLmfi)
     {
-	OrientationIter oitr;
+        oitr.rewind();
 
         const int gn = solnLmfi.index();
 
@@ -402,7 +397,6 @@ ABecLaplacian::Fsmooth (MultiFab&       solnL,
         const Mask& m4 = *mtuple[oitr()]; oitr++;
         const Mask& m5 = *mtuple[oitr()]; oitr++;
 #endif
-	const Box&       tbx     = solnLmfi.tilebox();
         const Box&       vbx     = solnLmfi.validbox();
         FArrayBox&       solnfab = solnL[gn];
         const FArrayBox& rhsfab  = rhsL[gn];
@@ -436,7 +430,7 @@ ABecLaplacian::Fsmooth (MultiFab&       solnL,
                   m2.dataPtr(), ARLIM(m2.loVect()),   ARLIM(m2.hiVect()),
                   f3fab.dataPtr(), ARLIM(f3fab.loVect()),   ARLIM(f3fab.hiVect()),
                   m3.dataPtr(), ARLIM(m3.loVect()),   ARLIM(m3.hiVect()),
-                  tbx.loVect(), tbx.hiVect(), vbx.loVect(), vbx.hiVect(),
+                  vbx.loVect(), vbx.hiVect(),
                   &nc, h[level], &redBlackFlag);
 #endif
 
@@ -460,7 +454,7 @@ ABecLaplacian::Fsmooth (MultiFab&       solnL,
                   m4.dataPtr(), ARLIM(m4.loVect()), ARLIM(m4.hiVect()),
                   f5fab.dataPtr(), ARLIM(f5fab.loVect()), ARLIM(f5fab.hiVect()),
                   m5.dataPtr(), ARLIM(m5.loVect()), ARLIM(m5.hiVect()),
-                  tbx.loVect(), tbx.hiVect(), vbx.loVect(), vbx.hiVect(),
+                  vbx.loVect(), vbx.hiVect(),
                   &nc, h[level], &redBlackFlag);
 #endif
     }
