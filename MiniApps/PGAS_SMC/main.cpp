3df1493aa128c9cfd7527beaab91f8ed77017324
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-09-02 20:40:23 -0400

    added bl_profiler

--- MiniApps/PGAS_SMC/main.cpp
+++ MiniApps/PGAS_SMC/main.cpp
@@ -7,9 +7,13 @@ main (int argc, char* argv[])
 {
     BoxLib::Initialize(argc,argv);
 
+    BL_PROFILE_VAR("main()", pmain);
+
     SMC smc;
 
     smc.evolve();
+
+    BL_PROFILE_VAR_STOP(pmain);
 
     BoxLib::Finalize();
 }
