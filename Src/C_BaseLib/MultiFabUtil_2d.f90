2a6b23af902f4c05f5c34bec0e7b9fc423d1a02d
Ann Almgren <asalmgren@lbl.gov>
2015-09-04 17:02:26 -0700

    Add the functionality to average down face-based MultiFabs as long as the crse BoxArray is a coarsened version of the fine BoxArray.

--- Src/C_BaseLib/MultiFabUtil_2d.f90
+++ Src/C_BaseLib/MultiFabUtil_2d.f90
@@ -25,3 +25,48 @@ subroutine bl_avg_fc_to_cc (lo, hi, &
   enddo
 
 end subroutine bl_avg_fc_to_cc
+
+subroutine bl_avgdown_faces (lo, hi, &
+     f, f_l1, f_l2, f_h1, f_h2, &
+     c, c_l1, c_l2, c_h1, c_h2, &
+     ratio, idir)
+
+  use bl_constants_module
+
+  implicit none
+  integer          :: lo(2),hi(2)
+  integer          :: f_l1, f_l2, f_h1, f_h2
+  integer          :: c_l1, c_l2, c_h1, c_h2
+  integer          :: ratio(3), idir
+  double precision :: f(f_l1:f_h1, f_l2:f_h2)
+  double precision :: c(c_l1:c_h1, c_l2:c_h2)
+
+  ! Local variables
+  integer i,j,n,facx,facy
+
+  facx = ratio(1)
+  facy = ratio(2)
+
+  if (idir .eq. 0) then
+         do j = lo(2), hi(2)
+            do i = lo(1), hi(1)
+               c(i,j) = ZERO
+               do n = 0,facy-1
+                  c(i,j) = c(i,j) + f(facx*i,facy*j+n)
+               end do
+               c(i,j) = c(i,j) / facy
+            end do
+         end do
+  else 
+         do i = lo(1), hi(1)
+            do j = lo(2), hi(2)
+               c(i,j) = ZERO
+               do n = 0,facx-1
+                  c(i,j) = c(i,j) + f(facx*i+n,facy*j)
+               end do
+               c(i,j) = c(i,j) / facx
+            end do
+         end do
+  end if
+
+end subroutine bl_avgdown_faces
--- Src/C_BaseLib/MultiFabUtil_3d.f90
+++ Src/C_BaseLib/MultiFabUtil_3d.f90
@@ -31,3 +31,72 @@ subroutine bl_avg_fc_to_cc (lo, hi, &
   enddo
 
 end subroutine bl_avg_fc_to_cc
+
+subroutine bl_avgdown_faces (lo, hi, &
+     f, f_l1, f_l2, f_l3, f_h1, f_h2, f_h3, &
+     c, c_l1, c_l2, c_l3, c_h1, c_h2, c_h3, &
+     ratio,idir)
+
+  use bl_constants_module
+
+  implicit none
+  integer          :: lo(3),hi(3)
+  integer          :: f_l1, f_l2, f_l3, f_h1, f_h2, f_h3
+  integer          :: c_l1, c_l2, c_l3, c_h1, c_h2, c_h3
+  integer          :: ratio(3), idir
+  double precision :: f(f_l1:f_h1, f_l2:f_h2, f_l3:f_h3)
+  double precision :: c(c_l1:c_h1, c_l2:c_h2, c_l3:c_h3)
+
+  ! Local variables
+  integer i,j,k,m,n,facx,facy,facz
+
+  facx = ratio(1)
+  facy = ratio(2)
+  facz = ratio(3)
+
+  if (idir .eq. 0) then
+         do k = lo(3), hi(3)
+            do j = lo(2), hi(2)
+               do i = lo(1), hi(1)
+                  c(i,j,k) = ZERO
+                  do n = 0,facy-1
+                     do m = 0,facz-1
+                        c(i,j,k) = c(i,j,k) + f(facx*i,facy*j+n,facz*k+m)
+                     end do
+                  end do
+                  c(i,j,k) = c(i,j,k) / (facy*facz)
+               end do
+            end do
+         end do
+  else if (idir .eq. 1) then
+         do k = lo(3), hi(3)
+            do j = lo(2), hi(2)
+               do i = lo(1), hi(1)
+                  c(i,j,k) = ZERO
+                  do n = 0,facx-1
+                     do m = 0,facz-1
+                        c(i,j,k) = c(i,j,k) + f(facx*i+n,facy*j,facz*k+m)
+                     end do
+                  end do
+                  c(i,j,k) = c(i,j,k) / (facx*facz)
+               end do
+            end do
+         end do
+  else
+         do k = lo(3), hi(3)
+            do j = lo(2), hi(2)
+               do i = lo(1), hi(1)
+                  c(i,j,k) = ZERO
+                  do n = 0,facx-1
+                     do m = 0,facy-1
+                        c(i,j,k) = c(i,j,k) + f(facx*i+n,facy*j+m,facz*k)
+                     end do
+                  end do
+                  c(i,j,k) = c(i,j,k) / (facx*facy)
+               end do
+            end do
+         end do
+  end if
+
+end subroutine bl_avgdown_faces
+
