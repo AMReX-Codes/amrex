b46bb53e161a418d6f14d5b6ab00a6224798a939
Brian Friesen <bfriesen@lbl.gov>
2015-11-02 15:04:19 -0800

    IndexType: move static variables "Cell" and "Node" to anonymous namespace

--- Src/C_BaseLib/IndexType.cpp
+++ Src/C_BaseLib/IndexType.cpp
@@ -4,6 +4,17 @@
 
 #include <IndexType.H>
 
+namespace
+{
+    static const IndexType Cell(D_DECL(IndexType::CELL,
+                                       IndexType::CELL,
+                                       IndexType::CELL));
+
+    static const IndexType Node(D_DECL(IndexType::NODE,
+                                       IndexType::NODE,
+                                       IndexType::NODE));
+}
+
 void
 IndexType::setType (int       dir,
                     CellIndex t)
@@ -14,18 +25,12 @@ IndexType::setType (int       dir,
 IndexType
 IndexType::TheCellType ()
 {
-    static const IndexType Cell(D_DECL(IndexType::CELL,
-                                       IndexType::CELL,
-                                       IndexType::CELL));
     return Cell;
 }
 
 IndexType
 IndexType::TheNodeType ()
 {
-    static const IndexType Node(D_DECL(IndexType::NODE,
-                                       IndexType::NODE,
-                                       IndexType::NODE));
     return Node;
 }
 
