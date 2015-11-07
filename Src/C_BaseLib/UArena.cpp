ca436aeb511d07e517a80b70ae10c1761c4ec221
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-11-06 19:58:40 -0800

    Added an Arena for UPC++

---
+++ Src/C_BaseLib/UArena.cpp
@@ -0,0 +1,23 @@
+
+#include <UArena.H>
+#include <ParallelDescriptor.H>
+
+void*
+UArena::alloc (std::size_t _sz)
+{
+    if (ParallelDescriptor::TeamSize() > 1) {
+	return upcxx::allocate(_sz);
+    } else {
+	return ::operator new(_sz);
+    }
+}
+
+void
+UArena::free (void* pt)
+{
+    if (ParallelDescriptor::TeamSize() > 1) {
+	upcxx::deallocate(pt);
+    } else {
+	::operator delete(pt);
+    }
+}
