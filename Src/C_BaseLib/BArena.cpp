27f7a8720ffb5fbe36fcb2cb26594cd795437d2d
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-11-06 19:58:40 -0800

    Added an Arena for UPC++

--- Src/C_BaseLib/BArena.cpp
+++ Src/C_BaseLib/BArena.cpp
@@ -1,28 +1,14 @@
 
 #include <BArena.H>
-#include <BoxLib.H>
-#include <ParallelDescriptor.H>
-
-#ifdef BL_USE_UPCXX
-#include <upcxx.h>
-#endif
 
 void*
 BArena::alloc (std::size_t _sz)
 {
-#ifdef BL_USE_UPCXX
-    return upcxx::allocate(_sz);
-#else
     return ::operator new(_sz);
-#endif
 }
 
 void
 BArena::free (void* pt)
 {
-#ifdef BL_USE_UPCXX
-    upcxx::deallocate(pt);
-#else
     ::operator delete(pt);
-#endif
 }
