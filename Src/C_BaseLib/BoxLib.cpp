957aa4a6c05a5c4e0baa3a146250852ff7396f89
Brian Friesen <bfriesen@lbl.gov>
2015-11-09 12:48:24 -0800

    C_BaseLib, F_BaseLib: free only the "local" MPI communicator when calling parallel_finalize() from C++ code
    When running C++/F90 codes with multiple communicators (i.e., with
    sidecars), the code will crash on BoxLib::Finalize() because
    in parallel_finalize() the sidecars are trying to free the "compute"
    communicator. So now we supply the "local" communicator as an argument
    to parallel_finalize().

--- Src/C_BaseLib/BoxLib.cpp
+++ Src/C_BaseLib/BoxLib.cpp
@@ -46,7 +46,7 @@
 #ifdef BL_USE_FORTRAN_MPI
 extern "C" {
     void bl_fortran_mpi_comm_init (int fcomm);
-    void bl_fortran_mpi_comm_free ();
+    void bl_fortran_mpi_comm_free (int fcomm);
 }
 #endif
 
@@ -412,7 +412,8 @@ BoxLib::Finalize (bool finalize_parallel)
     
     if (finalize_parallel) {
 #ifdef BL_USE_FORTRAN_MPI
-	bl_fortran_mpi_comm_free();
+    int fcomm = MPI_Comm_c2f(ParallelDescriptor::Communicator());
+    bl_fortran_mpi_comm_free(fcomm);
 #endif
         ParallelDescriptor::EndParallel();
     }
