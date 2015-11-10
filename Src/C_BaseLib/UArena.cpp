1535d1bfa2c195afc64edf6076ca7405d9ecfc22
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-11-10 14:28:40 -0800

    UArena: check nullptr

--- Src/C_BaseLib/UArena.cpp
+++ Src/C_BaseLib/UArena.cpp
@@ -1,13 +1,23 @@
-// TODO: cehcking the status of allocate
-
 #include <UArena.H>
 #include <ParallelDescriptor.H>
+#include <BaseFab.H>
 
 void*
 UArena::alloc (std::size_t _sz)
 {
     if (ParallelDescriptor::TeamSize() > 1) {
-	return upcxx::allocate(_sz);
+	auto p = upcxx::allocate(_sz);
+	if (p == nullptr) {
+	    std::cout << "===== Proc. " << ParallelDescriptor::MyProc() << " =====\n";
+	    std::cout << "   Failed to allocate " << _sz << " bytes global address space memory!\n";
+	    std::cout << "   Please try to increase the GASNET_MAX_SEGSIZE environment variable.\n";
+	    std::cout << "   For example, export GASNET_MAX_SEGSIZE=512MB\n";
+	    std::cout << "   Total Bytes Allocated in Fabs: " << BoxLib::TotalBytesAllocatedInFabs();
+	    std::cout << "   Highest Watermark in Fabs: " << BoxLib::TotalBytesAllocatedInFabsHWM();
+	    std::cout << std::endl;
+	    BoxLib::Abort("UArena: upcxx::allocate failed");
+	}
+	return p;
     } else {
 	return ::operator new(_sz);
     }
