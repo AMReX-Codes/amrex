b482e83ccceb5c81832e354f4a59575b159068e4
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-11-15 10:18:16 -0800

    AMR_Adv_C: added tagging

--- Tutorials/AMR_Adv_C/Source/Src_nd/Tagging_nd.f90
+++ Tutorials/AMR_Adv_C/Source/Src_nd/Tagging_nd.f90
@@ -23,7 +23,7 @@ subroutine state_error(tag,tag_lo,tag_hi, &
                        lo,hi,&
                        dx,problo,time,level)
 
-!  use tagging_params_module
+  use tagging_params_module, only : phierr, phigrad, max_phierr_lev, max_phigrad_lev
   implicit none
   
   integer          :: lo(3),hi(3)
@@ -35,6 +35,51 @@ subroutine state_error(tag,tag_lo,tag_hi, &
   integer          :: tag(tag_lo(1):tag_hi(1),tag_lo(2):tag_hi(2),tag_lo(3):tag_hi(3))
   double precision :: problo(3),dx(3),time
   integer          :: level,set,clear
+
+  double precision :: ax, ay, az
+  integer          :: i, j, k, dim
+
+  if (state_lo(3) .eq. state_hi(3)) then
+     dim = 2
+  else
+     dim = 3
+  end if
+  
+  ! Tag on regions of high phi
+  if (level .lt. max_phierr_lev) then
+     do       k = lo(3), hi(3)
+        do    j = lo(2), hi(2)
+           do i = lo(1), hi(1)
+              if (state(i,j,k) .ge. phierr) then
+                 tag(i,j,k) = set
+              endif
+           enddo
+        enddo
+     enddo
+  endif
+
+  ! Tag on regions of high phisity gradient
+  if (level .lt. max_phigrad_lev) then
+     do       k = lo(3), hi(3)
+        do    j = lo(2), hi(2)
+           do i = lo(1), hi(1)
+              ax = abs(state(i-1,j,k)-state(i,j,k))
+              ax = max(ax, abs(state(i,j,k)-state(i+1,j,k)))
+              ay = abs(state(i,j-1,k)-state(i,j,k))
+              ay = max(ay, abs(state(i,j,k)-state(i,j+1,k)))
+              if (max(ax,ay) .ge. phigrad) then
+                 tag(i,j,k) = set
+              else if (dim .eq. 3) then
+                 az = abs(state(i,j,k-1)-state(i,j,k))
+                 az = max(az, abs(state(i,j,k)-state(i,j,k+1)))
+                 if (az .ge. phigrad) then
+                    tag(i,j,k) = set
+                 end if
+              end if
+            enddo
+         enddo
+      end do
+   endif
   
 end subroutine state_error
 
