76b19d2f6f15a6c4031fed4ed6cbe1fcd0c78586
Andy Nonaka <AJNonaka@lbl.gov>
2015-11-16 10:59:34 -0800

    3d velocity field with w=1 and inputs file

--- Tutorials/AMR_Adv_F/Source/compute_velocity.f90
+++ Tutorials/AMR_Adv_F/Source/compute_velocity.f90
@@ -90,27 +90,27 @@ contains
     end do
 
   end subroutine compute_velocity
-
+  
   subroutine compute_velocity_2d(velx, vely, ng, lo, hi, dx, time)
 
     integer          :: lo(2), hi(2), ng
     double precision :: velx(lo(1)-ng:,lo(2)-ng:)
     double precision :: vely(lo(1)-ng:,lo(2)-ng:)
     double precision :: dx,time
- 
+
     ! local varables
     integer          :: i,j
     double precision :: x,y
     double precision :: psi(lo(1)-2:hi(1)+2,lo(2)-2:hi(2)+2)
 
     ! streamfunction psi
-      do j = lo(2)-2, hi(2)+2
-         y = (dble(j)+0.5d0)*dx
-         do i = lo(1)-2, hi(1)+2
-            x = (dble(i)+0.5d0)*dx
-            psi(i,j) =  sin(M_PI*x)**2 * sin(M_PI*y)**2 * cos (M_PI*time/2.d0) / M_PI
-         end do
-      end do
+    do j = lo(2)-2, hi(2)+2
+       y = (dble(j)+0.5d0)*dx
+       do i = lo(1)-2, hi(1)+2
+          x = (dble(i)+0.5d0)*dx
+          psi(i,j) =  sin(M_PI*x)**2 * sin(M_PI*y)**2 * cos (M_PI*time/2.d0) / M_PI
+       end do
+    end do
 
     ! x velocity
     do j=lo(2)-1,hi(2)+1
@@ -130,19 +130,56 @@ contains
        end do
     end do
 
-    end subroutine compute_velocity_2d
+  end subroutine compute_velocity_2d
 
-    subroutine compute_velocity_3d(velx, vely, velz, ng, lo, hi, dx, time)
+  subroutine compute_velocity_3d(velx, vely, velz, ng, lo, hi, dx, time)
 
     integer          :: lo(3), hi(3), ng
     double precision :: velx(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
     double precision :: vely(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
     double precision :: velz(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:)
     double precision :: dx,time
- 
+
     ! local varables
+    integer          :: i,j,k
+    double precision :: x,y
+    double precision :: psi(lo(1)-2:hi(1)+2,lo(2)-2:hi(2)+2,lo(3)-1:hi(3)+1)
 
+    ! streamfunction psi
+    do k = lo(3)-1, hi(3)+1
+       do j = lo(2)-2, hi(2)+2
+          y = (dble(j)+0.5d0)*dx
+          do i = lo(1)-2, hi(1)+2
+             x = (dble(i)+0.5d0)*dx
+             psi(i,j,k) =  sin(M_PI*x)**2 * sin(M_PI*y)**2 * cos (M_PI*time/2.d0) / M_PI
+          end do
+       end do
+    end do
 
+    ! x velocity
+    do k=lo(3)-1,hi(3)+1
+       do j=lo(2)-1,hi(2)+1
+          y = (dble(j)+0.5d0) * dx
+          do i=lo(1)-1,hi(1)+2
+             x = dble(i) * dx
+             velx(i,j,k) =  -( (psi(i,j+1,k)+psi(i-1,j+1,k)) - (psi(i,j-1,k)+psi(i-1,j-1,k)) ) / (4.d0*dx)
+          end do
+       end do
+    end do
+
+    ! y velocity
+    do k=lo(3)-1,hi(3)+1
+       do j=lo(2)-1,hi(2)+2
+          y = dble(j) * dx
+          do i=lo(1)-1,hi(1)+1
+             x = (dble(i)+0.5d0) * dx
+             vely(i,j,k) = ( (psi(i+1,j,k)+psi(i+1,j-1,k)) - (psi(i-1,j,k)+psi(i-1,j-1,k)) ) / (4.d0*dx)
+          end do
+       end do
+    end do
+
+    ! z velocity
+    velz = 1.d0
 
   end subroutine compute_velocity_3d
 
