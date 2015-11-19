c17675e1a2544abdf7e39ec69631736540250a1a
Brian Friesen <bfriesen@lbl.gov>
2015-11-19 09:20:09 -0800

    MultiFab, HPGMG: misc. fixes which mirror Sam's most recent changes in HPGMG

--- Src/C_BaseLib/MultiFab.cpp
+++ Src/C_BaseLib/MultiFab.cpp
@@ -1807,6 +1807,7 @@ MultiFab::CreateHPGMGLevel (level_type* level,
     level->allocated_blocks = 0;
     level->tag              = log2(level->dim.i);
     level->h                = h0;
+    level->fluxes           = NULL;
 
     // allocate 3D array of integers to hold the MPI rank of the corresponding box and initialize to -1 (unassigned)
     level->rank_of_box = (int*)malloc(level->boxes_in.i*level->boxes_in.j*level->boxes_in.k*sizeof(int));
@@ -1928,18 +1929,18 @@ MultiFab::CreateHPGMGLevel (level_type* level,
       );
     }
 
-
-    // build an assists data structure which specifies which cells are within the domain (used with STENCIL_FUSE_BC)
-    initialize_valid_region(level);
-
-
     // build an assist structure for Gauss Seidel Red Black that would facilitate unrolling and SIMDization...
+    level->RedBlack_base = NULL;
     level->RedBlack_FP = NULL;
     if(level->num_my_boxes){
       int i,j;
       int kStride = level->my_boxes[0].kStride;
       int jStride = level->my_boxes[0].jStride;
-      level->RedBlack_FP = (double*)malloc(2*kStride*sizeof(double));
+      level->RedBlack_base = (double*)malloc(2*kStride*sizeof(double)+256); // used for free()
+      level->RedBlack_FP   = level->RedBlack_base; // aligned version
+      // align first *non-ghost* zone element to a 64-Byte boundary...
+      while( (uint64_t)(level->RedBlack_FP + level->box_ghosts*(1+level->box_jStride)) & 0x3f ){level->RedBlack_FP++;}
+      // initialize RedBlack array...
       for(j=0-level->box_ghosts;j<level->box_dim+level->box_ghosts;j++){
       for(i=0-level->box_ghosts;i<level->box_dim+level->box_ghosts;i++){
         int ij = (i+level->box_ghosts) + (j+level->box_ghosts)*jStride;
