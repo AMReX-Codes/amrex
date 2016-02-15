eaca6d91abd090f99faf1956223199be5395f3cd
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-02-15 16:02:47 -0500

    Turn on floating point exception trapping for PGI 16 and later

--- Src/F_BaseLib/backtrace_c.cpp
+++ Src/F_BaseLib/backtrace_c.cpp
@@ -147,7 +147,7 @@ extern "C"
 	signal(SIGSEGV, backtrace_handler); // catch seg falult
 	signal(SIGINT,  backtrace_handler);
 #if (defined(BL_TESTING) || defined(DEBUG)) && defined(__linux__)
-#if !defined(__PGI)
+#if !defined(__PGI) || (__PGIC__ >= 16)
         // trap floating point exceptions
 	feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW);
 	signal(SIGFPE, backtrace_handler);
