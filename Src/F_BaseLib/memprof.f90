e4ffb3bf6059e09547600bebf02239ddf9e74225
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-03-14 17:47:03 -0700

    MemProfiler: fortran boxarray

--- Src/F_BaseLib/memprof.f90
+++ Src/F_BaseLib/memprof.f90
@@ -21,4 +21,20 @@ contains
     fab_numdoubles_hwm = fab_get_high_water_mark()
   end function fab_numdoubles_hwm
 
+  function boxarray_bytes() bind(c, name='memprof_boxarray_bytes')
+    use boxarray_module
+    integer(kind=c_long) :: boxarray_bytes
+    type(mem_stats) :: ms
+    ms = boxarray_mem_stats()
+    ! 7 integers/box * 4 bytes/integer = 28 bytes/box
+    boxarray_bytes = (ms%num_alloc - ms%num_dealloc) * 28
+  end function boxarray_bytes
+
+  function boxarray_bytes_hwm() bind(c, name='memprof_boxarray_bytes_hwm')
+    use boxarray_module
+    integer(kind=c_long) :: boxarray_bytes_hwm
+    ! 7 integers/box * 4 bytes/integer = 28 bytes/box
+    boxarray_bytes_hwm = boxarray_get_high_water_mark() * 28
+  end function boxarray_bytes_hwm
+
 end module memprof_module
