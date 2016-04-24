deba0673798f04d693b8e6253bd5cc5ad266233e
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-04-24 11:15:03 -0700

    FI: ParmParse module

--- Src/F_Interfaces/BaseLib/baselib_mod.f90
+++ Src/F_Interfaces/BaseLib/baselib_mod.f90
@@ -5,7 +5,9 @@ module baselib_module
   use box_module
   use boxarray_module
   use multifab_module
-  use parallel_module
   use omp_module
+  use parallel_module
+  use parmparse_module
+  use string_module
 
 end module baselib_module
