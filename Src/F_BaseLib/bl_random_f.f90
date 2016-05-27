e7fd43d0cfe8dff6f79d056fcba73b3d5c622b50
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-05-27 15:55:15 -0700

    Fortran bl_random: add poisson

--- Src/F_BaseLib/bl_random_f.f90
+++ Src/F_BaseLib/bl_random_f.f90
@@ -11,7 +11,7 @@ module bl_random_module
 
   private 
   public :: bl_rng_build, bl_rng_destroy, bl_rng_get, bl_rng_save, bl_rng_restore, &
-       bl_rng_uniform_real, bl_rng_normal
+       bl_rng_uniform_real, bl_rng_normal, bl_rng_poisson
 
   type bl_rng_uniform_real
      type(c_ptr), private :: p = c_null_ptr
@@ -21,29 +21,38 @@ module bl_random_module
      type(c_ptr), private :: p = c_null_ptr
   end type bl_rng_normal
 
+  type bl_rng_poisson
+     type(c_ptr), private :: p = c_null_ptr
+  end type bl_rng_poisson
+
   interface bl_rng_build
      module procedure bl_rng_build_uniform_real
      module procedure bl_rng_build_normal
+     module procedure bl_rng_build_poisson
   end interface bl_rng_build
 
   interface bl_rng_destroy
      module procedure bl_rng_destroy_uniform_real
      module procedure bl_rng_destroy_normal
+     module procedure bl_rng_destroy_poisson
   end interface bl_rng_destroy
 
   interface bl_rng_get
      module procedure bl_rng_get_uniform_real
      module procedure bl_rng_get_normal
+     module procedure bl_rng_get_poisson
   end interface bl_rng_get
 
   interface bl_rng_save
      module procedure bl_rng_save_uniform_real
      module procedure bl_rng_save_normal
+     module procedure bl_rng_save_poisson
   end interface bl_rng_save
 
   interface bl_rng_restore
      module procedure bl_rng_restore_uniform_real
      module procedure bl_rng_restore_normal
+     module procedure bl_rng_restore_poisson
   end interface bl_rng_restore
 
   ! uniform real distribution
@@ -120,6 +129,44 @@ module bl_random_module
        type(c_ptr) :: rng
        character(c_char), intent(in) :: name(*)
      end subroutine bl_rng_restore_normal_c
+  end interface
+
+  ! poisson distribution
+  interface
+     subroutine bl_rng_new_poisson_c(rng, s, mean, rank, nprocs) bind(c)
+       use, intrinsic :: iso_c_binding
+       implicit none
+       type(c_ptr) :: rng
+       integer(c_int), intent(in), value :: s, rank, nprocs
+       real(c_double), intent(in), value :: mean
+     end subroutine bl_rng_new_poisson_c
+
+     subroutine bl_rng_delete_poisson_c(rng) bind(c)
+       use, intrinsic :: iso_c_binding
+       implicit none
+       type(c_ptr), value :: rng
+     end subroutine bl_rng_delete_poisson_c
+
+     function bl_rng_get_poisson_c(rng) result(r) bind(c)
+       use, intrinsic :: iso_c_binding
+       implicit none
+       type(c_ptr), value :: rng
+       integer(c_int) :: r
+     end function bl_rng_get_poisson_c
+
+     subroutine bl_rng_save_poisson_c(rng, name) bind(c)
+       use, intrinsic :: iso_c_binding
+       implicit none
+       type(c_ptr), value :: rng
+       character(c_char), intent(in) :: name(*)
+     end subroutine bl_rng_save_poisson_c
+
+     subroutine bl_rng_restore_poisson_c(rng, name) bind(c)
+       use, intrinsic :: iso_c_binding
+       implicit none
+       type(c_ptr) :: rng
+       character(c_char), intent(in) :: name(*)
+     end subroutine bl_rng_restore_poisson_c
   end interface
 
 contains
@@ -227,6 +274,50 @@ contains
     call bl_rng_restore_normal_c(rng%p,filename)
     deallocate(filename)
   end subroutine bl_rng_restore_normal
+
+  ! 
+  ! poisson distribution
+  !
+  subroutine bl_rng_build_poisson(rng, seed, mean)
+    type(bl_rng_poisson), intent(inout) :: rng
+    integer(c_int), intent(in) :: seed
+    real(c_double), intent(in) :: mean
+    call bl_rng_new_poisson_c(rng%p, seed, mean, parallel_myproc(), parallel_nprocs())
+  end subroutine bl_rng_build_poisson
+  !
+  subroutine bl_rng_destroy_poisson(rng)
+    type(bl_rng_poisson), intent(inout) :: rng
+    call bl_rng_delete_poisson_c(rng%p)
+    rng%p = c_null_ptr
+  end subroutine bl_rng_destroy_poisson
+  !
+  function bl_rng_get_poisson(rng) result(r)
+    type(bl_rng_poisson), intent(inout) :: rng
+    integer(c_int) :: r
+    r = bl_rng_get_poisson_c(rng%p)    
+  end function bl_rng_get_poisson
+  !
+  subroutine bl_rng_save_poisson(rng, dirname)
+    type(bl_rng_poisson), intent(in) :: rng
+    character(len=*), intent(in) :: dirname
+    character(c_char), pointer :: filename(:)
+    if (parallel_IOProcessor()) then
+       call fabio_mkdir(dirname)
+    end if
+    call parallel_barrier()
+    call bl_rng_filename(filename, dirname)
+    call bl_rng_save_poisson_c(rng%p,filename)
+    deallocate(filename)
+  end subroutine bl_rng_save_poisson
+  !
+  subroutine bl_rng_restore_poisson(rng, dirname)
+    type(bl_rng_poisson), intent(inout) :: rng
+    character(len=*), intent(in) :: dirname
+    character(c_char), pointer :: filename(:)
+    call bl_rng_filename(filename, dirname)
+    call bl_rng_restore_poisson_c(rng%p,filename)
+    deallocate(filename)
+  end subroutine bl_rng_restore_poisson
 
 end module bl_random_module
 
--- Tutorials/Random_F/main.f90
+++ Tutorials/Random_F/main.f90
@@ -8,24 +8,44 @@ program main
   implicit none
   
   integer :: i
-  double precision :: ra, rb
+  double precision :: ra, rb, r(2)
+  integer :: ia, ib, ir(2)
   type(bl_rng_uniform_real) :: ur_orig, ur_restore
   type(bl_rng_normal) :: nm_orig, nm_restore
+  type(bl_rng_poisson) :: ps_orig, ps_restore
 
   call boxlib_initialize()
 
   ! uniform real distribution: [0.0d0, 1.0d0), seed is 42.
   call bl_rng_build(ur_orig, 42, 0.0d0, 1.0d0)
 
+  ! normal distribution with mean=0.d0, stddev=1.0d0
+  call bl_rng_build(nm_orig, 549, 0.0d0, 1.0d0)
+
+  ! poisson distribution with mean=10.d0
+  call bl_rng_build(ps_orig, 342, 10.0d0)
+
   if (parallel_myproc() .eq. 0) then
-     do i = 1, 10
-        print *, "uniform real: ", bl_rng_get(ur_orig)
-     end do
+     print *, 'uniform real, normal, poisson'
   end if
+  do i = 1, 10
+     r(1)  = bl_rng_get(ur_orig)
+     r(2)  = bl_rng_get(nm_orig)
+     ir(1) = bl_rng_get(ps_orig)
 
-  call bl_rng_save(ur_orig, "rng_state_uniform_real")
+     if (parallel_myproc() .eq. 0) &
+          print *, r(1), r(2), ir(1)
+  end do
 
+  call bl_rng_save   (ur_orig   , "rng_state_uniform_real")
   call bl_rng_restore(ur_restore, "rng_state_uniform_real")
+
+  call bl_rng_save   (nm_orig   , "rng_state_normal")
+  call bl_rng_restore(nm_restore, "rng_state_normal")
+
+  call bl_rng_save   (ps_orig   , "rng_state_poisson")
+  call bl_rng_restore(ps_restore, "rng_state_poisson")
+
 
   if (parallel_myproc() .eq. 0) then
      print *, 'uniform real: original, restart, difference'
@@ -40,19 +60,6 @@ program main
      end if
   end do
 
-  ! normal distribution with mean=0.d0, stddev=1.0d0
-  call bl_rng_build(nm_orig, 549, 0.0d0, 1.0d0)
-
-  if (parallel_myproc() .eq. 0) then
-     do i = 1, 10
-        print *, "normal: ", bl_rng_get(nm_orig)
-     end do
-  end if
-
-  call bl_rng_save(nm_orig, "rng_state_normal")
-
-  call bl_rng_restore(nm_restore, "rng_state_normal")
-
   if (parallel_myproc() .eq. 0) then
      print *, 'normal: original, restart, difference'
   end if
@@ -62,7 +69,20 @@ program main
      if (parallel_myproc() .eq. 0) then
         print *, ra, rb, ra-rb
      else if (ra .ne. rb) then
-        print *, "nomal error!!! ", ra, rb
+        print *, "normal error!!! ", ra, rb
+     end if
+  end do
+
+  if (parallel_myproc() .eq. 0) then
+     print *, 'poisson: original, restart, difference'
+  end if
+  do i = 1, 10
+     ia = bl_rng_get(ps_orig)
+     ib = bl_rng_get(ps_restore)
+     if (parallel_myproc() .eq. 0) then
+        print *, ia, ib, ia-ib
+     else if (ia .ne. ib) then
+        print *, "poisson error!!! ", ia, ib
      end if
   end do
   
@@ -70,6 +90,8 @@ program main
   call bl_rng_destroy(ur_restore)
   call bl_rng_destroy(nm_orig)
   call bl_rng_destroy(nm_restore)
+  call bl_rng_destroy(ps_orig)
+  call bl_rng_destroy(ps_restore)
 
   call boxlib_finalize()
 
