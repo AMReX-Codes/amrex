558017cc88efb0cff9aca1038fdbbad3a92729c0
Michael Zingale <michael.zingale@stonybrook.edu>
2016-06-21 12:58:44 -0400

    add some help

--- Tools/F_scripts/findparams.py
+++ Tools/F_scripts/findparams.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
-# a simple script that returns (to stdout) the paths to _parameter
-# files given a list of directories to search (as arguments on the
-# commandline).  This is used in various makefiles to get the input
-# for write_probin.py
-
 from __future__ import print_function
 
 import sys
 import os
 
 def findparams(param_file_dirs):
+    """
+    a simple script that returns (to stdout) the paths to _parameter
+    files given a list of directories to search (as arguments on the
+    commandline).  This is used in various makefiles to get the input for
+    write_probin.py
+
+    """
 
     params = []
 
     for d in param_file_dirs:
         f = os.path.normpath(d + "/_parameters")
-        if (os.path.isfile(f)):
+        if os.path.isfile(f):
             params.append(f)
 
     for f in params:
@@ -25,6 +27,7 @@ def findparams(param_file_dirs):
 
 if __name__ == "__main__":
 
-    param_file_dirs = sys.argv[1:]
+    if len(sys.argv) == 1:
+        sys.exit("invalid calling sequence.\n  findparams.py path-to-dir1 path-to-dir2 ...\n")
 
-    findparams(param_file_dirs)
+    findparams(sys.argv[1:])
