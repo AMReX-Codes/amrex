f60dc304fd66510a68d2b895eca8ed72b9024faa
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-08-09 14:37:14 -0700

    fix new bugs

--- Src/C_BaseLib/Periodicity.cpp
+++ Src/C_BaseLib/Periodicity.cpp
@@ -6,11 +6,18 @@ Periodicity::shiftIntVect () const
 {
     std::vector<IntVect> r;
 
-    int p[3] = { D_DECL(period[0], period[1], period[2]) };
+    int per[3] = {0,0,0};
+    int jmp[3] = {1,1,1};
 
-    for (int i = -p[0]; i <= p[0]; i += p[0]) {
-    for (int j = -p[1]; j <= p[1]; j += p[1]) {
-    for (int k = -p[2]; k <= p[2]; k += p[2]) {
+    for (int i = 0; i < BL_SPACEDIM; ++i) {
+	if (isPeriodic(i)) {
+	    per[i] = jmp[i] = period[i];
+	}
+    }
+
+    for (int i = -per[0]; i <= per[0]; i += jmp[0]) {
+    for (int j = -per[1]; j <= per[1]; j += jmp[1]) {
+    for (int k = -per[2]; k <= per[2]; k += jmp[2]) {
 	r.push_back(IntVect(D_DECL(i,j,k)));
     }
     }
