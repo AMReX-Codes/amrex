fe3f65c640cff8e25867a7b010002077270e7492
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-09-13 09:53:22 -0700

    BaseFab: dimension agnostic plus

--- Src/C_BaseLib/BaseFab_nd.f90
+++ Src/C_BaseLib/BaseFab_nd.f90
@@ -130,6 +130,49 @@ contains
   end function fort_fab_norm
 
 
+  function fort_fab_sum (lo, hi, src, slo, shi, ncomp, p) result(sm) &
+       bind(c,name='fort_fab_sum')
+    integer, intent(in) :: lo(3), hi(3), slo(3), shi(3), ncomp, p
+    real(c_real), intent(in) :: src(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),ncomp)
+    real(c_real) :: sm
+
+    integer :: i,j,k,n
+
+    sm = 0.0_c_real
+    do n = 1, ncomp
+       do       k = lo(3), hi(3)
+          do    j = lo(2), hi(2)
+             do i = lo(1), hi(1)
+                sm = sm + src(i,j,k,n)
+             end do
+          end do
+       end do
+    end do
+  end function fort_fab_sum
+
+
+  subroutine fort_fab_plus(lo, hi, dst, dlo, dhi, src, slo, shi, sblo, ncomp) &
+       bind(c,name='fort_fab_plus')
+    integer, intent(in) :: lo(3), hi(3), dlo(3), dhi(3), slo(3), shi(3), sblo(3), ncomp
+    real(c_real), intent(in   ) :: src(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),ncomp)
+    real(c_real), intent(inout) :: dst(dlo(1):dhi(1),dlo(2):dhi(2),dlo(3):dhi(3),ncomp)
+    
+    integer :: i,j,k,n,off(3)
+
+    off = lo - sblo
+
+    do n = 1, ncomp
+       do       k = lo(3), hi(3)
+          do    j = lo(2), hi(2)
+             do i = lo(1), hi(1)
+                dst(i,j,k,n) = dst(i,j,k,n) + src(i+off(1),j+off(2),k+off(3),n)
+             end do
+          end do
+       end do
+    end do
+  end subroutine fort_fab_plus
+
+
   ! dst += a*src
   subroutine fort_fab_saxpy(lo, hi, dst, dlo, dhi, a, src, slo, shi, sblo, ncomp) &
        bind(c,name='fort_fab_saxpy')
