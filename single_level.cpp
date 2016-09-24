6ad31fb261944d87133ab708b4a64c3e4bed2a72
Ann Almgren <asalmgren@lbl.gov>
2016-09-24 15:27:36 -0700

    Remove the other place we were writing out the FAB

--- single_level.cpp
+++ single_level.cpp
@@ -166,8 +166,6 @@ single_level(int nlevs, int nx, int ny, int nz, int max_grid_size, int nppc, boo
     std::cout << "BoxLib:Min of ChargeMF " << ChargeMF.min(0,0) << std::endl;
     std::cout << "BoxLib:Max of ChargeMF " << ChargeMF.max(0,0) << std::endl;
 
-    std::cout << ChargeMF[0] << std::endl;
-
     end_assd = ParallelDescriptor::second() - strt_assd;
 
     // **************************************************************************
