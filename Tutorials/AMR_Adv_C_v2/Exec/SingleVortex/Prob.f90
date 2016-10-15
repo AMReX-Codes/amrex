a38877815f798d3c0d6326e64ca9696c453683fa
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-10-15 11:59:18 -0700

    AmrAdv/AmrCore tutorial: init data on base level

--- Tutorials/AMR_Adv_C_v2/Exec/SingleVortex/Prob.f90
+++ Tutorials/AMR_Adv_C_v2/Exec/SingleVortex/Prob.f90
@@ -3,6 +3,8 @@ subroutine initdata(level, time, lo, hi, &
      phi, phi_lo, phi_hi, &
      dx, prob_lo) bind(C, name="initdata")
 
+  use bl_fort_module, only : bl_spacedim
+
   implicit none
   integer, intent(in) :: level, lo(3), hi(3), phi_lo(3), phi_hi(3)
   double precision, intent(in) :: time
@@ -11,16 +13,9 @@ subroutine initdata(level, time, lo, hi, &
        &                                 phi_lo(3):phi_hi(3))
   double precision, intent(in) :: dx(3), prob_lo(3)
 
-  integer          :: dm
   integer          :: i,j,k
   double precision :: x,y,z,r2
   
-  if (phi_lo(3) .eq. 0 .and. phi_hi(3) .eq. 0) then
-     dm = 2
-  else
-     dm = 3
-  end if
-
   !$omp parallel do private(i,j,k,x,y,z,r2) collapse(2)
   do k=lo(3),hi(3)
      do j=lo(2),hi(2)
@@ -29,7 +24,7 @@ subroutine initdata(level, time, lo, hi, &
         do i=lo(1),hi(1)
            x = prob_lo(1) + (dble(i)+0.5d0) * dx(1)
            
-           if ( dm.eq. 2) then
+           if ( bl_spacedim .eq. 2) then
               r2 = ((x-0.5d0)**2 + (y-0.75d0)**2) / 0.01d0
               phi(i,j,k) = 1.d0 + exp(-r2)
            else
--- Tutorials/AMR_Adv_C_v2/Source/AmrAdvInit.cpp
+++ Tutorials/AMR_Adv_C_v2/Source/AmrAdvInit.cpp
@@ -1,9 +1,50 @@
 
 #include <AmrAdv.H>
+#include <AmrAdv_F.H>
 
 void
 AmrAdv::InitData ()
 {
-
+    if (restart_chkfile.empty()) {
+	InitFromScratch();
+    } else {
+	InitFromCheckpoint();
+    }
 }
 
+void
+AmrAdv::InitFromScratch ()
+{
+    // define coarse level BoxArray and DistributionMap
+    {
+	SetBoxArray(0, MakeBaseGrids());
+	SetDistributionMap(0, DistributionMapping(grids[0], ParallelDescriptor::NProcs()));
+
+	int ncomp = 1;
+	int nghost = 0;
+	phi_new[0] = std::unique_ptr<MultiFab>(new MultiFab(grids[0], ncomp, nghost, dmap[0]));
+	phi_old[0] = std::unique_ptr<MultiFab>(new MultiFab(grids[0], ncomp, nghost, dmap[0]));
+
+	InitLevelData(0);
+    }
+}
+
+void AmrAdv::InitLevelData (int lev)
+{
+    const Real* dx = geom[lev].CellSize();
+    const Real* prob_lo = geom[lev].ProbLo();
+    Real cur_time = t_new[lev];
+
+    MultiFab& state = *phi_new[lev];
+
+    for (MFIter mfi(state); mfi.isValid(); ++mfi)
+	{
+        const Box& box = mfi.validbox();
+        const int* lo  = box.loVect();
+        const int* hi  = box.hiVect();
+
+	initdata(lev, cur_time, ARLIM_3D(lo), ARLIM_3D(hi),
+		 BL_TO_FORTRAN_3D(state[mfi]), ZFILL(dx),
+		 ZFILL(prob_lo));
+    }
+}
