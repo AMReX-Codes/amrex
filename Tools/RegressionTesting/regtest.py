b2e47b80c264fdfde6a25a62f7b0a53a98804e30
Jon Rood <jrood@nrel.gov>
2016-11-15 15:12:43 -0700

    Adding exception handling to a particular shutil.rmtree(). Otherwise, this command can cause errors on certain filesystems and the test suite can completely fail before finishing.

--- Tools/RegressionTesting/regtest.py
+++ Tools/RegressionTesting/regtest.py
@@ -853,7 +853,9 @@ def test_suite(argv):
                         suite.log.warn("unable to tar output file {}".format(pfile))
 
                     else:
-                        shutil.rmtree(pfile)
+                        try: shutil.rmtree(pfile)
+                        except:
+                            suite.log.warn("unable to remove {}".format(pfile))
 
 
         #----------------------------------------------------------------------
