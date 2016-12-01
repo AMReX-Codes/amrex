52550d6756a28f31ba5b76e8c0281942a1d0bf72
vince <vebeckner@lbl.gov>
2016-11-30 17:39:38 -0800

    added flush options to try to fix titan.

--- Src/C_BaseLib/VisMF.cpp
+++ Src/C_BaseLib/VisMF.cpp
@@ -871,6 +871,9 @@ VisMF::WriteHeader (const std::string &mf_name,
         //
         bytesWritten += VisMF::FileOffset(MFHdrFile);
 
+#ifdef BL_FLUSH_ALL_VISMF_WRITES
+        MFHdrFile.flush();
+#endif
         MFHdrFile.close();
 
 	if(checkFilePositions) {
@@ -988,7 +991,9 @@ VisMF::Write (const FabArray<FArrayBox>&    mf,
               writePosition += hLength + writeDataSize;
             }
             nfi.Stream().write(allFabData, bytesWritten);
-            //nfi.Stream().flush();
+#ifdef BL_FLUSH_ALL_VISMF_WRITES
+            nfi.Stream().flush();
+#endif
 	    delete [] allFabData;
 
 	  } else {    // ---- write fabs individually
@@ -1002,6 +1007,9 @@ VisMF::Write (const FabArray<FArrayBox>&    mf,
 	        fio.write_header(hss, fab, fab.nComp());
 	        hLength = hss.tellp();
                 nfi.Stream().write(hss.str().c_str(), hLength);    // ---- the fab header
+#ifdef BL_FLUSH_ALL_VISMF_WRITES
+                nfi.Stream().flush();
+#endif
 	      }
 	      if(doConvert) {
 	        char *cDataPtr = new char[writeDataSize];
@@ -1009,9 +1017,15 @@ VisMF::Write (const FabArray<FArrayBox>&    mf,
 		                                        writeDataItems,
 		                                        fab.dataPtr(), *whichRD);
                 nfi.Stream().write(cDataPtr, writeDataSize);
+#ifdef BL_FLUSH_ALL_VISMF_WRITES
+                nfi.Stream().flush();
+#endif
 	        delete [] cDataPtr;
 	      } else {    // ---- copy from the fab
                 nfi.Stream().write((char *) fab.dataPtr(), writeDataSize);
+#ifdef BL_FLUSH_ALL_VISMF_WRITES
+                nfi.Stream().flush();
+#endif
 	      }
             }
 	  }
