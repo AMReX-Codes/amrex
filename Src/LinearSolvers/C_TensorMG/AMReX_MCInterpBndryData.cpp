a58f76bfc08fbe59303b337cad64db362caf4ca0
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-12-27 12:02:14 -0800

    update C_TensorMG

--- Src/LinearSolvers/C_TensorMG/AMReX_MCInterpBndryData.cpp
+++ Src/LinearSolvers/C_TensorMG/AMReX_MCInterpBndryData.cpp
@@ -59,10 +59,11 @@ const int* fabhi = (fab).hiVect();           \
 const Real* fabdat = (fab).dataPtr();
 
 MCInterpBndryData::MCInterpBndryData (const BoxArray& _grids,
+				      const DistributionMapping& _dmap,
 				      int             _ncomp,
 				      const Geometry& geom)
     :
-    BndryData(_grids,MCLinOp::bcComponentsNeeded(_ncomp),geom)
+    BndryData(_grids,_dmap,MCLinOp::bcComponentsNeeded(_ncomp),geom)
 {}
 
 //
