73e7797d5664d6c938b0e0db2cbf5d1bc88b55b3
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-12-28 10:45:22 -0800

    some bugs fixed: forgot to pass MFInfo to MultiFab constructor

--- Src/LinearSolvers/C_to_F_MG/AMReX_MGT_Solver.cpp
+++ Src/LinearSolvers/C_to_F_MG/AMReX_MGT_Solver.cpp
@@ -925,7 +925,7 @@ MGT_Solver::set_cfa_const (Real alpha, int lev)
     } else {
 	MFInfo info;
 	info.SetAlloc(false);
-	MultiFab cc(m_grids[lev], m_dmap[lev], 1, 0); // cell-centered MF      
+	MultiFab cc(m_grids[lev], m_dmap[lev], 1, 0, info); // cell-centered MF      
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
