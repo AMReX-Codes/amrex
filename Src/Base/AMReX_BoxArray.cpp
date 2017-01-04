df75b1df631ab766706a975a1cdc39909dd45b7d
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-01-04 12:51:26 -0800

    remove unsafe assertion

--- Src/Base/AMReX_BoxArray.cpp
+++ Src/Base/AMReX_BoxArray.cpp
@@ -271,7 +271,6 @@ BoxArray::operator= (const BoxArray& rhs)
 void
 BoxArray::define (const Box& bx)
 {
-    BL_ASSERT(size() == 0);
     clear();
     m_transformer->setIxType(bx.ixType());
     m_ref->define(amrex::enclosedCells(bx));
@@ -280,7 +279,6 @@ BoxArray::define (const Box& bx)
 void
 BoxArray::define (const BoxList& bl)
 {
-    BL_ASSERT(size() == 0);
     clear();
     m_ref->define(bl);
     type_update();
