a49f5ce478735a9bc682b75ea89b26fbe4c4640c
Andrew Myers <atmyers2@gmail.com>
2017-01-12 14:16:17 -0800

    Use Copy with shifted boxes instead of custom Fortran routines to perform the shifting.

--- tests/MovingWindow/main.cpp
+++ tests/MovingWindow/main.cpp
@@ -12,8 +12,9 @@
 void testMovingWindow() {
   
   int n_steps, n_cell, max_grid_size, is_periodic[BL_SPACEDIM];
-  Array<Real> moving_window_x(BL_SPACEDIM, 0.0);
-  Array<Real> moving_window_v(BL_SPACEDIM, 0.0);
+  int do_moving_window, moving_window_dir;
+  Real moving_window_x = 0.0;
+  Real moving_window_v = 0.0;
   Array<Real> prob_lo(BL_SPACEDIM);
   Array<Real> prob_hi(BL_SPACEDIM);
 
@@ -25,17 +26,16 @@ void testMovingWindow() {
     pp.get("n_steps", n_steps);
     pp.get("n_cell", n_cell);
     pp.get("max_grid_size", max_grid_size);
+    pp.get("do_moving_window", do_moving_window);
+    pp.get("moving_window_dir", moving_window_dir);
+    pp.get("moving_window_v", moving_window_v);
     pp.getarr("prob_lo", prob_lo, 0, BL_SPACEDIM);
     pp.getarr("prob_hi", prob_hi, 0, BL_SPACEDIM);
-    pp.getarr("prob_lo", moving_window_x, 0, SPACEDIM);
-    pp.getarr("moving_window_v", moving_window_v, 0, BL_SPACEDIM);
   }
 
-  // multiply the moving window velocity by the speed of light
-  for (int dir = 0; dir < BL_SPACEDIM; dir++) {
-    moving_window_v[dir] *= PhysConst::c;
-  }
-
+  moving_window_x = prob_lo[moving_window_dir];
+  moving_window_v *= PhysConst::c;
+  
   BoxArray ba;
   Geometry geom;
   {
@@ -52,9 +52,8 @@ void testMovingWindow() {
 
     int coord = 0;  // cartesian
 
-    is_periodic[0] = 0;
-    is_periodic[1] = 0;
-    is_periodic[2] = 0;
+    for (int i = 0; i < BL_SPACEDIM; i++) is_periodic[i] = 1;
+    is_periodic[moving_window_dir] = 0;
 
     geom.define(domain, &real_box,coord, is_periodic);
   }
@@ -84,92 +83,40 @@ void testMovingWindow() {
     Array<std::string> varnames{"E"};
     BoxLib::WriteSingleLevelPlotfile(plotname, outputE, varnames, geom, 0.0, 0);
 
-    int num_shift[BL_SPACEDIM];
+    int num_shift;
+    int dir = moving_window_dir;
+
     Real new_lo[BL_SPACEDIM];
-    Real new_hi[BL_SPACEDIM];
-    
+    Real new_hi[BL_SPACEDIM];    
     const Real* current_lo = geom.ProbLo();
     const Real* current_hi = geom.ProbHi();
     const Real* dx = geom.CellSize();
 
-    for (int dir=0; dir<BL_SPACEDIM; dir++) {
-      moving_window_x[dir] += moving_window_v[dir] * dt;
-      num_shift[dir] = (moving_window_x[dir] - current_lo[dir]) / dx[dir];
-      new_lo[dir] = current_lo[dir] + num_shift[dir] * dx[dir];
-      new_hi[dir] = current_hi[dir] + num_shift[dir] * dx[dir];
+    moving_window_x += moving_window_v * dt;
+    num_shift = (moving_window_x - current_lo[dir]) / dx[dir];
+
+    for (int i=0; i<BL_SPACEDIM; i++) {
+      new_lo[i] = current_lo[i];
+      new_hi[i] = current_hi[i];
     }
-
-    int max_shift = num_shift[0];
-    for (int dir=1; dir<BL_SPACEDIM; dir++) {
-      max_shift = std::max(max_shift, num_shift[dir]);
-    }
-
-    if (max_shift == 0) continue;
-    if (max_shift <= Nghost) {
-
-      if (num_shift[0] > 0) {
-	for ( MFIter mfi(E); mfi.isValid(); ++mfi ) {
-	  const Box& bx = mfi.validbox();
-	  shift_x(E[mfi].dataPtr(),
-		  bx.loVect(), bx.hiVect(), &Nghost, &num_shift[0]);
-	}
-      }
-
-      if (num_shift[1] > 0) {
-	for ( MFIter mfi(E); mfi.isValid(); ++mfi ) {
-	  const Box& bx = mfi.validbox();
-	  shift_y(E[mfi].dataPtr(),
-		  bx.loVect(), bx.hiVect(), &Nghost, &num_shift[1]);
-	}
-      }
-
-      if (num_shift[2] > 0) {
-	for ( MFIter mfi(E); mfi.isValid(); ++mfi ) {
-	  const Box& bx = mfi.validbox();
-	  shift_z(E[mfi].dataPtr(),
-		  bx.loVect(), bx.hiVect(), &Nghost, &num_shift[2]);
-	}
-      }
-      E.FillBoundary(geom.periodicity());
-    }
-
-    if (max_shift > Nghost) {
-
-      MultiFab tmpE(ba, 1, max_shift);
-      MultiFab::Copy(tmpE, E, 0, 0, 1, 0);
-      tmpE.FillBoundary(geom.periodicity());
-      
-      if (num_shift[0] > 0) {
-	for ( MFIter mfi(tmpE); mfi.isValid(); ++mfi ) {
-	  const Box& bx = mfi.validbox();
-	  shift_x(tmpE[mfi].dataPtr(),
-		  bx.loVect(), bx.hiVect(), &max_shift, &num_shift[0]);
-	}
-      }
-
-      if (num_shift[1] > 0) {
-	for ( MFIter mfi(tmpE); mfi.isValid(); ++mfi ) {
-	  const Box& bx = mfi.validbox();
-	  shift_y(tmpE[mfi].dataPtr(),
-		  bx.loVect(), bx.hiVect(), &max_shift, &num_shift[1]);
-	}
-      }
-
-      if (num_shift[2] > 0) {
-	for ( MFIter mfi(tmpE); mfi.isValid(); ++mfi ) {
-	  const Box& bx = mfi.validbox();
-	  shift_z(tmpE[mfi].dataPtr(),
-		  bx.loVect(), bx.hiVect(), &max_shift, &num_shift[2]);
-	}
-      }
-      //      tmpE.FillBoundary(geom.periodicity());
-      MultiFab::Copy(E, tmpE, 0, 0, 1, Nghost);
-      E.FillBoundary(geom.periodicity());
-    }
-
+    new_lo[dir] = current_lo[dir] + num_shift * dx[dir];
+    new_hi[dir] = current_hi[dir] + num_shift * dx[dir];
     RealBox new_box(new_lo, new_hi);
     geom.ProbDomain(new_box);
 
+    MultiFab tmpE(ba, 1, abs(num_shift));
+    tmpE.setVal(0.0);
+    MultiFab::Copy(tmpE, E, 0, 0, 1, 0);
+    tmpE.FillBoundary(geom.periodicity());
+
+    for (MFIter mfi(E); mfi.isValid(); ++mfi ) {
+      const Box& dstBox = mfi.validbox();
+      Box srcBox(dstBox.smallEnd(), dstBox.bigEnd());
+      srcBox.shift(dir, num_shift);
+      E[mfi].copy(tmpE[mfi], srcBox, 0, dstBox, 0, 1);
+    }
+
+    E.FillBoundary(geom.periodicity());
   }
 }
 
