60ae64dc04523646bfadce0f78f34b9cfc080942
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-01-13 09:26:57 -0800

    remove accidentally checked in assertion

--- Exec/test_particle_injection/main.cpp
+++ Exec/test_particle_injection/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char* argv[])
 {
     boxlib_init(argc, argv);
 
+    static_assert(BL_SPACEDIM == 3, "3D only");
+
     warpx_init();
 
     for (int i = 1; i <= 10; ++i)
--- Source/main.cpp
+++ Source/main.cpp
@@ -13,8 +13,6 @@ int main(int argc, char* argv[])
 
     BL_PROFILE_VAR("main()", pmain);
 
-    static_assert(BL_SPACEDIM == 3, "3D only");
-
     const Real strt_total = ParallelDescriptor::second();
 
     {
