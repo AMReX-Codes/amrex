f8f07cb7227b54ecaa2fd4ebf30eeacfc583e803
atmyers <atmyers2@gmail.com>
2017-01-19 12:16:07 -0800

    Use ParticleProb and WarpXProb to make the moving window test more like the other test problems.

---
+++ tests/MovingWindow/ParticleProb.cpp
@@ -0,0 +1,17 @@
+
+//
+// Each problem must have its own version of SingleParticleContainer::InitData()
+// to initialize the particle data on this level
+//
+
+#include <cmath>
+
+#include <BLProfiler.H>
+
+#include <ParticleContainer.H>
+#include <WarpXConst.H>
+
+void
+SingleSpeciesContainer::InitData()
+{
+}
