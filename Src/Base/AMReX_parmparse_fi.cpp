0dccefa3c6fcb5bacfa5ff44f23bef15590f2770
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-01-26 12:39:04 -0800

    add parmparse to Fortran

---
+++ Src/Base/AMReX_parmparse_fi.cpp
@@ -0,0 +1,65 @@
+#include <cstring>
+#include <AMReX_ParmParse.H>
+
+using namespace amrex;
+
+extern "C"
+{
+    void amrex_new_parmparse (ParmParse*& pp, const char* name)
+    {
+	pp = new ParmParse(std::string(name));
+    }
+
+    void amrex_delete_parmparse (ParmParse* pp)
+    {
+	delete pp;
+    }
+
+    void amrex_parmparse_get_int (ParmParse* pp, const char* name, int* v)
+    {
+	pp->get(name, *v);
+    }
+
+    void amrex_parmparse_get_double (ParmParse* pp, const char* name, double* v)
+    {
+	pp->get(name, *v);
+    }
+
+    void amrex_parmparse_get_bool (ParmParse* pp, const char* name, int* v)
+    {
+	bool b;
+	pp->get(name, b);
+	*v = b;
+    }
+
+    void amrex_parmparse_get_string (ParmParse* pp, const char* name, char* v, int* len)
+    {
+      std::string b;
+      pp->get(name, b);
+      std::strncpy(v, b.c_str(), *len);
+    }
+
+    void amrex_parmparse_query_int (ParmParse* pp, const char* name, int* v)
+    {
+	pp->query(name, *v);
+    }
+
+    void amrex_parmparse_query_double (ParmParse* pp, const char* name, double* v)
+    {
+	pp->query(name, *v);
+    }
+
+    void amrex_parmparse_query_bool (ParmParse* pp, const char* name, int* v)
+    {
+	bool b = *v;
+	pp->query(name, b);
+	*v = b;
+    }
+
+    void amrex_parmparse_query_string (ParmParse* pp, const char* name, char* v, int* len)
+    {
+      std::string b;
+      pp->query(name, b);
+      std::strncpy(v, b.c_str(), *len);
+    }
+}
