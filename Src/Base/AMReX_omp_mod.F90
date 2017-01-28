e434e688e8edc19e60f634d41635683b38867b9f
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-01-28 12:48:33 -0800

    forgot to check in a new file

---
+++ Src/Base/AMReX_omp_mod.F90
@@ -0,0 +1,41 @@
+#ifdef BL_USE_OMP
+
+module amrex_omp_module
+
+  implicit none
+
+  integer, external :: omp_get_num_threads
+  integer, external :: omp_get_max_threads
+  integer, external :: omp_get_thread_num
+  logical, external :: omp_in_parallel
+
+end module amrex_omp_module
+
+#else
+
+module amrex_omp_module
+
+  implicit none
+
+contains
+
+  integer function omp_get_num_threads()
+    omp_get_num_threads = 1
+  end function omp_get_num_threads
+
+  integer function omp_get_max_threads()
+    omp_get_max_threads = 1
+  end function omp_get_max_threads
+
+  integer function omp_get_thread_num()
+    omp_get_thread_num = 0
+  end function omp_get_thread_num
+
+  logical function omp_in_parallel()
+    omp_in_parallel = .false.
+  end function omp_in_parallel
+
+end module amrex_omp_module
+
+#endif
+
