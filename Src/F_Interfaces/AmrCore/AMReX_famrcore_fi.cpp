de96be0defaa9ddf3a85433445213de6a3635197
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-02-02 12:28:47 -0800

    amrex_famr_core: add static data

--- Src/F_Interfaces/AmrCore/AMReX_famrcore_fi.cpp
+++ Src/F_Interfaces/AmrCore/AMReX_famrcore_fi.cpp
@@ -14,4 +14,17 @@ extern "C" {
     {
 	delete famrcore;
     }
+
+    int amrex_fi_get_max_level (const FAmrCore* famrcore)
+    {
+	return famrcore->maxLevel();
+    }
+
+    void amrex_fi_get_ref_ratio (int* ref_ratio, const FAmrCore* famrcore)
+    {
+	int n = famrcore->maxLevel();
+	for (int i = 0; i < n; ++i) {
+	    ref_ratio[i] = famrcore->MaxRefRatio(i);
+	}
+    }
 }
