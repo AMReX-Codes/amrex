6d690045b3421d2a7af6cbffb91615ba6cbd7003
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-02-03 14:56:21 -0800

    make base level

--- Tutorials/AMR_Adv_CF/Source/initdata.F90
+++ Tutorials/AMR_Adv_CF/Source/initdata.F90
@@ -23,13 +23,28 @@ contains
     type(amrex_distromap) :: dm0
     real(amrex_real) :: time
 
-    time = 0
+    time = 0._amrex_real
+
+    !
+    ! Level 0
+    !
+    call amrex_set_finest_level(0)
+
     call amrex_make_base_grids(ba0)
     call amrex_distromap_build(dm0, ba0)
 
-    call amrex_set_finest_level(0)
+    call make_new_level(0, time, ba0, dm0)
     
+    call amrex_boxarray_destroy(ba0)
+    call amrex_distromap_destroy(dm0)
+ 
+    call init_level_data(0)
 
   end subroutine init_from_scratch
+
+
+  subroutine init_level_data
+
+  end subroutine init_level_data
 
 end module initdata_module
