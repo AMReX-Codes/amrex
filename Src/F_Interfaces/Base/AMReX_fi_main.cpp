2f6849002973ef7770cfe9fd08e1068c5b9ac243
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-02-03 15:13:28 -0800

    Add amrex_fi_module.  It doesn't do anything now.  But in the future we might do some fortran specific init and finalize stuff

--- Src/F_Interfaces/Base/AMReX_fi_main.cpp
+++ Src/F_Interfaces/Base/AMReX_fi_main.cpp
@@ -2,17 +2,25 @@
 #include <AMReX.H>
 #include <AMReX_BLProfiler.H>
 
-extern "C" { void amrex_fmain(); }
+extern "C" { 
+    void amrex_fi_init();
+    void amrex_fi_finalize();
+    void amrex_fmain();
+}
 
 int main (int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
+
+    amrex_fi_init();
 
     BL_PROFILE_VAR("main()", pmain);
 
     amrex_fmain();
 
     BL_PROFILE_VAR_STOP(pmain);
+
+    amrex_fi_finalize();
 
     amrex::Finalize();
 }
