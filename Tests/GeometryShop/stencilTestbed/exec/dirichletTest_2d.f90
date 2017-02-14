a85d42dc28ea56c276be37efa46806ee5e1124bd
Marc Day <MSDay@lbl.gov>
2017-02-14 15:21:02 -0800

    Nearly trivial hack to stencil

--- Tests/GeometryShop/stencilTestbed/exec/dirichletTest_2d.f90
+++ Tests/GeometryShop/stencilTestbed/exec/dirichletTest_2d.f90
@@ -10,7 +10,7 @@ module stencil_test_module
 
 contains
 
-  subroutine get_bndry_grad_stencil(stencil,stencilB,baseiv,normals,centroids,ivs,num) bind(C,name="get_bndry_grad_stencil")
+  subroutine get_bndry_grad_stencil(stencil,stencilB,baseiv,normals,centroids,ivs,num,dx) bind(C,name="get_bndry_grad_stencil")
     integer, intent(in) :: num
     double precision, intent(inout) :: stencil(0:2,0:2,num)
     double precision, intent(inout) ::      stencilB(num)
@@ -18,8 +18,9 @@ contains
     double precision, intent(in   ) ::   normals(dim,num)
     double precision, intent(in   ) :: centroids(dim,num)
     integer,          intent(in   ) ::       ivs(dim,num)
+    double precision, intent(in   ) :: dx
     integer :: i, c(dim), s(dim), iv(dim)
-    double precision :: n(dim), b(dim), x(2), y(2), d(2)
+    double precision :: n(dim), b(dim), x(2), y(2), d(2), fac
 
     ! Rotate work to 1st quadrant, where 1-component is "indepenent".  Find 2 quadratically 
     ! interpolated values at x1=1 and x1=2 along ray normal to boundary, and intersecting
@@ -31,7 +32,9 @@ contains
     !                    + bcval(ivs(i)) . stencilB(i)
     !
     stencil(:,:,1:num) = 0.d0
-    baseiv(:,1:num) = 0    
+    baseiv(:,1:num) = 0
+
+    fac = 1.d0 / dx
 
     do i=1,num
        
@@ -52,26 +55,26 @@ contains
 
        iv(c(1)) = 1 * s(1) + ivs(c(1),i) - baseiv(c(1),i)
        iv(c(2)) = (1 - 1)*s(2) + ivs(c(2),i) - baseiv(c(2),i)
-       stencil(iv(1),iv(2),i) = (0.5d0 * (y(1)-1.d0) * (y(1)-2.d0)) * d(2)/(d(1)*(d(2)-d(1)))
+       stencil(iv(1),iv(2),i) = fac * (0.5d0 * (y(1)-1.d0) * (y(1)-2.d0)) * d(2)/(d(1)*(d(2)-d(1)))
 
        iv(c(2)) = (2 - 1)*s(2) + ivs(c(2),i) - baseiv(c(2),i)
-       stencil(iv(1),iv(2),i) = (- y(1) * (y(1)-2.d0))              * d(2)/(d(1)*(d(2)-d(1)))
+       stencil(iv(1),iv(2),i) = fac * (- y(1) * (y(1)-2.d0))              * d(2)/(d(1)*(d(2)-d(1)))
 
        iv(c(2)) = (3 - 1)*s(2) + ivs(c(2),i) - baseiv(c(2),i)
-       stencil(iv(1),iv(2),i) = (0.5d0 * y(1) * (y(1)-1.d0))        * d(2)/(d(1)*(d(2)-d(1)))
+       stencil(iv(1),iv(2),i) = fac * (0.5d0 * y(1) * (y(1)-1.d0))        * d(2)/(d(1)*(d(2)-d(1)))
 
 
        iv(c(1)) = 2 * s(1) + ivs(c(1),i) - baseiv(c(1),i)  
        iv(c(2)) = (1 - 1)*s(2) + ivs(c(2),i) - baseiv(c(2),i)
-       stencil(iv(1),iv(2),i) = (0.5d0 * (y(2)-1.d0) * (y(2)-2.d0)) * (-d(1)/(d(2)*(d(2)-d(1))))
+       stencil(iv(1),iv(2),i) = fac * (0.5d0 * (y(2)-1.d0) * (y(2)-2.d0)) * (-d(1)/(d(2)*(d(2)-d(1))))
 
        iv(c(2)) = (2 - 1)*s(2) + ivs(c(2),i) - baseiv(c(2),i)
-       stencil(iv(1),iv(2),i) = (- y(2) * (y(2)-2.d0))              * (-d(1)/(d(2)*(d(2)-d(1))))
+       stencil(iv(1),iv(2),i) = fac * (- y(2) * (y(2)-2.d0))              * (-d(1)/(d(2)*(d(2)-d(1))))
 
        iv(c(2)) = (3 - 1)*s(2) + ivs(c(2),i) - baseiv(c(2),i)
-       stencil(iv(1),iv(2),i) = (0.5d0 * y(2) * (y(2)-1.d0))        * (-d(1)/(d(2)*(d(2)-d(1))))
+       stencil(iv(1),iv(2),i) = fac * (0.5d0 * y(2) * (y(2)-1.d0))        * (-d(1)/(d(2)*(d(2)-d(1))))
 
-       stencilB(i) = - (d(2)+d(1))/(d(2)*d(1))
+       stencilB(i) = - fac * (d(2)+d(1))/(d(2)*d(1))
        
     enddo
 
@@ -80,7 +83,7 @@ contains
 
   subroutine apply_bndry_grad_stencil(dudn, b,&
        u,u_l1,u_l2,u_h1,u_h2, &
-       stencil,stencilB,baseiv,num,dx) bind(C,name="apply_bndry_grad_stencil")
+       stencil,stencilB,baseiv,num) bind(C,name="apply_bndry_grad_stencil")
     integer, intent(in) :: num
     double precision, intent(inout) :: dudn(num)
     double precision, intent(in   ) :: b(num)
@@ -89,19 +92,16 @@ contains
     double precision, intent(in   ) :: stencil(0:2,0:2,num)
     double precision, intent(in   ) :: stencilB(num)
     integer,          intent(in   ) :: baseiv(dim,num)
-    double precision, intent(in   ) :: dx
 
     double precision :: fac
     integer :: i, j, L
-
-    fac = 1.d0 / dx
 
     do L=1,num
        
        i = baseiv(1,L)
        j = baseiv(2,L)
 
-       dudn(L) = fac * (b(L) * stencilB(L) &
+       dudn(L) = b(L) * stencilB(L) &
             + u(i  ,j  ) * stencil(0,0,L) &
             + u(i+1,j  ) * stencil(1,0,L) &
             + u(i+2,j  ) * stencil(2,0,L) &
@@ -110,7 +110,7 @@ contains
             + u(i+2,j+1) * stencil(2,1,L) &
             + u(i  ,j+2) * stencil(0,2,L) &
             + u(i+1,j+2) * stencil(1,2,L) &
-            + u(i+2,j+2) * stencil(2,2,L) )
+            + u(i+2,j+2) * stencil(2,2,L)
     enddo
 
   end subroutine apply_bndry_grad_stencil
