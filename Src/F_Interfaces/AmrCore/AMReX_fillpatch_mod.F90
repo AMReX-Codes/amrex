362204271ac20a1c1ffd9c88be5914ecf3581360
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-03-08 18:10:45 -0800

    wip: need to fix circular dependency of fortran modules

--- Src/F_Interfaces/AmrCore/AMReX_fillpatch_mod.F90
+++ Src/F_Interfaces/AmrCore/AMReX_fillpatch_mod.F90
@@ -13,6 +13,18 @@ module amrex_fillpatch_module
      module procedure amrex_fillpatch_two
   end interface amrex_fillpatch
 
+#if 0
+  interface
+     subroutine amrex_physbc_proc (mf, scomp, ncomp, time) bind(c)
+!       import
+       implicit none
+       type(c_ptr), value :: mf
+       integer(c_int), value :: scomp, ncomp
+       real(amrex_real), value :: time
+     end subroutine amrex_physbc_proc
+  end interface
+#endif
+  
   interface
      subroutine amrex_fi_fillpatch_single(mf, time, smf, stime, ns, scomp, dcomp, ncomp, &
           geom, pbc) bind(c)
@@ -44,7 +56,67 @@ contains
        time, scomp, dcomp, ncomp)
     type(amrex_multifab), intent(inout) :: mf
     type(amrex_multifab), intent(in   ) :: mfold, mfnew
+    integer, intent(in) :: scomp, dcomp, ncomp
+    real(amrex_real), intent(in) :: told, tnew, time
+    type(amrex_geometry), intent(in) :: geom
+    procedure(amrex_physbc_proc) :: fill_physbc
+! #if 0
+!     teps = 1.e-3_amrex_real * (t_new(lev) - t_old(lev))
+!     if (abs(time-t_new(lev)) .lt. teps) then
+!        allocate(f_mf(1))
+!        allocate(f_time(1))
+!        f_mf(1) = phi_new(lev)
+!        f_time(1) = t_new(lev)
+!     else if (abs(time-t_old(lev)) .lt. teps) then
+!        allocate(f_mf(1))
+!        allocate(f_time(1))
+!        f_mf(1) = phi_old(lev)
+!        f_time(1) = t_old(lev)
+!     else
+!        allocate(f_mf(2))
+!        allocate(f_time(2))
+!        f_mf(1) = phi_old(lev)
+!        f_mf(2) = phi_new(lev)
+!        f_time(1) = t_old(lev)
+!        f_time(2) = t_new(lev)
+!     end if
+! #endif
+  end subroutine amrex_fillpatch_single
 
+  subroutine amrex_fillpatch_two (mf, told_c, mfold_c, tnew_c, mfnew_c, geom_c, fill_physbc_c, &
+       &                              told_f, mfold_f, tnew_f, mfnew_f, geom_f, fill_physbc_f, &
+       time, scomp, dcomp, ncomp)
+    type(amrex_multifab), intent(inout) :: mf
+    type(amrex_multifab), intent(in   ) :: mfold_c, mfnew_c, mfold_f, mfnew_f
+    integer, intent(in) :: scomp, dcomp, ncomp
+    real(amrex_real), intent(in) :: told_c, tnew_c, told_f, tnew_f, time
+    type(amrex_geometry), intent(in) :: geom_c, geom_f
+    procedure(amrex_physbc_proc) :: fill_physbc_c, fill_physbc_f
+! #if 0
+    
+!     teps = 1.e-3_amrex_real * (t_new(lev-1) - t_old(lev-1))
+!     if (abs(time-t_new(lev-1)) .lt. teps) then
+!        allocate(c_mf(1))
+!        allocate(c_time(1))
+!        c_mf(1) = phi_new(lev-1)
+!        c_time(1) = t_new(lev-1)
+!     else if (abs(time-t_old(lev-1)) .lt. teps) then
+!        allocate(c_mf(1))
+!        allocate(c_time(1))
+!        c_mf(1) = phi_old(lev-1)
+!        c_time(1) = t_old(lev-1)
+!     else
+!        allocate(c_mf(2))
+!        allocate(c_time(2))
+!        c_mf(1) = phi_old(lev-1)
+!        c_mf(2) = phi_new(lev-1)
+!        c_time(1) = t_old(lev-1)
+!        c_time(2) = t_new(lev-1)
+!     end if
+!#endif
+  end subroutine amrex_fillpatch_two
+
+#if 0  
   subroutine amrex_fillpatch_single (mf, time, smf, stime, scomp, dcomp, ncomp, geom, pbc)
     type(amrex_multifab), intent(inout) :: mf, smf(:)
     real(amrex_real) :: time, stime(*)
@@ -98,5 +170,6 @@ contains
          cmf_c, ctime, nc, fmf_c, ftime, nf, scomp-1, dcomp-1, ncomp, &
          cgeom%p, fgeom%p, cpbc%p, fpbc%p, rr, interp, lo_bc_c, hi_bc_c)
   end subroutine amrex_fillpatch_two
-
+#endif
+  
 end module amrex_fillpatch_module
