42a5988527c15d9b7e2bdcab1c763977878173bd
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-03-20 08:46:43 -0700

    AMR_Adv_CF_octree: no subcycling

--- Tutorials/AMR_Adv_CF_octree/Source/evolve_mod.F90
+++ Tutorials/AMR_Adv_CF_octree/Source/evolve_mod.F90
@@ -10,9 +10,8 @@ module evolve_module
 contains
 
   subroutine evolve ()
-    use my_amr_module, only : stepno, max_step, stop_time, dt, plot_int
+    use my_amr_module, only : stepno, max_step, stop_time, dtstep, plot_int
     use amr_data_module, only : phi_old, phi_new, t_new
-    use compute_dt_module, only : compute_dt
     use plotfile_module, only : writeplotfile
     real(amrex_real) :: cur_time
     integer :: last_plot_file_step, step, lev, substep, finest_level
@@ -20,7 +19,7 @@ contains
     cur_time = t_new(0)
     last_plot_file_step = 0;
     
-    do step = stepno(0), max_step-1
+    do step = stepno, max_step-1
        if (cur_time .ge. stop_time) exit
 
        if (amrex_parallel_ioprocessor()) then
@@ -28,16 +27,12 @@ contains
           print *, "STEP", step+1, "starts ..."
        end if
 
-       call compute_dt()
-
-       lev = 0
-       substep = 1
-       call timestep(lev, cur_time, substep)
+       call timestep(cur_time)
  
-       cur_time = cur_time + dt(0)
+       cur_time = cur_time + dtstep
 
        if (amrex_parallel_ioprocessor()) then
-          print *, "STEP", step+1, "end. TIME =", cur_time, "DT =", dt(0)
+          print *, "STEP", step+1, "end. TIME =", cur_time, "DT =", dtstep
        end if
 
        ! sync up time to avoid roundoff errors
@@ -51,81 +46,67 @@ contains
           call writeplotfile()
        end if
 
-       if (cur_time .ge. stop_time - 1.e-6_amrex_real*dt(0)) exit
+       if (cur_time .ge. stop_time - 1.e-6_amrex_real*dtstep) exit
     end do
 
-    if (plot_int .gt. 0 .and. stepno(0) .gt. last_plot_file_step) then
+    if (plot_int .gt. 0 .and. stepno .gt. last_plot_file_step) then
        call writeplotfile()
     end if
 
   end subroutine evolve
 
-  recursive subroutine timestep (lev, time, substep)
-    use my_amr_module, only : regrid_int, stepno, nsubsteps, dt, do_reflux
+  subroutine timestep (time)
+    use my_amr_module, only : regrid_int, stepno, dtstep, do_reflux
     use amr_data_module, only : t_old, t_new, phi_old, phi_new, flux_reg
-    use averagedown_module, only : averagedownto
-    integer, intent(in) :: lev, substep
+    use averagedown_module, only : averagedown
+    use compute_dt_module, only : compute_dt
     real(amrex_real), intent(in) :: time
     
-    integer, allocatable, save :: last_regrid_step(:)
-    integer :: k, old_finest_level, finest_level, fine_substep
+    integer, save :: last_regrid_step = 0
+    integer :: lev, finest_level
 
     if (regrid_int .gt. 0) then
-       if (.not.allocated(last_regrid_step)) then
-          allocate(last_regrid_step(0:amrex_max_level))
-          last_regrid_step = 0
-       end if
-
-       ! regrid doesn't change the base level, so we don't regrid on amrex_max_level
-       if (lev .lt. amrex_max_level .and. stepno(lev) .gt. last_regrid_step(lev)) then
-          if (mod(stepno(lev), regrid_int) .eq. 0) then
-
-             old_finest_level = amrex_get_finest_level()
-             call amrex_regrid(lev, time) ! the finest level may change during regrid
-             finest_level = amrex_get_finest_level()
-
-             do k = lev, finest_level
-                last_regrid_step(k) = stepno(k)
-             end do
-
-             do k = old_finest_level+1, finest_level
-                dt(k) = dt(k-1) / amrex_ref_ratio(k-1)             
-             end do
-          end if
+       if (stepno .gt. last_regrid_step .and. mod(stepno, regrid_int) .eq. 0) then
+          call amrex_regrid(0, time)
+          last_regrid_step = stepno
        end if
     end if
 
-    stepno(lev) = stepno(lev)+1
+    finest_level = amrex_get_finest_level()
+    stepno = stepno+1
+
+    call compute_dt()
 
     ! We need to update t_old(lev) and t_new(lev) before advance is called because of fillpath.
-    t_old(lev) = time
-    t_new(lev) = time + dt(lev)
-    ! swap phi_new(lev) and phi_old(lev) so they are consistent with t_new(lev) and t_old(lev)
-    call amrex_multifab_swap(phi_old(lev), phi_new(lev))
+    do lev = 0, finest_level
+       t_old(lev) = time
+       t_new(lev) = time + dtstep
+       ! swap phi_new(lev) and phi_old(lev) so they are consistent with t_new(lev) and t_old(lev)
+       call amrex_multifab_swap(phi_old(lev), phi_new(lev))
+    end do
 
-    call advance(lev, time, dt(lev), stepno(lev), substep, nsubsteps(lev))
 
-    if (lev .lt. amrex_get_finest_level()) then
-       do fine_substep = 1, nsubsteps(lev+1)
-          call timestep(lev+1, time+(fine_substep-1)*dt(lev+1), fine_substep)
-       end do
+    do lev = 0, finest_level
+       call advance(lev, time, dtstep, stepno)
+    end do
 
-       if (do_reflux) then
+    if (do_reflux) then
+       do lev = 0, finest_level-1
           call flux_reg(lev+1)%reflux(phi_new(lev), 1.0_amrex_real)
-       end if
+       end do
+    end if
 
-       call averagedownto(lev)
-    end if    
+    call averagedown()
   end subroutine timestep
 
   ! update phi_new(lev)
-  subroutine advance (lev, time, dt, step, substep, nsub)
+  subroutine advance (lev, time, dt, step)
     use my_amr_module, only : verbose, do_reflux
     use amr_data_module, only : phi_new, flux_reg
     use face_velocity_module, only : get_face_velocity
     use advect_module, only : advect
     use fillpatch_module, only : fillpatch
-    integer, intent(in) :: lev, step, substep, nsub
+    integer, intent(in) :: lev, step
     real(amrex_real), intent(in) :: time, dt
 
     integer, parameter :: ngrow = 3
--- Tutorials/AMR_Adv_CF_octree/Source/plotfile_mod.F90
+++ Tutorials/AMR_Adv_CF_octree/Source/plotfile_mod.F90
@@ -17,17 +17,18 @@ contains
     character(len=127) :: name
     character(len=16)  :: current_step
     type(amrex_string) :: varname(1)
-    
-    if      (stepno(0) .lt. 1000000) then
-       write(current_step,fmt='(i5.5)') stepno(0)
-    else if (stepno(0) .lt. 10000000) then
-       write(current_step,fmt='(i6.6)') stepno(0)
-    else if (stepno(0) .lt. 100000000) then
-       write(current_step,fmt='(i7.7)') stepno(0)
-    else if (stepno(0) .lt. 1000000000) then
-       write(current_step,fmt='(i8.8)') stepno(0)
+    integer, allocatable :: stepno_arr(:)
+
+    if      (stepno .lt. 1000000) then
+       write(current_step,fmt='(i5.5)') stepno
+    else if (stepno .lt. 10000000) then
+       write(current_step,fmt='(i6.6)') stepno
+    else if (stepno .lt. 100000000) then
+       write(current_step,fmt='(i7.7)') stepno
+    else if (stepno .lt. 1000000000) then
+       write(current_step,fmt='(i8.8)') stepno
     else
-       write(current_step,fmt='(i15.15)') stepno(0)
+       write(current_step,fmt='(i15.15)') stepno
     end if
     name = trim(plot_file) // current_step
 
@@ -35,8 +36,11 @@ contains
 
     call amrex_string_build(varname(1), "phi")
 
+    allocate(stepno_arr(0:nlevs-1))
+    stepno_arr = stepno
+
     call amrex_write_plotfile(name, nlevs, phi_new, varname, amrex_geom, &
-         t_new(0), stepno, amrex_ref_ratio)
+         t_new(0), stepno_arr, amrex_ref_ratio)
 
   end subroutine writeplotfile
 
