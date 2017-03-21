1070b28b11139c4f06dc160affb78fe2eee3f6cd
Andrew Myers <atmyers2@gmail.com>
2017-03-21 13:06:00 -0700

    Turn off the debug print in CustomDensityProb.cpp

--- Exec/plasma_acceleration/CustomDensityProb.cpp
+++ Exec/plasma_acceleration/CustomDensityProb.cpp
@@ -8,6 +8,5 @@ using namespace amrex;
 /// This "custom" density profile just does constant
 ///
 Real CustomDensityProfile::getDensity(Real x, Real y, Real z) const {
-  std::cout << "Using custom getDensity" << std::endl;
   return params[0];
 }
