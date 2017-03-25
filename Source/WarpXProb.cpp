1e1606e234279eec53b24614c4bea81cb555324e
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-03-25 11:18:47 -0700

    add GNUmakefile to warpx/

--- Source/WarpXProb.cpp
+++ Source/WarpXProb.cpp
@@ -1,9 +1,3 @@
-
-//
-// Each problem must have its own version of WarpX::InitLevelData(int lev)
-// to initialize the field data on this level.
-//
-
 #include <WarpX.H>
 
 using namespace amrex;
@@ -11,14 +5,9 @@ using namespace amrex;
 void
 WarpX::InitLevelData (int lev)
 {
-    static_assert(false,
-		  "Each problem must have its own version of WarpX::InitLevelData(int lev)");
-
-#if 0
     for (int i = 0; i < 3; ++i) {
 	current[lev][i]->setVal(0.0);
 	Efield[lev][i]->setVal(0.0);
 	Bfield[lev][i]->setVal(0.0);
     }
-#endif
 }
