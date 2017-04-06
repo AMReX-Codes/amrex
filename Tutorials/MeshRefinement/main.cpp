2278ac01e85e9199bac7407243005580cebfb2e3
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-04-05 17:02:51 -0700

    Add an example showing how to generate Amr meshes for testing

---
+++ Tutorials/MeshRefinement/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_VisMF.H>
+#include <AMReX_ParmParse.H>
+
+#include <MyAmr.H>
+
+using namespace amrex;
+
+int main(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+
+    // Add some ParmParse parameters.
+    // This could also be done by putting them in inputs file
+    {
+        ParmParse ppg("geometry");
+        ppg.add("coord_sys", 0);
+        ppg.addarr("prob_lo", std::vector<Real>{0.0, 0.0, 0.0});
+        ppg.addarr("prob_hi", std::vector<Real>{1.0, 1.0, 1.0});
+        // By default, geometry.is_periodic is false
+
+        ParmParse ppa("amr");
+        ppa.add("max_level", 2);
+        ppa.addarr("n_cell", std::vector<int>{64,64,64});
+        // amr.max_grid_size = 32
+        // amr.blocking_factor = 8
+    }
+
+    {
+        MyAmr amr;
+
+        Array<BoxArray> grids(amr.maxLevel()+1);
+        Array<DistributionMapping> dmap(amr.maxLevel()+1);
+
+        grids[0] = amr.MakeBaseGrids();
+        dmap[0] = DistributionMapping{grids[0]};
+
+        // must call these for MakeNewGrids to work
+        amr.SetFinestLevel(0);
+        amr.SetBoxArray(0, grids[0]);
+        amr.SetDistributionMap(0, dmap[0]);
+
+        Real time = 0.0;
+
+        for (int lbase = 0; lbase < amr.maxLevel(); ++lbase)
+        {
+            int new_finest;
+
+            // Add (at most) one level at a time.
+	    amr.MakeNewGrids(lbase, time, new_finest, grids);
+
+            if (new_finest <= amr.finestLevel()) break;
+
+            amr.SetFinestLevel(new_finest);
+            amr.SetBoxArray(new_finest, grids[new_finest]);
+            amr.SetDistributionMap(new_finest, DistributionMapping{grids[new_finest]});
+        }
+        
+        for (int lev = 0; lev <= amr.finestLevel(); ++lev)
+        {
+            MultiFab mf(amr.boxArray(lev), amr.DistributionMap(lev), 1, 0);
+            mf.setVal(0.0);
+            VisMF::Write(mf, "mf"+std::to_string(lev));
+        }
+    }
+
+    amrex::Finalize();
+}
+
