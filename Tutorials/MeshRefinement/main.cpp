af94b1e55c25ca4a6d9b6fd0a282e45099cddc74
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-04-05 17:12:15 -0700

    MeshRefinement test: write a mf with value set to amr level

--- Tutorials/MeshRefinement/main.cpp
+++ Tutorials/MeshRefinement/main.cpp
@@ -4,6 +4,7 @@
 #include <AMReX_MultiFab.H>
 #include <AMReX_VisMF.H>
 #include <AMReX_ParmParse.H>
+#include <AMReX_MultiFabUtil.H>
 
 #include <MyAmr.H>
 
@@ -59,12 +60,19 @@ int main(int argc, char* argv[])
             amr.SetDistributionMap(new_finest, DistributionMapping{grids[new_finest]});
         }
         
-        for (int lev = 0; lev <= amr.finestLevel(); ++lev)
+        MultiFab mf(amr.boxArray(0), amr.DistributionMap(0), 1, 0);
+        mf.setVal(0.0);
+
+        IntVect ref_ratio = IntVect::TheUnitVector();
+        for (int lev = 1; lev <= amr.finestLevel(); ++lev)
         {
-            MultiFab mf(amr.boxArray(lev), amr.DistributionMap(lev), 1, 0);
-            mf.setVal(0.0);
-            VisMF::Write(mf, "mf"+std::to_string(lev));
+            MultiFab fmf(amr.boxArray(lev), amr.DistributionMap(lev), 1, 0);
+            fmf.setVal(static_cast<Real>(lev));
+            ref_ratio *= amr.refRatio(lev-1);
+            amrex::average_down(fmf, mf, 0, 1, ref_ratio);
         }
+
+        VisMF::Write(mf, "mf");
     }
 
     amrex::Finalize();
