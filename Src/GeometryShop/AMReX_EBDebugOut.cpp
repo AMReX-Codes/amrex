3afa9e6b92673ca4ced6516b071941fec221ee04
Dan Graves <dtgraves@lbl.gov>
2017-04-17 10:41:22 -0700

    adding debugging tools so I can figure out what is wrong with AMReX_MeshRefine

---
+++ Src/GeometryShop/AMReX_EBDebugOut.cpp
@@ -0,0 +1,181 @@
+/*
+ *       {_       {__       {__{_______              {__      {__
+ *      {_ __     {_ {__   {___{__    {__             {__   {__  
+ *     {_  {__    {__ {__ { {__{__    {__     {__      {__ {__   
+ *    {__   {__   {__  {__  {__{_ {__       {_   {__     {__     
+ *   {______ {__  {__   {_  {__{__  {__    {_____ {__  {__ {__   
+ *  {__       {__ {__       {__{__    {__  {_         {__   {__  
+ * {__         {__{__       {__{__      {__  {____   {__      {__
+ *
+ */
+
+#include "AMReX_EBDebugOut.H"
+#include "AMReX_Print.H"
+#include "AMReX_BoxArray.H"
+#include <iomanip>
+
+
+namespace amrex
+{
+  void dumpFAB(const FArrayBox* a_fabPtr)
+  {
+    const FArrayBox& fab = *a_fabPtr;
+    BoxIterator bit(fab.box());
+    for (bit.reset(); bit.ok(); ++bit)
+    {
+      amrex::Print() << "\t" << bit() ;
+      for (int ivar = 0; ivar < fab.nComp(); ivar++)
+      {
+        amrex::Print() << "\t" << fab(bit(),ivar);
+      }
+      amrex::Print() << "\n";
+    }
+  }
+
+  void dumpBFR(const BaseFab<Real>* a_fabPtr)
+  {
+    const BaseFab<Real>& fab = *a_fabPtr;
+    BoxIterator bit(fab.box());
+    for (bit.reset(); bit.ok(); ++bit)
+    {
+      amrex::Print() << "\t" << bit() ;
+      for (int ivar = 0; ivar < fab.nComp(); ivar++)
+      {
+        amrex::Print() << "\t" << fab(bit(),ivar);
+      }
+      amrex::Print() << "\n";
+    }
+  }
+  void dumpBFI(const BaseFab<int>* a_fabPtr)
+  {
+    const BaseFab<int>& fab = *a_fabPtr;
+    BoxIterator bit(fab.box());
+    for (bit.reset(); bit.ok(); ++bit)
+    {
+      amrex::Print() << "\t" << bit() ;
+      for (int ivar = 0; ivar < fab.nComp(); ivar++)
+      {
+        amrex::Print() << "\t" << fab(bit(),ivar);
+      }
+      amrex::Print() << "\n";
+    }
+  }
+
+  void dumpBL(const BoxArray* a_dblInPtr)
+  {
+    amrex::Print() << "BoxArray: ";
+    for(int ibox = 0; ibox < a_dblInPtr->size(); ibox++)
+    {
+      amrex::Print() << (*a_dblInPtr)[ibox] << "   ";
+    }
+    amrex::Print() << "\n";
+  }
+
+  void dumpIVS(const IntVectSet* a_ivsPtr)
+  {
+    const IntVectSet& ivs = *a_ivsPtr;
+    IVSIterator it(ivs);
+
+    amrex::Print() << ": IntVects in the IVS are:" << "\n";
+
+    for (it.begin(); it.ok(); ++it)
+    {
+      amrex::Print() << it() << "  ";
+    }
+
+    amrex::Print() << "\n";
+  }
+
+  void dumpBox(const Box* a_boxPtr)
+  {
+    amrex::Print() << "Box:" << *a_boxPtr << "\n";
+  }
+
+
+  void dumpEBFAB(const EBCellFAB* a_fab)
+  {
+    const EBCellFAB& fab = *a_fab;
+    Box box = fab.getRegion();
+    IntVectSet ivs(box);
+    amrex::Print() << "valid and ghost data in ebcellfab" << "\n";
+
+    const EBGraph& ebgraph = a_fab->getEBISBox().getEBGraph();
+    const int ncomp = a_fab->nComp();
+    for (VoFIterator vofit(ivs, ebgraph); vofit.ok(); ++vofit)
+    {
+      const VolIndex& vof = vofit();
+      amrex::Print() << "vof= " << vof.gridIndex() << ", " << vof.cellIndex();
+
+      amrex::Print() << ";      data=";
+      for (int ivar = 0; ivar < ncomp; ivar++)
+      {
+        amrex::Print() << " "
+                       << setprecision(8)
+                       << setiosflags(ios::showpoint)
+                       << setiosflags(ios::scientific)
+                       << (*a_fab)(vof, ivar);
+      }
+      amrex::Print() << "\n";
+    }
+  }
+
+
+  void dumpIVFAB(const BaseIVFAB<Real>* a_vectPtr)
+  {
+ 
+    if(a_vectPtr->numVoFs()==0) 
+    {
+      amrex::Print()<<"empty ";
+      return;
+    }
+    const std::vector<VolIndex>& vofs = a_vectPtr->getVoFs();
+    for(int i=0; i<vofs.size(); i++)
+    {
+      const VolIndex& vof=vofs[i];
+      amrex::Print() << "vof= " << vof.gridIndex() << ", " << vof.cellIndex();
+      amrex::Print() << ";      data=";
+      for (int ivar = 0; ivar < a_vectPtr->nComp(); ivar++)
+      {
+        amrex::Print() << " "
+                       << setprecision(8)
+                       << setiosflags(ios::showpoint)
+                       << setiosflags(ios::scientific)
+                       << a_vectPtr->operator()(vof, ivar);
+      }
+    }
+    amrex::Print() << "\n";
+  }
+  void dumpEBFace(const EBFaceFAB* a_fab)
+  {
+    int idir = a_fab->direction();
+    const EBGraph& ebgraph = a_fab->getEBISBox().getEBGraph();
+    const int ncomp = a_fab->nComp();
+    const EBFaceFAB& fab = *a_fab;
+    Box box = fab.getCellRegion();
+    box &=  fab.getEBISBox().getDomain();
+    IntVectSet ivs(box);
+
+    for (FaceIterator faceit(ivs, ebgraph, idir, FaceStop::SurroundingWithBoundary); faceit.ok(); ++faceit)
+    {
+      const FaceIndex& face = faceit();
+
+      const VolIndex& voflo = face.getVoF(Side::Lo);
+      const VolIndex& vofhi = face.getVoF(Side::Hi);
+      amrex::Print() << "face= (("
+                     << voflo.gridIndex() << ", " << voflo.cellIndex() << "), ("
+                     << vofhi.gridIndex() << ", " << vofhi.cellIndex() << "))" ;
+
+      amrex::Print() << ";      data=";
+      for (int ivar = 0; ivar < ncomp; ivar++)
+      {
+        amrex::Print() << " "
+                       << setprecision(8)
+                       << setiosflags(ios::showpoint)
+                       << setiosflags(ios::scientific)
+                       << (*a_fab)(face, ivar);
+      }
+      amrex::Print() << "\n";
+    }
+  }
+
+}
---
+++ Src/GeometryShop/AMReX_EBFaceFAB.cpp
@@ -0,0 +1,386 @@
+
+/*
+ *       {_       {__       {__{_______              {__      {__
+ *      {_ __     {_ {__   {___{__    {__             {__   {__  
+ *     {_  {__    {__ {__ { {__{__    {__     {__      {__ {__   
+ *    {__   {__   {__  {__  {__{_ {__       {_   {__     {__     
+ *   {______ {__  {__   {_  {__{__  {__    {_____ {__  {__ {__   
+ *  {__       {__ {__       {__{__    {__  {_         {__   {__  
+ * {__         {__{__       {__{__      {__  {____   {__      {__
+ *
+ */
+
+#include "AMReX_EBFaceFAB.H"
+#include "AMReX_BoxIterator.H"
+
+namespace amrex
+{
+  /**********************/
+  /**********************/
+  EBFaceFAB::EBFaceFAB():BaseEBFaceFAB<Real>()
+  {
+  }
+               
+  /**********************/
+  /**********************/
+  EBFaceFAB::EBFaceFAB(const EBISBox& a_ebisBox,
+                       const Box& a_region,
+                       int a_iDir, int a_nComp)
+    :BaseEBFaceFAB<Real>(a_ebisBox, a_region, a_iDir, a_nComp)
+  {
+  }
+  void
+  EBFaceFAB::define(const EBISBox&  a_ebisBox,
+                    const Box& a_region,
+                    int a_iDir, int a_nComp)
+  {
+    BaseEBFaceFAB<Real>::define(a_ebisBox, a_region, a_iDir, a_nComp);
+  }
+               
+  /**********************/
+  /**********************/
+  EBFaceFAB::~EBFaceFAB()
+  {
+  }
+               
+  /**********************/
+  /**********************/
+  const FArrayBox&
+  EBFaceFAB::getFArrayBox() const
+  {
+    BL_ASSERT(isDefined());
+    return (const FArrayBox&)m_regFAB;
+  }
+  /**********************/
+  /**********************/
+  FArrayBox&
+  EBFaceFAB::getFArrayBox()
+  {
+    BL_ASSERT(isDefined());
+    return (FArrayBox&)m_regFAB;
+  }
+               
+  /**********************/
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::operator+=(const EBFaceFAB& a_src)
+  {
+    BL_ASSERT(a_src.m_nComp == m_nComp);
+    plus(a_src, 0, 0, m_nComp);
+               
+    return *this;
+  }
+               
+  EBFaceFAB&
+  EBFaceFAB::plus(const EBFaceFAB& a_src,
+                  int a_srccomp,
+                  int a_dstcomp,
+                  int a_numcomp)
+  {
+    BL_ASSERT(isDefined());
+    BL_ASSERT(a_src.isDefined());
+    //  BL_ASSERT(m_ebisBox == a_src.m_ebisBox);
+    BL_ASSERT(a_srccomp + a_numcomp <= a_src.m_nComp);
+    BL_ASSERT(a_dstcomp + a_numcomp <= m_nComp);
+               
+    Box locRegionFace = a_src.m_regionFace & m_regionFace;
+    Box locRegion = a_src.m_region & m_region;
+    if (!locRegion.isEmpty())
+    {
+      for(BoxIterator bit(locRegion); bit.ok(); ++bit)
+      {
+        for(int icomp = 0; icomp < a_numcomp; icomp++)
+        {
+          int isrc = a_srccomp + icomp;
+          int idst = a_dstcomp + icomp;
+          m_regFAB(bit(), idst) += a_src.m_regFAB(bit(), isrc);
+        }
+      }
+               
+      const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+      for (int iface = 0; iface < faces.size(); iface++)
+      {
+        const FaceIndex& face = faces[iface];
+        if (locRegionFace.contains(face.gridIndex(Side::Hi)))
+        {
+          for (int icomp = 0; icomp < a_numcomp; ++icomp)
+          {
+            m_irrFAB(face, a_dstcomp+icomp) +=
+              a_src.m_irrFAB(face, a_srccomp+icomp);
+          }
+        }
+      }
+    }
+    return *this;
+  }
+               
+  /**********************/
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::operator-=(const EBFaceFAB& a_src)
+  {
+    BL_ASSERT(a_src.m_nComp == m_nComp);
+               
+    minus(a_src, 0, 0, m_nComp);
+               
+    return *this;
+  }
+               
+  EBFaceFAB&
+  EBFaceFAB::minus(const EBFaceFAB& a_src,
+                   int a_srccomp,
+                   int a_dstcomp,
+                   int a_numcomp)
+  {
+    BL_ASSERT(isDefined());
+    BL_ASSERT(a_src.isDefined());
+    // (DFM 7/28/05) This assertion fails for multifluid cases
+    // where this and src have been defined using different
+    // factories; what we really need is a better implementation
+    // of the operator== for EBISBox
+    //BL_ASSERT(m_ebisBox == a_src.m_ebisBox);
+    BL_ASSERT(a_srccomp + a_numcomp <= a_src.m_nComp);
+    BL_ASSERT(a_dstcomp + a_numcomp <= m_nComp);
+               
+    Box locRegionFace = a_src.m_regionFace & m_regionFace;
+    Box locRegion = a_src.m_region & m_region;
+    if (!locRegion.isEmpty())
+    {
+      for(BoxIterator bit(locRegion); bit.ok(); ++bit)
+      {
+        for(int icomp = 0; icomp < a_numcomp; icomp++)
+        {
+          int isrc = a_srccomp + icomp;
+          int idst = a_dstcomp + icomp;
+          m_regFAB(bit(), idst) -= a_src.m_regFAB(bit(), isrc);
+        }
+      }
+               
+      const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+      for (int iface = 0; iface < faces.size(); iface++)
+      {
+        const FaceIndex& face = faces[iface];
+        if (locRegionFace.contains(face.gridIndex(Side::Hi)))
+        {
+          for (int icomp = 0; icomp < a_numcomp; ++icomp)
+          {
+            m_irrFAB(face, a_dstcomp+icomp) -=
+              a_src.m_irrFAB(face, a_srccomp+icomp);
+          }
+        }
+      }
+    }
+    return *this;
+  }
+               
+  /**********************/
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::operator*=(const EBFaceFAB& a_src)
+  {
+    BL_ASSERT(a_src.m_nComp == m_nComp);
+               
+    mult(a_src, 0,0,m_nComp);
+               
+    return *this;
+  }
+               
+               
+               
+  EBFaceFAB&
+  EBFaceFAB::mult(const EBFaceFAB& a_src,
+                  int a_srccomp,
+                  int a_dstcomp,
+                  int a_numcomp)
+  {
+    BL_ASSERT(isDefined());
+    BL_ASSERT(a_src.isDefined());
+    BL_ASSERT(m_ebisBox == a_src.m_ebisBox);
+    BL_ASSERT(a_srccomp + a_numcomp <= a_src.m_nComp);
+    BL_ASSERT(a_dstcomp + a_numcomp <= m_nComp);
+               
+    Box locRegionFace = a_src.m_regionFace & m_regionFace;
+    Box locRegion = a_src.m_region & m_region;
+    if (!locRegion.isEmpty())
+    {
+      for(BoxIterator bit(locRegion); bit.ok(); ++bit)
+      {
+        for(int icomp = 0; icomp < a_numcomp; icomp++)
+        {
+          int isrc = a_srccomp + icomp;
+          int idst = a_dstcomp + icomp;
+          m_regFAB(bit(), idst) *= a_src.m_regFAB(bit(), isrc);
+        }
+      }
+               
+      const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+      for (int iface = 0; iface < faces.size(); iface++)
+      {
+        const FaceIndex& face = faces[iface];
+        if (locRegionFace.contains(face.gridIndex(Side::Hi)))
+        {
+          for (int icomp = 0; icomp < a_numcomp; ++icomp)
+          {
+            m_irrFAB(face, a_dstcomp+icomp) *=
+              a_src.m_irrFAB(face, a_srccomp+icomp);
+          }
+        }
+      }
+    }
+    return *this;
+  }
+               
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::operator/=(const EBFaceFAB& a_src)
+  {
+    BL_ASSERT(a_src.m_nComp == m_nComp);
+               
+    divide(a_src, 0, 0, m_nComp);
+               
+    return *this;
+  }
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::divide(const EBFaceFAB& a_src,
+                    int a_srccomp,
+                    int a_dstcomp,
+                    int a_numcomp)
+  {
+    BL_ASSERT(isDefined());
+    BL_ASSERT(a_src.isDefined());
+    BL_ASSERT(m_ebisBox == a_src.m_ebisBox);
+    BL_ASSERT(a_srccomp + a_numcomp <= a_src.m_nComp);
+    BL_ASSERT(a_dstcomp + a_numcomp <= m_nComp);
+               
+    Box locRegionFace = a_src.m_regionFace & m_regionFace;
+    Box locRegion = a_src.m_region & m_region;
+    if (!locRegion.isEmpty())
+    {
+      for(BoxIterator bit(locRegion); bit.ok(); ++bit)
+      {
+        for(int icomp = 0; icomp < a_numcomp; icomp++)
+        {
+          int isrc = a_srccomp + icomp;
+          int idst = a_dstcomp + icomp;
+          m_regFAB(bit(), idst) /= a_src.m_regFAB(bit(), isrc);
+        }
+      }
+
+      const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+      for (int iface = 0; iface < faces.size(); iface++)
+      {
+        const FaceIndex& face = faces[iface];
+        if (locRegionFace.contains(face.gridIndex(Side::Hi)))
+        {
+          for (int icomp = 0; icomp < a_numcomp; ++icomp)
+          {
+            m_irrFAB(face, a_dstcomp+icomp) /=
+              a_src.m_irrFAB(face, a_srccomp+icomp);
+          }
+        }
+      }
+    }
+    return *this;
+  }
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::operator+=(const Real& a_src)
+  {
+    BL_ASSERT(isDefined());
+    for(BoxIterator bit(m_region); bit.ok(); ++bit)
+    {
+      for(int icomp = 0; icomp < nComp(); icomp++)
+      {
+        m_regFAB(bit(), icomp) += a_src;
+      }
+    }
+               
+    const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+    for (int iface = 0; iface < faces.size(); iface++)
+    {
+      const FaceIndex& face = faces[iface];
+      for (int icomp = 0; icomp < m_nComp; ++icomp)
+      {
+        m_irrFAB(face, icomp) += a_src;
+      }
+    }
+    return *this;
+  }
+               
+  /**********************/
+  EBFaceFAB&
+  EBFaceFAB::operator*=(const Real& a_src)
+  {
+    BL_ASSERT(isDefined());
+               
+    for(BoxIterator bit(m_region); bit.ok(); ++bit)
+    {
+      for(int icomp = 0; icomp < nComp(); icomp++)
+      {
+        m_regFAB(bit(), icomp) *= a_src;
+      }
+    }
+               
+    const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+    for (int iface = 0; iface < faces.size(); iface++)
+    {
+      const FaceIndex& face = faces[iface];
+      for (int icomp = 0; icomp < m_nComp; ++icomp)
+      {
+        m_irrFAB(face, icomp) *= a_src;
+      }
+    }
+    return *this;
+  }
+  /**********************/
+  Real
+  EBFaceFAB::max(int a_comp) const
+  {
+    BL_ASSERT(isDefined());
+    Real val = -1.0e30;
+               
+    for(BoxIterator bit(m_region); bit.ok(); ++bit)
+    {
+      val = std::max(val, m_regFAB(bit(), a_comp));
+    }
+               
+    // Find the max on irregular faces.
+    const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+    for (int iface = 0; iface < faces.size(); iface++)
+    {
+      const FaceIndex& face = faces[iface];
+      for (int icomp = 0; icomp < m_nComp; ++icomp)
+      {
+        val = std::max(val, m_irrFAB(face, icomp));
+      }
+    }
+    return val;
+  }
+  //-----------------------------------------------------------------------
+  Real
+  EBFaceFAB::min(int a_comp) const
+  {
+    BL_ASSERT(isDefined());
+    Real val = 1.0e30;
+               
+    for(BoxIterator bit(m_region); bit.ok(); ++bit)
+    {
+      val = std::min(val, m_regFAB(bit(), a_comp));
+    }
+               
+    // Find the max on irregular faces.
+    const std::vector<FaceIndex>& faces = m_irrFAB.getFaces();
+    for (int iface = 0; iface < faces.size(); iface++)
+    {
+      const FaceIndex& face = faces[iface];
+      for (int icomp = 0; icomp < m_nComp; ++icomp)
+      {
+        val = std::min(val, m_irrFAB(face, icomp));
+      }
+    }
+    return val;
+  }
+  //-----------------------------------------------------------------------
+               
+}
