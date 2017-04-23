7cc436205df8c48b7ec9a959ede113b8f72f4736
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-04-23 12:42:37 -0700

    configure: enforce libamrex and application are consistent on the use of OpenMP

--- Tools/libamrex/mkconfig.py
+++ Tools/libamrex/mkconfig.py
@@ -9,7 +9,7 @@ if sys.version_info < (2, 7):
 
 import argparse
 
-def doit(defines, undefines, comp, allow_diff_comp):
+def doit(defines, undefines, comp, allow_diff_comp, use_omp):
     print("#ifndef AMREX_CONFIG_H_")
     print("#define AMREX_CONFIG_H_")
 
@@ -27,26 +27,37 @@ def doit(defines, undefines, comp, allow_diff_comp):
 
     if comp == "gnu":
         print("#ifndef __GNUC__")
-        print('static_assert(false,"libamrex was compiled with GNU");')
+        print('static_assert(false,"libamrex was built with GNU");')
         print("#endif")
     elif comp == "intel":
         print("#ifndef __INTEL_COMPILER")
-        print('static_assert(false,"libamrex was compiled with Intel");')
+        print('static_assert(false,"libamrex was built with Intel");')
         print("#endif")
     elif comp == "cray":
         print("#ifndef _CRAYC")
-        print('static_assert(false,"libamrex was compiled with Cray");')
+        print('static_assert(false,"libamrex was built with Cray");')
         print("#endif")
     elif comp == "pgi":
         print("#ifndef __PGI")
-        print('static_assert(false,"libamrex was compiled with PGI");')
+        print('static_assert(false,"libamrex was built with PGI");')
         print("#endif")
     elif comp == "llvm":
         print("#ifndef __llvm__")
-        print('static_assert(false,"libamrex was compiled with Clang/LLVM");')
+        print('static_assert(false,"libamrex was built with Clang/LLVM");')
         print("#endif")
     else:
         sys.exit("ERROR: unknown compiler "+comp+" to mkconfig.py")
+
+    if use_omp == "TRUE":
+        print("#ifndef _OPENMP")
+        print('static_assert(false,"libamrex was built with OpenMP");')
+        print("#endif")
+    elif use_omp == "FALSE":
+        print("#ifdef _OPENMP")
+        print('static_assert(false,"libamrex was built without OpenMP");')
+        print("#endif")
+    else:
+        sys.exit("ERROR: unknown use_omp flag "+use_omp+" in mkconfig.py")
 
     print("#endif")
 
@@ -65,11 +76,15 @@ if __name__ == "__main__":
     parser.add_argument("--allow-different-compiler",
                         help="allow an application to use a different compiler than the one used to build libamrex",
                         choices=["TRUE","FALSE"])
+    parser.add_argument("--use-omp",
+                        help="use openmp",
+                        choices=["TRUE","FALSE"])
     args = parser.parse_args()
 
     try:
         doit(defines=args.defines, undefines=args.undefines, comp=args.comp,
-             allow_diff_comp=args.allow_different_compiler)
+             allow_diff_comp=args.allow_different_compiler,
+             use_omp=args.use_omp)
     except:
         # something went wrong
         print("$(error something went wrong in mkconfig.py)")
