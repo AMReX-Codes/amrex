cf0c02bfec6579c1737ae00dcd21ca632b438e34
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-05-02 06:01:14 -0700

    forgot a new file

---
+++ Src/Base/AMReX_filcc_mod.F90
@@ -0,0 +1,81 @@
+
+module amrex_filcc_module
+
+  use amrex_fort_module, only : amrex_real, amrex_spacedim
+
+  implicit none
+
+  interface amrex_filcc
+     module procedure amrex_filcc_1
+     module procedure amrex_filcc_n
+  end interface amrex_filcc
+
+  private
+  public :: amrex_filcc
+
+contains
+
+  subroutine amrex_filcc_n(q,qlo,qhi,domlo,domhi,dx,xlo,bclo,bchi)
+    integer, intent(in) :: qlo(4), qhi(4)
+    integer, dimension(amrex_spacedim), intent(in) :: domlo, domhi
+    real(amrex_real), intent(in) :: dx(amrex_spacedim), xlo(amrex_spacedim)
+    integer, intent(in) :: bclo(amrex_spacedim,*), bchi(amrex_spacedim,*)
+    real(amrex_real), intent(inout) :: q(qlo(1):qhi(1),qlo(2):qhi(2),qlo(3):qhi(3),qlo(4):qhi(4))
+    integer :: i, bc(amrex_spacedim,2)
+    do i = qlo(4), qhi(4)
+       bc(:,1) = bclo(:,i)
+       bc(:,2) = bchi(:,i)
+#if (BL_SPACEDIM == 3)
+       call filcc(q(:,:,:,i),qlo(1),qlo(2),qlo(3),qhi(1),qhi(2),qhi(3),domlo,domhi,dx,xlo,bc)
+#elif (BL_SPACEDIM == 2)
+       call filcc(q(:,:,:,i),qlo(1),qlo(2),       qhi(1),qhi(2),       domlo,domhi,dx,xlo,bc)
+#else
+       call filcc(q(:,:,:,i),qlo(1),              qhi(1),              domlo,domhi,dx,xlo,bc)
+#endif
+    end do
+  end subroutine amrex_filcc_n
+
+#if (BL_SPACEDIM == 3)
+
+  subroutine amrex_filcc_1(q,qlo1,qlo2,qlo3,qhi1,qhi2,qhi3,domlo,domhi,dx,xlo,bc)
+    integer, intent(in) :: qlo1,qlo2,qlo3,qhi1,qhi2,qhi3,domlo(amrex_spacedim),domhi(amrex_spacedim)
+    real(amrex_real), intent(in) :: dx(amrex_spacedim), xlo(amrex_spacedim)
+    integer, intent(in) :: bc(amrex_spacedim, 2)
+    real(amrex_real), intent(inout) :: q(qlo1:qhi1,qlo2:qhi2,qlo3:qhi3)
+    call filcc(q,qlo1,qlo2,qlo3,qhi1,qhi2,qhi3,domlo,domhi,dx,xlo,bc)
+  end subroutine amrex_filcc_1
+
+#elif (BL_SPACEDIM == 2)
+
+  subroutine amrex_filcc_1(q,qlo1,qlo2,qhi1,qhi2,domlo,domhi,dx,xlo,bc)
+    integer, intent(in) :: qlo1,qlo2,qhi1,qhi2,domlo(amrex_spacedim),domhi(amrex_spacedim)
+    real(amrex_real), intent(in) :: dx(amrex_spacedim), xlo(amrex_spacedim)
+    integer, intent(in) :: bc(amrex_spacedim, 2)
+    real(amrex_real), intent(inout) :: q(qlo1:qhi1,qlo2:qhi2)
+    call filcc(q,qlo1,qlo2,qhi1,qhi2,domlo,domhi,dx,xlo,bc)
+  end subroutine amrex_filcc_1
+
+  subroutine amrex_filcc_2(q,qlo1,qlo2,qhi1,qhi2,domlo,domhi,dx,xlo,bclo,bchi)
+    integer, intent(in) :: qlo1,qlo2,qhi1,qhi2,domlo(amrex_spacedim),domhi(amrex_spacedim)
+    real(amrex_real), intent(in) :: dx(amrex_spacedim), xlo(amrex_spacedim)
+    integer, intent(in) :: bclo(amrex_spacedim), bchi(amrex_spacedim)
+    real(amrex_real), intent(inout) :: q(qlo1:qhi1,qlo2:qhi2)
+    integer :: bc(amrex_spacedim,2)
+    bc(:,1) = bclo
+    bc(:,2) = bchi
+    call filcc(q,qlo1,qlo2,qhi1,qhi2,domlo,domhi,dx,xlo,bc)
+  end subroutine amrex_filcc_2
+
+#else
+
+  subroutine amrex_filcc_1(q,qlo1,qhi1,domlo,domhi,dx,xlo,bc)
+    integer, intent(in) :: qlo1,qhi1,domlo(amrex_spacedim),domhi(amrex_spacedim)
+    real(amrex_real), intent(in) :: dx(amrex_spacedim), xlo(amrex_spacedim)
+    integer, intent(in) :: bc(amrex_spacedim, 2)
+    real(amrex_real), intent(inout) :: q(qlo1:qhi1)
+    call filcc(q,qlo1,qhi1,domlo,domhi,dx,xlo,bc)
+  end subroutine amrex_filcc_1
+
+#endif
+
+end module amrex_filcc_module
