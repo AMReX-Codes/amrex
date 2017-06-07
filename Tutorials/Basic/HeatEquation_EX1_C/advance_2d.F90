6ebb6662a8a84ec1a01492edb4ce95948a1ad537
Max Katz <maximilian.katz@stonybrook.edu>
2017-06-07 10:23:24 -0400

    Get update_phi on the device

--- Tutorials/Basic/HeatEquation_EX1_C/advance_2d.F90
+++ Tutorials/Basic/HeatEquation_EX1_C/advance_2d.F90
@@ -39,8 +39,12 @@ contains
   end subroutine compute_flux_doit
 
 
-  subroutine update_phi (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
-       fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt) bind(C, name="update_phi")
+
+#ifdef CUDA
+  attributes(device) &
+#endif
+  subroutine update_phi_doit (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
+       fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
 
     use amrex_fort_module, only : amrex_real
     implicit none
@@ -69,7 +73,7 @@ contains
        end do
     end do
 
-  end subroutine update_phi
+  end subroutine update_phi_doit
 
 end module advance_module
 
@@ -125,11 +129,11 @@ subroutine compute_flux (lo, hi, phi, philo, phihi, &
 
   call threads_and_blocks(lo, hi, numBlocks, numThreads)
 
+  print *, lo, hi, numBlocks, numThreads
+
   call cuda_compute_flux<<<numBlocks, numThreads, 0, stream>>>(lo_d, hi_d, phi, philo_d, phihi_d, &
                                                                fluxx, fxlo_d, fxhi_d, &
                                                                fluxy, fylo_d, fyhi_d, dx_d)
-
-  cuda_result = cudaStreamSynchronize(stream)
 
 #else
 
@@ -169,4 +173,112 @@ subroutine cuda_compute_flux (lo, hi, phi, philo, phihi, &
   call compute_flux_doit(idx, idx, phi, philo, phihi, fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx)
 
 end subroutine cuda_compute_flux
+#endif
+
+
+
+subroutine update_phi (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
+     fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt, idx) bind(C, name="update_phi")
+
+  use amrex_fort_module, only : amrex_real
+  use advance_module, only: update_phi_doit
+#ifdef CUDA
+  use cuda_module, only: cuda_streams, stream_from_index, threads_and_blocks
+  use cudafor, only: cudaMemcpyAsync, cudaMemcpyHostToDevice, cudaStreamSynchronize, &
+                     cuda_stream_kind, dim3
+#endif
+
+  implicit none
+
+  integer lo(2), hi(2), polo(2), pohi(2), pnlo(2), pnhi(2), fxlo(2), fxhi(2), fylo(2), fyhi(2), idx
+  real(amrex_real), intent(in)    :: phiold(polo(1):pohi(1),polo(2):pohi(2))
+  real(amrex_real), intent(inout) :: phinew(pnlo(1):pnhi(1),pnlo(2):pnhi(2))
+  real(amrex_real), intent(in   ) :: fluxx (fxlo(1):fxhi(1),fxlo(2):fxhi(2))
+  real(amrex_real), intent(in   ) :: fluxy (fylo(1):fyhi(1),fylo(2):fyhi(2))
+  real(amrex_real), intent(in)    :: dx(2)
+  real(amrex_real), value         :: dt
+
+#ifdef CUDA
+  attributes(device) :: phi, fluxx, fluxy
+
+  integer :: cuda_result
+  integer(kind=cuda_stream_kind) :: stream
+  type(dim3) :: numThreads, numBlocks
+
+  integer, device :: lo_d(2), hi_d(2)
+  integer, device :: polo_d(2), pohi_d(2)
+  integer, device :: pnlo_d(2), pnhi_d(2)
+  integer, device :: fxlo_d(2), fxhi_d(2)
+  integer, device :: fylo_d(2), fyhi_d(2)
+  real(amrex_real), device :: dx_d(2), dt_d
+
+  stream = cuda_streams(stream_from_index(idx)+1)
+
+  cuda_result = cudaMemcpyAsync(lo_d, lo, 2, cudaMemcpyHostToDevice, stream)
+  cuda_result = cudaMemcpyAsync(hi_d, hi, 2, cudaMemcpyHostToDevice, stream)
+
+  cuda_result = cudaMemcpyAsync(polo_d, polo, 2, cudaMemcpyHostToDevice, stream)
+  cuda_result = cudaMemcpyAsync(pohi_d, pohi, 2, cudaMemcpyHostToDevice, stream)
+
+  cuda_result = cudaMemcpyAsync(pnlo_d, pnlo, 2, cudaMemcpyHostToDevice, stream)
+  cuda_result = cudaMemcpyAsync(pnhi_d, pnhi, 2, cudaMemcpyHostToDevice, stream)
+
+  cuda_result = cudaMemcpyAsync(fxlo_d, fxlo, 2, cudaMemcpyHostToDevice, stream)
+  cuda_result = cudaMemcpyAsync(fxhi_d, fxhi, 2, cudaMemcpyHostToDevice, stream)
+
+  cuda_result = cudaMemcpyAsync(fylo_d, fylo, 2, cudaMemcpyHostToDevice, stream)
+  cuda_result = cudaMemcpyAsync(fyhi_d, fyhi, 2, cudaMemcpyHostToDevice, stream)
+
+  cuda_result = cudaMemcpyAsync(dx_d, dx, 2, cudaMemcpyHostToDevice, stream)
+
+  cuda_result = cudaMemcpyAsync(dt_d, dt, 1, cudaMemcpyHostToDevice, stream)
+
+  call threads_and_blocks(lo, hi, numBlocks, numThreads)
+
+  call cuda_update_phi<<<numBlocks, numThreads, 0, stream>>>(lo_d, hi_d, phiold, polo_d, pohi_d, &
+                                                             phinew, pnlo_d, pnhi_d, fluxx, fxlo_d, fxhi_d, &
+                                                             fluxy, fylo_d, fyhi_d, dx_d, dt_d)
+
+#else
+
+  call update_phi_doit(lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
+                       fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
+
+#endif  
+
+end subroutine update_phi
+
+
+
+#ifdef CUDA
+attributes(global) &
+subroutine cuda_update_phi (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
+     fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
+
+  use amrex_fort_module, only : amrex_real
+  use advance_module, only: update_phi_doit
+
+  implicit none
+
+  integer lo(2), hi(2), polo(2), pohi(2), pnlo(2), pnhi(2), fxlo(2), fxhi(2), fylo(2), fyhi(2)
+  real(amrex_real), intent(in)    :: phiold(polo(1):pohi(1),polo(2):pohi(2))
+  real(amrex_real), intent(inout) :: phinew(pnlo(1):pnhi(1),pnlo(2):pnhi(2))
+  real(amrex_real), intent(in   ) :: fluxx (fxlo(1):fxhi(1),fxlo(2):fxhi(2))
+  real(amrex_real), intent(in   ) :: fluxy (fylo(1):fyhi(1),fylo(2):fyhi(2))
+  real(amrex_real), intent(in)    :: dx(2)
+  real(amrex_real), value         :: dt
+
+  integer :: idx(2)
+
+  ! Get our spatial index based on the CUDA thread index
+
+  idx(1) = lo(1) + (threadIdx%x - 1) + blockDim%x * (blockIdx%x - 1)
+  idx(2) = lo(2) + (threadIdx%y - 1) + blockDim%y * (blockIdx%y - 1)
+
+  if (idx(1) .gt. hi(1) .or. idx(2) .gt. hi(2)) return
+
+  call update_phi_doit(idx, idx, phiold, polo, pohi, phinew, pnlo, pnhi, &
+                       fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
+
+end subroutine cuda_update_phi
 #endif
