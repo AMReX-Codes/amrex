9ea34d41414490e781d7f5f2bf73c6fa26b1a954
Max Katz <maximilian.katz@stonybrook.edu>
2017-06-10 19:56:14 -0400

    Launch kernels directly, avoiding CUDA wrappers

--- Tutorials/Basic/HeatEquation_EX1_C/advance_2d.F90
+++ Tutorials/Basic/HeatEquation_EX1_C/advance_2d.F90
@@ -5,11 +5,11 @@ module advance_module
 contains
 
 #ifdef CUDA
-  attributes(device) &
+  attributes(global) &
 #endif
   subroutine compute_flux_doit (lo, hi, phi, p_lo, p_hi, flx, f_lo, f_hi, dx, idir)
 
-    use amrex_fort_module, only: rt => amrex_real
+    use amrex_fort_module, only: rt => amrex_real, get_loop_bounds
 
     implicit none
 
@@ -21,9 +21,12 @@ contains
 
     ! local variables
     integer :: i, j
+    integer :: blo(3), bhi(3)
 
-    do    j = lo(2), hi(2)
-       do i = lo(1), hi(1)
+    call get_loop_bounds(blo, bhi, [lo(1), lo(2), 0], [hi(1), hi(2), 0])
+
+    do    j = blo(2), bhi(2)
+       do i = blo(1), bhi(1)
           if (idir .eq. 1) then
              ! x-flux
              flx(i,j) = ( phi(i,j) - phi(i-1,j) ) / dx(1)
@@ -39,12 +42,12 @@ contains
 
 
 #ifdef CUDA
-  attributes(device) &
+  attributes(global) &
 #endif
   subroutine update_phi_doit (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
                               fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
 
-    use amrex_fort_module, only: rt => amrex_real
+    use amrex_fort_module, only: rt => amrex_real, get_loop_bounds
 
     implicit none
 
@@ -58,7 +61,10 @@ contains
 
     ! local variables
     integer i,j
+    integer :: blo(2), bhi(2)
     real(rt) :: dtdx(2)
+
+    call get_loop_bounds(blo, bhi, lo, hi)
 
     dtdx = dt/dx
 
@@ -137,7 +143,7 @@ subroutine compute_flux (lo, hi, phi, philo, phihi, &
 
   call threads_and_blocks(blo, bhi, numBlocks, numThreads)
 
-  call cuda_compute_flux<<<numBlocks, numThreads, 0, stream>>>(blo_d, bhi_d, phi, philo_d, phihi_d, &
+  call compute_flux_doit<<<numBlocks, numThreads, 0, stream>>>(blo_d, bhi_d, phi, philo_d, phihi_d, &
                                                                fluxx, fxlo_d, fxhi_d, dx_d, idir_d)
 
   bhi = [hi(1), hi(2)+1]
@@ -149,7 +155,7 @@ subroutine compute_flux (lo, hi, phi, philo, phihi, &
 
   call threads_and_blocks(blo, bhi, numBlocks, numThreads)
 
-  call cuda_compute_flux<<<numBlocks, numThreads, 0, stream>>>(blo_d, bhi_d, phi, philo_d, phihi_d, &
+  call compute_flux_doit<<<numBlocks, numThreads, 0, stream>>>(blo_d, bhi_d, phi, philo_d, phihi_d, &
                                                                fluxy, fylo_d, fyhi_d, dx_d, idir_d)
 
 #else
@@ -170,38 +176,6 @@ subroutine compute_flux (lo, hi, phi, philo, phihi, &
 #endif
 
 end subroutine compute_flux
-
-
-
-#ifdef CUDA
-attributes(global) &
-subroutine cuda_compute_flux (lo, hi, phi, philo, phihi, &
-                              flux, flo, fhi, dx, idir)
-
-  use amrex_fort_module, only: rt => amrex_real
-  use advance_module, only: compute_flux_doit
-
-  implicit none
-
-  integer lo(2), hi(2), philo(2), phihi(2), flo(2), fhi(2)
-  real(rt), intent(in   ) :: phi (philo(1):phihi(1),philo(2):phihi(2))
-  real(rt), intent(inout) :: flux(  flo(1):  fhi(1),  flo(2):  fhi(2))
-  real(rt), intent(in   ) :: dx(2)
-  integer,  intent(in   ) :: idir
-
-  integer :: idx(2)
-
-  ! Get our spatial index based on the CUDA thread index
-
-  idx(1) = lo(1) + (threadIdx%x - 1) + blockDim%x * (blockIdx%x - 1)
-  idx(2) = lo(2) + (threadIdx%y - 1) + blockDim%y * (blockIdx%y - 1)
-
-  if (idx(1) .gt. hi(1) .or. idx(2) .gt. hi(2)) return
-
-  call compute_flux_doit(idx, idx, phi, philo, phihi, flux, flo, fhi, dx, idir)
-
-end subroutine cuda_compute_flux
-#endif
 
 
 
@@ -263,7 +237,7 @@ subroutine update_phi (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
 
   call threads_and_blocks(lo, hi, numBlocks, numThreads)
 
-  call cuda_update_phi<<<numBlocks, numThreads, 0, stream>>>(lo_d, hi_d, phiold, polo_d, pohi_d, &
+  call update_phi_doit<<<numBlocks, numThreads, 0, stream>>>(lo_d, hi_d, phiold, polo_d, pohi_d, &
                                                              phinew, pnlo_d, pnhi_d, fluxx, fxlo_d, fxhi_d, &
                                                              fluxy, fylo_d, fyhi_d, dx_d, dt_d)
 
@@ -275,38 +249,3 @@ subroutine update_phi (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
 #endif  
 
 end subroutine update_phi
-
-
-
-#ifdef CUDA
-attributes(global) &
-subroutine cuda_update_phi (lo, hi, phiold, polo, pohi, phinew, pnlo, pnhi, &
-                            fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
-
-  use amrex_fort_module, only: rt => amrex_real
-  use advance_module, only: update_phi_doit
-
-  implicit none
-
-  integer lo(2), hi(2), polo(2), pohi(2), pnlo(2), pnhi(2), fxlo(2), fxhi(2), fylo(2), fyhi(2)
-  real(rt), intent(in)    :: phiold(polo(1):pohi(1),polo(2):pohi(2))
-  real(rt), intent(inout) :: phinew(pnlo(1):pnhi(1),pnlo(2):pnhi(2))
-  real(rt), intent(in   ) :: fluxx (fxlo(1):fxhi(1),fxlo(2):fxhi(2))
-  real(rt), intent(in   ) :: fluxy (fylo(1):fyhi(1),fylo(2):fyhi(2))
-  real(rt), intent(in   ) :: dx(2)
-  real(rt), intent(in   ) :: dt
-
-  integer :: idx(2)
-
-  ! Get our spatial index based on the CUDA thread index
-
-  idx(1) = lo(1) + (threadIdx%x - 1) + blockDim%x * (blockIdx%x - 1)
-  idx(2) = lo(2) + (threadIdx%y - 1) + blockDim%y * (blockIdx%y - 1)
-
-  if (idx(1) .gt. hi(1) .or. idx(2) .gt. hi(2)) return
-
-  call update_phi_doit(idx, idx, phiold, polo, pohi, phinew, pnlo, pnhi, &
-                       fluxx, fxlo, fxhi, fluxy, fylo, fyhi, dx, dt)
-
-end subroutine cuda_update_phi
-#endif
