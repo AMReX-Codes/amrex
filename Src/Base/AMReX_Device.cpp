38ba15a069592767664c70eb9e5e830d9a396b7a
Max Katz <maxpkatz@gmail.com>
2017-06-18 16:30:49 -0400

    Drive CUDA initialization from C++

--- Src/Base/AMReX_Device.cpp
+++ Src/Base/AMReX_Device.cpp
@@ -6,3 +6,10 @@ bool amrex::Device::in_device_launch_region = false;
 #ifdef CUDA
 int amrex::Device::cuda_device_id = 0;
 #endif
+
+void
+amrex::Device::initialize_cuda() {
+
+    initialize_cuda_f();
+
+}
