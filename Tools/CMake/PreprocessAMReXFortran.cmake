5ada997ad4a2b3fd1cf14ebbb63190e8e1fb0a76
Michele Rosso <mrosso@lbl.gov>
2017-06-22 14:59:15 -0700

    Move some utility functions in AMReX_Utils.cmake

--- Tools/CMake/PreprocessAMReXFortran.cmake
+++
@@ -1,7 +0,0 @@
-function(preprocess_boxlib_fortran OUTVAR)
-
-   set_source_files_properties(${ARGN} PROPERTIES COMPILE_DEFINITIONS "BL_LANG_FORT")
-   set_source_files_properties(${ARGN} PROPERTIES COMPILE_FLAGS "-ffixed-line-length-none")
-   set(${OUTVAR} ${ARGN} PARENT_SCOPE)
-
-endfunction(preprocess_boxlib_fortran)
