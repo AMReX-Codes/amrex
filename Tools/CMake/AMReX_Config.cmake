d2d65e732306a068bf801d5f2de5cde1c22f0e1a
Michele Rosso <mrosso@lbl.gov>
2017-06-22 14:59:15 -0700

    Move some utility functions in AMReX_Utils.cmake

--- Tools/CMake/AMReX_Config.cmake
+++ Tools/CMake/AMReX_Config.cmake
@@ -14,28 +14,6 @@ endif ()
 
 
 #
-# This is a macro for accumulating defines into AMREX_DEFINES
-#
-macro ( add_define NEW_DEFINE )
-   if ( ${ARGC} EQUAL 1 )
-      set ( AMREX_DEFINES  ${AMREX_DEFINES} " -D${NEW_DEFINE}" )
-   else ()
-      if ( ${ARGV1} ) 
-	 set ( AMREX_DEFINES  ${AMREX_DEFINES} " -D${NEW_DEFINE}" )	 
-      endif ()
-   endif ()
-endmacro ()
-
-#
-# This is a function for accumulating compiler flags
-#
-function ( add_compiler_flags NEW_FLAGS ALL_FLAGS )
-   if ( ${NEW_FLAGS} )
-      set ( ${ALL_FLAGS}  "${${ALL_FLAGS}} ${${NEW_FLAGS}}" PARENT_SCOPE )
-   endif ()
-endfunction ()
-
-#
 # Decide whether or not to use PIC 
 #
 if (ENABLE_PIC)
@@ -63,17 +41,17 @@ if ( ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "" )  AND
       ( FortranCInterface_GLOBAL_CASE STREQUAL "UPPER") )
    message(STATUS "Fortran name mangling scheme to UPPERCASE \
 (upper case, no append underscore)")
-   add_define ("BL_FORT_USE_UPPERCASE" )
+   add_define ( BL_FORT_USE_UPPERCASE AMREX_DEFINES )
 elseif ( ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "") AND
       ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER") )
    message(STATUS "Fortran name mangling scheme to LOWERCASE \
 (lower case, no append underscore)")
-   add_define ("BL_FORT_USE_LOWERCASE" )
+   add_define ( BL_FORT_USE_LOWERCASE AMREX_DEFINES )
 elseif ( ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "_" ) AND
       ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER") )
    message(STATUS "Fortran name mangling scheme to UNDERSCORE \
 (lower case, append underscore)")
-   add_define ("BL_FORT_USE_UNDERSCORE" )
+   add_define ( BL_FORT_USE_UNDERSCORE AMREX_DEFINES )
 else ()
    message(AUTHOR_WARNING "Fortran to C mangling not backward\
  compatible with older style BoxLib code") 
@@ -88,44 +66,39 @@ endif ()
 # This defines were always on in older version
 # of AMReX/CMake. Need more details on these???
 #
-add_define (BL_NOLINEVALUES)
-add_define (BL_PARALLEL_IO)
-add_define (BL_SPACEDIM=${BL_SPACEDIM})
-add_define (BL_${CMAKE_SYSTEM_NAME})
+add_define (BL_NOLINEVALUES AMREX_DEFINES)
+add_define (BL_PARALLEL_IO AMREX_DEFINES)
+add_define (BL_SPACEDIM=${BL_SPACEDIM} AMREX_DEFINES)
+add_define (BL_${CMAKE_SYSTEM_NAME} AMREX_DEFINES)
 
 
 if ( ENABLE_DP )
-   add_define (BL_USE_DOUBLE)
+   add_define (BL_USE_DOUBLE AMREX_DEFINES)
 else ()
-   add_define (BL_USE_FLOAT)
+   add_define (BL_USE_FLOAT AMREX_DEFINES)
 endif ()
 
-
-add_define (USE_PARTICLES ENABLE_PARTICLES)
-add_define (BL_PROFILING ENABLE_PROFILING) 
-add_define (BL_COMM_PROFILING ENABLE_COMM_PROFILING)
+add_define (USE_PARTICLES AMREX_DEFINES ENABLE_PARTICLES)
+add_define (BL_PROFILING AMREX_DEFINES ENABLE_PROFILING) 
+add_define (BL_COMM_PROFILING AMREX_DEFINES ENABLE_COMM_PROFILING)
 
 if ( ENABLE_PARTICLES AND ( NOT ENABLE_DP_PARTICLES ) ) 
-   add_define ( BL_SINGLE_PRECISION_PARTICLES )
+   add_define ( BL_SINGLE_PRECISION_PARTICLES AMREX_DEFINES )
 endif ()
 
 if (ENABLE_FORTRAN_MPI AND ENABLE_MPI)
-   add_define ( BL_USE_FORTRAN_MPI=1 )
+   add_define ( BL_USE_FORTRAN_MPI=1 AMREX_DEFINES )
 endif ()
 
-add_define (MG_USE_FBOXIB=1 ENABLE_MG_FBOXLIB)
-add_define (BL_USE_F_BASELIB=1  ENABLE_FBASELIB)
-add_define (BL_USE_MPI ENABLE_MPI)
-add_define (BL_USE_OMP ENABLE_OMP)
-
-
+add_define (MG_USE_FBOXIB=1 AMREX_DEFINES ENABLE_MG_FBOXLIB)
+add_define (BL_USE_F_BASELIB=1  AMREX_DEFINES ENABLE_FBASELIB)
+add_define ( BL_USE_MPI AMREX_DEFINES ENABLE_MPI)
+add_define (BL_USE_OMP AMREX_DEFINES ENABLE_OMP)
 
 #
 # Add all preprocessor definitions to compile string
 # 
 add_definitions ( ${AMREX_DEFINES} )
-
-
 
 # ------------------------------------------------------------- #
 #    Setup third party packages 
@@ -138,9 +111,9 @@ if (ENABLE_MPI)
    list (APPEND AMREX_EXTRA_C_INCLUDE_PATH "${MPI_C_INCLUDE_PATH}")
    list (APPEND AMREX_EXTRA_CXX_INCLUDE_PATH "${MPI_CXX_INCLUDE_PATH}")
    # Compile flags
-   list (APPEND AMREX_EXTRA_Fortran_FLAGS "${MPI_Fortran_COMPILE_FLAGS}")
-   list (APPEND AMREX_EXTRA_C_FLAGS "${MPI_C_COMPILE_FLAGS}")
-   list (APPEND AMREX_EXTRA_CXX_FLAGS "${MPI_CXX_COMPILE_FLAGS}")
+   append ( MPI_Fortran_COMPILE_FLAGS AMREX_EXTRA_Fortran_FLAGS ) 
+   append ( MPI_C_COMPILE_FLAGS AMREX_EXTRA_C_FLAGS )
+   append ( MPI_CXX_COMPILE_FLAGS AMREX_EXTRA_CXX_FLAGS )
    # Libraries
    list (APPEND AMREX_EXTRA_Fortran_LIBRARIES "${MPI_Fortran_LIBRARIES}")
    list (APPEND AMREX_EXTRA_C_LIBRARIES "${MPI_C_LIBRARIES}")
@@ -154,9 +127,9 @@ endif ()
 if (ENABLE_OMP)
    find_package (OpenMP REQUIRED)
    # Compile flags
-   list (APPEND AMREX_EXTRA_Fortran_FLAGS "${OpenMP_Fortran_FLAGS}")
-   list (APPEND AMREX_EXTRA_C_FLAGS "${OpenMP_C_FLAGS}")
-   list (APPEND AMREX_EXTRA_CXX_FLAGS "${OpenMP_CXX_FLAGS}")
+   append ( OpenMP_Fortran_FLAGS AMREX_EXTRA_Fortran_FLAGS ) 
+   append ( OpenMP_C_FLAGS AMREX_EXTRA_C_FLAGS )
+   append ( OpenMP_CXX_FLAGS AMREX_EXTRA_CXX_FLAGS )
 endif()
 
 
@@ -166,28 +139,29 @@ endif()
 if ( AMREX_FFLAGS_OVERRIDE )
    set ( AMREX_Fortran_FLAGS ${AMREX_FFLAGS_OVERRIDE} )
 else ()
-   add_compiler_flags ( AMREX_${FC_ID}_FFLAGS_${AMREX_BUILD_TYPE}
+   append ( AMREX_${FC_ID}_FFLAGS_${AMREX_BUILD_TYPE}
       AMREX_Fortran_FLAGS )
 endif ()
 
 if ( AMREX_CXXLAGS_OVERRIDE )
    set ( AMREX_CXX_FLAGS ${AMREX_CXXFLAGS_OVERRIDE} )
 else ()
-   add_compiler_flags ( AMREX_${CXX_ID}_CXXFLAGS_${AMREX_BUILD_TYPE}
+   append ( AMREX_${CXX_ID}_CXXFLAGS_${AMREX_BUILD_TYPE}
       AMREX_CXX_FLAGS )
 endif ()
 
-# Add extra flags
-add_compiler_flags ( AMREX_EXTRA_Fortran_FLAGS AMREX_Fortran_FLAGS )
-add_compiler_flags ( AMREX_EXTRA_CXX_FLAGS AMREX_CXX_FLAGS )
+append ( AMREX_EXTRA_Fortran_FLAGS AMREX_Fortran_FLAGS )
+append ( AMREX_EXTRA_CXX_FLAGS AMREX_CXX_FLAGS )
 
 # Add required flags
-add_compiler_flags ( AMREX_${FC_ID}_FFLAGS_REQUIRED AMREX_Fortran_FLAGS )
-add_compiler_flags ( AMREX_${CXX_ID}_CXXLAGS_REQUIRED AMREX_CXX_FLAGS )
+append ( AMREX_${FC_ID}_FFLAGS_REQUIRED AMREX_Fortran_FLAGS )
+append ( AMREX_${CXX_ID}_CXXLAGS_REQUIRED AMREX_CXX_FLAGS )
 
 # Set CMake compiler flags
 set ( CMAKE_Fortran_FLAGS_${AMREX_BUILD_TYPE} "${AMREX_Fortran_FLAGS}" ) 
 set ( CMAKE_CXX_FLAGS_${AMREX_BUILD_TYPE} "${AMREX_CXX_FLAGS}" )
+
+
 #
 # Config summary
 #
--- Tools/CMake/AMReX_Utils.cmake
+++ Tools/CMake/AMReX_Utils.cmake
@@ -10,33 +10,48 @@ function ( check_path  path  message_type )
 endfunction ()
 
 
-# ###################################################################
-# # Set default options for reasonable beahvior
-# ##################################################################
-# macro ( set_default_options )
+#
+# This function turns a list into a string
+# 
+function ( list_to_string list )
+   string (REPLACE ";" " " tmp "${${list}}")
+   set ( ${list} "${tmp}" PARENT_SCOPE)
+endfunction ()
 
-#    # Default to debug is no other build type specified
-#    if ( NOT CMAKE_BUILD_TYPE )
-#       set ( CMAKE_BUILD_TYPE "Debug" CACHE STRING
-# 	 "Choose the type of build, options are: Debug Release RelWithDebInfo MinSizeRel."
-# 	 FORCE )
-#    endif ()
+
+#
+# Append new_var to all_var
+# 
+function ( append new_var all_var )
+   if ( ${new_var} )
+      set ( tmp  "${${all_var}} ${${new_var}}" )
+
+      # Since I am OCD, remove the double spaces.
+      string ( REPLACE "  " " " tmp ${tmp} )
+      set ( ${all_var}  ${tmp} PARENT_SCOPE )
+   endif ()
+endfunction ()
+
+#
+# Function to accumulate preprocessor directives
+#
+function ( add_define new_define all_defines )
    
-#    # Compile options:
-#    option (ENABLE_FORTRAN_MPI "Enable Fortran MPI Communicator" ON)
-#    option (ENABLE_FMG "Enable Fortran for MultiGrid Solver" OFF)
-#    option (ENABLE_FBASELIB "Enable Fortran BaseLib" OFF)
-#    option (ENABLE_CXX11 "Enable C++11" ON)
-#    option (ENABLE_POSITION_INDEPENDENT_CODE "Compile with position-independent code enabled" OFF)
+   set ( condition  1 )
+   
+   if ( ${ARGC} EQUAL 3 ) #
+      set ( condition ${${ARGV2}} )
+   elseif ( ${ARGC} GREATER 3 )
+      message ( AUTHOR_WARNING "Function add_define accept AT MOST 3 args" )
+   endif ()
+   
+   if ( ${condition} )
+      set ( ${all_defines} "${${all_defines}} -D${new_define}" PARENT_SCOPE )
+   endif ()
+   
+endfunction ()
 
-#    set (BL_SPACEDIM 3 CACHE INT "Dimension of AMReX build")
-#    set (ENABLE_MPI 1 CACHE INT "Enable build with MPI")
-#    set (ENABLE_OpenMP 0 CACHE INT "Enable build with OpenMP")
-#    set (BL_PRECISION "DOUBLE" CACHE INT "Precision of AMReX build")
-#    set (BL_USE_PARTICLES 0 CACHE INT "Include Particles classes in AMReX build")
-#    set (ENABLE_PROFILING 0 CACHE INT "Include profiling information in AMReX build")
-#    set (ENABLE_BACKTRACE 1 CACHE INT "Include backtrace information in AMReX build")
-  
-
-
-# endmacro ()
+function(preprocess_boxlib_fortran OUTVAR)
+   set_source_files_properties(${ARGN} PROPERTIES COMPILE_DEFINITIONS "BL_LANG_FORT")
+   set(${OUTVAR} ${ARGN} PARENT_SCOPE)
+endfunction(preprocess_boxlib_fortran)
