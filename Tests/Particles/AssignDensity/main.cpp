d754f3d7aa7de087861a65fd939cc2a7b0f63dee
Andrew Myers <atmyers2@gmail.com>
2017-06-29 22:58:49 -0700

    don't need my own plot file routine for this test.

--- Tests/Particles/AssignDensity/main.cpp
+++ Tests/Particles/AssignDensity/main.cpp
@@ -4,112 +4,9 @@
 #include <AMReX_MultiFab.H>
 #include <AMReX_MultiFabUtil.H>
 #include "AMReX_Particles.H"
+#include "AMReX_PlotFileUtil.H"
 
 using namespace amrex;
-
-void
-writePlotFile (const std::string& dir,
-               const MultiFab&    mf,
-               const Geometry&    geom,
-	       const Real&        time)
-{
-
-  // Only let 64 CPUs be writing at any one time.
-  VisMF::SetNOutFiles(64);
-  
-  // Only the I/O processor makes the directory if it doesn't already exist.
-  if (ParallelDescriptor::IOProcessor())
-    if (!amrex::UtilCreateDirectory(dir, 0755))
-      amrex::CreateDirectoryFailed(dir);
-
-  // Force other processors to wait till directory is built.
-  ParallelDescriptor::Barrier();
-
-  std::string HeaderFileName = dir + "/Header";
-  
-  VisMF::IO_Buffer io_buffer(VisMF::IO_Buffer_Size);
-  
-  std::ofstream HeaderFile;
-  
-  HeaderFile.rdbuf()->pubsetbuf(io_buffer.dataPtr(), io_buffer.size());
-  
-  if (ParallelDescriptor::IOProcessor()) {
-
-    // Only the IOProcessor() writes to the header file.
-    HeaderFile.open(HeaderFileName.c_str(), std::ios::out|std::ios::trunc|std::ios::binary);
-    if (!HeaderFile.good())
-      amrex::FileOpenFailed(HeaderFileName);
-    HeaderFile << "NavierStokes-V1.1\n";
-    
-    HeaderFile << mf.nComp() << '\n';
-    
-    // variable names
-    HeaderFile << "density\n";
-	
-    // dimensionality
-    HeaderFile << BL_SPACEDIM << '\n';
-    // time
-    HeaderFile << time << '\n';
-    // maximum level number (0=single level)
-    HeaderFile << 0 << '\n';
-    for (int i = 0; i < BL_SPACEDIM; i++)
-      HeaderFile << geom.ProbLo(i) << ' ';
-    HeaderFile << '\n';
-    for (int i = 0; i < BL_SPACEDIM; i++)
-      HeaderFile << geom.ProbHi(i) << ' ';
-    HeaderFile << '\n';
-    HeaderFile << '\n';
-    HeaderFile << geom.Domain() << ' ';
-    HeaderFile << '\n';
-    HeaderFile << 0 << ' ';
-    HeaderFile << '\n';
-    for (int k = 0; k < BL_SPACEDIM; k++)
-      HeaderFile << geom.CellSize()[k] << ' ';
-    HeaderFile << '\n';
-    HeaderFile << geom.Coord() << '\n';
-    HeaderFile << "0\n";
-  }
-  // Build the directory to hold the MultiFab at this level.
-  // The name is relative to the directory containing the Header file.
-  static const std::string BaseName = "/Cell";
-  
-  std::string Level = amrex::Concatenate("Level_", 0, 1);
-
-  // Now for the full pathname of that directory.
-  std::string FullPath = dir;
-  if (!FullPath.empty() && FullPath[FullPath.length()-1] != '/')
-    FullPath += '/';
-  FullPath += Level;
-
-  // Only the I/O processor makes the directory if it doesn't already exist.
-  if (ParallelDescriptor::IOProcessor())
-    if (!amrex::UtilCreateDirectory(FullPath, 0755))
-      amrex::CreateDirectoryFailed(FullPath);
-
-  // Force other processors to wait till directory is built.
-  ParallelDescriptor::Barrier();
-  
-  if (ParallelDescriptor::IOProcessor()) {
-    HeaderFile << 0 << ' ' << mf.boxArray().size() << ' ' << 0 << '\n';
-    HeaderFile << 0 << '\n';
-    
-    for (int i = 0; i < mf.boxArray().size(); ++i) {
-      RealBox loc = RealBox(mf.boxArray()[i],geom.CellSize(),geom.ProbLo());
-      for (int n = 0; n < BL_SPACEDIM; n++)
-	HeaderFile << loc.lo(n) << ' ' << loc.hi(n) << '\n';
-    }
-    
-    std::string PathNameInHeader = Level;
-    PathNameInHeader += BaseName;
-    HeaderFile << PathNameInHeader << '\n';
-  }
-  
-  // Use the Full pathname when naming the MultiFab.
-  std::string TheFullPath = FullPath;
-  TheFullPath += BaseName;
-  
-  VisMF::Write(mf, TheFullPath);
-}
 
 struct TestParams {
   int nx;
@@ -156,10 +53,10 @@ void test_assign_density(TestParams& parms)
   MultiFab density(ba, dmap, 1, 0);
   density.setVal(0.0);
 
-  MultiFab partMF(ba, dmap, 1, 1);
+  MultiFab partMF(ba, dmap, 1 + BL_SPACEDIM, 1);
   partMF.setVal(0.0);
 
-  typedef ParticleContainer<1> MyParticleContainer;
+  typedef ParticleContainer<1 + BL_SPACEDIM> MyParticleContainer;
   MyParticleContainer myPC(geom, dmap, ba);
   myPC.SetVerbose(false);
 
@@ -171,15 +68,20 @@ void test_assign_density(TestParams& parms)
   int iseed = 451;
   Real mass = 10.0;
 
-  MyParticleContainer::ParticleInitData pdata = {mass};
+  MyParticleContainer::ParticleInitData pdata = {mass, 1.0, 2.0, 3.0};
   myPC.InitRandom(num_particles, iseed, pdata, serialize);
-  myPC.AssignCellDensitySingleLevelFort(0, partMF, 0, 1, 0);
+  myPC.AssignCellDensitySingleLevelFort(0, partMF, 0, 4, 0);
   
-  myPC.InterpolateSingleLevelFort(acceleration, 0);
+  //myPC.AssignDensitySingleLevel(0, partMF, 0, 4, 0);
+
+  //  myPC.InterpolateSingleLevelFort(acceleration, 0);
 
   MultiFab::Copy(density, partMF, 0, 0, 1, 0);
 
-  writePlotFile("plt00000", density, geom, 0.0);
+  WriteSingleLevelPlotfile("plt00000", partMF, 
+                           {"density", "vx", "vy", "vz"},
+                           geom, 0.0, 0);
+
   myPC.Checkpoint("plt00000", "particle0", true);
 }
 
