74085cb5336c5d01b86bf3106f41cc5fad5bef5e
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-07-10 16:01:00 -0700

    more hypre

--- Src/Extern/HYPRE/AMReX_HypreABecLap.cpp
+++ Src/Extern/HYPRE/AMReX_HypreABecLap.cpp
@@ -176,6 +176,68 @@ void HypreABecLap::setVerbose(int _verbose)
 }
 
 void HypreABecLap::solve(MultiFab& soln, const MultiFab& rhs, Real rel_tol, Real abs_tol,
+			 int max_iter, LinOpBCType bc_type, Real bc_value, const Geometry& geom)
+{
+    BndryData bd(soln.boxArray(), soln.DistributionMap(), 1, geom);
+    {
+        const int comp = 0;
+        for (int n=0; n<BL_SPACEDIM; ++n) {
+            for (MFIter mfi(rhs); mfi.isValid(); ++mfi ) {
+                int i = mfi.index(); 
+      
+                const Box& bx = mfi.validbox();
+                
+                // Our default will be that the face of this grid is either touching another grid
+                //  across an interior boundary or a periodic boundary.  We will test for the other
+                //  cases below.
+                {
+                    // Define the type of boundary conditions to be Dirichlet (even for periodic)
+                    bd.setBoundCond(Orientation(n, Orientation::low) ,i,comp,LO_DIRICHLET);
+                    bd.setBoundCond(Orientation(n, Orientation::high),i,comp,LO_DIRICHLET);
+	
+                    // Set the boundary conditions to the cell centers outside the domain
+                    bd.setBoundLoc(Orientation(n, Orientation::low) ,i,0.5*dx[n]);
+                    bd.setBoundLoc(Orientation(n, Orientation::high),i,0.5*dx[n]);
+                }
+
+                // Now test to see if we should override the above with Dirichlet or Neumann physical bc's
+                if (bc_type != LinOpBCType::interior)
+                { 
+                    int ibnd = static_cast<int>(bc_type); // either LO_DIRICHLET or LO_NEUMANN
+                    const Geometry& geom = bd.getGeom();
+
+                    // We are on the low side of the domain in coordinate direction n
+                    if (bx.smallEnd(n) == geom.Domain().smallEnd(n)) {
+                        // Set the boundary conditions to live exactly on the faces of the domain
+                        bd.setBoundLoc(Orientation(n, Orientation::low) ,i,0.0 );
+                        
+                        // Set the Dirichlet/Neumann boundary values 
+                        bd.setValue(Orientation(n, Orientation::low) ,i, bc_value);
+                        
+                        // Define the type of boundary conditions 
+                        bd.setBoundCond(Orientation(n, Orientation::low) ,i,comp,ibnd);
+                    }
+	
+                    // We are on the high side of the domain in coordinate direction n
+                    if (bx.bigEnd(n) == geom.Domain().bigEnd(n)) {
+                        // Set the boundary conditions to live exactly on the faces of the domain
+                        bd.setBoundLoc(Orientation(n, Orientation::high) ,i,0.0 );
+                        
+                        // Set the Dirichlet/Neumann boundary values
+                        bd.setValue(Orientation(n, Orientation::high) ,i, bc_value);
+                        
+                        // Define the type of boundary conditions 
+                        bd.setBoundCond(Orientation(n, Orientation::high) ,i,comp,ibnd);
+                    }
+                }
+            }
+        }
+    }
+
+    solve(soln,rhs,rel_tol,abs_tol,max_iter,bd);
+}
+
+void HypreABecLap::solve(MultiFab& soln, const MultiFab& rhs, Real rel_tol, Real abs_tol,
 			 int max_iter, const BndryData& bndry)
 {
   reltol = rel_tol;
