abff22ecb303c68c9fef81e679775381e4a1c65a
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-07-25 06:46:28 -0700

    forgot to check in new files

---
+++ Src/F_Interfaces/Base/AMReX_init_fi.cpp
@@ -0,0 +1,40 @@
+#include <AMReX.H>
+#include <AMReX_Array.H>
+
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <cstdlib>
+#include <cstring>
+
+extern "C"
+{
+    void amrex_fi_init (char* cmd)
+    {
+        std::istringstream is(cmd);
+        amrex::Array<std::string> argv_string
+            {std::istream_iterator<std::string>{is},
+             std::istream_iterator<std::string>{}};
+
+        int argc = argv_string.size();
+        char** argv = (char**)malloc(argc*sizeof(char*));
+        for (int i = 0; i < argc; ++i)
+        {
+            argv[i] = (char*)malloc(argv_string[i].size()+1);
+            strcpy(argv[i], argv_string[i].c_str());
+        }
+
+        amrex::Initialize(argc, argv);
+
+        for (int i = 0; i < argc; ++i)
+        {
+            free(argv[i]);
+        }
+        free(argv);
+    }
+
+    void amrex_fi_finalize ()
+    {
+        amrex::Finalize();
+    }
+}
