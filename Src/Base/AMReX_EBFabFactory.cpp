fb8fae806f1e9a56114517da2fbf9ecbf6d5c50a
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-07-28 16:35:28 -0700

    FArrayBoxFactory

--- Src/Base/AMReX_EBFabFactory.cpp
+++ Src/Base/AMReX_EBFabFactory.cpp
@@ -1,14 +1,18 @@
 
 #include <AMReX_FabFactory.H>
 #include <AMReX_FArrayBox.H>
+#include <AMReX_EBISLayout.H>
 
 namespace amrex
 {
 
-template <>
+FArrayBoxFactory::FArrayBoxFactory (const EBISLayout& a_ebisl)
+    : m_ebisl(a_ebisl)
+{}
+
 FArrayBox*
-DefaultFabFactory<FArrayBox>::create (const Box& box, int ncomps,
-                                      const FabInfo& info, int box_index) const
+FArrayBoxFactory::create (const Box& box, int ncomps,
+                          const FabInfo& info, int box_index) const
 {
     return new FArrayBox(box, ncomps, info.alloc, info.shared);
 }
