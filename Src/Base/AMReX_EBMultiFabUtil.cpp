bba2ad1b0f1d518dbbe129b8cd506c209ffb98af
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-07-31 16:36:45 -0700

    add a eb utility function to set all covered to the min val for the temporary purpose of visualization

---
+++ Src/Base/AMReX_EBMultiFabUtil.cpp
@@ -0,0 +1,42 @@
+
+#include <AMReX_EBMultiFabUtil.H>
+
+namespace amrex
+{
+
+void
+EB_set_covered (MultiFab& mf)
+{
+    EB_set_covered(mf, 0, mf.nComp());
+}
+
+void
+EB_set_covered (MultiFab& mf, int icomp, int ncomp)
+{
+    Array<Real> minvals(ncomp);
+    for (int i = icomp; i < icomp+ncomp; ++i) {
+        minvals[i] = mf.min(i,0,true);
+    }
+    ParallelDescriptor::ReduceRealMin(minvals.data(), ncomp);
+
+    const auto& factory = dynamic_cast<EBFArrayBoxFactory const&>(mf.Factory());
+    const auto& layout = factory.Layout();
+
+    for (MFIter mfi(mf); mfi.isValid(); ++mfi)
+    {
+        FArrayBox& fab = mf[mfi];
+        const EBISBox& ebis = layout[mfi];
+        for (BoxIterator bi(mfi.validbox()); bi.ok(); ++bi)
+        {
+            const IntVect& iv = bi();
+            if (ebis.isCovered(iv))
+            {
+                for (int i = icomp; i < icomp+ncomp; ++i) {
+                    fab(iv, i) = minvals[i-icomp];
+                }
+            }
+        }
+    }
+}
+
+}
