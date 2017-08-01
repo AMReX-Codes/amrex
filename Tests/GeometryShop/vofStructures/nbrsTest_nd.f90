d9500ba4f52eb6753db36b341a58b407d0385fd0
Marc Day <MSDay@lbl.gov>
2017-08-01 11:25:43 -0700

    Pass the stencil box to further clean fort code

--- Tests/GeometryShop/vofStructures/nbrsTest_nd.f90
+++ Tests/GeometryShop/vofStructures/nbrsTest_nd.f90
@@ -7,36 +7,21 @@ module nbrsTest_nd_module
 
 contains
 
-  subroutine fill_redist_stencil(lo, hi, sten, Nsten, mask, mask_lo, mask_hi, &
+  subroutine fill_redist_stencil(lo, hi, slo, shi, sten, Nsten, mask, mask_lo, mask_hi, &
        vf, vf_lo, vf_hi) bind(C,name="fill_redist_stencil")
 
-    integer,          intent(in) :: lo(0:2), hi(0:2)
+    integer,          intent(in) ::  lo(0:2),  hi(0:2)
+    integer,          intent(in) :: slo(0:2), shi(0:2)
     integer,          intent(in) :: Nsten
     type(nbr_sten),intent(inout) :: sten(0:Nsten-1)
     integer,          intent(in) :: mask_lo(3), mask_hi(3)
     integer,          intent(in) :: vf_lo(3), vf_hi(3)
     integer,          intent(in) :: mask(mask_lo(1):mask_hi(1),mask_lo(2):mask_hi(2),mask_lo(3):mask_hi(3))
     real(amrex_real), intent(in) :: vf(vf_lo(1):vf_hi(1),vf_lo(2):vf_hi(2),vf_lo(3):vf_hi(3))
-    integer :: i,j,k,n,ii,jj,kk,jjlo,jjhi,kklo,kkhi,iii,jjj,kkk
+    integer :: i,j,k,n,ii,jj,kk,iii,jjj,kkk
     real(amrex_real) :: kappa_tot
 
     ! Note: mask is such that 1=reg, 0=sv, -1=covered, 2=mv, -2=outside domain, -3=crse
-
-    if (dim .ge. 2) then
-       jjlo = -1
-       jjhi = +1
-    else
-       jjlo = 0
-       jjhi = 0
-    endif
-
-    if (dim .eq. 3) then
-       kklo = -1
-       kkhi = +1
-    else
-       kklo = 0
-       kkhi = 0
-    endif
 
     do n = 0, Nsten-1
        i = sten(n) % iv(0)
@@ -48,11 +33,11 @@ contains
             .and. k.ge.lo(2) .and. k.le.hi(2) ) then
 
           kappa_tot = 0.d0
-          do kk=kklo,kkhi
+          do kk=slo(2),shi(2)
              kkk = k+kk
-             do jj=jjlo,jjhi
+             do jj=slo(1),shi(1)
                 jjj = j+jj
-                do ii=-1,1
+                do ii=slo(0),shi(0)
                    iii = i+ii
                    if (mask(iii,jjj,kkk) .eq. 1 &
                         .or. mask(iii,jjj,kkk) .eq. 0) then
@@ -64,11 +49,11 @@ contains
 
           kappa_tot = 1.d0 / kappa_tot
 
-          do kk=kklo,kkhi
+          do kk=slo(2),shi(2)
              kkk = k+kk
-             do jj=jjlo,jjhi
+             do jj=slo(1),shi(1)
                 jjj = j+jj
-                do ii=-1,1
+                do ii=slo(0),shi(0)
                    iii = i+ii
                    if (mask(iii,jjj,kkk) .eq. 1 &
                         .or. mask(iii,jjj,kkk) .eq. 0) then
@@ -86,34 +71,19 @@ contains
 
   end subroutine fill_redist_stencil
 
-  subroutine apply_redist_stencil(lo, hi, sten, Nsten, vin, vin_lo, vin_hi, &
+  subroutine apply_redist_stencil(lo, hi, slo, shi, sten, Nsten, vin, vin_lo, vin_hi, &
     vout, vout_lo, vout_hi) bind(C,name="apply_redist_stencil")
 
-    integer,          intent(in   ) :: lo(0:2), hi(0:2)
+    integer,          intent(in   ) ::  lo(0:2),  hi(0:2)
+    integer,          intent(in   ) :: slo(0:2), shi(0:2)
     integer,          intent(in   ) :: Nsten
     type(nbr_sten),   intent(in   ) :: sten(0:Nsten-1)
     integer,          intent(in   ) ::  vin_lo(3),  vin_hi(3)
     integer,          intent(in   ) :: vout_lo(3), vout_hi(3)
     real(amrex_real), intent(in   ) ::  vin( vin_lo(1):vin_hi(1),  vin_lo(2):vin_hi(2),  vin_lo(3):vin_hi(3))
     real(amrex_real), intent(inout) :: vout(vout_lo(1):vout_hi(1),vout_lo(2):vout_hi(2),vout_lo(3):vout_hi(3))
-    integer :: i,j,k,n,ii,jj,kk,jjlo,jjhi,kklo,kkhi,iii,jjj,kkk
+    integer :: i,j,k,n,ii,jj,kk,iii,jjj,kkk
     real(amrex_real) :: vtot
-
-    if (dim .ge. 2) then
-       jjlo = -1
-       jjhi = +1
-    else
-       jjlo = 0
-       jjhi = 0
-    endif
-
-    if (dim .eq. 3) then
-       kklo = -1
-       kkhi = +1
-    else
-       kklo = 0
-       kkhi = 0
-    endif
 
     do n = 0, Nsten-1
        i = sten(n) % iv(0)
@@ -125,11 +95,11 @@ contains
             .and. k.ge.lo(2) .and. k.le.hi(2) ) then
 
           vtot = 0.d0
-          do kk=kklo,kkhi
+          do kk=slo(2),shi(2)
              kkk = k+kk
-             do jj=jjlo,jjhi
+             do jj=slo(1),shi(1)
                 jjj = j+jj
-                do ii=-1,1
+                do ii=slo(0),shi(0)
                    iii = i+ii
                    vtot = vtot + sten(n)%val(ii,jj,kk) * vin(iii,jjj,kkk)
                 enddo
