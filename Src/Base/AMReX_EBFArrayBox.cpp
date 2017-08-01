a0acc552295643bca48305d5fa8bba210efa8241
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-07-31 17:00:55 -0700

    because we don't have geometry information outside domain

--- Src/Base/AMReX_EBFArrayBox.cpp
+++ Src/Base/AMReX_EBFArrayBox.cpp
@@ -11,17 +11,19 @@ EBFArrayBox::EBFArrayBox (const EBISBox& ebisBox, const Box& box, int ncomps)
     : FArrayBox(box, ncomps),
       m_ebisbox(ebisBox)
 {
-    if (ebisBox.isRegular(box))
+    const Box& sect = box & ebisBox.getRegion();
+
+    if (ebisBox.isRegular(sect))
     {
         m_type = FabType::regular;
         return;  // nothing to do
     }
-    else if (ebisBox.isCovered(box))
+    else if (ebisBox.isCovered(sect))
     {
         m_type = FabType::allcovered;
         return;
     }
-    else if (ebisBox.getMultiCells(box).isEmpty())
+    else if (ebisBox.getMultiCells(sect).isEmpty())
     {
         m_type = FabType::singlevalue;
         return;
