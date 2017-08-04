f811aa41b33e29522b56ca46425c19ca1b8cd584
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-04 13:09:59 -0700

    EBLevel: add FabType

--- Src/Base/AMReX_EBCellFlag.cpp
+++ Src/Base/AMReX_EBCellFlag.cpp
@@ -4,4 +4,53 @@ namespace amrex {
 
 constexpr std::array<std::array<std::array<int,3>,3>,3> EBCellFlag::pos_ngbr;
 
+FabType
+EBFlagFab::getType (const Box& bx_in) const
+{
+    FabType thistype = getType();
+
+    if (thistype == FabType::regular)
+    {
+        return FabType::regular;
+    }
+    else if (thistype == FabType::allcovered)
+    {
+        return FabType::allcovered;
+    }
+    else
+    {
+        const Box& bx = amrex::enclosedCells(bx_in);
+        BL_ASSERT(this->box().contains(bx));
+
+        FabType t = FabType::undefined;
+        int nregular=0, nsingle=0, nmulti=0, ncovered=0;
+        int ncells = bx.numPts();
+        for (BoxIterator bi(bx); bi.ok(); ++bi)
+        {
+            const auto& cellflag = (*this)(bi());
+            if (cellflag.isRegular()) {
+                ++nregular;
+            } else if (cellflag.isSingleValued()) {
+                ++nsingle;
+            } else if (cellflag.isMultiValued()) {
+                ++nmulti;
+            } else {
+                ++ncovered;
+            }
+        }
+
+        if (nregular == ncells) {
+            t = FabType::regular;
+        } else if (ncovered == ncells) {
+            t = FabType::allcovered;
+        } else if (nmulti > 0) {
+            t = FabType::multivalue;
+        } else {
+            t = FabType::singlevalue;
+        }
+
+        return t;
+    }
+}
+
 }
