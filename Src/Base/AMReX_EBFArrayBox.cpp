88345e1c39f2b1740c1f969a971311225acf3e55
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-04 13:26:23 -0700

    EBFArrayBox can now get type info from EBLevel

--- Src/Base/AMReX_EBFArrayBox.cpp
+++ Src/Base/AMReX_EBFArrayBox.cpp
@@ -1,5 +1,6 @@
 
 #include <AMReX_EBFArrayBox.H>
+#include <AMReX_EBCellFlag.H>
 
 namespace amrex {
 
@@ -7,32 +8,13 @@ EBFArrayBox::EBFArrayBox ()
 {
 }
 
-EBFArrayBox::EBFArrayBox (const EBISBox& ebisBox, const Box& box, int ncomps)
+    EBFArrayBox::EBFArrayBox (const EBISBox& ebisBox, const EBFlagFab& ebflag,
+                              const Box& box, int ncomps)
     : FArrayBox(box, ncomps),
       m_ebisbox(ebisBox)
 {
     const Box& sect = amrex::enclosedCells(box) & ebisBox.getRegion();
-
-    if (ebisBox.isRegular(sect))
-    {
-        m_type = FabType::regular;
-        return;  // nothing to do
-    }
-    else if (ebisBox.isCovered(sect))
-    {
-        m_type = FabType::allcovered;
-        return;
-    }
-    else if (ebisBox.getMultiCells(sect).isEmpty())
-    {
-        m_type = FabType::singlevalue;
-        return;
-    }
-    else
-    {
-        m_type = FabType::multivalue;
-        return;
-    }
+    m_type = ebflag.getType(sect);
 }
 
 EBFArrayBox::~EBFArrayBox ()
