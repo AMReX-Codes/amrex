a2442addb45aa4ef8d38c69a141d0f50433a98da
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-07 09:43:35 -0700

    save pointers to ebflag fab in EbFArrayBox

--- Src/Base/AMReX_EBFArrayBox.cpp
+++ Src/Base/AMReX_EBFArrayBox.cpp
@@ -5,13 +5,17 @@
 namespace amrex {
 
 EBFArrayBox::EBFArrayBox ()
+    : FArrayBox(),
+      m_ebisbox(nullptr),
+      m_ebflag(nullptr)
 {
 }
 
-    EBFArrayBox::EBFArrayBox (const EBISBox& ebisBox, const EBFlagFab& ebflag,
-                              const Box& box, int ncomps)
+EBFArrayBox::EBFArrayBox (const EBISBox& ebisBox, const EBFlagFab& ebflag,
+                          const Box& box, int ncomps)
     : FArrayBox(box, ncomps),
-      m_ebisbox(ebisBox)
+      m_ebisbox(&ebisBox),
+      m_ebflag(&ebflag)
 {
     const Box& sect = amrex::enclosedCells(box) & ebisBox.getRegion();
     m_type = ebflag.getType(sect);
