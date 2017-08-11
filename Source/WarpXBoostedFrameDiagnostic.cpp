6f0aa23c45ae590ffa4e62c840f77aac4503cbef
Andrew Myers <atmyers2@gmail.com>
2017-08-11 11:33:36 -0700

    class for managing boosted frame IO.

---
+++ Source/WarpXBoostedFrameDiagnostic.cpp
@@ -0,0 +1,30 @@
+#include "WarpXBoostedFrameDiagnostic.H"
+
+BoostedFrameDiagnostic::
+BoostedFrameDiagnostic(Real zmin_lab, Real zmax_lab, Real v_window_lab,
+                       Real dt_snapshots_lab, int N_snapshots, 
+                       Real gamma_boost, Real dt_boost)
+    : gamma_boost_(gamma_boost), 
+      dt_snapshots_lab_(dt_snapshots_lab), 
+      dt_boost_(dt_boost),
+      N_snapshots_(N_snapshots)
+{
+    inv_gamma_boost_ = 1.0 / gamma_boost_;
+    beta_boost_ = std::sqrt(1.0 - inv_gamma_boost_*inv_gamma_boost_);
+    inv_beta_boost_ = 1.0 / beta_boost_;
+
+    dz_lab_ = PhysConst::c * dt_boost_ * inv_beta_boost_ * inv_gamma_boost_;
+    inv_dz_lab_ = 1.0 / dz_lab_;
+    Nz_lab_ = static_cast<int>((zmax_lab - zmin_lab) * inv_dz_lab_);
+    
+    for (int i = 0; i < N_snapshots; ++i) {
+        Real t_lab = i * dt_snapshots_lab_;
+        LabSnapShot snapshot(t_lab, zmin_lab + v_window_lab * t_lab,
+                                    zmax_lab + v_window_lab * t_lab, i);
+        snapshots_.push_back(snapshot);
+        const int nlevels = 1;
+        const std::string level_prefix = "Level_";
+        amrex::PreBuildDirectorHierarchy(snapshot.file_name, 
+                                         level_prefix, nlevels, true);
+    }
+}
