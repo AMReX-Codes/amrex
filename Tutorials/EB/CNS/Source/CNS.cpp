61e917f8f8e13fa8919bfe48fb8fb9104da33243
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-11 15:16:27 -0700

    skeleton for EB/CNS

---
+++ Tutorials/EB/CNS/Source/CNS.cpp
@@ -0,0 +1,321 @@
+
+#include <CNS.H>
+#include <AMReX_EBMultiFabUtil.H>
+
+#include <climits>
+
+using namespace amrex;
+
+constexpr int CNS::NUM_GROW;
+
+int       CNS::verbose = 0;
+ErrorList CNS::err_list;
+BCRec     CNS::phys_bc;
+IntVect   CNS::hydro_tile_size {1024,16,16};
+
+CNS::CNS ()
+{}
+
+CNS::CNS (Amr&            papa,
+          int             lev,
+          const Geometry& level_geom,
+          const BoxArray& bl,
+          const DistributionMapping& dm,
+          Real            time)
+    : AmrLevel(papa,lev,level_geom,bl,dm,time)
+{
+    // xxxxx need to build flux register
+
+    buildMetrics();
+
+    // xxxxx initialize EB
+}
+
+CNS::~CNS ()
+{}
+
+void
+CNS::init (AmrLevel& old)
+{
+    auto& oldlev = dynamic_cast<CNS&>(old);
+
+    Real dt_new    = parent->dtLevel(level);
+    Real cur_time  = oldlev.state[State_Type].curTime();
+    Real prev_time = oldlev.state[State_Type].prevTime();
+    Real dt_old    = cur_time - prev_time;
+    setTimeLevel(cur_time,dt_old,dt_new);
+
+    MultiFab& S_new = get_new_data(State_Type);
+    FillPatch(old,S_new,0,cur_time,State_Type,0,NUM_STATE);
+}
+
+void
+CNS::init ()
+{
+    Real dt        = parent->dtLevel(level);
+    Real cur_time  = getLevel(level-1).state[State_Type].curTime();
+    Real prev_time = getLevel(level-1).state[State_Type].prevTime();
+    Real dt_old = (cur_time - prev_time)/static_cast<Real>(parent->MaxRefRatio(level-1));
+    setTimeLevel(cur_time,dt_old,dt);
+
+    MultiFab& S_new = get_new_data(State_Type);
+    FillCoarsePatch(S_new, 0, cur_time, State_Type, 0, NUM_STATE);
+}
+
+void
+CNS::initData ()
+{
+    const Real* dx  = geom.CellSize();
+    const Real* prob_lo = geom.ProbLo();
+    MultiFab& S_new = get_new_data(State_Type);
+    Real cur_time   = state[State_Type].curTime();
+
+    for (MFIter mfi(S_new); mfi.isValid(); ++mfi)
+    {
+        const Box& box = mfi.validbox();
+        // xxxxx
+#if 0
+        cns_initdata(level, cur_time,
+                     BL_TO_FORTRAN_BOX(box),
+                     BL_TO_FORTRAN_ANYD(S_new[mfi]),
+                     ZFILL(prob_lo));
+#endif
+    }
+}
+
+void
+CNS::computeInitialDt (int                   finest_level,
+	  	               int                   sub_cycle,
+                               Array<int>&           n_cycle,
+                               const Array<IntVect>& ref_ratio,
+                               Array<Real>&          dt_level,
+                               Real                  stop_time)
+{
+    //
+    // Grids have been constructed, compute dt for all levels.
+    //
+    if (level > 0) {
+        return;
+    }
+
+    Real dt_0 = std::numeric_limits<Real>::max();
+    int n_factor = 1;
+    for (int i = 0; i <= finest_level; i++)
+    {
+        dt_level[i] = getLevel(i).initialTimeStep();
+        n_factor   *= n_cycle[i];
+        dt_0 = std::min(dt_0,n_factor*dt_level[i]);
+    }
+
+    //
+    // Limit dt's by the value of stop_time.
+    //
+    const Real eps = 0.001*dt_0;
+    Real cur_time  = state[State_Type].curTime();
+    if (stop_time >= 0.0) {
+        if ((cur_time + dt_0) > (stop_time - eps))
+            dt_0 = stop_time - cur_time;
+    }
+
+    n_factor = 1;
+    for (int i = 0; i <= finest_level; i++)
+    {
+        n_factor *= n_cycle[i];
+        dt_level[i] = dt_0/n_factor;
+    }
+}
+
+void
+CNS::computeNewDt (int                   finest_level,
+                   int                   sub_cycle,
+                   Array<int>&           n_cycle,
+                   const Array<IntVect>& ref_ratio,
+                   Array<Real>&          dt_min,
+                   Array<Real>&          dt_level,
+                   Real                  stop_time,
+                   int                   post_regrid_flag)
+{
+    //
+    // We are at the end of a coarse grid timecycle.
+    // Compute the timesteps for the next iteration.
+    //
+    if (level > 0) {
+        return;
+    }
+
+    for (int i = 0; i <= finest_level; i++)
+    {
+        dt_min[i] = getLevel(i).estTimeStep();
+    }
+
+    if (post_regrid_flag == 1) 
+    {
+	//
+	// Limit dt's by pre-regrid dt
+	//
+	for (int i = 0; i <= finest_level; i++)
+	{
+	    dt_min[i] = std::min(dt_min[i],dt_level[i]);
+	}
+    }
+    else 
+    {
+	//
+	// Limit dt's by change_max * old dt
+	//
+	static Real change_max = 1.1;
+	for (int i = 0; i <= finest_level; i++)
+	{
+	    dt_min[i] = std::min(dt_min[i],change_max*dt_level[i]);
+	}
+    }
+    
+    //
+    // Find the minimum over all levels
+    //
+    Real dt_0 = std::numeric_limits<Real>::max();
+    int n_factor = 1;
+    for (int i = 0; i <= finest_level; i++)
+    {
+        n_factor *= n_cycle[i];
+        dt_0 = std::min(dt_0,n_factor*dt_min[i]);
+    }
+
+    //
+    // Limit dt's by the value of stop_time.
+    //
+    const Real eps = 0.001*dt_0;
+    Real cur_time  = state[State_Type].curTime();
+    if (stop_time >= 0.0) {
+        if ((cur_time + dt_0) > (stop_time - eps)) {
+            dt_0 = stop_time - cur_time;
+        }
+    }
+
+    n_factor = 1;
+    for (int i = 0; i <= finest_level; i++)
+    {
+        n_factor *= n_cycle[i];
+        dt_level[i] = dt_0/n_factor;
+    }
+}
+
+void
+CNS::post_regrid (int lbase, int new_finest)
+{}
+
+void
+CNS::post_timestep (int iteration)
+{
+    // xxxxx some reflux stuff
+
+    if (level < parent->finestLevel()) {
+        avgDown();
+    }
+}
+
+void
+CNS::post_init (Real)
+{
+    if (level > 0) return;
+    for (int k = parent->finestLevel()-1; k >= 0; --k) {
+        getLevel(k).avgDown();
+    }
+}
+
+void
+CNS::errorEst (TagBoxArray& tags, int clearval, int tagval, Real time, int n_error_buf, int ngrow)
+{
+#if 0
+    const Real* dx        = geom.CellSize();
+    const Real* prob_lo   = geom.ProbLo();
+
+    MultiFab& S_new = get_new_data(State_Type);
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+    {
+        Array<int>  itags;
+	
+	for (MFIter mfi(S_new,true); mfi.isValid(); ++mfi)
+	{
+	    const Box&  tilebx  = mfi.tilebox();
+
+            TagBox&     tagfab  = tags[mfi];
+	    
+	    // We cannot pass tagfab to Fortran becuase it is BaseFab<char>.
+	    // So we are going to get a temporary integer array.
+	    tagfab.get_itags(itags, tilebx);
+	    
+            // data pointer and index space
+	    int*        tptr    = itags.dataPtr();
+	    const int*  tlo     = tilebx.loVect();
+	    const int*  thi     = tilebx.hiVect();
+
+	    state_error(tptr,  ARLIM_3D(tlo), ARLIM_3D(thi),
+			BL_TO_FORTRAN_3D(S_new[mfi]),
+			&tagval, &clearval, 
+			ARLIM_3D(tilebx.loVect()), ARLIM_3D(tilebx.hiVect()), 
+			ZFILL(dx), ZFILL(prob_lo), &time, &level);
+	    //
+	    // Now update the tags in the TagBox.
+	    //
+	    tagfab.tags_and_untags(itags, tilebx);
+	}
+    }
+#endif
+}
+
+void
+CNS::read_params ()
+{
+    // xxxxx
+}
+
+void
+CNS::avgDown ()
+{
+    if (level == parent->finestLevel()) return;
+
+    auto& fine_lev = getLevel(level+1);
+
+    MultiFab& S_crse =          get_new_data(State_Type);
+    MultiFab& S_fine = fine_lev.get_new_data(State_Type);
+
+    MultiFab volume(S_fine.boxArray(), S_fine.DistributionMap(), 1, 0);
+    volume.setVal(1.0);
+    amrex::EB_average_down(S_fine, S_crse, volume, fine_lev.volFrac(),
+                           0, S_fine.nComp(), fine_ratio);
+
+    const int nghost = 0;
+    computeTemp (S_crse, nghost);
+}
+
+void
+CNS::buildMetrics ()
+{
+    volfrac.clear();
+    volfrac.define(grids,dmap,1,NUM_GROW,MFInfo(),Factory());
+    amrex::EB_set_volume_fraction(volfrac);
+}
+
+Real
+CNS::estTimeStep ()
+{
+    // xxxxx
+    return 0.0;
+}
+
+Real
+CNS::initialTimeStep ()
+{
+    return estTimeStep();
+}
+
+void
+CNS::computeTemp (MultiFab& State, int ng)
+{
+    // xxxxx
+    return;
+}
