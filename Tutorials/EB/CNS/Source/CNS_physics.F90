1462329252451079cdabd7cdfbc4d2b14b466f72
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-11 21:48:24 -0700

    CNS/EB: derive

--- Tutorials/EB/CNS/Source/CNS_physics.F90
+++ Tutorials/EB/CNS/Source/CNS_physics.F90
@@ -10,4 +10,9 @@ module cns_physics_module
   real(rt), parameter, public :: C_S   = 1.458d-5  ! constant in Sutherland's law
   real(rt), parameter, public :: T_S   = 110.4d0   ! Sutherland temperature
 
+  real(rt), parameter, public :: Ru = 8.31451d+07
+
+  real(rt), parameter, public :: R_mu = Ru/mu
+  real(rt), parameter, public :: cv = Ru/(mu*(gamma-1.d0))
+
 end module cns_physics_module
