f5389eb2c98701be96fcb42d9e191edc7d1bac72
Andrew Myers <atmyers2@gmail.com>
2017-08-16 16:51:24 -0700

    minor tweaks.

--- Tutorials/Particles/LoadBalance/create_binary_particle_file.py
+++ Tutorials/Particles/LoadBalance/create_binary_particle_file.py
@@ -1,7 +1,9 @@
 import yt
 import numpy as np
 
-ds = yt.load("enzo_tiny_cosmology/DD0046/DD0046")
+#ds = yt.load("enzo_tiny_cosmology/DD0020/DD0020")
+ds = yt.load("Enzo_64/DD0040/data0040")
+#ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
 ad = ds.all_data()
 
 pos = ad['particle_position']
@@ -13,6 +15,8 @@ nxtra = np.array([0], dtype=np.int32)
 particle_positions = pos.reshape(pos.shape[0]*pos.shape[1])
 
 data = particle_positions.d
+
+print(num_particles)
 
 with file("binary_particle_file.dat", "w") as f:
     f.write(num_particles.tobytes())
