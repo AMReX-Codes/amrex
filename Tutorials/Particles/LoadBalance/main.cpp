a8a1cba2eff6985b701c316fb4027c8521fa2cb3
Andrew Myers <atmyers2@gmail.com>
2017-08-17 10:23:16 -0700

    some reorganization.

--- Tutorials/Particles/LoadBalance/main.cpp
+++ Tutorials/Particles/LoadBalance/main.cpp
@@ -11,7 +11,7 @@
 using namespace amrex;
 
 struct KDNode {
-
+    
     Box box;
     KDNode* left;
     KDNode* right;
@@ -26,75 +26,105 @@ struct KDNode {
     }
 };
 
-int getLongestDir(const Box& box) {
-    IntVect size = box.size();
-    int argmax = 0;
-    int max = size[0];
-    for (int i = 1; i < BL_SPACEDIM; ++i) {
-        if (size[i] > max) {
-            max = size[i];
-            argmax = i;
+class KDTree {
+    
+public:
+    
+    KDTree(const Box& domain, const FArrayBox& cost, int num_procs) {
+        Real total_cost = cost.sum(0);
+        root = new KDNode(domain, total_cost, num_procs);
+        buildKDTree(root, cost);
+    }
+    
+    ~KDTree() {
+        freeKDTree(root);
+    }
+    
+    void GetBoxes(BoxList& bl, Array<Real>& costs) {        
+        walkKDTree(root, bl, costs);
+    }
+    
+private:
+    
+    void buildKDTree(KDNode* node, const FArrayBox& cost) {
+        if (node->num_procs_left == 1) return;
+        
+        partitionNode(node, cost);
+        
+        buildKDTree(node->left,  cost);
+        buildKDTree(node->right, cost);
+    }
+
+    void freeKDTree(KDNode* node) {
+        
+        if (node != NULL) {
+            
+            KDNode* left  = node->left;
+            KDNode* right = node->right;
+            
+            delete node;
+            
+            freeKDTree(left);
+            freeKDTree(right);
         }
     }
-    return argmax;
-}
 
-void splitBox(int split, int dir, const Box& box, Box& left, Box& right) {
-
-    left = box;
-    right = box;
-
-    left.setBig(dir, split);
-    right.setSmall(dir, split+1);
-
-}
-
-void partitionNode(KDNode* node, const FArrayBox& cost) {
-
-    const Box& box = node->box;
-    BL_ASSERT(cost.box().contains(box));
-    
-    int split;
-    Real cost_left, cost_right;
-    int dir = getLongestDir(box);
-    compute_best_partition(cost.dataPtr(), cost.loVect(), cost.hiVect(),
-                           box.loVect(), box.hiVect(), node->cost, dir,
-                           &cost_left, &cost_right, &split);
-    
-    Box left, right;
-    splitBox(split, dir, box, left, right);
-    
-    node->left  = new KDNode(left,  cost_left,  node->num_procs_left/2);
-    node->right = new KDNode(right, cost_right, node->num_procs_left/2);
-}
-
-void build_kd_tree_recursive(KDNode* node, const FArrayBox& cost) {
-    if (node->num_procs_left == 1) return;
-    
-    partitionNode(node, cost);
-
-    build_kd_tree_recursive(node->left,  cost);
-    build_kd_tree_recursive(node->right, cost);
-}
-
-KDNode* build_kd_tree(const Box& domain, const FArrayBox& cost, int num_procs) {
-    Real total_cost = cost.sum(0);
-    KDNode* root = new KDNode(domain, total_cost, num_procs);
-    build_kd_tree_recursive(root, cost);
-    return root;
-}
-
-void walk_kd_tree(KDNode* node, BoxList& bl, Array<Real>& costs) {
-
-    if (node->left == NULL && node->right == NULL) {
-        costs.push_back(node->cost);
-        bl.push_back(node->box);
-        return;
+    void walkKDTree(KDNode* node, BoxList& bl, Array<Real>& costs) {
+        
+        if (node->left == NULL && node->right == NULL) {
+            costs.push_back(node->cost);
+            bl.push_back(node->box);
+            return;
+        }
+        
+        walkKDTree(node->left,  bl, costs);
+        walkKDTree(node->right, bl, costs);
     }
     
-    walk_kd_tree(node->left, bl, costs);
-    walk_kd_tree(node->right, bl, costs);
-}
+    void partitionNode(KDNode* node, const FArrayBox& cost) {
+        
+        const Box& box = node->box;
+        BL_ASSERT(cost.box().contains(box));
+        
+        int split;
+        Real cost_left, cost_right;
+        int dir = getLongestDir(box);
+        compute_best_partition(cost.dataPtr(), cost.loVect(), cost.hiVect(),
+                               box.loVect(), box.hiVect(), node->cost, dir,
+                               &cost_left, &cost_right, &split);
+        
+        Box left, right;
+        splitBox(split, dir, box, left, right);
+        
+        node->left  = new KDNode(left,  cost_left,  node->num_procs_left/2);
+        node->right = new KDNode(right, cost_right, node->num_procs_left/2);
+    }
+
+    int getLongestDir(const Box& box) {
+        IntVect size = box.size();
+        int argmax = 0;
+        int max = size[0];
+        for (int i = 1; i < BL_SPACEDIM; ++i) {
+            if (size[i] > max) {
+                max = size[i];
+                argmax = i;
+            }
+        }
+        return argmax;
+    }
+    
+    void splitBox(int split, int dir,
+                  const Box& box, Box& left, Box& right) {
+        left = box;
+        right = box;
+        
+        left.setBig(dir, split);
+        right.setSmall(dir, split+1);        
+    }
+
+    KDNode* root;
+    
+};
 
 class MyParticleContainer 
     : public ParticleContainer<0>
@@ -178,17 +208,17 @@ int main(int argc, char* argv[])
         cost = &global_cost[mfi];
     }
 
-    KDNode* root = build_kd_tree(domain, *cost, num_procs);
+    KDTree tree = KDTree(domain, *cost, num_procs);
 
     BoxList new_bl;
     Array<Real> box_costs;
-    walk_kd_tree(root, new_bl, box_costs);
+    tree.GetBoxes(new_bl, box_costs);
     BoxArray new_ba(new_bl);
-
+    
     for (int i = 0; i < box_costs.size(); ++i) {
         std::cout << box_costs[i] << " " << new_ba[i] << std::endl;
     }
-
+    
     Array<int> new_pmap;
     for (int i = 0; i < new_ba.size(); ++i) {
         new_pmap.push_back(0);
