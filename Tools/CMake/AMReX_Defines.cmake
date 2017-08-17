93280c1536824becc995230a101c159306623a0f
Michele Rosso <mrosso@lbl.gov>
2017-08-17 16:57:20 -0700

    CMake: update   * Add support for ProfParser   * Re-factor souce files lists   * Change some config args.

--- Tools/CMake/AMReX_Defines.cmake
+++ Tools/CMake/AMReX_Defines.cmake
@@ -60,75 +60,106 @@ endfunction ()
 include ( FortranCInterface )
 include ( ${FortranCInterface_BINARY_DIR}/Output.cmake )
 
-if ( ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "" )  AND
-      ( FortranCInterface_GLOBAL_CASE STREQUAL "UPPER") )
-   message(STATUS "Fortran name mangling scheme to UPPERCASE \
-(upper case, no append underscore)")
-   add_define ( BL_FORT_USE_UPPERCASE AMREX_DEFINES )
-elseif ( ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "") AND
-      ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER") )
-   message(STATUS "Fortran name mangling scheme to LOWERCASE \
-(lower case, no append underscore)")
-   add_define ( BL_FORT_USE_LOWERCASE AMREX_DEFINES )
-elseif ( ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "_" ) AND
-      ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER") )
-   message(STATUS "Fortran name mangling scheme to UNDERSCORE \
-(lower case, append underscore)")
-   add_define ( BL_FORT_USE_UNDERSCORE AMREX_DEFINES )
+set (FORTLINK "")
+
+if ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "" )
+
+   set (FORTLINK "${FortranCInterface_GLOBAL_CASE}CASE" )
+   message (STATUS "Fortran name mangling scheme: ${FORTLINK} (no append underscore)")
+
+elseif ( (FortranCInterface_GLOBAL_SUFFIX STREQUAL "_")  AND
+      ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER" ) )
+
+   set (FORTLINK "UNDERSCORE")
+   message (STATUS "Fortran name mangling scheme: ${FORTLINK} (lower case, append underscore)")
+
 else ()
-   message(AUTHOR_WARNING "Fortran to C mangling not backward\
- compatible with older style BoxLib code") 
+   message (AUTHOR_WARNING "Fortran to C mangling not compatible with AMReX code")
 endif ()
 
 
 # 
-# Set preprocessor flags 
-# 
-add_define ( AMREX_NOLINEVALUES )
-add_define ( AMREX_PARALLEL_IO )
-add_define ( AMREX_SPACEDIM=${BL_SPACEDIM} )
+# Set preprocessor flags (trying to mimic GNUMake setup) 
+#
+
+# Git version
+list ( APPEND AMREX_DEFINES -DAMREX_GIT_VERSION=\"${AMREX_GIT_VERSION}\" )
+
+# Debug flag
+if (DEBUG)
+   add_define (DEBUG)
+else ()
+   add_define (NDEBUG)
+endif ()
+
+# Base profiling options
+add_define ( AMREX_PROFILING IF ENABLE_BASE_PROFILE )
+
+add_define ( AMREX_TRACE_PROFILING IF ENABLE_TRACE_PROFILE )
+
+add_define ( AMREX_COMM_PROFILING  IF ENABLE_COMM_PROFILE )
+
+# Tiny profiler
+add_define ( AMREX_TINY_PROFILING IF ENABLE_TINY_PROFILE )
+
+# Mem profiler 
+add_define ( AMREX_MEM_PROFILING IF ENABLE_MEM_PROFILE )
+
+# Profparser 
+add_define ( AMREX_USE_PROFPARSER IF ENABLE_PROFPARSER )
+
+# Backtrace
+if (ENABLE_BACKTRACE)
+   add_define ( AMREX_BACKTRACING )
+   add_define ( AMREX_TESTING )
+endif ()
+
+# MPI
+add_define ( AMREX_USE_MPI IF ENABLE_MPI )
+
+# OpenMP
+add_define ( AMREX_USE_OMP IF ENABLE_OMP )
+
+# Precision
+if (NOT ENABLE_DP)
+   add_define (AMREX_USE_FLOAT)
+endif ()
+
+# Fortran/C mangling scheme
+add_define ( AMREX_FORT_USE_${FORTLINK} )
+
+# Dimensionality
+add_define ( AMREX_SPACEDIM=${DIM} )
+
+# System
 add_define ( AMREX_${CMAKE_SYSTEM_NAME} )
 
-if ( ENABLE_DP )
-   add_define ( AMREX_USE_DOUBLE )
-else ()
-   add_define ( AMREX_USE_FLOAT )
+# Particles
+if (ENABLE_PARTICLES)
+   add_define ( USE_PARTICLES )
+   add_define ( PARTICLES )
+
+   if (NOT ENABLE_DP_PARTICLES)
+      add_define ( AMREX_SINGLE_PRECISION_PARTICLES )
+   endif ()
 endif ()
 
-add_define ( USE_PARTICLES IF ENABLE_PARTICLES )
-
-# Profiling flags 
-add_define ( AMREX_PROFILING       IF AMREX_ANY_PROFILING ) 
-add_define ( AMREX_TINY_PROFILING  IF ENABLE_TINY_PROFILING )
-add_define ( AMREX_COMM_PROFILING  IF ENABLE_COMM_PROFILING )
-add_define ( AMREX_TRACE_PROFILING IF ENABLE_TRACE_PROFILING )
-add_define ( AMREX_MEM_PROFILING   IF ENABLE_MEM_PROFILING )
-
-if ( ENABLE_PARTICLES AND ( NOT ENABLE_DP_PARTICLES ) ) 
-   add_define ( AMREX_SINGLE_PRECISION_PARTICLES )
-endif ()
+#  Assertions
+add_define ( AMREX_USE_ASSERTION IF ENABLE_ASSERTION )
 
 if (ENABLE_FBASELIB)
    add_define ( AMREX_USE_FORTRAN_MPI IF ENABLE_MPI)
    add_define ( AMREX_USE_F_BASELIB )
 endif ()
 
-
-add_define ( AMREX_USE_MPI IF ENABLE_MPI )
-
-add_define ( AMREX_USE_OMP IF ENABLE_OMP )
-
 add_define ( AMREX_USE_F_INTERFACES IF ENABLE_FORTRAN_INTERFACES )
 
 add_define ( AMREX_USE_ASSERTION IF ENABLE_ASSERTIONS ) 
 
-# Here I use explicitly a list because BL_GIT_VERSION (legacy define) does not
-# exist
-list ( APPEND AMREX_DEFINES -DAMREX_GIT_VERSION=\"${AMREX_GIT_VERSION}\" )
-
 #
 # Add all preprocessor definitions to compile string
 # 
+print_list (AMREX_DEFINES)
 add_definitions ( ${AMREX_DEFINES} )
 
 
