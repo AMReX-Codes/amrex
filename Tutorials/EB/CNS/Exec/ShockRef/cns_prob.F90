9a2aaf961397d5bcd9fc72dd4bb12e28b053ecb8
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-18 12:36:59 -0700

    EB/CNS: mod inputs

--- Tutorials/EB/CNS/Exec/ShockRef/cns_prob.F90
+++ Tutorials/EB/CNS/Exec/ShockRef/cns_prob.F90
@@ -5,7 +5,9 @@ module probdata_module
   real(rt), save :: p1   = 7.42d6
   real(rt), save :: rho0 = 3.29d-5
   real(rt), save :: rho1 = 3.61d-4
-  real(rt), save :: x1 = 4.2
+  real(rt), save :: v0   = 0.d0
+  real(rt), save :: v1   = 0.d0
+  real(rt), save :: x1   = 4.2
 end module probdata_module
 
 
@@ -23,6 +25,8 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   call pp%query("p1", p1)
   call pp%query("rho0", rho0)
   call pp%query("rho1", rho1)
+  call pp%query("v0", v0)
+  call pp%query("v1", v1)
   call pp%query("x1", x1)
   call amrex_parmparse_destroy(pp)
 end subroutine amrex_probinit
@@ -32,7 +36,7 @@ subroutine cns_initdata(level, time, lo, hi, u, ulo, uhi, dx, prob_lo) bind(C, n
   use amrex_fort_module, only : rt => amrex_real
   use cns_physics_module, only : gamma, cv
   use cns_module, only : center, nvar, urho, umx, umy, umz, ueden, ueint, utemp
-  use probdata_module, only : p0,p1,rho0,rho1,x1
+  use probdata_module, only : p0,p1,rho0,rho1,v0,v1,x1
   implicit none
   integer, intent(in) :: level, lo(3), hi(3), ulo(3), uhi(3)
   real(rt), intent(in) :: time
@@ -51,13 +55,15 @@ subroutine cns_initdata(level, time, lo, hi, u, ulo, uhi, dx, prob_lo) bind(C, n
            if (x .gt. x1) then
               u(i,j,k,urho) = rho0
               u(i,j,k,ueint) = p0 / (gamma-1.d0)
+              u(i,j,k,umx) = u(i,j,k,urho)*v0
            else
               u(i,j,k,urho) = rho1
               u(i,j,k,ueint) = p1 / (gamma-1.d0)
+              u(i,j,k,umx) = u(i,j,k,urho)*v1
            end if
 
-           u(i,j,k,umx:umz) = 0.d0
-           u(i,j,k,ueden) = u(i,j,k,ueint)
+           u(i,j,k,umy:umz) = 0.d0
+           u(i,j,k,ueden) = u(i,j,k,ueint) + 0.5d0*u(i,j,k,umx)**2/u(i,j,k,urho)
            u(i,j,k,utemp) = u(i,j,k,ueint)/(u(i,j,k,urho)*cv)
 
         end do
