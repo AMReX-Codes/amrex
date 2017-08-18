bb497048504639db8bc9b93b6215e78be5fbc045
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-18 09:23:45 -0700

    EB/CNS: fix a bug in 2-shock riemann solver; add HLL; floor density and pressure

--- Tutorials/EB/CNS/Source/hydro/CNS_hydro_mod.F90
+++ Tutorials/EB/CNS/Source/hydro/CNS_hydro_mod.F90
@@ -112,6 +112,7 @@ contains
 
   subroutine ctoprim (lo,hi,u,ulo,uhi,q,qlo,qhi)
     use cns_physics_module, only : gamma, cv
+    use cns_module, only : smallp, smallr
     integer, intent(in) :: lo(3),hi(3),ulo(3),uhi(3),qlo(3),qhi(3)
     real(rt), intent(in   ) :: u(ulo(1):uhi(1),ulo(2):uhi(2),ulo(3):uhi(3),nvar)
     real(rt), intent(inout) :: q(qlo(1):qhi(1),qlo(2):qhi(2),qlo(3):qhi(3),qvar)
@@ -122,8 +123,8 @@ contains
     do       k = lo(3), hi(3)
        do    j = lo(2), hi(2)
           do i = lo(1), hi(1)
-             rhoinv = 1.d0/u(i,j,k,urho)
-             q(i,j,k,qrho) = u(i,j,k,urho)
+             q(i,j,k,qrho) = max(smallr,u(i,j,k,urho))
+             rhoinv = 1.d0/q(i,j,k,qrho)
              q(i,j,k,qu) = u(i,j,k,umx)*rhoinv
              q(i,j,k,qv) = u(i,j,k,umy)*rhoinv
              q(i,j,k,qw) = u(i,j,k,umz)*rhoinv
@@ -132,7 +133,7 @@ contains
              if (q(i,j,k,qeint) .le. 0.d0) then
                 q(i,j,k,qeint) = u(i,j,k,ueint) * rhoinv
              end if
-             q(i,j,k,qp) = (gamma-1.d0)*q(i,j,k,qeint)*q(i,j,k,qrho)
+             q(i,j,k,qp) = max(smallp,(gamma-1.d0)*q(i,j,k,qeint)*q(i,j,k,qrho))
              q(i,j,k,qc) = sqrt(gamma*q(i,j,k,qp)*rhoinv)
              q(i,j,k,qtemp) = q(i,j,k,qeint) * (1.d0/cv)
           end do
