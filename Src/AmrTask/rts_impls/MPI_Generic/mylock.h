a15e0044e48bb82b3e6771709f4869dd210501e8
Tan Nguyen <tannguyen@lbl.gov>
2017-08-25 11:45:06 -0700

    MPI runtime implementation

---
+++ Src/AmrTask/rts_impls/MPI_Generic/mylock.h
@@ -0,0 +1,23 @@
+#include <pthread.h>
+
+class MyLock
+{
+    private: 
+	pthread_mutex_t _lock;
+
+    public:
+	MyLock(){
+            pthread_mutex_init(&_lock, NULL);
+	}
+	~MyLock(){
+	    pthread_mutex_destroy(&_lock);
+	}
+	void lock()
+	{
+	    pthread_mutex_lock(&_lock);
+	}
+	void unlock()
+	{
+	    pthread_mutex_unlock(&_lock);
+	}
+};
