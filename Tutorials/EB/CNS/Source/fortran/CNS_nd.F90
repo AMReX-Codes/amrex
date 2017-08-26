4973d49ecf950b4d8cbc7bfaa8d177dc7ef0f5bd
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-08-26 07:25:56 -0700

    EB/CNS: clean up

--- Tutorials/EB/CNS/Source/fortran/CNS_nd.F90
+++ Tutorials/EB/CNS/Source/fortran/CNS_nd.F90
@@ -56,36 +56,6 @@ contains
     end do
   end subroutine cns_estdt
 
-
-  subroutine cns_compute_dsdt (lo,hi,ut,ulo,uhi,fx,fxlo,fxhi,fy,fylo,fyhi,fz,fzlo,fzhi,dx) &
-       bind(c,name='cns_compute_dsdt')
-    integer, intent(in) :: lo(3),hi(3),ulo(3),uhi(3),fxlo(3),fxhi(3),fylo(3),fyhi(3),fzlo(3),fzhi(3)
-    real(rt), intent(inout) :: ut( ulo(1): uhi(1), ulo(2): uhi(2), ulo(3): uhi(3),nvar)
-    real(rt), intent(in   ) :: fx(fxlo(1):fxhi(1),fxlo(2):fxhi(2),fxlo(3):fxhi(3),nvar)
-    real(rt), intent(in   ) :: fy(fylo(1):fyhi(1),fylo(2):fyhi(2),fylo(3):fyhi(3),nvar)
-    real(rt), intent(in   ) :: fz(fzlo(1):fzhi(1),fzlo(2):fzhi(2),fzlo(3):fzhi(3),nvar)
-    real(rt), intent(in) :: dx(3)
-
-    integer :: i,j,k,n
-    real(rt) :: dxinv(3)
-
-    dxinv = 1.d0/dx
-
-    do n = 1, nvar
-       do       k = lo(3),hi(3)
-          do    j = lo(2),hi(2)
-             do i = lo(1),hi(1)
-                ut(i,j,k,n) = ut(i,j,k,n) &
-                     + (fx(i,j,k,n)-fx(i+1,j,k,n)) * dxinv(1) &
-                     + (fy(i,j,k,n)-fy(i,j+1,k,n)) * dxinv(2) &
-                     + (fz(i,j,k,n)-fz(i,j,k+1,n)) * dxinv(3)
-             end do
-          end do
-       end do
-    end do
-  end subroutine cns_compute_dsdt
-
-
   subroutine ctoprim (lo,hi,u,ulo,uhi,q,qlo,qhi)
     use cns_physics_module, only : gamma, cv
     use cns_module, only : smallp, smallr
