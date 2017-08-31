51be12d26e34fd0c8825f7f0246b9e65d37cc938
Tan Nguyen <tannguyen@lbl.gov>
2017-08-30 18:11:50 -0700

    add task association feature to support dynamic task parallelism

--- Src/AmrTask/rts_impls/MPI_Generic/rts.C
+++ Src/AmrTask/rts_impls/MPI_Generic/rts.C
@@ -17,7 +17,6 @@
 using namespace std;
 #include <cassert>
 
-#include "dl_malloc.h"
 namespace amrex{
     //we don't use template for task and message queuese since in the future we may implement them in different ways
     class _TaskQueue {
@@ -111,20 +110,16 @@ namespace amrex{
 	    free(_threads);
 	}
     };
-    mspace *mspaces;
-    void ** segment_ptrs;
     int numa_nodes;
     RtsDomain *dom;
     int **_stopSignal;
     AbstractTaskGraph<Task>* graph;
     char* _DedicatedScheduler;
-
-    void NUMA_malloc(int numaID, void** ptr, size_t size){
-	*ptr = mspace_memalign(mspaces[numaID], 64, size);
-    }
-    void NUMA_free(int numaID, void* ptr){
-	mspace_free(mspaces[numaID], ptr);
-    }
+    std::queue< std::pair<MPI_Request*, Data*> > _SendRequests;
+    std::queue< std::pair<MPI_Request*, char*> > _RecvRequests;
+    std::queue<char*> _recvBuffers;
+    MyLock _l;
+#define MAX_RECV_QUEUE 4
 
     int RTS::ProcCount(){
 	int nProcs=1;
@@ -243,8 +238,6 @@ namespace amrex{
 	numaAware= (env!=NULL);
 	if(numaAware){ //the process covers multiple NUMA nodes
 	    numa_nodes= hw.numa_per_node;
-	    mspaces= new mspace[numa_nodes];
-	    segment_ptrs= new void*[numa_nodes];
 	    int worker_per_numa = _nWrks / numa_nodes;
 	    int remainder= _nWrks % numa_nodes;
 	    int r=0;
@@ -264,9 +257,6 @@ namespace amrex{
 		localID++;
 		if(localID==0){
 		    domNo++;
-		    segment_ptrs[domNo] = new char[SEGMENT_SIZE];
-		    mspaces[domNo] = create_mspace_with_base(segment_ptrs[domNo], SEGMENT_SIZE, 1);
-		    mspace_set_footprint_limit(mspaces[domNo], SEGMENT_SIZE);
 		}
 		CPU_ZERO(&cpuset);
 		CPU_SET(base+localID, &cpuset);
@@ -292,11 +282,6 @@ namespace amrex{
 	    numa_nodes= 1;
 	    _stopSignal= new int*[numa_nodes];
 	    dom= new RtsDomain[1];
-	    mspaces= new mspace[1];
-	    segment_ptrs= new void*[1];
-	    segment_ptrs[0]= new char[SEGMENT_SIZE];
-	    mspaces[0] = create_mspace_with_base(segment_ptrs[0], SEGMENT_SIZE, 1);
-	    mspace_set_footprint_limit(mspaces[0], SEGMENT_SIZE);
 
 	    //create a single list of persistent threads and set the thread affinity 
 	    cpu_set_t cpuset;
@@ -344,15 +329,12 @@ namespace amrex{
 	    }
 	    if(alldone) break;
 	}
-	for(int i=0; i<numa_nodes; i++)
-	    free(segment_ptrs[i]);
-	free(segment_ptrs);
 	free(_stopSignal);
-	free(mspaces);
     }
 
     void RTS::Run(void* taskgraph){
 	_DedicatedScheduler= getenv("DEDICATED_SCHEDULER");
+	char* env= getenv("MAX_MSG_SIZE");
 	//the master thread distributes tasks to workers
 	graph= (AbstractTaskGraph<Task>*)taskgraph;
 	//visit all initial tasks 
@@ -376,10 +358,13 @@ namespace amrex{
 	}
 	bool keepRunning=true;
 	//allocate a static buffer for incoming messages
-	size_t max_buf_size=SEGMENT_SIZE/16;
-	char* env= getenv("MAX_MSG_SIZE");
+	size_t max_buf_size=2<<24;
 	if(env) max_buf_size= atoi(env);
-	char* _recvbuffer= new char[max_buf_size];
+	for(int i=0; i< MAX_RECV_QUEUE; i++){
+	    char* _recvBuffer= new char[max_buf_size];
+	    _recvBuffers.push(_recvBuffer);
+	}
+
 	dom[0]._TaskBuffers[0].SetNoLoad();
 	while (keepRunning){
 	    //Handle communication
@@ -397,9 +382,61 @@ namespace amrex{
 				    Task* t= graph->LocateTask(name);
 				    t->GetInputs().push_back(msg->GetSource(), msg, msg->GetTag());
 				}
-				else dom[d]._MsgQueue.push(msg); //Recipient has not been created
+				else{ //Recipient is either on a remote node or has not been created
+				    int destRank= graph->FindProcessAssociation(name);
+				    if(destRank== MyProc()) dom[d]._MsgQueue.push(msg);  //keep in local message queue since recipient task has not been created
+				    else {//remote node
+					MPI_Request* req= new MPI_Request;
+					MPI_Isend(msg->SerializeData(), msg->GetSerializedSize(), MPI_CHAR, destRank, 0, MPI_COMM_WORLD, req);
+					_SendRequests.push(std::pair<MPI_Request*, Data*>(req, msg));
+				    }
+				}
 			    }
 			}
+		    }
+		    //prepost receives
+		    if(_RecvRequests.size() < MAX_RECV_QUEUE){
+			MPI_Request* req= new MPI_Request;
+			char* _recvBuffer=NULL;
+			if(_recvBuffers.size()){
+			    _recvBuffer= _recvBuffers.front();
+			    _recvBuffers.pop();
+			}else _recvBuffer= new char[max_buf_size];
+			MPI_Irecv(_recvBuffer, max_buf_size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, req);
+			_RecvRequests.push(std::pair<MPI_Request*, char*>(req, _recvBuffer));
+		    }
+		    //check send status
+		    int nSendRequests= _SendRequests.size();
+		    for(int i=0; i<nSendRequests; i++){
+			int done=0;
+			std::pair<MPI_Request*, Data*> p= _SendRequests.front();
+			MPI_Request *req= p.first;
+			_SendRequests.pop();
+			MPI_Test(req, &done, MPI_STATUS_IGNORE);
+			if(done){
+			    Data* d= p.second;  
+			    d->Free();
+			    free(req);
+			}else _SendRequests.push(p);
+		    }
+		    //check recv status
+		    int nRecvRequests= _RecvRequests.size();
+		    for(int i=0; i<nRecvRequests; i++){
+			int done=0;
+			std::pair<MPI_Request*, char*> p= _RecvRequests.front();
+			MPI_Request *req= p.first;
+			_RecvRequests.pop();
+			MPI_Test(req, &done, MPI_STATUS_IGNORE);
+			if(done){
+			    Data* msg= new Data(p.second);  //deserialize
+			    TaskName name= msg->GetRecipient();
+			    TaskName src= msg->GetSource();
+			    Task* t= graph->LocateTask(name);
+			    if(t){
+				t->GetInputs().push_back(msg->GetSource(), msg, msg->GetTag());
+			    }else dom[0]._MsgQueue.push(msg); 
+			    free(req);
+			}else _RecvRequests.push(p);
 		    }
 		}else{
 		}
@@ -416,7 +453,6 @@ namespace amrex{
 			}else{
 			    dom[d]._WaitingQueue.push(t);
 			}
-
 		    }
 		}
 	    }
@@ -513,8 +549,20 @@ namespace amrex{
 		    }
 		}
 	    }
+	}//end while (keepRunning)
+	//cancel all unused preposted requests
+	while(_SendRequests.size()){
+	    MPI_Cancel(_SendRequests.front().first);
+	    free(_SendRequests.front().first);
+	    free(_SendRequests.front().second);
+	    _SendRequests.pop();
 	}
-	free(_recvbuffer);
+
+	//free recv buffers if any left
+	while(_recvBuffers.size()){
+	    free(_recvBuffers.front());
+	    _recvBuffers.pop();
+	}
     }
 
     const double kMicro = 1.0e-6;
