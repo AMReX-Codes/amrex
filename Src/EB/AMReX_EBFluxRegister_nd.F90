3e99b9ea51ebc6346e0b61e8cb46f8342a056085
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-09-21 17:47:46 -0700

    yet another flux register

--- Src/EB/AMReX_EBFluxRegister_nd.F90
+++ Src/EB/AMReX_EBFluxRegister_nd.F90
@@ -1,11 +1,10 @@
 module amrex_eb_flux_reg_nd_module
   use amrex_fort_module, only : amrex_real
+  use amrex_ya_flux_reg_nd_module, only : crse_cell, crse_fine_boundary_cell, fine_cell
   implicit none
-  private
 
-  integer, parameter, public :: crse_cell = 0
-  integer, parameter, public :: crse_fine_boundary_cell = 1
-  integer, parameter, public :: fine_cell = 2
+  public
+
   real(amrex_real), parameter, public :: reredistribution_threshold = 1.d-4
 
 end module amrex_eb_flux_reg_nd_module
