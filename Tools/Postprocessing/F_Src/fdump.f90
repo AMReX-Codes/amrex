3d469e56245aa61ad225dba009a7b0c73d678a2d
Max Katz <maxpkatz@gmail.com>
2017-09-25 01:37:52 -0400

    Add index options to fdump

--- Tools/Postprocessing/F_Src/fdump.f90
+++ Tools/Postprocessing/F_Src/fdump.f90
@@ -44,12 +44,16 @@ program fdump
   character(len=256) :: minval_str, maxval_str
   real(kind=dp_t) :: minval, maxval
 
+  character(len=256) :: idx_str
+  integer :: idx(3)
+
   integer :: dm
   type(box) :: bx_a
 
   minval = -1.d200
   maxval = 1.d200
 
+  idx(:) = -1
 
   !---------------------------------------------------------------------------
   ! process the command line arguments
@@ -82,6 +86,21 @@ program fdump
         farg = farg + 1
         call get_command_argument(farg, value = maxval_str)
         read(maxval_str, *) maxval
+
+     case ('--i')
+        farg = farg + 1
+        call get_command_argument(farg, value = idx_str)
+        read(idx_str, *) idx(1)
+
+     case ('--j')
+        farg = farg + 1
+        call get_command_argument(farg, value = idx_str)
+        read(idx_str, *) idx(2)
+
+     case ('--k')
+        farg = farg + 1
+        call get_command_argument(farg, value = idx_str)
+        read(idx_str, *) idx(3)
 
      case default
         exit
@@ -139,8 +158,12 @@ program fdump
         endif
         
         do kk = lo_a(3)-ng, hi_a(3)+ng
+           if (idx(3) >= 0 .and. kk /= idx(3)) cycle
            do jj = lo_a(2)-ng, hi_a(2)+ng
+              if (idx(2) >= 0 .and. jj /= idx(2)) cycle
               do ii = lo_a(1)-ng, hi_a(1)+ng
+                 if (idx(1) >= 0 .and. ii /= idx(1)) cycle
+
                  if (p_a(ii,jj,kk,1) >= minval .and. p_a(ii,jj,kk,1) <= maxval) then
                     print *, ii, jj, kk, p_a(ii,jj,kk,1)
                  end if
