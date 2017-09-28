d5da75fadcfd7dff874c4178430f3d7cdec14c4a
vince <vebeckner@lbl.gov>
2017-09-28 14:37:17 -0700

    fix compiler warnings.

--- Src/Extern/amrdata/AMReX_AmrData.cpp
+++ Src/Extern/amrdata/AMReX_AmrData.cpp
@@ -865,7 +865,7 @@ bool AmrData::ReadNonPlotfileData(const string &filename, Amrvis::FileType filet
     dataGridsDefined[LevelZero].resize(nComp);
     dataGridsDefined[LevelZero][ComponentZero].resize(1);
     dataGrids[LevelZero][ComponentZero] = new MultiFab;
-    int nGrow(0);
+    int fabNGrow(0);
 
     Array<int> pMap(fabBoxArray.size());
     pMap[BoxZero] = iopNum;
@@ -875,7 +875,7 @@ bool AmrData::ReadNonPlotfileData(const string &filename, Amrvis::FileType filet
     Fab_noallocate.SetAlloc(false);
 
     dataGrids[LevelZero][ComponentZero]->define(fabBoxArray, dMap, NVarZero,
-                                                nGrow, Fab_noallocate);
+                                                fabNGrow, Fab_noallocate);
     if(ParallelDescriptor::IOProcessor()) {
       dataGrids[LevelZero][ComponentZero]->setFab(FabZero, newfab);
     }
@@ -885,7 +885,7 @@ bool AmrData::ReadNonPlotfileData(const string &filename, Amrvis::FileType filet
     for(int iComp = 1; iComp < nComp; ++iComp) {
       dataGrids[LevelZero][iComp] = new MultiFab;
       dataGrids[LevelZero][iComp]->define(fabBoxArray, dMap, NVarZero,
-                                          nGrow, Fab_noallocate);
+                                          fabNGrow, Fab_noallocate);
       if(ParallelDescriptor::IOProcessor()) {
         newfab = new FArrayBox;
         is.seekg(0, ios::beg);
@@ -927,7 +927,7 @@ bool AmrData::ReadNonPlotfileData(const string &filename, Amrvis::FileType filet
     fabBoxArray.resize(1);
     fabBoxArray.set(BoxZero, probDomain[LevelZero]);
 
-    int nGrow(0);
+    int mfNGrow(0);
     const int N(64);
     char fabname[N];  // arbitrarily
     plotVars.resize(nComp);
@@ -954,7 +954,7 @@ bool AmrData::ReadNonPlotfileData(const string &filename, Amrvis::FileType filet
       dataGridsDefined[LevelZero][iComp].resize(1, false);
       dataGrids[LevelZero][iComp] = new MultiFab;
       dataGrids[LevelZero][iComp]->define(levelZeroBoxArray, dMap, NVarZero,
-                                          nGrow, Fab_noallocate);
+                                          mfNGrow, Fab_noallocate);
       if(ParallelDescriptor::IOProcessor()) {
         FArrayBox *newfab = new FArrayBox(probDomain[LevelZero], 1);
         Real levelZeroValue, zvMin, zvMax;
@@ -1005,7 +1005,7 @@ bool AmrData::ReadNonPlotfileData(const string &filename, Amrvis::FileType filet
 	const BoxArray& ba = visMF[LevelOne][currentVisMF]->boxArray();
 	if (!dmap) dmap.reset(new DistributionMapping(ba));
         int iComp(currentIndexComp);
-        nGrow = visMF[LevelOne][currentVisMF]->nGrow();
+        mfNGrow = visMF[LevelOne][currentVisMF]->nGrow();
         currentIndexComp += visMF[LevelOne][currentVisMF]->nComp();
         for(int currentVisMFComponent(0); iComp < currentIndexComp; ++iComp) {
           // make single component multifabs for level one
@@ -1286,32 +1286,32 @@ void AmrData::FillVar(MultiFab &destMultiFab, int finestFillLevel,
       if(destMultiFab.DistributionMap()[currentIndex] != myProc) {
 	continue;
       }
-      for(int currentLevel(0); currentLevel <= finestFillLevel; ++currentLevel) {
+      for(int currLevel(0); currLevel <= finestFillLevel; ++currLevel) {
         for(int currentBox(0);
-            currentBox < fillBoxId[currentIndex][currentLevel].size();
+            currentBox < fillBoxId[currentIndex][currLevel].size();
             ++currentBox)
         {
             Box tempCoarseBox(
-		       fillBoxId[currentIndex][currentLevel][currentBox][0].box());
+		       fillBoxId[currentIndex][currLevel][currentBox][0].box());
             FArrayBox tempCoarseDestFab(tempCoarseBox, 1);
             tempCoarseDestFab.setVal(1.e30);
-            multiFabCopyDesc.FillFab(stateDataMFId[currentLevel],
-			  fillBoxId[currentIndex][currentLevel][currentBox][0],
+            multiFabCopyDesc.FillFab(stateDataMFId[currLevel],
+			  fillBoxId[currentIndex][currLevel][currentBox][0],
 			  tempCoarseDestFab);
 
-            Box intersectDestBox(savedFineBox[currentIndex][currentLevel][currentBox]);
+            Box intersectDestBox(savedFineBox[currentIndex][currLevel][currentBox]);
             intersectDestBox &= destMultiFab[currentIndex].box();
 
             const BoxArray &filledBoxes =
-                fillBoxIdBAs[currentIndex][currentLevel][currentBox][0];
+                fillBoxIdBAs[currentIndex][currLevel][currentBox][0];
             BoxArray fboxes(filledBoxes);
             FArrayBox *copyFromThisFab;
             const BoxArray *copyFromTheseBoxes;
             FArrayBox tempCurrentFillPatchedFab;
 
             if(intersectDestBox.ok()) {
-              if(currentLevel != finestFillLevel) {
-                fboxes.refine(cumulativeRefRatios[currentLevel]);
+              if(currLevel != finestFillLevel) {
+                fboxes.refine(cumulativeRefRatios[currLevel]);
                 // Interpolate up to fine patch.
                 tempCurrentFillPatchedFab.resize(intersectDestBox, nFillComps);
                 tempCurrentFillPatchedFab.setVal(1.e30);
@@ -1319,7 +1319,7 @@ void AmrData::FillVar(MultiFab &destMultiFab, int finestFillLevel,
 		BL_ASSERT(tempCoarseDestFab.box().ok());
 		PcInterp(tempCurrentFillPatchedFab,
 			 tempCoarseDestFab, intersectDestBox,
-			 cumulativeRefRatios[currentLevel]);
+			 cumulativeRefRatios[currLevel]);
                 copyFromThisFab = &tempCurrentFillPatchedFab;
                 copyFromTheseBoxes = &fboxes;
               } else {
@@ -1508,35 +1508,35 @@ void AmrData::FillVar(Array<FArrayBox *> &destFabs, const Array<Box> &destBoxes,
 
 
     for(int currentIndex = 0; currentIndex < destBoxes.size(); ++currentIndex) {
-      for(int currentLevel = 0; currentLevel <= finestFillLevel; ++currentLevel) {
+      for(int currLevel = 0; currLevel <= finestFillLevel; ++currLevel) {
 	if(myproc != procWithFabs) {
 	  break;
 	}
         for(int currentBox(0);
-            currentBox < fillBoxId[currentIndex][currentLevel].size();
+            currentBox < fillBoxId[currentIndex][currLevel].size();
             ++currentBox)
         {
             Box tempCoarseBox(
-		       fillBoxId[currentIndex][currentLevel][currentBox][0].box());
+		       fillBoxId[currentIndex][currLevel][currentBox][0].box());
             FArrayBox tempCoarseDestFab(tempCoarseBox, numFillComps);
             tempCoarseDestFab.setVal(1.e30);
-            multiFabCopyDesc.FillFab(stateDataMFId[currentLevel],
-			  fillBoxId[currentIndex][currentLevel][currentBox][0],
+            multiFabCopyDesc.FillFab(stateDataMFId[currLevel],
+			  fillBoxId[currentIndex][currLevel][currentBox][0],
 			  tempCoarseDestFab);
 
-            Box intersectDestBox(savedFineBox[currentIndex][currentLevel][currentBox]);
+            Box intersectDestBox(savedFineBox[currentIndex][currLevel][currentBox]);
             intersectDestBox &= destFabs[currentIndex]->box();
 
             const BoxArray &filledBoxes =
-                fillBoxIdBAs[currentIndex][currentLevel][currentBox][0];
+                fillBoxIdBAs[currentIndex][currLevel][currentBox][0];
             BoxArray fboxes(filledBoxes);
             FArrayBox *copyFromThisFab;
             const BoxArray *copyFromTheseBoxes;
             FArrayBox tempCurrentFillPatchedFab;
 
             if(intersectDestBox.ok()) {
-              if(currentLevel != finestFillLevel) {
-                    fboxes.refine(cumulativeRefRatios[currentLevel]);
+              if(currLevel != finestFillLevel) {
+                    fboxes.refine(cumulativeRefRatios[currLevel]);
                     // Interpolate up to fine patch.
                     tempCurrentFillPatchedFab.resize(intersectDestBox, numFillComps);
                     tempCurrentFillPatchedFab.setVal(1.e30);
@@ -1545,7 +1545,7 @@ void AmrData::FillVar(Array<FArrayBox *> &destFabs, const Array<Box> &destBoxes,
 		    PcInterp(tempCurrentFillPatchedFab,
 			     tempCoarseDestFab,
 			     intersectDestBox,
-			     cumulativeRefRatios[currentLevel]);
+			     cumulativeRefRatios[currLevel]);
                     copyFromThisFab = &tempCurrentFillPatchedFab;
                     copyFromTheseBoxes = &fboxes;
               } else {
@@ -1744,9 +1744,9 @@ void AmrData::FlushGrids(int componentIndex) {
     {
       BoxArray ba = dataGrids[lev][componentIndex]->boxArray();
       DistributionMapping dm = dataGrids[lev][componentIndex]->DistributionMap();
-      int nGrow = dataGrids[lev][componentIndex]->nGrow();
+      int flushNGrow = dataGrids[lev][componentIndex]->nGrow();
       delete dataGrids[lev][componentIndex];
-      dataGrids[lev][componentIndex] = new MultiFab(ba, dm, 1, nGrow, Fab_noallocate);
+      dataGrids[lev][componentIndex] = new MultiFab(ba, dm, 1, flushNGrow, Fab_noallocate);
       for(MFIter mfi(*dataGrids[lev][componentIndex]); mfi.isValid(); ++mfi) {
          dataGridsDefined[lev][componentIndex][mfi.index()] = false;
       }
@@ -2043,7 +2043,7 @@ void AmrData::PcInterp(FArrayBox &fine, const FArrayBox &crse,
 // ---------------------------------------------------------------
 FArrayBox *AmrData::ReadGrid(std::istream &is, int numVar) {
    long i, gstep;
-   Real time;
+   Real timeIn;
    static int gridCount(0);
    Box gbox;
    int glev;
@@ -2055,9 +2055,9 @@ FArrayBox *AmrData::ReadGrid(std::istream &is, int numVar) {
    VSHOWVAL(verbose, gbox)
    VSHOWVAL(verbose, glev)
 
-   is >> gstep >> time;
+   is >> gstep >> timeIn;
    VSHOWVAL(verbose, gstep)
-   VSHOWVAL(verbose, time)
+   VSHOWVAL(verbose, timeIn)
 
    for(i = 0; i < BL_SPACEDIM; ++i) {
      Real xlo, xhi;
