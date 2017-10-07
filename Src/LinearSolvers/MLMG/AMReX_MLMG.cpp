d3fcbb948196f1d3eed0708c159898e0ba3c20b9
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-10-06 17:12:30 -0700

    MLMG::solve WIP

--- Src/LinearSolvers/MLMG/AMReX_MLMG.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLMG.cpp
@@ -1,5 +1,6 @@
 
 #include <AMReX_MLMG.H>
+#include <AMReX_MultiFabUtil.H>
 
 namespace amrex {
 
@@ -11,10 +12,22 @@ MLMG::~MLMG ()
 {}
 
 void
-MLMG::solve (const Vector<MultiFab*>& sol, const Vector<MultiFab const*>& rhs,
+MLMG::solve (const Vector<MultiFab*>& sol, const Vector<MultiFab*>& rhs,
              Real a_tol_real, Real a_tol_abs)
 {
     AMREX_ASSERT(sol[0]->nGrow() > 0);
+
+    const int namrlevs = m_lp.NAMRLevels();
+    AMREX_ASSERT(namrlevs <= sol.size());
+    AMREX_ASSERT(namrlevs <= rhs.size());
+    
+    const auto& amrrr = m_lp.AMRRefRatio();
+
+    for (int falev = namrlevs-1; falev > 0; --falev)
+    {
+        amrex::average_down(*sol[falev], *sol[falev-1], 0, 1, amrrr[falev-1]);
+        amrex::average_down(*rhs[falev], *rhs[falev-1], 0, 1, amrrr[falev-1]);
+    }
 }
 
 }
