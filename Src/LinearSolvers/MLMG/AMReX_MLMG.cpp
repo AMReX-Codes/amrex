f1bd3901f0a35926b32e6c18ccef982d64547463
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-10-08 12:06:21 -0700

    MLMG: notes

--- Src/LinearSolvers/MLMG/AMReX_MLMG.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLMG.cpp
@@ -5,31 +5,33 @@
 namespace amrex {
 
 MLMG::MLMG (MLLinOp& a_lp)
-    : linop(a_lp)
+    : linop(a_lp),
+      namrlevs(a_lp.NAMRLevels()),
+      finest_amr_lev(a_lp.NAMRLevels()-1)
 {}
 
 MLMG::~MLMG ()
 {}
 
 void
-MLMG::solve (const Vector<MultiFab*>& sol, const Vector<MultiFab*>& rhs,
+MLMG::solve (const Vector<MultiFab*>& sol, const Vector<MultiFab const*>& a_rhs,
              Real a_tol_real, Real a_tol_abs)
 {
     AMREX_ASSERT(sol[0]->nGrow() > 0);
+    AMREX_ASSERT(namrlevs <= sol.size());
+    AMREX_ASSERT(namrlevs <= a_rhs.size());
 
     linop.prepareForSolve();
 
-    const int namrlevs = linop.NAMRLevels();
-    const int finest_amr_lev = namrlevs-1;
-    AMREX_ASSERT(namrlevs <= sol.size());
-    AMREX_ASSERT(namrlevs <= rhs.size());
-
-    Array<MultiFab> res(namrlevs);
+    rhs.resize(namrlevs);
+    res.resize(namrlevs);
     for (int alev = 0; alev < namrlevs; ++alev)
     {
-        const auto& ba = rhs[alev]->boxArray();
-        const auto& dm = rhs[alev]->DistributionMap();
+        const auto& ba = a_rhs[alev]->boxArray();
+        const auto& dm = a_rhs[alev]->DistributionMap();
+        rhs[alev].define(ba, dm, 1, 0);
         res[alev].define(ba, dm, 1, 0);
+        MultiFab::Copy(rhs[alev], *a_rhs[alev], 0, 0, 1, 0);
     }
     
     const auto& amrrr = linop.AMRRefRatio();
@@ -37,7 +39,7 @@ MLMG::solve (const Vector<MultiFab*>& sol, const Vector<MultiFab*>& rhs,
     for (int falev = finest_amr_lev; falev > 0; --falev)
     {
         amrex::average_down(*sol[falev], *sol[falev-1], 0, 1, amrrr[falev-1]);
-        amrex::average_down(*rhs[falev], *rhs[falev-1], 0, 1, amrrr[falev-1]);
+        amrex::average_down( rhs[falev],  rhs[falev-1], 0, 1, amrrr[falev-1]);
     }
 
     //
@@ -85,28 +87,87 @@ MLMG::solve (const Vector<MultiFab*>& sol, const Vector<MultiFab*>& rhs,
     {
         for (int iter = 0; iter < max_iters; ++iter)
         {
-            // OneSolve
+            oneIter();
+
+            // test convergence
         }
     }
 }
 
-}
-
-#if 0
-
-void OneSolve ()
+void
+MLMG::oneIter ()
 {
-    const int namrlevs = linop.NAMRLevels();
-    const int finest_amr_lev = namrlevs-1;
+    // if converged?
+    //    return
 
-    uu = 0;
-    uu_hold = 0;
+    // uu: correction
 
-    // Down the V-cycle
-    for (int alev = finest_amr_lev; alev >= 0; --alev)
+
+    for (int alev = finest_amr_lev; alev > 0; --alev)
     {
+        // Update uu[alev] with res[alev] as rhs
+        // Example: mg.f90
+        // uu = 0
+        miniCycle ();
+
+        // Add sol[n] += cor[n]
+
+        // This can be put in a function.
+        // compute residual on the coarse amrlevel
+        // compute current level residual (using correction)
+        // update crse residual with crse/fine residual and restriction of fine residual
+
+        // uu_hold = uu // save it for the up cycle
+        // zero uu[alev] ???  don't think this is needed because of interpolation going up.
+    }
+
+    // coarest amr level
+    {    
+        // enforce solvability if appropriate
+        // Update uu[0] with res[0] as rhs
+        mgCycle (); // Example: mg.f90
         
+        // Add sol[0] += cor[0]
+    }
+
+    for (int alev = 1; alev <= finest_amr_lev; ++alev)
+    {
+        // interpolate uu[alev] from uu[alev-1] && uu_hold[alev-1]
+
+        // soln[alev] += uu
+
+        // if (alev < finest_amr_lev) call plus_plus(uu_hold(n), uu(n), nghost(uu(n)))
+
+        // This can be put in a function
+        // Interpolate uu[alev-1] to supply boundary conditions for new residual calculation
+        // compute defect: res = res - L(uu)
+
+        // uu = 0
+        minicycle ();
+     
+        // soln += uu
+    }
+
+    for (int alev = finest_amr_lev; alev > 0; --alev)
+    {
+        // restrict sol[alev] to sol[alev-1]
+    }
+
+    for (int alev = 1; alev <= finest_amr_lev; ++alev)
+    {
+        // Interpolate soln to supply boundary conditions 
     }
 }
 
-#endif
+void
+MLMG::miniCycle ()
+{
+}
+
+void
+MLMG::mgCycle ()
+{
+}
+
+}
+
