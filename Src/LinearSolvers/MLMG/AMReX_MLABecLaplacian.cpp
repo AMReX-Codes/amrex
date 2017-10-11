6e8930d82915d186e5dbaa5eb7fdee0c1f6c696f
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-10-11 15:35:38 -0700

    MLMG: Reflux

--- Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
@@ -291,9 +291,9 @@ MLABecLaplacian::Fsmooth (int amrlev, int mglev, MultiFab& sol, const MultiFab& 
 }
 
 void
-MLABecLaplacian::FCrseFlux (int amrlev, const MFIter& mfi,
-                            std::array<FArrayBox,AMREX_SPACEDIM>& flux,
-                            const FArrayBox& sol) const
+MLABecLaplacian::FFlux (int amrlev, const MFIter& mfi,
+                        std::array<FArrayBox,AMREX_SPACEDIM>& flux,
+                        const FArrayBox& sol, const int face_only) const
 {
     const int mglev = 0;
     AMREX_D_TERM(const auto& bx = m_b_coeffs[amrlev][mglev][0][mfi];,
@@ -302,15 +302,15 @@ MLABecLaplacian::FCrseFlux (int amrlev, const MFIter& mfi,
     const Box& box = mfi.tilebox();
     const Real* dxinv = m_geom[amrlev][mglev].InvCellSize();
 
-    amrex_mlabeclap_crseflux(BL_TO_FORTRAN_BOX(box),
-                             AMREX_D_DECL(BL_TO_FORTRAN_ANYD(flux[0]),
-                                          BL_TO_FORTRAN_ANYD(flux[1]),
-                                          BL_TO_FORTRAN_ANYD(flux[2])),
-                             BL_TO_FORTRAN_ANYD(sol),
-                             AMREX_D_DECL(BL_TO_FORTRAN_ANYD(bx),
-                                          BL_TO_FORTRAN_ANYD(by),
-                                          BL_TO_FORTRAN_ANYD(bz)),
-                              dxinv, m_b_scalar);
+    amrex_mlabeclap_flux(BL_TO_FORTRAN_BOX(box),
+                         AMREX_D_DECL(BL_TO_FORTRAN_ANYD(flux[0]),
+                                      BL_TO_FORTRAN_ANYD(flux[1]),
+                                      BL_TO_FORTRAN_ANYD(flux[2])),
+                         BL_TO_FORTRAN_ANYD(sol),
+                         AMREX_D_DECL(BL_TO_FORTRAN_ANYD(bx),
+                                      BL_TO_FORTRAN_ANYD(by),
+                                      BL_TO_FORTRAN_ANYD(bz)),
+                         dxinv, m_b_scalar, face_only);
 }
 
 }
