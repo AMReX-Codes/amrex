709bbc49208aa9eaa125c8c8690d900cecb8daa6
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-10-20 16:34:18 -0700

    improve amrex_array_to_vector.sh

--- Tools/Migration/amrex_array_to_vector.sh
+++ Tools/Migration/amrex_array_to_vector.sh
@@ -2,7 +2,7 @@
 
 find . -type d \( -name .git -o -path ./Tools/Migration -o -path ./Docs/Migration \) -prune -o -type f \( -name "*.H" -o -name "*.h" -o -name "*.cpp" -o -name "*.c" -o -name "*.cc" -o -name "*.tex" \) -exec grep -Iq . {} \; -exec sed -i 's/\(#include\s*\(<\|\"\)\s*\)'AMReX_Array\.H'\(\s*\(>\|\"\)\)/#include <AMReX_Vector\.H>/g' {} +
 
-find . -type d \( -name .git -o -path ./Tools/Migration -o -path ./Docs/Migration \) -prune -o -type f \( -name "*.H" -o -name "*.h" -o -name "*.cpp" -o -name "*.c" -o -name "*.cc" -o -name "*.tex" \) -exec grep -Iq . {} \; -exec sed -i 's/amrex::Array</amrex::Vector</g' {} +
+find . -type d \( -name .git -o -path ./Tools/Migration -o -path ./Docs/Migration \) -prune -o -type f \( -name "*.H" -o -name "*.h" -o -name "*.cpp" -o -name "*.c" -o -name "*.cc" -o -name "*.tex" \) -exec grep -Iq . {} \; -exec sed -i 's/amrex::Array\(\s*\)</amrex::Vector\1</g' {} +
 
 find . -type d \( -name .git -o -path ./Tools/Migration -o -path ./Docs/Migration \) -prune -o -type f \( -name "*.H" -o -name "*.h" -o -name "*.cpp" -o -name "*.c" -o -name "*.cc" -o -name "*.tex" \) -exec grep -Iq . {} \; -exec sed -i 's/\(\s\+\)Array\(\s*\)</\1Vector\2</g' {} +
 
