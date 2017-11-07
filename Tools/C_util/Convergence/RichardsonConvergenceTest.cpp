145c1a13f0ebca0bd83043bf87f9cd1d613fd384
Marc Day <MSDay@lbl.gov>
2017-11-06 18:22:35 -0800

    Lower limit error mag for computing order

--- Tools/C_util/Convergence/RichardsonConvergenceTest.cpp
+++ Tools/C_util/Convergence/RichardsonConvergenceTest.cpp
@@ -477,7 +477,7 @@ main (int   argc,
       Real order;
       Real finenorm = normsMedi[icomp];
       Real coarnorm = normsCoar[icomp];
-      if(std::abs(finenorm) > 1.0e-12)
+      if(std::abs(finenorm) > 1.0e-40)
       {
         order = log(std::abs(coarnorm/finenorm))/log(2.0);
         printOrder = true;
