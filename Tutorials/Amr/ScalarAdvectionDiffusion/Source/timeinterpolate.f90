6a5b7972f2b00f0f6e86d3defd9b7404b9a35054
dtgraves <dtgraves@lbl.gov>
2017-11-08 13:20:41 -0800

    finished coding time interpolation for TVD rk3 scheme.

--- Tutorials/Amr/ScalarAdvectionDiffusion/Source/timeinterpolate.f90
+++ Tutorials/Amr/ScalarAdvectionDiffusion/Source/timeinterpolate.f90
@@ -1,5 +1,6 @@
 
 
+
 subroutine timeinterpolaterk4(xi, lo, hi, &
      phi,  phi_lo, phi_hi, &
      old, old_lo, old_hi, &
@@ -64,3 +65,56 @@ subroutine timeinterpolaterk4(xi, lo, hi, &
   end do
 
 end subroutine timeinterpolaterk4
+
+
+subroutine timeinterpolaterk3(xi, lo, hi, &
+     phi,  phi_lo, phi_hi, &
+     old, old_lo, old_hi, &
+     k1 , k1_lo, k1_hi, &
+     k2 , k2_lo, k2_hi &
+     ) bind(C, name="timeinterpolaterk3")
+
+  use amrex_fort_module, only : amrex_real, dim=>bl_spacedim
+  implicit none
+  integer, intent(in) :: lo(3), hi(3)
+  integer, intent(in) :: phi_lo(3), phi_hi(3)
+  integer, intent(in) :: old_lo(3), old_hi(3)
+  integer, intent(in) :: k1_lo(3), k1_hi(3)
+  integer, intent(in) :: k2_lo(3), k2_hi(3)
+
+  double precision, intent(in) :: xi
+  double precision, intent(inout) :: phi(phi_lo(1):phi_hi(1), &
+                                         phi_lo(2):phi_hi(2), &
+                                         phi_lo(3):phi_hi(3))
+
+  double precision, intent(in)    :: old(old_lo(1):old_hi(1), &
+                                         old_lo(2):old_hi(2), &
+                                         old_lo(3):old_hi(3))
+
+  double precision, intent(in)    :: k1(k1_lo(1):k1_hi(1), &
+                                        k1_lo(2):k1_hi(2), &
+                                        k1_lo(3):k1_hi(3))
+  double precision, intent(in)    :: k2(k2_lo(2):k2_hi(2), &
+                                        k2_lo(2):k2_hi(2), &
+                                        k2_lo(3):k2_hi(3))
+
+  integer          :: i,j,k
+  double precision :: k_1, k_2,  squcoef,  phival
+  !$omp parallel do private(i,j,k,x,y,z,r2) collapse(2)
+  do k=lo(3),hi(3)
+     do j=lo(2),hi(2)
+        do i=lo(1),hi(1)
+
+           k_1 = k1(i,j,k)
+           k_2 = k2(i,j,k)
+           !straight outta fok and rosales
+           squcoef = 0.5d0*(k_2 - k_1)
+
+           phival = old(i,j,k) + xi*k_1 + xi*xi*squcoef 
+           phi(i,j,k) = phival
+
+        end do
+     end do
+  end do
+
+end subroutine timeinterpolaterk3
