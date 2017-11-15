b01636b4698ba47f5ee545a37d46b92fd61b16db
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-11-15 13:14:25 -0800

    F_Interfaces/LinearSolvers: WIP

--- Src/F_Interfaces/LinearSolvers/AMReX_linop_mod.F90
+++ Src/F_Interfaces/LinearSolvers/AMReX_linop_mod.F90
@@ -4,44 +4,83 @@ module amrex_linop_module
   implicit none
 
   private
-  public :: amrex_linop, amrex_linop_build, amrex_linop_destroy
+  public :: amrex_linop
 
   type, public :: amrex_linop
      logical     :: owner =.false.
      type(c_ptr) :: p = c_null_ptr
    contains
-     generic :: assignment(=) => amrex_linop_assign   ! shallow copy
-     procedure, private :: amrex_linop_assign
-#if !defined(__GFORTRAN__) || (__GNUC__ > 4)
-     final :: amrex_linop_destroy
-#endif
+     procedure :: set_maxorder             => amrex_linop_set_maxorder
+     procedure :: set_domain_bc            => amrex_linop_set_domain_bc
+     procedure :: set_bc_with_coarse_data  => amrex_linop_set_bc_with_coarse_data
+     procedure :: set_level_bc             => amrex_linop_set_level_bc
+     procedure, private :: amrex_linop_set_maxorder
+     procedure, private :: amrex_linop_set_domain_bc
+     procedure, private :: amrex_linop_set_bc_with_coarse_data
+     procedure, private :: amrex_linop_set_level_bc
   end type amrex_linop
+
+  ! interfaces to C++ functions
+
+  interface
+     subroutine amrex_fi_linop_set_maxorder (linop, ord) bind(c)
+       import
+       implicit none
+       type(c_ptr), value :: linop
+       integer(c_int), value :: ord
+     end subroutine amrex_fi_linop_set_maxorder
+
+     subroutine amrex_fi_linop_set_domain_bc (linop, lobc, hibc) bind(c)
+       import
+       implicit none
+       type(c_ptr), value :: linop
+       integer(c_int), intent(in) :: lobc(*), hibc(*)
+     end subroutine amrex_fi_linop_set_domain_bc
+
+     subroutine amrex_fi_linop_set_bc_with_coarse_data (linop, mf, crse_ratio) bind(c)
+       import
+       implicit none
+       type(c_ptr), value :: linop, mf
+       integer(c_int), value :: crse_ratio
+     end subroutine amrex_fi_linop_set_bc_with_coarse_data
+
+     subroutine amrex_fi_linop_set_level_bc (linop, lev, mf) bind(c)
+       import
+       implicit none
+       type(c_ptr), value :: linop, mf
+       integer(c_int), value :: lev
+     end subroutine amrex_fi_linop_set_level_bc
+  end interface
 
 contains
 
-  subroutine amrex_linop_assign (dst, src)
-    class(amrex_linop), intent(inout) :: dst
-    type (amrex_linop), intent(in   ) :: src
-    call amrex_linop_destroy(dst)
-    dst%owner = .false.
-    dst%p     = src%p
-  end subroutine amrex_linop_assign
+  subroutine amrex_linop_set_maxorder (this, ord)
+    class(amrex_linop), intent(inout) :: this
+    integer(c_int), intent(in) :: ord
+    call amrex_fi_linop_set_maxorder(this%p, ord)
+  end subroutine amrex_linop_set_maxorder
 
 
-  subroutine amrex_linop_build (linop)
-    type(amrex_linop), intent(inout) :: linop
-  end subroutine amrex_linop_build
+  subroutine amrex_linop_set_domain_bc (this, lobc, hibc)
+    class(amrex_linop), intent(inout) :: this
+    integer, dimension(amrex_spacedim), intent(in) :: lobc, hibc
+    call amrex_fi_linop_set_domain_bc(this%p, lobc, hibc)
+  end subroutine amrex_linop_set_domain_bc
 
 
-  subroutine amrex_linop_destroy (this)
-    type(amrex_linop), intent(inout) :: this
-    if (this%owner) then
-       if (c_associated(this%p)) then
-          ! call amrex_fi_delete_linop
-       end if
-    end if
-    this%owner = .false.
-    this%p = c_null_ptr
-  end subroutine amrex_linop_destroy
+  subroutine amrex_linop_set_bc_with_coarse_data (this, crse_bcdata, crse_ratio)
+    class(amrex_linop), intent(inout) :: this
+    type(amrex_multifab), intent(in) :: crse_bcdata
+    integer, intent(in) :: crse_ratio
+    call amrex_fi_linop_set_bc_with_coarse_data(this%p, crse_bcdata%p, crse_ratio)
+  end subroutine amrex_linop_set_bc_with_coarse_data
 
+
+  subroutine amrex_linop_set_level_bc (this, lev, bcdata)
+    class(amrex_linop), intent(inout) :: this
+    integer, intent(in) :: lev
+    type(amrex_multifab), intent(in) :: bcdata
+    call amrex_fi_linop_set_level_bc(this%p, lev, bcdata%p)
+  end subroutine amrex_linop_set_level_bc
+  
 end module amrex_linop_module
