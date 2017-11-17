c95c6d83191fade9c0ef76710593985803fb4e77
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-11-17 13:04:41 -0800

    implement amrex_abeclaplacian_module

--- Src/F_Interfaces/LinearSolvers/AMReX_linear_solver_mod.F90
+++ Src/F_Interfaces/LinearSolvers/AMReX_linear_solver_mod.F90
@@ -4,7 +4,7 @@ module amrex_linear_solver_module
   use amrex_lo_bctypes_module
   use amrex_linop_module
   use amrex_poisson_module
-  use amrex_abeclapacian_module
+  use amrex_abeclaplacian_module
   use amrex_multigrid_module
 
 end module amrex_linear_solver_module
