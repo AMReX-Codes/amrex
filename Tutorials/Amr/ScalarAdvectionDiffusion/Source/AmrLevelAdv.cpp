22b62209a9bd0125a7e324e24efe3a6f30fa3d44
dtgraves <dtgraves@lbl.gov>
2017-11-22 13:48:32 -0800

    we now have three versions of time interpolation.   for super smooth problems, the simply polynomial seems to work best.

--- Tutorials/Amr/ScalarAdvectionDiffusion/Source/AmrLevelAdv.cpp
+++ Tutorials/Amr/ScalarAdvectionDiffusion/Source/AmrLevelAdv.cpp
@@ -69,7 +69,7 @@ namespace amrex
         const int* lo      = box.loVect();
         const int* hi      = box.hiVect();
 
-        timeinterprk3_jbb(&a_stage, ARLIM_3D(lo), ARLIM_3D(hi),
+        timeinterprk3_pwm(&a_stage, ARLIM_3D(lo), ARLIM_3D(hi),
                           BL_TO_FORTRAN_3D(  phiC[mfi]),
                           BL_TO_FORTRAN_3D(  oldC[mfi]),
                           BL_TO_FORTRAN_3D(    k1[mfi]),
@@ -153,7 +153,7 @@ namespace amrex
         const int* lo      = box.loVect();
         const int* hi      = box.hiVect();
 
-        timeinterprk4_jbb(&a_stage, ARLIM_3D(lo), ARLIM_3D(hi),
+        timeinterprk4_pwm(&a_stage, ARLIM_3D(lo), ARLIM_3D(hi),
                           BL_TO_FORTRAN_3D(  phiC[mfi]),
                           BL_TO_FORTRAN_3D(  oldC[mfi]),
                           BL_TO_FORTRAN_3D(    k1[mfi]),
