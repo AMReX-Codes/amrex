d5fc770aae596a2b4643164a205931f73862e9ed
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-11-29 17:15:27 -0800

    average nodal coefficients from cell centers to edges

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLap_2d.F90
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLap_2d.F90
@@ -4,7 +4,31 @@ module amrex_mlnodelap_2d_module
   implicit none
 
   private
+  public :: amrex_mlndlap_sigma_cctoedge
 
 contains
+
+  subroutine amrex_mlndlap_sigma_cctoedge (xlo, xhi, ylo, yhi, sigx, sxlo, sxhi, sigy, sylo, syhi, &
+       sigcc, clo, chi) bind(c, name='amrex_mlndlap_sigma_cctoedge')
+    integer, dimension(2), intent(in) :: xlo, xhi, ylo, yhi, sxlo, sxhi, sylo, syhi, clo, chi
+    real(amrex_real), intent(inout) :: sigx (sxlo(1):sxhi(1),sxlo(2):sxhi(2))
+    real(amrex_real), intent(inout) :: sigy (sylo(1):syhi(1),sylo(2):syhi(2))
+    real(amrex_real), intent(in   ) :: sigcc( clo(1): chi(1), clo(2): chi(2))
+
+    integer :: i, j
+
+    do    j = xlo(2), xhi(2)
+       do i = xlo(1), xhi(1)
+          sigx(i,j) = 0.5d0*(sigcc(i,j-1)+sigcc(i,j))
+       end do
+    end do
+
+    do    j = ylo(2), yhi(2)
+       do i = ylo(1), yhi(1)
+          sigy(i,j) = 0.5d0*(sigcc(i-1,j)+sigcc(i,j))
+       end do
+    end do
+
+  end subroutine amrex_mlndlap_sigma_cctoedge
 
 end module amrex_mlnodelap_2d_module
--- Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian.cpp
@@ -1,8 +1,18 @@
 
+#include <limits>
+
 #include <AMReX_MLNodeLaplacian.H>
 #include <AMReX_MLNodeLap_F.H>
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 namespace amrex {
+
+namespace {
+    const Real bogus_value = std::numeric_limits<Real>::quiet_NaN();
+}
 
 MLNodeLaplacian::MLNodeLaplacian (const Vector<Geometry>& a_geom,
                                   const Vector<BoxArray>& a_grids,
@@ -34,7 +44,7 @@ MLNodeLaplacian::define (const Vector<Geometry>& a_geom,
     m_sigma.resize(m_num_amr_levels);
     for (int amrlev = 0; amrlev < m_num_amr_levels; ++amrlev)
     {
-        m_sigma.resize(m_num_mg_levels[amrlev]);
+        m_sigma[amrlev].resize(m_num_mg_levels[amrlev]);
         for (int mglev = 0; mglev < m_num_mg_levels[amrlev]; ++mglev)
         {
             for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
@@ -43,16 +53,49 @@ MLNodeLaplacian::define (const Vector<Geometry>& a_geom,
                 iv[idim] = 0;
                 const BoxArray& ba = amrex::convert(m_grids[amrlev][mglev], iv);
                 m_sigma[amrlev][mglev][idim].define(ba, m_dmap[amrlev][mglev], 1, 1);
+                m_sigma[amrlev][mglev][idim].setVal(bogus_value);
             }
         }
     }
 }
 
 void
-MLNodeLaplacian::setSigma (int amrlev, const std::array<MultiFab const*,AMREX_SPACEDIM>& a_sigma)
+MLNodeLaplacian::setSigma (int amrlev, const MultiFab& a_sigma)
 {
-    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-        MultiFab::Copy(m_sigma[amrlev][0][idim], *a_sigma[idim], 0, 0, 1, 0);
+    auto& nd_sigma = m_sigma[amrlev][0];
+
+    std::array<IntVect, AMREX_SPACEDIM> nodal{AMREX_D_DECL(nd_sigma[0].ixType().ixType(),
+                                                           nd_sigma[1].ixType().ixType(),
+                                                           nd_sigma[2].ixType().ixType())};
+    std::array<IntVect, AMREX_SPACEDIM> ngrow{AMREX_D_DECL(IntVect::TheDimensionVector(0),
+                                                           IntVect::TheDimensionVector(1),
+                                                           IntVect::TheDimensionVector(2))};
+
+    MultiFab cc_sigma(a_sigma.boxArray(), a_sigma.DistributionMap(), 1, 1);
+    cc_sigma.setVal(bogus_value);
+    MultiFab::Copy(cc_sigma, a_sigma, 0, 0, 1, 0);
+    cc_sigma.FillBoundary(m_geom[amrlev][0].periodicity());
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+    for (MFIter mfi(cc_sigma, true); mfi.isValid(); ++mfi)
+    {
+        AMREX_D_TERM(const Box& xbx = mfi.tilebox(nodal[0],ngrow[0]);,
+                     const Box& ybx = mfi.tilebox(nodal[1],ngrow[1]);,
+                     const Box& zbx = mfi.tilebox(nodal[2],ngrow[2]););
+        AMREX_D_TERM(FArrayBox& xfab = nd_sigma[0][mfi];,
+                     FArrayBox& yfab = nd_sigma[1][mfi];,
+                     FArrayBox& zfab = nd_sigma[2][mfi];);
+        const FArrayBox& ccfab = cc_sigma[mfi];
+
+        amrex_mlndlap_sigma_cctoedge(AMREX_D_DECL(BL_TO_FORTRAN_BOX(xbx),
+                                                  BL_TO_FORTRAN_BOX(ybx),
+                                                  BL_TO_FORTRAN_BOX(zbx)),
+                                     AMREX_D_DECL(BL_TO_FORTRAN_ANYD(xfab),
+                                                  BL_TO_FORTRAN_ANYD(yfab),
+                                                  BL_TO_FORTRAN_ANYD(zfab)),
+                                     BL_TO_FORTRAN_ANYD(ccfab));
     }
 }
 
