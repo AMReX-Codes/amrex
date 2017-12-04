176528a9498b17c22279af621a8abc1647f5413a
Dave Grote <grote1@llnl.gov>
2017-12-04 13:51:49 -0800

    Added WarpXPIC.py back in

--- Python/pywarpx/__init__.py
+++ Python/pywarpx/__init__.py
@@ -12,5 +12,6 @@ from .AMReX import AMReX
 #from .timestepper import TimeStepper
 from .PGroup import PGroup
 from .PGroup import PGroups
+from .WarpXPIC import WarpXPIC
 
 from ._libwarpx import add_particles
