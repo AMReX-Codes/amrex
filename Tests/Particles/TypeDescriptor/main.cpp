0487afe8c88da6d463f611ad9c3fea5df72e0881
atmyers <atmyers2@gmail.com>
2017-12-04 12:23:33 -0800

    also support long data; some reorganization.

--- Tests/Particles/TypeDescriptor/main.cpp
+++ Tests/Particles/TypeDescriptor/main.cpp
@@ -22,110 +22,107 @@ void readRealData(Real* data, std::size_t size, std::istream& is,
     return;
 }
 
-void writeIntData(const int* data, std::size_t size, std::ostream& os,
-                  const IntDescriptor& id = FPC::NativeIntDescriptor())
-{
-    if (id == FPC::NativeIntDescriptor())
-    {
-        os.write((char*) data, size*sizeof(int));
-    }
-    else if (id.numBytes() == 2)
-    {
-        writeIntData<std::int16_t>(data, size, os, id);
-    }
-    else if (id.numBytes() == 4)
-    {
-        writeIntData<std::int32_t>(data, size, os, id);
-    }
-    else if (id.numBytes() == 8)
-    {
-        writeIntData<std::int64_t>(data, size, os, id);
-    }
-    else {
-        amrex::Error("Don't know how to work with this integer type.");
-    }
-}
-
-void readIntData(int* data, std::size_t size, std::istream& is,
-                 const IntDescriptor& id)
-{
-    if (id == FPC::NativeIntDescriptor()) 
-    {
-        is.read((char*) data, size * id.numBytes());
-    } 
-    else if (id.numBytes() == 2)
-    {
-        readIntData<std::int16_t>(data, size, is, id);
-    }
-    else if (id.numBytes() == 4)
-    {
-        readIntData<std::int32_t>(data, size, is, id);
-    }
-    else if (id.numBytes() == 8)
-    {
-        readIntData<std::int64_t>(data, size, is, id);
-    }
-    else {
-        amrex::Error("Don't know how to work with this integer type.");
-    }
-}
-
 void testIntIO(const IntDescriptor& id_out) {
 
-  std::string data_file_name = "int_data.dat";
-  std::string header_file_name = "int_header_H";
+    std::string data_file_name   = "int_data.dat";
+    std::string header_file_name = "int_header_H";
+    
+    amrex::Vector<int> idata_out;
+    for (int i = 0; i < 100; ++i) {
+        idata_out.push_back(i);
+    }
+    
+    std::ofstream ofs;
+    ofs.open(data_file_name.c_str(), std::ios::out|std::ios::binary);
+    writeIntData(idata_out.data(), idata_out.size(), ofs, id_out);
+    ofs.close();
+    
+    ofs.open(header_file_name.c_str(), std::ios::out);
+    ofs << id_out << "\n";
+    ofs.close();
+    
+    IntDescriptor id_in;
+    std::ifstream ifs;
+    ifs.open(header_file_name.c_str(), std::ios::in);
+    ifs >> id_in;
+    ifs.close();
+    
+    AMREX_ALWAYS_ASSERT(id_out == id_in);
+    
+    amrex::Vector<int> idata_in(100);
+    ifs.open(data_file_name.c_str(), std::ios::in|std::ios::binary);
+    readIntData(idata_in.data(), idata_in.size(), ifs, id_in);
+    ifs.close();
+    
+    for (int i = 0; i < 100; ++i) {
+        AMREX_ALWAYS_ASSERT(idata_in[i] == idata_out[i]);
+    }
+}
 
-  amrex::Vector<int> idata_out;
-  for (int i = 0; i < 100; ++i) {
-      idata_out.push_back(i);
-  }
+void testLongIO(const IntDescriptor& id_out) {
 
-  std::ofstream ofs;
-  ofs.open(data_file_name.c_str(), std::ios::out|std::ios::binary);
-  writeIntData(idata_out.data(), idata_out.size(), ofs, id_out);
-  ofs.close();
-
-  ofs.open(header_file_name.c_str(), std::ios::out);
-  ofs << id_out << "\n";
-  ofs.close();
-
-  IntDescriptor id_in;
-  std::ifstream ifs;
-  ifs.open(header_file_name.c_str(), std::ios::in);
-  ifs >> id_in;
-  ifs.close();
-
-  AMREX_ALWAYS_ASSERT(id_out == id_in);
-
-  amrex::Vector<int> idata_in(100);
-  ifs.open(data_file_name.c_str(), std::ios::in|std::ios::binary);
-  readIntData(idata_in.data(), idata_in.size(), ifs, id_in);
-  ifs.close();
-
-  for (int i = 0; i < 100; ++i) {
-      AMREX_ALWAYS_ASSERT(idata_in[i] == idata_out[i]);
-  }
+    std::string data_file_name   = "long_data.dat";
+    std::string header_file_name = "long_header_H";
+    
+    amrex::Vector<long> idata_out;
+    for (int i = 0; i < 100; ++i) {
+        idata_out.push_back(i);
+    }
+    
+    std::ofstream ofs;
+    ofs.open(data_file_name.c_str(), std::ios::out|std::ios::binary);
+    writeLongData(idata_out.data(), idata_out.size(), ofs, id_out);
+    ofs.close();
+    
+    ofs.open(header_file_name.c_str(), std::ios::out);
+    ofs << id_out << "\n";
+    ofs.close();
+    
+    IntDescriptor id_in;
+    std::ifstream ifs;
+    ifs.open(header_file_name.c_str(), std::ios::in);
+    ifs >> id_in;
+    ifs.close();
+    
+    AMREX_ALWAYS_ASSERT(id_out == id_in);
+    
+    amrex::Vector<long> idata_in(100);
+    ifs.open(data_file_name.c_str(), std::ios::in|std::ios::binary);
+    readLongData(idata_in.data(), idata_in.size(), ifs, id_in);
+    ifs.close();
+    
+    for (int i = 0; i < 100; ++i) {
+        AMREX_ALWAYS_ASSERT(idata_in[i] == idata_out[i]);
+    }
 }
 
 int main(int argc, char* argv[])
 {
-  amrex::Initialize(argc,argv);
-
-  IntDescriptor little16(2, IntDescriptor::ReverseOrder);
-  IntDescriptor little32(4, IntDescriptor::ReverseOrder);
-  IntDescriptor little64(8, IntDescriptor::ReverseOrder);
-
-  IntDescriptor big16(2, IntDescriptor::NormalOrder);
-  IntDescriptor big32(4, IntDescriptor::NormalOrder);
-  IntDescriptor big64(8, IntDescriptor::NormalOrder);
-
-  testIntIO(little16);
-  testIntIO(little32);
-  testIntIO(little64);
-
-  testIntIO(big16);
-  testIntIO(big32);
-  testIntIO(big64);
-
-  amrex::Finalize();
+    amrex::Initialize(argc,argv);
+    
+    IntDescriptor little16(2, IntDescriptor::ReverseOrder);
+    IntDescriptor little32(4, IntDescriptor::ReverseOrder);
+    IntDescriptor little64(8, IntDescriptor::ReverseOrder);
+    
+    IntDescriptor big16(2, IntDescriptor::NormalOrder);
+    IntDescriptor big32(4, IntDescriptor::NormalOrder);
+    IntDescriptor big64(8, IntDescriptor::NormalOrder);
+    
+    testIntIO(little16);
+    testIntIO(little32);
+    testIntIO(little64);
+    
+    testIntIO(big16);
+    testIntIO(big32);
+    testIntIO(big64);
+    
+    testLongIO(little16);
+    testLongIO(little32);
+    testLongIO(little64);
+    
+    testLongIO(big16);
+    testLongIO(big32);
+    testLongIO(big64);
+    
+    amrex::Finalize();
 }
