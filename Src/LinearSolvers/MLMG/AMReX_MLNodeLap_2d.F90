54336b9aea3ab710c4139deda023cb350494c033
Weiqun Zhang <weiqunzhang@lbl.gov>
2017-12-16 14:56:19 -0800

    nodal projection: fix fine contribution

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLap_2d.F90
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLap_2d.F90
@@ -863,110 +863,82 @@ contains
     real(amrex_real), intent(in   ) :: vel(vlo(1):vhi(1),vlo(2):vhi(2),2)
     integer, intent(in) :: msk(mlo(1):mhi(1),mlo(2):mhi(2))
 
-    integer :: i, j, ii, jj, s
+    integer :: i, j, ii, jj, side
     real(amrex_real) :: facx, facy, facx_s, facy_s
-    real(amrex_real) :: Dfx(lo(1):hi(1))
-    real(amrex_real) :: Dfy(lo(2):hi(2))
-    real(amrex_real), parameter :: w1 = 1.d0/16.d0
-    real(amrex_real), parameter :: w2 = 1.d0/32.d0
-    real(amrex_real), parameter :: r1 = 0.25d0
-    real(amrex_real), parameter :: r2 = 0.125d0
+    real(amrex_real), parameter :: w = 0.125d0
+    real(amrex_real), parameter :: xtrfac = 3.d0
 
-    facx = 0.5d0*dxinv(1)
-    facy = 0.5d0*dxinv(2)
+    ! note that dxinv is fine dxinv
+    facx = 0.25d0*dxinv(1)
+    facy = 0.25d0*dxinv(2)
 
     ! note that lo = 2*clo and hi = 2*chi
     ! msk .ne. 0 means coarse/fine or external dirichlet boundary
 
-    do i = clo(1), chi(1), chi(1)-clo(1)
-       ii = 2*i
-       if (i .eq. clo(1)) then
-          s = 0
-          facx_s = facx
+    do side = 0, 1
+       if (side .eq. 0) then
+          j = clo(2)
+          jj = lo(2)
           facy_s = facy
        else
-          s = 1
-          facx_s = -facx
-          facy_s =  facy
+          j = chi(2)
+          jj = hi(2)
+          facy_s = -facy
        end if
 
-       do jj = lo(2), hi(2)
-          Dfy(jj) = 0.d0
+       do i = clo(1), chi(1)
+          ii = 2*i
           if (msk(ii,jj) .ne. 0) then
-             if (jj .lt. hi(2)) then
-                Dfy(jj) = Dfy(jj) + facx_s*vel(ii-s,jj,1) + facy_s*vel(ii-s,jj,2)
-             end if
-             if (jj .gt. lo(2)) then
-                Dfy(jj) = Dfy(jj) + facx_s*vel(ii-s,jj-1,1) - facy_s*vel(ii-s,jj-1,2)                
-             end if
-          end if
-       end do
-       
-       do j = clo(2), chi(2)
-          jj = 2*j
-          if (msk(ii,jj) .ne. 0) then
-             if (j .gt. clo(2)) then
-                if (msk(ii,jj-1) .ne. 0) then
-                   wgt(i,j) = wgt(i,j) + w2
-                   rhs(i,j) = rhs(i,j) + r2*Dfy(jj-1)
+             if (i.gt.clo(1)) then
+                if (msk(ii-1,jj) .ne. 0) then
+                   wgt(i,j) = wgt(i,j) + w
+                   rhs(i,j) = rhs(i,j) - facx   * (       vel(ii-1,jj,1) + vel(ii-2,jj,1)) &
+                        &              + facy_s * (xtrfac*vel(ii-1,jj,2) + vel(ii-2,jj,2))
                 end if
              end if
-             wgt(i,j) = wgt(i,j) + w1
-             rhs(i,j) = rhs(i,j) + r1*Dfy(jj)             
-             if (j .lt. chi(2)) then
-                if (msk(ii,jj+1) .ne. 0) then
-                   wgt(i,j) = wgt(i,j) + w2
-                   rhs(i,j) = rhs(i,j) + r2*Dfy(jj+1)
+             
+             if (i.lt.chi(1)) then
+                if (msk(ii+1,jj) .ne. 0) then
+                   wgt(i,j) = wgt(i,j) + w
+                   rhs(i,j) = rhs(i,j) + facx   * (       vel(ii,jj,1) + vel(ii+1,jj,1)) &
+                        &              + facy_s * (xtrfac*vel(ii,jj,2) + vel(ii+1,jj,2))
                 end if
              end if
           end if
        end do
     end do
 
-    do j = clo(2), chi(2), chi(2)-clo(2)
-       jj = 2*j
-       if (j .eq. clo(2)) then
-          s = 0
+    do side = 0, 1
+       if (side .eq. 0) then
+          i = clo(1)
+          ii = lo(1)
           facx_s = facx
-          facy_s = facy
        else
-          s =1
-          facx_s =  facx
-          facy_s = -facy
+          i = chi(1)
+          ii = hi(1)
+          facx_s = -facx
        end if
 
-       ! the corners have been taken care
-       do ii = lo(1)+1, hi(1)-1
-          Dfx(ii) = 0.d0
+       do j = clo(2), chi(2)
+          jj = 2*j
           if (msk(ii,jj) .ne. 0) then
-             Dfx(ii) = Dfx(ii) + facx_s*(-vel(ii-1,jj-s,1)+vel(ii,jj-s,1)) &
-                  &            + facy_s*( vel(ii-1,jj-s,2)+vel(ii,jj-s,2))
-          end if
-       end do
-
-       if (msk(lo(1),jj) .ne. 0) then
-          wgt(clo(1),j) = wgt(clo(1),j) + w2
-          rhs(clo(1),j) = rhs(clo(1),j) + r2*Dfx(lo(1)+1)
-       end if
-       do i = clo(1)+1, chi(1)-1
-          ii = 2*i
-          if (msk(ii,jj) .ne. 0) then
-             if (msk(ii-1,jj) .ne. 0) then
-                wgt(i,j) = wgt(i,j) + w2
-                rhs(i,j) = rhs(i,j) + r2*Dfx(ii-1)
+             if (j.gt.clo(2)) then
+                if (msk(ii,jj-1) .ne. 0) then
+                   wgt(i,j) = wgt(i,j) + w
+                   rhs(i,j) = rhs(i,j) + facx_s * (xtrfac*vel(ii,jj-1,1) + vel(ii,jj-2,1)) &
+                        &              - facy   * (       vel(ii,jj-1,2) + vel(ii,jj-2,2))
+                end if
              end if
-             wgt(i,j) = wgt(i,j) + w1
-             rhs(i,j) = rhs(i,j) + r1*Dfx(ii)
-             if (msk(ii+1,jj) .ne. 0) then
-                wgt(i,j) = wgt(i,j) + w2
-                rhs(i,j) = rhs(i,j) + r2*Dfx(ii+1)
+             
+             if (j.lt.chi(2)) then
+                if (msk(ii,jj+1) .ne. 0) then
+                   wgt(i,j) = wgt(i,j) + w
+                   rhs(i,j) = rhs(i,j) + facx_s * (xtrfac*vel(ii,jj,1) + vel(ii,jj+1,1)) &
+                        &              + facy   * (       vel(ii,jj,2) + vel(ii,jj+1,2))
+                end if
              end if
           end if
        end do
-       if (msk(hi(1),jj) .ne. 0) then
-          wgt(chi(1),j) = wgt(chi(1),j) + w2
-          rhs(chi(1),j) = rhs(chi(1),j) + r2*Dfx(hi(1)-1)
-       end if
     end do
 
     ! xxxxx what do we do at physical boundaries?
