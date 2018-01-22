1d85da3b256d49950e2e32acb77a1cdf6a6013b3
Andrew Myers <atmyers2@gmail.com>
2018-01-22 14:37:38 -0800

    override the default page width for the read the docs theme, which is optimized for mobile.

--- Docs/sphinx/source/conf.py
+++ Docs/sphinx/source/conf.py
@@ -103,7 +103,13 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
+        ],
+    }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
