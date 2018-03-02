b3f5199f311950c2cf15100f7118f9fa4776099d
Brandon Runnels <brunnels@uccs.edu>
2018-03-01 17:45:47 -0700

    Basic multicomponent MLMG solves working in 2D.

--- Src/LinearSolvers/MLMG/AMReX_MLLinOp_nd.F90
+++ Src/LinearSolvers/MLMG/AMReX_MLLinOp_nd.F90
@@ -34,7 +34,8 @@ contains
        mask, mlo, mhi, &
        cdir, bct, bcl, &
        bcval, blo, bhi, &
-       maxorder, dxinv, inhomog, nc) &
+       maxorder, dxinv, inhomog, &
+       nc) &
        bind(c,name='amrex_mllinop_apply_bc')
     integer, dimension(3), intent(in) :: lo, hi, hlo, hhi, mlo, mhi, blo, bhi
     integer, value, intent(in) :: cdir, bct, maxorder, inhomog, nc
@@ -186,7 +187,7 @@ contains
              do    j = lo(2), hi(2)
                 do i = lo(1), hi(1)
                    if (mask(i,j,lo(3)-1) .gt. 0) then
-                      phi(i,j,lo(3)-1) = sum(phi(i,j,lo(3):lo(3)+lenx)*coef(0:lenx))
+                      phi(i,j,lo(3)-1,n) = sum(phi(i,j,lo(3):lo(3)+lenx,n)*coef(0:lenx))
                       if (inhomogeneous) then
                          phi(i,j,lo(3)-1,n) = phi(i,j,lo(3)-1,n) + bcval(i,j,lo(3)-1,n)*coef(-1)
                       end if
@@ -215,167 +216,177 @@ contains
   end subroutine amrex_mllinop_apply_bc
 
 
-  subroutine amrex_mllinop_comp_interp_coef0 (lo, hi, den, dlo, dhi, mask, mlo, mhi, &
-       cdir, bct, bcl, maxorder, dxinv) bind(c,name='amrex_mllinop_comp_interp_coef0')
+  subroutine amrex_mllinop_comp_interp_coef0 (lo, hi, &
+       den, dlo, dhi, &
+       mask, mlo, mhi, &
+       cdir, bct, bcl, maxorder, dxinv, &
+       nc) bind(c,name='amrex_mllinop_comp_interp_coef0')
     integer, dimension(3), intent(in) :: lo, hi, dlo, dhi, mlo, mhi
-    integer, value, intent(in) :: cdir, bct, maxorder
+    integer, value, intent(in) :: cdir, bct, maxorder, nc
     real(amrex_real), value, intent(in) :: bcl
     real(amrex_real), intent(in) :: dxinv(3)
-    real(amrex_real), intent(inout) ::  den(dlo(1):dhi(1),dlo(2):dhi(2),dlo(3):dhi(3))
+    real(amrex_real), intent(inout) ::  den(dlo(1):dhi(1),dlo(2):dhi(2),dlo(3):dhi(3),nc)
     integer         , intent(in   ) :: mask(mlo(1):mhi(1),mlo(2):mhi(2),mlo(3):mhi(3))
 
-    integer :: i,j,k,idim,lenx,m
+    integer :: i,j,k,idim,lenx,m,n
     real(amrex_real) ::    x(-1:maxorder-2)
     real(amrex_real) :: coef(-1:maxorder-2)
     real(amrex_real), parameter :: xInt = -0.5D0
     real(amrex_real) :: c0
     
-    if (bct == LO_NEUMANN) then
+    do n = 1, nc
+       if (bct == LO_NEUMANN) then
 
-       select case (cdir)
-       case (xlo_dir)
-          do    k = lo(3), hi(3)
-             do j = lo(2), hi(2)
-                den(lo(1),j,k) = 1.d0
+          select case (cdir)
+          case (xlo_dir)
+             do    k = lo(3), hi(3)
+                do j = lo(2), hi(2)
+                   den(lo(1),j,k,n) = 1.d0
+                end do
              end do
-          end do
-       case (xhi_dir)
-          do    k = lo(3), hi(3)
-             do j = lo(2), hi(2)
-                den(hi(1),j,k) = 1.d0
+          case (xhi_dir)
+             do    k = lo(3), hi(3)
+                do j = lo(2), hi(2)
+                   den(hi(1),j,k,n) = 1.d0
+                end do
              end do
-          end do
 #if (AMREX_SPACEDIM >= 2)
-       case (ylo_dir)
-          do    k = lo(3), hi(3)
-             do i = lo(1), hi(1)
-                den(i,lo(2),k) = 1.d0
+          case (ylo_dir)
+             do    k = lo(3), hi(3)
+                do i = lo(1), hi(1)
+                   den(i,lo(2),k,n) = 1.d0
+                end do
              end do
-          end do
-       case (yhi_dir)
-          do    k = lo(3), hi(3)
-             do i = lo(1), hi(1)
-                den(i,hi(2),k) = 1.d0
+          case (yhi_dir)
+             do    k = lo(3), hi(3)
+                do i = lo(1), hi(1)
+                   den(i,hi(2),k,n) = 1.d0
+                end do
              end do
-          end do
 #if (AMREX_SPACEDIM == 3)
-       case (zlo_dir)
-          do    j = lo(2), hi(2)
-             do i = lo(1), hi(1)
-                den(i,j,lo(3)) = 1.d0
+          case (zlo_dir)
+             do    j = lo(2), hi(2)
+                do i = lo(1), hi(1)
+                   den(i,j,lo(3),n) = 1.d0
+                end do
              end do
-          end do
-       case (zhi_dir)
-          do    j = lo(2), hi(2)
-             do i = lo(1), hi(1)
-                den(i,j,hi(3)) = 1.d0
+          case (zhi_dir)
+             do    j = lo(2), hi(2)
+                do i = lo(1), hi(1)
+                   den(i,j,hi(3),n) = 1.d0
+                end do
              end do
-          end do
 #endif
 #endif
-       end select
+          end select
 
-    else if (bct == LO_REFLECT_ODD .or. bct == LO_DIRICHLET) then
+       else if (bct == LO_REFLECT_ODD .or. bct == LO_DIRICHLET) then
 
-       if (bct == LO_REFLECT_ODD) then
-          c0 = 1.d0
-       else
-          idim = mod(cdir,amrex_spacedim) + 1 ! cdir starts with 0; idim starts with 1
-          lenx = MIN(hi(idim)-lo(idim), maxorder-2)
-          
-          x(-1) = -bcl*dxinv(idim)
-          do m=0,maxorder-2
-             x(m) = m + 0.5D0
-          end do
-          
-          call polyInterpCoeff(xInt, x, lenx+2, coef)
+          if (bct == LO_REFLECT_ODD) then
+             c0 = 1.d0
+          else
+             idim = mod(cdir,amrex_spacedim) + 1 ! cdir starts with 0; idim starts with 1
+             lenx = MIN(hi(idim)-lo(idim), maxorder-2)
 
-          c0 = coef(0)
+             x(-1) = -bcl*dxinv(idim)
+             do m=0,maxorder-2
+                x(m) = m + 0.5D0
+             end do
+
+             call polyInterpCoeff(xInt, x, lenx+2, coef)
+
+             c0 = coef(0)
+          end if
+
+          select case (cdir)
+          case (xlo_dir)
+             do    k = lo(3), hi(3)
+                do j = lo(2), hi(2)
+                   if (mask(lo(1)-1,j,k) .gt. 0) then
+                      den(lo(1),j,k,n) = c0
+                   else
+                      den(lo(1),j,k,n) = 0.d0
+                   end if
+                end do
+             end do
+          case (xhi_dir)
+             do    k = lo(3), hi(3)
+                do j = lo(2), hi(2)
+                   if (mask(hi(1)+1,j,k) .gt. 0) then
+                      den(hi(1),j,k,n) = c0
+                   else
+                      den(hi(1),j,k,n) = 0.d0
+                   end if
+                end do
+             end do
+#if (AMREX_SPACEDIM >= 2)
+          case (ylo_dir)
+             do    k = lo(3), hi(3)
+                do i = lo(1), hi(1)
+                   if (mask(i,lo(2)-1,k) .gt. 0) then
+                      den(i,lo(2),k,n) = c0
+                   else
+                      den(i,lo(2),k,n) = 0.d0
+                   end if
+                end do
+             end do
+          case (yhi_dir)
+             do    k = lo(3), hi(3)
+                do i = lo(1), hi(1)
+                   if (mask(i,hi(2)+1,k) .gt. 0) then
+                      den(i,hi(2),k,n) = c0
+                   else
+                      den(i,hi(2),k,n) = 0.d0
+                   end if
+                end do
+             end do
+#if (AMREX_SPACEDIM == 3)
+          case (zlo_dir)
+             do    j = lo(2), hi(2)
+                do i = lo(1), hi(1)
+                   if (mask(i,j,lo(3)-1) .gt. 0) then
+                      den(i,j,lo(3),n) = c0
+                   else
+                      den(i,j,lo(3),n) = 0.d0
+                   end if
+                end do
+             end do
+          case (zhi_dir)
+             do    j = lo(2), hi(2)
+                do i = lo(1), hi(1)
+                   if (mask(i,j,hi(3)+1) .gt. 0) then
+                      den(i,j,hi(3),n) = c0
+                   else
+                      den(i,j,hi(3),n) = 0.d0
+                   end if
+                end do
+             end do
+#endif
+#endif
+          end select
+
        end if
-       
-       select case (cdir)
-       case (xlo_dir)
-          do    k = lo(3), hi(3)
-             do j = lo(2), hi(2)
-                if (mask(lo(1)-1,j,k) .gt. 0) then
-                   den(lo(1),j,k) = c0
-                else
-                   den(lo(1),j,k) = 0.d0
-                end if
-             end do
-          end do
-       case (xhi_dir)
-          do    k = lo(3), hi(3)
-             do j = lo(2), hi(2)
-                if (mask(hi(1)+1,j,k) .gt. 0) then
-                   den(hi(1),j,k) = c0
-                else
-                   den(hi(1),j,k) = 0.d0
-                end if
-             end do
-          end do
-#if (AMREX_SPACEDIM >= 2)
-       case (ylo_dir)
-          do    k = lo(3), hi(3)
-             do i = lo(1), hi(1)
-                if (mask(i,lo(2)-1,k) .gt. 0) then
-                   den(i,lo(2),k) = c0
-                else
-                   den(i,lo(2),k) = 0.d0
-                end if
-             end do
-          end do
-       case (yhi_dir)
-          do    k = lo(3), hi(3)
-             do i = lo(1), hi(1)
-                if (mask(i,hi(2)+1,k) .gt. 0) then
-                   den(i,hi(2),k) = c0
-                else
-                   den(i,hi(2),k) = 0.d0
-                end if
-             end do
-          end do
-#if (AMREX_SPACEDIM == 3)
-       case (zlo_dir)
-          do    j = lo(2), hi(2)
-             do i = lo(1), hi(1)
-                if (mask(i,j,lo(3)-1) .gt. 0) then
-                   den(i,j,lo(3)) = c0
-                else
-                   den(i,j,lo(3)) = 0.d0
-                end if
-             end do
-          end do
-       case (zhi_dir)
-          do    j = lo(2), hi(2)
-             do i = lo(1), hi(1)
-                if (mask(i,j,hi(3)+1) .gt. 0) then
-                   den(i,j,hi(3)) = c0
-                else
-                   den(i,j,hi(3)) = 0.d0
-                end if
-             end do
-          end do
-#endif
-#endif
-       end select
-       
-    end if
+    end do
     
   end subroutine amrex_mllinop_comp_interp_coef0
 
 
-  subroutine amrex_mllinop_apply_metric (lo, hi, d, dlo, dhi, r, rlo, rhi) &
+  subroutine amrex_mllinop_apply_metric (lo, hi, &
+       d, dlo, dhi, &
+       r, rlo, rhi, &
+       nc) &
        bind(c,name='amrex_mllinop_apply_metric')
-    integer, intent(in) :: lo(3), hi(3), dlo(3), dhi(3), rlo, rhi
-    real(amrex_real), intent(inout) :: d(dlo(1):dhi(1),dlo(2):dhi(2),dlo(3):dhi(3))
+    integer, intent(in) :: lo(3), hi(3), dlo(3), dhi(3), rlo, rhi, nc
+    real(amrex_real), intent(inout) :: d(dlo(1):dhi(1),dlo(2):dhi(2),dlo(3):dhi(3),nc)
     real(amrex_real), intent(in) :: r(rlo:rhi)
 
-    integer :: i,j,k
-    do       k = lo(3), hi(3)
-       do    j = lo(2), hi(2)
-          do i = lo(1), hi(1)
-             d(i,j,k) = d(i,j,k) * r(i)
+    integer :: i,j,k,n
+    do n = 1, nc
+       do       k = lo(3), hi(3)
+          do    j = lo(2), hi(2)
+             do i = lo(1), hi(1)
+                d(i,j,k,n) = d(i,j,k,n) * r(i)
+             end do
           end do
        end do
     end do
--- Src/LinearSolvers/MLMG/AMReX_MLMG_1d.F90
+++ Src/LinearSolvers/MLMG/AMReX_MLMG_1d.F90
@@ -8,35 +8,42 @@ module amrex_mlmg_interp_module
 
 contains
 
-  subroutine amrex_mlmg_lin_cc_interp (lo, hi, ff, fflo, ffhi, cc, cclo, cchi, ratio) &
+  subroutine amrex_mlmg_lin_cc_interp (lo, hi, ff, fflo, ffhi, cc, cclo, cchi, ratio, nc) &
        bind(c,name='amrex_mlmg_lin_cc_interp')
     integer, dimension(1), intent(in) :: lo, hi, fflo, ffhi, cclo, cchi
-    integer, intent(in) :: ratio
-    real(amrex_real), intent(in   ) :: cc(cclo(1):cchi(1))
-    real(amrex_real), intent(inout) :: ff(fflo(1):ffhi(1))
+    integer, intent(in) :: ratio, nc
+    real(amrex_real), intent(in   ) :: cc(cclo(1):cchi(1),nc)
+    real(amrex_real), intent(inout) :: ff(fflo(1):ffhi(1),nc)
 
-    integer :: i, ic, ioff
+    integer :: i,n, ic, ioff
 
+    
     if (ratio == 2) then
 
-       do i = lo(1), hi(1)
-          ic = i/2
-          ioff = 2*(i-ic*2)-1
-          ff(i) = 0.75d0*cc(ic) + 0.25d0*cc(ic+ioff)
+       do n = 1, nc
+          do i = lo(1), hi(1)
+             ic = i/2
+             ioff = 2*(i-ic*2)-1
+             ff(i,n) = 0.75d0*cc(ic,n) + 0.25d0*cc(ic+ioff,n)
+          end do
        end do
        
     else if (ratio == 4) then
 
-       do i = lo(1), hi(1)
-          ic = i/4
-          ff(i) = cc(ic)
+       do n = 1, nc
+          do i = lo(1), hi(1)
+             ic = i/4
+             ff(i,n) = cc(ic,n)
+          end do
        end do
        
     else
 
-       do i = lo(1), hi(1)
-          ic = i/ratio
-          ff(i) = cc(ic)
+       do n = 1, nc
+          do i = lo(1), hi(1)
+             ic = i/ratio
+             ff(i,n) = cc(ic,n)
+          end do
        end do
 
     end if
@@ -44,19 +51,22 @@ contains
   end subroutine amrex_mlmg_lin_cc_interp
 
 
-  subroutine amrex_mlmg_lin_nd_interp (clo, chi, flo, fhi, fine, fdlo, fdhi, crse, cdlo, cdhi) &
+  subroutine amrex_mlmg_lin_nd_interp (clo, chi, flo, fhi, fine, fdlo, fdhi, crse, cdlo, cdhi, nc) &
        bind(c,name='amrex_mlmg_lin_nd_interp')
     integer, dimension(1) :: clo, chi, flo, fhi, fdlo, fdhi, cdlo, cdhi
+    integer, intent(in) :: nc
     real(amrex_real), intent(inout) :: fine(fdlo(1):fdhi(1))
     real(amrex_real), intent(in   ) :: crse(cdlo(1):cdhi(1))
     
-    integer :: i,ii
+    integer :: i,n,ii
 
-    do i = clo(1), chi(1)-1
-       fine(2*i  ) = crse(i)
-       fine(2*i+1) = 0.5d0*(crse(i)+crse(i+1))
+    do n = 1, nc
+       do i = clo(1), chi(1)-1
+          fine(2*i  ,n) = crse(i,n)
+          fine(2*i+1,n) = 0.5d0*(crse(i,n)+crse(i+1,n))
+       end do
+       fine(fhi(1,n)) = crse(chi(1,n))
     end do
-    fine(fhi(1)) = crse(chi(1))
   end subroutine amrex_mlmg_lin_nd_interp
 
 end module amrex_mlmg_interp_module
