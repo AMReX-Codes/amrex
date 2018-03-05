be1b3293546291d67b9be72d46ea954adb0c9d40
Ann Almgren <asalmgren@lbl.gov>
2018-03-05 09:28:45 -0800

    For consistency, remove logo at top of files.  If we decide on a common header later we can then add it to all files uniformly

--- Src/GeometryShop/AMReX_SlabService.cpp
+++ Src/GeometryShop/AMReX_SlabService.cpp
@@ -1,15 +1,3 @@
-#ifdef CH_LANG_CC
-/*
- *      _______              __
- *     / ___/ /  ___  __ _  / /  ___
- *    / /__/ _ \/ _ \/  V \/ _ \/ _ \
- *    \___/_//_/\___/_/_/_/_.__/\___/
- *    Please refer to Copyright.txt, in Chombo's root directory.
- */
-#endif
-
-//  ANAG, LBNL, DTG
-
 #include "SlabService.H"
 #include "BoxIterator.H"
 #include "VoFIterator.H"
