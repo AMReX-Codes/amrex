3ecede96d91f2ae836865eefd08b0e14c3fa46de
dtgraves <dtgraves@lbl.gov>
2018-03-14 15:58:42 -0700

    lots of progress made in the big slog.

--- Src/GeometryShop/AMReX_IrregNode.cpp
+++ Src/GeometryShop/AMReX_IrregNode.cpp
@@ -72,4 +72,79 @@ namespace amrex
       }
   }
 
+/*******************************/
+  void 
+  IrregNode::
+  setNormalizedStuff(const Real& a_dx)
+  {
+    Real fullCellVolume = D_TERM(a_dx, *a_dx, *a_dx);
+    Real fullFaceArea   = D_TERM(1.0,  *a_dx, *a_dx);
+    Real volScaleFactor  = 1./fullCellVolume;
+    Real areaScaleFactor = 1./fullFaceArea;
+
+    Real volume = m_volumeMoments[IvSpaceDim::Zero];
+    m_volFrac = volume*volScaleFactor;
+    m_volCentroid   = RealVect::Zero;
+    m_bndryCentroid = RealVect::Zero;
+
+    for (int idir = 0; idir < SpaceDim; ++idir)
+    {
+      if(volume > 0.);
+      {
+        ///volume centroid
+        m_volCentroid[idir] = m_volumeMoments[BASISV_TM<int,SpaceDim>(idir)];
+
+        //divide by the volume
+        m_volCentroid[idir] /= volume;
+
+        //convert to relative coordinate 
+        m_volCentroid[idir] /= a_dx;
+
+      }
+      ///boundary centroid
+      Real area = m_EBMoments[IvSpaceDim::Zero];
+      if(area > 0.) //can be zero if not really cut.
+      {
+        m_bndryCentroid[idir] = m_EBMoments[BASISV_TM<int,SpaceDim>(idir)];
+
+        m_bndryCentroid[idir] /= area;
+
+        //convert to relative coordinate 
+        m_bndryCentroid[idir] /= a_dx;
+      }
+    }
+
+    for(int ifacedir = 0; ifacedir < SpaceDim; ifacedir++)
+    {
+      for(SideIterator sit; sit.ok(); ++sit)
+      {
+        //areafrac
+        int iilist = this->index(ifacedir, sit());
+        Real area = m_faceMoments[iilist][IvSDMinOne::Zero];
+        m_areaFrac[iilist].resize(1);
+        m_areaFrac[iilist][0] = area*areaScaleFactor;
+
+        m_faceCentroid[iilist].resize(1);
+        m_faceCentroid[iilist][0] = RealVect::Zero;
+        if(area > 0.) //can be zero if there is not really a face
+        {
+          //face centroids
+          int iindex = 0;
+          for (int idir = 0; idir < SpaceDim; ++idir)
+          {
+            if(idir != ifacedir)
+            {
+              IvSDMinOne mono = BASISV_TM<int,SpaceDim-1>(iindex);
+              m_faceCentroid[iilist][0][idir] = m_faceMoments[iilist][mono];
+              //normalize  by area
+              m_faceCentroid[iilist][0][idir] /= area;
+              //normalize by dx
+              m_faceCentroid[iilist][0][idir] /= a_dx;
+              iindex++;
+            }
+          }
+        }
+      }
+    }
+  }
 }
