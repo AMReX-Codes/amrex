c33e04b022b53765e06add9ef4b4f6cc18b6f9e0
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-03-15 14:22:37 -0700

    move task splitting stuff from ParallelContext into ForkJoin

--- Src/Base/AMReX_ForkJoin.cpp
+++ Src/Base/AMReX_ForkJoin.cpp
@@ -1,4 +1,5 @@
 #include <AMReX_ForkJoin.H>
+#include <AMReX_Print.H>
 
 namespace amrex {
 
@@ -6,8 +7,10 @@ ForkJoin::ForkJoin (Vector<int> trn)
     : task_rank_n(std::move(trn))
 {
     auto rank_n = ParallelContext::NProcs(); // number of ranks in current frame
-    AMREX_ASSERT(task_n() >= 2);
+    AMREX_ASSERT(NTasks() >= 2);
     AMREX_ASSERT(std::accumulate(task_rank_n.begin(),task_rank_n.end(),0) == rank_n);
+
+    compute_split_bounds();
 }
 
 ForkJoin::ForkJoin (const Vector<double> &task_rank_pct)
@@ -25,10 +28,13 @@ ForkJoin::ForkJoin (const Vector<double> &task_rank_pct)
         prev = cur;
     }
     AMREX_ASSERT(std::accumulate(task_rank_n.begin(),task_rank_n.end(),0) == rank_n);
+
+    compute_split_bounds();
 }
 
-void ForkJoin::reg_mf (MultiFab &mf, const std::string &name, int idx,
-                     Strategy strategy, Intent intent, int owner)
+void
+ForkJoin::reg_mf (MultiFab &mf, const std::string &name, int idx,
+                  Strategy strategy, Intent intent, int owner)
 {
     if (idx >= data[name].size()) {
         data[name].resize(idx + 1);
@@ -36,12 +42,11 @@ void ForkJoin::reg_mf (MultiFab &mf, const std::string &name, int idx,
     data[name][idx] = MFFork(&mf, strategy, intent, owner);
 }
 
-// this is called before ParallelContext::split
-// the parent task is the top frame in ParallelContext's stack
-void ForkJoin::copy_data_to_tasks ()
+void
+ForkJoin::copy_data_to_tasks (MPI_Comm /*task_comm*/)
 {
-    if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-        std::cout << "Copying data into fork-join tasks ..." << std::endl;
+    if (flag_verbose) {
+        amrex::Print() << "Copying data into fork-join tasks ...\n";
     }
     for (auto &p : data) { // for each name
         const auto &mf_name = p.first;
@@ -52,8 +57,8 @@ void ForkJoin::copy_data_to_tasks ()
             Vector<MultiFab> &forked = mff.forked;
             int comp_n = orig.nComp(); // number of components in original
 
-            forked.reserve(task_n()); // does nothing if forked MFs already created
-            for (int i = 0; i < task_n(); ++i) {
+            forked.reserve(NTasks()); // does nothing if forked MFs already created
+            for (int i = 0; i < NTasks(); ++i) {
                 // check if this task needs this MF
                 if (mff.strategy != Strategy::single || i == mff.owner_task) {
 
@@ -61,8 +66,8 @@ void ForkJoin::copy_data_to_tasks ()
                     int comp_lo, comp_hi;
                     if (mff.strategy == Strategy::split) {
                         // split components across tasks
-                        comp_lo = comp_n *  i    / task_n();
-                        comp_hi = comp_n * (i+1) / task_n();
+                        comp_lo = comp_n *  i    / NTasks();
+                        comp_hi = comp_n * (i+1) / NTasks();
                     } else {
                         // copy all components to task
                         comp_lo = 0;
@@ -71,23 +76,23 @@ void ForkJoin::copy_data_to_tasks ()
 
                     // create task's MF if first time through
                     if (forked.size() <= i) {
-                        if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-                            std::cout << "  Creating forked " << mf_name << "[" << idx << "] for task " << i
-                                      << (mff.strategy == Strategy::split ? " (split)" : " (whole)") << std::endl;
+                        if (flag_verbose) {
+                            amrex::Print() << "  Creating forked " << mf_name << "[" << idx << "] for task " << i
+                                           << (mff.strategy == Strategy::split ? " (split)" : " (whole)") << std::endl;
                         }
                         // look up the distribution mapping for this (box array, task) pair
                         const DistributionMapping &dm = get_dm(ba, i);
                         forked.emplace_back(ba, dm, comp_hi - comp_lo, 0);
-                    } else if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-                        std::cout << "  Forked " << mf_name << "[" << idx << "] for task " << i
-                                  << " already created" << std::endl;
+                    } else if (flag_verbose) {
+                        amrex::Print() << "  Forked " << mf_name << "[" << idx << "] for task " << i
+                                       << " already created" << std::endl;
                     }
                     AMREX_ASSERT(i < forked.size());
 
                     // copy data if needed
                     if (mff.intent == Intent::in || mff.intent == Intent::inout) {
-                        if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-                            std::cout << "    Copying " << mf_name << "[" << idx << "] into to task " << i << std::endl;
+                        if (flag_verbose) {
+                            amrex::Print() << "    Copying " << mf_name << "[" << idx << "] into to task " << i << std::endl;
                         }
                         // parallel copy data into forked MF
                         forked[i].copy(orig, comp_lo, 0, comp_hi - comp_lo);
@@ -101,16 +106,18 @@ void ForkJoin::copy_data_to_tasks ()
                     }
                 }
             }
-            AMREX_ASSERT(forked.size() == task_n());
+            AMREX_ASSERT(forked.size() == NTasks());
         }
     }
 }
 
 // this is called after ParallelContext::unsplit
 // the parent task is the top frame in ParallelContext's stack
-void ForkJoin::copy_data_from_tasks () {
-    if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-        std::cout << "Copying data out of fork-join tasks ..." << std::endl;
+void
+ForkJoin::copy_data_from_tasks ()
+{
+    if (flag_verbose) {
+        amrex::Print() << "Copying data out of fork-join tasks ...\n";
     }
     for (auto &p : data) { // for each name
         const auto &mf_name = p.first;
@@ -122,18 +129,18 @@ void ForkJoin::copy_data_from_tasks () {
                 const Vector<MultiFab> &forked = mff.forked;
                 if (mff.strategy == Strategy::split) {
                     // gather components from across tasks
-                    for (int i = 0; i < task_n(); ++i) {
-                        if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-                            std::cout << "  Copying " << mf_name << "[" << idx << "] out from task " << i << "  (unsplit)" << std::endl;
+                    for (int i = 0; i < NTasks(); ++i) {
+                        if (flag_verbose) {
+                            amrex::Print() << "  Copying " << mf_name << "[" << idx << "] out from task " << i << "  (unsplit)" << std::endl;
                         }
-                        int comp_lo = comp_n *  i    / task_n();
-                        int comp_hi = comp_n * (i+1) / task_n();
+                        int comp_lo = comp_n *  i    / NTasks();
+                        int comp_hi = comp_n * (i+1) / NTasks();
                         orig.copy(forked[i], 0, comp_lo, comp_hi - comp_lo);
                     }
                 } else { // mff.strategy == single or duplicate
                     // copy all components from owner_task
-                    if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-                        std::cout << "Copying " << mf_name << " out from task " << mff.owner_task << "  (whole)" << std::endl;
+                    if (flag_verbose) {
+                        amrex::Print() << "Copying " << mf_name << " out from task " << mff.owner_task << "  (whole)" << std::endl;
                     }
                     orig.copy(forked[mff.owner_task], 0, 0, comp_n);
                 }
@@ -145,20 +152,21 @@ void ForkJoin::copy_data_from_tasks () {
 // multiple MultiFabs may share the same box array
 // only compute the DM once per unique (box array, task) pair and cache it
 // create map from box array RefID to vector of DistributionMapping indexed by task ID
-const DistributionMapping & ForkJoin::get_dm (const BoxArray& ba, int task_idx)
+const DistributionMapping &
+ForkJoin::get_dm (const BoxArray& ba, int task_idx)
 {
     auto &dm_vec = dms[ba.getRefID()];
 
     if (dm_vec.size() == 0) {
         // new entry
-        dm_vec.resize(task_n());
+        dm_vec.resize(NTasks());
     }
     AMREX_ASSERT(task_idx < dm_vec.size());
 
     if (dm_vec[task_idx] == nullptr) {
         // create DM of current box array over current task's ranks
 #if 0
-        auto task_bounds = ParallelContext::get_split_bounds(task_rank_n);
+        auto task_bounds = ParallelContext::compute_split_bounds(task_rank_n);
         auto task_glo_rank_lo = ParallelContext::local_to_global_rank(task_bounds[task_idx].first);
         auto task_glo_rank_hi = ParallelContext::local_to_global_rank(task_bounds[task_idx].second);
         dm_vec[task_idx].reset(new DistributionMapping(ba, task_glo_rank_lo, task_glo_rank_hi));
@@ -169,20 +177,64 @@ const DistributionMapping & ForkJoin::get_dm (const BoxArray& ba, int task_idx)
         int nprocs = ParallelDescriptor::NProcs();
         dm_vec[task_idx].reset(new DistributionMapping(ba, nprocs));
 #endif
-        if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-            std::cout << "    Creating DM for (box array, task id) = ("
+        if (flag_verbose) {
+            amrex::Print() << "    Creating DM for (box array, task id) = ("
                       << ba.getRefID() << ", " << task_idx << ")" << std::endl;
         }
+
+//        amrex::Print() << " xxxxx get_dm " << task_idx << ", " << *dm_vec[task_idx] << "\n";
+
     } else {
         // DM has already been created
-        if (flag_verbose && ParallelDescriptor::IOProcessor()) {
-            std::cout << "    DM for (box array, task id) = (" << ba.getRefID() << ", " << task_idx
-                      << ") already created" << std::endl;
+        if (flag_verbose) {
+            amrex::Print() << "    DM for (box array, task id) = (" << ba.getRefID() << ", " << task_idx
+                           << ") already created" << std::endl;
         }
     }
     AMREX_ASSERT(dm_vec[task_idx] != nullptr);
 
     return *dm_vec[task_idx];
+}
+
+// split ranks in current frame into contiguous chunks
+// task i has ranks over the interval [result[i], result[i+1])
+void
+ForkJoin::compute_split_bounds ()
+{
+    AMREX_ASSERT(std::accumulate(task_rank_n.begin(),task_rank_n.end(),0) == ParallelContext::NProcs());
+
+    const auto ntasks = task_rank_n.size();
+    split_bounds.resize(ntasks + 1);
+    split_bounds[0] = 0;
+    for (int i = 0; i < ntasks; ++i) {
+        split_bounds[i + 1] = split_bounds[i] + task_rank_n[i];
+    }
+}
+
+// split top frame of stack
+// TODO: write version that takes cached comm object as argument in case of repeated identical split calls
+MPI_Comm
+ForkJoin::split_tasks ()
+{
+    const auto ntasks = task_rank_n.size();
+    int myproc = ParallelContext::MyProc();
+    for (task_me = 0; task_me < ntasks; ++task_me) {
+        int lo = split_bounds[task_me];
+        int hi = split_bounds[task_me + 1];
+        if (myproc >= lo && myproc < hi) {
+            break;
+        }
+    }
+    AMREX_ASSERT(task_me < ntasks);
+
+#ifdef BL_USE_MPI
+    MPI_Comm new_comm;
+    MPI_Comm_split(ParallelContext::Communicator(), task_me, myproc, &new_comm);
+#else
+    MPI_Comm new_comm = ParallelContext::Communicator();
+#endif
+
+    return new_comm;
 }
 
 }
