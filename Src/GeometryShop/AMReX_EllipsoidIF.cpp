bc57cfce59a1115c878d955b253d7c8bf8868b47
dtgraves <dtgraves@lbl.gov>
2018-03-21 14:51:46 -0700

    serialization still broken for higher order but all the other tests pass

--- Src/GeometryShop/AMReX_EllipsoidIF.cpp
+++ Src/GeometryShop/AMReX_EllipsoidIF.cpp
@@ -61,6 +61,53 @@ namespace amrex
     return retval;
   }
 
+  Real 
+  EllipsoidIF::
+  derivative(const  IntVect& a_deriv,
+             const RealVect& a_point) const
+  {
+    //negative derivs make no sense
+    BL_ASSERT(a_deriv.min() >= 0);
+    Real retval = 0;
+    int maxDir = a_deriv.maxDir(false);
+    int derivativeOrder = a_deriv.sum();
+
+    if (derivativeOrder == 0)
+    {
+      retval = value(a_point);
+    }
+    else if (derivativeOrder == 1)
+    {
+      retval = 2.0*(a_point[maxDir] - m_center[maxDir])/m_radii2[maxDir];
+    }
+    else if (derivativeOrder == 2)
+    {
+      if (a_deriv[maxDir] == 2)
+      {
+        // unmixed second partial = 2.0
+        retval = 2.0/m_radii2[maxDir];
+      }
+      else
+      {
+        // mixed partials = 0.0
+        retval = 0.0;
+      }
+    }
+    else
+    {
+      // higher partials = 0.0
+      retval = 0.0;
+    }
+
+    // Change the sign to change inside to outside
+    if (!m_inside && derivativeOrder > 0)
+    {
+      retval = -retval;
+    }
+
+    return retval;
+  }
+
   BaseIF* EllipsoidIF::newImplicitFunction() const
   {
     EllipsoidIF* ellipsoidPtr = new EllipsoidIF(m_radii,
