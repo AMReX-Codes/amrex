6c1482f9c93327772b099c25cc4816c77c3c89f7
Johannes Blaschke <jpblaschke@lbl.gov>
2018-03-21 13:30:48 -0700

    fix over-zealous deletions

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -29,7 +29,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf out/docs_html/**/* || exit 0
 
 # build the Doxygen documentation
 cd Docs/Doxygen
