fa8efd3728f18892af0f55015758167d5e955f7e
Johannes Blaschke <jpblaschke@lbl.gov>
2018-03-21 10:52:32 -0700

    update travis to reflect new web root for docs

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -36,16 +36,18 @@ cd Docs/Doxygen
 doxygen doxygen.conf
 cd ../..
 
+mkdir out/docs_html
+
 # move it to the right place
-mkdir -p out/doxygen
-mv Docs/Doxygen/html/* out/doxygen/
+mkdir -p out/docs_html/doxygen
+mv Docs/Doxygen/html/* out/docs_html/doxygen/
 
 # now do sphinx
 cd Docs/sphinx
 make html
 cd ../../
 
-mv Docs/sphinx/build/html/* out/
+mv Docs/sphinx/build/html/* out/docs_html/
 touch out/.nojekyll
 
 # Now let's go have some fun with the cloned repo
