45e881e80b50764cd556db9394da28a37af9389f
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-03-22 12:44:41 -0700

    use ParallelContext in ParallelDescriptor

--- Src/Base/AMReX.cpp
+++ Src/Base/AMReX.cpp
@@ -10,7 +10,6 @@
 
 #include <AMReX.H>
 #include <AMReX_ParallelDescriptor.H>
-#include <AMReX_ParallelContext.H>
 #include <AMReX_BLProfiler.H>
 #include <AMReX_BLFort.H>
 #include <AMReX_Utility.H>
@@ -300,8 +299,6 @@ amrex::Initialize (int& argc, char**& argv, bool build_parm_parse,
     BL_MPI_REQUIRE( MPI_Win_create_dynamic(MPI_INFO_NULL, MPI_COMM_WORLD, &ParallelDescriptor::fb_win) );
 #endif
 
-    ParallelContext::init();
-
     while ( ! The_Initialize_Function_Stack.empty())
     {
         //
@@ -481,8 +478,6 @@ amrex::Finalize (bool finalize_parallel)
 #endif
     
     ParallelDescriptor::EndTeams();
-
-    ParallelContext::finalize();
 
 #ifdef BL_USE_UPCXX
     upcxx::finalize();
