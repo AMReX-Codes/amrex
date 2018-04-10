dc39d17dc9296e9b1be3b759e778142c4f664367
Ann Almgren <asalmgren@lbl.gov>
2018-04-09 17:19:04 -0700

    Adding source code for SWFFT so that the Tutorial can run.

---
+++ Src/Extern/SWFFT/Error.h
@@ -0,0 +1,97 @@
+/*
+ *                 Copyright (C) 2017, UChicago Argonne, LLC
+ *                            All Rights Reserved
+ *
+ *           Hardware/Hybrid Cosmology Code (HACC), Version 1.0
+ *
+ * Salman Habib, Adrian Pope, Hal Finkel, Nicholas Frontiere, Katrin Heitmann,
+ *      Vitali Morozov, Jeffrey Emberson, Thomas Uram, Esteban Rangel
+ *                        (Argonne National Laboratory)
+ *
+ *  David Daniel, Patricia Fasel, Chung-Hsing Hsu, Zarija Lukic, James Ahrens
+ *                      (Los Alamos National Laboratory)
+ *
+ *                               George Zagaris
+ *                                 (Kitware)
+ *
+ *                            OPEN SOURCE LICENSE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer. Software changes,
+ *      modifications, or derivative works, should be noted with comments and
+ *      the author and organization’s name.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   3. Neither the names of UChicago Argonne, LLC or the Department of Energy
+ *      nor the names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission.
+ *
+ *   4. The software and the end-user documentation included with the
+ *      redistribution, if any, must include the following acknowledgment:
+ *
+ *     "This product includes software produced by UChicago Argonne, LLC under
+ *      Contract No. DE-AC02-06CH11357 with the Department of Energy."
+ *
+ * *****************************************************************************
+ *                                DISCLAIMER
+ * THE SOFTWARE IS SUPPLIED "AS IS" WITHOUT WARRANTY OF ANY KIND. NEITHER THE
+ * UNITED STATES GOVERNMENT, NOR THE UNITED STATES DEPARTMENT OF ENERGY, NOR 
+ * UCHICAGO ARGONNE, LLC, NOR ANY OF THEIR EMPLOYEES, MAKES ANY WARRANTY, 
+ * EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR RESPONSIBILITY FOR THE
+ * ACCURARY, COMPLETENESS, OR USEFULNESS OF ANY INFORMATION, DATA, APPARATUS,
+ * PRODUCT, OR PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE
+ * PRIVATELY OWNED RIGHTS.
+ *
+ * *****************************************************************************
+ */
+
+#ifndef HACC_ERROR_H
+#define HACC_ERROR_H
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+#include <errno.h>
+#include <string.h>
+
+namespace hacc {
+// Use this class to generate a fatal error, like this:
+// Error() << "Something bad happened: " << 5 << " is not " << 6;
+// which will format a string and throw a std::runtime_error.
+class Error {
+public:
+  Error(bool UseErrno = false) : SysError(0) {
+    if (UseErrno)
+      SysError = strerror(errno);
+  }
+
+  ~Error() {
+    if (SysError)
+      SS << ": " << SysError;
+
+    throw std::runtime_error(SS.str());
+  }
+
+  template <typename T>
+  Error &operator << (const T &Value) {
+    SS << Value;
+    return *this; 
+  }
+
+protected:
+  std::stringstream SS;
+  const char *SysError;
+};
+} // namespace hacc
+
+#endif // HACC_ERROR_H
+
