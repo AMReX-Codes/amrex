08eb70fc5023f5258a8bf076c31ad796c91d24a3
Maxence Thevenet <mthevenet@lbl.gov>
2018-04-20 15:30:55 -0700

    run test for NCI corrector

--- Example/nci_corrector/nci_corrector_analysis.py
+++ Example/nci_corrector/nci_corrector_analysis.py
@@ -3,11 +3,13 @@ import yt
 import shutil
 import numpy as np
 import scipy.constants as scc
-
+import subprocess
 yt.funcs.mylog.setLevel(0)
+
+os.system('cp ../../Bin/main2d.gnu.MPI.OMP.ex ./main2d.gnu.MPI.OMP.ex')
 os.system('./main2d.gnu.MPI.OMP.ex inputs2d')
 
-filename = 'plt02000'
+filename = 'plt01000'
 ds = yt.load( filename )
 ad = ds.all_data()
 ex = np.reshape(ad['boxlib', 'Ex'].v,(128,128))
@@ -20,7 +22,7 @@ print('#### 2d test ####')
 print('#################')
 print('Energy = ' + str(ener*1.e-15))
 print('Should be: ')
-print('FILTER OFF: 2.6570911530826467e+17')
-print('FILTER ON : 1.3963228479075573')
+print('FILTER OFF: 15.39863351071166')
+print('FILTER ON : 0.006157639585545')
 
 os.system('rm -r ./plt*')
