83c6b21fa9f21079598702520ff24764e3987af8
Maxence Thevenet <mthevenet@lbl.gov>
2018-04-23 15:18:08 -0700

    use capital X and Y in laser function as they are different from simulation x and y

--- Source/LaserParticleContainer.cpp
+++ Source/LaserParticleContainer.cpp
@@ -68,12 +68,12 @@ LaserParticleContainer::LaserParticleContainer (AmrCore* amr_core, int ispecies)
 
   if ( profile == laser_t::parse_field_function ) {
        // Parse the properties of the parse_field_function profile
-	   pp.get("field_function(x,y,t)", field_function);
+	   pp.get("field_function(X,Y,t)", field_function);
        // User-defined constants: replace names by value
        my_constants.ReadParameters();
 	   field_function = my_constants.replaceStringValue(field_function);
        // Pass math expression and list of variables to Fortran as char*
-       const char *str_var  = "x,y,t";
+       const char *str_var  = "X,Y,t";
        const char *str_func = field_function.c_str();
        parser_instance_number = WRPX_PARSER_INIT_FUNCTION(str_func, str_var);      
 	}
