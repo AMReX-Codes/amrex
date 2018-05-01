3ebc68f8f0d9f3100b0a68bde5275ceaa7569f0a
Mahesh Natarajan <natarajan@Maheshs-MacBook-Pro.local>
2018-05-01 06:10:04 -0400

    Adding FullyFortran to Tests

--- Tests/FullyFortran/Advection3d/Source/template.mk
+++ Tests/FullyFortran/Advection3d/Source/template.mk
@@ -5,9 +5,9 @@
 AMREX_DIR := ../../../../tmp_install_dir
 ############
 # Commands Macors
-FC = /opt/local/bin/mpif90
-CC = /opt/local/bin/mpicc
-CXX = /opt/local/bin/mpicxx
-LD = /opt/local/bin/mpicc $(MAIN_PROGRAM)
+FC = mpif90
+CC = mpicc
+CXX = mpicxx
+LD = mpicc $(MAIN_PROGRAM)
 FFLAGS +=-I$(AMREX_DIR)/include -DBL_SPACEDIM=3 -DAMREX_SPACEDIM=3 -O2
 LDFLAGS= -L. -L$(AMREX_DIR)/lib -lamrex -lmpichf90 -lgfortran -lstdc++ -std=c++11 -lm -Wl,-flat_namespace -Wl,-commons,use_dylibs -lmpifort -lmpi -lpmpi  -lgfortran -lquadmath
