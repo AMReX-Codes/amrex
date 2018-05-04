9f899a9015cba1673d27336553c31d3142d74b4b
Andrew Myers <atmyers2@gmail.com>
2018-05-04 12:14:00 -0700

    make the swapBytes function templated on size rather than type to make it work on Titan with cce

--- Src/Base/AMReX_IntConv.cpp
+++ Src/Base/AMReX_IntConv.cpp
@@ -2,36 +2,36 @@
 
 namespace amrex {
 
-template<> std::int16_t swapBytes<std::int16_t>(std::int16_t val)
+template<> std::int16_t swapBytes<2>(std::int16_t val)
 {
     return (val << 8) | ((val >> 8) & 0xFF);
 }
 
-template<> std::int32_t swapBytes<std::int32_t>(std::int32_t val)
+template<> std::int32_t swapBytes<4>(std::int32_t val)
 {
     val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF ); 
     return (val << 16) | ((val >> 16) & 0xFFFF);
 }
 
-template<> std::int64_t swapBytes<std::int64_t>(std::int64_t val)
+template<> std::int64_t swapBytes<8>(std::int64_t val)
 {
     val = ((val << 8) & 0xFF00FF00FF00FF00ULL ) | ((val >> 8) & 0x00FF00FF00FF00FFULL );
     val = ((val << 16) & 0xFFFF0000FFFF0000ULL ) | ((val >> 16) & 0x0000FFFF0000FFFFULL );
     return (val << 32) | ((val >> 32) & 0xFFFFFFFFULL);
 }
 
-template<> std::uint16_t swapBytes<std::uint16_t>(std::uint16_t val)
+template<> std::uint16_t swapBytes<2>(std::uint16_t val)
 {
     return (val << 8) | (val >> 8 );
 }
 
-template<> std::uint32_t swapBytes<std::uint32_t>(std::uint32_t val)
+template<> std::uint32_t swapBytes<4>(std::uint32_t val)
 {
     val = ((val << 8) & 0xFF00FF00 ) | ((val >> 8) & 0xFF00FF ); 
     return (val << 16) | (val >> 16);
 }
 
-template<> std::uint64_t swapBytes<std::uint64_t>(std::uint64_t val)
+template<> std::uint64_t swapBytes<8>(std::uint64_t val)
 {
     val = ((val << 8) & 0xFF00FF00FF00FF00ULL ) | ((val >> 8) & 0x00FF00FF00FF00FFULL );
     val = ((val << 16) & 0xFFFF0000FFFF0000ULL ) | ((val >> 16) & 0x0000FFFF0000FFFFULL );
