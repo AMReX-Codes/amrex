0655db9d6930d09deaeeb8ae49b3985afab9278d
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-05-07 09:45:25 -0700

    add geom_type box to Tests/EBbcent

--- Tests/EBbcent/main.cpp
+++ Tests/EBbcent/main.cpp
@@ -3,9 +3,13 @@
 #include <AMReX_Geometry.H>
 #include <AMReX_MultiFab.H>
 #include <AMReX_ParmParse.H>
+#include <AMReX_GeometryShop.H>
 #include <AMReX_WrappedGShop.H>
 #include <AMReX_EBLevelGrid.H>
+#include <AMReX_EBTower.H>
+#include <AMReX_VisMF.H>
 #include <AMReX_PlaneIF.H>
+#include <AMReX_IntersectionIF.H>
 
 using namespace amrex;
 
@@ -18,7 +22,12 @@ int main (int argc, char* argv[])
     {
         int n_cell = 64;
         int max_grid_size = 32;
-        std::string geom_type = "ramp_normal_point";
+        std::string geom_type = "box";
+        
+        {
+            ParmParse pp;
+            pp.query("geom_type", geom_type);
+        }
 
         BoxArray grids;
         DistributionMapping dmap;
@@ -36,7 +45,10 @@ int main (int argc, char* argv[])
             dmap.define(grids);
         }
 
+        if (geom_type == "ramp_normal_point")
         {
+            debug_cell = IntVect(AMREX_D_DECL(32, 6, 0));
+
             Vector<Real> pointvec {AMREX_D_DECL( 0.5 ,0.0, 0.0)};
             Vector<Real> normalvec{AMREX_D_DECL(-1.0, 0.0, 0.0)};
 
@@ -55,8 +67,42 @@ int main (int argc, char* argv[])
             AMReX_EBIS::instance()->define(geom.Domain(), RealVect::Zero, dx[0], gshop,
                                            max_grid_size, 0);
         }
+        else if (geom_type == "box")
+        {
+            debug_cell = IntVect(AMREX_D_DECL(16, 16, 0));
+
+            const Real* dx = geom.CellSize();
+            Vector<BaseIF*> planes;
+            for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+                RealVect normalLo = BASISREALV(idim);
+                RealVect pointLo = (0.25+0.1*dx[idim]) * BASISREALV(idim);
+                planes.push_back(new PlaneIF(normalLo, pointLo, true));
+                
+                RealVect normalHi = -BASISREALV(idim);
+                RealVect pointHi = (0.75-0.1*dx[idim]) * BASISREALV(idim);
+                planes.push_back(new PlaneIF(normalHi, pointHi, true));
+            }
+
+            IntersectionIF bif(planes);
+
+            bool verbose = true;
+            WrappedGShop gshop(bif, verbose);
+//            GeometryShop gshop(bif, verbose);
+            AMReX_EBIS::instance()->define(geom.Domain(), RealVect::Zero, dx[0], gshop,
+                                           max_grid_size, 0);
+        }
+        else
+        {
+            amrex::Print() << " bogus geom_type input = " << geom_type << "\n";
+            amrex::Error("invalid inputs");
+        }
 
         {
+            EBTower::Build();
+            EBFArrayBoxFactory factory(geom, grids, dmap, {1,1,1}, EBSupport::full);
+            const auto& vfrac = factory.getVolFrac();
+            VisMF::Write(vfrac, "vfrc");
+
             EBLevelGrid eblg(grids, dmap, geom.Domain(), 1);
             const EBISLayout& ebisl = eblg.getEBISL();
             for (MFIter mfi(grids,dmap); mfi.isValid(); ++mfi) {
