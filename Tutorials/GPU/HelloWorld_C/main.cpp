aef815aaaaa5da78b98d60d450195452f1d76430
Kevin Gott <kngott@lbl.gov>
2018-05-21 17:21:24 -0400

    BaseFab::setVal prepped except for 'ForEach'

--- Tutorials/GPU/HelloWorld_C/main.cpp
+++ Tutorials/GPU/HelloWorld_C/main.cpp
@@ -4,12 +4,17 @@
 
 #include <AMReX_Vector.H>
 #include <AMReX_IntVect.H>
+#include <AMReX_BaseFab.H>
 
 DEVICE_FUNCTION
-void kernel_Vector()
+void kernel_Vector(amrex::Vector<int> vec1, amrex::Vector<int> vec2)
 {
+/*   for (int i=0; i<vec1.size(); ++i)
+   {
+     vec1[i] = vec1[i] + vec2[i];
+   }
+*/
 }
-
 
 DEVICE_FUNCTION
 void kernel_IntVect(amrex::IntVect *iv1, amrex::IntVect *iv2)
@@ -18,6 +23,19 @@ void kernel_IntVect(amrex::IntVect *iv1, amrex::IntVect *iv2)
 
    amrex::IntVect local(3, 3, 3);
    *iv1 = *iv1 + local; 
+}
+
+DEVICE_FUNCTION
+void kernel_Box(amrex::Box *box1, amrex::Box *box2)
+{
+
+}
+
+DEVICE_FUNCTION
+void kernel_BaseFab(amrex::BaseFab<int> *bf1, amrex::BaseFab<int> *bf2)
+{
+   amrex::Box baseBox(amrex::IntVect(0,0,0), amrex::IntVect(9,9,9));
+   bf1->setVal(1, baseBox, 1, 1);
 }
 
 int main (int argc, char* argv[])
@@ -34,23 +52,39 @@ int main (int argc, char* argv[])
       Priority List
       -------------
       0) AMReX_Vector
+      0-1) Functions with std calls.
       1) IntVect
-      2) Box 
-      3) BaseFab
-      4) FabArray
-      5) Geometry 
+      2) IndexType
+      3) Box 
+      4) BaseFab
+      5) FabArray
+      6) Geometry 
+
+      Top Down
+      --------
+      A) Setval
+      B) Copy
+      C) Fortran Functions
     */
     // ===================================
     // AMReX_Vector
-    {
-      amrex::Vector *vector1 = new amrex::vector(10, -1);
-
-      int blockSize = 256;
-      int numBlocks = 1;
-      kernel_Vector<<<numBlocks,blockSize>>>(device1, device2);
-      cudaDeviceSynchronize();
-    }
-
+    //{
+    //  amrex::Vector<int> *vector1 = new amrex::Vector<int>(10, -1);
+    //  amrex::Vector<int> *vector2 = new amrex::Vector<int>(10, 5);
+    //
+    //  int blockSize = 256;
+    //  int numBlocks = 1;
+    //  kernel_Vector<<<numBlocks,blockSize>>>(*vector1, *vector2);
+    //  cudaDeviceSynchronize();
+    //
+    //  amrex::Print() << "vector1 :: vector 2:" << std::endl;
+    //  for (int i=0; i<vector1->size(); ++i)
+    //  {
+    //    amrex::Print() << (*vector1)[i] << " :: " << (*vector2)[i] << std::endl;
+    //  }
+    //}
+    // ===================================
+    // Array 
     // ===================================
     // IntVect
     {
@@ -68,7 +102,37 @@ int main (int argc, char* argv[])
                         "device1 = " << *device1 << std::endl <<
                         "device2 = " << *device2 << std::endl;
     }
+/*
+    // ===================================
+    // Box 
+    {
+      amrex::Box host_box(amrex::IntVect(0,0,0), amrex::IntVect(9,9,9));
+      amrex::Box *d_box1 = new amrex::Box(amrex::IntVect(1,1,1), amrex::IntVect(5,5,5));
+      amrex::Box *d_box2 = new amrex::Box(amrex::IntVect(3,3,3), amrex::IntVect(6,6,6));
 
+      int blockSize = 256;
+      int numBlocks = 1;
+      kernel_Box<<<numBlocks,blockSize>>>(d_box1, d_box2);
+      cudaDeviceSynchronize();
+
+      amrex::Print() << "host = " << host << std::endl << 
+                        "device1 = " << *d_box1 << std::endl <<
+                        "device2 = " << *d_box2 << std::endl;
+    }
+*/
+    // ===================================
+    // BaseFab
+    {
+      amrex::Box baseBox(amrex::IntVect(0,0,0), amrex::IntVect(13,13,13));
+      amrex::BaseFab<int> *d_bf1 = new amrex::BaseFab<int>(baseBox);
+      amrex::BaseFab<int> *d_bf2 = new amrex::BaseFab<int>(baseBox);
+//      amrex::BaseFab<int> h_bf(baseBox);
+
+      int blockSize = 256;
+      int numBlocks = 1;
+      kernel_BaseFab<<<numBlocks,blockSize>>>(d_bf1, d_bf2);
+      cudaDeviceSynchronize();
+    }
 
 
     amrex::Finalize();
