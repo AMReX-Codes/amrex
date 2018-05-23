bf0281d50a9b767bf676c533a5370a683a652b25
Kevin Gott <kngott@lbl.gov>
2018-05-22 21:52:47 -0400

    compiler no longer complaining

--- Tutorials/GPU/HelloWorld_C/main.cpp
+++ Tutorials/GPU/HelloWorld_C/main.cpp
@@ -2,9 +2,11 @@
 #include <AMReX.H>
 #include <AMReX_Print.H>
 
+#include <AMReX_ArrayLim.H>
 #include <AMReX_Vector.H>
 #include <AMReX_IntVect.H>
 #include <AMReX_BaseFab.H>
+#include <AMReX_BaseFab_f.H>
 
 DEVICE_FUNCTION
 void kernel_Vector(amrex::Vector<int> vec1, amrex::Vector<int> vec2)
@@ -32,10 +34,19 @@ void kernel_Box(amrex::Box *box1, amrex::Box *box2)
 }
 
 DEVICE_FUNCTION
-void kernel_BaseFab(amrex::BaseFab<int> *bf1, amrex::BaseFab<int> *bf2)
+void kernel_BaseFab(amrex::BaseFab<amrex::Real> *bf1, int *val)
 {
-   amrex::Box baseBox(amrex::IntVect(0,0,0), amrex::IntVect(9,9,9));
-   bf1->setVal(1, baseBox, 1, 1);
+   amrex::Real value = 20.0;
+   int ncomp = 1;
+// bf1->getVal(&val[0],amrex::IntVect(0,0,0) ,0,1);
+   amrex::Box bx(amrex::IntVect(0,0,0), amrex::IntVect(13,13,13));
+//   bf1->performSetVal(value, bx, 0, 1);
+ 
+//   amrex_fort_fab_setval(bx.loVect(), bx.hiVect(), bf1->dataPtr(0), bx.loVect(), bx.hiVect(), &ncomp, &value);
+
+//   bf1->setVal(20, bx, 0, 1);
+//   bf1->getVal(&val[0],amrex::IntVect(0,0,0),0,1);
+
 }
 
 int main (int argc, char* argv[])
@@ -48,60 +59,25 @@ int main (int argc, char* argv[])
     cudaGetDeviceCount(&devices);
     amrex::Print() << "Hello world from AMReX version " << amrex::Version() << ". GPU devices: " << devices << "\n";
     // ===================================
-    /*
-      Priority List
-      -------------
-      0) AMReX_Vector
-      0-1) Functions with std calls.
-      1) IntVect
-      2) IndexType
-      3) Box 
-      4) BaseFab
-      5) FabArray
-      6) Geometry 
-
-      Top Down
-      --------
-      A) Setval
-      B) Copy
-      C) Fortran Functions
-    */
-    // ===================================
-    // AMReX_Vector
-    //{
-    //  amrex::Vector<int> *vector1 = new amrex::Vector<int>(10, -1);
-    //  amrex::Vector<int> *vector2 = new amrex::Vector<int>(10, 5);
-    //
-    //  int blockSize = 256;
-    //  int numBlocks = 1;
-    //  kernel_Vector<<<numBlocks,blockSize>>>(*vector1, *vector2);
-    //  cudaDeviceSynchronize();
-    //
-    //  amrex::Print() << "vector1 :: vector 2:" << std::endl;
-    //  for (int i=0; i<vector1->size(); ++i)
-    //  {
-    //    amrex::Print() << (*vector1)[i] << " :: " << (*vector2)[i] << std::endl;
-    //  }
-    //}
-    // ===================================
-    // Array 
-    // ===================================
     // IntVect
+
     {
       amrex::IntVect host(1,1,1);
       amrex::IntVect *device1 = new amrex::IntVect(1,1,1);
       amrex::IntVect *device2 = new amrex::IntVect(2,2,2);
 
-      int blockSize = 256;
+      int blockSize = 1;
       int numBlocks = 1;
 //      SIMPLE_LAUNCH(numBlocks, blockSize, kernel_IntVect, device1, device2);
       kernel_IntVect<<<numBlocks,blockSize>>>(device1, device2);
       cudaDeviceSynchronize();
 
-      amrex::Print() << "host = " << host << std::endl << 
-                        "device1 = " << *device1 << std::endl <<
-                        "device2 = " << *device2 << std::endl;
+      amrex::Print() << "IntVect Test" << std::endl <<
+                        "host = "      << host << std::endl << 
+                        "device1 = "   << *device1 << std::endl <<
+                        "device2 = "   << *device2 << std::endl << std::endl;
     }
+
 /*
     // ===================================
     // Box 
@@ -123,15 +99,36 @@ int main (int argc, char* argv[])
     // ===================================
     // BaseFab
     {
-      amrex::Box baseBox(amrex::IntVect(0,0,0), amrex::IntVect(13,13,13));
-      amrex::BaseFab<int> *d_bf1 = new amrex::BaseFab<int>(baseBox);
-      amrex::BaseFab<int> *d_bf2 = new amrex::BaseFab<int>(baseBox);
-//      amrex::BaseFab<int> h_bf(baseBox);
+//      int val_gpu = 101;
+//      int *d_val_gpu;
+//      cudaMalloc(&d_val_gpu, size_t(sizeof(int)));
+//      cudaMemcpy(d_val_gpu, &val_gpu, sizeof(int), cudaMemcpyHostToDevice);
 
-      int blockSize = 256;
+      amrex::Real val_old = 10, val_new = 11;
+      amrex::Box baseBox(amrex::IntVect(0,0,0), amrex::IntVect(13,13,13));
+//      amrex::BaseFab<amrex::Real> *d_bf1 = new amrex::BaseFab<amrex::Real>(baseBox);
+      amrex::BaseFab<amrex::Real> d_bf1(baseBox);
+
+      amrex::BaseFab<int> *d_bf2 = new amrex::BaseFab<int>(baseBox);
+      d_bf1.setVal(11, baseBox, 0, 1);
+      d_bf1.getVal(&val_old,amrex::IntVect(1,1,1));
+
+      amrex::Print() << "Box = " << baseBox << std::endl;
+
+//      amrex::Real value_new = 20.0;
+//      amrex_fort_fab_setval(baseBox.loVect(), baseBox.hiVect(), d_bf1->dataPtr(0), baseBox.loVect(), baseBox.hiVect(), 0, &value_new);
+
+      int blockSize = 1;
       int numBlocks = 1;
-      kernel_BaseFab<<<numBlocks,blockSize>>>(d_bf1, d_bf2);
-      cudaDeviceSynchronize();
+//      kernel_BaseFab<<<numBlocks,blockSize>>>(d_bf1, d_val_gpu);
+//      cudaDeviceSynchronize();
+//      cudaMemcpy(&val_gpu, d_val_gpu, sizeof(int), cudaMemcpyDeviceToHost); 
+
+ //     d_bf1->getVal(&val_new,amrex::IntVect(0,0,0));
+      amrex::Print() << "BaseFab Test "  << std::endl <<
+                        "pre setVal = "  << val_old << std::endl << 
+//                        "gpu Val = "     << val_gpu << std::endl <<
+                        "post setVal = " << val_new << std::endl; 
     }
 
 
