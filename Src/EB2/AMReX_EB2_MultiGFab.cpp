a0c2470a4b33050d6c7c244baa6fa99daac929df
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-05-24 10:25:41 -0700

    3d build_types

--- Src/EB2/AMReX_EB2_MultiGFab.cpp
+++ Src/EB2/AMReX_EB2_MultiGFab.cpp
@@ -9,12 +9,23 @@ GFab::buildTypes ()
 {
     static_assert(sizeof(Type_t) == sizeof(int), "sizeof c_int is not 32");
     
+#if (AMREX_SPACEDIM == 2)
     amrex_eb2_gfab_build_types (BL_TO_FORTRAN_BOX(m_validbox),
                                 BL_TO_FORTRAN_ANYD(m_levelset),
                                 BL_TO_FORTRAN_ANYD(m_celltype),
-                                AMREX_D_DECL(BL_TO_FORTRAN_ANYD(m_facetype[0]),
-                                             BL_TO_FORTRAN_ANYD(m_facetype[1]),
-                                             BL_TO_FORTRAN_ANYD(m_facetype[2])));
+                                BL_TO_FORTRAN_ANYD(m_facetype[0]),
+                                BL_TO_FORTRAN_ANYD(m_facetype[1]));
+#elif (AMREX_SPACEDIM == 3)
+    amrex_eb2_gfab_build_types (BL_TO_FORTRAN_BOX(m_validbox),
+                                BL_TO_FORTRAN_ANYD(m_levelset),
+                                BL_TO_FORTRAN_ANYD(m_celltype),
+                                BL_TO_FORTRAN_ANYD(m_facetype[0]),
+                                BL_TO_FORTRAN_ANYD(m_facetype[1]),
+                                BL_TO_FORTRAN_ANYD(m_facetype[2]),
+                                BL_TO_FORTRAN_ANYD(m_edgetype[0]),
+                                BL_TO_FORTRAN_ANYD(m_edgetype[1]),
+                                BL_TO_FORTRAN_ANYD(m_edgetype[2]));
+#endif
 }
 
 MultiFab
