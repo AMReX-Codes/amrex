bd870dfcc50acb7a43151ae74236dd95efda1f5b
Andrew Myers <atmyers2@gmail.com>
2018-05-25 15:27:18 -0700

    writing plot files.

--- Tutorials/Amr/Advection_F/Source/initdata.F90
+++ Tutorials/Amr/Advection_F/Source/initdata.F90
@@ -2,6 +2,8 @@ module initdata_module
 
   use amrex_amr_module
 
+  use amrex_amrtracerparticlecontainer_module, only: amrex_amrtracerparticlecontainer_init, &
+       amrex_init_particles_one_per_cell
   use my_amr_module, only : restart, plot_int
   use plotfile_module, only : writeplotfile
   use averagedown_module, only : averagedown
@@ -15,9 +17,15 @@ module initdata_module
 contains
 
   subroutine initdata ()
+    type(c_ptr) :: amrcore = c_null_ptr
     if (len_trim(restart) .eq. 0) then
        call amrex_init_from_scratch(0.0_amrex_real)
        call averagedown()
+
+       amrcore = amrex_get_amrcore()
+       call amrex_amrtracerparticlecontainer_init(amrcore)
+       call amrex_init_particles_one_per_cell()
+
        if (plot_int .gt. 0) call writeplotfile
     else
        call amrex_abort("init from checkpoint not implemented yet")
