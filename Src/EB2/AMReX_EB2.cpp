14efdc35d03668ceb818bb26dc50663deccd54b9
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-06-03 07:35:35 -0700

    clean up

--- Src/EB2/AMReX_EB2.cpp
+++ Src/EB2/AMReX_EB2.cpp
@@ -1,8 +1,8 @@
 
-#include <AMReX_EB2_IF_Sphere.H>
-#include <AMReX_EB2_IF_Cylinder.H>
 #include <AMReX_EB2_IF_Box.H>
+#include <AMReX_EB2_IF_Cylinder.H>
 #include <AMReX_EB2_IF_Plane.H>
+#include <AMReX_EB2_IF_Sphere.H>
 
 #include <AMReX_EB2_GeometryShop.H>
 #include <AMReX_EB2.H>
@@ -19,23 +19,22 @@ Initialize (const Geometry& geom, const Info& info)
     std::string geom_type;
     pp.get("geom_type", geom_type);
 
-    if (geom_type == "sphere")
+    if (geom_type == "box")
     {
-        std::vector<Real> vc;
-        pp.getarr("sphere_center", vc);
-        AMREX_ALWAYS_ASSERT_WITH_MESSAGE(vc.size() >= AMREX_SPACEDIM,
-                                         "eb2.sphere_center doesn't have enough items");
-        RealArray center{AMREX_D_DECL(vc[0],vc[1],vc[2])};
+        std::vector<Real> vlo;
+        pp.getarr("box_lo", vlo);
+        RealArray lo{AMREX_D_DECL(vlo[0],vlo[1],vlo[2])};
 
-        Real radius;
-        pp.get("sphere_radius", radius);
+        std::vector<Real> vhi;
+        pp.getarr("box_hi", vhi);
+        RealArray hi{AMREX_D_DECL(vhi[0],vhi[1],vhi[2])};
 
         bool has_fluid_inside;
-        pp.get("sphere_has_fluid_inside", has_fluid_inside);
+        pp.get("box_has_fluid_inside", has_fluid_inside);
+        
+        EB2::BoxIF bf(lo, hi, has_fluid_inside);
 
-        EB2::SphereIF sf(radius, center, has_fluid_inside);
-
-        EB2::GeometryShop<EB2::SphereIF> gshop(sf);
+        EB2::GeometryShop<EB2::BoxIF> gshop(bf);
         EB2::Initialize(gshop, geom, info);
     }
     else if (geom_type == "cylinder")
@@ -65,24 +64,6 @@ Initialize (const Geometry& geom, const Info& info)
         EB2::GeometryShop<EB2::CylinderIF> gshop(cf);
         EB2::Initialize(gshop, geom, info);
     }
-    else if (geom_type == "box")
-    {
-        std::vector<Real> vlo;
-        pp.getarr("box_lo", vlo);
-        RealArray lo{AMREX_D_DECL(vlo[0],vlo[1],vlo[2])};
-
-        std::vector<Real> vhi;
-        pp.getarr("box_hi", vhi);
-        RealArray hi{AMREX_D_DECL(vhi[0],vhi[1],vhi[2])};
-
-        bool has_fluid_inside;
-        pp.get("box_has_fluid_inside", has_fluid_inside);
-        
-        EB2::BoxIF bf(lo, hi, has_fluid_inside);
-
-        EB2::GeometryShop<EB2::BoxIF> gshop(bf);
-        EB2::Initialize(gshop, geom, info);
-    }
     else if (geom_type == "plane")
     {
         std::vector<Real> vpoint;
@@ -96,6 +77,25 @@ Initialize (const Geometry& geom, const Info& info)
         EB2::PlaneIF pf(point, normal);
 
         EB2::GeometryShop<EB2::PlaneIF> gshop(pf);
+        EB2::Initialize(gshop, geom, info);
+    }
+    else if (geom_type == "sphere")
+    {
+        std::vector<Real> vc;
+        pp.getarr("sphere_center", vc);
+        AMREX_ALWAYS_ASSERT_WITH_MESSAGE(vc.size() >= AMREX_SPACEDIM,
+                                         "eb2.sphere_center doesn't have enough items");
+        RealArray center{AMREX_D_DECL(vc[0],vc[1],vc[2])};
+
+        Real radius;
+        pp.get("sphere_radius", radius);
+
+        bool has_fluid_inside;
+        pp.get("sphere_has_fluid_inside", has_fluid_inside);
+
+        EB2::SphereIF sf(radius, center, has_fluid_inside);
+
+        EB2::GeometryShop<EB2::SphereIF> gshop(sf);
         EB2::Initialize(gshop, geom, info);
     }
     else
