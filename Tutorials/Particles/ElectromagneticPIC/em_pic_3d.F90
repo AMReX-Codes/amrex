49f8a08abba78bb8382c998bd2bb3a3b7d6b4f1f
Andrew Myers <atmyers@lbl.gov>
2018-06-06 11:36:34 -0400

    take abs of the exact solution when computing the max norm of the error.

--- Tutorials/Particles/ElectromagneticPIC/em_pic_3d.F90
+++ Tutorials/Particles/ElectromagneticPIC/em_pic_3d.F90
@@ -835,7 +835,7 @@ subroutine check_langmuir_solution(boxlo, boxhi, testlo, testhi, jx, jxlo, jxhi,
   do l       = lo(3), hi(3)
      do k    = lo(2), hi(2)
         do j = lo(1), hi(1)
-           error = max(error, abs(jx(j,k,l) - exact) / exact)
+           error = max(error, abs(jx(j,k,l) - exact) / abs(exact))
         end do
      end do
   end do
