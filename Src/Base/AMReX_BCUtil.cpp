fffe5c5be22d6189187ea42e5b3810fab5169243
Kevin Gott <kngott@lbl.gov>
2018-06-06 17:03:22 -0400

    HeatEquation_EX2_C with BCs and managed vector running with small problem. Also awaiting features for further improvement.

--- Src/Base/AMReX_BCUtil.cpp
+++ Src/Base/AMReX_BCUtil.cpp
@@ -5,7 +5,7 @@
 
 namespace amrex
 {
-    void FillDomainBoundary (MultiFab& phi, const Geometry& geom, const Vector<BCRec>& bc)
+    void FillDomainBoundary (MultiFab& phi, const Geometry& geom, const BCRecVector& bc)
     {
         if (Geometry::isAllPeriodic()) return;
         if (phi.nGrow() == 0) return;
