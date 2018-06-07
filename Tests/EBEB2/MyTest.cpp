745fa508a53b4792cd949ff81027507b2c196735
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-06-07 15:52:03 -0700

    scale

--- Tests/EBEB2/MyTest.cpp
+++ Tests/EBEB2/MyTest.cpp
@@ -3,10 +3,12 @@
 #include <AMReX_EB2_IF_Union.H>
 #include <AMReX_EB2_IF_Intersection.H>
 #include <AMReX_EB2_IF_Complement.H>
+#include <AMReX_EB2_IF_Scale.H>
 #include <AMReX_EB2_IF_Translation.H>
 #include <AMReX_EB2_IF_Lathe.H>
 #include <AMReX_EB2_IF_Box.H>
 #include <AMReX_EB2_IF_Cylinder.H>
+#include <AMReX_EB2_IF_Ellipsoid.H>
 #include <AMReX_EB2_IF_Sphere.H>
 #include <AMReX_EB2_IF_Plane.H>
 
