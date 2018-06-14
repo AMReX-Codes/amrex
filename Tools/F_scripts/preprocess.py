00d636f0363337074e4729718bc67d685fa6cfe4
Michael Zingale <michael.zingale@stonybrook.edu>
2018-06-14 18:17:16 -0400

    remove the check on the gcc return code for now it works for gcc > 6.4.0 but not earlier

--- Tools/F_scripts/preprocess.py
+++ Tools/F_scripts/preprocess.py
@@ -58,10 +58,10 @@ class Preprocessor(object):
 
         stdout, rc = run(command, outfile=processed_file)
 
-        if rc == 0:
-            sf.cpp_name = processed_file
-        else:
-            raise ValueError("cpp process failed for {}".format(sf.name))
+        #if rc == 0:
+        sf.cpp_name = processed_file
+        #else:
+        #    raise ValueError("cpp process failed for {}".format(sf.name))
 
         return command
 
