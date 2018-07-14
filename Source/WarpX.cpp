d75e586fd23dd1ecfd0f02fe689dcbb05f3808ef
Maxence Thévenet <mthevenet@lbl.gov>
2018-07-14 11:41:02 -0700

    make maxwell_fdtd_solver an optional parameter. Default is yee

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -397,15 +397,20 @@ WarpX::ReadParameters ()
 	pp.query("charge_deposition", charge_deposition_algo);
 	pp.query("field_gathering", field_gathering_algo);
 	pp.query("particle_pusher", particle_pusher_algo);
-	std::string s_solver;
-	pp.get("maxwell_fdtd_solver", s_solver);
-	if (s_solver == "yee") {
-	   maxwell_fdtd_solver_id = 0;
-	} else if (s_solver == "ckc") {
-	   maxwell_fdtd_solver_id = 1;
-	} else {
-	   amrex::Abort("Unknown FDTD Solver type " + s_solver);
-	}
+	std::string s_solver = "";
+	pp.query("maxwell_fdtd_solver", s_solver);
+	// if maxwell_fdtd_solver is specified, set the value
+	// of maxwell_fdtd_solver_id accordingly.
+    // Otherwise keep the default value maxwell_fdtd_solver_id=0
+	if (s_solver != "") {
+      if (s_solver == "yee") {
+	    maxwell_fdtd_solver_id = 0;
+	  } else if (s_solver == "ckc") {
+	    maxwell_fdtd_solver_id = 1;
+	  } else {
+	    amrex::Abort("Unknown FDTD Solver type " + s_solver);
+	  }
+    }
     }
 
 #ifdef WARPX_USE_PSATD
