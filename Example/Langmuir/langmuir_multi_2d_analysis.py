87c4ba4481ba7883fa759c28eea5837063007be2
Remi Lehe <remi.lehe@normalesup.org>
2018-07-20 17:05:47 -0700

    Update test

--- Example/Langmuir/langmuir_multi_2d_analysis.py
+++ Example/Langmuir/langmuir_multi_2d_analysis.py
@@ -22,21 +22,18 @@ fn = sys.argv[1]
 epsilon = 0.01
 n = 4.e24
 n_osc_x = 2
-n_osc_y = 2
 n_osc_z = 2
-xmin = -20e-6; xmax = 20.e-6; Nx = 64
-ymin = -20e-6; ymax = 20.e-6; Ny = 64
-zmin = -20e-6; zmax = 20.e-6; Nz = 64
+xmin = -20e-6; xmax = 20.e-6; Nx = 128
+zmin = -20e-6; zmax = 20.e-6; Nz = 128
 
 # Wave vector of the wave
 kx = 2.*np.pi*n_osc_x/(xmax-xmin)
-ky = 2.*np.pi*n_osc_y/(ymax-ymin)
 kz = 2.*np.pi*n_osc_z/(zmax-zmin)
 # Plasma frequency
 wp = np.sqrt((n*e**2)/(m_e*epsilon_0))
 
-k = {'Ex':kx, 'Ey':ky, 'Ez':kz}
-cos = {'Ex': (0,1,1), 'Ey':(1,0,1), 'Ez':(1,1,0)}
+k = {'Ex':kx, 'Ez':kz}
+cos = {'Ex': (0,1,1), 'Ez':(1,1,0)}
 
 def get_contribution( is_cos, k ):
     du = (xmax-xmin)/Nx
@@ -50,12 +47,10 @@ def get_theoretical_field( field, t ):
     amplitude = epsilon * (m_e*c**2*k[field])/e * np.sin(wp*t)
     cos_flag = cos[field]
     x_contribution = get_contribution( cos_flag[0], kx )
-    y_contribution = get_contribution( cos_flag[1], ky )
     z_contribution = get_contribution( cos_flag[2], kz )
 
-    E = amplitude * x_contribution[:, np.newaxis, np.newaxis] \
-                  * y_contribution[np.newaxis, :, np.newaxis] \
-                  * z_contribution[np.newaxis, np.newaxis, :]
+    E = amplitude * x_contribution[:, np.newaxis ] \
+                  * z_contribution[np.newaxis, :]
 
     return( E )
 
@@ -68,7 +63,7 @@ data = ds.covering_grid(level=0, left_edge=ds.domain_left_edge,
 # Check the validity of the fields
 overall_max_error = 0
 for field in ['Ex', 'Ez']:
-    E_sim = data[field].to_ndarray()
+    E_sim = data[field].to_ndarray()[:,:,0]
     E_th = get_theoretical_field(field, t0)
     max_error = abs(E_sim-E_th).max()/abs(E_th).max()
     print('%s: Max error: %.2e' %(field,max_error))
@@ -76,15 +71,15 @@ for field in ['Ex', 'Ez']:
 
 # Plot the last field from the loop (Ez at iteration 40)
 plt.subplot2grid( (1,2), (0,0) )
-plt.imshow( E_sim[:,:] )
+plt.imshow( E_sim )
 plt.colorbar()
 plt.title('Ez, last iteration\n(simulation)')
 plt.subplot2grid( (1,2), (0,1) )
-plt.imshow( E_th[:,:] )
+plt.imshow( E_th )
 plt.colorbar()
 plt.title('Ez, last iteration\n(theory)')
 plt.tight_layout()
 plt.savefig('langmuir_multi_2d_analysis.png')
 
 # Automatically check the validity
-assert overall_max_error < 0.035
+assert overall_max_error < 0.02
