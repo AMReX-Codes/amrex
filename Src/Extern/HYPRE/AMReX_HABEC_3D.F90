914395fb1cf5f7d654cb4be627953e94b8dc8f81
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-07-25 13:59:39 -0700

    ij matrix for 3d all_regular eb

--- Src/Extern/HYPRE/AMReX_HABEC_3D.F90
+++ Src/Extern/HYPRE/AMReX_HABEC_3D.F90
@@ -423,6 +423,159 @@ contains
     end do
   end subroutine amrex_hpeb_copy_to_vec
 
+
+  subroutine amrex_hpeb_ijmatrix (lo, hi, nrows, ncols, rows, cols, mat, &
+       cell_id, clo, chi, cell_id_begin, a, alo, ahi, &
+       bx, bxlo, bxhi, by, bylo, byhi, bz, bzlo, bzhi, &
+       flag, flo, fhi, vfrc, vlo, vhi, &
+       apx, axlo, axhi, apy, aylo, ayhi, apz, azlo, azhi, &
+       fcx, fxlo, fxhi, fcy, fylo, fyhi, fcz, fzlo, fzhi, &
+       sa, sb, dx, bct, bcl, bho) &
+       bind(c,name='amrex_hpeb_ijmatrix')
+    use amrex_ebcellflag_module, only : is_covered_cell, is_regular_cell
+    integer(hypre_int), intent(in) :: nrows, cell_id_begin;
+    integer(hypre_int), dimension(0:nrows-1), intent(out) :: ncols, rows
+    integer(hypre_int), dimension(0:nrows*27-1), intent(out) :: cols
+    real(rt)          , dimension(0:nrows*27-1), intent(out) :: mat
+    integer, dimension(3), intent(in) :: lo, hi, clo, chi, alo, ahi, bxlo, bxhi, bylo, byhi, &
+         bzlo, bzhi, flo, fhi, vlo, vhi, axlo, axhi, aylo, ayhi, azlo, azhi, &
+         fxlo, fxhi, fylo, fyhi, fzlo, fzhi
+    integer(hypre_int), intent(in) :: cell_id( clo(1): chi(1), clo(2): chi(2), clo(3): chi(3))
+    real(rt)          , intent(in) :: a      ( alo(1): ahi(1), alo(2): ahi(2), alo(3): ahi(3))
+    real(rt)          , intent(in) :: bx     (bxlo(1):bxhi(1),bxlo(2):bxhi(2),bxlo(3):bxhi(3))
+    real(rt)          , intent(in) :: by     (bylo(1):byhi(1),bylo(2):byhi(2),bylo(3):byhi(3))
+    real(rt)          , intent(in) :: bz     (bzlo(1):bzhi(1),bzlo(2):bzhi(2),bzlo(3):bzhi(3))
+    integer           , intent(in) :: flag   ( flo(1): fhi(1), flo(2): fhi(2), flo(3): fhi(3))
+    real(rt)          , intent(in) :: vfrc   ( vlo(1): vhi(1), vlo(2): vhi(2), vlo(3): vhi(3))
+    real(rt)          , intent(in) :: apx    (axlo(1):axhi(1),axlo(2):axhi(2),axlo(3):axhi(3))
+    real(rt)          , intent(in) :: apy    (aylo(1):ayhi(1),aylo(2):ayhi(2),aylo(3):ayhi(3))
+    real(rt)          , intent(in) :: apz    (azlo(1):azhi(1),azlo(2):azhi(2),azlo(3):azhi(3))
+    real(rt)          , intent(in) :: fcx    (fxlo(1):fxhi(1),fxlo(2):fxhi(2),fxlo(3):fxhi(3),2)
+    real(rt)          , intent(in) :: fcy    (fylo(1):fyhi(1),fylo(2):fyhi(2),fylo(3):fyhi(3),2)
+    real(rt)          , intent(in) :: fcz    (fzlo(1):fzhi(1),fzlo(2):fzhi(2),fzlo(3):fzhi(3),2)
+    integer, intent(in) :: bct(0:5), bho
+    real(rt), intent(in) :: sa, sb, dx(3), bcl(0:5)
+
+    integer :: i,j,k, irow, imat, cdir, idim, ii,jj,kk, ioff, joff, koff
+    real(rt) :: fac(3), mat_tmp(-1:1,-1:1,-1:1)
+    real(rt) :: bf1(0:5), bf2(0:5), h, h2, h3, bflo(0:5)
+    real(rt) :: fracx, fracy, fracz, area, bc
+
+    fac = sb/dx**2
+
+    do cdir = 0, 5
+       if (cdir .eq. 0 .or. cdir .eq. 3) then
+          idim = 1
+       else if (cdir .eq. 1 .or. cdir .eq. 4) then
+          idim = 2
+       else
+          idim = 3
+       end if
+       h = dx(idim)
+       if (bct(cdir) .eq. amrex_lo_dirichlet) then
+          h2 = half * h
+          bflo(cdir) = fac(idim) * ( h / (bcl(cdir) + h2) - one)
+          if (bho.ge.1) then
+             h3 = three * h2
+             bf1(cdir) = fac(idim) * ((h3 - bcl(cdir)) / (bcl(cdir) + h2) - one)
+             bf2(cdir) = fac(idim) * (bcl(cdir) - h2) / (bcl(cdir) + h3)
+          else
+             bf1(cdir) = bflo(cdir)
+             bf2(cdir) = zero
+          end if
+       else if (bct(cdir) .eq. amrex_lo_neumann) then
+          bflo(cdir) = -fac(idim)
+          bf1(cdir) = -fac(idim)
+          bf2(cdir) = zero
+       end if
+    end do
+
+    irow = 0
+    imat = 0
+    do       k = lo(3), hi(3)
+       do    j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+             if (.not.is_covered_cell(flag(i,j,k))) then
+                rows(irow) = cell_id(i,j,k)
+                ncols(irow) = 0
+                mat_tmp = zero
+
+                if (is_regular_cell(flag(i,j,k))) then
+
+                   mat_tmp(0,0,0) = sa*a(i,j,k) + fac(1)*(bx(i,j,k)+bx(i+1,j,k)) &
+                        &                       + fac(2)*(by(i,j,k)+by(i,j+1,k)) &
+                        &                       + fac(3)*(bz(i,j,k)+bz(i,j,k+1))
+                   mat_tmp(-1, 0, 0) = -fac(1)*bx(i,j,k)
+                   mat_tmp( 1, 0, 0) = -fac(1)*bx(i+1,j,k)
+                   mat_tmp( 0,-1, 0) = -fac(2)*by(i,j,k)
+                   mat_tmp( 0, 1, 0) = -fac(2)*by(i,j+1,k)
+                   mat_tmp( 0, 0,-1) = -fac(3)*bz(i,j,k)
+                   mat_tmp( 0, 0, 1) = -fac(3)*bz(i,j,k+1)
+
+                   if (i.eq.lo(1) .and. cell_id(i-1,j,k).lt.0) then
+                      cdir = 0
+                      mat_tmp(0,0,0) = mat_tmp(0,0,0) + bf1(cdir)*bx(i,j,k)
+                      mat_tmp(-1,0,0) = zero
+                      mat_tmp(1,0,0) = mat_tmp(1,0,0) + bf2(cdir)*bx(i,j,k)
+                   end if
+
+                   if (i.eq.hi(1) .and. cell_id(i+1,j,k).lt.0) then
+                      cdir = 3
+                      mat_tmp(0,0,0) = mat_tmp(0,0,0) + bf1(cdir)*bx(i+1,j,k)
+                      mat_tmp(1,0,0) = zero
+                      mat_tmp(-1,0,0) = mat_tmp(-1,0,0) + bf2(cdir)*bx(i+1,j,k)
+                   end if
+
+                   if (j.eq.lo(2) .and. cell_id(i,j-1,k).lt.0) then
+                      cdir = 1
+                      mat_tmp(0,0,0) = mat_tmp(0,0,0) + bf1(cdir)*by(i,j,k)
+                      mat_tmp(0,-1,0) = zero
+                      mat_tmp(0,1,0) = mat_tmp(0,1,0) + bf2(cdir)*by(i,j,k)
+                   end if
+
+                   if (j.eq.hi(2) .and. cell_id(i,j+1,k).lt.0) then
+                      cdir = 4
+                      mat_tmp(0,0,0) = mat_tmp(0,0,0) + bf1(cdir)*by(i,j+1,k)
+                      mat_tmp(0,1,0) = zero
+                      mat_tmp(0,-1,0) = mat_tmp(0,-1,0) + bf2(cdir)*by(i,j+1,k)
+                   end if
+
+                   if (k.eq.lo(3) .and. cell_id(i,j,k-1).lt.0) then
+                      cdir = 2
+                      mat_tmp(0,0,0) = mat_tmp(0,0,0) + bf1(cdir)*bz(i,j,k)
+                      mat_tmp(0,0,-1) = zero
+                      mat_tmp(0,0,1) = mat_tmp(0,0,1) + bf2(cdir)*bz(i,j,k)
+                   end if
+
+                   if (k.eq.hi(3) .and. cell_id(i,j,k+1).lt.0) then
+                      mat_tmp(0,0,0) = mat_tmp(0,0,0) + bf1(cdir)*bz(i,j,k+1)
+                      mat_tmp(0,0,1) = zero
+                      mat_tmp(0,0,-1) = mat_tmp(0,0,-1) + bf2(cdir)*bz(i,j,k+1)
+                   end if
+                   
+                else
+                end if
+                
+                do koff = -1, 1
+                   do joff = -1, 1
+                      do ioff = -1, 1
+                         if (mat_tmp(ioff,joff,koff) .ne. zero) then
+                            ncols(irow) = ncols(irow) + 1
+                            cols(imat) = cell_id(i+ioff,j+joff,k+koff)
+                            mat(imat) = mat_tmp(ioff,joff,koff)
+                            imat = imat + 1
+                         end if
+                      end do
+                end do
+             end do
+             irow = irow + 1
+                
+             end if
+          end do
+       end do
+    end do
+  end subroutine amrex_hpeb_ijmatrix
+    
 #endif
 
 end module amrex_habec_module
