ef58fd6eb506c705b82870d17e34859eabe4c8e7
Luna Lin <y.lin2783@gmail.com>
2018-07-31 09:32:27 -0700

    Convert push_electric(magnetic)_field_x(y,z) subroutine to OpenACC version. Strip whitesopaces in acc/.

--- Tutorials/Particles/ElectromagneticPIC/acc/Evolve.cpp
+++ Tutorials/Particles/ElectromagneticPIC/acc/Evolve.cpp
@@ -5,7 +5,7 @@
 
 using namespace amrex;
 
-Real compute_dt(const Geometry& geom) 
+Real compute_dt(const Geometry& geom)
 {
     const static Real cfl = 1.0;
     const Real* dx = geom.CellSize();
@@ -21,42 +21,39 @@ void evolve_electric_field(      MultiFab& Ex,       MultiFab& Ey,       MultiFa
                            const Geometry& geom, Real dt)
 {
     BL_PROFILE("evolve_electric_field");
-    
+
     const Real mu_c2_dt = (PhysConst::mu0*PhysConst::c*PhysConst::c) * dt;
     const Real foo = (PhysConst::c*PhysConst::c) * dt;
-    
-    const Real* dx = geom.CellSize();    
+
+    const Real* dx = geom.CellSize();
     const std::array<Real,3> dtsdx_c2 {foo/dx[0], foo/dx[1], foo/dx[2]};
-    
+
     for (MFIter mfi(Ex); mfi.isValid(); ++mfi)
     {
         const Box& tbx  = mfi.tilebox(YeeGrid::Ex_nodal_flag);
         const Box& tby  = mfi.tilebox(YeeGrid::Ey_nodal_flag);
         const Box& tbz  = mfi.tilebox(YeeGrid::Ez_nodal_flag);
 
-        AMREX_FORT_LAUNCH(tbx, push_electric_field_x,
-                          BL_TO_FORTRAN_BOX(tbx),
-                          BL_TO_FORTRAN_3D(Ex[mfi]),
-                          BL_TO_FORTRAN_3D(By[mfi]),
-                          BL_TO_FORTRAN_3D(Bz[mfi]),
-                          BL_TO_FORTRAN_3D(jx[mfi]),
-                          mu_c2_dt, dtsdx_c2[1], dtsdx_c2[2]);
+        FTOC(push_electric_field_x)(BL_TO_FORTRAN_BOX(tbx),
+                                    BL_TO_FORTRAN_3D(Ex[mfi]),
+                                    BL_TO_FORTRAN_3D(By[mfi]),
+                                    BL_TO_FORTRAN_3D(Bz[mfi]),
+                                    BL_TO_FORTRAN_3D(jx[mfi]),
+                                    mu_c2_dt, dtsdx_c2[1], dtsdx_c2[2]);
 
-        AMREX_FORT_LAUNCH(tby, push_electric_field_y,
-                          BL_TO_FORTRAN_BOX(tby),
-                          BL_TO_FORTRAN_3D(Ey[mfi]),
-                          BL_TO_FORTRAN_3D(Bx[mfi]),
-                          BL_TO_FORTRAN_3D(Bz[mfi]),
-                          BL_TO_FORTRAN_3D(jy[mfi]),
-                          mu_c2_dt, dtsdx_c2[0], dtsdx_c2[2]);
+        FTOC(push_electric_field_y)(BL_TO_FORTRAN_BOX(tby),
+                                    BL_TO_FORTRAN_3D(Ey[mfi]),
+                                    BL_TO_FORTRAN_3D(Bx[mfi]),
+                                    BL_TO_FORTRAN_3D(Bz[mfi]),
+                                    BL_TO_FORTRAN_3D(jy[mfi]),
+                                    mu_c2_dt, dtsdx_c2[0], dtsdx_c2[2]);
 
-        AMREX_FORT_LAUNCH(tbz, push_electric_field_z,
-                          BL_TO_FORTRAN_BOX(tbz),
-                          BL_TO_FORTRAN_3D(Ez[mfi]),
-                          BL_TO_FORTRAN_3D(Bx[mfi]),
-                          BL_TO_FORTRAN_3D(By[mfi]),
-                          BL_TO_FORTRAN_3D(jz[mfi]),
-                          mu_c2_dt, dtsdx_c2[0], dtsdx_c2[1]);
+        FTOC(push_electric_field_z)(BL_TO_FORTRAN_BOX(tbz),
+                                    BL_TO_FORTRAN_3D(Ez[mfi]),
+                                    BL_TO_FORTRAN_3D(Bx[mfi]),
+                                    BL_TO_FORTRAN_3D(By[mfi]),
+                                    BL_TO_FORTRAN_3D(jz[mfi]),
+                                    mu_c2_dt, dtsdx_c2[0], dtsdx_c2[1]);
     }
 }
 
@@ -65,7 +62,7 @@ void evolve_magnetic_field(const MultiFab& Ex, const MultiFab& Ey, const MultiFa
                            const Geometry& geom, Real dt)
 {
     BL_PROFILE("evolve_magnetic_field");
-    
+
     const Real* dx = geom.CellSize();
     const std::array<Real,3> dtsdx {dt/dx[0], dt/dx[1], dt/dx[2]};
 
@@ -75,41 +72,38 @@ void evolve_magnetic_field(const MultiFab& Ex, const MultiFab& Ey, const MultiFa
         const Box& tby = mfi.tilebox(YeeGrid::By_nodal_flag);
         const Box& tbz = mfi.tilebox(YeeGrid::Bz_nodal_flag);
 
-        AMREX_FORT_LAUNCH(tbx, push_magnetic_field_x,
-                          BL_TO_FORTRAN_BOX(tbx),
-                          BL_TO_FORTRAN_3D(Bx[mfi]),
-                          BL_TO_FORTRAN_3D(Ey[mfi]),
-                          BL_TO_FORTRAN_3D(Ez[mfi]),
-                          dtsdx[1], dtsdx[2]);
-        
-        AMREX_FORT_LAUNCH(tby, push_magnetic_field_y,
-                          BL_TO_FORTRAN_BOX(tby),
-                          BL_TO_FORTRAN_3D(By[mfi]),
-                          BL_TO_FORTRAN_3D(Ex[mfi]),
-                          BL_TO_FORTRAN_3D(Ez[mfi]),
-                          dtsdx[0], dtsdx[2]);
-        
-        AMREX_FORT_LAUNCH(tbz, push_magnetic_field_z,
-                          BL_TO_FORTRAN_BOX(tbz),
-                          BL_TO_FORTRAN_3D(Bz[mfi]),
-                          BL_TO_FORTRAN_3D(Ex[mfi]),
-                          BL_TO_FORTRAN_3D(Ey[mfi]),
-                          dtsdx[0], dtsdx[1]);
-    }    
+        FTOC(push_magnetic_field_x)(BL_TO_FORTRAN_BOX(tbx),
+                                    BL_TO_FORTRAN_3D(Bx[mfi]),
+                                    BL_TO_FORTRAN_3D(Ey[mfi]),
+                                    BL_TO_FORTRAN_3D(Ez[mfi]),
+                                    dtsdx[1], dtsdx[2]);
+
+        FTOC(push_magnetic_field_y)(BL_TO_FORTRAN_BOX(tby),
+                                    BL_TO_FORTRAN_3D(By[mfi]),
+                                    BL_TO_FORTRAN_3D(Ex[mfi]),
+                                    BL_TO_FORTRAN_3D(Ez[mfi]),
+                                    dtsdx[0], dtsdx[2]);
+
+        FTOC(push_magnetic_field_z)(BL_TO_FORTRAN_BOX(tbz),
+                                    BL_TO_FORTRAN_3D(Bz[mfi]),
+                                    BL_TO_FORTRAN_3D(Ex[mfi]),
+                                    BL_TO_FORTRAN_3D(Ey[mfi]),
+                                    dtsdx[0], dtsdx[1]);
+    }
 }
 
 void fill_boundary_electric_field(MultiFab& Ex, MultiFab& Ey, MultiFab& Ez, const Geometry& geom)
-{    
+{
     const auto& period = geom.periodicity();
     Ex.FillBoundary(period);
     Ey.FillBoundary(period);
-    Ez.FillBoundary(period);    
+    Ez.FillBoundary(period);
 }
 
 void fill_boundary_magnetic_field(MultiFab& Bx, MultiFab& By, MultiFab& Bz, const Geometry& geom)
-{    
+{
     const auto& period = geom.periodicity();
     Bx.FillBoundary(period);
     By.FillBoundary(period);
-    Bz.FillBoundary(period);    
+    Bz.FillBoundary(period);
 }
