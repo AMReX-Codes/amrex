a47ceafe0d46c0743b418f961c6d7f3f39fefeef
Maxence Thevenet <mthevenet@lbl.gov>
2018-08-28 09:05:52 -0700

    fix issue with filename

--- Example/PML/analysis_pml.py
+++ Example/PML/analysis_pml.py
@@ -4,26 +4,17 @@ import yt ; yt.funcs.mylog.setLevel(0)
 import numpy as np
 import scipy.constants as scc
 
-plotfiles = 'plotfiles'
-#################################
-### READ INITIAL LASER ENERGY ###
-#################################
-ds = yt.load( './' + plotfiles + '/plt00100' )
-all_data_level_0 = ds.covering_grid(level=0,left_edge=ds.domain_left_edge, dims=ds.domain_dimensions)
-Bx = all_data_level_0['boxlib', 'Bx'].v.squeeze()
-By = all_data_level_0['boxlib', 'By'].v.squeeze()
-Bz = all_data_level_0['boxlib', 'Bz'].v.squeeze()
-Ex = all_data_level_0['boxlib', 'Ex'].v.squeeze()
-Ey = all_data_level_0['boxlib', 'Ey'].v.squeeze()
-Ez = all_data_level_0['boxlib', 'Ez'].v.squeeze()
-energyE = np.sum(scc.epsilon_0/2*(Ex**2+Ey**2+Ez**2))
-energyB = np.sum(1./scc.mu_0/2*(Bx**2+By**2+Bz**2))
-energy_start = energyE + energyB
+filename = sys.argv[1]
 
-###############################
-### READ FINAL LASER ENERGY ###
-###############################
-ds = yt.load( './' + plotfiles + '/plt00300' )
+############################
+### INITIAL LASER ENERGY ###
+############################
+energy_start = 9.1301289517e-08
+
+##########################
+### FINAL LASER ENERGY ###
+##########################
+ds = yt.load( filename )
 all_data_level_0 = ds.covering_grid(level=0,left_edge=ds.domain_left_edge, dims=ds.domain_dimensions)
 Bx = all_data_level_0['boxlib', 'Bx'].v.squeeze()
 By = all_data_level_0['boxlib', 'By'].v.squeeze()
