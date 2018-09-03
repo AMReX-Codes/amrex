6580f739038a0ecc7a9d4549fe79f65933ab9d79
Michael Zingale <michael.zingale@stonybrook.edu>
2018-09-03 12:13:50 -0400

    split out preprocess like we do in GPU

--- Tools/F_scripts/dep.py
+++ Tools/F_scripts/dep.py
@@ -37,6 +37,7 @@ import re
 import os
 import argparse
 import subprocess
+import preprocess
 
 # modules to ignore in the dependencies
 IGNORES = ["iso_c_binding", "iso_fortran_env", "omp_lib", "mpi"]
@@ -56,70 +57,6 @@ module_proc_re = re.compile(r"(\s*)(module)(\s+)(procedure)(\s+)((?:[a-z][a-z_0-
 # see (txt2re.com)
 use_re = re.compile(r"^(\s*)([Uu][Ss][Ee])(\s+)((?:[a-z_][a-z_0-9]+))",
                     re.IGNORECASE|re.DOTALL)
-
-
-def run(command, stdin=False, outfile=None):
-    """ run a command in the unix shell """
-
-    sin = None
-    if stdin: sin = subprocess.PIPE
-    p0 = subprocess.Popen(command, stdin=sin, stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT, shell=True)
-
-    stdout0 = p0.communicate()
-    if stdin: p0.stdin.close()
-    rc = p0.returncode
-    p0.stdout.close()
-
-    if outfile is not None:
-        try: cf = io.open(outfile, "w", encoding="latin-1")
-        except IOError:
-            sys.exit("ERROR: unable to open file for writing: {}".format(outfile))
-        else:
-            for line in stdout0:
-                if line is not None:
-                    cf.write(line.decode('latin-1'))
-            cf.close()
-
-    return stdout0, rc
-
-
-class Preprocessor(object):
-    """ hold the information about preprocessing """
-
-    def __init__(self, temp_dir=None, cpp_cmd=None,
-                 defines=None, f90_preprocess=None):
-
-        self.temp_dir = temp_dir
-        self.cpp_cmd = cpp_cmd
-        self.defines = defines
-        self.f90_preprocess = f90_preprocess
-
-    def preprocess(self, sf):
-        """ preprocess the file described by a SourceFile object sf """
-
-        # we want to do:
-        # $(FORT_CPP) $(CPPFLAGS) $< | $(F90PREP) > $(f77TempDir)/$*.f90
-        # we output to the temporary directory
-
-        processed_file = "{}/F90PP-{}".format(self.temp_dir,
-                                             os.path.basename(sf.name))
-
-        if self.f90_preprocess != "":
-            command = "{} {} {} | {}".format(self.cpp_cmd, self.defines,
-                                             sf.name, self.f90_preprocess)
-        else:
-            command = "{} {} {}".format(self.cpp_cmd, self.defines,
-                                        sf.name)
-
-        stdout, rc = run(command, outfile=processed_file)
-
-        if rc == 0:
-            sf.cpp_name = processed_file
-        else:
-            raise ValueError("cpp process failed for {}".format(sf.name))
-
-        return command
 
 
 class SourceFile(object):
@@ -343,8 +280,8 @@ if __name__ == "__main__":
 
     # create a preprocessor object
     if args.cpp != "":
-        cpp_pass = Preprocessor(temp_dir=temp_dir, cpp_cmd=args.cpp,
-                                defines=args.defines, f90_preprocess=args.f90_preprocess)
+        cpp_pass = preprocess.Preprocessor(temp_dir=temp_dir, cpp_cmd=args.cpp,
+                                           defines=args.defines, f90_preprocess=args.f90_preprocess)
     else:
         cpp_pass = None
 
