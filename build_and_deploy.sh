c9748733bc2d6519a06d0d085797f1193b9f2077
Andrew Myers <atmyers2@gmail.com>
2018-09-13 12:55:18 -0700

    try to use 3.6 instead of 3.4

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -46,9 +46,9 @@ mv Docs/Doxygen/html/* out/docs_html/doxygen/
 
 # now do sphinx
 cd Docs/sphinx_documentation
-make SPHINX_BUILD="python3 -msphinx" html
+make SPHINX_BUILD="python3.6 -msphinx" html
 cd ../sphinx_tutorials
-make SPHINX_BUILD="python3 -msphinx" html
+make SPHINX_BUILD="python3.6 -msphinx" html
 cd ../../
 
 mv Docs/sphinx_documentation/build/html/* out/docs_html/
