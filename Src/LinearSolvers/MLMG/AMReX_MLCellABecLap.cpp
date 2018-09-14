729f866edaa63eb22b4f78fba1d515633090442b
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-09-14 11:33:32 -0700

    move getFluxes from MLMG to MLCellABecLap

--- Src/LinearSolvers/MLMG/AMReX_MLCellABecLap.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLCellABecLap.cpp
@@ -31,4 +31,24 @@ MLCellABecLap::update ()
     if (MLCellLinOp::needsUpdate()) MLCellLinOp::update();
 }
 
+void
+MLCellABecLap::getFluxes (const Vector<Array<MultiFab*,AMREX_SPACEDIM> >& a_flux,
+                          const Vector<MultiFab*>& a_sol,
+                          Location a_loc) const
+{
+    BL_PROFILE("MLMG::getFluxes()");
+
+    const Real betainv = 1.0 / getBScalar();
+    const int nlevs = NAMRLevels();
+    for (int alev = 0; alev < nlevs; ++alev) {
+        compFlux(alev, a_flux[alev], *a_sol[alev], a_loc);
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+            unapplyMetricTerm(alev, 0, *a_flux[alev][idim]);
+            if (betainv != 1.0) {
+                a_flux[alev][idim]->mult(betainv);
+            }
+        }
+    }
+}
+
 }
