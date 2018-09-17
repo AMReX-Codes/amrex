6963ebf53a03d3e4a589d1092bd1eb438ac79dd5
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-09-17 16:59:18 -0700

    tweak macros

--- Src/Base/AMReX_BaseFab_c.c
+++ Src/Base/AMReX_BaseFab_c.c
@@ -5,14 +5,16 @@
     const int kstride_##q = jstride_##q*(qhi[1]-qlo[1]+1); \
     const int nstride_##q = kstride_##q*(qhi[2]-qlo[2]+1);
 
-#define INDEX(q,i,j,k,n) \
-    i-q##lo[0] + (j-q##lo[1])*jstride_##q + (k-q##lo[2])*kstride_##q + n*nstride_##q
+#define INDEX(q,i,j,k) \
+    i-q##lo[0] + (j-q##lo[1])*jstride_##q + (k-q##lo[2])*kstride_##q
+#define INDEX_N(q,i,j,k,n) \
+    INDEX(q,i,j,k) + n*nstride_##q
 
-#define INDEX_OFFSET(q,i,j,k,n,off) \
-    i-q##lo[0]+off[0] + (j-q##lo[1]+off[1])*jstride_##q + (k-q##lo[2]+off[2])*kstride_##q + n*nstride_##q
+#define GET_VALUE(q,i,j,k) q[INDEX(q,i,j,k)]
+#define GET_VALUE_N(q,i,j,k,n) q[INDEX_N(q,i,j,k,n)]
 
-#define GET_VALUE(q,i,j,k,n) q[INDEX(q,i,j,k,n)]
-#define GET_VALUE_OFFSET(q,i,j,k,n,off) q[INDEX_OFFSET(q,i,j,k,n,off)]
+#define GET_PTR(q,i,j,k) q+INDEX(q,i,j,k)
+#define GET_PTR_N(q,i,j,k,n) q+INDEX_N(q,i,j,k,n)
 
 amrex_real
 amrex_c_fab_dot (const int* lo, const int* hi,
@@ -29,7 +31,7 @@ amrex_c_fab_dot (const int* lo, const int* hi,
         for (int k = lo[2]; k <= hi[2]; ++k) {
             for (int j = lo[1]; j <= hi[1]; ++j) {
                 for (int i = lo[0]; i <= hi[0]; ++i) {
-                    r = r + GET_VALUE(x,i,j,k,n) * GET_VALUE_OFFSET(y,i,j,k,n,yblo);
+                    r = r + GET_VALUE_N(x,i,j,k,n) * GET_VALUE_N(y,i+yblo[0],j+yblo[1],k+yblo[2],n);
                 }
             }
         }
