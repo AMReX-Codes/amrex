ed042b43a3d6ab05f3d24c458c7bdb1bf66a7684
Andrew Myers <atmyers2@gmail.com>
2018-09-25 16:49:32 -0700

    also give the pml version of f the right number of ghost cells

--- Source/WarpXPML.cpp
+++ Source/WarpXPML.cpp
@@ -346,7 +346,8 @@ PML::PML (const BoxArray& grid_ba, const DistributionMapping& grid_dm,
     int nge = 2;
     int ngb = 2;
     int ngf = (do_moving_window) ? 2 : 0;
-
+    if (WarpX::maxwell_fdtd_solver_id == 1) ngf = std::max( ngf, 1 );
+    
     pml_E_fp[0].reset(new MultiFab(amrex::convert(ba,WarpX::Ex_nodal_flag), dm, 3, nge));
     pml_E_fp[1].reset(new MultiFab(amrex::convert(ba,WarpX::Ey_nodal_flag), dm, 3, nge));
     pml_E_fp[2].reset(new MultiFab(amrex::convert(ba,WarpX::Ez_nodal_flag), dm, 3, nge));
