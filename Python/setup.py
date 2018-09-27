635d8784fa40ca3644a9eecf24974d768c6155c8
Remi Lehe <remi.lehe@normalesup.org>
2018-09-27 10:09:57 -0700

    Switch from distutils to setuptools and add dependency

--- Python/setup.py
+++ Python/setup.py
@@ -7,12 +7,7 @@ setup.py file for WarpX
 import sys
 import argparse
 
-from distutils.core import setup
-
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-    from distutils.command.build_py import build_py
+from setuptools import setup
 
 argparser = argparse.ArgumentParser(add_help=False)
 argparser.add_argument('--with-libwarpx', type=int, default=None, help='Install libwarpx with the given value as DIM. This option is only used by the makefile.')
@@ -29,5 +24,5 @@ setup (name = 'pywarpx',
        package_dir = {'pywarpx':'pywarpx'},
        description = """Wrapper of WarpX""",
        package_data = package_data,
-       cmdclass={'build_py': build_py}
+       install_requires=['picmistandard']
        )
