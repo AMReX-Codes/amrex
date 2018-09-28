62be6d6affd053b9a4f130a8d4fc662693c3c5ce
Guy Moore <gmoore@lbl.gov>
2018-09-28 10:11:00 -0700

    First commit of SWFFT_simple tutorial, in which only a forward FFT is performed

---
+++ Tutorials/SWFFT_simple/main.cpp
@@ -0,0 +1,16 @@
+#include <AMReX.H>
+#include <SWFFT_Test.H>
+
+int main (int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+    
+    {
+        BL_PROFILE("main()");
+        SWFFT_Test sw_test;
+        sw_test.solve();
+    }
+    
+    amrex::Finalize();
+    return 0;
+}
