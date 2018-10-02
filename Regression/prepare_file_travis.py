43bfeb8c10bef68fe0c68f0d37d4088457429f7a
Remi Lehe <remi.lehe@normalesup.org>
2018-10-02 11:39:02 -0700

    Enable Travis CI (#3)
    * Switch from Shippable to Travis
    
    * Remove .shippable
    
    * Update dependencies
    
    * Update path to miniconda
    
    * Update paths
    
    * Update travis CI paths
    
    * Update paths
    
    * Correct path
    
    * Update Travis CI
    
    * Final clean-up for Travis tests
    
    * Correct path
    
    * Correct paths
    
    * Change Linux distribution
    
    * Run 2D and 3D on different workers
    
    * Modify spacing

--- Regression/prepare_file_travis.py
+++ Regression/prepare_file_travis.py
@@ -1,10 +1,17 @@
 # This script modifies `WarpX-test.ini` (which is used for nightly builds)
-# and creates the file `shippable-test.ini` (which is used for continous
-# integration on Shippable (https://app.shippable.com/))
+# and creates the file `travis-test.ini` (which is used for continous
+# integration on TravisCI (https://travis-ci.org/)
+# The subtests that are selected are controlled by the environement WARPX_DIM
 import re
+import os
+# Get relevant environment variables
+dim = os.environ.get('WARPX_DIM', None)
 
 with open('WarpX-tests.ini') as f:
     text = f.read()
+
+# Replace default folder name
+text = re.sub('/home/regtester/AMReX_RegTesting/', '/home/travis/', text)
 
 # Add doComparison = 0 for each test
 text = re.sub( '\[(?P<name>.*)\]\nbuildDir = ',
@@ -13,7 +20,7 @@ text = re.sub( '\[(?P<name>.*)\]\nbuildDir = ',
 # Use only 2 cores for compiling
 text = re.sub( 'numMakeJobs = \d+', 'numMakeJobs = 2', text )
 
-# Remove Python test (does not compile in the Docker container)
+# Remove Python test (does not compile)
 text = re.sub( '\[Python_Langmuir\]\n(.+\n)*', '', text)
 
 # Remove Langmuir_x/y/z test (too long; not that useful)
@@ -21,8 +28,13 @@ text = re.sub( '\[Langmuir_[xyz]\]\n(.+\n)*', '', text)
 # Skip unit tests (too long; not that useful)
 text = re.sub( '\[UnitTest_[a-zA-Z]+\]\n(.+\n)*', '', text)
 
+# Remove tests that do not have the right dimension
+if dim is not None:
+    print('Selecting tests with dim = %s' %dim)
+    text = re.sub('\[.+\n(.+\n)*dim = [^%s]\n(.+\n)*' %dim, '', text)
+
 # Prevent emails from being sent
 text = re.sub( 'sendEmailWhenFail = 1', 'sendEmailWhenFail = 0', text )
 
-with open('shippable-tests.ini', 'w') as f:
+with open('travis-tests.ini', 'w') as f:
     f.write(text)
