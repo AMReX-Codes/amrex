03e61ad58135e8cb8f8fa6a954d447d3551fba10
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-10-09 16:09:55 -0700

    revert changes to the_arena

--- Src/Base/AMReX_Arena.cpp
+++ Src/Base/AMReX_Arena.cpp
@@ -1,58 +1,15 @@
 
-#include <memory>
-#include <AMReX.H>
 #include <AMReX_Arena.H>
-#include <AMReX_BArena.H>
-#include <AMReX_CArena.H>
+#include <AMReX.H>
 
-namespace amrex {
+const unsigned int amrex::Arena::align_size;
 
-namespace {
-  static std::unique_ptr<Arena> the_arena;
-}
-
-#if !defined(AMREX_FORTRAN_BOXLIB)
-void
-Arena_Initialize()
-{
-#ifdef BL_COALESCE_FABS
-    the_arena.reset(new CArena);
-#else
-    the_arena.reset(new BArena);
-#endif
-
-#ifdef AMREX_USE_CUDA
-    the_arena->SetPreferred();
-#endif
-
-    amrex::ExecOnFinalize(amrex::Arena_Finalize);
-}
-
-void
-Arena_Finalize()
-{
-    the_arena.reset();
-}
-#endif
-
-Arena*
-The_Arena ()
-{
-    BL_ASSERT(the_arena != 0);
-
-    return the_arena.get();
-}
-
-const unsigned int Arena::align_size;
-
-Arena::~Arena () {}
+amrex::Arena::~Arena () {}
 
 std::size_t
-Arena::align (std::size_t s)
+amrex::Arena::align (std::size_t s)
 {
     std::size_t x = s + (align_size-1);
     x -= x & (align_size-1);
     return x;
-}
-
 }
