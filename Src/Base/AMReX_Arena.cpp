c4ef60b1cb2eba17a6ec4de3b77d4126f3184564
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-10-25 20:24:03 -0400

    reorganize arenas. Managed class uses the managed arena. remove BaseFab Fortran functions

--- Src/Base/AMReX_Arena.cpp
+++ Src/Base/AMReX_Arena.cpp
@@ -5,13 +5,13 @@
 
 #ifndef AMREX_FORTRAN_BOXLIB
 #include <AMReX.H>
+#include <AMReX_Print.H>
+#include <AMReX_ParallelDescriptor.H>
 #endif
 
 namespace amrex {
 
 namespace {
-    // The_Arena and The_Nvar_Arena are initialized in BaseFab
-    Arena* the_cuda_arena = nullptr;
     bool initialized = false;
 }
 
@@ -42,25 +42,55 @@ void
 Arena::Finalize ()
 {
 #ifndef AMREX_FORTRAN_BOXLIB
-    delete the_cuda_arena;
-    the_cuda_arena = nullptr;
     initialized = false;
-#endif
-}
-
-Arena*
-The_Cuda_Arena ()
-{
-    if (the_cuda_arena == nullptr) {
-#if AMREX_USE_CUDA
-        the_cuda_arena = new CArena;
-        the_cuda_arena->SetDeviceMemory();
-#else
-        the_cuda_arena = new BArena;
-#endif
+    if (amrex::Verbose() > -1) {
+        const int IOProc   = ParallelDescriptor::IOProcessorNumber();
+        if (The_Arena()) {
+            CArena* p = dynamic_cast<CArena*>(The_Arena());
+            if (p) {
+                long min_kilobytes = p->heap_space_used() / 1024;
+                long max_kilobytes = min_kilobytes;
+                ParallelDescriptor::ReduceLongMin(min_kilobytes, IOProc);
+                ParallelDescriptor::ReduceLongMax(max_kilobytes, IOProc);
+                amrex::Print() << "[The Arena] space (kilobyte) used spread across MPI: ["
+                               << min_kilobytes << " ... " << max_kilobytes << "]\n";
+            }
+        }
+        if (The_Cuda_Arena()) {
+            CArena* p = dynamic_cast<CArena*>(The_Cuda_Arena());
+            if (p) {
+                long min_kilobytes = p->heap_space_used() / 1024;
+                long max_kilobytes = min_kilobytes;
+                ParallelDescriptor::ReduceLongMin(min_kilobytes, IOProc);
+                ParallelDescriptor::ReduceLongMax(max_kilobytes, IOProc);
+                amrex::Print() << "[The CUDA Arena] space (kilobyte) used spread across MPI: ["
+                               << min_kilobytes << " ... " << max_kilobytes << "]\n";
+            }
+        }
+        if (The_Managed_Arena()) {
+            CArena* p = dynamic_cast<CArena*>(The_Managed_Arena());
+            if (p) {
+                long min_kilobytes = p->heap_space_used() / 1024;
+                long max_kilobytes = min_kilobytes;
+                ParallelDescriptor::ReduceLongMin(min_kilobytes, IOProc);
+                ParallelDescriptor::ReduceLongMax(max_kilobytes, IOProc);
+                amrex::Print() << "[The Managed Arena] space (kilobyte) used spread across MPI: ["
+                               << min_kilobytes << " ... " << max_kilobytes << "]\n";
+            }
+        }
+        if (The_Pinned_Arena()) {
+            CArena* p = dynamic_cast<CArena*>(The_Pinned_Arena());
+            if (p) {
+                long min_kilobytes = p->heap_space_used() / 1024;
+                long max_kilobytes = min_kilobytes;
+                ParallelDescriptor::ReduceLongMin(min_kilobytes, IOProc);
+                ParallelDescriptor::ReduceLongMax(max_kilobytes, IOProc);
+                amrex::Print() << "[The Pinned Arena] space (kilobyte) used spread across MPI: ["
+                               << min_kilobytes << " ... " << max_kilobytes << "]\n";
+            }
+        }
     }
-
-    return the_cuda_arena;
+#endif
 }
 
 }
