3808405a6a51a4a29235b5c897f5e7cf0ddeb8ed
Lawrence Minion <mlminion@lbl.gov>
2018-10-29 14:12:56 -0700

    Added multi-implicit capability

--- Src/SDC/AMReX_SDCstruct.cpp
+++ Src/SDC/AMReX_SDCstruct.cpp
@@ -18,7 +18,10 @@ SDCstruct::SDCstruct(int Nnodes_in,int Npieces_in, MultiFab& sol_in)
   sol.resize(Nnodes);
   res.resize(Nnodes);
   f.resize(Npieces);
+  if (Npieces == 3) 
+    Ithree.resize(Nnodes);
 
+  //  Assign  geomety and multifab info
   const BoxArray &ba=sol_in.boxArray();
   const DistributionMapping &dm=sol_in.DistributionMap();
   const int Nghost=sol_in.nGrow();
@@ -33,6 +36,8 @@ SDCstruct::SDCstruct(int Nnodes_in,int Npieces_in, MultiFab& sol_in)
 	{
 	  f[i][sdc_m].define(ba, dm, Ncomp, Nghost);
 	}
+      if (Npieces == 3)
+	Ithree[sdc_m].define(ba, dm, Ncomp, Nghost);      
     }
   
 }
@@ -48,12 +53,25 @@ void SDCstruct::SDC_rhs_integrals(Real dt)
       for ( MFIter mfi(res[sdc_m]); mfi.isValid(); ++mfi )
 	{
 	  res[sdc_m].setVal(0.0);
+	  if (Npieces == 3)
+	    Ithree[sdc_m].setVal(0.0);
 	  for (int sdc_n = 0; sdc_n < Nnodes; sdc_n++)
 	    {
-	      qij = dt*(qmats[0][sdc_m][sdc_n]-qmats[2][sdc_m][sdc_n]);
-	      res[sdc_m][mfi].saxpy(qij,f[1][sdc_n][mfi]);
 	      qij = dt*(qmats[0][sdc_m][sdc_n]-qmats[1][sdc_m][sdc_n]);
 	      res[sdc_m][mfi].saxpy(qij,f[0][sdc_n][mfi]);
+	      qij = dt*(qmats[0][sdc_m][sdc_n]-qmats[2][sdc_m][sdc_n]);
+	      res[sdc_m][mfi].saxpy(qij,f[1][sdc_n][mfi]);
+	    }
+	  if (Npieces == 3)
+	    {
+	      for (int sdc_n = 0; sdc_n < Nnodes; sdc_n++)
+		{ //  // MISDC pieces
+		  qij = dt*(qmats[0][sdc_m][sdc_n]);  // leave off -dt*Qtil and add it later
+		  res[sdc_m][mfi].saxpy(qij,f[2][sdc_n][mfi]);
+		  // Compute seperate integral for f_3 piece		  
+		  qij = -dt*(qmats[2][sdc_m][sdc_n]);  
+		  Ithree[sdc_m][mfi].saxpy(qij,f[2][sdc_n][mfi]);
+		}
 	    }
 	}
     }
@@ -61,6 +79,7 @@ void SDCstruct::SDC_rhs_integrals(Real dt)
 
 void SDCstruct::SDC_rhs_k_plus_one(MultiFab& sol_new, Real dt,int sdc_m)
 {
+  //  Compute the rhs terms for the implicit solve
   Real qij;
   
   //  Copy first the initial value
@@ -78,6 +97,23 @@ void SDCstruct::SDC_rhs_k_plus_one(MultiFab& sol_new, Real dt,int sdc_m)
 	{
 	  qij = dt*qmats[2][sdc_m][sdc_n];
 	  sol_new[mfi].saxpy(qij,f[1][sdc_n][mfi]);
+	}
+    }
+  
+}
+void SDCstruct::SDC_rhs_misdc(MultiFab& sol_new, Real dt,int sdc_m)
+{
+  //  Add the terms to the rhs before the second implicit solve
+  Real qij;
+  
+  for ( MFIter mfi(sol_new); mfi.isValid(); ++mfi )
+    {
+      const Box& bx = mfi.validbox();
+      sol_new[mfi].saxpy(1.0,Ithree[sdc_m][mfi]);
+      for (int sdc_n = 0; sdc_n < sdc_m+1; sdc_n++)
+	{
+	  qij = dt*qmats[2][sdc_m][sdc_n];
+	  sol_new[mfi].saxpy(qij,f[2][sdc_n][mfi]);
 	}
     }
   
