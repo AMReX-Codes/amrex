9d921899f75417a174f1b2b9489aef90122ecfc2
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-10-30 13:11:44 -0700

    implement DeviceFab

--- Src/Base/AMReX_CudaFab.cpp
+++ Src/Base/AMReX_CudaFab.cpp
@@ -1,24 +1,69 @@
 
+#include <AMReX.H>
 #include <AMReX_CudaFab.H>
-#include <AMReX_FArrayBox.H>
+#include <AMReX_CudaFabImpl.H>
+#include <AMReX_CudaDevice.H>
+
+#ifdef AMREX_USE_CUDA
+extern "C" {
+    void CUDART_CB amrex_devicefab_delete (cudaStream_t stream, cudaError_t error, void* p) {
+        delete (amrex::Cuda::DeviceFabImpl*)p;
+    }
+}
+#endif
 
 namespace amrex {
 namespace Cuda {
 
+void
+DeviceFab::Initialize ()
+{
+    DeviceFabImpl::Initialize();
+    amrex::ExecOnFinalize(DeviceFab::Finalize);
+}
+
+void
+DeviceFab::Finalize ()
+{
+    DeviceFabImpl::Finalize();
+}
+
 DeviceFab::DeviceFab ()
 {
-    static_assert(AMREX_IS_TRIVIALLY_COPYABLE(BaseFabData<Real>),
-                  "BaseFabData must be trivially copyable");
-//    m_fab = new FArrayBox();
+    m_impl.reset(new DeviceFabImpl());
 }
 
 DeviceFab::DeviceFab (Box const& bx, int ncomp)
 {
+    m_impl.reset(new DeviceFabImpl(bx,ncomp));
+}
+
+void
+DeviceFab::resize (Box const& bx, int ncomp)
+{
+    m_impl->resize(bx, ncomp);
+}
+
+FArrayBox*
+DeviceFab::fabPtr ()
+{
+    return m_impl->fabPtr();
 }
 
 DeviceFab::~DeviceFab ()
 {
-
+#ifdef AMREX_USE_CUDA
+    if (inLaunchRegion())
+    {
+        DeviceFabImpl* p = m_impl.release();
+// CUDA 10        CudaAPICheck(cudaLaunchHostFunc(Device::cudaStream(), amrex_devicefab_delete, p));
+        CudaAPICheck(cudaStreamAddCallback(Device::cudaStream(), amrex_devicefab_delete, p, 0));
+    }
+    else
+#endif
+    {
+        m_impl.reset();
+    }
 }
 
 }
