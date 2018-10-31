6346c5074829c9790f20f6250149100c97c8fd0f
Andrew Myers <atmyers2@gmail.com>
2018-10-31 15:44:11 -0700

    use enough ghost cells in the redistribute mask when using the moving window

--- Source/WarpXEvolve.cpp
+++ Source/WarpXEvolve.cpp
@@ -144,10 +144,12 @@ WarpX::EvolveEM (int numsteps)
         bool move_j = is_synchronized || to_make_plot || do_insitu;
         // If is_synchronized we need to shift j too so that next step we can evolve E by dt/2.
         // We might need to move j because we are going to make a plotfile.
-	MoveWindow(move_j);
 
+	int num_moved = MoveWindow(move_j);
+        
         if (max_level == 0) {
-            mypc->RedistributeLocal();
+            int num_redistribute_ghost = num_moved + 1;
+            mypc->RedistributeLocal(num_redistribute_ghost);
         }
         else {
             mypc->Redistribute();
