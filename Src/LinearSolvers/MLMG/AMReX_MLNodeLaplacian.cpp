e7a7de58b7ad03d817b553a5842dfa2b92c89bb8
Ann Almgren <asalmgren@lbl.gov>
2018-10-30 18:28:53 -0700

    In preparation for being able to use Algoim to construct the 3d EB nodal integrals for use in the 3d nodal projection

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian.cpp
@@ -1,8 +1,15 @@
 #include <limits>
+#endif
 
 #include <AMReX_MLNodeLaplacian.H>
 #include <AMReX_MLNodeLap_F.H>
 #include <AMReX_MultiFabUtil.H>
+
+#ifdef AMREX_USE_EB
+#ifdef USE_ALGOIM
+#include <Algoim_integrals.H>
+#endif
+#endif
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -78,8 +85,15 @@ MLNodeLaplacian::define (const Vector<Geometry>& a_geom,
     m_integral.resize(m_num_amr_levels);
     for (int amrlev = 0; amrlev < m_num_amr_levels; ++amrlev)
     {
+#ifdef AMREX_USE_EB
+        m_integral[amrlev].reset
+            (new MultiFab(m_grids[amrlev][0], m_dmap[amrlev][0], ncomp_i, 1,
+                          MFInfo(), *m_factory[amrlev][0]));
+
+#else
         m_integral[amrlev].reset
             (new MultiFab(m_grids[amrlev][0], m_dmap[amrlev][0], ncomp_i, 1));
+#endif
     }
 #endif
 
@@ -2169,7 +2183,6 @@ MLNodeLaplacian::reflux (int crse_amrlev,
 }
 
 #ifdef AMREX_USE_EB
-
 void
 MLNodeLaplacian::buildIntegral ()
 {
@@ -2179,6 +2192,7 @@ MLNodeLaplacian::buildIntegral ()
 
     m_integral_built = true;
 
+#if (AMREX_SPACEDIM == 2)
     for (int amrlev = 0; amrlev < m_num_amr_levels; ++amrlev)
     {
         MultiFab* intg = m_integral[amrlev].get();
@@ -2219,6 +2233,17 @@ MLNodeLaplacian::buildIntegral ()
             }
         }
     }
+#else
+#ifdef USE_ALGOIM
+    for (int amrlev = 0; amrlev < m_num_amr_levels; ++amrlev)
+    {
+        MultiFab* intg = m_integral[amrlev].get();
+        amrex::compute_integrals(intg);
+    }
+#else
+    amrex::Abort("Need to set USE_ALGOIM = TRUE in order to build 3D EB integrals");
+#endif
+#endif
 }
 
 #endif
