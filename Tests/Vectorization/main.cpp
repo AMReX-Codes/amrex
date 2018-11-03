64efcfa03bdc4a60688d17d4c9f954994b3b2286
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-11-03 12:06:37 -0700

    add non-simd version

--- Tests/Vectorization/main.cpp
+++ Tests/Vectorization/main.cpp
@@ -1,27 +1,10 @@
 
 #include <AMReX.H>
 #include <AMReX_Print.H>
-#include <AMReX_VisMF.H>
-#include <AMReX_MultiFab.H>
-#include <AMReX_BLFort.H>
+
+#include <kdecl.H>
 
 using namespace amrex;
-
-void flux_to_dudt_c (Box const& bx,
-                     FArrayBox& dudtfab,
-                     FArrayBox const& fxfab,
-                     FArrayBox const& fyfab,
-         FArrayBox const& fzfab,
-                     Array<Real,AMREX_SPACEDIM> const& dxinv);
-
-extern "C" {
-    void flux_to_dudt_f (const int* lo, const int* hi,
-                         Real* dudt, const int* ulo, const int* uhi,
-                         const Real* fx, const int* xlo, const int* xhi,
-                         const Real* fy, const int* ylo, const int* yhi,
-                         const Real* fz, const int* zlo, const int* zhi,
-                         const Real* dxinv, const int* ncomp);
-}
 
 int main(int argc, char* argv[])
 {
@@ -40,29 +23,41 @@ int main(int argc, char* argv[])
 
         Array<Real,3> dxinv {10.,10.,10.};
 
-        double t0 = amrex::second();
+        // flux_to_dudt 
+        {
+            double t0 = amrex::second();
 
-        for (int i = 0; i < 1000; ++i) {
-            __asm__ __volatile__("");
-            flux_to_dudt_c(bx, dudtfab, fxfab, fyfab, fzfab, dxinv);
+            for (int i = 0; i < 1000; ++i) {
+                __asm__ __volatile__("");
+                flux_to_dudt_f(BL_TO_FORTRAN_BOX(bx),
+                               BL_TO_FORTRAN_ANYD(dudtfab),
+                               BL_TO_FORTRAN_ANYD(fxfab),
+                               BL_TO_FORTRAN_ANYD(fyfab),
+                               BL_TO_FORTRAN_ANYD(fzfab),
+                               dxinv.data(),
+                               &ncomp);
+            }
+            
+            double t1 = amrex::second();
+            
+            for (int i = 0; i < 1000; ++i) {
+                __asm__ __volatile__("");
+                flux_to_dudt_c_simd(bx, dudtfab, fxfab, fyfab, fzfab, dxinv);
+            }
+            
+            double t2 = amrex::second();
+            
+            for (int i = 0; i < 1000; ++i) {
+                __asm__ __volatile__("");
+                flux_to_dudt_c_nosimd(bx, dudtfab, fxfab, fyfab, fzfab, dxinv);
+            }
+            
+            double t3 = amrex::second();
+            
+            amrex::Print() << "flux_to_dudt: Fortran time: " << t1-t0 << "\n"
+                           << "              C++ w/ simd time: " << t2-t1  << "\n"
+                           << "              C++ w/o simd time: " << t3-t2 << std::endl;
         }
-
-        double t1 = amrex::second();
-
-        for (int i = 0; i < 1000; ++i) {
-            __asm__ __volatile__("");
-            flux_to_dudt_f(BL_TO_FORTRAN_BOX(bx),
-                           BL_TO_FORTRAN_ANYD(dudtfab),
-                           BL_TO_FORTRAN_ANYD(fxfab),
-                           BL_TO_FORTRAN_ANYD(fyfab),
-                           BL_TO_FORTRAN_ANYD(fzfab),
-                           dxinv.data(),
-                           &ncomp);
-        }
-
-        double t2 = amrex::second();
-
-        amrex::Print() << " C++ time: " << t1-t0 << ", Fortran time: " << t2-t1 << std::endl;
     }
     amrex::Finalize();
 }
