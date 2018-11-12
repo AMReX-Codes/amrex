37c555f7799f5ea5fd909a5a59c6ad80d98a1f07
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-11-12 12:25:53 -0800

    filcc 1D

--- Src/Base/AMReX_FilCC_3D_C.cpp
+++ Src/Base/AMReX_FilCC_3D_C.cpp
@@ -33,75 +33,117 @@ filcc_cell (const IntVect& iv, FArrayBox& dest_fab,
 
         if (i < ilo)
         {
-            if (bc.lo(0) == BCType::foextrap) {
+            if (bc.lo(0) == BCType::foextrap)
+            {
                 q(0,0,0,n) = q(ilo-i,0,0,n);
-            } else if (bc.lo(0) == BCType::hoextrap) {
+            }
+            else if (bc.lo(0) == BCType::hoextrap)
+            {
                 amrex::Abort("BCType::hoextrap TODO");
-            } else if (bc.lo(0) == BCType::reflect_even) {
+            }
+            else if (bc.lo(0) == BCType::reflect_even)
+            {
                 q(0,0,0,n) = q(2*(ilo-i)-1,0,0,n);
-            } else if (bc.lo(0) == BCType::reflect_odd) {
+            }
+            else if (bc.lo(0) == BCType::reflect_odd)
+            {
                 q(0,0,0,n) = -q(2*(ilo-i)-1,0,0,n);
             }
         }
         else if (i > ihi)
         {
-            if (bc.hi(0) == BCType::foextrap) {
+            if (bc.hi(0) == BCType::foextrap)
+            {
                 q(0,0,0,n) = q(ihi-i,0,0,n);
-            } else if (bc.hi(0) == BCType::hoextrap) {
+            }
+            else if (bc.hi(0) == BCType::hoextrap)
+            {
                 amrex::Abort("BCType::hoextrap TODO");
-            } else if (bc.hi(0) == BCType::reflect_even) {
+            }
+            else if (bc.hi(0) == BCType::reflect_even)
+            {
                 q(0,0,0,n) = q(2*(ihi-i)+1,0,0,n);
-            } else if (bc.hi(0) == BCType::reflect_odd) {
+            }
+            else if (bc.hi(0) == BCType::reflect_odd)
+            {
                 q(0,0,0,n) = -q(2*(ihi-i)+1,0,0,n);
             }
         }
 
         if (j < jlo)
         {
-            if (bc.lo(1) == BCType::foextrap) {
+            if (bc.lo(1) == BCType::foextrap)
+            {
                 q(0,0,0,n) = q(0,jlo-j,0,n);
-            } else if (bc.lo(1) == BCType::hoextrap) {
+            }
+            else if (bc.lo(1) == BCType::hoextrap)
+            {
                 amrex::Abort("BCType::hoextrap TODO");
-            } else if (bc.lo(1) == BCType::reflect_even) {
+            }
+            else if (bc.lo(1) == BCType::reflect_even)
+            {
                 q(0,0,0,n) = q(0,2*(jlo-j)-1,0,n);
-            } else if (bc.lo(1) == BCType::reflect_odd) {
+            }
+            else if (bc.lo(1) == BCType::reflect_odd)
+            {
                 q(0,0,0,n) = -q(0,2*(jlo-j)-1,0,n);
             }
         }
         else if (j > jhi)
         {
-            if (bc.hi(1) == BCType::foextrap) {
+            if (bc.hi(1) == BCType::foextrap)
+            {
                 q(0,0,0,n) = q(0,jhi-j,0,n);
-            } else if (bc.hi(1) == BCType::hoextrap) {
+            }
+            else if (bc.hi(1) == BCType::hoextrap)
+            {
                 amrex::Abort("BCType::hoextrap TODO");
-            } else if (bc.hi(1) == BCType::reflect_even) {
+            }
+            else if (bc.hi(1) == BCType::reflect_even)
+            {
                 q(0,0,0,n) = q(0,2*(jhi-j)+1,0,n);
-            } else if (bc.hi(1) == BCType::reflect_odd) {
+            }
+            else if (bc.hi(1) == BCType::reflect_odd)
+            {
                 q(0,0,0,n) = -q(0,2*(jhi-j)+1,0,n);
             }
         }
 
         if (k < klo)
         {
-            if (bc.lo(2) == BCType::foextrap) {
+            if (bc.lo(2) == BCType::foextrap)
+            {
                 q(0,0,0,n) = q(0,0,klo-k,n);
-            } else if (bc.lo(2) == BCType::hoextrap) {
+            }
+            else if (bc.lo(2) == BCType::hoextrap)
+            {
                 amrex::Abort("BCType::hoextrap TODO");
-            } else if (bc.lo(2) == BCType::reflect_even) {
+            }
+            else if (bc.lo(2) == BCType::reflect_even)
+            {
                 q(0,0,0,n) = q(0,0,2*(klo-k)-1,n);
-            } else if (bc.lo(2) == BCType::reflect_odd) {
+            }
+            else if (bc.lo(2) == BCType::reflect_odd)
+            {
                 q(0,0,0,n) = -q(0,0,2*(klo-k)-1,n);
             }
         }
         else if (k > khi)
         {
-            if (bc.hi(2) == BCType::foextrap) {
+            if (bc.hi(2) == BCType::foextrap)
+            {
                 q(0,0,0,n) = q(0,0,khi-k,n);
-            } else if (bc.hi(2) == BCType::hoextrap) {
+            }
+            else if (bc.hi(2) == BCType::hoextrap)
+            {
                 amrex::Abort("BCType::hoextrap TODO");
-            } else if (bc.hi(2) == BCType::reflect_even) {
+            }
+            else if (bc.hi(2) == BCType::reflect_even)
+            {
                 q(0,0,0,n) = q(0,0,2*(khi-k)+1,n);
-            } else if (bc.hi(2) == BCType::reflect_odd) {
+            }
+            else if (bc.hi(2) == BCType::reflect_odd)
+            {
                 q(0,0,0,n) = -q(0,0,2*(khi-k)+1,n);
             }
         }
