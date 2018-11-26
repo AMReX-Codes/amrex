9f5bd9b44a0af3516e03c6b1218ab39fd60ea2f6
kngott <kngott@lbl.gov>
2018-11-26 11:51:50 -0800

    First attempt at slope_3d.cpp.

--- Tutorials/GPU/Advection_AmrCore/Source/Src_3d/slope_3d.cpp
+++ Tutorials/GPU/Advection_AmrCore/Source/Src_3d/slope_3d.cpp
@@ -1,327 +1,196 @@
 #ifndef _slope_cpp_
 #define _slope_cpp_
 
+#include <AmrCoreAdv_F.H>
+#include <AMReX_CudaDevice.H>
+
+using namespace amrex;
+
 void slopex(Box const& bx,
-           const FArrayBox &q,
-           FArrayBox &ddq)
+           const FArrayBox &qfab,
+           FArrayBox &dq4fab)
 {
-
      const Box xbx = amrex::grow(bx, 0, 1);
-     Gpu::DeviceFab fdq (bx, 1);
-     FArrayBox* phix
+     Gpu::DeviceFab fabdq (xbx, 1);
+     FArrayBox* dqptr = fabdq.fabPtr();
 
-
+     // Find 2nd order slope
      AMREX_LAUNCH_DEVICE_LAMBDA(xbx, tbx,
      {
-         slopex2(tbx, q, dq);
+         const auto len = length(bx);
+         const auto lo  = lbound(bx);
+         const auto q   =  qfab.view(lo);
+         const auto dq  = dqptr->view(lo);
+
+         for         (int k = 0; k < len.z; ++k) {
+             for     (int j = 0; j < len.y; ++j) {
+                 for (int i = 0; i < len.x; ++i) {
+                     Real dlft = q(i,j,k) - q(i-1,j,k);
+                     Real drgt = q(i+1,j,k) - q(i,j,k);
+                     Real dcen = 0.5*(dlft+drgt);
+                     Real dsgn = copysign(1.0, dcen);
+                     Real dslop = 2.0 * ((std::abs(dlft) < std::abs(drgt)) ?
+                                          std::abs(dlft) : std::abs(drgt));
+                     Real dlim = (dlft*drgt >= 0.0) ? dslop : 0.0;
+                     dq(i,j,k) = dsgn*std::min(dlim, std::abs(dcen));
+                 }
+             }
+         }
      });
 
-     Device::synchronize();
+     Cuda::Device::synchronize();
 
+     // Calc 4th order slope from 2nd order slope
      AMREX_LAUNCH_DEVICE_LAMBDA(bx, tbx,
      {
-         slopex4(tbx, q, dq);
+         const auto len = length(bx);
+         const auto lo  = lbound(bx);
+         const auto q   =   qfab.view(lo);
+         const auto dq  =  dqptr->view(lo);
+         const auto dq4 = dq4fab.view(lo);
+
+         for         (int k = 0; k < len.z; ++k) {
+             for     (int j = 0; j < len.y; ++j) {
+                 for (int i = 0; i < len.x; ++i) {
+                     Real dlft = q(i,j,k) - q(i-1,j,k);
+                     Real drgt = q(i+1,j,k) - q(i,j,k);
+                     Real dcen = 0.5*(dlft+drgt);
+                     Real dsgn = copysign(1.0, dcen);
+                     Real dslop = 2.0 * ((std::abs(dlft) < std::abs(drgt)) ?
+                                          std::abs(dlft) : std::abs(drgt));
+                     Real dlim = (dlft*drgt >= 0.0) ? dslop : 0.0;
+                     Real dq1 = 4.0/3.0*dcen - (1.0/6.0)*(dq(i+1,j,k) + dq(i-1,j,k));
+                     dq4(i,j,k) = dsgn*std::min(dlim, std::abs(dq1));
+                 }
+             }
+         }
+     });
+}
+
+// ***********************************************************
+
+void slopey(Box const& bx,
+           const FArrayBox &qfab,
+           FArrayBox &dq4fab)
+{
+     const Box ybx = amrex::grow(bx, 1, 1);
+     Gpu::DeviceFab fabdq (ybx, 1);
+     FArrayBox* dqptr = fabdq.fabPtr();
+
+     // Find 2nd order slope
+     AMREX_LAUNCH_DEVICE_LAMBDA(ybx, tbx,
+     {
+         const auto len = length(bx);
+         const auto lo  = lbound(bx);
+         const auto q   =  qfab.view(lo);
+         const auto dq  = dqptr->view(lo);
+
+         for         (int k = 0; k < len.z; ++k) {
+             for     (int j = 0; j < len.y; ++j) {
+                 for (int i = 0; i < len.x; ++i) {
+                     Real dlft = q(i,j,k) - q(i,j-1,k);
+                     Real drgt = q(i,j+1,k) - q(i,j,k);
+                     Real dcen = 0.5*(dlft+drgt);
+                     Real dsgn = copysign(1.0, dcen);
+                     Real dslop = 2.0 * ((std::abs(dlft) < std::abs(drgt)) ?
+                                          std::abs(dlft) : std::abs(drgt));
+                     Real dlim = (dlft*drgt >= 0.0) ? dslop : 0.0;
+                     dq(i,j,k) = dsgn*std::min(dlim, std::abs(dcen));
+                 }
+             }
+         }
      });
 
+     Cuda::Device::synchronize();
+
+     // Calc 4th order slope from 2nd order slope
+     AMREX_LAUNCH_DEVICE_LAMBDA(bx, tbx,
+     {
+         const auto len = length(bx);
+         const auto lo  = lbound(bx);
+         const auto q   =   qfab.view(lo);
+         const auto dq  =  dqptr->view(lo);
+         const auto dq4 = dq4fab.view(lo);
+
+         for         (int k = 0; k < len.z; ++k) {
+             for     (int j = 0; j < len.y; ++j) {
+                 for (int i = 0; i < len.x; ++i) {
+                     Real dlft = q(i,j,k) - q(i,j-1,k);
+                     Real drgt = q(i,j+1,k) - q(i,j,k);
+                     Real dcen = 0.5*(dlft+drgt);
+                     Real dsgn = copysign(1.0, dcen);
+                     Real dslop = 2.0 * ((std::abs(dlft) < std::abs(drgt)) ?
+                                          std::abs(dlft) : std::abs(drgt));
+                     Real dlim = (dlft*drgt >= 0.0) ? dslop : 0.0;
+                     Real dq1 = 4.0/3.0*dcen - (1.0/6.0)*(dq(i,j+1,k) + dq(i,j-1,k));
+                     dq4(i,j,k) = dsgn*std::min(dlim, std::abs(dq1));
+                 }
+             }
+         }
+     });
 }
 
+// ***********************************************************
 
-
-void slopex2nd(Box const& bx,
-            const FArrayBox &qfab
-            FArrayBox &dqfab
+void slopez(Box const& bx,
+           const FArrayBox &qfab,
+           FArrayBox &dq4fab)
 {
-    const auto len = length(bx);
-    const auto lo  = lbound(bx);
-    const auto q   =  qfab.view(lo);
-    const auto dq  = dqfab.view(lo);
+     const Box zbx = amrex::grow(bx, 1, 1);
+     Gpu::DeviceFab fabdq (zbx, 1);
+     FArrayBox* dqptr = fabdq.fabPtr();
 
-    for         (int k = 0; k < len.z; ++k) {
-        for     (int j = 0; j < len.y; ++j) {
-            for (int i = 0; i < len.x; ++i) {
-                 Real dlft = q(i,j,k) - q(i-1,j,k);
-                 Real drgt = q(i+1,j,k) - q(i,j,k);
-                 Real dcen = 0.5*(dlft+drgt);
-                 Real dsgn = copysign(1.0, dcen);
-                 Real dslop = 2.0 * ((abs(dlft) < abs(drgt)) ?
-                                     abs(dlft) : abs(drgt));
-                 Real dlim = (dlft*drgt >= 0.0) ? slop : 0.0;
-                 dq(i,j,k) = dsgn*amrex::min(dlim, abs(dcen));
-            }
-        }
-    }
+     // Find 2nd order slope
+     AMREX_LAUNCH_DEVICE_LAMBDA(zbx, tbx,
+     {
+         const auto len = length(bx);
+         const auto lo  = lbound(bx);
+         const auto q   =  qfab.view(lo);
+         const auto dq  = dqptr->view(lo);
 
-    do    k = lo(3), hi(3)
-       do j = lo(2), hi(2)
+         for         (int k = 0; k < len.z; ++k) {
+             for     (int j = 0; j < len.y; ++j) {
+                 for (int i = 0; i < len.x; ++i) {
+                     Real dlft = q(i,j,k) - q(i,j,k-1);
+                     Real drgt = q(i,j,k+1) - q(i,j,k);
+                     Real dcen = 0.5*(dlft+drgt);
+                     Real dsgn = copysign(1.0, dcen);
+                     Real dslop = 2.0 * ((std::abs(dlft) < std::abs(drgt)) ?
+                                          std::abs(dlft) : std::abs(drgt));
+                     Real dlim = (dlft*drgt >= 0.0) ? dslop : 0.0;
+                     dq(i,j,k) = dsgn*std::min(dlim, std::abs(dcen));
+                 }
+             }
+         }
+     });
 
-          ! first compute Fromm slopes
-          do i = lo(1)-1, hi(1)+1
-             dlft = q(i  ,j,k) - q(i-1,j,k)
-             drgt = q(i+1,j,k) - q(i  ,j,k)
-             dcen(i) = .5d0 * (dlft+drgt)
-             dsgn(i) = sign(one, dcen(i))
-             if (dlft*drgt .ge. 0.d0) then
-                dlim(i) = 2.d0 * min( abs(dlft), abs(drgt) )
-             else
-                dlim(i) = 0.d0
-             endif
-             df(i) = dsgn(i)*min( dlim(i), abs(dcen(i)) )
-          end do
-          
-          ! Now limited fourth order slopes
-          do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i) - sixth*(df(i+1) + df(i-1))
-             dq(i,j,k) = dsgn(i)*min(dlim(i),abs(dq1))
-          end do
-       end do
-    end do
+     Cuda::Device::synchronize();
+
+     // Calc 4th order slope from 2nd order slope
+     AMREX_LAUNCH_DEVICE_LAMBDA(bx, tbx,
+     {
+         const auto len = length(bx);
+         const auto lo  = lbound(bx);
+         const auto q   =   qfab.view(lo);
+         const auto dq  =  dqptr->view(lo);
+         const auto dq4 = dq4fab.view(lo);
+
+         for         (int k = 0; k < len.z; ++k) {
+             for     (int j = 0; j < len.y; ++j) {
+                 for (int i = 0; i < len.x; ++i) {
+                     Real dlft = q(i,j,k) - q(i,j,k-1);
+                     Real drgt = q(i,j,k+1) - q(i,j,k);
+                     Real dcen = 0.5*(dlft+drgt);
+                     Real dsgn = copysign(1.0, dcen);
+                     Real dslop = 2.0 * ((std::abs(dlft) < std::abs(drgt)) ?
+                                          std::abs(dlft) : std::abs(drgt));
+                     Real dlim = (dlft*drgt >= 0.0) ? dslop : 0.0;
+                     Real dq1 = 4.0/3.0*dcen - (1.0/6.0)*(dq(i,j,k+1) + dq(i,j,k-1));
+                     dq4(i,j,k) = dsgn*std::min(dlim, std::abs(dq1));
+                 }
+             }
+         }
+     });
 }
-
-void slopex4th(Box const& bx,
-               const FArrayBox &q,
-               FArrayBox &dq)
-{
-    do    k = lo(3), hi(3)
-       do j = lo(2), hi(2)
-
-          ! first compute Fromm slopes
-          do i = lo(1)-1, hi(1)+1
-             dlft = q(i  ,j,k) - q(i-1,j,k)
-             drgt = q(i+1,j,k) - q(i  ,j,k)
-             dcen(i) = .5d0 * (dlft+drgt)
-             dsgn(i) = sign(one, dcen(i))
-             if (dlft*drgt .ge. 0.d0) then
-                dlim(i) = 2.d0 * min( abs(dlft), abs(drgt) )
-             else
-                dlim(i) = 0.d0
-             endif
-             df(i) = dsgn(i)*min( dlim(i), abs(dcen(i)) )
-          end do
-          
-          ! Now limited fourth order slopes
-          do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i) - sixth*(df(i+1) + df(i-1))
-             dq(i,j,k) = dsgn(i)*min(dlim(i),abs(dq1))
-          end do
-       end do
-    end do
-}
-
-
-
-
-module slope_module
-
-  use amrex_fort_module, only : amrex_real
-  use amrex_constants_module, only : one
-  implicit none
-
-  real(amrex_real), parameter:: four3rd=4.d0/3.d0, sixth=1.d0/6.d0
-  
-  private
- 
-  public :: slopex, slopey, slopez
- 
-contains
- 
-  subroutine slopex(lo, hi, &
-                    q, qlo, qhi, &
-                    dq, dqlo, dqhi)
-
-    implicit none
-
-    integer, intent(in) :: lo(3), hi(3), qlo(3), qhi(3), dqlo(3), dqhi(3)
-    real(amrex_real), intent(in ) ::  q( qlo(1): qhi(1), qlo(2): qhi(2), qlo(3): qhi(3))
-    real(amrex_real), intent(out) :: dq(dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
-
-    integer :: i, j, k
-    real(amrex_real), dimension(lo(1)-1:hi(1)+1) :: dsgn, dlim, df, dcen
-    real(amrex_real) :: dlft, drgt, dq1
-
-    do    k = lo(3), hi(3)
-       do j = lo(2), hi(2)
-
-          ! first compute Fromm slopes
-          do i = lo(1)-1, hi(1)+1
-             dlft = q(i  ,j,k) - q(i-1,j,k)
-             drgt = q(i+1,j,k) - q(i  ,j,k)
-             dcen(i) = .5d0 * (dlft+drgt)
-             dsgn(i) = sign(one, dcen(i))
-             if (dlft*drgt .ge. 0.d0) then
-                dlim(i) = 2.d0 * min( abs(dlft), abs(drgt) )
-             else
-                dlim(i) = 0.d0
-             endif
-             df(i) = dsgn(i)*min( dlim(i), abs(dcen(i)) )
-          end do
-          
-          ! Now limited fourth order slopes
-          do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i) - sixth*(df(i+1) + df(i-1))
-             dq(i,j,k) = dsgn(i)*min(dlim(i),abs(dq1))
-          end do
-       end do
-    end do
-
-  end subroutine slopex
-
-
-  subroutine slopey(lo, hi, &
-                    q, qlo, qhi, &
-                    dq, dqlo, dqhi)
-
-    use amrex_mempool_module, only : bl_allocate, bl_deallocate
-
-    integer, intent(in) :: lo(3), hi(3), qlo(3), qhi(3), dqlo(3), dqhi(3)
-    real(amrex_real), intent(in ) ::  q( qlo(1): qhi(1), qlo(2): qhi(2), qlo(3): qhi(3))
-    real(amrex_real), intent(out) :: dq(dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
-
-    ! Some compiler may not support 'contiguous'.  Remove it in that case.
-    real(amrex_real), dimension(:,:), pointer, contiguous :: dsgn, dlim, df, dcen
-
-    call bl_allocate(dsgn, lo(1), hi(1), lo(2)-1, hi(2)+1)
-    call bl_allocate(dlim, lo(1), hi(1), lo(2)-1, hi(2)+1)
-    call bl_allocate(df  , lo(1), hi(1), lo(2)-1, hi(2)+1)
-    call bl_allocate(dcen, lo(1), hi(1), lo(2)-1, hi(2)+1)
-
-    call slopey_doit(lo, hi, &
-                     q, qlo, qhi, &
-                     dq, dqlo, dqhi, &
-                     dsgn, dlim, df, dcen, (/lo(1),lo(2)-1/), (/hi(1),hi(2)+1/))
-
-    call bl_deallocate(dsgn)
-    call bl_deallocate(dlim)
-    call bl_deallocate(df)
-    call bl_deallocate(dcen)
-
-  end subroutine slopey
-
-  subroutine slopey_doit(lo, hi, &
-                         q, qlo, qhi, &
-                         dq, dqlo, dqhi, &
-                         dsgn, dlim, df, dcen, ddlo, ddhi)
-
-    integer, intent(in) :: lo(3), hi(3), qlo(3), qhi(3), dqlo(3), dqhi(3), &
-         ddlo(2), ddhi(2)
-    real(amrex_real), intent(in ) ::  q  ( qlo(1): qhi(1), qlo(2): qhi(2), qlo(3): qhi(3))
-    real(amrex_real), intent(out) :: dq  (dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
-    real(amrex_real)              :: dsgn(ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-    real(amrex_real)              :: dlim(ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-    real(amrex_real)              :: df  (ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-    real(amrex_real)              :: dcen(ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-
-    integer :: i, j, k
-    real(amrex_real) :: dlft, drgt, dq1
-
-    do k = lo(3), hi(3)
-
-       ! first compute Fromm slopes
-       do j    = lo(2)-1, hi(2)+1
-          do i = lo(1)  , hi(1)
-             dlft = q(i,j  ,k) - q(i,j-1,k)
-             drgt = q(i,j+1,k) - q(i,j  ,k)
-             dcen(i,j) = .5d0 * (dlft+drgt)
-             dsgn(i,j) = sign( one, dcen(i,j) )
-             if (dlft*drgt .ge. 0.d0) then
-                dlim(i,j) = 2.d0 * min( abs(dlft), abs(drgt) )
-             else
-                dlim(i,j) = 0.d0
-             endif
-             df(i,j) = dsgn(i,j)*min( dlim(i,j),abs(dcen(i,j)) )
-          end do
-       end do
-       
-       ! Now compute limited fourth order slopes
-       do j    = lo(2), hi(2)
-          do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i,j) - sixth*( df(i,j+1) + df(i,j-1) )
-             dq(i,j,k) = dsgn(i,j)*min(dlim(i,j),abs(dq1))
-          end do
-       end do
-
-    end do
-
-  end subroutine slopey_doit
-
-
-  subroutine slopez(lo, hi, &
-                    q, qlo, qhi, &
-                    dq, dqlo, dqhi)
-
-    use amrex_mempool_module, only : bl_allocate, bl_deallocate
-
-    integer, intent(in) :: lo(3), hi(3), qlo(3), qhi(3), dqlo(3), dqhi(3)
-    real(amrex_real), intent(in ) ::  q( qlo(1): qhi(1), qlo(2): qhi(2), qlo(3): qhi(3))
-    real(amrex_real), intent(out) :: dq(dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
-
-    ! Some compiler may not support 'contiguous'.  Remove it in that case.
-    real(amrex_real), dimension(:,:,:), pointer, contiguous :: dsgn, dlim, df, dcen
-
-    call bl_allocate(dsgn, lo(1), hi(1), lo(2), hi(2), lo(3)-1, hi(3)+1)
-    call bl_allocate(dlim, lo(1), hi(1), lo(2), hi(2), lo(3)-1, hi(3)+1)
-    call bl_allocate(df  , lo(1), hi(1), lo(2), hi(2), lo(3)-1, hi(3)+1)
-    call bl_allocate(dcen, lo(1), hi(1), lo(2), hi(2), lo(3)-1, hi(3)+1)
-
-    call slopez_doit(lo, hi, &
-                     q, qlo, qhi, &
-                     dq, dqlo, dqhi, &
-                     dsgn, dlim, df, dcen, &
-                     (/lo(1),lo(2),lo(3)-1/), (/hi(1),hi(2),hi(3)+1/))
-
-    call bl_deallocate(dsgn)
-    call bl_deallocate(dlim)
-    call bl_deallocate(df)
-    call bl_deallocate(dcen)
-
-  end subroutine slopez
-
-  subroutine slopez_doit(lo, hi, &
-                         q, qlo, qhi, &
-                         dq, dqlo, dqhi, &
-                         dsgn, dlim, df, dcen, ddlo, ddhi)
-
-    integer, intent(in) :: lo(3), hi(3), qlo(3), qhi(3), dqlo(3), dqhi(3), &
-         ddlo(3), ddhi(3)
-    real(amrex_real), intent(in ) ::  q  ( qlo(1): qhi(1), qlo(2): qhi(2), qlo(3): qhi(3))
-    real(amrex_real), intent(out) :: dq  (dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
-    real(amrex_real)              :: dsgn(ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
-    real(amrex_real)              :: dlim(ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
-    real(amrex_real)              :: df  (ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
-    real(amrex_real)              :: dcen(ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
-
-    integer :: i, j, k
-    real(amrex_real) :: dlft, drgt, dq1
-
-    ! first compute Fromm slopes
-    do k       = lo(3)-1, hi(3)+1
-       do j    = lo(2)  , hi(2)
-          do i = lo(1)  , hi(1)
-             dlft = q(i,j,k  ) - q(i,j,k-1)
-             drgt = q(i,j,k+1) - q(i,j,k  )
-             dcen(i,j,k) = .5d0 * (dlft+drgt)
-             dsgn(i,j,k) = sign( one, dcen(i,j,k) )
-             if (dlft*drgt .ge. 0.d0) then
-                dlim(i,j,k) = 2.d0 * min( abs(dlft), abs(drgt) )
-             else
-                dlim(i,j,k) = 0.d0
-             endif
-             df(i,j,k) = dsgn(i,j,k)*min( dlim(i,j,k),abs(dcen(i,j,k)) )
-          end do
-       end do
-    end do
-       
-    ! Now compute limited fourth order slopes
-    do k       = lo(3), hi(3)
-       do j    = lo(2), hi(2)
-          do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i,j,k) - sixth*( df(i,j,k+1) + df(i,j,k-1) )
-             dq(i,j,k) = dsgn(i,j,k)*min(dlim(i,j,k),abs(dq1))
-          end do
-       end do
-    end do
-
-  end subroutine slopez_doit
-
-end module slope_module 
 
 #endif
