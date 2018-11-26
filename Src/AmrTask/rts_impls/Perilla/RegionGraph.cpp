4d7a76e604525d4a7d7b842306df91863edadf82
Tan Nguyen <tannguyen@lbl.gov>
2018-11-25 16:42:13 -0800

    update destructors for AMFIter related classes

--- Src/AmrTask/rts_impls/Perilla/RegionGraph.cpp
+++ Src/AmrTask/rts_impls/Perilla/RegionGraph.cpp
@@ -1,6 +1,8 @@
 #include <RegionGraph.H>
 #include <WorkerThread.H>
+#include <stdlib.h>
 
+using namespace std;
 using namespace amrex;
 using namespace perilla;
 
@@ -805,22 +807,28 @@ void RegionGraph::workerTeardown()
 
 RegionGraph::~RegionGraph()
 {
-    lMap.clear();
-    sMap.clear();
-    rMap.clear();
+    delete[] okToReset;
+    for(int tg=0; tg<perilla::NUM_THREAD_TEAMS; tg++)delete worker[tg];
+    worker.clear();
+    for(int i=0; i<task.size(); i++) delete task[i];
+    task.clear();
 
-//    for(int i=0; i<task.size(); i++) delete task[i];
-    for(int tg=0; tg<perilla::NUM_THREAD_TEAMS; tg++) delete worker[tg];
-    for(int i=0; i<fabTiles.size(); i++) delete fabTiles[i];
-    for(int i=0; i<fabTiles_gtbx.size(); i++) delete fabTiles_gtbx[i];
-
-    fabTiles.clear();
-    fabTiles_gtbx.clear();
     if(sCopyMapHead != 0)
       delete sCopyMapHead;
     if(rCopyMapHead != 0)
       delete rCopyMapHead;
-    worker.clear();
-    task.clear();
-    delete[] okToReset;
+
+    for(int i=0; i<lMap.size(); i++) delete lMap[i];
+    for(int i=0; i<sMap.size(); i++) delete sMap[i];
+    for(int i=0; i<rMap.size(); i++) delete rMap[i];
+
+    lMap.clear();
+    sMap.clear();
+    rMap.clear();
+
+    //for(int i=0; i<fabTiles.size(); i++) delete fabTiles[i];
+    //for(int i=0; i<fabTiles_gtbx.size(); i++) delete fabTiles_gtbx[i];
+
+    fabTiles.clear();
+    fabTiles_gtbx.clear();
 }
