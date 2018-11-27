881e74d519892a8e7e1f6ef3bd55864d2f1c30a1
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-11-27 11:54:01 -0800

    gpu: amrex_avg_nd_to_cc

--- Src/Base/AMReX_MultiFabUtil_3D_C.cpp
+++ Src/Base/AMReX_MultiFabUtil_3D_C.cpp
@@ -1,3 +1,28 @@
 
 #include <AMReX_MultiFabUtil_C.H>
 
+namespace amrex {
+
+AMREX_GPU_HOST_DEVICE
+void amrex_avg_nd_to_cc (Box const& bx, FArrayBox& ccfab, FArrayBox const& ndfab,
+                         int cccomp, int ndcomp, int ncomp)
+{
+    const auto len = length(bx);
+    const auto lo  = lbound(bx);
+    const auto cc = ccfab.view(lo,cccomp);
+    const auto nd = ndfab.view(lo,ndcomp);
+
+    for (int n = 0; n < ncomp; ++n) {
+        for (int k = 0; k < len.z; ++k) {
+        for (int j = 0; j < len.y; ++j) {
+        AMREX_PRAGMA_SIMD
+        for (int i = 0; i < len.x; ++i) {
+            cc(i,j,k,n) = 0.125*( nd(i,j  ,k  ,n) + nd(i+1,j  ,k  ,n)
+                                + nd(i,j+1,k  ,n) + nd(i+1,j+1,k  ,n)
+                                + nd(i,j  ,k+1,n) + nd(i+1,j  ,k+1,n)
+                                + nd(i,j+1,k+1,n) + nd(i+1,j+1,k+1,n));
+        }}}
+    }
+}
+
+}
