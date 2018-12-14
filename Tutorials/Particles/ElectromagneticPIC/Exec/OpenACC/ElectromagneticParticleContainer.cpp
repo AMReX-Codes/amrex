2e0e870f254672fabb5c81f010a294e55c0a7c79
Andrew Myers <atmyers2@gmail.com>
2018-12-13 16:31:40 -0800

    make corresponding changes to the OpenACC version

--- Tutorials/Particles/ElectromagneticPIC/Exec/OpenACC/ElectromagneticParticleContainer.cpp
+++ Tutorials/Particles/ElectromagneticPIC/Exec/OpenACC/ElectromagneticParticleContainer.cpp
@@ -1,11 +1,3 @@
-#include <AMReX_Geometry.H>
-#include <AMReX_BoxArray.H>
-#include <AMReX_DistributionMapping.H>
-#include <AMReX_Utility.H>
-#include <AMReX_MultiFab.H>
-#include <AMReX_StructOfArrays.H>
-#include <AMReX_RedistributeStrategy.H>
-
 #include "ElectromagneticParticleContainer.H"
 #include "Constants.H"
 
@@ -48,27 +40,9 @@ ElectromagneticParticleContainer(const Geometry            & a_geom,
                                  const int                   a_species_id,
                                  const Real                  a_charge,
                                  const Real                  a_mass)
-    : m_ba(a_ba), m_geom(a_geom), m_dmap(a_dmap),
-      m_species_id(a_species_id), m_charge(a_charge), m_mass(a_mass)
-{
-    BL_PROFILE("ElectromagneticParticleContainer::ElectromagneticParticleContainer");
-
-    const int ng = 1;
-    m_mask_ptr.reset(new MultiFab(m_ba, m_dmap, 1, ng));
-    m_mask_ptr->setVal(-1);
-    for (MFIter mfi(*m_mask_ptr, false); mfi.isValid(); ++mfi) {
-        const Box& box = mfi.tilebox();
-        const int grid_id = mfi.index();
-        m_mask_ptr->setVal(grid_id, box, 0, 1);
-    }
-    m_mask_ptr->FillBoundary(m_geom.periodicity());
-    
-#ifdef AMREX_USE_CUDA
-    m_redistribute_strategy.reset(new RedistributeStrategyGPU<Particles>() );
-#else
-    m_redistribute_strategy.reset(new RedistributeStrategyCPU<Particles>() );
-#endif    
-}
+    : ParticleContainer<0, 0, PIdx::nattribs, 0>(a_geom, a_dmap, a_ba),
+    m_species_id(a_species_id), m_charge(a_charge), m_mass(a_mass)
+{}
 
 void
 ElectromagneticParticleContainer::
@@ -80,30 +54,35 @@ InitParticles(const IntVect& a_num_particles_per_cell,
               const int      a_problem)
 {
     BL_PROFILE("ElectromagneticParticleContainer::InitParticles");
-
+    
     const Real* dx = m_geom.CellSize();
-
+    
     const int num_ppc = AMREX_D_TERM( a_num_particles_per_cell[0],
-                                     *a_num_particles_per_cell[1],
-                                     *a_num_particles_per_cell[2]);
+                                      *a_num_particles_per_cell[1],
+                                      *a_num_particles_per_cell[2]);
     const Real scale_fac = dx[0]*dx[1]*dx[2]/num_ppc;
-
+    
+    std::array<Real,PIdx::nattribs> attribs;
+    attribs.fill(0.0);    
+    
     for(MFIter mfi(*m_mask_ptr); mfi.isValid(); ++mfi)
     {
         const Box& tile_box  = mfi.tilebox();
         const Real* plo = m_geom.ProbLo();
         const int grid_id = mfi.index();
         const int tile_id = mfi.LocalTileIndex();
-        auto& particles = m_particles[std::make_pair(grid_id, tile_id)];
+        const auto& pair_index = std::make_pair(grid_id, tile_id);
+        auto& particles = m_particles[pair_index];
         for (IntVect iv = tile_box.smallEnd(); iv <= tile_box.bigEnd(); tile_box.next(iv)) {
             for (int i_part=0; i_part<num_ppc;i_part++) {
                 Real r[3];
                 Real u[3];
-
+                
                 get_position_unit_cell(r, a_num_particles_per_cell, i_part);
-
+                
                 if (a_problem == 0) {
-                    get_gaussian_random_momentum(u, a_thermal_momentum_mean, a_thermal_momentum_std);
+                    get_gaussian_random_momentum(u, a_thermal_momentum_mean,
+                                                 a_thermal_momentum_std);
                 }
                 else if (a_problem == 1 ) {
                     u[0] = 0.01;
@@ -112,43 +91,63 @@ InitParticles(const IntVect& a_num_particles_per_cell,
                 } else {
                     amrex::Abort("problem type not valid");
                 }
-
+                
                 Real x = plo[0] + (iv[0] + r[0])*dx[0];
                 Real y = plo[1] + (iv[1] + r[1])*dx[1];
                 Real z = plo[2] + (iv[2] + r[2])*dx[2];
-
+                
                 if (x >= a_bounds.hi(0) || x < a_bounds.lo(0) ||
                     y >= a_bounds.hi(1) || y < a_bounds.lo(1) ||
                     z >= a_bounds.hi(2) || z < a_bounds.lo(2) ) continue;
-
-                particles.x().push_back(x);
-                particles.y().push_back(y);
-                particles.z().push_back(z);
-
-                particles.ux().push_back(u[0] * PhysConst::c);
-                particles.uy().push_back(u[1] * PhysConst::c);
-                particles.uz().push_back(u[2] * PhysConst::c);
-
-                particles.w().push_back(a_density * scale_fac);
-
-                particles.ex().push_back(0);
-                particles.ey().push_back(0);
-                particles.ez().push_back(0);
-                particles.bx().push_back(0);
-                particles.by().push_back(0);
-                particles.bz().push_back(0);
-                particles.ginv().push_back(0);
+                
+                ParticleType p;
+                p.id()  = ParticleType::NextID();
+                p.cpu() = ParallelDescriptor::MyProc();                
+                p.pos(0) = x;
+                p.pos(1) = y;
+                p.pos(2) = z;
+                
+                attribs[PIdx::ux] = u[0] * PhysConst::c;
+                attribs[PIdx::uy] = u[1] * PhysConst::c;
+                attribs[PIdx::uz] = u[2] * PhysConst::c;
+                attribs[PIdx::w ] = a_density * scale_fac;
+                
+                host_particles.push_back(p);
+                for (int kk = 0; kk < PIdx::nattribs; ++kk)
+                    host_attribs[kk].push_back(attribs[kk]);
+                
+                attribs[PIdx::ux] = u[0] * PhysConst::c;
+                attribs[PIdx::uy] = u[1] * PhysConst::c;
+                attribs[PIdx::uz] = u[2] * PhysConst::c;
+                attribs[PIdx::w ] = a_density * scale_fac;                                
             }
+        }
+        
+        auto& particle_tile = GetParticles(lev)[std::make_pair(grid_id,tile_id)];
+        auto old_size = particle_tile.GetArrayOfStructs().size();
+        auto new_size = old_size + host_particles.size();
+        particle_tile.resize(new_size);
+        
+        thrust::copy(host_particles.begin(),
+                     host_particles.end(),
+                     particle_tile.GetArrayOfStructs().begin() + old_size);
+        
+        for (int kk = 0; kk < PIdx::nattribs; ++kk)
+        {
+            thrust::copy(host_attribs[kk].begin(),
+                         host_attribs[kk].end(),
+                         particle_tile.GetStructOfArrays().GetRealData(kk).begin() + old_size);
         }
     }
 }
 
-void ElectromagneticParticleContainer::PushAndDeposeParticles(const MultiFab& Ex, const MultiFab& Ey, const MultiFab& Ez,
-                                                              const MultiFab& Bx, const MultiFab& By, const MultiFab& Bz,
-                                                              MultiFab& jx, MultiFab& jy, MultiFab& jz, Real dt)
+void ElectromagneticParticleContainer::
+PushAndDeposeParticles(const MultiFab& Ex, const MultiFab& Ey, const MultiFab& Ez,
+                       const MultiFab& Bx, const MultiFab& By, const MultiFab& Bz,
+                       MultiFab& jx, MultiFab& jy, MultiFab& jz, Real dt)
 {
     BL_PROFILE("ElectromagneticParticleContainer::PushAndDeposeParticles");
-
+    
     const Real* dx  = m_geom.CellSize();
     const Real* plo = m_geom.ProbLo();
 
@@ -200,11 +199,12 @@ void ElectromagneticParticleContainer::PushAndDeposeParticles(const MultiFab& Ex
     }
 }
 
-void ElectromagneticParticleContainer::PushParticleMomenta(const MultiFab& Ex, const MultiFab& Ey, const MultiFab& Ez,
-                                                           const MultiFab& Bx, const MultiFab& By, const MultiFab& Bz, Real dt)
+void ElectromagneticParticleContainer::
+PushParticleMomenta(const MultiFab& Ex, const MultiFab& Ey, const MultiFab& Ez,
+                    const MultiFab& Bx, const MultiFab& By, const MultiFab& Bz, Real dt)
 {   
     BL_PROFILE("ElectromagneticParticleContainer::PushParticleMomenta");
-
+    
     const Real* dx  = m_geom.CellSize();
     const Real* plo = m_geom.ProbLo();
 
@@ -245,7 +245,7 @@ void ElectromagneticParticleContainer::PushParticleMomenta(const MultiFab& Ex, c
 void ElectromagneticParticleContainer::PushParticlePositions(Real dt)
 {
     BL_PROFILE("ElectromagneticParticleContainer::PushParticlePositions");
-
+    
     for (MFIter mfi(*m_mask_ptr, false); mfi.isValid(); ++mfi)
     {
         const int grid_id = mfi.index();
@@ -265,35 +265,3 @@ void ElectromagneticParticleContainer::PushParticlePositions(Real dt)
                                   particles.ginv().data(), dt);
     }
 }
-
-void ElectromagneticParticleContainer::EnforcePeriodicBCs()
-{
-    BL_PROFILE("ElectromagneticParticleContainer::EnforcePeriodicBCs");
-
-    for (MFIter mfi(*m_mask_ptr, false); mfi.isValid(); ++mfi)
-    {
-        const int grid_id = mfi.index();
-        const int tile_id = mfi.LocalTileIndex();
-        auto& particles = m_particles[std::make_pair(grid_id, tile_id)];
-        const int np    = particles.numParticles();
-
-        if (np == 0) continue;
-
-        const Real* plo = m_geom.ProbLo();
-        const Real* phi = m_geom.ProbHi();
-
-        FTOC(enforce_periodic)(np,
-                               particles.x().data(),  particles.y().data(),  particles.z().data(),
-                               plo, phi);
-    }
-}
-
-void ElectromagneticParticleContainer::OK ()
-{
-    m_redistribute_strategy->OK(m_particles, m_ba, m_dmap, m_geom, m_mask_ptr.get());
-}
-
-void ElectromagneticParticleContainer::Redistribute()
-{
-    m_redistribute_strategy->Redistribute(m_particles, m_ba, m_dmap, m_geom, m_mask_ptr.get());
-} 
