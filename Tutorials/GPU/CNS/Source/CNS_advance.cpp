a1c11e222bba7164184bbfbf82e77bd60417be65
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-12-14 15:57:35 -0800

    Tutorials/GPU/CNS: amr

--- Tutorials/GPU/CNS/Source/CNS_advance.cpp
+++ Tutorials/GPU/CNS/Source/CNS_advance.cpp
@@ -19,17 +19,21 @@ CNS::advance (Real time, Real dt, int iteration, int ncycle)
     MultiFab& S_old = get_old_data(State_Type);
     MultiFab dSdt(grids,dmap,NUM_STATE,0,MFInfo(),Factory());
     MultiFab Sborder(grids,dmap,NUM_STATE,NUM_GROW,MFInfo(),Factory());
-  
+
     FluxRegister* fr_as_crse = nullptr;
-//    if (do_reflux && level < parent->finestLevel()) {
-//        CNS& fine_level = getLevel(level+1);
-//        fr_as_crse = fine_level.flux_reg.get();
-//    }
+    if (do_reflux && level < parent->finestLevel()) {
+        CNS& fine_level = getLevel(level+1);
+        fr_as_crse = fine_level.flux_reg.get();
+    }
 
     FluxRegister* fr_as_fine = nullptr;
-//    if (do_reflux && level > 0) {
-//        fr_as_fine = flux_reg.get();
-//    }
+    if (do_reflux && level > 0) {
+        fr_as_fine = flux_reg.get();
+    }
+
+    if (fr_as_crse) {
+        fr_as_crse->setVal(0.0);
+    }
 
     // RK2 stage 1
     FillPatch(*this, Sborder, NUM_GROW, time, State_Type, 0, NUM_STATE);
@@ -37,7 +41,7 @@ CNS::advance (Real time, Real dt, int iteration, int ncycle)
     // U^* = U^n + dt*dUdt^n
     MultiFab::LinComb(S_new, 1.0, Sborder, 0, dt, dSdt, 0, 0, NUM_STATE, 0);
     computeTemp(S_new,0);
-    
+
     // RK2 stage 2
     // After fillpatch Sborder = U^n+dt*dUdt^n
     FillPatch(*this, Sborder, NUM_GROW, time+dt, State_Type, 0, NUM_STATE);
@@ -54,13 +58,13 @@ CNS::advance (Real time, Real dt, int iteration, int ncycle)
 
 void
 CNS::compute_dSdt (const MultiFab& S, MultiFab& dSdt, Real dt,
-                   FluxRegister* /*fr_as_crse*/, FluxRegister* /*fr_as_fine*/)
+                   FluxRegister* fr_as_crse, FluxRegister* fr_as_fine)
 {
     BL_PROFILE("CNS::compute_dSdt()");
 
     const auto dx = geom.CellSizeArray();
     const auto dxinv = geom.InvCellSizeArray();
-    const int ncomp = dSdt.nComp();
+    const int ncomp = NUM_STATE;
     const int neqns = 5;
     const int ncons = 7;
     const int nprim = 8;
@@ -140,6 +144,22 @@ CNS::compute_dSdt (const MultiFab& S, MultiFab& dSdt, Real dt,
         {
             cns_flux_to_dudt(tbx, *dsdtfab, AMREX_D_DECL(*fxfab,*fyfab,*fzfab), dxinv);
         });
+    }
+
+    if (fr_as_crse) {
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+            const Real dA = (idim == 0) ? dx[1]*dx[2] : ((idim == 1) ? dx[0]*dx[2] : dx[0]*dx[1]);
+            const Real scale = -dt*dA;
+            fr_as_crse->CrseInit(fluxes[idim], idim, 0, 0, NUM_STATE, scale, FluxRegister::ADD);
+        }
+    }
+
+    if (fr_as_fine) {
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+            const Real dA = (idim == 0) ? dx[1]*dx[2] : ((idim == 1) ? dx[0]*dx[2] : dx[0]*dx[1]);
+            const Real scale = dt*dA;
+            fr_as_fine->FineAdd(fluxes[idim], idim, 0, 0, NUM_STATE, scale);
+        }
     }
 }
 
