49026564f65f731205ad5895fd6aaacb6972ae8c
Andrew Myers <atmyers2@gmail.com>
2018-12-17 11:25:40 -0800

    forgot namespace

--- Tutorials/Particles/ElectromagneticPIC/Exec/OpenACC/EMParticleContainer.cpp
+++ Tutorials/Particles/ElectromagneticPIC/Exec/OpenACC/EMParticleContainer.cpp
@@ -128,15 +128,15 @@ InitParticles(const IntVect& a_num_particles_per_cell,
         auto new_size = old_size + host_particles.size();
         particle_tile.resize(new_size);
         
-        thrust_copy(host_particles.begin(),
-                    host_particles.end(),
-                    particle_tile.GetArrayOfStructs().begin() + old_size);
+        Cuda::thrust_copy(host_particles.begin(),
+                          host_particles.end(),
+                          particle_tile.GetArrayOfStructs().begin() + old_size);
         
         for (int kk = 0; kk < PIdx::nattribs; ++kk)
         {
-            thrust_copy(host_attribs[kk].begin(),
-                        host_attribs[kk].end(),
-                        particle_tile.GetStructOfArrays().GetRealData(kk).begin() + old_size);
+            Cuda::thrust_copy(host_attribs[kk].begin(),
+                              host_attribs[kk].end(),
+                              particle_tile.GetStructOfArrays().GetRealData(kk).begin() + old_size);
         }
     }
 }
