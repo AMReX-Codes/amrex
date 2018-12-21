c24e38c249711d23be669aa36e0596fefcc57b9b
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-12-20 17:04:58 -0800

    Tutorials/GPU/CNS: add RT test

---
+++ Tutorials/GPU/CNS/Exec/RT/cns_prob.cpp
@@ -0,0 +1,10 @@
+
+#include <AMReX_PROB_AMR_F.H>
+
+extern "C" {
+    void amrex_probinit (const int* init,
+                         const int* name,
+                         const int* namelen,
+                         const amrex_real* problo,
+                         const amrex_real* probhi) {}
+}
