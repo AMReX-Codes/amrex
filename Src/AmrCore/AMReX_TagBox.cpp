1276a87130770a1da6b9f1c3d81df8063c293d87
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-12-22 12:31:17 -0800

    add fabHostPtr and fabDevicePtr to FabArray and make fabPtr's return depends on whether it is in gpu launch region

--- Src/AmrCore/AMReX_TagBox.cpp
+++ Src/AmrCore/AMReX_TagBox.cpp
@@ -646,9 +646,9 @@ TagBoxArray::coarsen (const IntVect & ratio)
 #endif
     for (MFIter mfi(*this,flags); mfi.isValid(); ++mfi)
     {
-        this->fabPtr(mfi)->coarsen(ratio,isOwner(mfi.LocalIndex()));
+        this->fabDevicePtr(mfi)->coarsen(ratio,isOwner(mfi.LocalIndex()));
 #ifdef AMREX_USE_GPU
-        (*this)[mfi].coarsen(ratio,false);
+        this->fabHostPtr(mfi)->coarsen(ratio,false);
 #endif
     }
 
