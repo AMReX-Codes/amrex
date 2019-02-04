edbf4c97d228111cc9f46df03a295532d84fc39e
Andrew Myers <atmyers2@gmail.com>
2019-02-04 12:50:33 -0800

    rename solve_with_f90

--- OldTutorials/PIC_C/solve_with_mlmg.cpp
+++ OldTutorials/PIC_C/solve_with_mlmg.cpp
@@ -6,6 +6,7 @@
 #include <AMReX_MultiFabUtil.H>
 #include <AMReX_MLABecLaplacian.H>
 #include <AMReX_MLPoisson.H>
+#include <AMReX_MLMG.H>
 
 using namespace amrex;
 
@@ -19,21 +20,26 @@ solve_with_mlmg(const Vector<MultiFab*>& rhs,
     constexpr int max_coarsening_level = 30;
     constexpr bool agglomeration = true;
     constexpr bool consolidation = true;
-
+    constexpr int linop_maxorder = 3;
+    constexpr int max_iter = 100;
+    constexpr int max_fmg_iter = 100;
+    constexpr int verbose = 2;
+    constexpr int bottom_verbose = 0;
+    
     LPInfo info;
     info.setAgglomeration(agglomeration);
     info.setConsolidation(consolidation);
     info.setMaxCoarseningLevel(max_coarsening_level);
 
-    int nlevs = finest_level - base_level + 1;
+    int nlevels = finest_level - base_level + 1;
     
     // Have to do some packing because these arrays does not always start with base_level
-    Vector<Geometry> geom_p(nlevs);
-    Vector<MultiFab*> rhs_p(nlevs);
-    Vector<MultiFab*> phi_p(nlevs);
-    Vector<BoxArray> grids(nlevs);
-    Vector<DistributionMapping> dmap(nlevs);
-    for (int ilev = 0; ilev < nlevs; ++ilev) {
+    Vector<Geometry> geom_p(nlevels);
+    Vector<const MultiFab*> rhs_p(nlevels);
+    Vector<MultiFab*> phi_p(nlevels);
+    Vector<BoxArray> grids(nlevels);
+    Vector<DistributionMapping> dmap(nlevels);
+    for (int ilev = 0; ilev < nlevels; ++ilev) {
 	geom_p[ilev] = geom[ilev+base_level];
 	rhs_p[ilev]  = rhs[ilev+base_level];
 	phi_p[ilev]  = phi[ilev+base_level];
@@ -42,7 +48,28 @@ solve_with_mlmg(const Vector<MultiFab*>& rhs,
     }
     
     MLPoisson mlpoisson(geom_p, grids, dmap, info);
+
+    mlpoisson.setMaxOrder(linop_maxorder);
+
+    // This is a 3d problem with Periodic BCs
+    mlpoisson.setDomainBC({AMREX_D_DECL(LinOpBCType::Periodic,
+                                        LinOpBCType::Periodic,
+                                        LinOpBCType::Periodic)},
+        {AMREX_D_DECL(LinOpBCType::Periodic,
+                      LinOpBCType::Periodic,
+                      LinOpBCType::Periodic)});
     
+    for (int ilev = 0; ilev < nlevels; ++ilev)
+    {
+        mlpoisson.setLevelBC(ilev, phi[ilev]);
+    }
+
+    MLMG mlmg(mlpoisson);
+    mlmg.setMaxIter(max_iter);
+    mlmg.setMaxFmgIter(max_fmg_iter);
+    mlmg.setVerbose(verbose);
+    mlmg.setBottomVerbose(bottom_verbose);
+    mlmg.solve(phi_p, rhs_p, tol, abs_tol);
 }
 
 // void 
