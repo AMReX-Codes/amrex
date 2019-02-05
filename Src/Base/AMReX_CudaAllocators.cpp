3c286fd647a1a7f836f7ed5b24dc41cb192bb5c9
Andrew Myers <atmyers@lbl.gov>
2019-02-04 19:18:02 -0500

    a Pinned allocator for use with Thrust

--- Src/Base/AMReX_CudaAllocators.cpp
+++ Src/Base/AMReX_CudaAllocators.cpp
@@ -7,11 +7,13 @@ namespace amrex
 namespace
 {
     ThrustCachedAllocator g_allocator;
+    ThrustPinnedAllocator g_pinned_allocator;
 }
 
 namespace Cuda
 {
     ThrustCachedAllocator& The_ThrustCachedAllocator () { return g_allocator; };
+    ThrustPinnedAllocator& The_ThrustPinnedAllocator () { return g_pinned_allocator; };
 }
 
 }
