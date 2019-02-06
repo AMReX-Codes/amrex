500934cec055fa0dea360691af579db2ef17583d
Andrew Myers <atmyers@lbl.gov>
2019-02-06 17:08:26 -0500

    hardwire nppc to (1, 1, 1)

--- Tests/Particles/GPUNeighborList/main.cpp
+++ Tests/Particles/GPUNeighborList/main.cpp
@@ -9,8 +9,7 @@ using namespace amrex;
 
 struct TestParams
 {
-    IntVect ncell;      // num cells in domain
-    IntVect nppc;       // number of particles per cell in each dim
+    int size;
     int max_grid_size;
     int nsteps;
     bool write_plot;
@@ -28,8 +27,7 @@ int main (int argc, char* argv[])
 void get_test_params(TestParams& params)
 {
     ParmParse pp;
-    pp.get("ncell", params.ncell);
-    pp.get("nppc",  params.nppc);
+    pp.get("size", params.size);
     pp.get("max_grid_size", params.max_grid_size);
     pp.get("nsteps", params.nsteps);
     pp.get("write_plot", params.write_plot);
@@ -44,11 +42,11 @@ void main_main ()
     for (int n = 0; n < BL_SPACEDIM; n++)
     {
         real_box.setLo(n, 0.0);
-        real_box.setHi(n, 1.0);
+        real_box.setHi(n, params.size);
     }
 
     IntVect domain_lo(AMREX_D_DECL(0, 0, 0));
-    IntVect domain_hi(AMREX_D_DECL(params.ncell[0]-1,params.ncell[1]-1,params.ncell[2]-1));
+    IntVect domain_hi(AMREX_D_DECL(params.size-1,params.size-1,params.size-1));
     const Box domain(domain_lo, domain_hi);
 
     int coord = 0;
@@ -63,7 +61,9 @@ void main_main ()
 
     MDParticleContainer pc(geom, dm, ba);
 
-    pc.InitParticles(params.nppc, 0.01, 10.0);
+    IntVect nppc = IntVect(AMREX_D_DECL(1, 1, 1));
+
+    pc.InitParticles(nppc, 1.0, 0.0);
 
     pc.BuildNeighborList();
 }
