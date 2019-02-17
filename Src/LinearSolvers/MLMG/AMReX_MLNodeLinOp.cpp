9e94e935e84dad2a702c9d3b317eec04f0eded50
Brandon Runnels <brunnels@uccs.edu>
2019-02-16 17:51:49 -0700

    Merge branch development into nodeghostcells

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLinOp.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLinOp.cpp
@@ -131,26 +131,18 @@ MLNodeLinOp::solutionResidual (int amrlev, MultiFab& resid, MultiFab& x, const M
     const int ncomp = b.nComp();
     apply(amrlev, mglev, resid, x, BCMode::Inhomogeneous, StateMode::Solution);
 
-    if (resid.nGrow() != 2) amrex::Abort();
-    if (x.nGrow() != 2) amrex::Abort();
-    if (b.nGrow() != 2) amrex::Abort();
-
-    MultiFab::Xpay(resid, -1.0, b, 0, 0, ncomp, 2);
-    return;
-
     const iMultiFab& dmsk = *m_dirichlet_mask[amrlev][0];
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
     for (MFIter mfi(resid, true); mfi.isValid(); ++mfi)
     {
-     	    //const Box& bx = mfi.tilebox();
-	    Box bx = mfi.tilebox();
-     	    amrex_mlndlap_solution_residual(BL_TO_FORTRAN_BOX(bx),
-     					    BL_TO_FORTRAN_ANYD(resid[mfi]),
-     					    BL_TO_FORTRAN_ANYD(b[mfi]),
-     					    BL_TO_FORTRAN_ANYD(dmsk[mfi]),
-     					    &ncomp);
+	    const Box& bx = mfi.tilebox();
+	    amrex_mlndlap_solution_residual(BL_TO_FORTRAN_BOX(bx),
+					    BL_TO_FORTRAN_ANYD(resid[mfi]),
+					    BL_TO_FORTRAN_ANYD(b[mfi]),
+					    BL_TO_FORTRAN_ANYD(dmsk[mfi]),
+					    &ncomp);
     }
 }
 
