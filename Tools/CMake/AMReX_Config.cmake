1b6af3cb63061a11df4b224cd08753f07adfb8ca
Michele Rosso <mrosso@lbl.gov>
2019-03-14 15:18:32 -0700

    CMake: enable linking against amrex in shared lib mode. Fixes Issue #425

--- Tools/CMake/AMReX_Config.cmake
+++ Tools/CMake/AMReX_Config.cmake
@@ -45,7 +45,15 @@ function (configure_amrex)
    if ( ENABLE_PIC OR BUILD_SHARED_LIBS )
       set_target_properties ( amrex PROPERTIES POSITION_INDEPENDENT_CODE True )
    endif ()
-   
+
+   if(BUILD_SHARED_LIBS)
+      if(APPLE)
+         target_link_options(amrex PUBLIC -Wl,-undefined,warning)
+      else()
+         target_link_options(amrex PUBLIC -Wl,--warn-unresolved-symbols)
+      endif()
+   endif()
+  
    
    # 
    # Location of Fortran modules
