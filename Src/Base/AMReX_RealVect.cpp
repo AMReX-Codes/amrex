c3412db7c753be603b54bfe7d1437944d1f61848
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-03-15 15:21:17 -0700

    more noexcepts. remove BroadcastDistributionMap and BroadcastBoxArray

--- Src/Base/AMReX_RealVect.cpp
+++ Src/Base/AMReX_RealVect.cpp
@@ -13,41 +13,41 @@ namespace amrex
   const RealVect RealVect::Zero(AMREX_D_DECL(0.0,0.0,0.0));
 
   const Real*
-  RealVect::dataPtr() const
+  RealVect::dataPtr() const noexcept
   {
     return vect;
   }
 
   Real*
-  RealVect::dataPtr()
+  RealVect::dataPtr() noexcept
   {
     return vect;
   }
 
-  RealVect::RealVect (AMREX_D_DECL(Real i, Real j, Real k))
+  RealVect::RealVect (AMREX_D_DECL(Real i, Real j, Real k)) noexcept
   {
     AMREX_D_EXPR(vect[0] = i, vect[1] = j, vect[2] = k);
   }
 
-  RealVect::RealVect (const std::vector<Real>& vr )
+  RealVect::RealVect (const std::vector<Real>& vr ) noexcept
   {
     AMREX_D_EXPR(vect[0]=vr[0], vect[1]=vr[1], vect[2] = vr[2]);
   }
 
-  RealVect::RealVect ()
+  RealVect::RealVect () noexcept
   {
     AMREX_D_EXPR(vect[0]=0.0, vect[1]=0.0, vect[2] = 0.0);
   }
 
 
   RealVect&
-  RealVect::operator= (const RealVect &iv)
+  RealVect::operator= (const RealVect &iv) noexcept
   {
     AMREX_D_EXPR(vect[0]=iv.vect[0], vect[1]=iv.vect[1], vect[2]=iv.vect[2]);
     return *this;
   }
 
-  Real RealVect::dotProduct(const RealVect& a_rhs) const
+  Real RealVect::dotProduct(const RealVect& a_rhs) const noexcept
   {
     return AMREX_D_TERM(vect[0]*a_rhs.vect[0], +
                   vect[1]*a_rhs.vect[1], +
@@ -55,89 +55,89 @@ namespace amrex
   }
 
   bool
-  RealVect::operator== (const RealVect& p) const
+  RealVect::operator== (const RealVect& p) const noexcept
   {
     return AMREX_D_TERM(vect[0] == p[0], && vect[1] == p[1], && vect[2] == p[2]);
   }
 
   bool
-  RealVect::operator!= (const RealVect& p) const
+  RealVect::operator!= (const RealVect& p) const noexcept
   {
     return AMREX_D_TERM(vect[0] != p[0], || vect[1] != p[1], || vect[2] != p[2]);
   }
 
   RealVect&
-  RealVect::operator+= (Real s)
+  RealVect::operator+= (Real s) noexcept
   {
     AMREX_D_EXPR(vect[0] += s, vect[1] += s, vect[2] += s);
     return *this;
   }
 
   RealVect&
-  RealVect::operator+= (const RealVect& p)
+  RealVect::operator+= (const RealVect& p) noexcept
   {
     AMREX_D_EXPR(vect[0] += p[0], vect[1] += p[1], vect[2] += p[2]);
     return *this;
   }
 
   RealVect&
-  RealVect::operator*= (Real s)
+  RealVect::operator*= (Real s) noexcept
   {
     AMREX_D_EXPR(vect[0] *= s, vect[1] *= s, vect[2] *= s);
     return *this;
   }
 
   RealVect&
-  RealVect::operator*= (const RealVect &p)
+  RealVect::operator*= (const RealVect &p) noexcept
   {
     AMREX_D_EXPR(vect[0] *= p[0], vect[1] *= p[1], vect[2] *= p[2]);
     return *this;
   }
 
   RealVect
-  RealVect::operator* (Real s) const
+  RealVect::operator* (Real s) const noexcept
   {
     RealVect v(AMREX_D_DECL(vect[0]*s, vect[1]*s, vect[2]*s));
     return v;
   }
 
   RealVect
-  RealVect::operator- (Real s) const
+  RealVect::operator- (Real s) const noexcept
   {
     RealVect v(AMREX_D_DECL(vect[0]-s, vect[1]-s, vect[2]-s));
     return v;
   }
 
   RealVect
-  RealVect::operator+ (Real s) const
+  RealVect::operator+ (Real s) const noexcept
   {
     RealVect v(AMREX_D_DECL(vect[0]+s, vect[1]+s, vect[2]+s));
     return v;
   }
 
   RealVect&
-  RealVect::operator/= (Real s)
+  RealVect::operator/= (Real s) noexcept
   {
     AMREX_D_EXPR(vect[0] /= s, vect[1] /= s, vect[2] /= s);
     return *this;
   }
 
   RealVect&
-  RealVect::operator/= (const RealVect& p)
+  RealVect::operator/= (const RealVect& p) noexcept
   {
     AMREX_D_EXPR(vect[0] /= p[0], vect[1] /= p[1], vect[2] /= p[2]);
     return *this;
   }
 
   RealVect
-  RealVect::operator/ (Real s) const
+  RealVect::operator/ (Real s) const noexcept
   {
     RealVect result( AMREX_D_DECL( vect[0] / s, vect[1] / s, vect[2] / s));
     return result ;
   }
 
   int
-  RealVect::minDir(const bool& a_doAbs) const
+  RealVect::minDir(const bool& a_doAbs) const noexcept
   {
     int mDir = 0;
     for (int idir=0; idir<SpaceDim; idir++)
@@ -161,7 +161,7 @@ namespace amrex
   }
 
   int
-  RealVect::maxDir(const bool& a_doAbs) const
+  RealVect::maxDir(const bool& a_doAbs) const noexcept
   {
     int mDir = 0;
     for (int idir=0; idir<SpaceDim; idir++)
@@ -185,7 +185,7 @@ namespace amrex
   }
 
   RealVect
-  BASISREALV (int dir)
+  BASISREALV (int dir) noexcept
   {
     assert(dir >= 0 && dir < SpaceDim);
     RealVect tmp = RealVect::Zero ;
@@ -195,55 +195,55 @@ namespace amrex
 
   RealVect
   operator/ (Real            s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(s/p[0], s/p[1], s/p[2]));
   }
   RealVect
   operator+ (Real            s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(p[0] + s, p[1] + s, p[2] + s));
   }
 
   RealVect
   operator- (Real            s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(s - p[0], s - p[1], s - p[2]));
   }
 
   RealVect
   operator* (Real            s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(s * p[0], s * p[1], s * p[2]));
   }
 
   RealVect
   operator/ (const RealVect& s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(s[0] / p[0], s[1] /p[1], s[2] / p[2]));
   }
 
   RealVect
   operator+ (const RealVect& s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(p[0] + s[0], p[1] +s[1], p[2] + s[2]));
   }
 
   RealVect
   operator- (const RealVect& s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(s[0] - p[0], s[1] - p[1], s[2] - p[2]));
   }
 
   RealVect
   operator* (const RealVect& s,
-             const RealVect& p)
+             const RealVect& p) noexcept
   {
     return RealVect(AMREX_D_DECL(p[0] * s[0], p[1] *s[1], p[2] * s[2]));
   }
