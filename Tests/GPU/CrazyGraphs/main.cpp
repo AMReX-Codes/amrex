95106907a52f675201c078663ad029486769264b
kngott <kngott@lbl.gov>
2019-03-17 21:04:15 -0700

    Crazy Graphs. Attempt to reuse graphs with different parameters without re-instantiating. Needs CUDA 10.1 to test.

---
+++ Tests/GPU/CrazyGraphs/main.cpp
@@ -0,0 +1,119 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+__global__ void func (double *ptr)
+{
+    *ptr = 3;
+}
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+int main (int argc, char* argv[])
+{
+    std::cout << "**********************************\n";
+
+    {
+        // ===================================
+        // Simple cuda action to make sure all tests have cuda.
+        // Allows nvprof to return data.
+        int devices = 0;
+        cudaGetDeviceCount(&devices);
+
+        std::cout << "GPU devices: " << devices << "\n";
+        std::cout << "**********************************\n"; 
+        // ===================================
+
+        // Malloc value for setval testing.
+        double *p, *q;
+        cudaMallocManaged(&p, sizeof(double));
+        cudaMallocManaged(&q, sizeof(double));
+        *p = 0.0;
+        *q = 0.0;
+
+        cudaStream_t    stream;
+        cudaStreamCreate(&stream);
+
+        cudaGraph_t     graphP, graphQ;
+        cudaGraphExec_t graphExec;
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//      Create P graph, instantiate and execute. 
+
+        {
+#ifdef CRAZY 
+            cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+#else
+            cudaStreamBeginCapture(stream);
+#endif
+            func<<<1,1,0,stream>>>(p);
+            cudaStreamEndCapture(stream, &(graphP));
+
+            cudaGraphInstantiate(&graphExec, graphP, NULL, NULL, 0);
+            cudaGraphLaunch(graphExec, stream);  
+
+            cudaDeviceSynchronize();
+
+            std::cout << "1: p, q = " << *p << ", " << *q << std::endl;
+
+// ------------------------------------------------------------------------------- 
+//          Make Q graph
+
+#if CRAZY 
+            cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+#else
+            cudaStreamBeginCapture(stream);
+#endif
+            func<<<1,1,0,stream>>>(q);
+            cudaStreamEndCapture(stream, &(graphQ));
+
+// ------------------------------------------------------------------------------- 
+//          Make an empty node
+
+            cudaGraphNode_t emptyNode = NULL;
+            {
+                cudaGraph_t empty;
+                cudaGraphCreate(&empty, 0);
+                cudaGraphAddEmptyNode(&emptyNode, empty, NULL, 0);
+            }
+
+// ------------------------------------------------------------------------------- 
+//          Get nodes from graphs
+
+            size_t numP, numQ;
+            cudaGraphGetNodes(graphP, NULL, &numP);
+            cudaGraphGetNodes(graphQ, NULL, &numQ); 
+
+            std::cout << "numP, numQ = " << numP << ", " << numQ << std::endl;
+
+            cudaGraphNode_t *nodesP[numP], *nodesQ[numQ];
+            for (int i=0; i<numP; ++i)
+            {
+                nodesP[i] = &(emptyNode);
+                nodesQ[i] = &(emptyNode);
+            }
+
+            cudaGraphGetNodes(graphP, nodesP[0], &numP);
+            cudaGraphGetNodes(graphQ, nodesQ[0], &numQ);
+
+// ------------------------------------------------------------------------------- 
+//          Swap params in CudaGraphExec and re-execute without instantiation.
+
+            cudaKernelNodeParams knp;
+            cudaGraphKernelNodeGetParams(*nodesQ[0], &knp);
+#ifdef CRAZY
+            cudaGraphExecKernelNodeSetParams(graphExec, *nodesP[0], &knp);
+#endif
+            cudaGraphLaunch(graphExec, stream);  
+            cudaDeviceSynchronize();
+
+            std::cout << "2: p, q = " << *p << ", " << *q << std::endl;
+        }
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+        std::cout << "Test Completed." << std::endl;
+    }
+
+}
