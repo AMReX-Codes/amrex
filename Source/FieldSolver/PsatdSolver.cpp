279c3120fa298ebd33431c883a24ddc8c05def36
Remi Lehe <remi.lehe@normalesup.org>
2019-04-17 14:46:40 -0700

    Add coefficients and pusher

--- Source/FieldSolver/PsatdSolver.cpp
+++ Source/FieldSolver/PsatdSolver.cpp
@@ -1,3 +1,5 @@
+#include <WarpXConst.H>
+#include <cmath>
 
 using namespace amrex;
 using namespace Gpu;
@@ -14,8 +16,7 @@ class PsatdSolver
 
     private:
         SpectralVector kx, ky, kz;
-        SpectralCoefficients C_coef, S_coef;
-        Real dt;
+        SpectralCoefficients C_coef, S_ck_coef, X1_coef, X2_coef, X3_coef;
 };
 
 /*
@@ -23,7 +24,7 @@ class PsatdSolver
  * dm: DistributionMapping for spectral space
  */
 PsatdSolver::PsatdSolver( const BoxArray& ba, const DistributionMapping& dm,
-                          const Real* dx, const Real dt ) : dt(dt)
+                          const Real* dx, const Real dt )
 {
     // Allocate the 1D vectors
     kx = SpectralVector( ba, dm );
@@ -38,80 +39,134 @@ PsatdSolver::PsatdSolver( const BoxArray& ba, const DistributionMapping& dm,
 
     // Allocate the arrays of coefficients
     C_coef = SpectralMatrix( ba, dm, 1, 0 );
-    S_coef = SpectralMatrix( ba, dm, 1, 0 );
-    // Fill them with the right values
+    S_ck_coef = SpectralMatrix( ba, dm, 1, 0 );
+    X1_coef = SpectralMatrix( ba, dm, 1, 0 );
+    X2_coef = SpectralMatrix( ba, dm, 1, 0 );
+    X3_coef = SpectralMatrix( ba, dm, 1, 0 );
+
+    // Fill them with the right values:
+    // Loop over boxes
     for ( MFIter mfi(ba, dm); mfi.isValid(); ++mfi ){
-        FillCoefficients( C_coef[mfi], S_coef[mfi], kx[mfi], ky[mfi], kz[mfi] );
+
+        const Box& bx = mfi.box();
+
+        // Extract pointers for the k vectors
+        const Real* kx = kx[mfi].dataPtr();
+        const Real* ky = ky[mfi].dataPtr();
+        const Real* kz = kz[mfi].dataPtr();
+        // Extract arrays for the coefficients
+        Array4<Real> C = C_coef[mfi].array();
+        Array4<Real> S_ck = S_ck_coef[mfi].array();
+        Array4<Real> X1 = X1_coef[mfi].array();
+        Array4<Real> X2 = X2_coef[mfi].array();
+        Array4<Real> X3 = X3_coef[mfi].array();
+
+        // Loop over indices within one box
+        ParallelFor( bx,
+        [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
+        {
+            // Calculate norm of vector
+            const Real k_norm = std::sqrt( kx[i]*kx[i] + ky[j]*ky[j] + kz[k]*kz[k] );
+
+            // Calculate coefficients
+            constexpr Real c = PhysConst::c;
+            constexpr Real ep0 = PhysConst::ep0;
+            if ( k_norm != 0 ){
+                C(i,j,k) = std::cos( c*k_norm*dt );
+                S_ck(i,j,k) = std::sin( c*k_norm*dt )/( c*k_norm );
+                X1(i,j,k) = (1. - C(i,j,k))/(ep0 * c*c * k_norm*k_norm);
+                X2(i,j,k) = (1. - S_ck(i,j,k)/dt )/(ep0 * k_norm*k_norm);
+                X3(i,j,k) = (C(i,j,k) - S_ck(i,j,k)/dt )/(ep0 * k_norm*k_norm);
+            } else { // Handle k_norm = 0, by using the analytical limit
+                C(i,j,k) = 1.;
+                S_ck(i,j,k) = dt;
+                X1(i,j,k) = 0.5 * dt*dt / ep0;
+                X2(i,j,k) = c*c * dt*dt / (6.*ep0);
+                X3(i,j,k) = - c*c * dt*dt / (3.*ep0);
+            }
+        }
     }
 }
 
 void
 PsatdSolver::pushSpectralFields( SpectralFields& f ) const{
 
-
     // Loop over boxes
-    for ( MFIter mfi(ba, dm); mfi.isValid(); ++mfi ){
+    for ( MFIter mfi(f.Ex); mfi.isValid(); ++mfi ){
 
         const Box& bx = mfi.box();
 
         // Extract arrays for the fields to be updated
-        Array4<Complex> Ex = f.Ex[mfi].array();
-        Array4<Complex> Ey = f.Ey[mfi].array();
-        Array4<Complex> Ez = f.Ez[mfi].array();
-        Array4<Complex> Bx = f.Bx[mfi].array();
-        Array4<Complex> By = f.By[mfi].array();
-        Array4<Complex> Bz = f.Bz[mfi].array();
+        Array4<Complex> Ex_arr = f.Ex[mfi].array();
+        Array4<Complex> Ey_arr = f.Ey[mfi].array();
+        Array4<Complex> Ez_arr = f.Ez[mfi].array();
+        Array4<Complex> Bx_arr = f.Bx[mfi].array();
+        Array4<Complex> By_arr = f.By[mfi].array();
+        Array4<Complex> Bz_arr = f.Bz[mfi].array();
         // Extract arrays for J
         const Array4<Complex> Jx_arr = f.Jx[mfi].array();
         const Array4<Complex> Jy_arr = f.Jy[mfi].array();
         const Array4<Complex> Jz_arr = f.Jz[mfi].array();
-        const Array4<Complex> rho_old = f.rho_old[mfi].array();
-        const Array4<Complex> rho_new = f.rho_new[mfi].array();
+        const Array4<Complex> rho_old_arr = f.rho_old[mfi].array();
+        const Array4<Complex> rho_new_arr = f.rho_new[mfi].array();
         // Extract arrays for the coefficients
         const Array4<Real> C_arr = C_coef[mfi].array();
-        const Array4<Real> S_over_k_arr = S_over_k_coef[mfi].array();
-        const Array4<Real> inv_k2 =
+        const Array4<Real> S_ck_arr = S_ck_coef[mfi].array();
+        const Array4<Real> inv_k2_arr =
         // Extract pointers for the k vectors
         const Real* kx_arr = kx[mfi].dataPtr();
         const Real* ky_arr = ky[mfi].dataPtr();
         const Real* kz_arr = kz[mfi].dataPtr();
 
+        // Loop over indices within one box
         ParallelFor( bx,
-        [=] AMREX_GPU_DEVICE(int i, int j, int k)
+        [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept
         {
             // Record old values of the fields to be updated
-            const Complex Ex_old = Ex(i,j,k);
-            const Complex Ey_old = Ey(i,j,k);
-            const Complex Ez_old = Ez(i,j,k);
-            const Complex Bx_old = Bx(i,j,k);
-            const Complex By_old = By(i,j,k);
-            const Complex Bz_old = Bz(i,j,k);
+            const Complex Ex_old = Ex_arr(i,j,k);
+            const Complex Ey_old = Ey_arr(i,j,k);
+            const Complex Ez_old = Ez_arr(i,j,k);
+            const Complex Bx_old = Bx_arr(i,j,k);
+            const Complex By_old = By_arr(i,j,k);
+            const Complex Bz_old = Bz_arr(i,j,k);
             // k vector values, and coefficients
             const Real kx = kx_arr[i];
             const Real ky = ky_arr[j];
             const Real kz = kz_arr[k];
+            constexpr Real c2 = PhysConst::c*PhysConst::c;
+            constexpr Real inv_ep0 = 1./PhysConst::ep0;
+            constexpr Complex I = Complex{0,1};
             const Real C = C_arr(i,j,k);
-            const Real S_over_k = S_over_k_arr(i,j,k);
-            const Real inv_k2 = inv_k2_arr(i,j,k);
-            const I = Complex{0,1};
-            // Calculate divE and divJ
-            const k_dot_E = (kx*Ex_old + ky*Ey_old + kz*Ez_old);
-            const k_dot_J = (kx*Jx + ky*Jy + kz*Jz);
+            const Real S_ck = S_ck_arr(i,j,k);
+            const Real X1 = X1_arr(i,j,k);
+            const Real X2 = X2_arr(i,j,k);
+            const Real X3 = X3_arr(i,j,k);
+            // Short cut for the values of J and rho
+            const Complex Jx = Jx_arr(i,j,k);
+            const Complex Jy = Jy_arr(i,j,k);
+            const Complex Jz = Jz_arr(i,j,k);
 
-            // Update E
-            Ex(i,j,k) = C*Ex_old + I*S_over_k*( ky*Bz_old - kz*By_old )
-                        - S_over_k*Jx
-                        + (1.-C)*inv_k2*k_dot_E*kx
-                        + (S_over_k - dt)*inv_k2*k_dot_J*kx;
-
-
-
-            // Update B
-
-
+            // Update E (see WarpX online documentation: theory section)
+            Ex_arr(i,j,k) = C*Ex_old
+                        + S_ck*( c2*I*(ky*Bz_old - kz*By_old) - inv_ep0*Jx )
+                        - I*( X2*rho_new - X3*rho_old )*kx;
+            Ey_arr(i,j,k) = C*Ey_old
+                        + S_ck*( c2*I*(kz*Bx_old - kx*Bz_old) - inv_ep0*Jy )
+                        - I*( X2*rho_new - X3*rho_old )*ky;
+            Ez_arr(i,j,k) = C*Ez_old
+                        + S_ck*( c2*I*(kx*By_old - ky*Bx_old) - inv_ep0*Jz )
+                        - I*( X2*rho_new - X3*rho_old )*kz;
+            // Update B (see WarpX online documentation: theory section)
+            Bx_arr(i,j,k) = C*Bx_old
+                        - S_ck*I*(ky*Ez_old - kz*Ey_old)
+                        +   X1*I*(ky*Jz_old - kz*Jy_old);
+            By_arr(i,j,k) = C*By_old
+                        - S_ck*I*(kz*Ex_old - kx*Ez_old)
+                        +   X1*I*(kz*Jx_old - kx*Jz_old);
+            Bz_arr(i,j,k) = C*Bz_old
+                        - S_ck*I*(kx*Ey_old - ky*Ex_old)
+                        +   X1*I*(kx*Jy_old - ky*Jx_old);
         });
-
-
     }
 }
 
