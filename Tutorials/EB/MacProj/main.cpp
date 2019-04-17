2d3d15269503b3f832c36f6337e53dafcfd46590
Andy Nonaka <AJNonaka@lbl.gov>
2019-04-17 15:36:31 -0700

    this tutorial now uses a mac projection to compute flow around a sphere given an initial velocity field of (1,0,0).  There is also now a plotfile output that outputs the initial and final velocity and divergence

--- Tutorials/EB/MacProj/main.cpp
+++ Tutorials/EB/MacProj/main.cpp
@@ -1,10 +1,10 @@
 
 #include <AMReX.H>
 #include <AMReX_ParmParse.H>
-#include <AMReX_VisMF.H>
-#include <AMReX_EB2.H>
 #include <AMReX_EBMultiFabUtil.H>
 #include <AMReX_MacProjector.H>
+#include <AMReX_PlotFileUtil.H>
+#include <AMReX_MultiFabUtil.H>
 #include "initEB.H"
 
 using namespace amrex;
@@ -22,7 +22,6 @@ int main (int argc, char* argv[])
         // read parameters
         {
             ParmParse pp;
-            pp.query("v", verbose);
             pp.query("verbose", verbose);
             pp.query("n_cell", n_cell);
             pp.query("max_grid_size", max_grid_size);
@@ -52,29 +51,35 @@ int main (int argc, char* argv[])
         const EB2::Level& eb_level = eb_is.getLevel(geom);
         EBFArrayBoxFactory factory(eb_level, geom, grids, dmap, {2,2,2}, EBSupport::full);
 
+        // plot variables; velocity-before, div-before, velocity-after, div-after
+        MultiFab plotfile_mf;
+        plotfile_mf.define(grids, dmap, 2*AMREX_SPACEDIM+2, 0, MFInfo(), factory);
+
         Array<MultiFab,AMREX_SPACEDIM> vel;
         Array<MultiFab,AMREX_SPACEDIM> beta;
         for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
             vel[idim].define(amrex::convert(grids,IntVect::TheDimensionVector(idim)),
                              dmap, 1, 1, MFInfo(), factory);
-            vel[idim].setVal(2.0*(idim+1));
             beta[idim].define(amrex::convert(grids,IntVect::TheDimensionVector(idim)),
                               dmap, 1, 0, MFInfo(), factory);
             beta[idim].setVal(1.0);
         }
 
+        // set initial velocity to u=(1,0,0)
+        AMREX_D_TERM(vel[0].setVal(1.0);,
+                     vel[1].setVal(0.0);,
+                     vel[2].setVal(0.0););
+
+        // copy velocity into plotfile
+        average_face_to_cellcenter(plotfile_mf,0,amrex::GetArrOfConstPtrs(vel));
+        
         MultiFab divu(grids, dmap, 1, 0, MFInfo(), factory);
-
-        AMREX_D_TERM(amrex::VisMF::Write(vel[0], "vx-pre");,
-                     amrex::VisMF::Write(vel[1], "vy-pre");,
-                     amrex::VisMF::Write(vel[2], "vz-pre"););
-        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-            vel[idim].FillBoundary(geom.periodicity());
-        }
         EB_computeDivergence(divu, amrex::GetArrOfConstPtrs(vel), geom);
-//        amrex::VisMF::Write(divu, "divu-pre");
         amrex::Print() << "\nmax-norm of divu before projection is " << divu.norm0() << "\n" << std::endl;
-
+        
+        // copy divergence into plotfile
+        plotfile_mf.copy(divu,0,AMREX_SPACEDIM,1);
+        
         MacProjector macproj({amrex::GetArrOfPtrs(vel)},       // mac velocity
                              {amrex::GetArrOfConstPtrs(beta)}, // beta 
                              {geom});                          // Geometry
@@ -91,16 +96,26 @@ int main (int argc, char* argv[])
         Real reltol = 1.e-12;
         macproj.project(reltol);
 
-//        AMREX_D_TERM(amrex::VisMF::Write(vel[0], "vx");,
-//                     amrex::VisMF::Write(vel[1], "vy");,
-//                     amrex::VisMF::Write(vel[2], "vz"););
-
         for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
             vel[idim].FillBoundary(geom.periodicity());
         }
+
+        // copy velocity into plotfile
+        average_face_to_cellcenter(plotfile_mf,AMREX_SPACEDIM+1,amrex::GetArrOfConstPtrs(vel));
+
         EB_computeDivergence(divu, amrex::GetArrOfConstPtrs(vel), geom);
-//        amrex::VisMF::Write(divu, "divu");
         amrex::Print() << "\nmax-norm of divu after projection is " << divu.norm0() << "\n" << std::endl;
+
+        // copy divergence into plotfile
+        plotfile_mf.copy(divu,0,2*AMREX_SPACEDIM+1,1);
+
+#if (AMREX_SPACEDIM == 2)        
+        EB_WriteSingleLevelPlotfile("plt", plotfile_mf, {"before-vx", "before-vy", "divu-before",
+                                    "after-vx", "after-vy", "divu-after"}, geom, 0.0, 0);
+#elif (AMREX_SPACEDIM == 3)
+        EB_WriteSingleLevelPlotfile("plt", plotfile_mf, {"before-vx", "before-vy", "before-vz", "divu-before",
+                                                         "after-vx", "after-vy", "after-vz", "divu-after"}, geom, 0.0, 0);
+#endif
     }
 
     amrex::Finalize();
