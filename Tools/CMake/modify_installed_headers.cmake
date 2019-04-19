be74cd825280900764e5bf1fc9370431017524dc
Mark Meredith <mwm126@gmail.com>
2019-04-19 10:55:17 -0400

    Remove dependency on sed

--- Tools/CMake/modify_installed_headers.cmake
+++ Tools/CMake/modify_installed_headers.cmake
@@ -1,12 +1,12 @@
 #
-# Add line #include<AMReX_Config.H> to all installed headers
-# 
-message(STATUS "Modifying installed headers")
+# Prepend line #include<AMReX_Config.H> to all installed headers
+#
+message(STATUS "Adding #include <AMReX_Config.H> to installed headers")
 file(GLOB _installed_headers "${CMAKE_INSTALL_PREFIX}/include/*.H")
 list(REMOVE_ITEM _installed_headers "${CMAKE_INSTALL_PREFIX}/include/AMReX_Config.H")
 
-foreach( _header IN LISTS _installed_headers)
-   # This command only affect the first line of each file. Therefore, it can be used countless
-   # times on the same file and it always gives the same result (i.e. it does not keep prepending)
-   execute_process(COMMAND bash "-c" "sed -i '1s/^/#include <AMReX_Config.H>\\n\\n/' ${_header}" )
+foreach( _header IN LISTS _installed_headers )
+  file(READ "${_header}" HEADER_CONTENTS)
+  file(WRITE "${_header}" "#include <AMReX_Config.H>\n")
+  file(APPEND "${_header}" "${HEADER_CONTENTS}")
 endforeach()
