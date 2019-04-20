a9f9781195445dacedcc8e7ecaa1c7500bbe17fd
Michele Rosso <mrosso@lbl.gov>
2019-04-19 17:17:01 -0700

    CMake: treat C++ and Fortran flags in a target-oriented fashion
    * Define INTERFACE targets for C++ and Fortran flags.
    * Export those targets so that app codes can use them.

--- Tools/CMake/AMReX_Compilers.cmake
+++ Tools/CMake/AMReX_Compilers.cmake
@@ -71,68 +71,17 @@ function ( set_compiler_flags_preset _target )
    set(_cxx_apple_dbg   "$<AND:${_cxx},${_debug},${_apple}>")
    set(_cxx_apple_rel   "$<AND:${_cxx},${_release},${_apple}>")
 
-
-   #
-   # Set Fortran Flags only if not provided by user
-   # Since genex "Fortran_COMPILER_ID" is not supported, we use genex
-   # "STREQUAL" as workaround 
-   #
-   if ( NOT CMAKE_Fortran_FLAGS )     
-       target_compile_options ( ${_target}
-         PUBLIC
-         # GNU Debug
-         $<BUILD_INTERFACE:
-         $<${_debug}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","GNU">:$<${_fortran}:
-         -O0 -ggdb -fbounds-check -fbacktrace -Wuninitialized -Wunused -finit-real=snan -finit-integer=2147483647>>>
-         # GNU Release
-         $<${_release}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","GNU">:$<${_fortran}:
-         >>>
-         # Intel Debug
-         $<${_debug}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Intel">:$<${_fortran}:
-         -O0 -traceback -check bounds,uninit,pointers>>>
-         # Intel Release
-         $<${_release}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Intel">:$<${_fortran}:
-         -ip -qopt-report=5 -qopt-report-phase=vec>>>
-         # Cray Debug
-         $<${_debug}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Cray">:$<${_fortran}:-O0 -e i>>>
-         # Cray Release 
-         $<${_release}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Cray">:$<${_fortran}:>>>
-         # PGI Debug
-         $<${_debug}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","PGI">:$<${_fortran}:-O0 -Mbounds -Mchkptr>>>
-         # PGI Release
-         $<${_release}:$<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","PGI">:$<${_fortran}:-gopt -fast>>>
-         >
-         )            
-   endif ()
    
-   #
-   # Set REQUIRED fortran flags (required by at least amrdata)
-   # 
-   target_compile_options ( ${_target}
-      PRIVATE
-      $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","GNU">:$<${_fortran}:-ffixed-line-length-none -ffree-line-length-none>> 
-      $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Intel">:$<${_fortran}:-extend_source>>
-      $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Cray">:$<${_fortran}:-N 255 -h list=a>>
-      $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","PGI">:$<${_fortran}:-Mextend>> )
+   # #
+   # # Set REQUIRED fortran flags (required by at least amrdata)
+   # # 
+   # target_compile_options ( ${_target}
+   #    PRIVATE
+   #    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","GNU">:$<${_fortran}:-ffixed-line-length-none -ffree-line-length-none>> 
+   #    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Intel">:$<${_fortran}:-extend_source>>
+   #    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Cray">:$<${_fortran}:-N 255 -h list=a>>
+   #    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","PGI">:$<${_fortran}:-Mextend>> )
 
-   #
-   # Set C++ Flags only if not provided by user
-   # 
-   if ( NOT CMAKE_CXX_FLAGS )
-      target_compile_options ( ${_target}
-         PUBLIC
-         $<BUILD_INTERFACE:
-         $<${_cxx_gnu_dbg}:-O0 -fno-inline -ggdb -Wall -Wno-sign-compare>
-         $<${_cxx_gnu_rel}:>
-         $<${_cxx_intel_dbg}:-O0 -traceback -Wcheck>
-         $<${_cxx_intel_rel}:-ip -qopt-report=5 -qopt-report-phase=vec>
-         $<${_cxx_pgi_dbg}:-O0 -Mbounds>
-         $<${_cxx_pgi_rel}:-gopt -fast>
-         $<${_cxx_cray_dbg}:-O0>
-         $<${_cxx_cray_rel}:>
-         >
-         )          
-   endif ()
 
    # C++ REQUIRED flags
    # Until "cxx_std_11" and similar options are available (CMake >= 3.8 )
@@ -154,29 +103,7 @@ function ( set_compiler_flags_preset _target )
          $<${_cxx_apple}:-std=${_cxx_std}>
          )
    
-   #
-   # Floating-point exceptions flags only if enabled
-   # 
-   if (DEFINED ENABLE_FPE)
-      if (ENABLE_FPE)
-   	 target_compile_options ( ${_target}
-   	    PUBLIC
-	    # GNU
-	    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","GNU">:$<${_fortran}:-ffpe-trap=invalid,zero -ftrapv>>
-	    $<${_cxx_gnu}:-ftrapv>
-	    # Intel
-	    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Intel">:$<${_fortran}:-fpe3>>
-	    $<${_cxx_intel}:-fpe3>
-	    # Cray
-	    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","Cray">:$<${_fortran}:-K trap=fp>>
-	    $<${_cxx_cray}:-K trap=fp>
-	    #  PGI
-	    $<$<STREQUAL:"${CMAKE_Fortran_COMPILER_ID}","PGI">:$<${_fortran}:-Ktrap=divz,inv>>
-	    $<${_cxx_pgi}:> )
-      endif ()
-   else ()
-      message (AUTHOR_WARNING "Variable ENABLE_FPE is not defined")
-   endif ()
+
    
 endfunction () 
 
