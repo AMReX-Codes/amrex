b7ff3247713c4e84cd3eddb3e4d2df25cf66a970
Maikel Nadolski <maikel.nadolski@gmail.com>
2019-04-24 09:32:08 +0200

    Improve AmrCore and AmrMesh constructors.
    Change the refinement ratio paramter of AmrMesh's constructor from
    std::vector<int> to amrex::Vector<IntVect> to be more consistent with
    non-uniform refinement ratios.
    
    Add a refrat parameter to AmrCore's constructor. Before this change
    AmrMesh's refinment parameter was never set to this custom value.

--- Src/AmrCore/AMReX_AmrMesh.cpp
+++ Src/AmrCore/AMReX_AmrMesh.cpp
@@ -37,7 +37,7 @@ AmrMesh::AmrMesh ()
 }
 
 AmrMesh::AmrMesh (const RealBox* rb, int max_level_in, const Vector<int>& n_cell_in, int coord,
-                  std::vector<int> a_refrat)
+                  Vector<IntVect> a_refrat)
 {
   Initialize();
 
@@ -51,7 +51,7 @@ AmrMesh::~AmrMesh ()
 }
 
 void
-AmrMesh::InitAmrMesh (int max_level_in, const Vector<int>& n_cell_in, std::vector<int> a_refrat)
+AmrMesh::InitAmrMesh (int max_level_in, const Vector<int>& n_cell_in, Vector<IntVect> a_refrat)
 {
     verbose   = 0;
     grid_eff  = 0.7;
@@ -215,8 +215,7 @@ AmrMesh::InitAmrMesh (int max_level_in, const Vector<int>& n_cell_in, std::vecto
     {
       for (int i = 0; i < max_level; i++)
       {
-        for (int n = 0; n < AMREX_SPACEDIM; n++)
-          ref_ratio[i][n] = a_refrat[i];
+          ref_ratio[i] = a_refrat[i];
       }
     }
 
