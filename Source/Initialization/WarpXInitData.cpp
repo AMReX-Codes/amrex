8736febeb8c416d24a2aec432fde2e6d06474291
MaxThevenet <mthevenet@lbl.gov>
2019-05-08 15:35:12 -0700

    Remove old NCI corrector (using PICSAR)

--- Source/Initialization/WarpXInitData.cpp
+++ Source/Initialization/WarpXInitData.cpp
@@ -176,16 +176,13 @@ WarpX::InitNCICorrector ()
             }
             cdtodz = PhysConst::c * dt[lev] / dz;
 
+            // Initialize Godfrey filters
             nci_godfrey_filter_exeybz[lev].reset( new NCIGodfreyFilter(godfrey_coeff_set::Ex_Ey_Bz, cdtodz, WarpX::l_lower_order_in_v) );
             nci_godfrey_filter_bxbyez[lev].reset( new NCIGodfreyFilter(godfrey_coeff_set::Bx_By_Ez, cdtodz, WarpX::l_lower_order_in_v) );
 
+            // Compute Godfrey filters stencils
             nci_godfrey_filter_exeybz[lev]->ComputeStencils();
             nci_godfrey_filter_bxbyez[lev]->ComputeStencils();
-
-            //WRPX_PXR_NCI_CORR_INIT( (mypc->fdtd_nci_stencilz_ex)[lev].data(),
-            //                        (mypc->fdtd_nci_stencilz_by)[lev].data(),
-            //                        mypc->nstencilz_fdtd_nci_corr, cdtodz,
-            //                        WarpX::l_lower_order_in_v);
         }
     }
 }
