6b4dfc514111ce16919a90f6b9220bfd7dbb5251
kngott <kngott@lbl.gov>
2019-05-22 14:49:40 -0700

    Updated fused Test.

--- Tests/GPU/FusedLaunches/main.cpp
+++ Tests/GPU/FusedLaunches/main.cpp
@@ -32,6 +32,62 @@ void copy (int size, amrex::Box* bx, amrex::Dim3* offset,
     }
 }
 
+/*
+AMREX_GPU_GLOBAL
+void copy (int size, amrex::Box* bx, amrex::Dim3* offset,
+           amrex::Array4<Real>* src, amrex::Array4<Real>* dst, 
+           int scomp, int dcomp, int ncomp, int simul)
+{
+    for (int l = 0; l < size; l+=simul)
+    {
+        for (int m = 0; m < simul; m++)
+        {
+
+        }
+
+        int ncells = bx[l].numPts();
+        const auto lo = amrex::lbound(bx[l]);
+        const auto len = amrex::length(bx[l]);
+ 
+        for (int icell = blockDim.x*blockIdx.x+threadIdx.x, stride = blockDim.x*gridDim.x;
+                 icell < ncells; icell += stride) {
+            int k =  icell /   (len.x*len.y);
+            int j = (icell - k*(len.x*len.y)) /   len.x;
+            int i = (icell - k*(len.x*len.y)) - j*len.x;
+            i += lo.x;
+            j += lo.y;
+            k += lo.z;
+            for (int n = 0; n < ncomp; ++n) {
+                (dst[l])(i,j,k,dcomp+n) = (src[l])(i+offset[l].x,j+offset[l].y,k+offset[l].z,scomp+n); 
+            }
+        }
+    }
+}
+*/
+
+AMREX_GPU_GLOBAL
+void copy (amrex::Box bx,
+           amrex::Dim3 offset, amrex::Array4<Real> src, amrex::Array4<Real> dst, 
+           int scomp, int dcomp, int ncomp)
+{
+    int ncells = bx.numPts();
+    const auto lo = amrex::lbound(bx);
+    const auto len = amrex::length(bx);
+
+    for (int icell = blockDim.x*blockIdx.x+threadIdx.x, stride = blockDim.x*gridDim.x;
+             icell < ncells; icell += stride) {
+        int k =  icell /   (len.x*len.y);
+        int j = (icell - k*(len.x*len.y)) /   len.x;
+        int i = (icell - k*(len.x*len.y)) - j*len.x;
+        i += lo.x;
+        j += lo.y;
+        k += lo.z;
+        for (int n = 0; n < ncomp; ++n) {
+            dst(i,j,k,dcomp+n) = src(i+offset.x,j+offset.y,k+offset.z,scomp+n); 
+        }
+    }
+}
+
 AMREX_GPU_GLOBAL
 void copy (amrex::Dim3 lo, amrex::Dim3 len, int ncells,
            amrex::Dim3 offset, amrex::Array4<Real> src, amrex::Array4<Real> dst, 
@@ -50,6 +106,82 @@ void copy (amrex::Dim3 lo, amrex::Dim3 len, int ncells,
         }
     }
 }
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+void standardLaunch(MultiFab& src_fab, MultiFab& dst_fab, Real src_val, Real dst_val,
+                                      int Ncomp, int num_streams = Gpu::Device::numCudaStreams())
+{
+    src_fab.setVal(src_val);
+    dst_fab.setVal(dst_val);
+
+    if (num_streams > Gpu::Device::numCudaStreams())
+    {
+        num_streams = Gpu::Device::numCudaStreams();
+        std::cout << "Too many streams requested. Using maximum value of " << num_streams << std::endl;
+    }
+
+    BL_PROFILE_VAR(std::string("STANDARD " + std::to_string(num_streams)), standard);
+    for (MFIter mfi(src_fab); mfi.isValid(); ++mfi)
+    {
+        Gpu::Device::setStreamIndex(mfi.LocalIndex() % num_streams);
+        const Box bx = mfi.validbox();
+
+        const auto src = src_fab.array(mfi);
+        const auto dst = dst_fab.array(mfi);
+
+        const int ncells = bx.numPts();
+        const auto lo  = amrex::lbound(bx);
+        const auto len = amrex::length(bx);
+        const Dim3 offset = {0,0,0};
+
+        const auto ec = Cuda::ExecutionConfig(bx.numPts());
+        AMREX_CUDA_LAUNCH_GLOBAL(ec, copy,
+                                 lo, len, ncells,
+                                 offset, src, dst,
+                                 0, 0, Ncomp);
+    }
+    BL_PROFILE_VAR_STOP(standard);
+
+    amrex::Print() << "Standard sum = " << dst_fab.sum() 
+                   << "; Expected value = " << src_fab.sum() << std::endl;
+}
+
+void standardBoxLaunch(MultiFab& src_fab, MultiFab& dst_fab, Real src_val, Real dst_val,
+                                         int Ncomp, int num_streams = Gpu::Device::numCudaStreams())
+{
+    src_fab.setVal(src_val);
+    dst_fab.setVal(dst_val);
+
+    if (num_streams > Gpu::Device::numCudaStreams())
+    {
+        num_streams = Gpu::Device::numCudaStreams();
+        std::cout << "Too many streams requested. Using maximum value of " << num_streams << std::endl;
+    }
+
+    BL_PROFILE_VAR(std::string("STANDARD BOX " + std::to_string(num_streams)), standard);
+    for (MFIter mfi(src_fab); mfi.isValid(); ++mfi)
+    {
+        Gpu::Device::setStreamIndex(mfi.LocalIndex() % num_streams);
+        const Box bx = mfi.validbox();
+
+        const auto src = src_fab.array(mfi);
+        const auto dst = dst_fab.array(mfi);
+
+        const Dim3 offset = {0,0,0};
+
+        const auto ec = Cuda::ExecutionConfig(bx.numPts());
+        AMREX_CUDA_LAUNCH_GLOBAL(ec, copy, bx,
+                                 offset, src, dst,
+                                 0, 0, Ncomp);
+    }
+    BL_PROFILE_VAR_STOP(standard);
+
+    amrex::Print() << "Standard sum = " << dst_fab.sum() 
+                   << "; Expected value = " << src_fab.sum() << std::endl;
+}
+
+
 
 // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
@@ -112,181 +244,33 @@ int main (int argc, char* argv[])
                        << "Number of boxes per MultiFab: " << x.size() << std::endl << std::endl;
 
 // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//      Launch without graphs
-/*
-        {
-            x.setVal(4.5);
-            y.setVal(5.3);
 
-            BL_PROFILE("HtoD Copy Clean-up");
+        standardLaunch(x, y, 0.004, 0.0027, 1, 16);
+        standardLaunch(x, y, 0.867, 0.5309, 1, 2);
+        standardLaunch(x, y, 0.123, 0.4560, 1, 1);
 
-            for (MFIter mfi(x); mfi.isValid(); ++mfi)
-            {
-                const Box& bx = mfi.validbox();
-                Array4<Real> src = x.array(mfi);
-                Array4<Real> dst = y.array(mfi);
-                Dim3 offset {0,0,0};
-                int dcomp = 0;
-                int scomp = 0;
-
-                AMREX_HOST_DEVICE_FOR_4D (bx, Ncomp, i, j, k, n,
-                {
-                    dst(i,j,k,dcomp+n) = src(i+offset.x,j+offset.y,k+offset.z,scomp+n); 
-                });
-            }
-
-            amrex::Print() << "Clean up sum = " << y.sum() << "; Expected value = " << x.sum() << std::endl;
-        }
-
-// ---------------------------------------
+        standardBoxLaunch(x, y, 0.004, 0.0027, 1, 16);
+        standardBoxLaunch(x, y, 0.867, 0.5309, 1, 2);
+        standardBoxLaunch(x, y, 0.123, 0.4560, 1, 1);
 
         {
-            x.setVal(4.0);
-            y.setVal(5.0);
+            x.setVal(0.123);
+            y.setVal(0.456);
 
-            BL_PROFILE("Lambda Copy - Array4");
-
-            for (MFIter mfi(x); mfi.isValid(); ++mfi)
-            {
-                const Box& bx = mfi.validbox();
-                Array4<Real> src = x.array(mfi);
-                Array4<Real> dst = y.array(mfi);
-                Dim3 offset {0,0,0};
-                int dcomp = 0;
-                int scomp = 0;
-
-                AMREX_HOST_DEVICE_FOR_4D ( bx, Ncomp, i, j, k, n,
-                {
-                    dst(i,j,k,dcomp+n) = src(i+offset.x,j+offset.y,k+offset.z,scomp+n); 
-                });
-            }
-
-            amrex::Print() << "No Graph Lambda sum = " << y.sum() << "; Expected value = " << x.sum() << std::endl;
-        }
-*/
-// ---------------------------------------
-
-        {
-            x.setVal(0.004);
-            y.setVal(0.0027);
-
-            BL_PROFILE_VAR("First", first);
-
-            const int numStreams = 16;
-
-            for (MFIter mfi(x); mfi.isValid(); ++mfi)
-            {
-                Gpu::Device::setStreamIndex(mfi.LocalIndex() % numStreams);
-                const Box bx = mfi.validbox();
-
-                const auto src = x.array(mfi);
-                const auto dst = y.array(mfi);
-
-                const int ncells = bx.numPts();
-                const auto lo  = amrex::lbound(bx);
-                const auto len = amrex::length(bx);
-                const Dim3 offset = {0,0,0};
-
-                const auto ec = Cuda::ExecutionConfig(bx.numPts());
-                AMREX_CUDA_LAUNCH_GLOBAL(ec, copy,
-                                         lo, len, ncells,
-                                         offset, src, dst,
-                                         0, 0, Ncomp);
-            }
-            BL_PROFILE_VAR_STOP(first);
-
-            amrex::Print() << "First = " << y.sum() << "; Expected value = " << x.sum() << std::endl;
-        }
-
-
-        {
-            x.setVal(0.75);
-            y.setVal(0.25);
-
-            BL_PROFILE_VAR("STANDARD: Function Copy - Array4 (16)", sixteen);
-
-            const int numStreams = 16;
-
-            for (MFIter mfi(x); mfi.isValid(); ++mfi)
-            {
-                Gpu::Device::setStreamIndex(mfi.LocalIndex() % numStreams);
-                const Box bx = mfi.validbox();
-
-                const auto src = x.array(mfi);
-                const auto dst = y.array(mfi);
-
-                const int ncells = bx.numPts();
-                const auto lo  = amrex::lbound(bx);
-                const auto len = amrex::length(bx);
-                const Dim3 offset = {0,0,0};
-
-                const auto ec = Cuda::ExecutionConfig(bx.numPts());
-                AMREX_CUDA_LAUNCH_GLOBAL(ec, copy,
-                                         lo, len, ncells,
-                                         offset, src, dst,
-                                         0, 0, Ncomp);
-            }
-            BL_PROFILE_VAR_STOP(sixteen);
-
-            amrex::Print() << "Standard Function Array sum = " << y.sum() << "; Expected value = " << x.sum() << std::endl;
-        }
-
-        {
-            x.setVal(0.74);
-            y.setVal(0.47);
-
-            BL_PROFILE_VAR("STANDARD: Function Copy - Array4 (1)", one);
-
-            const int numStreams = 1;
-
-            for (MFIter mfi(x); mfi.isValid(); ++mfi)
-            {
-                Gpu::Device::setStreamIndex(mfi.LocalIndex() % numStreams);
-                const Box bx = mfi.validbox();
-
-                const auto src = x.array(mfi);
-                const auto dst = y.array(mfi);
-
-                const int ncells = bx.numPts();
-                const auto lo  = amrex::lbound(bx);
-                const auto len = amrex::length(bx);
-                const Dim3 offset = {0,0,0};
-
-                const auto ec = Cuda::ExecutionConfig(bx.numPts());
-                AMREX_CUDA_LAUNCH_GLOBAL(ec, copy,
-                                         lo, len, ncells,
-                                         offset, src, dst,
-                                         0, 0, Ncomp);
-            }
-            BL_PROFILE_VAR_STOP(one);
-
-
-            amrex::Print() << "Standard Function Array sum = " << y.sum() << "; Expected value = " << x.sum() << std::endl;
-        }
-
-
-        {
-            x.setVal(0.0001);
-            y.setVal(0.0);
-
-            BL_PROFILE_VAR("FUSED: Function Copy - Array4", fusedfull);
+            BL_PROFILE_VAR("FUSED 1", fused);
 
             BL_PROFILE_VAR("FUSED: ALLOC", fusedalloc);
 
             int lsize = x.local_size();
-            Box* bx_h =           static_cast<Box*>(std::malloc(lsize*sizeof(Box)));
-            Dim3* offset_h =      static_cast<Dim3*>(std::malloc(lsize*sizeof(Dim3)));
-            Array4<Real>* src_h = static_cast<Array4<Real>*>(std::malloc(lsize*sizeof(Array4<Real>)));
-            Array4<Real>* dst_h = static_cast<Array4<Real>*>(std::malloc(lsize*sizeof(Array4<Real>)));
+            Box* bx_h =           static_cast<Box*>         (The_Pinned_Arena()->alloc(lsize*sizeof(Box)));
+            Dim3* offset_h =      static_cast<Dim3*>        (The_Pinned_Arena()->alloc(lsize*sizeof(Dim3)));
+            Array4<Real>* src_h = static_cast<Array4<Real>*>(The_Pinned_Arena()->alloc(lsize*sizeof(Array4<Real>)));
+            Array4<Real>* dst_h = static_cast<Array4<Real>*>(The_Pinned_Arena()->alloc(lsize*sizeof(Array4<Real>)));
 
-            Box* bx_d;
-            Dim3* offset_d;
-            Array4<Real>* src_d, *dst_d;
-
-            AMREX_GPU_SAFE_CALL(cudaMalloc(&bx_d,     lsize*sizeof(Box)));
-            AMREX_GPU_SAFE_CALL(cudaMalloc(&offset_d, lsize*sizeof(Dim3)));
-            AMREX_GPU_SAFE_CALL(cudaMalloc(&src_d,    lsize*sizeof(Array4<Real>)));
-            AMREX_GPU_SAFE_CALL(cudaMalloc(&dst_d,    lsize*sizeof(Array4<Real>)));
+            Box* bx_d =           static_cast<Box*>         (The_Device_Arena()->alloc(lsize*sizeof(Box)));
+            Dim3* offset_d =      static_cast<Dim3*>        (The_Device_Arena()->alloc(lsize*sizeof(Dim3)));
+            Array4<Real>* src_d = static_cast<Array4<Real>*>(The_Device_Arena()->alloc(lsize*sizeof(Array4<Real>)));
+            Array4<Real>* dst_d = static_cast<Array4<Real>*>(The_Device_Arena()->alloc(lsize*sizeof(Array4<Real>))); 
 
             BL_PROFILE_VAR_STOP(fusedalloc);
 
@@ -300,23 +284,22 @@ int main (int argc, char* argv[])
                 offset_h[idx] = {0,0,0};
                 src_h[idx] = x.array(mfi);
                 dst_h[idx] = y.array(mfi);
-//                std::memcpy(&src_h[idx], &src, sizeof(Array4<Real>));
-//                std::memcpy(&dst_h[idx], &dst, sizeof(Array4<Real>));
             }
             BL_PROFILE_VAR_STOP(fusedsetup);
             BL_PROFILE_VAR("FUSED: MEMCPY", fusedmem);
 
-            AMREX_GPU_SAFE_CALL(cudaMemcpy(bx_d,     bx_h,     lsize*sizeof(Box),          cudaMemcpyHostToDevice));
-            AMREX_GPU_SAFE_CALL(cudaMemcpy(offset_d, offset_h, lsize*sizeof(Dim3),         cudaMemcpyHostToDevice));
-            AMREX_GPU_SAFE_CALL(cudaMemcpy(src_d,    src_h,    lsize*sizeof(Array4<Real>), cudaMemcpyHostToDevice));
-            AMREX_GPU_SAFE_CALL(cudaMemcpy(dst_d,    dst_h,    lsize*sizeof(Array4<Real>), cudaMemcpyHostToDevice));
+            AMREX_GPU_SAFE_CALL(cudaMemcpyAsync(bx_d,     bx_h,     lsize*sizeof(Box),          cudaMemcpyHostToDevice));
+            AMREX_GPU_SAFE_CALL(cudaMemcpyAsync(offset_d, offset_h, lsize*sizeof(Dim3),         cudaMemcpyHostToDevice));
+            AMREX_GPU_SAFE_CALL(cudaMemcpyAsync(src_d,    src_h,    lsize*sizeof(Array4<Real>), cudaMemcpyHostToDevice));
+            AMREX_GPU_SAFE_CALL(cudaMemcpyAsync(dst_d,    dst_h,    lsize*sizeof(Array4<Real>), cudaMemcpyHostToDevice));
 
             amrex::Gpu::Device::synchronize();
             BL_PROFILE_VAR_STOP(fusedmem);
 
             BL_PROFILE_VAR("FUSED: LAUNCH", fusedl);
 
-            const auto ec = Cuda::ExecutionConfig(bx_h[0].numPts());
+            int cpt = 1;
+            const auto ec = Cuda::ExecutionConfig((bx_h[0].numPts()+cpt-1)/cpt);
 
             AMREX_CUDA_LAUNCH_GLOBAL(ec, copy, lsize, 
                                      bx_d, offset_d,
@@ -326,21 +309,21 @@ int main (int argc, char* argv[])
             amrex::Gpu::Device::synchronize();
             BL_PROFILE_VAR_STOP(fusedl);
 
-            BL_PROFILE_VAR_STOP(fusedfull);
+            BL_PROFILE_VAR_STOP(fused);
 
             amrex::Print() << "Fused Function Array sum = " << y.sum() << "; Expected value = " << x.sum() << std::endl;
 
-            std::free(bx_h);
-            std::free(offset_h);
-            std::free(src_h);
-            std::free(dst_h);
+            The_Pinned_Arena()->free(bx_h);
+            The_Pinned_Arena()->free(offset_h);
+            The_Pinned_Arena()->free(src_h);
+            The_Pinned_Arena()->free(dst_h);
 
-            cudaFree(bx_d);
-            cudaFree(offset_d);
-            cudaFree(src_d);
-            cudaFree(dst_d);
-
+            The_Device_Arena()->free(bx_d);
+            The_Device_Arena()->free(offset_d);
+            The_Device_Arena()->free(src_d);
+            The_Device_Arena()->free(dst_d);
         }
+
     }
 
     amrex::Finalize();
