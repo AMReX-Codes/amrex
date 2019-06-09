9963851ab2ee40df0b891f4834240b2e8d88fb71
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-06-09 10:48:16 -0700

    add a new field int ncomp to Array4.  add new constructor to fabs so that we can make fab from Array4 on device

--- Tutorials/GPU/Launch/main.cpp
+++ Tutorials/GPU/Launch/main.cpp
@@ -238,10 +238,32 @@ void main_main ()
         }
     }
 
-#if defined(AMREX_USE_CUDA_FORTRAN) || !defined(AMREX_USE_GPU)
-    // (11) launch CUDA Fortran kernel to add 1
+    // (11) C++ , amrex::launch function, Capture Array4 and then make a Fab on device
     {
-        BL_PROFILE("11-fortran-func");
+        BL_PROFILE("11-amrex_launch_array4-fab-func");
+        for (MFIter mfi(mf,TilingIfNotGPU()); mfi.isValid(); ++mfi)
+        {
+            // Tiling is off in case of gpu.
+            // In that case, tilebox simply return validbox
+            const Box& bx = mfi.tilebox();
+            // Get Array4 object
+            Array4<Real> const& arr = mf.array(mfi);
+            // Enough threads are launched to work over bx,
+            // and tbx is a thread's work box
+            amrex::launch(bx,
+            [=] AMREX_GPU_DEVICE (Box const& tbx)
+            {
+                // Array4<Real> arr is captured
+                FArrayBox fab(arr);
+                plusone_cudacpp(tbx, fab);
+            });
+        }
+    }
+
+#if defined(AMREX_USE_CUDA_FORTRAN) || !defined(AMREX_USE_GPU)
+    // (12) launch CUDA Fortran kernel to add 1
+    {
+        BL_PROFILE("12-fortran-func");
         for (MFIter mfi(mf,TilingIfNotGPU()); mfi.isValid(); ++mfi)
         {
             const Box& bx = mfi.tilebox();
@@ -255,9 +277,9 @@ void main_main ()
         }
     }
 
-    // (12) launch CUDA Fortran kernel to add 1
+    // (13) launch CUDA Fortran kernel to add 1
     {
-        BL_PROFILE("12-fortran-macro");
+        BL_PROFILE("13-fortran-macro");
         for (MFIter mfi(mf,TilingIfNotGPU()); mfi.isValid(); ++mfi)
         {
             const Box& bx = mfi.tilebox();
@@ -272,9 +294,9 @@ void main_main ()
 #endif
 
 #ifdef AMREX_USE_ACC
-    // (13) launch OpenACC kernel to add 1 if supported
+    // (14) launch OpenACC kernel to add 1 if supported
     {
-        BL_PROFILE("13-acc");
+        BL_PROFILE("14-acc");
         for (MFIter mfi(mf,TilingIfNotGPU()); mfi.isValid(); ++mfi)
         {
             const Box& bx = mfi.tilebox();
@@ -288,9 +310,9 @@ void main_main ()
 #endif
 
 #ifdef AMREX_OMP_OFFLOAD
-    // (14) launch OpenOMP kernel to add 1 if supported
+    // (15) launch OpenOMP kernel to add 1 if supported
     {
-        BL_PROFILE("14-omp");
+        BL_PROFILE("15-omp");
         for (MFIter mfi(mf,TilingIfNotGPU()); mfi.isValid(); ++mfi)
         {
             const Box& bx = mfi.tilebox();
