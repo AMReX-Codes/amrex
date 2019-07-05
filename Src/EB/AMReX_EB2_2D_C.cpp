02702ff93c93d39232e6d0a5800faf17a0c26ece
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-07-05 06:28:53 -0700

    WIP: EB2::build_faces on gpu

--- Src/EB/AMReX_EB2_2D_C.cpp
+++ Src/EB/AMReX_EB2_2D_C.cpp
@@ -1,13 +1,6 @@
-#ifndef AMREX_EB2_C_H_
-#define AMREX_EB2_C_H_
+#include <AMReX_EB2_C.H>
 
-#include <AMReX_FArrayBox.H>
-#include <AMReX_EBCellFlag.H>
-#include <AMReX_EB2_Graph.H>
-
-#if (AMREX_SPACEDIM == 2)
-
-#include <AMReX_EB2_2D_C.H>
+namespace amrex { namespace EB2 {
 
 void build_faces (Box const& bx, Array4<EBCellFlag> const& cell,
                   Array4<Type_t> const& fx, Array4<Type_t> const& fy,
@@ -16,12 +9,8 @@ void build_faces (Box const& bx, Array4<EBCellFlag> const& cell,
                   Array4<Real> const& apx, Array4<Real> const& apy,
                   Array4<Real> const& fcx, Array4<Real> const& fcy,
                   GpuArray<Real,AMREX_SPACEDIM> const& dx,
-                  GpuArray<Real,AMREX_SPACEDIM> const& problo);
+                  GpuArray<Real,AMREX_SPACEDIM> const& problo)
+{
+}
 
-#elif (AMREX_SPACEDIM == 3)
-
-#include <AMReX_EB2_3D_C.H>
-
-#endif
-
-#endif
+}}
---
+++ Src/EB/AMReX_EB2_3D_C.cpp
@@ -0,0 +1,17 @@
+#include <AMReX_EB2_C.H>
+
+namespace amrex { namespace EB2 {
+
+//void build_faces (Box const& bx, Array4<EBCellFlag> const& cell,
+//                  Array4<Type_t> const& fx, Array4<Type_t> const& fy,
+//                  Array4<Real const> const& levset,
+//                  Array4<Real const> const& interx, Array4<Real const> const& intery,
+//                  Array4<Real> const& apx, Array4<Real> const& apy,
+//                  Array4<Real> const& fcx, Array4<Real> const& fcy,
+//                  GpuArray<Real,AMREX_SPACEDIM> const& dx,
+//                  GpuArray<Real,AMREX_SPACEDIM> const& problo)
+//{
+//}
+//
+
+}}
