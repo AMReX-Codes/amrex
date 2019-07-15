2329c6d9135e2d5a0140287be1e9c0f862920c16
Johannes Blaschke <jpblaschke@lbl.gov>
2019-07-14 22:34:10 -0700

    fix off-by-epsilon error when filling level-set

--- Src/EB/AMReX_EB_geometry.F90
+++ Src/EB/AMReX_EB_geometry.F90
@@ -312,6 +312,9 @@ contains
             facet_normal = (/ 0., 0., 0. /)
             facet_normal(tmp_facet) = 1.  ! whether facing inwards or outwards is not important here
 
+            ! thenically this should never be called, leaving here for reference
+            !if (all(abs(eb_normal) == abs(facet_normal))) cycle
+
             ind_cell = ind_loop(tmp_facet)
             ind_nb = ind_pt(tmp_facet)
 
