73ab017b410e628587fe6d89b3d74e3e58bc9624
Rupert Nash <r.nash@epcc.ed.ac.uk>
2019-07-19 13:17:57 +0100

    make header modification use cmake to be more cross-platform

--- Tools/CMake/modify_installed_headers.cmake
+++ Tools/CMake/modify_installed_headers.cmake
@@ -1,4 +1,4 @@
-#
+r#
 # Add line #include<AMReX_Config.H> to all installed headers
 # 
 message(STATUS "Modifying installed headers")
@@ -6,7 +6,7 @@ file(GLOB _installed_headers "${CMAKE_INSTALL_PREFIX}/include/*.H")
 list(REMOVE_ITEM _installed_headers "${CMAKE_INSTALL_PREFIX}/include/AMReX_Config.H")
 
 foreach( _header IN LISTS _installed_headers)
-   # This command only affect the first line of each file. Therefore, it can be used countless
-   # times on the same file and it always gives the same result (i.e. it does not keep prepending)
-   execute_process(COMMAND bash "-c" "sed -i '1s/^/#include <AMReX_Config.H>\\n\\n/' ${_header}" )
+   file(READ ${_header} _contents)
+   string(PREPEND _contents "#include <AMReX_Config.H>\n")
+   file(WRITE ${_header} "${_contents}")
 endforeach()
