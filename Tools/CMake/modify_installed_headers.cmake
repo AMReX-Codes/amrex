6614b20ab8eb39a9c031658ad3c2a25b9d9e0fdb
Rupert Nash <r.nash@epcc.ed.ac.uk>
2019-07-19 14:26:19 +0100

    insert include AMReX_Config.H within include guard

--- Tools/CMake/modify_installed_headers.cmake
+++ Tools/CMake/modify_installed_headers.cmake
@@ -1,4 +1,4 @@
-r#
+#
 # Add line #include<AMReX_Config.H> to all installed headers
 # 
 message(STATUS "Modifying installed headers")
@@ -6,7 +6,27 @@ file(GLOB _installed_headers "${CMAKE_INSTALL_PREFIX}/include/*.H")
 list(REMOVE_ITEM _installed_headers "${CMAKE_INSTALL_PREFIX}/include/AMReX_Config.H")
 
 foreach( _header IN LISTS _installed_headers)
-   file(READ ${_header} _contents)
-   string(PREPEND _contents "#include <AMReX_Config.H>\n")
-   file(WRITE ${_header} "${_contents}")
+  file(READ ${_header} _contents)
+  # Regex looks for:
+  # - Anything before include guard
+  # - #ifndef CPP_IDENTIFIER
+  # - #define CPP_IDENTIFIER
+  # - Rest of file
+  if (_contents MATCHES "^(.*)#ifndef +([A-Za-z0-9_]+)\n#define +([A-Za-z0-9_]+) *\n(.+)")
+    # We have found an include guard
+    if (${CMAKE_MATCH_2} STREQUAL ${CMAKE_MATCH_3})
+      # The guard defines match and are hence OK
+    else()
+      message(FATAL_ERROR "Include guard is wrong up for header file ${_header}")
+    endif()
+
+    # Insert the #include <AMReX_Config.H> between include guard and the rest of the file
+    string(CONCAT "_contents" "${CMAKE_MATCH_1}" "#ifndef " "${CMAKE_MATCH_2}" "\n#define " "${CMAKE_MATCH_3}" "\n#include <AMReX_Config.H>\n" "${CMAKE_MATCH_4}")
+
+  else()
+    message(WARNING "No include guard in header ${_header}")
+    string(PREPEND _contents "#include <AMReX_Config.H>\n")
+  endif()
+  # Rewrite
+  file(WRITE ${_header} "${_contents}")
 endforeach()
