4b857626a1706e60fdca133b8e6693276c6de288
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2019-07-24 15:40:17 -0700

    Deleted accidental pushes

--- Tests/GPU/RandomNumberGeneration/launc_gpu.sh
+++ Tests/GPU/RandomNumberGeneration/launc_gpu.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#SBATCH -J Random
+#SBATCH -J Random2
 #SBATCH -A m1759
 #SBATCH -N 1
-#SBATCH -C gpu
+#SBATCH -n 1
+#SBATCH --ntasks-per-node 1
+#SBATCH -c 1
 #SBATCH --gres=gpu:1
-#SBATCH -c 10
-#SBATCH --mem=30GB
-#SBATCH -p debug
-#SBATCH -t 00:05:00
 
-cuda-memcheck ./main3d.pgi.CUDA.ex
+
+
+#SBATCH -C gpu
+#SBATCH -p debug
+#SBATCH -t 00:10:00
+
+nvprof -o profile.nvvp --analysis-metrics ./main3d.pgi.MPI.CUDA.ex
--- Tests/GPU/RandomNumberGeneration/main.cpp
+++ Tests/GPU/RandomNumberGeneration/main.cpp
@@ -4,7 +4,11 @@
 #include <AMReX_Utility.H>
 #include <AMReX_Array.H>
 #include <AMReX_CudaContainers.H>
-
+#include <AMReX_ParallelDescriptor.H>
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
 using namespace amrex;
 
 
@@ -14,15 +18,29 @@ int main (int argc, char* argv[])
 {
 
     amrex::Initialize(argc,argv);
-    RandomNumGen();
+    {
+	using namespace std::chrono;
+
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+	RandomNumGen();
+
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+	std::cout << "It took me " << time_span.count() << " seconds.";
+	std::cout << std::endl;
+     
+    }
     amrex::Finalize();
 
 }
 
 void RandomNumGen ()
 {
-    int N = 1E5;
-    int num_rngs = N/20;
+    int N = 1E5/ParallelDescriptor::NProcs();
+    int num_rngs = N;
     
 #ifdef AMREX_USE_CUDA    
     amrex::InitRandSeedOnDevice(num_rngs);
@@ -35,30 +53,49 @@ void RandomNumGen ()
 #else
     amrex::InitRandom(1024UL,1);
 #endif
- 
+
     amrex::Vector<double> hx(N);
     double *hxpos = hx.dataPtr();    
-        
+
+
+
+#ifdef AMREX_USE_CUDA
+    if ( d_xpos.size() < N){
+      CheckSeedArraySizeAndResize(N);
+      d_xpos.resize(N);
+
+      dxpos = d_xpos.dataPtr();
+
+    }
+#endif 
+    if ( hx.size() < N){
+      hx.resize(N);
+
+      hxpos = hx.dataPtr();
+
+    }
+
+              
         AMREX_PARALLEL_FOR_1D (N, idx,
         {
 #ifdef AMREX_USE_CUDA    
-           dxpos[idx] = amrex::gpusafe_Random();
+	  dxpos[idx] = amrex::gpusafe_Random();
 #else
-           hx[idx] = amrex::gpusafe_Random();
+           hx[idx] = amrex::Random();
 #endif
         });
-   
+    
 #ifdef AMREX_USE_CUDA    
         cudaMemcpy(hxpos,dxpos,sizeof(double)*N,cudaMemcpyDeviceToHost);
 #endif
 
-        for (int i = 0; i < N; i++ )
-	  {
-	    amrex::Print() << i << " " << hx[i]  << "\n";
-        }
-     	
-	hx.resize(0);
-	hx.shrink_to_fit();
+
+	amrex::Print() << "size of hx = " << hx.size() << std::endl;
+
+	for (int w; w <= hx.size(); w++)
+	 {
+	   	    amrex::Print() << "hx[" << w << "] = " << hx[w] << std::endl;
+	 }
 }
 
 
