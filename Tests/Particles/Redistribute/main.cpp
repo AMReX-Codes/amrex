8ece044e46899d04b44182ee3f80dda23748cc83
Andrew Myers <atmyers@lbl.gov>
2019-07-26 17:57:56 -0400

    checking particle redistribute test

---
+++ Tests/Particles/Redistribute/main.cpp
@@ -0,0 +1,203 @@
+#include <AMReX.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_Particles.H>
+
+using namespace amrex;
+
+static constexpr int NSR = 2;
+static constexpr int NSI = 1;
+static constexpr int NAR = 0;
+static constexpr int NAI = 0;
+
+void get_position_unit_cell(Real* r, const IntVect& nppc, int i_part)
+{
+    int nx = nppc[0];
+    int ny = nppc[1];
+    int nz = nppc[2];
+    
+    int ix_part = i_part/(ny * nz);
+    int iy_part = (i_part % (ny * nz)) % ny;
+    int iz_part = (i_part % (ny * nz)) / ny;
+    
+    r[0] = (0.5+ix_part)/nx;
+    r[1] = (0.5+iy_part)/ny;
+    r[2] = (0.5+iz_part)/nz;
+}
+
+class TestParticleContainer
+    : public amrex::ParticleContainer<NSR, NSI, NAR, NAI>
+{
+
+public:
+
+    TestParticleContainer (const amrex::Geometry            & a_geom,
+                           const amrex::DistributionMapping & a_dmap,
+                           const amrex::BoxArray            & a_ba)
+        : amrex::ParticleContainer<NSR, NSI, NAR, NAI>(a_geom, a_dmap, a_ba)
+    {}
+
+    void RedistributeLocal ()
+    {
+        const int lev_min = 0;
+        const int lev_max = 0;
+        const int nGrow = 0;
+        const int local = 1;
+        RedistributeOptimized(lev_min, lev_max, nGrow, local);
+    }
+
+    void InitParticles (const amrex::IntVect& a_num_particles_per_cell)
+    {
+        const int lev = 0;   
+        const Real* dx = Geom(lev).CellSize();
+        const Real* plo = Geom(lev).ProbLo();
+    
+        const int num_ppc = AMREX_D_TERM( a_num_particles_per_cell[0],
+                                         *a_num_particles_per_cell[1],
+                                         *a_num_particles_per_cell[2]);
+
+        for(MFIter mfi = MakeMFIter(lev); mfi.isValid(); ++mfi)
+        {
+            const Box& tile_box  = mfi.tilebox();
+
+            Cuda::HostVector<ParticleType> host_particles;
+        
+            for (IntVect iv = tile_box.smallEnd(); iv <= tile_box.bigEnd(); tile_box.next(iv))
+            {
+                for (int i_part=0; i_part<num_ppc;i_part++) {
+                    Real r[3];
+                    get_position_unit_cell(r, a_num_particles_per_cell, i_part);
+                
+                    Real x = plo[0] + (iv[0] + r[0])*dx[0];
+                    Real y = plo[1] + (iv[1] + r[1])*dx[1];
+                    Real z = plo[2] + (iv[2] + r[2])*dx[2];
+                
+                    ParticleType p;
+                    p.id()  = ParticleType::NextID();
+                    p.cpu() = ParallelDescriptor::MyProc();                
+                    p.pos(0) = x;
+                    p.pos(1) = y;
+                    p.pos(2) = z;
+                    
+                    host_particles.push_back(p);
+                }
+            }
+        
+            auto& particles = GetParticles(lev);
+            auto& particle_tile = particles[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
+            auto old_size = particle_tile.GetArrayOfStructs().size();
+            auto new_size = old_size + host_particles.size();
+            particle_tile.resize(new_size);
+            
+            Cuda::thrust_copy(host_particles.begin(),
+                              host_particles.end(),
+                              particle_tile.GetArrayOfStructs().begin() + old_size);        
+        }
+    }
+
+    void moveParticles ()
+    {
+        BL_PROFILE("TestParticleContainer::moveParticles");
+
+        const int lev = 0;
+        const Geometry& geom = Geom(lev);
+        const auto dx = Geom(lev).CellSizeArray();
+        auto& plev  = GetParticles(lev);
+        
+        for(MFIter mfi = MakeMFIter(lev); mfi.isValid(); ++mfi)
+        {
+            int gid = mfi.index();
+            int tid = mfi.LocalTileIndex();
+            
+            auto& ptile = plev[std::make_pair(gid, tid)];
+            auto& aos   = ptile.GetArrayOfStructs();
+            ParticleType* pstruct = &(aos[0]);
+            
+            const size_t np = aos.numParticles();
+            
+            // now we move the particles
+            AMREX_FOR_1D ( np, i,
+            {
+                ParticleType& p = pstruct[i];
+                p.pos(0) += dx[0];
+                p.pos(1) += dx[1];
+                p.pos(2) += dx[2];
+            });
+        }
+    }
+};
+
+struct TestParams
+{
+    IntVect size;
+    int max_grid_size;
+    int num_ppc;
+    int is_periodic;
+};
+
+void testRedistribute();
+
+int main (int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+
+    amrex::Print() << "Running redistribute test \n";
+    testRedistribute();
+
+    amrex::Finalize();
+}
+
+void get_test_params(TestParams& params, const std::string& prefix)
+{
+    ParmParse pp(prefix);
+    pp.get("size", params.size);
+    pp.get("max_grid_size", params.max_grid_size);
+    pp.get("num_ppc", params.num_ppc);
+    pp.get("is_periodic", params.is_periodic);
+}
+
+void testRedistribute ()
+{
+    BL_PROFILE("testRedistribute");
+    TestParams params;
+    get_test_params(params, "redistribute");
+
+    RealBox real_box;
+    for (int n = 0; n < BL_SPACEDIM; n++)
+    {
+        real_box.setLo(n, 0.0);
+        real_box.setHi(n, params.size[n]);
+    }
+
+    IntVect domain_lo(AMREX_D_DECL(0, 0, 0));
+    IntVect domain_hi(AMREX_D_DECL(params.size[0]-1,params.size[1]-1,params.size[2]-1));
+    const Box domain(domain_lo, domain_hi);
+
+    int coord = 0;
+    int is_per[BL_SPACEDIM];
+    for (int i = 0; i < BL_SPACEDIM; i++)
+        is_per[i] = params.is_periodic;
+    Geometry geom(domain, &real_box, coord, is_per);
+    
+    BoxArray ba(domain);
+    ba.maxSize(params.max_grid_size);
+    DistributionMapping dm(ba);
+
+    TestParticleContainer pc(geom, dm, ba);
+
+    int npc = params.num_ppc;
+    IntVect nppc = IntVect(AMREX_D_DECL(npc, npc, npc));
+
+    if (ParallelDescriptor::MyProc() == dm[0])
+        amrex::Print() << "About to initialize particles \n";
+
+    pc.InitParticles(nppc);
+
+    const int nsteps = 10;
+    for (int i = 0; i < nsteps; ++i)
+    {
+        pc.moveParticles();
+        pc.RedistributeLocal();
+        AMREX_ALWAYS_ASSERT(pc.OK());
+    }
+}
