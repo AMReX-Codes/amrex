209fcf219017a8852aac9806240fbadc5fbabafe
MaxThevenet <mthevenet@lbl.gov>
2019-07-31 11:37:54 -0700

    comment GPUParser and custom density/momentum profiles

--- Source/Parser/GpuParser.cpp
+++ Source/Parser/GpuParser.cpp
@@ -5,6 +5,8 @@ GpuParser::GpuParser (WarpXParser const& wp)
 #ifdef AMREX_USE_GPU
 
     struct wp_parser* a_wp = wp.m_parser;
+    // Initialize GPU parser: allocate memory in CUDA managed memory,
+    // copy all data needed on GPU to m_gpu_parser
     m_gpu_parser.sz_mempool = wp_ast_size((struct wp_node*)a_wp);
     m_gpu_parser.p_root = (struct wp_node*)
         amrex::The_Managed_Arena()->alloc(m_gpu_parser.sz_mempool);
@@ -15,6 +17,8 @@ GpuParser::GpuParser (WarpXParser const& wp)
     wp_parser_regvar_gpu(&m_gpu_parser, "y", 1);
     wp_parser_regvar_gpu(&m_gpu_parser, "z", 2);
 
+    // Initialize CPU parser: allocate memory in CUDA managed memory,
+    // copy all data needed on CPU to m_cpu_parser
     m_cpu_parser.sz_mempool = wp_ast_size((struct wp_node*)a_wp);
     m_cpu_parser.p_root = (struct wp_node*)
         amrex::The_Managed_Arena()->alloc(m_cpu_parser.sz_mempool);
@@ -25,13 +29,13 @@ GpuParser::GpuParser (WarpXParser const& wp)
     wp_parser_regvar(&m_cpu_parser, "y", &(m_var.y));
     wp_parser_regvar(&m_cpu_parser, "z", &(m_var.z));
     
-#else
+#else // not defined AMREX_USE_GPU
 
 #ifdef _OPENMP
     nthreads = omp_get_max_threads();
-#else
+#else // _OPENMP
     nthreads = 1;
-#endif
+#endif // _OPENMP
 
     m_parser = ::new struct wp_parser*[nthreads];
     m_var = ::new amrex::XDim3[nthreads];
@@ -40,15 +44,15 @@ GpuParser::GpuParser (WarpXParser const& wp)
     {
 #ifdef _OPENMP
         m_parser[tid] = wp_parser_dup(wp.m_parser[tid]);
-#else
+#else // _OPENMP
         m_parser[tid] = wp_parser_dup(wp.m_parser);
-#endif
+#endif // _OPENMP
         wp_parser_regvar(m_parser[tid], "x", &(m_var[tid].x));
         wp_parser_regvar(m_parser[tid], "y", &(m_var[tid].y));
         wp_parser_regvar(m_parser[tid], "z", &(m_var[tid].z));
     }
 
-#endif
+#endif // AMREX_USE_GPU
 }
 
 void
