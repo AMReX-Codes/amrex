ab4a72d15242cd2a03accc7b532d1b297c3f82f5
Ryan Houim <rhouim@mics.mae.ufl.edu>
2019-08-12 15:52:36 -0400

    This commit fixes a strange issue with AMReX cell bilinear interp.
    We setup a 1D simulation (planar flame) on a 2D grid.  After some time steps there would be significant transverse velocities (which should be zero) that affected the flame.  This only occured with the Intel compiler with DEBUG=False and with the cell_bilinear interpolation.  Compiling with GNU orIntel with DEBUG=True did not produce this behavior.  Rearranging the interpolation procedure to the method typically done by students when interpolating tables by hand seems to work fine where the data is first interpolated long x-lines, then the x-line interpolated data is interpolated in the y-direction, and finally the y-interpolated data is interpolated in the z-direction to give the final value.

--- Src/AmrCore/AMReX_INTERP_2D.F90
+++ Src/AmrCore/AMReX_INTERP_2D.F90
@@ -57,38 +57,53 @@ contains
       real(amrex_real)  sl(cb_l1:cb_h1,num_slp)
       real(amrex_real)  strip(strip_lo:strip_hi)
 
+
 #define SLX 1
 #define SLY 2
 #define SLXY 3
 
       integer lx, ly, hratx, hraty, ic, jc, jfn, jfc, i, n
-      real(amrex_real) x, y, denomx, denomy
+      real(amrex_real) x, y, denomx, denomy, su, xt, xb, yt, yb
 
-      denomx = one/dble(2*lratiox)
-      denomy = one/dble(2*lratioy)
+      denomx = 1._amrex_real/real(2*lratiox,amrex_real)
+      denomy = 1._amrex_real/real(2*lratioy,amrex_real)
+
+    !  denomx = 1./dble(2*lratiox)
+    !  denomy = 1./dble(2*lratioy)
 
       hratx = lratiox/2
       hraty = lratioy/2
 
-      do n = 1, nvar 
+      do n = 1, nvar
          do jc = cb_l2, cb_h2-1 
+
             do ic = cb_l1, cb_h1-1
                sl(ic,SLX) = crse(ic+1,jc,n)-crse(ic,jc,n)
-               sl(ic,SLY) = crse(ic,jc+1,n)-crse(ic,jc,n)
-               sl(ic,SLXY) = crse(ic+1,jc+1,n)-crse(ic+1,jc,n) &
-                    - crse(ic  ,jc+1,n)+crse(ic  ,jc,n)
+               sl(ic,SLY) = crse(ic,jc+1,n)-crse(ic,jc,n) 
+               sl(ic,SLXY) = (crse(ic+1,jc+1,n)-crse(ic+1,jc,n)) + (- crse(ic  ,jc+1,n)+crse(ic  ,jc,n))
             end do
+
             do ly = 0, lratioy-1 
                jfn = jc*lratioy + ly
                jfc = jfn + hraty
                if (jfc .ge. fb_l2  .and.  jfc .le. fb_h2) then
-                  y = denomy*(two*ly + one)
+                  y = denomy*(2._amrex_real*real(ly,amrex_real) + 1._amrex_real)
                   do lx = 0, lratiox-1
                      do ic = cb_l1, cb_h1-1
                         i = ic*lratiox + lx
-                        x = denomx*(two*lx + one)
+                        x = denomx*(2._amrex_real*real(lx,amrex_real) + 1._amrex_real)
+                        xB = crse(ic,jc,n) + x*(crse(ic+1,jc,n)-crse(ic,jc,n))
+                        xT = crse(ic,jc+1,n) + x*(crse(ic+1,jc+1,n)-crse(ic,jc+1,n))
+                        strip(i) = XB + y*(XT - XB)
+
                         strip(i) = crse(ic,jc,n) + x*sl(ic,SLX) + &
                                    y*sl(ic,SLY) + x*y*sl(ic,SLXY)
+
+                       !  yB = crse(ic,jc,n) + y*(crse(ic,jc+1,n)-crse(ic,jc,n))
+                       !  yT = crse(ic+1,jc,n) + y*(crse(ic+1,jc+1,n)-crse(ic+1,jc,n))
+                       !  strip(i) = YB + x*(YT - YB)
+                         !strip(i) = crse(ic,jc,n) + x*(crse(ic+1,jc,n)-crse(ic,jc,n)) + y*(crse(ic,jc+1,n)-crse(ic,jc,n)) + &
+                         !           x*y*((crse(ic+1,jc+1,n) - crse(ic,jc+1,n)) - (crse(ic+1,jc,n) - crse(ic,jc,n)))
                      end do
                   end do
                   do i = fb_l1, fb_h1 
