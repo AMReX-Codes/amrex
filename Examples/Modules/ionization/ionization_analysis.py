8469c5419922cbdbcec31fbf1d80fdb0d88674a1
MaxThevenet <mthevenet@lbl.gov>
2019-08-12 11:17:59 -0700

    add plotting capability in ionization rt analysis script

--- Examples/Modules/ionization/ionization_analysis.py
+++ Examples/Modules/ionization/ionization_analysis.py
@@ -30,4 +30,48 @@ print("Number of ions: " + str(ilev.size))
 print("Number of N5+ : " + str(ilev[ilev == 5].size))
 print("N5_fraction   : " + str(N5_fraction))
 
+do_plot = False
+if do_plot:
+    import matplotlib.pyplot as plt
+    all_data_level_0 = ds.covering_grid(level=0,left_edge=ds.domain_left_edge,
+                                        dims=ds.domain_dimensions)
+    F = all_data_level_0['boxlib', 'Ex'].v.squeeze()
+    extent = [ ds.domain_left_edge[1], ds.domain_right_edge[1],
+               ds.domain_left_edge[0], ds.domain_right_edge[0] ]
+    ad = ds.all_data()
+
+    # Plot ions with ionization levels
+    species = 'ions';
+    xi = ad[species, 'particle_position_x'].v
+    zi = ad[species, 'particle_position_y'].v
+    ii = ad[species, 'particle_ionization_level'].v
+    plt.figure(figsize=(10,10))
+    plt.subplot(211)
+    plt.imshow(np.abs(F), extent=extent, aspect='auto',
+               cmap='magma', origin='default')
+    plt.colorbar()
+    for lev in range(int(np.max(ii)+1)):
+        select = (ii == lev)
+        plt.scatter(zi[select],xi[select],s=.2,
+                    label='ionization level: ' + str(lev))
+    plt.legend()
+    plt.title("abs(Ex) (V/m) and ions")
+    plt.xlabel("z (m)")
+    plt.ylabel("x (m)")
+    plt.subplot(212)
+    plt.imshow(np.abs(F), extent=extent, aspect='auto',
+               cmap='magma', origin='default')
+    plt.colorbar()
+
+    # Plot electrons
+    species = 'electrons';
+    if species in [x[0] for x in ds.field_list]:
+        xe = ad[species, 'particle_position_x'].v
+        ze = ad[species, 'particle_position_y'].v
+        plt.scatter(ze,xe,s=.1,c='r',label='electrons')
+    plt.title("abs(Ex) (V/m) and electrons")
+    plt.xlabel("z (m)")
+    plt.ylabel("x (m)")
+    plt.savefig("image_ionization.pdf", bbox_inches='tight')
+
 assert ((N5_fraction > 0.30) and (N5_fraction < 0.34))
