34391250c1e65465e1868a2b13cb530b811754b5
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-08-14 13:26:59 -0700

    forgot a new file

---
+++ Src/Boundary/AMReX_LO_BCTYPES.cpp
@@ -0,0 +1,58 @@
+#include <AMReX_LO_BCTYPES.H>
+#include <AMReX.H>
+#include <iostream>
+
+namespace amrex {
+
+std::ostream& operator<< (std::ostream& os, const LinOpBCType& t)
+{
+    switch (t) {
+        case LinOpBCType::interior:
+        {
+            os << "interior";
+            break;
+        }
+        case LinOpBCType::Dirichlet:
+        {
+            os << "Dirichlet";
+            break;
+        }
+        case LinOpBCType::Neumann:
+        {
+            os << "Neumann";
+            break;
+        }
+        case LinOpBCType::reflect_odd:
+        {
+            os << "reflect_odd";
+            break;
+        }
+        case LinOpBCType::Marshak:
+        {
+            os << "Marshak";
+            break;
+        }
+        case LinOpBCType::SanchezPomraning:
+        {
+            os << "SanchezPomraning";
+            break;
+        }
+        case LinOpBCType::inflow:
+        {
+            os << "inflow";
+            break;
+        }
+        case LinOpBCType::Periodic:
+        {
+            os << "Periodic";
+            break;
+        }
+        default:
+        {
+            os << "bogus";
+        }
+    };
+    return os;
+}
+
+}
