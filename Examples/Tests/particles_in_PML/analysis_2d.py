5a87d52099943fee974a4a83044f9b648fb3a799
Remi Lehe <remi.lehe@normalesup.org>
2019-08-21 14:42:44 -0700

    Update test

--- Examples/Tests/particles_in_PML/analysis_2d.py
+++ Examples/Tests/particles_in_PML/analysis_2d.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 """
 This script tests the absorption of particles in the PML.
 
@@ -23,4 +24,5 @@ Ex_array = ad0['Ex'].to_ndarray()
 Ey_array = ad0['Ey'].to_ndarray()
 Ez_array = ad0['Ez'].to_ndarray()
 max_Efield = max(Ex_array.max(), Ey_array.max(), Ez_array.max())
+print( max_Efield )
 assert max_Efield < 0.0003
