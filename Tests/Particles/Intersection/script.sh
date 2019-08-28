b56b964b7e1b98b31d667e5a9ae0168604d6eda6
Andrew Myers <atmyers@lbl.gov>
2019-08-28 17:29:47 -0400

    test for the GPU boxarray intersection class

---
+++ Tests/Particles/Intersection/script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#BSUB -P CSC308
+#BSUB -W 0:02
+#BSUB -nnodes 1
+#BSUB -J IntersectionTest
+#BSUB -o IntersectionTesto.%J
+#BSUB -e IntersectionTeste.%J
+
+module load gcc
+module load cuda
+module list
+set -x
+
+omp=1
+export OMP_NUM_THREADS=${omp}
+EXE="../main3d.gnu.TPROF.MPI.CUDA.ex"
+JSRUN="jsrun -n 1 -a 1 -g 1 -c 1 --bind=packed:${omp} "
+
+rundir="${LSB_JOBNAME}-${LSB_JOBID}"
+mkdir $rundir
+cp $0 $rundir
+cp inputs $rundir
+cd $rundir
+
+# 1. Run normally
+${JSRUN} --smpiargs="-gpu" ${EXE} inputs
+
+# 2. Run under nvprof and direct all stdout and stderr to nvprof.txt
+#${JSRUN} --smpiargs="-gpu" nvprof --profile-child-processes ${EXE} inputs &> nvprof.txt
+
+# 3. Run under nvprof and store performance data in a nvvp file
+# Can be converted to text using nvprof -i nvprof-timeline-%p.nvvp
+#${JSRUN} --smpiargs="-gpu" nvprof --profile-child-processes --openmp-profiling off  -o nvprof-timeline-%p.nvvp ${EXE} inputs
+
+# COLLECT PERFORMANCE METRICS - THIS IS MUCH SLOWER. Set nsteps=2 in the inputs files
+# 4. Run under nvprof and collect metrics for a subset of kernels
+#${JSRUN} --smpiargs="-gpu" nvprof --profile-child-processes --kernels '(deposit_current|gather_\w+_field|push_\w+_boris)' --analysis-metrics -o nvprof-metrics-kernel-%p.nvvp ${EXE} inputs
+
+# 5. Run under nvprof and collect metrics for all kernels -- much slower!
+#${JSRUN} --smpiargs="-gpu" nvprof --profile-child-processes --analysis-metrics -o nvprof-metrics-%p.nvvp ${EXE} inputs
+
+cp ../IntersectionTest*.${LSB_JOBID} .
