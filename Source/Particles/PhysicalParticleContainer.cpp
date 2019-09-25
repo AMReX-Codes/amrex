eee301aa9c25cc9efa859728df20a9e5bf47ae95
Remi Lehe <remi.lehe@normalesup.org>
2019-09-25 10:29:18 -0700

    Use explicit floating point operation

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -1324,9 +1324,9 @@ PhysicalParticleContainer::SplitParticles(int lev)
         // before splitting results in a uniform distribution after splitting
         const amrex::Vector<int> ppc_nd = plasma_injector->num_particles_per_cell_each_dim;
         const std::array<Real,3>& dx = WarpX::CellSize(lev);
-        amrex::Vector<amrex::Real> split_offset = {dx[0]/2/ppc_nd[0],
-                                                   dx[1]/2/ppc_nd[1],
-                                                   dx[2]/2/ppc_nd[2]};
+        amrex::Vector<amrex::Real> split_offset = {dx[0]/2./ppc_nd[0],
+                                                   dx[1]/2./ppc_nd[1],
+                                                   dx[2]/2./ppc_nd[2]};
 
         // particle Array Of Structs data
         auto& particles = pti.GetArrayOfStructs();
