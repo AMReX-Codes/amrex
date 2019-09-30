1fa9cb09e6a681d37c9e3ff8b22df98b9bc21ddf
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-09-29 21:37:49 -0700

    gpu nodal: jacobi stencil version

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLap_2d.F90
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLap_2d.F90
@@ -48,7 +48,7 @@ module amrex_mlnodelap_2d_module
        amrex_mlndlap_zero_fine
 
   ! RAP
-  public:: amrex_mlndlap_jacobi_sten, &
+  public:: &
        amrex_mlndlap_interpolation_rap, &
        amrex_mlndlap_restriction_rap, &
        amrex_mlndlap_stencil_rap
@@ -538,33 +538,6 @@ contains
        end do
     end do
   end subroutine amrex_mlndlap_zero_fine
-
-
-  subroutine amrex_mlndlap_jacobi_sten (lo, hi, sol, slo, shi, Ax, alo, ahi, &
-       rhs, rlo, rhi, sten, stlo, sthi, msk, mlo, mhi) &
-       bind(c,name='amrex_mlndlap_jacobi_sten')
-    integer, dimension(2),intent(in) :: lo,hi,slo,shi,alo,ahi,rlo,rhi,stlo,sthi,mlo,mhi
-    real(amrex_real), intent(inout) :: sol( slo(1): shi(1), slo(2): shi(2))
-    real(amrex_real), intent(in   ) :: Ax ( alo(1): ahi(1), alo(2): ahi(2))
-    real(amrex_real), intent(in   ) :: rhs( rlo(1): rhi(1), rlo(2): rhi(2))
-    real(amrex_real), intent(in   ) ::sten(stlo(1):sthi(1),stlo(2):sthi(2),5)
-    integer, intent(in) :: msk(mlo(1):mhi(1),mlo(2):mhi(2))
-
-    integer :: i,j
-    real(amrex_real), parameter :: omega = 2.d0/3.d0
-
-    do    j = lo(2), hi(2)
-       do i = lo(1), hi(1)
-          if (msk(i,j) .ne. dirichlet) then
-             if (sten(i,j,1) .ne. 0.d0) then
-                sol(i,j) = sol(i,j) + omega * (rhs(i,j) - Ax(i,j)) / sten(i,j,1)
-             end if
-          else
-             sol(i,j) = 0.d0
-          end if
-       end do
-    end do
-  end subroutine amrex_mlndlap_jacobi_sten
 
 
   subroutine amrex_mlndlap_interpolation_rap (clo, chi, fine, fflo, ffhi, crse, cflo, cfhi, &
