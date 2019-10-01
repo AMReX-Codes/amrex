34010107597c0dbded205cd31863f7c8245b36ee
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-09-30 20:07:45 -0700

    fix dangling pointer

--- Src/Base/AMReX.cpp
+++ Src/Base/AMReX.cpp
@@ -129,7 +129,8 @@ amrex::write_to_stderr_without_buffering (const char* str)
     {
 	std::ostringstream procall;
 	procall << ParallelDescriptor::MyProc() << "::";
-	const char *cprocall = procall.str().c_str();
+        auto tmp = procall.str();
+	const char *cprocall = tmp.c_str();
         const char * const end = " !!!\n";
 	fwrite(cprocall, strlen(cprocall), 1, stderr);
         fwrite(str, strlen(str), 1, stderr);
