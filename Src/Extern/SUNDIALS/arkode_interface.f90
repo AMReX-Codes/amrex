26096c0869474715ae8166b2cec46a8ba8aae688
Cody J. Balos <balos1@llnl.gov>
2019-10-29 14:56:32 -0700

    add interface to SUNDIALS 5

---
+++ Src/Extern/SUNDIALS/arkode_interface.f90
@@ -0,0 +1,22 @@
+module arkode_interface
+  use farkode_mod
+  use farkode_arkstep_mod
+  use fsunmatrix_dense_mod
+  use fsunlinsol_dense_mod
+  contains
+  integer(c_int) function FARKDense(arkode_mem, N) 
+    use, intrinsic :: iso_c_binding
+    use fnvector_serial_mod
+    implicit none 
+    type(c_ptr),           value    :: arkode_mem
+    integer(c_long),       value    :: N
+    type(SUNMatrix),       pointer  :: sunmat_A
+    type(SUNLinearSolver), pointer  :: sunlinsol_LS
+    type(N_Vector),        pointer  :: sunvec_y
+    integer(c_int)                  :: ierr
+    sunvec_y => FN_VNewEmpty_Serial(N)
+    sunmat_A => FSUNDenseMatrix(N, N)
+    sunlinsol_LS => FSUNDenseLinearSolver(sunvec_y, sunmat_A)
+    ierr = FARKStepSetLinearSolver(arkode_mem, sunlinsol_LS, sunmat_A)
+  end function FARKDense
+end module arkode_interface
