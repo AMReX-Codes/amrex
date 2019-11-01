45f4644bbdb0eb9c91debe78ea51035a7bd071e7
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-11-01 16:00:42 -0700

    MLNodeTensorLaplacian: WIP

--- Src/LinearSolvers/MLMG/AMReX_MLNodeTensorLaplacian.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeTensorLaplacian.cpp
@@ -1,4 +1,7 @@
 #include <AMReX_MLNodeTensorLaplacian.H>
+#include <AMReX_MLNodeLap_K.H>
+#include <AMReX_MLNodeTensorLap_K.H>
+#include <AMReX_MultiFabUtil.H>
 
 namespace amrex {
 
@@ -6,7 +9,9 @@ MLNodeTensorLaplacian::MLNodeTensorLaplacian (const Vector<Geometry>& a_geom,
                                               const Vector<BoxArray>& a_grids,
                                               const Vector<DistributionMapping>& a_dmap,
                                               const LPInfo& a_info)
-{}
+{
+    define(a_geom, a_grids, a_dmap, a_info);
+}
 
 MLNodeTensorLaplacian::~MLNodeTensorLaplacian ()
 {}
@@ -16,25 +21,100 @@ MLNodeTensorLaplacian::define (const Vector<Geometry>& a_geom,
                                const Vector<BoxArray>& a_grids,
                                const Vector<DistributionMapping>& a_dmap,
                                const LPInfo& a_info)
-{}
+{
+    BL_PROFILE("MLNodeTensorLaplacian::define()");
+
+    // This makes sure grids are cell-centered;
+    Vector<BoxArray> cc_grids = a_grids;
+    for (auto& ba : cc_grids) {
+        ba.enclosedCells();
+    }
+
+    MLNodeLinOp::define(a_geom, cc_grids, a_dmap, a_info);
+}
 
 void
 MLNodeTensorLaplacian::restriction (int amrlev, int cmglev, MultiFab& crse, MultiFab& fine) const
 {
+    BL_PROFILE("MLNodeTensorLaplacian::restriction()");
+
+    applyBC(amrlev, cmglev-1, fine, BCMode::Homogeneous, StateMode::Solution);
+
+    bool need_parallel_copy = !amrex::isMFIterSafe(crse, fine);
+    MultiFab cfine;
+    if (need_parallel_copy) {
+        const BoxArray& ba = amrex::coarsen(fine.boxArray(), 2);
+        cfine.define(ba, fine.DistributionMap(), 1, 0);
+    }
+
+    MultiFab* pcrse = (need_parallel_copy) ? &cfine : &crse;
+    const iMultiFab& dmsk = *m_dirichlet_mask[amrlev][cmglev-1];
+
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+    for (MFIter mfi(*pcrse, TilingIfNotGPU()); mfi.isValid(); ++mfi)
+    {
+        const Box& bx = mfi.tilebox();
+        Array4<Real> cfab = pcrse->array(mfi);
+        Array4<Real const> const& ffab = fine.const_array(mfi);
+        Array4<int const> const& mfab = dmsk.const_array(mfi);
+        AMREX_HOST_DEVICE_PARALLEL_FOR_3D(bx, i, j, k,
+        {
+            mlndlap_restriction(i,j,k,cfab,ffab,mfab);
+        });
+    }
+
+    if (need_parallel_copy) {
+        crse.ParallelCopy(cfine);
+    }
 }
 
 void
 MLNodeTensorLaplacian::interpolation (int amrlev, int fmglev, MultiFab& fine,
                                       const MultiFab& crse) const
 {
+    BL_PROFILE("MLNodeTensorLaplacian::interpolation()");
 
+    bool need_parallel_copy = !amrex::isMFIterSafe(crse, fine);
+    MultiFab cfine;
+    const MultiFab* cmf = &crse;
+    if (need_parallel_copy) {
+        const BoxArray& ba = amrex::coarsen(fine.boxArray(), 2);
+        cfine.define(ba, fine.DistributionMap(), 1, 0);
+        cfine.ParallelCopy(crse);
+        cmf = &cfine;
+    }
+
+    const iMultiFab& dmsk = *m_dirichlet_mask[amrlev][fmglev];
+
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+    for (MFIter mfi(fine, TilingIfNotGPU()); mfi.isValid(); ++mfi)
+    {
+        Box const& bx = mfi.tilebox();
+        Array4<Real> const& ffab = fine.array(mfi);
+        Array4<Real const> const& cfab = cmf->const_array(mfi);
+        Array4<int const> const& mfab = dmsk.const_array(mfi);
+        AMREX_HOST_DEVICE_PARALLEL_FOR_3D(bx, i, j, k,
+        {
+            mlndtslap_interpadd(i,j,k,ffab,cfab,mfab);
+        });
+    }
 }
 
 void
 MLNodeTensorLaplacian::averageDownSolutionRHS (int camrlev, MultiFab& crse_sol, MultiFab& crse_rhs,
                                                const MultiFab& fine_sol, const MultiFab& fine_rhs)
 {
-    amrex::Abort("MLNodeTensorLaplacian::averageDownSolutionRHS: TODO");
+    const auto& amrrr = AMRRefRatio(camrlev);
+    amrex::average_down(fine_sol, crse_sol, 0, 1, amrrr);
+
+    if (isSingular(0))
+    {
+        amrex::Abort("MLNodeTensorLaplacian::averageDownSolutionRHS: TODO");
+    }
 }
 
 void
@@ -47,7 +127,13 @@ MLNodeTensorLaplacian::reflux (int crse_amrlev,
 
 void
 MLNodeTensorLaplacian::prepareForSolve ()
-{}
+{
+    BL_PROFILE("MLNodeTensorLaplacian::prepareForSolve()");
+
+    MLNodeLinOp::prepareForSolve();
+
+    buildMasks();
+}
 
 void
 MLNodeTensorLaplacian::applyBC (int amrlev, int mglev, MultiFab& phi, BCMode bc_mode, StateMode s_mode,
