a26d63806a249ade6bb554c0e526ce98bda14032
Junmin Gu <jgu@lbl.gov>
2019-11-06 19:52:29 -0800

    field data and particle data are now in the same openPMD file for  the same iteration

--- Source/Diagnostics/WarpXIO.cpp
+++ Source/Diagnostics/WarpXIO.cpp
@@ -527,6 +527,11 @@ WarpX::WritePlotFile () const
     }
 
 #ifdef WARPX_USE_OPENPMD
+    m_OpenPMDPlotWriter->SetStep(istep[0]);
+    if (dump_openpmd)
+      m_OpenPMDPlotWriter->WriteOpenPMDFields(varnames, 
+					      *output_mf[0], output_geom[0], istep[0], t_new[0] );
+    /*
     if (dump_openpmd){
         // Write openPMD format: only for level 0
         std::string filename = std::string("diags/");
@@ -534,11 +539,12 @@ WarpX::WritePlotFile () const
         filename += amrex::Concatenate("/data", istep[0]);
         filename += std::string(".") + openpmd_backend;
         WriteOpenPMDFields( filename, varnames,
-                      *output_mf[0], output_geom[0], istep[0], t_new[0] );
+	                    *output_mf[0], output_geom[0], istep[0], t_new[0] );
     }
+    */
 #endif
 
-    if (dump_plotfiles ||  dump_openpmd){
+    if (dump_plotfiles ||  dump_openpmd) {
 
     // Write the fields contained in `mf_avg`, and corresponding to the
     // names `varnames`, into a plotfile.
@@ -623,14 +629,20 @@ WarpX::WritePlotFile () const
 
 #ifdef WARPX_USE_OPENPMD            
     // Write openPMD format: only for level 0
+
+    /*
     std::string filename = std::string("diags/");
     filename.append(openpmd_backend);
     filename += amrex::Concatenate("/particle", istep[0]);
     filename += std::string(".") + openpmd_backend;
     
-    //mypc->WritePlotFile(filename);        
-    WarpXOpenPMDParticle openPMDWriter(filename);
+
+    WarpXOpenPMDParticle  openPMDWriter(filename, openpmd_backend);
     openPMDWriter.SaveContainerPlots(mypc);
+    */
+    
+ //m_OpenPMDPlotWriter->SetStep(istep[0]);
+    m_OpenPMDPlotWriter->SaveContainerPlots(mypc);
 #else
     mypc->WritePlotFile(plotfilename);
 #endif
