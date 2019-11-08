1af6f92ca4f4cd4fac5870e982c9009616a6cd84
Luca Fedeli <luca.fedeli.88@gmail.com>
2019-11-08 07:33:56 -0600

    Update Source/Laser/LaserParticleContainer.cpp
    Co-Authored-By: Axel Huebl <axel.huebl@plasma.ninja>

--- Source/Laser/LaserParticleContainer.cpp
+++ Source/Laser/LaserParticleContainer.cpp
@@ -51,7 +51,7 @@ LaserParticleContainer::LaserParticleContainer (AmrCore* amr_core, int ispecies,
 
     //Select laser profile
     if(laser_profiles_dictionary.count(laser_type_s) == 0){
-        amrex::Abort("Unknown laser type");
+        amrex::Abort(std::string("Unknown laser type: ").append(laser_type_s));
     }
     m_up_laser_profile = laser_profiles_dictionary.at(laser_type_s)();
     //__________
