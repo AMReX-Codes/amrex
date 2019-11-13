1ab692d865b4d56ce66580ad5144ca0e939acbfb
Kevin Gott <kngott@lbl.gov>
2019-11-12 16:56:20 -0800

    More no-GPU fixes.

--- Src/Base/AMReX_Random.cpp
+++ Src/Base/AMReX_Random.cpp
@@ -16,7 +16,7 @@
 #endif
 
 #ifdef AMREX_USE_HIP
-using randState_t = hiprandState_t;,
+using randState_t = hiprandState_t;
 #elif defined(AMREX_USE_CUDA)
 using randState_t =  curandState_t; 
 #endif
