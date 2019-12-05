52b41848a0b56293f73377742a2da6a9981a5404
Andrew Myers <atmyers2@gmail.com>
2019-12-05 10:50:33 -0800

    reimplement lock using 64-bit CAS to simultaneously update the block and the counts.

--- Src/Base/AMReX_Random.cpp
+++ Src/Base/AMReX_Random.cpp
@@ -23,11 +23,11 @@ namespace
     AMREX_GPU_DEVICE curandState_t* states_d_ptr;
     curandState_t* states_h_ptr = nullptr;
 
-    AMREX_GPU_DEVICE int* locks_d_ptr;
-    int* locks_h_ptr = nullptr;
+    AMREX_GPU_DEVICE unsigned long long* locks_d_ptr;
+    unsigned long long* locks_h_ptr = nullptr;
 
-    AMREX_GPU_DEVICE unsigned int* n_work_threads_d_ptr;
-    unsigned int* n_work_threads_h_ptr = nullptr;
+    static_assert(sizeof(unsigned long long) == 2*sizeof(int),
+                  "sizeof unsigned long long != 2 * sizeof int");
 #endif
 
 }
@@ -72,20 +72,31 @@ int amrex::get_state (int tid)
     int nstates = cuda_nstates - (cuda_nstates % bsize);
     int i = tid % nstates;
 
-    while (true)
-    {
-        int r = atomicCAS(&locks_d_ptr[i], -1, blockIdx.x);
-        if ((r == -1) or (r == blockIdx.x))
-        {
-            break;
-        }
-        else {
-            continue;
-        }
-    }
-    atomicInc(&n_work_threads_d_ptr[i], 100000000);
+    unsigned long long* d_state = locks_d_ptr + i;
+    unsigned long long old = *d_state;
+    unsigned long long assumed;
 
-    return i;    
+    do {
+        assumed = old;
+        int* assumed_block = (int*)(&assumed);
+        int* assumed_count = assumed_block + 1;
+
+        unsigned long long val; // The value we try to store
+        int* val_block = (int*)(&val);
+        int* val_count = val_block + 1;
+        *val_block = blockIdx.x;
+
+        if (*assumed_block == blockIdx.x) { // locked by this block. so the goal is ++count
+            *val_count = *assumed_count + 1;
+        } else { // unlocked or locked by another block. so the goal is to lock.
+            *val_count = 1;
+            *assumed_block = -1; // only able to lock if the address hold (-1,0).
+            *assumed_count = 0;
+        }
+        old = atomicCAS(d_state, assumed, val);
+    } while (assumed != old);
+        
+    return i;
 }
 
 AMREX_GPU_DEVICE
@@ -95,10 +106,30 @@ void amrex::free_state (int tid)
     int nstates = cuda_nstates - (cuda_nstates % bsize);
     int i = tid % nstates;
 
-    if (atomicDec(&n_work_threads_d_ptr[i], 100000000) == 1)
-    {
-        atomicExch(&locks_d_ptr[i], -1);
-    }
+    unsigned long long* d_state = locks_d_ptr + i;    
+    unsigned long long old = *d_state;
+    unsigned long long assumed;
+
+    do {
+        assumed = old;
+        int* assumed_block = (int*)(&assumed);
+        int* assumed_count = assumed_block + 1;
+        // since we are trying to release the lock (i.e. we still hold the lock),
+        // the assumed blockid is blockIdx.x.
+
+        unsigned long long val;
+        int* val_block = (int*)(&val);
+        int* val_count = val_block+1;
+
+        if (*assumed_count == 1) { // last thread is responsible for releasing the lock
+            *val_block = -1; // lock will be released of we can it to (-1,0).
+            *val_count = 0;
+        } else { // try to --count, but cannot release the lock yet
+            *val_block = *assumed_block;
+            *val_count = *assumed_count - 1;
+        }
+        old = atomicCAS(d_state, assumed, val);
+    } while(assumed != old);
 }
 #endif
 
@@ -281,12 +312,10 @@ amrex::ResizeRandomSeed (int N)
     int SizeDiff = N - PrevSize;
 
     curandState_t* new_data;
-    int* new_mutex;
-    unsigned int* new_work_thread_count;
+    unsigned long long* new_mutex;
 
     AMREX_CUDA_SAFE_CALL(cudaMalloc(&new_data, N*sizeof(curandState_t)));
-    AMREX_CUDA_SAFE_CALL(cudaMalloc(&new_mutex, N*sizeof(int)));
-    AMREX_CUDA_SAFE_CALL(cudaMalloc(&new_work_thread_count, N*sizeof(unsigned int)));
+    AMREX_CUDA_SAFE_CALL(cudaMalloc(&new_mutex, N*sizeof(unsigned long long)));
     
     if (states_h_ptr != nullptr) {
 
@@ -296,20 +325,15 @@ amrex::ResizeRandomSeed (int N)
                                         PrevSize*sizeof(curandState_t),
                                         cudaMemcpyDeviceToDevice));
         AMREX_CUDA_SAFE_CALL(cudaMemcpy(new_mutex, locks_h_ptr,
-                                        PrevSize*sizeof(int),
-                                        cudaMemcpyDeviceToDevice));
-        AMREX_CUDA_SAFE_CALL(cudaMemcpy(new_work_thread_count, n_work_threads_h_ptr,
-                                        PrevSize*sizeof(unsigned int),
+                                        PrevSize*sizeof(unsigned long long),
                                         cudaMemcpyDeviceToDevice));
         
         AMREX_CUDA_SAFE_CALL(cudaFree(states_h_ptr));
         AMREX_CUDA_SAFE_CALL(cudaFree(locks_h_ptr));
-        AMREX_CUDA_SAFE_CALL(cudaFree(n_work_threads_h_ptr));
     }
 
     states_h_ptr = new_data;
     locks_h_ptr = new_mutex;
-    n_work_threads_h_ptr = new_work_thread_count;
     cuda_nstates = N;
 
     AMREX_CUDA_SAFE_CALL(cudaMemcpyToSymbol(states_d_ptr,
@@ -317,10 +341,7 @@ amrex::ResizeRandomSeed (int N)
                                             sizeof(curandState_t *)));
     AMREX_CUDA_SAFE_CALL(cudaMemcpyToSymbol(locks_d_ptr,
                                             &locks_h_ptr,
-                                            sizeof(int *)));
-    AMREX_CUDA_SAFE_CALL(cudaMemcpyToSymbol(n_work_threads_d_ptr,
-                                            &n_work_threads_h_ptr,
-                                            sizeof(unsigned int *)));
+                                            sizeof(unsigned long long*)));
 
     const int MyProc = amrex::ParallelDescriptor::MyProc();
     AMREX_PARALLEL_FOR_1D (SizeDiff, idx,
@@ -328,8 +349,13 @@ amrex::ResizeRandomSeed (int N)
         unsigned long seed = MyProc*1234567UL + 12345UL ;
         int seqstart = idx + 10 * idx ;
         int loc = idx + PrevSize;
-        locks_d_ptr[loc] = -1;
-        n_work_threads_d_ptr[loc] = 0;
+
+        int* val_block = (int*)(locks_d_ptr + loc);
+        int* val_count = val_block+1;
+
+        *val_block = -1;
+        *val_count = 0;
+        
         curand_init(seed, seqstart, 0, &states_d_ptr[loc]);
     });
 
@@ -350,12 +376,6 @@ amrex::DeallocateRandomSeedDevArray ()
     {
         cudaFree(locks_h_ptr);
         locks_h_ptr = nullptr;
-    }
-
-    if (n_work_threads_h_ptr != nullptr)
-    {
-        cudaFree(n_work_threads_h_ptr);
-        n_work_threads_h_ptr = nullptr;
     }
 
     cuda_nstates = 0;
