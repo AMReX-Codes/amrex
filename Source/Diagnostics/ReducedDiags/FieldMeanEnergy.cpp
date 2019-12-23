7c1028c752391eef135010cfd81ff80b6d58a9e2
Yinjian Zhao <yinjianzhao@lbl.gov>
2019-12-23 10:32:28 -0800

    First test of FieldMeanEnergy.

--- Source/Diagnostics/ReducedDiags/FieldMeanEnergy.cpp
+++ Source/Diagnostics/ReducedDiags/FieldMeanEnergy.cpp
@@ -2,6 +2,7 @@
 #include "WarpX.H"
 #include "WarpXConst.H"
 #include "AMReX_REAL.H"
+#include "AMReX_ParticleReduce.H"
 #include <iostream>
 #include <cmath>
 
@@ -55,20 +56,77 @@ void FieldMeanEnergy::ComputeDiags (int step)
     /// the extra one is for total energy
     m_data.resize(3,0.0);
 
-    /// loop over refinement levels
-    for (int lev = 0; lev <= 0; ++lev)
-    {
-        auto & Ex = warpx.getEfield(lev,0);
-        auto & Ey = warpx.getEfield(lev,1);
-        auto & Ez = warpx.getEfield(lev,2);
-        auto & Bx = warpx.getBfield(lev,0);
-        auto & By = warpx.getBfield(lev,1);
-        auto & Bz = warpx.getBfield(lev,2);
-        for ( MFIter mfi(Ex, TilingIfNotGPU()); mfi.isValid(); ++mfi )
-        {
-            //Ex(i,j,k)
-        }
-    }
+    //auto & Ex = warpx.getEfield(0,0);
+    //auto & Ey = warpx.getEfield(0,1);
+    //auto & Ez = warpx.getEfield(0,2);
+    //auto & Bx = warpx.getBfield(0,0);
+    //auto & By = warpx.getBfield(0,1);
+    //auto & Bz = warpx.getBfield(0,2);
+
+    //Real Exs = ReduceSum( Ex,
+    //[=] AMREX_GPU_HOST_DEVICE () -> Real
+    //{
+    //    return Ex*Ex;
+    //});
+
+    const MultiFab & Ex = warpx.getEfield(0,0);
+    const MultiFab & Ey = warpx.getEfield(0,1);
+    const MultiFab & Ez = warpx.getEfield(0,2);
+    const MultiFab & Bx = warpx.getBfield(0,0);
+    const MultiFab & By = warpx.getBfield(0,1);
+    const MultiFab & Bz = warpx.getBfield(0,2);
+
+    const int nghost = 0;
+    const int xcomp = 0;
+    const int ycomp = 0;
+    const int numcomp = 1;
+    Real Es = 0.0;
+    Real Bs = 0.0;
+
+//    Real sm = 0;
+//    sm += amrex::ReduceSum(
+//        Ex, nghost,
+//        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab) -> Real
+//        { return xfab.dot(bx,0,0,1); }
+//        );
+
+    Es += amrex::ReduceSum(
+        Ex, Ex, nghost,
+        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab, FArrayBox const& yfab) -> Real
+        { return xfab.dot(bx,xcomp,yfab,bx,ycomp,numcomp); }
+        );
+    Es += amrex::ReduceSum(
+        Ey, Ey, nghost,
+        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab, FArrayBox const& yfab) -> Real
+        { return xfab.dot(bx,xcomp,yfab,bx,ycomp,numcomp); }
+        );
+    Es += amrex::ReduceSum(
+        Ez, Ez, nghost,
+        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab, FArrayBox const& yfab) -> Real
+        { return xfab.dot(bx,xcomp,yfab,bx,ycomp,numcomp); }
+        );
+    Bs += amrex::ReduceSum(
+        Bx, Bx, nghost,
+        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab, FArrayBox const& yfab) -> Real
+        { return xfab.dot(bx,xcomp,yfab,bx,ycomp,numcomp); }
+        );
+    Bs += amrex::ReduceSum(
+        By, By, nghost,
+        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab, FArrayBox const& yfab) -> Real
+        { return xfab.dot(bx,xcomp,yfab,bx,ycomp,numcomp); }
+        );
+    Bs += amrex::ReduceSum(
+        Bz, Bz, nghost,
+        [=] AMREX_GPU_HOST_DEVICE (Box const& bx, FArrayBox const& xfab, FArrayBox const& yfab) -> Real
+        { return xfab.dot(bx,xcomp,yfab,bx,ycomp,numcomp); }
+        );
+    //if (!local) ParallelAllReduce::Sum(sm, ParallelContext::CommunicatorSub());
+    ParallelAllReduce::Sum(Es, ParallelContext::CommunicatorSub());
+    ParallelAllReduce::Sum(Bs, ParallelContext::CommunicatorSub());
+
+    m_data[1] = 0.5*Es*PhysConst::ep0;
+    m_data[2] = 0.5*Bs/PhysConst::mu0;
+    m_data[0] = m_data[1] + m_data[2];
 
     /// reduced sum for mpi ranks
     //ParallelDescriptor::ReduceRealSum();
