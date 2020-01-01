1da10a4706271ad10315a2ad83ec0cbe139779c2
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-12-31 23:11:54 -0800

    clean up and avoid potential OMP issues

--- Src/Base/AMReX_ParallelDescriptor.cpp
+++ Src/Base/AMReX_ParallelDescriptor.cpp
@@ -46,6 +46,12 @@ namespace amrex {
 
 namespace ParallelDescriptor
 {
+#ifdef AMREX_USE_MPI
+    template <> MPI_Datatype Mpi_typemap<IntVect>::type();
+    template <> MPI_Datatype Mpi_typemap<IndexType>::type();
+    template <> MPI_Datatype Mpi_typemap<Box>::type();
+#endif
+
 #ifdef AMREX_USE_GPU
     int use_gpu_aware_mpi = false;
 #else
@@ -55,10 +61,6 @@ namespace ParallelDescriptor
     ProcessTeam m_Team;
 
     MPI_Comm m_comm = MPI_COMM_NULL;    // communicator for all ranks, probably MPI_COMM_WORLD
-
-#ifdef AMREX_USE_MPI
-    MPI_Datatype m_lull_mpi_data_type = MPI_DATATYPE_NULL;
-#endif
 
     int m_MinTag = 1000, m_MaxTag = -1;
 
@@ -312,9 +314,11 @@ ParallelDescriptor::StartParallel (int*    argc,
 
     ParallelContext::push(m_comm);
 
-    if (m_lull_mpi_data_type == MPI_DATATYPE_NULL) {
-        MPI_Type_contiguous(sizeof(lull_t), MPI_CHAR, &m_lull_mpi_data_type);
-    }
+    // Create these types outside OMP parallel region
+    auto t1 = Mpi_typemap<IntVect>::type();
+    auto t2 = Mpi_typemap<IndexType>::type();
+    auto t3 = Mpi_typemap<Box>::type();
+    auto t4 = Mpi_typemap<ParallelDescriptor::lull_t>::type();
 
     // ---- find the maximum value for a tag
     int flag(0), *p;
@@ -1586,7 +1590,13 @@ template <>
 MPI_Datatype
 ParallelDescriptor::Mpi_typemap<ParallelDescriptor::lull_t>::type ()
 {
-    return  m_lull_mpi_data_type;
+    static MPI_Datatype mine = MPI_DATATYPE_NULL;
+    if (mine == MPI_DATATYPE_NULL)
+    {
+        BL_MPI_REQUIRE( MPI_Type_contiguous(sizeof(lull_t), MPI_CHAR, &mine) );
+        BL_MPI_REQUIRE( MPI_Type_commit(&mine) );
+    }
+    return mine;
 }
 
 void
