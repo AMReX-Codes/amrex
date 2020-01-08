cddb7b7ceac213b1432ce97a3828ac0ee885ffeb
Andrew Myers <atmyers2@gmail.com>
2020-01-07 17:04:51 -0800

    add code for computing common components in smart copy

---
+++ Source/Particles/ParticleCreation/SmartCopy.cpp
@@ -0,0 +1,34 @@
+#include "SmartCopy.H"
+
+SmartCopyTag getSmartCopyTag (const NameMap& src, const NameMap& dst)
+{
+    SmartCopyTag tag;
+
+    // we use the fact that maps are sorted
+    auto i_src = src.begin();
+    auto i_dst = dst.begin();
+    while ( (i_src != src.end()) and (i_dst != dst.end()) )
+    {
+        if (i_src->first < i_dst->first)
+        {
+            // names are not the same and src is lower
+            ++i_src;
+        }
+        else if (i_src->first > i_dst->first)
+        {
+            // names are not the same and dst is lower
+            ++i_dst;
+        }
+        else
+        {
+            // name is in both...
+            tag.common_names.push_back(i_src->first);
+            tag.src_comps.push_back(i_src->second);
+            tag.dst_comps.push_back(i_dst->second);
+            ++i_src;
+            ++i_dst;
+        }
+    }
+    
+    return tag;
+}
