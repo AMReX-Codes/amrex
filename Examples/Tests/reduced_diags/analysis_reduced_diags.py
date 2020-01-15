ccf10b39ecfb44cddba0e4f59f3d65ab460e1ce4
Yin-YinjiaZhao <yinjianzhao@lbl.gov>
2020-01-14 20:11:55 -0800

    Add scipy.constants.

--- Examples/Tests/reduced_diags/analysis_reduced_diags.py
+++ Examples/Tests/reduced_diags/analysis_reduced_diags.py
@@ -6,7 +6,7 @@
 # using the reduced diagnostics.
 # And they will be compared with the data in the plotfiles.
 
-# Tolerance: 1.0e-10 for particle energy, 1.0e-3 for field energy.
+# Tolerance: 1.0e-8 for particle energy, 1.0e-3 for field energy.
 # The difference of the field energy is relatively large,
 # because fields data in plotfiles are cell-centered,
 # but fields data in reduced diagnostics are staggered.
@@ -17,13 +17,7 @@ import sys
 import yt
 import math
 import numpy
-
-# constants
-c   = 299792458.0
-me  = 9.1093837015e-31
-mp  = 1.67262192369000e-27
-ep0 = 8.8541878128e-12
-mu0 = 1.25663706212e-6
+import scipy.constants as scc
 
 fn = sys.argv[1]
 
@@ -39,14 +33,14 @@ py = ad['electrons','particle_momentum_y'].to_ndarray()
 pz = ad['electrons','particle_momentum_z'].to_ndarray()
 w  = ad['electrons','particle_weight'].to_ndarray()
 for i in range(w.size):
-    EPyt = EPyt + (math.sqrt((px[i]**2+py[i]**2+pz[i]**2)*c**2+me**2*c**4)-me*c**2)*w[i]
+    EPyt = EPyt + (math.sqrt((px[i]**2+py[i]**2+pz[i]**2)*scc.c**2+scc.m_e**2*scc.c**4)-scc.m_e*scc.c**2)*w[i]
 # proton
 px = ad['protons','particle_momentum_x'].to_ndarray()
 py = ad['protons','particle_momentum_y'].to_ndarray()
 pz = ad['protons','particle_momentum_z'].to_ndarray()
 w  = ad['protons','particle_weight'].to_ndarray()
 for i in range(w.size):
-    EPyt = EPyt + (math.sqrt((px[i]**2+py[i]**2+pz[i]**2)*c**2+mp**2*c**4)-mp*c**2)*w[i]
+    EPyt = EPyt + (math.sqrt((px[i]**2+py[i]**2+pz[i]**2)*scc.c**2+scc.m_p**2*scc.c**4)-scc.m_p*scc.c**2)*w[i]
 
 ad = ds.covering_grid(level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions)
 Ex = ad['Ex'].to_ndarray()
@@ -59,7 +53,7 @@ Es = numpy.sum(Ex**2)+numpy.sum(Ey**2)+numpy.sum(Ez**2)
 Bs = numpy.sum(Bx**2)+numpy.sum(By**2)+numpy.sum(Bz**2)
 N  = numpy.array( ds.domain_width / ds.domain_dimensions )
 dV = N[0]*N[1]*N[2]
-EFyt = 0.5*Es*ep0*dV + 0.5*Bs/mu0*dV
+EFyt = 0.5*Es*scc.epsilon_0*dV + 0.5*Bs/scc.mu_0*dV
 
 # PART2: get results from reduced diagnostics
 
@@ -70,8 +64,7 @@ with open('./diags/reducedfiles/EF.txt') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 2:
-            EF = numpy.array(row[2])
-            EF = EF.astype(numpy.float)
+            EF = numpy.array(row[2]).astype(numpy.float)
         line_count += 1
 
 with open('./diags/reducedfiles/EP.txt') as csv_file:
@@ -79,8 +72,7 @@ with open('./diags/reducedfiles/EP.txt') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 2:
-            EP = numpy.array(row[2])
-            EP = EP.astype(numpy.float)
+            EP = numpy.array(row[2]).astype(numpy.float)
         line_count += 1
 
 # PART3: print and assert
@@ -91,4 +83,4 @@ print('difference of particle energy:', abs(EPyt-EP))
 print('tolerance of particle energy:', 1.0e-10)
 
 assert(abs(EFyt-EF) < 1.0e-3)
-assert(abs(EPyt-EP) < 1.0e-10)
+assert(abs(EPyt-EP) < 1.0e-8)
