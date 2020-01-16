94e2366d0b0ea4a46761830f1529c8bb83be3f27
Andrew Myers <atmyers@lbl.gov>
2020-01-16 09:46:00 -0800

    No thrust (#629)
    * remove thrust from warpx
    
    * add in header for std::complex

--- Source/FieldSolver/SpectralSolver/SpectralKSpace.cpp
+++ Source/FieldSolver/SpectralSolver/SpectralKSpace.cpp
@@ -144,12 +144,7 @@ SpectralKSpace::getSpectralShiftFactor( const DistributionMapping& dm,
         }
         const Complex I{0,1};
         for (int i=0; i<k.size(); i++ ){
-#ifdef AMREX_USE_GPU
-            shift[i] = thrust::exp( I*sign*k[i]*0.5*dx[i_dim] );
-#else
-            shift[i] = std::exp( I*sign*k[i]*0.5*dx[i_dim] );
-#endif
-
+            shift[i] = exp( I*sign*k[i]*0.5*dx[i_dim]);
         }
     }
     return shift_factor;
