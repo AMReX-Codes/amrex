2a157e43ee529ebf91bd402842c9bbdb49b7d86b
gtrichardson <gtrichardson@berkeley.edu>
2020-01-19 14:49:13 -0800

    removed unecessary import and adjusted vetheory to try to correct tests

--- Examples/Tests/Maxwell_Hybrid_QED/analysis_Maxwell_QED_Hybrid.py
+++ Examples/Tests/Maxwell_Hybrid_QED/analysis_Maxwell_QED_Hybrid.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-import sys
 import yt ; ty.funcs.mylog.setLevel(0)
 import numpy as np
 
-QEDfile = './plt'
-time = '00300'
-dsQED = yt.load(QEDfile + time)
+dsQED = yt.load(.plt300)
 QED_all_data_level_0 = dsQED.covering_grid(level=0,left_edge=(dsQED.domain_left_edge),
                                            dims=dsQED.domain_dimensions)
 EyQED = QED_all_data_level_0['boxlib', 'Ey'].v.squeeze()
 resolution = dsQED.domain_dimensions[1]
 dx = ((-0.5*resolution)+np.where(EyQED[0,:] == np.max(EyQED[0,:]))[0])*1024/resolution
 v = dx/(float(dsQED.current_time))
-vtheory = 299792458/np.sqrt((1+(12*10**(-13))/(8.8541878128*10**(-12)))/(1+(4*10**(-13))/(8.8541878128*10**(-12))))
+vtheory = 287618197.8265535
 
 print('Simulation velocity: ', v)
 print('Theory veclority: ', vtheory)
