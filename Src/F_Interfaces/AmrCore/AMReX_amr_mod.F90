05eee0753e044cd006f36dc18d288ce975107ad1
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-01-30 13:43:23 -0800

    pass boxarray and distributionmap when building amrex_flash_fluxregister

--- Src/F_Interfaces/AmrCore/AMReX_amr_mod.F90
+++ Src/F_Interfaces/AmrCore/AMReX_amr_mod.F90
@@ -7,5 +7,6 @@ module amrex_amr_module
   use amrex_fillpatch_module
   use amrex_interpolater_module
   use amrex_fluxregister_module
+  use amrex_flash_fluxregister_module
 
 end module amrex_amr_module
