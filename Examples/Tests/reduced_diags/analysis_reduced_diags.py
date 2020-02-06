845988a7f73fe38d1b95754cb7dc0b43c0c86a1d
Yinjian Zhao <yinjianzhao@lbl.gov>
2020-02-06 15:23:21 -0800

    Test.

--- Examples/Tests/reduced_diags/analysis_reduced_diags.py
+++ Examples/Tests/reduced_diags/analysis_reduced_diags.py
@@ -61,6 +61,7 @@ EFyt = 0.5*Es*scc.epsilon_0*dV + 0.5*Bs/scc.mu_0*dV
 
 # PART2: get results from reduced diagnostics
 
+EF = 0.0
 with open('./diags/reducedfiles/EF.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -69,6 +70,7 @@ with open('./diags/reducedfiles/EF.txt') as csv_file:
             EF = np.array(row[2]).astype(np.float)
         line_count += 1
 
+EP = 0.0
 with open('./diags/reducedfiles/EP.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
