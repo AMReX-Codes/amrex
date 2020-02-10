e725f2e97326927d14c889572bb7935326d2f2ec
Yinjian Zhao <yinjianzhao@lbl.gov>
2020-02-10 12:06:03 -0800

    Change back.

--- Examples/Tests/reduced_diags/analysis_reduced_diags.py
+++ Examples/Tests/reduced_diags/analysis_reduced_diags.py
@@ -23,7 +23,6 @@ import sys
 import yt
 import numpy as np
 import scipy.constants as scc
-from read_raw_data import read_reduced_diags
 
 fn = sys.argv[1]
 
@@ -61,11 +60,10 @@ EFyt = 0.5*Es*scc.epsilon_0*dV + 0.5*Bs/scc.mu_0*dV
 
 # PART2: get results from reduced diagnostics
 
-metadata, data = read_reduced_diags( './diags/reducedfiles/EF.txt' )
-EF = data['total_lev0'][-1] # last iteration
-
-metadata, data = read_reduced_diags( './diags/reducedfiles/EP.txt' )
-EP = data['total'][-1] # last iteration
+EFdata = np.genfromtxt("./diags/reducedfiles/EF.txt")
+EPdata = np.genfromtxt("./diags/reducedfiles/EP.txt")
+EF = EFdata[1][2]
+EP = EPdata[1][2]
 
 # PART3: print and assert
 
