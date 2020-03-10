57ff5dcd3429c749bb2ee002830713351d645362
David Grote <grote1@llnl.gov>
2020-03-10 14:02:51 -0700

    With RZ, always write out theta (#782)

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -130,6 +130,13 @@ PhysicalParticleContainer::PhysicalParticleContainer (AmrCore* amr_core, int isp
                 plot_flags[ParticleStringNames::to_index.at(var)] = 1;
             }
         }
+
+#ifdef WARPX_DIM_RZ
+        // Always write out theta, whether or not it's requested,
+        // to be consistent with always writing out r and z.
+        plot_flags[ParticleStringNames::to_index.at("theta")] = 1;
+#endif
+
     }
 
     // Parse galilean velocity
