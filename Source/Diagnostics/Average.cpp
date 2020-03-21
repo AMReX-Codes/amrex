6244e0506095e82681475574bb6192a033a333fd
Edoardo Zoni <59625522+EZoni@users.noreply.github.com>
2020-03-20 21:27:45 -0700

    Move kernel ToCellCenter to Average.H to ensure inlining (#832)

--- Source/Diagnostics/Average.cpp
+++ Source/Diagnostics/Average.cpp
@@ -2,32 +2,6 @@
 
 using namespace amrex;
 
-AMREX_GPU_HOST_DEVICE
-AMREX_FORCE_INLINE
-Real Average::ToCellCenter ( Array4<Real const> const& mf_in_arr,
-                             const IntVect stag,
-                             const int i,
-                             const int j,
-                             const int k,
-                             const int comp)
-{
-    const int sx = stag[0];
-    const int sy = stag[1];
-#if   (AMREX_SPACEDIM == 2)
-    constexpr int sz = 0;
-#elif (AMREX_SPACEDIM == 3)
-    const int sz = stag[2];
-#endif
-    return 0.125_rt * (   mf_in_arr(i   ,j   ,k   ,comp)
-                        + mf_in_arr(i+sx,j   ,k   ,comp)
-                        + mf_in_arr(i   ,j+sy,k   ,comp)
-                        + mf_in_arr(i   ,j   ,k+sz,comp)
-                        + mf_in_arr(i+sx,j+sy,k   ,comp)
-                        + mf_in_arr(i   ,j+sy,k+sz,comp)
-                        + mf_in_arr(i+sx,j   ,k+sz,comp)
-                        + mf_in_arr(i+sx,j+sy,k+sz,comp) );
-}
-
 void
 Average::ToCellCenter ( MultiFab& mf_out,
                         const MultiFab& mf_in,
