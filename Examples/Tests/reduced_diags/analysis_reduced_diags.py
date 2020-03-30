[mini-PR] Fix energy calculation for photons in reduced diagnostics (#861)
* fix energy calculation for photons

* fixed typo and mada calculation clearer

* added photon particles in reduced diags test

* Update Source/Diagnostics/ReducedDiags/ParticleEnergy.cpp

Co-Authored-By: MaxThevenet <mthevenet@lbl.gov>

* Update Source/Diagnostics/ReducedDiags/ParticleEnergy.cpp

Co-Authored-By: MaxThevenet <mthevenet@lbl.gov>

Co-authored-by: MaxThevenet <mthevenet@lbl.gov>

Examples/Tests/reduced_diags/analysis_reduced_diags.py
@@ -7,7 +7,7 @@
 # License: BSD-3-Clause-LBNL
 
 # This script tests the reduced diagnostics.
-# The setup is a uniform plasma with electrons and protons.
+# The setup is a uniform plasma with electrons, protons and photons.
 # Particle energy and field energy will be outputed
 # using the reduced diagnostics.
 # And they will be compared with the data in the plotfiles.
@@ -44,6 +44,13 @@ py = ad['protons','particle_momentum_y'].to_ndarray()
 pz = ad['protons','particle_momentum_z'].to_ndarray()
 w  = ad['protons','particle_weight'].to_ndarray()
 EPyt = EPyt + np.sum( (np.sqrt((px**2+py**2+pz**2)*scc.c**2+scc.m_p**2*scc.c**4)-scc.m_p*scc.c**2)*w )
+
+# photon
+px = ad['photons','particle_momentum_x'].to_ndarray()
+py = ad['photons','particle_momentum_y'].to_ndarray()
+pz = ad['photons','particle_momentum_z'].to_ndarray()
+w  = ad['photons','particle_weight'].to_ndarray()
+EPyt = EPyt + np.sum( (np.sqrt(px**2+py**2+pz**2)*scc.c)*w )
 
 ad = ds.covering_grid(level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions)
 Ex = ad['Ex'].to_ndarray()