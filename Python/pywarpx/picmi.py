6f0b426c352330a317236814d6369f7640561092
L. Diana Amorim <LDianaAmorim@lbl.gov>
2020-03-30 09:11:46 -0700

    Added blocking factor to 2d and RZ geometries (#864)

--- Python/pywarpx/picmi.py
+++ Python/pywarpx/picmi.py
@@ -331,6 +331,7 @@ class CylindricalGrid(picmistandard.PICMI_CylindricalGrid):
         # Maximum allowable size of each subdomain in the problem domain;
         #    this is used to decompose the domain for parallel calculations.
         pywarpx.amr.max_grid_size = self.max_grid_size
+        pywarpx.amr.blocking_factor = self.blocking_factor
 
         assert self.lower_bound[0] >= 0., Exception('Lower radial boundary must be >= 0.')
         assert self.bc_rmin != 'periodic' and self.bc_rmax != 'periodic', Exception('Radial boundaries can not be periodic')
@@ -373,6 +374,7 @@ class Cartesian2DGrid(picmistandard.PICMI_Cartesian2DGrid):
         # Maximum allowable size of each subdomain in the problem domain;
         #    this is used to decompose the domain for parallel calculations.
         pywarpx.amr.max_grid_size = self.max_grid_size
+        pywarpx.amr.blocking_factor = self.blocking_factor
 
         # Geometry
         pywarpx.geometry.coord_sys = 0  # Cartesian
@@ -411,7 +413,6 @@ class Cartesian3DGrid(picmistandard.PICMI_Cartesian3DGrid):
         # Maximum allowable size of each subdomain in the problem domain;
         #    this is used to decompose the domain for parallel calculations.
         pywarpx.amr.max_grid_size = self.max_grid_size
-
         pywarpx.amr.blocking_factor = self.blocking_factor
 
         # Geometry
