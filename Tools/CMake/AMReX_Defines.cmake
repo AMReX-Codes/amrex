effc622d2f89093c5f32414e507d3acff8e52469
Michele Rosso <mrosso@lbl.gov>
2020-04-08 21:04:08 -0700

    CMake: implement ENABLE_FORTRAN (BL_NO_FORT) option

--- Tools/CMake/AMReX_Defines.cmake
+++ Tools/CMake/AMReX_Defines.cmake
@@ -1,4 +1,4 @@
-# 
+#
 # FUNCTION: set_amrex_defines
 #
 # Populate the "compile definitions" property of target "amrex".
@@ -12,15 +12,15 @@
 # CPPFLAGS should overwrite AMReX_DEFINES. Since this is not possible without
 # breaking the build (most of the defines here are necessary for AMReX to compile),
 # for the time being we will not follow this requirement.
-# 
-#  
+#
+#
 # Author: Michele Rosso
 # Date  : June 26, 2018
 #
-# 
+#
 function ( set_amrex_defines )
 
-   # 
+   #
    # Check if target "amrex" has been defined before
    # calling this macro
    #
@@ -35,7 +35,7 @@ function ( set_amrex_defines )
    # Git version
    add_amrex_define( "AMREX_GIT_VERSION=\"${AMREX_GIT_VERSION}\"" NO_LEGACY )
 
-   # XSDK mode 
+   # XSDK mode
    add_amrex_define( AMREX_XSDK IF USE_XSDK_DEFAULTS )
 
    # Debug flag
@@ -51,7 +51,7 @@ function ( set_amrex_defines )
    # Tiny profiler
    add_amrex_define( AMREX_TINY_PROFILING IF ENABLE_TINY_PROFILE )
 
-   # Mem profiler 
+   # Mem profiler
    add_amrex_define( AMREX_MEM_PROFILING IF ENABLE_MEM_PROFILE )
 
    # Backtrace
@@ -65,7 +65,7 @@ function ( set_amrex_defines )
 
    # OpenMP
    add_amrex_define( AMREX_USE_OMP IF ENABLE_OMP )
-  
+
    # Precision
    if (NOT ENABLE_DP)
       add_amrex_define(AMREX_USE_FLOAT)
@@ -76,38 +76,43 @@ function ( set_amrex_defines )
 
    # System
    add_amrex_define( AMREX_${CMAKE_SYSTEM_NAME} )
-   
+
    #  Assertions
    add_amrex_define( AMREX_USE_ASSERTION IF ENABLE_ASSERTIONS )
 
    #
    # Fortran-specific defines: BL_LANG_FORT and AMREX_LANG_FORT
    #
-   target_compile_definitions ( amrex PUBLIC
-      $<$<COMPILE_LANGUAGE:Fortran>:BL_LANG_FORT> )
-   target_compile_definitions ( amrex PUBLIC
-      $<$<COMPILE_LANGUAGE:Fortran>:AMREX_LANG_FORT> )
+   if (ENABLE_FORTRAN)
+      target_compile_definitions ( amrex PUBLIC
+         $<$<COMPILE_LANGUAGE:Fortran>:BL_LANG_FORT> )
+      target_compile_definitions ( amrex PUBLIC
+         $<$<COMPILE_LANGUAGE:Fortran>:AMREX_LANG_FORT> )
 
-   # 
-   # Fortran/C mangling scheme
-   # 
-   include( FortranCInterface )
-   include( ${FortranCInterface_BINARY_DIR}/Output.cmake )
+      #
+      # Fortran/C mangling scheme
+      #
+      include( FortranCInterface )
+      include( ${FortranCInterface_BINARY_DIR}/Output.cmake )
 
-   set( FORTLINK "" )
-   
-   if ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "" )
-      set(FORTLINK "${FortranCInterface_GLOBAL_CASE}CASE" )
-      message(STATUS "Fortran name mangling scheme: ${FORTLINK} (no append underscore)")
-   elseif ( (FortranCInterface_GLOBAL_SUFFIX STREQUAL "_")  AND
-	 ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER" ) )
-      set(FORTLINK "UNDERSCORE")
-      message(STATUS "Fortran name mangling scheme: ${FORTLINK} (lower case, append underscore)")
+      set( FORTLINK "" )
+
+      if ( FortranCInterface_GLOBAL_SUFFIX STREQUAL "" )
+         set(FORTLINK "${FortranCInterface_GLOBAL_CASE}CASE" )
+         message(STATUS "Fortran name mangling scheme: ${FORTLINK} (no append underscore)")
+      elseif ( (FortranCInterface_GLOBAL_SUFFIX STREQUAL "_")  AND
+	    ( FortranCInterface_GLOBAL_CASE STREQUAL "LOWER" ) )
+         set(FORTLINK "UNDERSCORE")
+         message(STATUS "Fortran name mangling scheme: ${FORTLINK} (lower case, append underscore)")
+      else ()
+         message(AUTHOR_WARNING "Fortran to C mangling not compatible with AMReX code")
+      endif ()
+
+      add_amrex_define( AMREX_FORT_USE_${FORTLINK} )
+
    else ()
-      message(AUTHOR_WARNING "Fortran to C mangling not compatible with AMReX code")
+      add_amrex_define(BL_NO_FORT)
    endif ()
-
-   add_amrex_define( AMREX_FORT_USE_${FORTLINK} )
 
    # SENSEI Insitu
    add_amrex_define( AMREX_USE_SENSEI_INSITU IF ENABLE_SENSEI_INSITU )
@@ -121,7 +126,7 @@ function ( set_amrex_defines )
    # EB
    add_amrex_define( AMREX_USE_EB NO_LEGACY IF ENABLE_EB )
 
-   # 
+   #
    # CUDA
    #
    add_amrex_define( AMREX_USE_CUDA NO_LEGACY IF ENABLE_CUDA )
@@ -130,8 +135,8 @@ function ( set_amrex_defines )
       IF ENABLE_CUDA )
 
    # Fortran macros used by application code only
-   # (they are not present in AMReX source code) 
-   if (ENABLE_CUDA AND ENABLE_AMREX_FORTRAN )    
+   # (they are not present in AMReX source code)
+   if (ENABLE_CUDA AND ENABLE_AMREX_FORTRAN )
       target_compile_definitions( amrex PUBLIC
          $<$<COMPILE_LANGUAGE:Fortran>:AMREX_LAUNCH=attributes\(global\);
          AMREX_DEVICE=attributes\(device\);
@@ -150,10 +155,10 @@ function ( set_amrex_defines )
    #
    # OpenACC
    #
-   add_amrex_define( AMREX_USE_ACC NO_LEGACY IF ENABLE_ACC ) 
+   add_amrex_define( AMREX_USE_ACC NO_LEGACY IF ENABLE_ACC )
 
    #
-   # General setup for any GPUs 
+   # General setup for any GPUs
    #
    if (ENABLE_CUDA OR ENABLE_ACC)
       add_amrex_define( AMREX_USE_GPU  NO_LEGACY )
@@ -163,11 +168,7 @@ function ( set_amrex_defines )
          NO_LEGACY IF GPUS_PER_SOCKET)
 
       add_amrex_define( AMREX_GPUS_PER_NODE=${GPUS_PER_NODE}
-         NO_LEGACY IF GPUS_PER_NODE)         
+         NO_LEGACY IF GPUS_PER_NODE)
    endif ()
-   
+
 endfunction ()
-
-
-
-
