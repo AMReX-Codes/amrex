0fd287af0b24d8b441d0f06d5ff2efde2533c2f2
Andrew Myers <atmyers2@gmail.com>
2020-05-01 13:27:04 -0700

    add test for InitFromAscii

---
+++ Tests/Particles/InitFromAscii/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_MultiFabUtil.H>
+#include "AMReX_Particles.H"
+#include "AMReX_PlotFileUtil.H"
+
+using namespace amrex;
+
+struct TestParams {
+  int nx;
+  int ny;
+  int nz;
+  int max_grid_size;
+  bool verbose;
+};
+
+void test_init_ascii (TestParams& parms)
+{
+    RealBox real_box;
+    for (int n = 0; n < AMREX_SPACEDIM; n++)
+    {
+        real_box.setLo(n, 0.0);
+        real_box.setHi(n, 1.0);
+    }
+    
+    IntVect domain_lo(AMREX_D_DECL(0, 0, 0));
+    IntVect domain_hi(AMREX_D_DECL(parms.nx - 1, parms.ny - 1, parms.nz-1));
+    const Box domain(domain_lo, domain_hi);
+    
+    // This sets the boundary conditions to be doubly or triply periodic
+    int is_per[AMREX_SPACEDIM];
+    for (int i = 0; i < AMREX_SPACEDIM; i++) 
+        is_per[i] = 1;
+    Geometry geom(domain, &real_box, CoordSys::cartesian, is_per);
+    
+    BoxArray ba(domain);
+    ba.maxSize(parms.max_grid_size);
+
+    DistributionMapping dmap(ba);
+
+    constexpr int ncomps = 1 + AMREX_SPACEDIM;
+    typedef ParticleContainer<ncomps> MyParticleContainer;
+    MyParticleContainer myPC(geom, dmap, ba);
+
+    myPC.InitFromAsciiFile("particles.txt", ncomps);
+    
+    amrex::Print() << myPC.TotalNumberOfParticles() << "\n";
+}
+
+int main(int argc, char* argv[])
+{
+  amrex::Initialize(argc,argv);
+  
+  ParmParse pp;
+  
+  TestParams parms; 
+  pp.get("nx", parms.nx);
+  pp.get("ny", parms.ny);
+  pp.get("nz", parms.nz);
+  pp.get("max_grid_size", parms.max_grid_size);
+  
+  test_init_ascii(parms);
+  
+  amrex::Finalize();
+}
