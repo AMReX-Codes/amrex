ad63449780e4d2a7c428937dfb0476624fefa569
WeiqunZhang <WeiqunZhang@lbl.gov>
2020-05-02 13:15:21 -0700

    DPC++ scan (#837)
    * DPC++ scan
    
    * ParallelScan tutorial: add std version and put thrust version unside ifdef AMREX_USE_CUDA

--- Tutorials/GPU/ParallelScan/main.cpp
+++ Tutorials/GPU/ParallelScan/main.cpp
@@ -3,9 +3,14 @@
 #include <AMReX_ParmParse.H>
 #include <AMReX_Scan.H>
 
+#ifdef AMREX_USE_CUDA
 #include <thrust/device_malloc_allocator.h>
 #include <thrust/scan.h>
+#endif
 
+#include <numeric>
+
+#ifdef AMREX_USE_CUDA
 namespace amrex
 {
     template<class T>
@@ -43,6 +48,7 @@ namespace amrex
         };
     }
 }
+#endif
 
 using namespace amrex;
 
@@ -73,10 +79,15 @@ void main_main ()
     for (auto& x: h_in) {
         x = static_cast<T>((Random()-0.5)*100.);
     }
-    Vector<T> h_inclusive_thrust(N);
+
+    Vector<T> h_exclusive_cpu(N);
+    Vector<T> h_inclusive_cpu(N);
     Vector<T> h_inclusive_amrex(N);
-    Vector<T> h_exclusive_thrust(N);
     Vector<T> h_exclusive_amrex(N);
+#ifdef AMREX_USE_CUDA
+    Vector<T> h_exclusive_thrust(N);
+    Vector<T> h_inclusive_thrust(N);
+#endif
 
     std::size_t nbytes = h_in.size()*sizeof(T);
     T* d_in = (T*)The_Device_Arena()->alloc(nbytes);
@@ -88,10 +99,12 @@ void main_main ()
     Scan::InclusiveSum(N, d_in, d_out);
     Scan::ExclusiveSum(N, d_in, d_out);
     Gpu::synchronize();
+#ifdef AMREX_USE_CUDA
     thrust::inclusive_scan(Gpu::The_ThrustCachedPolicy(),
                            d_in, d_in+N, d_out);
     thrust::exclusive_scan(Gpu::The_ThrustCachedPolicy(),
                            d_in, d_in+N, d_out);
+#endif
 
     {
         BL_PROFILE("amrex::inclusive_scan");
@@ -107,6 +120,7 @@ void main_main ()
     }
     amrex::Gpu::dtoh_memcpy(h_exclusive_amrex.data(), d_out, nbytes);
 
+#ifdef AMREX_USE_CUDA
     {
         BL_PROFILE("thrust::inclusive_scan");
         thrust::inclusive_scan(Gpu::The_ThrustCachedPolicy(),
@@ -122,18 +136,44 @@ void main_main ()
         Gpu::synchronize();
     }
     amrex::Gpu::dtoh_memcpy(h_exclusive_thrust.data(), d_out, nbytes);
+#endif
+
+    {
+        BL_PROFILE("std::partial_sum-inclusive");
+        // With C+17, we could use std::inclusive_scan
+        std::partial_sum(h_in.begin(), h_in.end(), h_inclusive_cpu.begin());
+    }
+
+    {
+        BL_PROFILE("std::partial_sum-exclusive");
+        // With C+17, we could use std::exclusive_scan
+        h_exclusive_cpu[0] = 0;
+        std::partial_sum(h_in.begin(), h_in.end()-1, h_exclusive_cpu.begin()+1);
+    }
 
     for (int i = 0; i < N; ++i) {
-        if (h_inclusive_thrust[i] != h_inclusive_amrex[i]) {
-            amrex::Print() << "i = " << i << ", thrust = " << h_inclusive_thrust[i]
-                           << ", amrex = " << h_inclusive_amrex[i] << "\n"; 
-            amrex::Abort("inclusive scan failed");
+        if (h_inclusive_cpu[i] != h_inclusive_amrex[i]) {
+            amrex::Print() << "i = " << i << ", std = " << h_inclusive_cpu[i]
+                           << ", amrex = " << h_inclusive_amrex[i] << "\n";
+            amrex::Abort("amrex inclusive scan failed");
         }
-        if (h_exclusive_thrust[i] != h_exclusive_amrex[i]) {
-            amrex::Print() << "i = " << i << ", thrust = " << h_exclusive_thrust[i]
-                           << ", amrex = " << h_exclusive_amrex[i] << "\n"; 
-            amrex::Abort("exclusive scan failed");
+        if (h_exclusive_cpu[i] != h_exclusive_amrex[i]) {
+            amrex::Print() << "i = " << i << ", std = " << h_exclusive_cpu[i]
+                           << ", amrex = " << h_exclusive_amrex[i] << "\n";
+            amrex::Abort("amrex exclusive scan failed");
         }
+#ifdef AMREX_USE_CUDA
+        if (h_inclusive_cpu[i] != h_inclusive_thrust[i]) {
+            amrex::Print() << "i = " << i << ", std = " << h_inclusive_cpu[i]
+                           << ", thrust = " << h_inclusive_thrust[i] << "\n";
+            amrex::Abort("thrust inclusive scan failed");
+        }
+        if (h_exclusive_cpu[i] != h_exclusive_thrust[i]) {
+            amrex::Print() << "i = " << i << ", std = " << h_exclusive_cpu[i]
+                           << ", thrust = " << h_exclusive_thrust[i] << "\n";
+            amrex::Abort("thrust exclusive scan failed");
+        }
+#endif
     }
 
     The_Device_Arena()->free(d_in);
