d555fcad06bfc4e7d3fb076a819064664deb92a4
WeiqunZhang <WeiqunZhang@lbl.gov>
2020-05-05 12:46:33 -0700

    Move prefetch from AmrLevel WritePlotfile to VisMF::Write.  Avoid unn… (#843)
    * Move prefetch from AmrLevel WritePlotfile to VisMF::Write.  Avoid unnecessary work if no ghost cells.
    
    * If not manged memory, we do not need to start MFIter

--- Src/Amr/AMReX_AmrLevel.cpp
+++ Src/Amr/AMReX_AmrLevel.cpp
@@ -347,16 +347,12 @@ AmrLevel::writePlotFile (const std::string& dir,
     MultiFab::Copy(plotMF,factory->getVolFrac(),0,cnt,1,nGrow);
 #endif
 
-    amrex::prefetchToHost(plotMF);
-
     //
     // Use the Full pathname when naming the MultiFab.
     //
     std::string TheFullPath = FullPath;
     TheFullPath += BaseName;
     VisMF::Write(plotMF,TheFullPath,how,true);
-
-    amrex::prefetchToDevice(plotMF);
 
     levelDirectoryCreated = false;  // ---- now that the plotfile is finished
 }
