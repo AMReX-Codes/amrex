fc08afe8327f96d507cda7f9f9a6423787381b2b
Andrew Myers <atmyers@lbl.gov>
2020-05-05 14:35:06 -0700

    For cuda > 10, use cudaLaunchHostFunc instead of cudaStreamAddCallback, which has been marked as deprecated (#844)

--- Src/Base/AMReX_GpuAsyncArray.cpp
+++ Src/Base/AMReX_GpuAsyncArray.cpp
@@ -11,7 +11,11 @@
 extern "C" {
 AMREX_HIP_OR_CUDA(
          void HIPRT_CB  amrex_asyncarray_delete ( hipStream_t stream,  hipError_t error, void* p),
+#if ( defined(__CUDACC__) && (__CUDACC_VER_MAJOR__ >= 10) )
+         void CUDART_CB amrex_asyncarray_delete (void* p))
+#else
          void CUDART_CB amrex_asyncarray_delete (cudaStream_t stream, cudaError_t error, void* p))
+#endif
     {
         void** pp = (void**)p;
         void* dp = pp[0];
--- Src/Base/AMReX_GpuElixir.cpp
+++ Src/Base/AMReX_GpuElixir.cpp
@@ -20,7 +20,11 @@ namespace {
 extern "C" {
 AMREX_HIP_OR_CUDA(
          void HIPRT_CB  amrex_elixir_delete ( hipStream_t stream,  hipError_t error, void* p),
+#if ( defined(__CUDACC__) && (__CUDACC_VER_MAJOR__ >= 10) )         
+         void CUDART_CB amrex_elixir_delete (void* p))
+#else
          void CUDART_CB amrex_elixir_delete (cudaStream_t stream, cudaError_t error, void* p))
+#endif
     {
         void** pp = (void**)p;
         void* d = pp[0];
@@ -48,8 +52,13 @@ Elixir::clear () noexcept
             AMREX_HIP_OR_CUDA(
                 AMREX_HIP_SAFE_CALL ( hipStreamAddCallback(Gpu::gpuStream(),
                                                            amrex_elixir_delete, p, 0));,
+#if ( defined(__CUDACC__) && (__CUDACC_VER_MAJOR__ >= 10) )
+                AMREX_CUDA_SAFE_CALL(cudaLaunchHostFunc(Gpu::gpuStream(),
+                                                        amrex_elixir_delete, p)););
+#else
                 AMREX_CUDA_SAFE_CALL(cudaStreamAddCallback(Gpu::gpuStream(),
                                                            amrex_elixir_delete, p, 0)););
+#endif
             Gpu::callbackAdded();
 #elif defined(AMREX_USE_DPCPP)
             // xxxxx DPCPP todo
