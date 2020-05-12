91030da0318be1ddc0d1600760cc6531a6992568
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-05-11 17:27:45 -0700

    add inline namespace literals and use in CNS and Heat Equation tutorials

--- Tutorials/GPU/CNS/Source/CNS_parm.cpp
+++ Tutorials/GPU/CNS/Source/CNS_parm.cpp
@@ -3,7 +3,7 @@
 
 void Parm::Initialize ()
 {
-    constexpr amrex::Real Ru = 8.31451e7;
-    cv = Ru / (eos_mu * (eos_gamma-1.0));
-    cp = eos_gamma * Ru / (eos_mu * (eos_gamma-1.0));
+    constexpr amrex::Real Ru = 8.31451e7_rt;
+    cv = Ru / (eos_mu * (eos_gamma-1.0_rt));
+    cp = eos_gamma * Ru / (eos_mu * (eos_gamma-1.0_rt));
 }
