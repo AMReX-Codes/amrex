af68380aa4b23ce7f01223d696aafa82cfa5e06c
mic84 <mrosso@lbl.gov>
2020-06-01 12:27:45 -0700

    CMake: improve handling of options (#946)
    * CMake: move general setup operations out of AMReX_Options.cmake
    
    * CMake: better handling of option inter-dependencies via cmake_dependent_option
    
    * CMake: refactor AMReX_Options.cmake
    
    * Update Tools/CMake/AMReX_Options.cmake
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * Update CMakeLists.txt
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    * Update Tools/CMake/AMReX_Options.cmake
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- Tools/CMake/AMReX_Options.cmake
+++ Tools/CMake/AMReX_Options.cmake
@@ -6,29 +6,14 @@
 ###############################################
 include_guard(GLOBAL)
 
-#
-# Include module
-#
 include(CMakeDependentOption)
-
 
 #
 # Define a macro to check the value of the inputs integer options
 #
 macro (print_option var)
-   message ( STATUS "   ${var} = ${${var}}")
+   message( STATUS "   ${var} = ${${var}}")
 endmacro ()
-
-#
-# Check if CMAKE_BUILD_TYPE is given. If not, use default
-#
-if ( NOT CMAKE_BUILD_TYPE )
-   message(STATUS "Setting build type to Release as none was specified.")
-   set( CMAKE_BUILD_TYPE Release )
-else ()
-   message(STATUS "Build type set by user to '${CMAKE_BUILD_TYPE}'.")
-endif()
-
 
 #
 # Populate the cache and check the value of the user-definable options
@@ -39,12 +24,21 @@ message(STATUS "Configuring AMReX with the following options: ")
 #
 # This is the option to enable/disable xSDK mode
 #
+# To handle both XDSK options and correponding plain AMReX options,
+# we make use of policy CMP0077 introduced as a default in CMake 3.13
+# Under policy CMP0077, normal variables prevent option()
+# to set internal variables of the same name.
+# Example: if XSDK mode is ON and XSDK_ENABLE_Fortran=ON, a normal
+# variable ENABLE_FORTRAN will be created and set to ON.
+# This will stop the subsequent option( ENABLE_FORTRAN "Enable Fortran language" ON )
+# from being executed and no entry ENABLE_FORTRAN will be created in the cache
+#
 option( USE_XSDK_DEFAULTS "Enable xSDK mode"  OFF )
 print_option( USE_XSDK_DEFAULTS )
 
 
 #
-# Option to control the type of library build: static vs shared
+# Option to control the type of library: static vs shared  ===================
 #
 if ( USE_XSDK_DEFAULTS )
    option( BUILD_SHARED_LIBS "Build AMReX shared library" ON )
@@ -53,71 +47,81 @@ else ()
 endif ()
 print_option( BUILD_SHARED_LIBS )
 
-#
-# Print out info on install path
-#
-print_option( CMAKE_INSTALL_PREFIX )
-
 
 #
-# Option to control if Fortran must be enabled
+# Option to control if Fortran must be enabled  ==============================
 #
 if ( USE_XSDK_DEFAULTS )
    option( XSDK_ENABLE_Fortran "Enable Fortran language" OFF )
-   set( ENABLE_FORTRAN ${XSDK_ENABLE_Fortran} CACHE INTERNAL "")
+   set( ENABLE_FORTRAN ${XSDK_ENABLE_Fortran} )
    print_option(XSDK_ENABLE_Fortran)
-else()
-   option( ENABLE_FORTRAN "Enable Fortran language" ON )
-   print_option( ENABLE_FORTRAN )
 endif ()
 
+option( ENABLE_FORTRAN "Enable Fortran language" ON )
+print_option( ENABLE_FORTRAN )
 
+#
+# Option to control precision of the build  ==================================
+#
+if ( USE_XSDK_DEFAULTS )
+   set( XSDK_PRECISION "DOUBLE" CACHE STRING "Precision:<SINGLE,DOUBLE>" )
+   if ( XSDK_PRECISION STREQUAL "DOUBLE" )
+      set( ENABLE_DP ON )
+      set( ENABLE_DP_PARTICLES ON )
+   elseif ( XSDK_PRECISION STREQUAL "SINGLE" )
+      set( ENABLE_DP OFF )
+      set( ENABLE_DP_PARTICLES OFF )
+   else ()
+      message( FATAL_ERROR "\nUnsupported precision ${XSDK_PRECISION}\n" )
+   endif ()
+   print_option( XSDK_PRECISION )
+endif ()
+
+option( ENABLE_DP "Enable double precision" ON )
+print_option( ENABLE_DP )
+
+#
+# Dimensionality of the build  ===============================================
+#
 set (DIM 3 CACHE STRING "Dimension of AMReX build")
-if ( (${DIM} GREATER 3) OR (${DIM} LESS 1) )
-   message ( FATAL_ERROR "DIM must be either 1, 2 or 3.")
+if ( (DIM GREATER 3) OR (DIM LESS 1) )
+   message( FATAL_ERROR "DIM must be either 1, 2 or 3.")
 endif ()
 print_option( DIM )
 
-option( ENABLE_PIC "Build position-independent code" OFF)
-print_option( ENABLE_PIC )
-
+#
+# Programming models  ========================================================
+#
 option( ENABLE_MPI  "Enable MPI"  ON)
 print_option( ENABLE_MPI )
 
 option( ENABLE_OMP  "Enable OpenMP" OFF)
 print_option( ENABLE_OMP )
 
+option( ENABLE_CUDA "Enable GPU support via CUDA" OFF )
+print_option( ENABLE_CUDA )
 
-if ( USE_XSDK_DEFAULTS )
-   set ( XSDK_PRECISION "DOUBLE" CACHE STRING "Precision:<SINGLE,DOUBLE>" )
-   if ( "${XSDK_PRECISION}" STREQUAL "DOUBLE" )
-      set ( ENABLE_DP ON )
-   elseif ( "${XSDK_PRECISION}" STREQUAL "SINGLE" )
-      set ( ENABLE_DP OFF )
-   else ()
-      message ( WARNING "Unsupported precision ${XSDK_PRECISION}: defaulting to DOUBLE" )
-      set ( XSDK_PRECISION "DOUBLE" CACHE STRING "Precision:<SINGLE,DOUBLE>" )
-      set ( ENABLE_DP ON )
-   endif ()
-   print_option( XSDK_PRECISION )
-else ()
-   option( ENABLE_DP "Enable double precision" ON )
-   print_option( ENABLE_DP )
+option( ENABLE_ACC  "Enable GPU support via OpenACC" OFF )
+print_option( ENABLE_ACC )
+
+if (ENABLE_CUDA OR ENABLE_ACC)
+   set(GPUS_PER_SOCKET "IGNORE" CACHE STRING "Number of GPUs per socket" )
+   print_option(GPUS_PER_SOCKET)
+
+   set(GPUS_PER_NODE "IGNORE" CACHE STRING "Number of GPUs per node" )
+   print_option(GPUS_PER_NODE)
 endif ()
 
 
 #
-# AMReX components selection
+# AMReX components selection  ================================================
 #
 option( ENABLE_EB "Build EB Code" OFF )
 print_option(ENABLE_EB)
 
-if (ENABLE_FORTRAN)
-   option( ENABLE_FORTRAN_INTERFACES "Build Fortran API" OFF )
-   print_option(ENABLE_FORTRAN_INTERFACES)
-else ()
-   set(ENABLE_FORTRAN_INTERFACES OFF CACHE INTERNAL "Build Fortran API")
-endif ()
+cmake_dependent_option( ENABLE_FORTRAN_INTERFACES "Build Fortran API" OFF
+   "ENABLE_FORTRAN" OFF )
+print_option(ENABLE_FORTRAN_INTERFACES)
 
 option( ENABLE_LINEAR_SOLVERS  "Build AMReX Linear solvers" ON )
 print_option( ENABLE_LINEAR_SOLVERS )
@@ -128,57 +132,49 @@ print_option( ENABLE_AMRDATA )
 option( ENABLE_PARTICLES "Build particle classes" OFF)
 print_option( ENABLE_PARTICLES )
 
-if ( ENABLE_PARTICLES )
-   if ( USE_XSDK_DEFAULTS )
-      if ( "${XSDK_PRECISION}" STREQUAL "DOUBLE" )
-	 set ( ENABLE_DP_PARTICLES ON )
-      elseif ( "${XSDK_PRECISION}" STREQUAL "SINGLE" )
-	 set ( ENABLE_DP_PARTICLES OFF )
-      endif ()
-   else ()
-      option( ENABLE_DP_PARTICLES "Enable double-precision particle data" ON )
-      print_option( ENABLE_DP_PARTICLES )
-   endif ()
-endif ()
+cmake_dependent_option( ENABLE_DP_PARTICLES "Enable double-precision particle data" ON
+   "ENABLE_PARTICLES" OFF )
+print_option( ENABLE_DP_PARTICLES )
 
+
+#
+# External packages  =========================================================
+#
+
+# sensei
 option( ENABLE_SENSEI_INSITU "Enable SENSEI in situ infrastructure" OFF )
 print_option( ENABLE_SENSEI_INSITU )
 
+# Conduit (requires CONDUIT_DIR)
+option( ENABLE_CONDUIT "Enable Conduit support" OFF )
+print_option( ENABLE_CONDUIT )
 
-#
-# Conduit Support (for features in Src/Extern/Conduit)
-# Note: ENABLE_CONDUIT = ON, requires CONDUIT_DIR.
-#
-option ( ENABLE_CONDUIT "Enable Conduit support" OFF )
-print_option ( ENABLE_CONDUIT )
+# Ascent
+cmake_dependent_option( ENABLE_ASCENT "Enable Ascent support" OFF
+   "ENABLE_CONDUIT" OFF )
+print_option( ENABLE_ASCENT )
 
-if (ENABLE_CONDUIT)
-   option ( ENABLE_ASCENT "Enable Ascent support" OFF )
-   print_option ( ENABLE_ASCENT )
-endif ()
-
-
-#
-# External packages
-#
-option(ENABLE_SUNDIALS "Enable SUNDIALS4 interfaces" OFF)
+# SUNDIALS
+cmake_dependent_option(ENABLE_SUNDIALS "Enable SUNDIALS4 interfaces" OFF
+   "ENABLE_FORTRAN_INTERFACES" OFF)
 print_option(ENABLE_SUNDIALS)
 
 # Hypre
-if (ENABLE_LINEAR_SOLVERS)
-   option(ENABLE_HYPRE "Enable Hypre interfaces" OFF)
-   print_option(ENABLE_HYPRE)
-   option(ENABLE_PETSC "Enable PETSc interfaces" OFF)
-   print_option(ENABLE_PETSC)
-else ()
-   set(ENABLE_HYPRE OFF CACHE INTERNAL "Enable Hypre interfaces")
-   set(ENABLE_PETSC OFF CACHE INTERNAL "Enable PETSc interfaces")
-endif ()
+cmake_dependent_option(ENABLE_HYPRE "Enable Hypre interfaces" OFF
+   "ENABLE_FORTRAN;ENABLE_LINEAR_SOLVERS" OFF)
+print_option(ENABLE_HYPRE)
 
+# PETSc
+cmake_dependent_option(ENABLE_PETSC "Enable PETSc interfaces" OFF
+   "ENABLE_FORTRAN;ENABLE_LINEAR_SOLVERS" OFF )
+print_option(ENABLE_PETSC)
 
 #
-# Compilation options
+# Miscellanoues options  =====================================================
 #
+option( ENABLE_PIC "Build position-independent code" OFF)
+print_option( ENABLE_PIC )
+
 option(ENABLE_FPE "Enable Floating Point Exceptions checks" OFF)
 print_option( ENABLE_FPE )
 
@@ -192,81 +188,50 @@ print_option( ENABLE_ASSERTIONS )
 
 
 #
-# Profiling options
+# Profiling options  =========================================================
 #
 option( ENABLE_BASE_PROFILE "Enable basic profiling" OFF )
 print_option( ENABLE_BASE_PROFILE )
 
-option( ENABLE_TINY_PROFILE "NOT ENABLE_BASE_DEBUG" OFF)
+cmake_dependent_option( ENABLE_TINY_PROFILE "Enable tiny profiling" OFF
+   "NOT ENABLE_BASE_PROFILE" OFF)
 print_option( ENABLE_TINY_PROFILE )
 
-option( ENABLE_TRACE_PROFILE "Enable trace-profiling" OFF )
+cmake_dependent_option( ENABLE_TRACE_PROFILE "Enable trace-profiling" OFF
+   "ENABLE_BASE_PROFILE" OFF)
 print_option( ENABLE_TRACE_PROFILE )
 
 option( ENABLE_MEM_PROFILE   "Enable memory profiling" OFF )
 print_option( ENABLE_MEM_PROFILE )
 
-option( ENABLE_COMM_PROFILE  "Enable communicator-profiling" OFF )
+cmake_dependent_option( ENABLE_COMM_PROFILE  "Enable communicator-profiling" OFF
+   "ENABLE_BASE_PROFILE" OFF)
 print_option( ENABLE_COMM_PROFILE )
 
 option( ENABLE_BACKTRACE "Enable backtracing" OFF)
 print_option( ENABLE_BACKTRACE )
 
-option( ENABLE_PROFPARSER "Enable profile parser" OFF)
+cmake_dependent_option(ENABLE_PROFPARSER "Enable profile parser" OFF
+   "ENABLE_BASE_PROFILE;ENABLE_TRACE_PROFILE;ENABLE_AMRDATA" OFF)
 print_option( ENABLE_PROFPARSER )
 
-set ( TP_PROFILE "None" CACHE STRING "Third-party profiling options:<CRAYPAT,FORGE,VTUNE>")
+set(TP_PROFILE_VALUES None CRAYPAT FORGE VTUNE)
+set(TP_PROFILE None CACHE STRING "Third-party profiling options: <CRAYPAT,FORGE,VTUNE>")
+set_property(CACHE TP_PROFILE PROPERTY STRINGS ${TP_PROFILE_VALUES})
+if(NOT TP_PROFILE IN_LIST TP_PROFILE_VALUES)
+    message(FATAL_ERROR "TP_PROFILE (${TP_PROFILE}) must be one of ${TP_PROFILE_VALUES}")
+endif()
 print_option( TP_PROFILE )
 
-
-# Modify the profiling options if needed ( because of dependencies between
-# the options )
-if (ENABLE_PROFPARSER)
-   set (ENABLE_BASE_PROFILE ON)
-   set (ENABLE_TRACE_PROFILE ON)
-   set (ENABLE_AMRDATA ON)
-endif ()
-
-if (ENABLE_TRACE_PROFILE OR ENABLE_COMM_PROFILE)
-   set (ENABLE_BASE_PROFILE ON)
-endif ()
-
-if (ENABLE_BASE_PROFILE)
-   set (ENABLE_TINY_PROFILE OFF)
-endif()
-
 # Check profile options
-if ( NOT ( ${CMAKE_C_COMPILER_ID} MATCHES "Intel" ) AND
-      ( ${TP_PROFILE} MATCHES "VTUNE") )
-   message ( WARNING "VTUNE cannot be used with ${CMAKE_C_COMPILER_ID} compiler: ignoring TP_PROFILE" )
-   set ( TP_PROFILE "")
+if ( NOT ( CMAKE_C_COMPILER_ID STREQUAL "Intel" ) AND
+      ( TP_PROFILE STREQUAL "VTUNE") )
+   message( FATAL_ERROR "VTUNE cannot be used with ${CMAKE_C_COMPILER_ID} compiler" )
 endif ()
 
-if (  ( ( ${TP_PROFILE} MATCHES "CRAYPAT" ) OR
-        ( ${TP_PROFILE} MATCHES "FORGE"   ) OR
-        ( ${TP_PROFILE} MATCHES "VTUNE"   )   ) AND
+if (  ( ( TP_PROFILE STREQUAL "CRAYPAT" ) OR
+        ( TP_PROFILE STREQUAL "FORGE"   ) OR
+        ( TP_PROFILE STREQUAL "VTUNE"   )   ) AND
      (ENABLE_BASE_PROFILE OR ENABLE_TINY_PROFILE) )
-   message (WARNING "This configuration should only be used to profile BL_PROFILE!")
+   message(WARNING "This configuration should only be used to profile BL_PROFILE!")
 endif()
-
-#
-# GPU options
-#
-
-# More CUDA options in AMReX_SetupCUDA
-option( ENABLE_CUDA "Enable GPU support via CUDA" OFF )
-print_option( ENABLE_CUDA )
-
-option( ENABLE_ACC  "Enable GPU support via OpenACC" OFF )
-print_option( ENABLE_ACC )
-
-# GNU shared options
-if (ENABLE_CUDA OR ENABLE_ACC)
-   set(GPUS_PER_SOCKET "IGNORE" CACHE STRING
-      "Number of GPUs per socket" )
-   print_option(GPUS_PER_SOCKET)
-
-   set(GPUS_PER_NODE "IGNORE" CACHE STRING
-      "Number of GPUs per node" )
-   print_option(GPUS_PER_NODE)
-endif ()
