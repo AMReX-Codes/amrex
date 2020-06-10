aeba5d36a4bba025b2626737d3481c6db3f15105
atmyers <atmyers2@gmail.com>
2020-06-10 13:17:59 -0700

    update test

--- Tests/Particles/ParallelContext/main.cpp
+++ Tests/Particles/ParallelContext/main.cpp
@@ -26,11 +26,11 @@ void get_position_unit_cell(Real* r, const IntVect& nppc, int i_part)
 #else
     int nz = 1;
 #endif
-    
+
     int ix_part = i_part/(ny * nz);
     int iy_part = (i_part % (ny * nz)) % ny;
     int iz_part = (i_part % (ny * nz)) / ny;
-    
+
     r[0] = (0.5+ix_part)/nx;
     r[1] = (0.5+iy_part)/ny;
     r[2] = (0.5+iz_part)/nz;
@@ -42,11 +42,10 @@ class TestParticleContainer
 
 public:
 
-    TestParticleContainer (const Vector<amrex::Geometry>            & a_geom,
-                           const Vector<amrex::DistributionMapping> & a_dmap,
-                           const Vector<amrex::BoxArray>            & a_ba,
-                           const Vector<amrex::IntVect>             & a_rr)
-        : amrex::ParticleContainer<NSR, NSI, NAR, NAI>(a_geom, a_dmap, a_ba, a_rr)
+    TestParticleContainer (const amrex::Geometry& a_geom,
+                           const amrex::DistributionMapping& a_dmap,
+                           const amrex::BoxArray& a_ba)
+        : amrex::ParticleContainer<NSR, NSI, NAR, NAI>(a_geom, a_dmap, a_ba)
     {
         for (int i = 0; i < num_runtime_real; ++i)
         {
@@ -61,29 +60,20 @@ public:
     void RedistributeLocal ()
     {
         const int lev_min = 0;
-        const int lev_max = finestLevel();
+        const int lev_max = 0;
         const int nGrow = 0;
         const int local = 1;
         Redistribute(lev_min, lev_max, nGrow, local);
     }
 
-    void RedistributeGlobal ()
-    {
-        const int lev_min = 0;
-        const int lev_max = finestLevel();
-        const int nGrow = 0;
-        const int local = 0;
-        Redistribute(lev_min, lev_max, nGrow, local);
-    }
-    
     void InitParticles (const amrex::IntVect& a_num_particles_per_cell)
     {
         BL_PROFILE("InitParticles");
-        
+
         const int lev = 0;  // only add particles on level 0
         const Real* dx = Geom(lev).CellSize();
         const Real* plo = Geom(lev).ProbLo();
-    
+
         const int num_ppc = AMREX_D_TERM( a_num_particles_per_cell[0],
                                          *a_num_particles_per_cell[1],
                                          *a_num_particles_per_cell[2]);
@@ -104,10 +94,10 @@ public:
                 for (int i_part=0; i_part<num_ppc;i_part++) {
                     Real r[3];
                     get_position_unit_cell(r, a_num_particles_per_cell, i_part);
-                
+
                     ParticleType p;
                     p.id()  = ParticleType::NextID();
-                    p.cpu() = ParallelDescriptor::MyProc();                
+                    p.cpu() = ParallelDescriptor::MyProc();
                     p.pos(0) = plo[0] + (iv[0] + r[0])*dx[0];
 #if AMREX_SPACEDIM > 1
                     p.pos(1) = plo[1] + (iv[1] + r[1])*dx[1];
@@ -115,10 +105,10 @@ public:
 #if AMREX_SPACEDIM > 2
                     p.pos(2) = plo[2] + (iv[2] + r[2])*dx[2];
 #endif
-                    
+
                     for (int i = 0; i < NSR; ++i) p.rdata(i) = p.id();
                     for (int i = 0; i < NSI; ++i) p.idata(i) = p.id();
-                    
+
                     host_particles.push_back(p);
                     for (int i = 0; i < NAR; ++i)
                         host_real[i].push_back(p.id());
@@ -130,16 +120,16 @@ public:
                         host_runtime_int[i].push_back(p.id());
                 }
             }
-        
+
             auto& particle_tile = DefineAndReturnParticleTile(lev, mfi.index(), mfi.LocalTileIndex());
             auto old_size = particle_tile.GetArrayOfStructs().size();
             auto new_size = old_size + host_particles.size();
             particle_tile.resize(new_size);
-            
+
             Gpu::copy(Gpu::hostToDevice,
                       host_particles.begin(),
                       host_particles.end(),
-                      particle_tile.GetArrayOfStructs().begin() + old_size);        
+                      particle_tile.GetArrayOfStructs().begin() + old_size);
 
             auto& soa = particle_tile.GetStructOfArrays();
             for (int i = 0; i < NAR; ++i)
@@ -186,14 +176,14 @@ public:
             const Geometry& geom = Geom(lev);
             const auto dx = Geom(lev).CellSizeArray();
             auto& plev  = GetParticles(lev);
-        
+
             for(MFIter mfi = MakeMFIter(lev); mfi.isValid(); ++mfi)
             {
                 int gid = mfi.index();
-                int tid = mfi.LocalTileIndex();            
+                int tid = mfi.LocalTileIndex();
                 auto& ptile = plev[std::make_pair(gid, tid)];
                 auto& aos   = ptile.GetArrayOfStructs();
-                ParticleType* pstruct = &(aos[0]);            
+                ParticleType* pstruct = &(aos[0]);
                 const size_t np = aos.numParticles();
 
                 if (do_random == 0)
@@ -209,7 +199,7 @@ public:
                         p.pos(2) += move_dir[2]*dx[2];
 #endif
                     });
-                }            
+                }
                 else
                 {
                     AMREX_FOR_1D ( np, i,
@@ -232,9 +222,9 @@ public:
     void checkAnswer () const
     {
         BL_PROFILE("TestParticleContainer::checkAnswer");
-        
+
         AMREX_ALWAYS_ASSERT(OK());
-        
+
         int num_rr = NumRuntimeRealComps();
         int num_ii = NumRuntimeIntComps();
 
@@ -243,15 +233,15 @@ public:
             const Geometry& geom = Geom(lev);
             const auto dx = Geom(lev).CellSizeArray();
             auto& plev  = GetParticles(lev);
-            
+
             for(MFIter mfi = MakeMFIter(lev); mfi.isValid(); ++mfi)
             {
                 int gid = mfi.index();
-                int tid = mfi.LocalTileIndex();            
+                int tid = mfi.LocalTileIndex();
                 auto& ptile = plev.at(std::make_pair(gid, tid));
                 const auto ptd = ptile.getConstParticleTileData();
                 const size_t np = ptile.numParticles();
-                
+
                 AMREX_FOR_1D ( np, i,
                 {
                     for (int j = 0; j < NSR; ++j)
@@ -293,7 +283,6 @@ struct TestParams
     IntVect move_dir;
     int do_random;
     int nsteps;
-    int nlevs;
     int do_regrid;
     int sort;
 };
@@ -318,9 +307,8 @@ void get_test_params(TestParams& params, const std::string& prefix)
     pp.get("num_ppc", params.num_ppc);
     pp.get("is_periodic", params.is_periodic);
     pp.get("move_dir", params.move_dir);
-    pp.get("do_random", params.do_random);    
+    pp.get("do_random", params.do_random);
     pp.get("nsteps", params.nsteps);
-    pp.get("nlevs", params.nlevs);
     pp.get("do_regrid", params.do_regrid);
     pp.query("num_runtime_real", num_runtime_real);
     pp.query("num_runtime_int", num_runtime_int);
@@ -333,20 +321,21 @@ void testRedistribute ()
 {
     BL_PROFILE("testRedistribute");
 
+    AMREX_ALWAYS_ASSERT(AMREX_SPACEDIM == 3);
+
+    // we always make two subcommunicators.
+    // one takes the left half of the domain in direction 0, the other the right.
     int rank_n = ParallelContext::NProcsSub();
     int myproc = ParallelContext::MyProcSub();
-    int task_me = myproc / 2;
+    int task_me = myproc / (amrex::max(rank_n, 2) / 2);
 
     MPI_Comm new_comm;
     MPI_Comm_split(ParallelContext::CommunicatorSub(), task_me, myproc, &new_comm);
-    
+
     const int io_rank = 0;
     ParallelContext::push(new_comm, task_me, io_rank);
-    
+
     {
-
-        amrex::Print() << ParallelContext::NProcsSub() << "\n";
-
         TestParams params;
         get_test_params(params, "redistribute");
 
@@ -354,64 +343,83 @@ void testRedistribute ()
         for (int i = 0; i < BL_SPACEDIM; i++)
             is_per[i] = params.is_periodic;
 
-        Vector<IntVect> rr(params.nlevs-1);
-        for (int lev = 1; lev < params.nlevs; lev++)
-            rr[lev-1] = IntVect(D_DECL(2,2,2));
+        // Each comm gets a different domain
+        IntVect hs = params.size / 2;
 
         RealBox real_box;
         for (int n = 0; n < BL_SPACEDIM; n++)
         {
-            real_box.setLo(n, 0.0);
-            real_box.setHi(n, params.size[n]);
+            Real physlo = (n == 0) ? task_me*hs[n] : 0.0;
+            Real physhi = (n == 0) ? task_me*hs[n] + hs[n] : params.size[n];
+            real_box.setLo(n, physlo);
+            real_box.setHi(n, physhi);
         }
 
-        IntVect domain_lo(AMREX_D_DECL(0, 0, 0));
-        IntVect domain_hi(AMREX_D_DECL(params.size[0]-1,params.size[1]-1,params.size[2]-1));
+        IntVect domain_lo(AMREX_D_DECL(task_me*hs[0], 0, 0));
+        IntVect domain_hi(AMREX_D_DECL(task_me*hs[0] + hs[0] - 1,
+                                       params.size[1] - 1,
+                                       params.size[2] - 1));
         const Box base_domain(domain_lo, domain_hi);
 
-        Vector<Geometry> geom(params.nlevs);
-        geom[0].define(base_domain, &real_box, CoordSys::cartesian, is_per);
-        for (int lev = 1; lev < params.nlevs; lev++) {
-            geom[lev].define(amrex::refine(geom[lev-1].Domain(), rr[lev-1]),
-                             &real_box, CoordSys::cartesian, is_per);
-        }
+        Geometry geom;
+        geom.define(base_domain, &real_box, CoordSys::cartesian, is_per);
 
-        Vector<BoxArray> ba(params.nlevs);
-        Vector<DistributionMapping> dm(params.nlevs);
-        IntVect lo = IntVect(D_DECL(0, 0, 0));
-        IntVect size = params.size;
-        for (int lev = 0; lev < params.nlevs; ++lev)
+        BoxArray ba;
+        ba.define(base_domain);
+        ba.maxSize(params.max_grid_size);
+
+        DistributionMapping dm;
+        dm.define(ba, ParallelContext::NProcsSub());
+
+        // by doing it this way, we have each subcommunicator making
+        // separate Redisitribute calls at the same time.
+        if (task_me == 0)
         {
-            ba[lev].define(Box(lo, lo+params.size-1));
-            ba[lev].maxSize(params.max_grid_size);
-            dm[lev].define(ba[lev], ParallelContext::NProcsSub());
-            lo += size/2;
-            size *= 2;
-        }
+            TestParticleContainer pc(geom, dm, ba);
 
-        TestParticleContainer pc(geom, dm, ba, rr);
+            int npc = params.num_ppc;
+            IntVect nppc = IntVect(AMREX_D_DECL(npc, npc, npc));
 
-        int npc = params.num_ppc;
-        IntVect nppc = IntVect(AMREX_D_DECL(npc, npc, npc));
+            pc.InitParticles(nppc);
 
-        pc.InitParticles(nppc);
-
-        pc.checkAnswer();
-
-        auto np_old = pc.TotalNumberOfParticles();
-
-        for (int i = 0; i < params.nsteps; ++i)
-        {
-            pc.moveParticles(params.move_dir, params.do_random);
-            pc.RedistributeLocal();
-            if (params.sort) pc.SortParticlesByCell();
             pc.checkAnswer();
+
+            auto np_old = pc.TotalNumberOfParticles();
+
+            for (int i = 0; i < params.nsteps; ++i)
+            {
+                pc.moveParticles(params.move_dir, params.do_random);
+                pc.RedistributeLocal();
+                if (params.sort) pc.SortParticlesByCell();
+                pc.checkAnswer();
+            }
+
+            if (geom.isAllPeriodic()) AMREX_ALWAYS_ASSERT(np_old == pc.TotalNumberOfParticles());
         }
 
-        amrex::Print() << np_old << " " << pc.TotalNumberOfParticles() << "\n";
+        if (task_me == 1)
+        {
+            TestParticleContainer pc(geom, dm, ba);
 
-        if (geom[0].isAllPeriodic()) AMREX_ALWAYS_ASSERT(np_old == pc.TotalNumberOfParticles());
+            int npc = params.num_ppc;
+            IntVect nppc = IntVect(AMREX_D_DECL(npc, npc, npc));
 
+            pc.InitParticles(nppc);
+
+            pc.checkAnswer();
+
+            auto np_old = pc.TotalNumberOfParticles();
+
+            for (int i = 0; i < params.nsteps; ++i)
+            {
+                pc.moveParticles(params.move_dir, params.do_random);
+                pc.RedistributeLocal();
+                if (params.sort) pc.SortParticlesByCell();
+                pc.checkAnswer();
+            }
+
+            if (geom.isAllPeriodic()) AMREX_ALWAYS_ASSERT(np_old == pc.TotalNumberOfParticles());
+        }
     }
 
     ParallelContext::pop();
