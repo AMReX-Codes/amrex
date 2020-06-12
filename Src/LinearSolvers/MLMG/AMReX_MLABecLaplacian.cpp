ea7ee960692f24d6a4360ee26de05de1ab2ccd6d
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-06-11 21:44:22 -0700

    cell-centered ABecLaplacian linear solver for overset

--- Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
@@ -15,6 +15,16 @@ MLABecLaplacian::MLABecLaplacian (const Vector<Geometry>& a_geom,
     define(a_geom, a_grids, a_dmap, a_info, a_factory);
 }
 
+MLABecLaplacian::MLABecLaplacian (const Vector<Geometry>& a_geom,
+                                  const Vector<BoxArray>& a_grids,
+                                  const Vector<DistributionMapping>& a_dmap,
+                                  const Vector<iMultiFab const*>& a_overset_mask,
+                                  const LPInfo& a_info,
+                                  const Vector<FabFactory<FArrayBox> const*>& a_factory)
+{
+    define(a_geom, a_grids, a_dmap, a_overset_mask, a_info, a_factory);
+}
+
 void
 MLABecLaplacian::define (const Vector<Geometry>& a_geom,
                          const Vector<BoxArray>& a_grids,
@@ -30,10 +40,12 @@ MLABecLaplacian::define (const Vector<Geometry>& a_geom,
 
     m_a_coeffs.resize(m_num_amr_levels);
     m_b_coeffs.resize(m_num_amr_levels);
+    m_overset_mask.resize(m_num_amr_levels);
     for (int amrlev = 0; amrlev < m_num_amr_levels; ++amrlev)
     {
         m_a_coeffs[amrlev].resize(m_num_mg_levels[amrlev]);
         m_b_coeffs[amrlev].resize(m_num_mg_levels[amrlev]);
+        m_overset_mask[amrlev].resize(m_num_mg_levels[amrlev]);
         for (int mglev = 0; mglev < m_num_mg_levels[amrlev]; ++mglev)
         {
             m_a_coeffs[amrlev][mglev].define(m_grids[amrlev][mglev],
@@ -47,6 +59,83 @@ MLABecLaplacian::define (const Vector<Geometry>& a_geom,
                                                        m_dmap[amrlev][mglev],
                                                        ncomp, 0, MFInfo(), *m_factory[amrlev][mglev]);
             }
+        }
+    }
+}
+
+void
+MLABecLaplacian::define (const Vector<Geometry>& a_geom,
+                         const Vector<BoxArray>& a_grids,
+                         const Vector<DistributionMapping>& a_dmap,
+                         const Vector<iMultiFab const*>& a_overset_mask,
+                         const LPInfo& a_info,
+                         const Vector<FabFactory<FArrayBox> const*>& a_factory)
+{
+    BL_PROFILE("MLABecLaplacian::define(overset)");
+
+    int namrlevs = a_geom.size();
+    m_overset_mask.resize(namrlevs);
+    for (int amrlev = 0; amrlev < namrlevs; ++amrlev)
+    {
+        m_overset_mask[amrlev].emplace_back(new iMultiFab(a_grids[amrlev], a_dmap[amrlev], 1, 0));
+        iMultiFab::Copy(*m_overset_mask[amrlev][0], *a_overset_mask[amrlev], 0, 0, 1, 0);
+        if (amrlev > 1) {
+            AMREX_ALWAYS_ASSERT(amrex::refine(a_geom[amrlev-1].Domain(),2)
+                                == a_geom[amrlev].Domain());
+        }
+    }
+
+    int max_overset_mask_coarsening_level = 0;
+    int amrlev = 0;
+    Box dom = a_geom[0].Domain();
+    for (int mglev = 1; mglev <= a_info.max_coarsening_level; ++mglev)
+    {
+        AMREX_ALWAYS_ASSERT(mg_coarsen_ratio == 2);
+        iMultiFab const& fine = *m_overset_mask[amrlev][mglev-1];
+        if (dom.coarsenable(2) and fine.boxArray().coarsenable(2)) {
+            std::unique_ptr<iMultiFab> crse(new iMultiFab(amrex::coarsen(fine.boxArray(),2),
+                                                          fine.DistributionMap(), 1, 0));
+            ReduceOps<ReduceOpSum> reduce_op;
+            ReduceData<int> reduce_data(reduce_op);
+            using ReduceTuple = typename decltype(reduce_data)::Type;
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+            for (MFIter mfi(*crse, TilingIfNotGPU()); mfi.isValid(); ++mfi)
+            {
+                const Box& bx = mfi.tilebox();
+                Array4<int const> const& fmsk = fine.const_array(mfi);
+                Array4<int> const& cmsk = crse->array(mfi);
+                reduce_op.eval(bx, reduce_data,
+                [=] AMREX_GPU_HOST_DEVICE (Box const& b) -> ReduceTuple
+                {
+                    return { coarsen_overset_mask(b, cmsk, fmsk) };
+                });
+            }
+            ReduceTuple hv = reduce_data.value();
+            if (amrex::get<0>(hv) == 0) {
+                m_overset_mask[amrlev].push_back(std::move(crse));
+                max_overset_mask_coarsening_level = mglev;
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    LPInfo info = a_info;
+    info.max_coarsening_level = std::min(a_info.max_coarsening_level,
+                                         max_overset_mask_coarsening_level);
+    define(a_geom, a_grids, a_dmap, info, a_factory);
+
+    amrlev = 0;
+    for (int mglev = 1; mglev < m_num_mg_levels[amrlev]; ++mglev) {
+        if (! isMFIterSafe(*m_overset_mask[amrlev][mglev], m_a_coeffs[amrlev][mglev])) {
+            std::unique_ptr<iMultiFab> osm(new iMultiFab(m_grids[amrlev][mglev],
+                                                         m_dmap[amrlev][mglev], 1, 0));
+            osm->ParallelCopy(*m_overset_mask[amrlev][mglev]);
+            std::swap(osm, m_overset_mask[amrlev][mglev]);
         }
     }
 }
@@ -123,7 +212,6 @@ MLABecLaplacian::setBCoeffs (int amrlev, Vector<Real> const& beta)
     }
     m_needs_update = true;
 }
-
 
 void
 MLABecLaplacian::averageDownCoeffs ()
@@ -211,6 +299,8 @@ MLABecLaplacian::prepareForSolve ()
 
     MLCellABecLap::prepareForSolve();
 
+    const int ncomp = getNComp();
+
 #if (AMREX_SPACEDIM != 3)
     applyMetricTermsCoeffs();
 #endif
@@ -273,12 +363,20 @@ MLABecLaplacian::Fapply (int amrlev, int mglev, MultiFab& out, const MultiFab& i
         AMREX_D_TERM(const auto& bxfab = bxcoef.array(mfi);,
                      const auto& byfab = bycoef.array(mfi);,
                      const auto& bzfab = bzcoef.array(mfi););
-
-        AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
-        {
-            mlabeclap_adotx(tbx, yfab, xfab, afab, AMREX_D_DECL(bxfab,byfab,bzfab),
-                            dxinv, ascalar, bscalar, ncomp);
-        });
+        if (m_overset_mask[amrlev][mglev]) {
+            const auto& osm = m_overset_mask[amrlev][mglev]->array(mfi);
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
+            {
+                mlabeclap_adotx_os(tbx, yfab, xfab, afab, AMREX_D_DECL(bxfab,byfab,bzfab),
+                                   osm, dxinv, ascalar, bscalar, ncomp);
+            });
+        } else {
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
+            {
+                mlabeclap_adotx(tbx, yfab, xfab, afab, AMREX_D_DECL(bxfab,byfab,bzfab),
+                                dxinv, ascalar, bscalar, ncomp);
+            });
+        }
     }
 }
 
@@ -417,29 +515,60 @@ MLABecLaplacian::Fsmooth (int amrlev, int mglev, MultiFab& sol, const MultiFab& 
 #endif
         Gpu::AsyncArray<Array4<Real const> > aa(ha.data(), 2*AMREX_SPACEDIM);
         auto dp = aa.data();
-        AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( tbx, thread_box,
-        {
-            abec_gsrb(thread_box, solnfab, rhsfab, alpha, afab,
-                      AMREX_D_DECL(dhx, dhy, dhz),
-                      AMREX_D_DECL(bxfab, byfab, bzfab),
-                      AMREX_D_DECL(m0,m2,m4),
-                      AMREX_D_DECL(m1,m3,m5),
-                      AMREX_D_DECL(dp[0],dp[2],dp[4]),
-                      AMREX_D_DECL(dp[1],dp[3],dp[5]),
-                      vbx, redblack, nc);
-        });
+
+        if (m_overset_mask[amrlev][mglev]) {
+            const auto& osm = m_overset_mask[amrlev][mglev]->array(mfi);
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( tbx, thread_box,
+            {
+                abec_gsrb_os(thread_box, solnfab, rhsfab, alpha, afab,
+                             AMREX_D_DECL(dhx, dhy, dhz),
+                             AMREX_D_DECL(bxfab, byfab, bzfab),
+                             AMREX_D_DECL(m0,m2,m4),
+                             AMREX_D_DECL(m1,m3,m5),
+                             AMREX_D_DECL(dp[0],dp[2],dp[4]),
+                             AMREX_D_DECL(dp[1],dp[3],dp[5]),
+                             osm, vbx, redblack, nc);
+            });
+        } else {
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( tbx, thread_box,
+            {
+                abec_gsrb(thread_box, solnfab, rhsfab, alpha, afab,
+                          AMREX_D_DECL(dhx, dhy, dhz),
+                          AMREX_D_DECL(bxfab, byfab, bzfab),
+                          AMREX_D_DECL(m0,m2,m4),
+                          AMREX_D_DECL(m1,m3,m5),
+                          AMREX_D_DECL(dp[0],dp[2],dp[4]),
+                          AMREX_D_DECL(dp[1],dp[3],dp[5]),
+                          vbx, redblack, nc);
+            });
+        }
 #else
-        AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( tbx, thread_box,
-        {
-            abec_gsrb(thread_box, solnfab, rhsfab, alpha, afab,
-                      AMREX_D_DECL(dhx, dhy, dhz),
-                      AMREX_D_DECL(bxfab, byfab, bzfab),
-                      AMREX_D_DECL(m0,m2,m4),
-                      AMREX_D_DECL(m1,m3,m5),
-                      AMREX_D_DECL(f0fab,f2fab,f4fab),
-                      AMREX_D_DECL(f1fab,f3fab,f5fab),
-                      vbx, redblack, nc);
-        });
+        if (m_overset_mask[amrlev][mglev]) {
+            const auto& osm = m_overset_mask[amrlev][mglev]->array(mfi);
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( tbx, thread_box,
+            {
+                abec_gsrb_os(thread_box, solnfab, rhsfab, alpha, afab,
+                             AMREX_D_DECL(dhx, dhy, dhz),
+                             AMREX_D_DECL(bxfab, byfab, bzfab),
+                             AMREX_D_DECL(m0,m2,m4),
+                             AMREX_D_DECL(m1,m3,m5),
+                             AMREX_D_DECL(f0fab,f2fab,f4fab),
+                             AMREX_D_DECL(f1fab,f3fab,f5fab),
+                             osm, vbx, redblack, nc);
+            });
+        } else {
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( tbx, thread_box,
+            {
+                abec_gsrb(thread_box, solnfab, rhsfab, alpha, afab,
+                          AMREX_D_DECL(dhx, dhy, dhz),
+                          AMREX_D_DECL(bxfab, byfab, bzfab),
+                          AMREX_D_DECL(m0,m2,m4),
+                          AMREX_D_DECL(m1,m3,m5),
+                          AMREX_D_DECL(f0fab,f2fab,f4fab),
+                          AMREX_D_DECL(f1fab,f3fab,f5fab),
+                          vbx, redblack, nc);
+            });
+        }
 #endif
     }
 }
@@ -567,6 +696,37 @@ MLABecLaplacian::update ()
     }
 
     m_needs_update = false;
+}
+
+void
+MLABecLaplacian::interpolation (int amrlev, int fmglev, MultiFab& fine, const MultiFab& crse) const
+{
+    const int ncomp = getNComp();
+
+    if (m_overset_mask[amrlev][fmglev])
+    {
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+        for (MFIter mfi(fine,TilingIfNotGPU()); mfi.isValid(); ++mfi)
+        {
+            const Box& bx    = mfi.tilebox();
+            Array4<Real const> const& cfab = crse.const_array(mfi);
+            Array4<Real> const& ffab = fine.array(mfi);
+            const auto& osm = m_overset_mask[amrlev][fmglev]->array(mfi);
+            AMREX_HOST_DEVICE_PARALLEL_FOR_4D ( bx, ncomp, i, j, k, n,
+            {
+                if (!osm(i,j,k)) {
+                    int ic = amrex::coarsen(i,2);
+                    int jc = amrex::coarsen(j,2);
+                    int kc = amrex::coarsen(k,2);
+                    ffab(i,j,k,n) += cfab(ic,jc,kc,n);
+                }
+            });
+        }
+    } else {
+        MLCellABecLap::interpolation(amrlev, fmglev, fine, crse);
+    }
 }
 
 }
