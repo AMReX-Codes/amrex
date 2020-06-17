9c7315c555f2c01b715f69d4eae77e1d636b7473
atmyers <atmyers2@gmail.com>
2020-06-17 09:14:13 -0700

    stub for Async IO Particle test

---
+++ Tests/Particles/AsyncIO/main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_MultiFabUtil.H>
+#include "AMReX_Particles.H"
+#include "AMReX_PlotFileUtil.H"
+#include "AMReX_ParticleAsyncIO.H"
+
+using namespace amrex;
+
+struct TestParams {
+  int nx;
+  int ny;
+  int nz;
+  int max_grid_size;
+  int nppc;
+  bool verbose;
+};
+
+void test_async_io (TestParams& parms)
+{
+
+  RealBox real_box;
+  for (int n = 0; n < BL_SPACEDIM; n++) {
+    real_box.setLo(n, 0.0);
+    real_box.setHi(n, 1.0);
+  }
+
+  IntVect domain_lo(AMREX_D_DECL(0, 0, 0));
+  IntVect domain_hi(AMREX_D_DECL(parms.nx - 1, parms.ny - 1, parms.nz-1));
+  const Box domain(domain_lo, domain_hi);
+
+  // This says we are using Cartesian coordinates
+  int coord = 0;
+
+  // This sets the boundary conditions to be doubly or triply periodic
+  int is_per[BL_SPACEDIM];
+  for (int i = 0; i < BL_SPACEDIM; i++)
+    is_per[i] = 1;
+  Geometry geom(domain, &real_box, CoordSys::cartesian, is_per);
+
+  BoxArray ba(domain);
+  ba.maxSize(parms.max_grid_size);
+  if (parms.verbose && ParallelDescriptor::IOProcessor()) {
+    std::cout << "Number of boxes              : " << ba[0].size() << '\n' << '\n';
+  }
+
+  DistributionMapping dmap(ba);
+
+  MultiFab acceleration(ba, dmap, 3, 1);
+  acceleration.setVal(5.0, 1);
+
+  MultiFab density(ba, dmap, 1, 0);
+  density.setVal(0.0);
+
+  MultiFab partMF(ba, dmap, 1 + BL_SPACEDIM, 1);
+  partMF.setVal(0.0);
+
+  typedef ParticleContainer<1 + BL_SPACEDIM> MyParticleContainer;
+  MyParticleContainer myPC(geom, dmap, ba);
+  myPC.SetVerbose(false);
+
+  int num_particles = parms.nppc * parms.nx * parms.ny * parms.nz;
+  if (ParallelDescriptor::IOProcessor())
+    std::cout << "Total number of particles    : " << num_particles << '\n' << '\n';
+
+  bool serialize = true;
+  int iseed = 451;
+  Real mass = 10.0;
+
+  MyParticleContainer::ParticleInitData pdata = {mass, AMREX_D_DECL(1.0, 2.0, 3.0)};
+  myPC.InitRandom(num_particles, iseed, pdata, serialize);
+  myPC.AssignCellDensitySingleLevel(0, partMF, 0, 4, 0);
+
+  MultiFab::Copy(density, partMF, 0, 0, 1, 0);
+
+  WriteSingleLevelPlotfile("plt00000", partMF,
+                           {"density", "vx", "vy", "vz"},
+                           geom, 0.0, 0);
+
+  myPC.Checkpoint("plt00000", "particle0");
+}
+
+int main(int argc, char* argv[])
+{
+  amrex::Initialize(argc,argv);
+
+  ParmParse pp;
+
+  TestParams parms;
+
+  pp.get("nx", parms.nx);
+  pp.get("ny", parms.ny);
+  pp.get("nz", parms.nz);
+  pp.get("max_grid_size", parms.max_grid_size);
+  pp.get("nppc", parms.nppc);
+  if (parms.nppc < 1 && ParallelDescriptor::IOProcessor())
+    amrex::Abort("Must specify at least one particle per cell");
+
+  parms.verbose = false;
+  pp.query("verbose", parms.verbose);
+
+  if (parms.verbose && ParallelDescriptor::IOProcessor()) {
+    std::cout << std::endl;
+    std::cout << "Number of particles per cell : ";
+    std::cout << parms.nppc  << std::endl;
+    std::cout << "Size of domain               : ";
+    std::cout << parms.nx << " " << parms.ny << " " << parms.nz << std::endl;
+  }
+
+  test_async_io(parms);
+
+  amrex::Finalize();
+}
