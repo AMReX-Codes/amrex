AMReX-Codes/amrex

commit f71d122d0ed6831ae946e108116a88bee9b563d7

TagBoxArray::hasTags (#1274)
In #1258, TagBoxArray::numtags was removed.  However, IAMR still needs it.
So a new function, hasTags, is added for IAMR.

Src/AmrCore/AMReX_TagBox.cpp
@@ -716,5 +716,54 @@ TagBoxArray::coarsen (const IntVect & ratio)
     n_grow = new_n_grow;
 }
 
+bool
+TagBoxArray::hasTags (Box const& a_bx) const
+{
+    bool has_tags = false;
+#ifdef AMREX_USE_GPU
+    if (Gpu::inLaunchRegion()) {
+        ReduceOps<ReduceOpLogicalOr> reduce_op;
+        ReduceData<int> reduce_data(reduce_op);
+        using ReduceTuple = typename decltype(reduce_data)::Type;
+
+        for (MFIter mfi(*this); mfi.isValid(); ++mfi)
+        {
+            Box const& b = a_bx & mfi.fabbox();
+            if (b.ok()) {
+                const auto& arr = this->const_array(mfi);
+                reduce_op.eval(b, reduce_data,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k) -> ReduceTuple
+                {
+                    int tr = arr(i,j,k) != TagBox::CLEAR;
+                    return {tr};
+                });
+            }
+        }
+
+        ReduceTuple hv = reduce_data.value();
+        has_tags = static_cast<bool>(amrex::get<0>(hv));
+    } else
+#endif
+    {
+#ifdef _OPENMP
+#pragma omp parallel reduction(||:has_Tags)
+#endif
+        for (MFIter mfi(*this); mfi.isValid(); ++mfi)
+        {
+            Box const& b = a_bx & mfi.fabbox();
+            if (b.ok()) {
+                Array4<char const> const& arr = this->const_array(mfi);
+                AMREX_LOOP_3D(b, i, j, k,
+                {
+                    has_tags = has_tags || (arr(i,j,k) != TagBox::CLEAR);
+                });
+            }
+        }
+    }
+
+    ParallelAllReduce::Or(has_tags, ParallelContext::CommunicatorSub());
+    return has_tags;
+}
+
 }
 

Src/AmrCore/AMReX_TagBox.H
@@ -225,6 +225,9 @@ public:
     */
     void collate (Vector<IntVect>& TheGlobalCollateSpace) const;
 
+    // \brief Are there tags in the region defined by bx?
+    bool hasTags (Box const& bx) const;
+
     void local_collate_cpu (Vector<IntVect>& v) const;
 #ifdef AMREX_USE_GPU
     void local_collate_gpu (Vector<IntVect>& v) const;

commit 133a2eeba400f08f08df81044e28574ccaa75f2a

Fix typo for OMP
The typo was introduced in #1274.

Src/AmrCore/AMReX_TagBox.cpp
@@ -746,7 +746,7 @@ TagBoxArray::hasTags (Box const& a_bx) const
 #endif
     {
 #ifdef _OPENMP
-#pragma omp parallel reduction(||:has_Tags)
+#pragma omp parallel reduction(||:has_tags)
 #endif
         for (MFIter mfi(*this); mfi.isValid(); ++mfi)
         {