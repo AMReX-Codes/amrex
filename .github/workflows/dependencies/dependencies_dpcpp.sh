d8747f753f9f5524aba04e1a70fee3d1665654bd
Axel Huebl <axel.huebl@plasma.ninja>
2020-09-04 13:39:44 -0700

    CI: Move DPC++ dependencies (#1336)
    Move install script into unified location.
    
    ## Summary
    
    ## Additional background
    
    ## Checklist
    
    The proposed changes:
    - [ ] fix a bug or incorrect behavior in AMReX
    - [ ] add new capabilities to AMReX
    - [ ] changes answers in the test suite to more than roundoff level
    - [ ] are likely to significantly affect the results of downstream AMReX users
    - [ ] are described in the proposed changes to the AMReX documentation, if appropriate

--- .github/workflows/dependencies/dependencies_dpcpp.sh
+++ .github/workflows/dependencies/dependencies_dpcpp.sh
@@ -9,9 +9,10 @@ set -eu -o pipefail
 
 # Ref.: https://github.com/rscohn2/oneapi-ci
 # intel-basekit intel-hpckit are too large in size
-sudo wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+wget -q -O - https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB \
+  | sudo apt-key add -
+echo "deb https://apt.repos.intel.com/oneapi all main" \
+  | sudo tee /etc/apt/sources.list.d/oneAPI.list
 
 sudo apt-get update
 
