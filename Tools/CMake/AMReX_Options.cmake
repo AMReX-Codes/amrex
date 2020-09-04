61ace283fc087f7205a85b46bd0fc9aacb21d298
Michele Rosso <mrosso@lbl.gov>
2020-09-04 12:38:24 -0700

    CMake: EB is made avaialble only if DIM>1

--- Tools/CMake/AMReX_Options.cmake
+++ Tools/CMake/AMReX_Options.cmake
@@ -144,7 +144,8 @@ endif ()
 #
 # AMReX components selection  ================================================
 #
-option( ENABLE_EB "Build EB Code" OFF )
+cmake_dependent_option( ENABLE_EB "Build EB code" OFF
+   "NOT DIM EQUAL 1" OFF )
 print_option(ENABLE_EB)
 
 cmake_dependent_option( ENABLE_FORTRAN_INTERFACES "Build Fortran API" OFF
