f2e180e2428c6460dd2893fe1b21e2887259886f
Michele Rosso <mrosso@lbl.gov>
2020-10-19 10:37:01 -0700

    CMake: look only for the C component of HDF5

--- Tools/CMake/AMReXThirdPartyLibraries.cmake
+++ Tools/CMake/AMReXThirdPartyLibraries.cmake
@@ -3,7 +3,7 @@
 #
 if (ENABLE_HDF5)
     set(HDF5_PREFER_PARALLEL TRUE)
-    find_package(HDF5 1.10.4 REQUIRED COMPONENTS CXX)
+    find_package(HDF5 1.10.4 REQUIRED)
     if (NOT HDF5_IS_PARALLEL)
         message(FATAL_ERROR "\nHDF5 library does not support parallel I/O")
     endif ()
