6f07fe4e84a4dd57a0e89c2d2cb84c9953fda85d
Weiqun Zhang <WeiqunZhang@lbl.gov>
2020-10-23 16:23:17 -0700

    Workaround for Mac CI (#1482)

--- .github/workflows/dependencies/dependencies_mac.sh
+++ .github/workflows/dependencies/dependencies_mac.sh
@@ -7,6 +7,10 @@
 
 set -eu -o pipefail
 
+brew uninstall openssl@1.0.2t
+brew uninstall python@2.7.17
+brew untap local/openssl
+brew untap local/python2
 brew update
 brew install libomp
 brew install open-mpi
