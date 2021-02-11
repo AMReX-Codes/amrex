3bb4bd1a689f602f54352bf18b620ae203bb1484
Andrew Myers <atmyers2@gmail.com>
2021-02-11 13:02:49 -0800

    try cuda 9.2

--- .github/workflows/dependencies/dependencies_nvcc9.sh
+++ .github/workflows/dependencies/dependencies_nvcc9.sh
@@ -18,8 +18,21 @@ sudo apt-get install -y --no-install-recommends\
     g++-7               \
     gfortran-7          \
     libopenmpi-dev      \
-    openmpi-bin         \
-    nvidia-cuda-dev     \
-    nvidia-cuda-toolkit
+    openmpi-bin
+
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-key add 7fa2af80.pub
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" \
+    | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get install -y \
+    cuda-command-line-tools-9-2 \
+    cuda-compiler-9-2           \
+    cuda-cupti-dev-9-2          \
+    cuda-minimal-build-9-2      \
+    cuda-nvml-dev-9-2           \
+    cuda-nvtx-9-2               \
+    libcurand-dev-9-2
+sudo ln -s cuda-9.2 /usr/local/cuda
 
 
