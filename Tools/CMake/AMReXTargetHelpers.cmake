dd0b12f29cc7832237fe032bca3c40ae116ddf59
Michele Rosso <mrosso@lbl.gov>
2021-05-12 14:33:58 -0700

    CMake: fix bug

--- Tools/CMake/AMReXTargetHelpers.cmake
+++ Tools/CMake/AMReXTargetHelpers.cmake
@@ -1,3 +1,4 @@
+
 #[=======================================================================[
 AMReXTargetHelpers
 ----------------
@@ -118,7 +119,6 @@ function (setup_target_for_cuda_compilation _target)
    set_target_properties( ${_target}
       PROPERTIES
       CUDA_SEPARABLE_COMPILATION ON      # This adds -dc
-      CUDA_ARCHITECTURES ${AMREX_CUDA_ARCHS}
       )
    set_cpp_sources_to_cuda_language(${_target})
 
