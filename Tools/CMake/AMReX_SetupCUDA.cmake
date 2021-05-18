a1faf316ff6556aabf76fd6d605bf4a59d6c0f59
Weiqun Zhang <WeiqunZhang@lbl.gov>
2021-05-18 08:32:04 -0700

    nvcc --display-error-number --diag-error errNum (#2033)
    Since 11.2, nvcc can display a diagnostic number for any warning message and
    it can turn specified warnings into errors These two new flags are added to
    the build system, and it's now an error to write a __device__ variable in
    a host function.
    
    Co-authored-by: Michele Rosso <mrosso@lbl.gov>

--- Tools/CMake/AMReX_SetupCUDA.cmake
+++ Tools/CMake/AMReX_SetupCUDA.cmake
@@ -129,3 +129,7 @@ if (AMReX_CUDA_BACKTRACE)
         string(APPEND CMAKE_CUDA_FLAGS " -Xcompiler -rdynamic")
     endif ()
 endif ()
+
+if (CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL 11.2)
+   string(APPEND CMAKE_CUDA_FLAGS " --display-error-number --diag-error 20092")
+endif ()
