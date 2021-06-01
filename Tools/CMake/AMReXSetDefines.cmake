702af37a91c60bf3916306884ef78319beb5b527
Cody Balos <balos1@llnl.gov>
2021-06-01 13:11:23 -0700

    add interface between sundials SUNMemory and the Arena (#1800)
    ## Summary
    
    As @asalmgren @jmsexton03 @mic84 @ajnonaka @dwillcox @gardner48 @cswoodward and I discussed... this PR adds an interface between the AMReX Arena and the SUNDIALS SUNMemory API. This is currently used in Pele and will be used in Nyx also. In general, any AMReX based app that also uses SUNDIALS (and its provided data structures) will likely find this useful when targeting GPUs.
    
    CC @drummerdoc 
    
    ## Additional background
    
    In SUNDIALS 5.4.0, the SUNMemory API was introduced to allow users to substitute their own memory management underneath SUNDIALS data structures. In Pele, using the AMReX arena underneath the SUNDIALS data structures results in as much as a 15% speedup.
    
    I have tried to structure the code to follow AMReX conventions. Since we also will be bringing in an interface between the MultiFab and the SUNDIALS N_Vector, I have also tried to structure the code so that it will be easy to add that interface in as well.
    
    Co-authored-by: Weiqun Zhang <WeiqunZhang@lbl.gov>
    Co-authored-by: David Gardner <gardner48@llnl.gov>
    Co-authored-by: Michele Rosso <mrosso@lbl.gov>
    Co-authored-by: Jean M. Sexton <jmsexton@lbl.gov>

--- Tools/CMake/AMReXSetDefines.cmake
+++ Tools/CMake/AMReXSetDefines.cmake
@@ -161,6 +161,12 @@ endif ()
 add_amrex_define(AMREX_USE_HDF5 NO_LEGACY IF AMReX_HDF5)
 add_amrex_define(AMREX_USE_HDF5_ASYNC NO_LEGACY IF AMReX_HDF5_ASYNC)
 
+
+#
+# SUNDIALS
+#
+add_amrex_define(AMREX_USE_SUNDIALS NO_LEGACY IF AMReX_SUNDIALS)
+
 #
 # Miscellaneous
 #
