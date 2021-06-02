376d4f542998716469d8d9517188bf28b2095780
Corey Wetterer-Nelson <c.wetterer-nelson@kitware.com>
2021-06-01 22:54:14 -0600

    refactor templating for particle bridges

--- Src/Extern/SENSEI/AMReX_AmrParticleMeshInSituBridge.cpp
+++
@@ -1,8 +0,0 @@
-#include <AMReX_AmrParticleMeshInSituBridge.H>
-
-#ifdef AMREX_PARTICLES
-namespace amrex {
-// explicit template specialization for use with AmrParticleContainer class
-template<> class AmrParticleMeshInSituBridge<3,0,0,0>;
-}
-#endif
