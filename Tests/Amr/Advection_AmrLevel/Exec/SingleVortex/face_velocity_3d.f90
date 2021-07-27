119a17ff1a02a32db6b40e25cc79f78d7d644902
Wileam Phan <wphan@vols.utk.edu>
2021-07-27 19:51:45 -0400

    Reindent SingleVortex/face_velocity_3d.f90

--- Tests/Amr/Advection_AmrLevel/Exec/SingleVortex/face_velocity_3d.f90
+++ Tests/Amr/Advection_AmrLevel/Exec/SingleVortex/face_velocity_3d.f90
@@ -42,24 +42,24 @@ subroutine get_face_velocity(level, time, &
 
   ! x velocity
   do k = vx_l3, vx_h3
-  do j = vx_l2, vx_h2
-     y = (dble(j)+0.5d0) * dx(2) + prob_lo(2)
-     do i = vx_l1, vx_h1
-        x = dble(i) * dx(1) + prob_lo(1)
-        vx(i,j,k) =  -( (psi(i,j+1)+psi(i-1,j+1)) - (psi(i,j-1)+psi(i-1,j-1)) ) * (0.25d0/dx(2))
+     do j = vx_l2, vx_h2
+        y = (dble(j)+0.5d0) * dx(2) + prob_lo(2)
+        do i = vx_l1, vx_h1
+           x = dble(i) * dx(1) + prob_lo(1)
+           vx(i,j,k) = -( (psi(i,j+1)+psi(i-1,j+1)) - (psi(i,j-1)+psi(i-1,j-1)) ) * (0.25d0/dx(2))
+        end do
      end do
-  end do
   end do
 
   ! y velocity
   do k = vy_l3, vy_h3
-  do j = vy_l2, vy_h2
-     y = dble(j) * dx(2) + prob_lo(2)
-     do i = vy_l1, vy_h1
-        x = (dble(i)+0.5d0) * dx(1) + prob_lo(1)
-        vy(i,j,k) = ( (psi(i+1,j)+psi(i+1,j-1)) - (psi(i-1,j)+psi(i-1,j-1)) ) * (0.25d0/dx(1))
+     do j = vy_l2, vy_h2
+        y = dble(j) * dx(2) + prob_lo(2)
+        do i = vy_l1, vy_h1
+           x = (dble(i)+0.5d0) * dx(1) + prob_lo(1)
+           vy(i,j,k) =  ( (psi(i+1,j)+psi(i+1,j-1)) - (psi(i-1,j)+psi(i-1,j-1)) ) * (0.25d0/dx(1))
+        end do
      end do
-  end do
   end do
 
   ! z velocity
