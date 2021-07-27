4b572502cb66d2a3816c5453390cc8a6db8bd273
atmyers <atmyers2@gmail.com>
2021-07-27 12:39:42 -0700

    refactor test

--- Tests/Particles/DenseBins/main.cpp
+++ Tests/Particles/DenseBins/main.cpp
@@ -5,6 +5,57 @@
 
 using namespace amrex;
 
+void checkAnswer (const amrex::DenseBins<int>& bins,
+                  const amrex::Vector<int>& items)
+{
+    BL_PROFILE("checkAnswer");
+    const auto perm = bins.permutationPtr();
+    const auto bins_ptr = bins.binsPtr();
+    const auto offsets = bins.offsetsPtr();
+    for (int i = 0; i < bins.numItems()-1; ++i)
+    {
+        AMREX_ALWAYS_ASSERT(bins_ptr[perm[i]] <= bins_ptr[perm[i+1]]);
+    }
+
+    for (int i = 0; i < bins.numBins(); ++i) {
+        auto start = offsets[i  ];
+        auto stop  = offsets[i+1];
+        if (start == stop) continue;
+        for (int j = start+1; j < stop; ++j)
+        {
+            AMREX_ALWAYS_ASSERT(bins_ptr[perm[start]] == bins_ptr[perm[j]]);
+        }
+    }
+}
+
+void testOpenMP (int nbins, const amrex::Vector<int>& items)
+{
+    amrex::DenseBins<int> bins;
+    bins.buildOpenMP(items.size(), items.data(), nbins, [=] (int j) noexcept -> unsigned int { return j ; });
+
+    checkAnswer(bins, items);
+}
+
+void testSerial (int nbins, const amrex::Vector<int>& items)
+{
+    amrex::DenseBins<int> bins;
+    bins.build(items.size(), items.data(), nbins, [=] (int j) noexcept -> unsigned int { return j ; });
+
+    checkAnswer(bins, items);
+}
+
+void initData (int nbins, amrex::Vector<int>& items)
+{
+    BL_PROFILE("init");
+
+    const int nitems = items.size();
+
+#ifdef AMREX_USE_OMP
+#pragma omp parallel for
+#endif
+    for (int i = 0; i < nitems; ++i) { items[i] = amrex::Random_int(nbins); }
+}
+
 void testDenseBins ()
 {
     int nitems;
@@ -15,39 +66,10 @@ void testDenseBins ()
     pp.get("nbins" , nbins);
 
     amrex::Vector<int> items(nitems);
-    {
-        BL_PROFILE("init");
-#ifdef AMREX_USE_OMP
-#pragma omp parallel for
-#endif
-        for (int i = 0; i < nitems; ++i) { items[i] = amrex::Random_int(nbins); }
-    }
+    initData(nbins, items);
 
-    amrex::DenseBins<int> bins;
-    bins.buildOpenMP(nitems, items.data(), nbins, [=] (int j) noexcept -> unsigned int { return j ; });
-
-    {
-        BL_PROFILE("checkAnswer");
-        const auto perm = bins.permutationPtr();
-        const auto bins_ptr = bins.binsPtr();
-        const auto offsets = bins.offsetsPtr();
-        const auto counts = bins.countsPtr();
-        for (int i = 0; i < nitems-1; ++i)
-        {
-            AMREX_ALWAYS_ASSERT(bins_ptr[perm[i]] <= bins_ptr[perm[i+1]]);
-        }
-
-        for (int i = 0; i < bins.numBins(); ++i) {
-            auto start = offsets[i  ];
-            auto stop  = offsets[i+1];
-            AMREX_ALWAYS_ASSERT(counts[i] == stop - start);
-            if (start == stop) continue;
-            for (int j = start+1; j < stop; ++j)
-            {
-                AMREX_ALWAYS_ASSERT(bins_ptr[perm[start]] == bins_ptr[perm[j]]);
-            }
-        }
-    }
+    testSerial(nbins, items);
+    testOpenMP(nbins, items);
 }
 
 int main (int argc, char* argv[])
