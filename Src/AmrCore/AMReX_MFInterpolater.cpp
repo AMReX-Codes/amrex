c9711b11b9539f56e64922b4f755502986cdf8c1
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-07-30 15:43:47 -0700

    Bilinear interpolation for cell data
    Add MFCellBilinear for cell data.  Port CellBilinear to C++.

--- Src/AmrCore/AMReX_MFInterpolater.cpp
+++ Src/AmrCore/AMReX_MFInterpolater.cpp
@@ -9,6 +9,7 @@ namespace amrex {
 MFPCInterp          mf_pc_interp;
 MFCellConsLinInterp mf_cell_cons_interp(false);
 MFCellConsLinInterp mf_lincc_interp(true);
+MFCellBilinear      mf_cell_bilinear_interp;
 
 Box
 MFPCInterp::CoarseBox (const Box& fine, const IntVect& ratio)
@@ -289,6 +290,70 @@ MFCellConsLinInterp::interp (MultiFab const& crsemf, int ccomp, MultiFab& finemf
                     });
                 }
             }
+        }
+    }
+}
+
+Box
+MFCellBilinear::CoarseBox (const Box& fine, const IntVect& ratio)
+{
+    const int* lo = fine.loVect();
+    const int* hi = fine.hiVect();
+
+    Box crse(amrex::coarsen(fine,ratio));
+    const int* clo = crse.loVect();
+    const int* chi = crse.hiVect();
+
+    for (int i = 0; i < AMREX_SPACEDIM; i++) {
+        if ((lo[i]-clo[i]*ratio[i])*2 < ratio[i]) {
+            crse.growLo(i,1);
+        }
+        if ((hi[i]-chi[i]*ratio[i])*2 >= ratio[i]) {
+            crse.growHi(i,1);
+        }
+    }
+    return crse;
+}
+
+Box
+MFCellBilinear::CoarseBox (const Box& fine, int ratio)
+{
+    return CoarseBox(fine,IntVect(ratio));
+}
+
+void
+MFCellBilinear::interp (MultiFab const& crsemf, int ccomp, MultiFab& finemf, int fcomp, int nc,
+                        IntVect const& ng, Geometry const&, Geometry const&,
+                        Box const& dest_domain, IntVect const& ratio,
+                        Vector<BCRec> const&, int)
+{
+#ifdef AMREX_USE_GPU
+    if (Gpu::inLaunchRegion()) {
+        auto const& crse = crsemf.const_arrays();
+        auto const& fine = finemf.arrays();
+        experimental::ParallelFor(finemf, ng, nc,
+        [=] AMREX_GPU_DEVICE (int box_no, int i, int j, int k, int n) noexcept
+        {
+            if (dest_domain.contains(i,j,k)) {
+                mf_cell_bilin_interp(i,j,k,n, fine[box_no], fcomp, crse[box_no], ccomp, ratio);
+            }
+        });
+        Gpu::streamSynchronize();
+    } else
+#endif
+    {
+#ifdef AMREX_USE_OMP
+#pragma omp parallel
+#endif
+        for (MFIter mfi(finemf); mfi.isValid(); ++mfi) {
+            auto const& fine = finemf.array(mfi);
+            auto const& crse = crsemf.const_array(mfi);
+            Box const& fbox = amrex::grow(mfi.validbox(),ng) & dest_domain;
+            amrex::LoopConcurrentOnCpu(fbox, nc,
+            [=] (int i, int j, int k, int n) noexcept
+            {
+                mf_cell_bilin_interp(i,j,k,n, fine, fcomp, crse, ccomp, ratio);
+            });
         }
     }
 }
