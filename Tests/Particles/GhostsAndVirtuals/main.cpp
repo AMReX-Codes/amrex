08c0e9c0a6b178ded3f187ddecee6f2305525249
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-08-15 12:56:21 -0700

    Fix Particle CI Tests
    A number of CI tests had issues with mixing single precision particle data
    with double precision amrex::Real.

--- Tests/Particles/GhostsAndVirtuals/main.cpp
+++ Tests/Particles/GhostsAndVirtuals/main.cpp
@@ -90,7 +90,7 @@ void test_ghosts_and_virtuals (TestParams& parms)
     int num_particles = parms.nppc * AMREX_D_TERM(parms.nx, * parms.ny, * parms.nz);
     bool serialize = true;
     int iseed = 451;
-    Real mass = 10.0;
+    double mass = 10.0;
     MyParticleContainer::ParticleInitData pdata = {{mass}, {}, {}, {}};
 
     myPC.InitRandom(num_particles, iseed, pdata, serialize);
@@ -420,8 +420,8 @@ void test_ghosts_and_virtuals_randomperbox (TestParams& parms)
 
     int num_particles = parms.nppc;
     int iseed = 451;
-    Real mass = 10.0;
-    Real xvel, yvel, zvel;
+    double mass = 10.0;
+    double xvel, yvel, zvel;
     Real total_virts_test = 0.0;
     Real tol = 1e-4;
     xvel = 1.0;
@@ -611,8 +611,8 @@ void test_ghosts_and_virtuals_onepercell (TestParams& parms)
     MyParticleContainer myPC(geom, dmap, ba, rr);
     myPC.SetVerbose(false);
 
-    Real mass = 10.0;
-    Real xvel, yvel, zvel;
+    double mass = 10.0;
+    double xvel, yvel, zvel;
     Real xoff, yoff, zoff;
     xvel = 1.0;
     yvel = 2.0;
