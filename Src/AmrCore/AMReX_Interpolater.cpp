7c5b0eb563d61f7744dc48247e2e95b60117d565
Wileam Phan <wphan@vols.utk.edu>
2021-09-21 16:55:45 -0400

    Another CUDA gripe

--- Src/AmrCore/AMReX_Interpolater.cpp
+++ Src/AmrCore/AMReX_Interpolater.cpp
@@ -758,7 +758,8 @@ CellConservativeProtected::protect (const FArrayBox& crse,
 
 #if (AMREX_SPACEDIM == 3)
                 // Calculate number of fine cells
-                int numFineCells = (ihi-ilo+1) * (jhi-jlo+1) * (khi-klo+1);
+                int numFineCells;
+                numFineCells = (ihi-ilo+1) * (jhi-jlo+1) * (khi-klo+1);
 #endif
 
                 if ( (crseTot > 0) && (crseTot > Math::abs(SumN)) ) {
