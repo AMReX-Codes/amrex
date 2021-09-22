55811e0e6daf6f4fc2289a62ee7c5dce63a27136
Kevin Gott <kngott@lbl.gov>
2021-09-22 14:45:36 -0700

    Store temp mf to remove passing a handler.

--- Src/Base/AMReX_MultiFab.cpp
+++ Src/Base/AMReX_MultiFab.cpp
@@ -1787,4 +1787,24 @@ MultiFab::OverrideSync (const iMultiFab& msk, const Periodicity& period)
     amrex::OverrideSync(*this, msk, period);
 }
 
+void
+MultiFab::OverrideSync_nowait (const Periodicity& period)
+{
+    if (ixType().cellCentered()) return;
+    auto msk = this->OwnerMask(period);
+    amrex::OverrideSync_nowait(*this, *msk, period);
+}
+
+void
+MultiFab::OverrideSync_nowait (const iMultiFab& msk, const Periodicity& period)
+{
+    amrex::OverrideSync_nowait(*this, msk, period);
+}
+
+void
+MultiFab::OverrideSync_finish ()
+{
+    amrex::OverrideSync_finish(*this);
+}
+
 }
