13dd991c9f1ed04571fdce0955c137252eec9c42
Andrew Myers <atmyers2@gmail.com>
2021-12-10 09:47:36 -0800

    add assertion for ixType

--- Src/Base/AMReX_DistributionMapping.cpp
+++ Src/Base/AMReX_DistributionMapping.cpp
@@ -1896,6 +1896,8 @@ DistributionMapping MakeSimilarDM (const BoxArray& ba, const MultiFab& mf, const
 DistributionMapping MakeSimilarDM (const BoxArray& ba, const BoxArray& src_ba,
                                    const DistributionMapping& src_dm, const IntVect& ng)
 {
+    AMREX_ASSERT(ba.ixType() == src_ba.ixType());
+
     Vector<int> pmap(ba.size());
     for (Long i = 0; i < ba.size(); ++i) {
         Box box = ba[i];
