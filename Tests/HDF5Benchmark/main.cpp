df6893521551ab14b9d87f9610786a42eafcd9d9
Jean Luca Bez <jeanlucabez@gmail.com>
2021-12-21 08:44:38 -0800

    Update read path for HDF5 benchmark (#2542)
    Update path that was not used after including the directory for read

--- Tests/HDF5Benchmark/main.cpp
+++ Tests/HDF5Benchmark/main.cpp
@@ -214,10 +214,10 @@ void test ()
         MyPC newPC(geom, dmap, ba, ref_ratio);
 #ifdef AMREX_USE_HDF5
         sprintf(directory_path, "%s%s", directory.c_str(), "plt00000/particle0");
-        newPC.RestartHDF5("plt00000/particle0", "particle0");
+        newPC.RestartHDF5(directory_path, "particle0");
 #else
         sprintf(directory_path, "%s%s", directory.c_str(), "plt00000");
-        newPC.Restart("plt00000", "particle0");
+        newPC.Restart(directory_path, "particle0");
 #endif
 
         using PType = typename MyPC::SuperParticleType;
