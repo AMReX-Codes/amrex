33311710acf9eac146d6a4a977892163efc95d99
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-03-01 19:29:55 -0500

    Removed shadowed variables

--- Src/thornado/AMReX_ThornadoInterfaceModule_fi.cpp
+++ Src/thornado/AMReX_ThornadoInterfaceModule_fi.cpp
@@ -9,14 +9,14 @@ using namespace thornado;
 extern "C"
 {
   void amrex_fi_initializemeshrefinement_thornado
-    ( int nNodes[], Real ProjectionMatrix[],
+    ( int nN[], Real ProjMatrix[],
       Real WeightsX1[], Real WeightsX2[], Real WeightsX3[],
       Real LX_X1_Refined_Packed[],
       Real LX_X2_Refined_Packed[],
       Real LX_X3_Refined_Packed[] )
   {
     InitializeMeshRefinement_Thornado
-      ( nNodes, ProjectionMatrix,
+      ( nN, ProjMatrix,
         WeightsX1, WeightsX2, WeightsX3,
         LX_X1_Refined_Packed, LX_X2_Refined_Packed, LX_X3_Refined_Packed );
   }
