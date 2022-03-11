d66de1c9973f2bf8092914d61adef89d6f74be9c
Axel Huebl <axel.huebl@plasma.ninja>
2022-03-11 09:09:11 -0800

    Rewrite .cpp: To work w/o declaration
    ```
    ../Src/Base/AMReX_Version.cpp:7:13: error: use of undeclared identifier 'amrex'
    ```

--- Src/Base/AMReX_Version.cpp
+++ Src/Base/AMReX_Version.cpp
@@ -4,11 +4,14 @@
 #include <string>
 
 
-std::string amrex::Version ()
+namespace amrex
 {
+    std::string Version ()
+    {
 #ifdef AMREX_GIT_VERSION
-    return std::string(AMREX_GIT_VERSION);
+        return std::string(AMREX_GIT_VERSION);
 #else
-    return std::string("Unknown");
+        return std::string("Unknown");
 #endif
-}
+    }
+} // namespace amrex
