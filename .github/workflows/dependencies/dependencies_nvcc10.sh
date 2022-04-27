b9404f5d6b70b6097c47226e23487498661898ed
Weiqun Zhang <weiqunzhang@lbl.gov>
2022-04-27 11:17:54 -0700

    Drop CUDA 9 support
    * Modify CI to test CUDA 10.2 instead of CUDA 9.2.
    
    * Remove workarounds for CUDA 9.2
    
    * No need to test for CUDA >= 10.
    
    * Update Documentation.
    
    * Update GNU Make and CMake.

--- .github/workflows/dependencies/dependencies_nvcc10.sh
+++ .github/workflows/dependencies/dependencies_nvcc10.sh
@@ -4,11 +4,6 @@
 #
 # License: BSD-3-Clause-LBNL
 
-# search recursive inside a folder if a file contains tabs
-#
-# @result 0 if no files are found, else 1
-#
-
 set -eu -o pipefail
 
 sudo apt-get update
@@ -22,17 +17,15 @@ sudo apt-get install -y --no-install-recommends\
 
 sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt-key add 7fa2af80.pub
-echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" \
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" \
     | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
 sudo apt-get install -y \
-    cuda-command-line-tools-9-2 \
-    cuda-compiler-9-2           \
-    cuda-minimal-build-9-2      \
-    cuda-nvml-dev-9-2           \
-    cuda-nvtx-9-2               \
-    cuda-curand-dev-9.2
-
-sudo ln -s cuda-9.2 /usr/local/cuda
-
-
+    cuda-command-line-tools-10-2 \
+    cuda-compiler-10-2           \
+    cuda-cupti-dev-10-2          \
+    cuda-minimal-build-10-2      \
+    cuda-nvml-dev-10-2           \
+    cuda-nvtx-10-2               \
+    cuda-curand-dev-10-2
+sudo ln -s cuda-10.2 /usr/local/cuda
