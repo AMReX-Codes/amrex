d5de914225f0ccbe0878273a16c528ac6d67170a
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-06-13 16:37:31 -0500

    Update sensei CI container for sensei v4.0 integration

--- .github/workflows/docker/sensei/install_sensei.sh
+++ .github/workflows/docker/sensei/install_sensei.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-DOWNLOAD_ROOT=/root/downloads
-EXTRACT_ROOT=/root/downloads/extracted
+set -x
+set -e
+
+DOWNLOAD_ROOT=/tmp/downloads
+EXTRACT_ROOT=/tmp/downloads/extracted
 mkdir -p ${DOWNLOAD_ROOT}
 mkdir -p ${EXTRACT_ROOT}
 
@@ -11,19 +14,21 @@ export CMAKE_GENERATOR=Ninja
 cd ${DOWNLOAD_ROOT}
 mkdir sensei
 cd sensei
-git clone https://gitlab.kitware.com/sensei/sensei.git &&
-cd sensei &&
-git checkout ${SENSEI_VERSION} &&
-mkdir build &&
-cd build &&
+git clone https://github.com/SENSEI-insitu/sensei.git
+cd sensei
+git checkout develop
+mkdir build
+cd build
 cmake .. \
+  -D SENSEI_VERSION=${SENSEI_VERSION} \
   -D BUILD_TESTING=OFF \
   -D ENABLE_PYTHON=ON \
   -D ENABLE_MANDELBROT=OFF \
   -D ENABLE_OSCILLATORS=OFF \
+  -D ENABLE_VTK_IO=ON \
   -D ENABLE_VTK_MPI=ON \
-  -D MPI4PY_INCLUDE_DIR=/usr/lib64/python3.9/site-packages/openmpi/mpi4py/include &&
-cmake --build . &&
+  -D MPI4PY_INCLUDE_DIR=/usr/lib64/python3.10/site-packages/openmpi/mpi4py/include
+cmake --build .
 cmake --install . --prefix ${SENSEI_DIR}
 
 rm -rf ${DOWNLOAD_ROOT}
--- .github/workflows/docker/sensei/install_vtk_minimal.sh
+++ .github/workflows/docker/sensei/install_vtk_minimal.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -x
+set -e
+
 # Include common build tools
 . $(dirname ${BASH_SOURCE[0]})/tools.sh
 
-DOWNLOAD_ROOT=/root/downloads
-EXTRACT_ROOT=/root/downloads/extracted
+DOWNLOAD_ROOT=/tmp/downloads
+EXTRACT_ROOT=/tmp/downloads/extracted
 mkdir -p ${DOWNLOAD_ROOT}
 mkdir -p ${EXTRACT_ROOT}
 
@@ -13,51 +16,42 @@ export CMAKE_GENERATOR=Ninja
 export CC=clang
 export CXX=clang++
 
+if [[ -z ${VTK_VERSION} ]]; then
+  VTK_VERSION=9.1.0
+fi
+
 parse_version_env VTK_VERSION
 curl https://vtk.org/files/release/${VTK_VERSION_MAJOR}.${VTK_VERSION_MINOR}/VTK-${VTK_VERSION_MAJOR}.${VTK_VERSION_MINOR}.${VTK_VERSION_PATCH}.tar.gz \
   --output ${DOWNLOAD_ROOT}/vtk.tar.gz \
   --silent
 extract ${DOWNLOAD_ROOT}/vtk.tar.gz -C ${EXTRACT_ROOT}/vtk
 
-cd ${EXTRACT_ROOT}/vtk/VTK-${VTK_VERSION_MAJOR}.${VTK_VERSION_MINOR}.${VTK_VERSION_PATCH} &&
-mkdir -p build &&
-cd build &&
+cd ${EXTRACT_ROOT}/vtk/VTK-${VTK_VERSION_MAJOR}.${VTK_VERSION_MINOR}.${VTK_VERSION_PATCH}
+patch -i /tmp/vtk_use_mpi.patch
+mkdir -p build
+cd build
 cmake .. \
  -DCMAKE_BUILD_TYPE=Release \
  -DVTK_BUILD_EXAMPLES=OFF \
  -DVTK_BUILD_TESTING=OFF \
  -DVTK_BUILD_DOCUMENTATION=OFF \
  -DVTK_BUILD_SHARED_LIBS=ON \
- -DVTK_USE_MPI=ON \
- -DVTK_GROUP_ENABLE_StandAlone=NO \
+ -DVTK_GROUP_ENABLE_StandAlone=DONT_WANT \
  -DVTK_GROUP_ENABLE_Imaging=NO \
  -DVTK_GROUP_ENABLE_Qt=NO \
  -DVTK_GROUP_ENABLE_Rendering=NO \
  -DVTK_GROUP_ENABLE_Views=NO \
  -DVTK_GROUP_ENABLE_Web=NO \
- -DVTK_WRAP_PYTHON=YES \
- -DVTK_PYTHON_VERSION=3 \
- -DVTK_MODULE_ENABLE_VTK_PythonInterpreter=YES \
- -DVTK_MODULE_ENABLE_VTK_WrappingPythonCore=YES \
- -DVTK_MODULE_ENABLE_VTK_CommonDataModel=YES \
- -DVTK_MODULE_ENABLE_VTK_CommonMisc=YES \
- -DVTK_MODULE_ENABLE_VTK_CommonExecutionModel=YES \
- -DVTK_MODULE_ENABLE_VTK_IOParallelNetCDF=NO \
- -DVTK_MODULE_ENABLE_VTK_IOMPIParallel=NO \
- -DVTK_MODULE_ENABLE_VTK_IOMPIImage=NO \
- -DVTK_MODULE_ENABLE_VTK_FiltersParallelVerdict=NO \
- -DVTK_MODULE_ENABLE_VTK_FiltersParallelGeometry=NO \
- -DVTK_MODULE_ENABLE_VTK_DomainsParallelChemistry=NO \
- -DVTK_MODULE_ENABLE_VTK_FiltersParallelMPI=NO \
- -DVTK_MODULE_ENABLE_VTK_ParallelCore=YES \
- -DVTK_MODULE_ENABLE_VTK_CommonSystem=YES \
- -DVTK_MODULE_ENABLE_VTK_IOLegacy=YES \
- -DVTK_MODULE_ENABLE_VTK_IOCore=YES \
- -DVTK_MODULE_ENABLE_VTK_IOXMLParser=YES \
+ -DVTK_GROUP_ENABLE_MPI=DONT_WANT \
+ -DVTK_USE_MPI=ON \
+ -DVTK_MODULE_ENABLE_VTK_ParallelMPI=YES \
+ -DVTK_MODULE_ENABLE_VTK_IOParallelXML=YES \
  -DVTK_MODULE_ENABLE_VTK_IOXML=YES \
- -DVTK_MODULE_ENABLE_VTK_CommonMath=YES \
- -DVTK_MODULE_ENABLE_VTK_CommonTransforms=YES &&
-cmake --build . &&
+ -DVTK_MODULE_ENABLE_VTK_IOLegacy=YES \
+ -DVTK_ENABLE_KITS=OFF \
+ -DVTK_WRAP_PYTHON=YES \
+ -DVTK_PYTHON_VERSION=3
+cmake --build .
 cmake --install . --prefix ${VTK_DIR}
 
 rm -rf ${DOWNLOAD_ROOT}
