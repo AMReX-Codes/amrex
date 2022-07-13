37b321be6e8ff5db875651fd346311f121b1c9c9
Deepak Rangarajan <deepak.rangarajan@netl.doe.gov>
2022-07-13 19:25:38 -0400

    some more fixes

--- Src/EB/AMReX_EB2_Level_chkpt_file.cpp
+++ Src/EB/AMReX_EB2_Level_chkpt_file.cpp
@@ -23,8 +23,37 @@ ChkptFileLevel::ChkptFileLevel (IndexSpace const* is, ChkptFile const& chkpt_fil
     Box bounding_box = (extend_domain_face) ? domain : domain_grown;
     bounding_box.surroundingNodes();
 
+   BoxList bl(domain);
+   bl.maxSize(max_grid_size);
+    if (m_ngrow != 0) {
+        const IntVect& domlo = domain.smallEnd();
+        const IntVect& domhi = domain.bigEnd();
+        for (auto& b : bl) {
+            for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+                if (m_ngrow[idim] != 0) {
+                    if (b.smallEnd(idim) == domlo[idim]) {
+                        b.growLo(idim,m_ngrow[idim]);
+                    }
+                    if (b.bigEnd(idim) == domhi[idim]) {
+                        b.growHi(idim,m_ngrow[idim]);
+                    }
+                }
+            }
+        }
+    }
+
+
     chkpt_file.fill_from_chkpt_file(m_grids, m_dmap, m_volfrac, m_centroid, m_bndryarea,
             m_bndrycent, m_bndrynorm, m_areafrac, m_facecent, m_edgecent, m_levelset, GFab::ng);
+
+    BoxList uncovered_bl = m_grids.boxList();
+    BoxList covered_bl;
+    for (const auto& bx: bl) {
+        if (!uncovered_bl.contains(BoxList(bx))) {
+            covered_bl.push_back(bx);
+        }
+    }
+
 
     m_mgf.define(m_grids, m_dmap);
     const int ng = GFab::ng;
@@ -41,10 +70,11 @@ ChkptFileLevel::ChkptFileLevel (IndexSpace const* is, ChkptFile const& chkpt_fil
             auto& gfab = m_mgf[mfi];
 
             const auto& levelset = m_levelset.const_array(mfi);
-            const Box& bx = gfab.getLevelSet().box();
+            const Box& bxg2 = amrex::grow(gfab.validbox(),2);
+            const Box& nodal_box = amrex::surroundingNodes(bxg2);
             const auto& ls = gfab.getLevelSet().array();
 
-            AMREX_HOST_DEVICE_PARALLEL_FOR_3D(bx, i, j, k,
+            AMREX_HOST_DEVICE_PARALLEL_FOR_3D(nodal_box, i, j, k,
             {
                 ls(i,j,k) = levelset(i,j,k);
             });
@@ -55,7 +85,6 @@ ChkptFileLevel::ChkptFileLevel (IndexSpace const* is, ChkptFile const& chkpt_fil
     }
 
     Vector<Box> cut_boxes;
-    Vector<Box> covered_boxes;
     for (MFIter mfi(m_grids, m_dmap); mfi.isValid(); ++mfi)
     {
         const Box& vbx = mfi.validbox();
@@ -66,27 +95,24 @@ ChkptFileLevel::ChkptFileLevel (IndexSpace const* is, ChkptFile const& chkpt_fil
         const auto& vfrac = m_volfrac.const_array(mfi);
         const auto& apx = m_areafrac[0].const_array(mfi);
 
-        if (flagfab.getType(gbx & bounding_box) == FabType::covered) {
-            covered_boxes.push_back(vbx);
-        } else if (flagfab.getType(gbx & bounding_box) == FabType::singlevalued) {
+        if (flagfab.getType(gbx & bounding_box) == FabType::singlevalued) {
             cut_boxes.push_back(vbx);
         }
     }
 
     AllGatherBoxes(cut_boxes);
-    AllGatherBoxes(covered_boxes);
 
     //Print() << "cut_boxes = " << cut_boxes.size() << std::endl;
     //Print() << "covered_boxes = " << covered_boxes.size() << std::endl;
 
     if ( cut_boxes.empty() &&
-            !covered_boxes.empty())
+            !covered_bl.isEmpty())
     {
         Abort("AMReX_EB2_Level.H: Domain is completely covered");
     }
 
-    if (!covered_boxes.empty()) {
-        m_covered_grids = BoxArray(BoxList(std::move(covered_boxes)));
+    if (!covered_bl.isEmpty()) {
+        m_covered_grids = BoxArray(covered_bl);
     }
 
     if (cut_boxes.empty()) {
@@ -106,7 +132,7 @@ ChkptFileLevel::ChkptFileLevel (IndexSpace const* is, ChkptFile const& chkpt_fil
     {
         for (MFIter mfi(m_mgf); mfi.isValid(); ++mfi)
         {
-            const auto& gfab = m_mgf[mfi];
+            auto& gfab = m_mgf[mfi];
             const Box& vbx = mfi.validbox();
             const Box& bxg1 = amrex::grow(vbx,1);
             Array4<EBCellFlag> const& cell = m_cellflag.array(mfi);
@@ -114,10 +140,64 @@ ChkptFileLevel::ChkptFileLevel (IndexSpace const* is, ChkptFile const& chkpt_fil
             cellflagtmp.resize(m_cellflag[mfi].box());
             Array4<EBCellFlag> const& ctmp = cellflagtmp.array();
 
-            const auto& facetype = gfab.getFaceType();
-            AMREX_D_TERM(Array4<Type_t const> const& fx = facetype[0].const_array();,
-                         Array4<Type_t const> const& fy = facetype[1].const_array();,
-                         Array4<Type_t const> const& fz = facetype[2].const_array(););
+            auto& facetype = gfab.getFaceType();
+            AMREX_D_TERM(Array4<Type_t> const& fx = facetype[0].array();,
+                         Array4<Type_t> const& fy = facetype[1].array();,
+                         Array4<Type_t> const& fz = facetype[2].array(););
+
+
+            AMREX_D_TERM(Array4<Real const> const& apx = m_areafrac[0].const_array(mfi);,
+                         Array4<Real const> const& apy = m_areafrac[1].const_array(mfi);,
+                         Array4<Real const> const& apz = m_areafrac[2].const_array(mfi););
+
+            auto tmp = amrex::surroundingNodes(vbx,0);
+            const Box& xbx = amrex::grow(amrex::surroundingNodes(vbx,0),0);
+            AMREX_HOST_DEVICE_FOR_3D ( xbx, i, j, k,
+            {
+                if (apx(i,j,k) == 0.0_rt) {
+                    fx(i,j,k) = Type::covered;
+                } else if (apx(i,j,k) == 1.0_rt) {
+                    fx(i,j,k) = Type::regular;
+                }
+            });
+
+            const Box& ybx = amrex::grow(amrex::surroundingNodes(vbx,1),0);
+            AMREX_HOST_DEVICE_FOR_3D ( ybx, i, j, k,
+            {
+                if (apy(i,j,k) == 0.0_rt) {
+                    fy(i,j,k) = Type::covered;
+                } else if (apy(i,j,k) == 1.0_rt) {
+                    fy(i,j,k) = Type::regular;
+                }
+            });
+
+            const Box& zbx = amrex::grow(amrex::surroundingNodes(vbx,2),0);
+            AMREX_HOST_DEVICE_FOR_3D ( zbx, i, j, k,
+            {
+                if (apz(i,j,k) == 0.0_rt) {
+                    fz(i,j,k) = Type::covered;
+                } else if (apz(i,j,k) == 1.0_rt) {
+                    fz(i,j,k) = Type::regular;
+                }
+            });
+
+            AMREX_HOST_DEVICE_FOR_3D ( bxg1, i, j, k,
+            {
+                if (cell(i,j,k).isSingleValued()) {
+                    if (fx(i,j,k) == Type::covered && fx(i+1,j,k) == Type::covered &&
+                        fy(i,j,k) == Type::covered && fy(i,j+1,k) == Type::covered &&
+                        fz(i,j,k) == Type::covered && fz(i,j,k+1) == Type::covered)
+                    {
+                        cell(i,j,k).setCovered();
+                    }
+                    else if (fx(i,j,k) == Type::regular && fx(i+1,j,k) == Type::regular &&
+                             fy(i,j,k) == Type::regular && fy(i,j+1,k) == Type::regular &&
+                             fz(i,j,k) == Type::regular && fz(i,j,k+1) == Type::regular)
+                    {
+                        cell(i,j,k).setRegular();
+                    }
+                }
+            });
 
 
             // Build neighbors.  By default all 26 neighbors are already set.
