AMReX-Codes/amrex

commit 94693291667bd0435819aa09cf28a293da226bf4

MLMG interface (#2858)
These changes are made to support a generic type (i.e., amrex::Any) in MLMG.
This is still work in progress.  But it should not break any existing codes.

Src/Base/AMReX_Any.H
@@ -60,14 +60,17 @@ public:
 private:
     struct innards_base {
         virtual const std::type_info& Type () const = 0;
+        virtual ~innards_base () = default;
     };
 
     template <typename MF>
     struct innards : innards_base
     {
-        innards(MF && mf)
+        innards (MF && mf)
             : m_mf(std::forward<MF>(mf))
             {}
+
+        virtual ~innards () = default;
 
         virtual const std::type_info& Type () const override {
             return typeid(MF);

Src/Base/AMReX_BaseFab.H
@@ -260,7 +260,7 @@ public:
      */
     void clear () noexcept;
 
-    // Release ownership of memory
+    //! Release ownership of memory
     std::unique_ptr<T,DataDeleter> release () noexcept;
 
     //! Returns how many bytes used

Src/Base/AMReX_FabArray.H
@@ -2848,7 +2848,7 @@ FabArray<FAB>::SumBoundary_nowait (int scomp, int ncomp, IntVect const& src_ngho
 
     FabArray<FAB>* tmp = new FabArray<FAB>( boxArray(), DistributionMap(), ncomp, src_nghost, MFInfo(), Factory() );
     amrex::Copy(*tmp, *this, scomp, 0, ncomp, src_nghost);
-    this->setVal(0.0, scomp, ncomp, dst_nghost);
+    this->setVal(typename FAB::value_type(0), scomp, ncomp, dst_nghost);
     this->ParallelCopy_nowait(*tmp,0,scomp,ncomp,src_nghost,dst_nghost,period,FabArrayBase::ADD);
 
     // All local. Operation complete.

Src/LinearSolvers/MLMG/AMReX_MLABecLaplacian.cpp
@@ -323,10 +323,10 @@ MLABecLaplacian::applyMetricTermsCoeffs ()
     for (int alev = 0; alev < m_num_amr_levels; ++alev)
     {
         const int mglev = 0;
-        applyMetricTerm(alev, mglev, m_a_coeffs[alev][mglev]);
+        applyMetricTermToMF(alev, mglev, m_a_coeffs[alev][mglev]);
         for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
         {
-            applyMetricTerm(alev, mglev, m_b_coeffs[alev][mglev][idim]);
+            applyMetricTermToMF(alev, mglev, m_b_coeffs[alev][mglev][idim]);
         }
     }
 #endif

Src/LinearSolvers/MLMG/AMReX_MLCGSolver.H
@@ -40,6 +40,11 @@ public:
                Real            eps_rel,
                Real            eps_abs);
 
+    int solve (Any&       solnL,
+               const Any& rhsL,
+               Real       eps_rel,
+               Real       eps_abs);
+
     void setVerbose (int _verbose) { verbose = _verbose; }
     int getVerbose () const { return verbose; }
 

Src/LinearSolvers/MLMG/AMReX_MLCGSolver.cpp
@@ -79,6 +79,13 @@ MLCGSolver::solve (MultiFab&       sol,
 }
 
 int
+MLCGSolver::solve (Any& sol, const Any& rhs, Real eps_rel, Real eps_abs)
+{
+    AMREX_ASSERT(sol.is<MultiFab>()); // xxxxx TODO: MLCGSolver Any
+    return solve(sol.get<MultiFab>(), rhs.get<MultiFab>(), eps_rel, eps_abs);
+}
+
+int
 MLCGSolver::solve_bicgstab (MultiFab&       sol,
                             const MultiFab& rhs,
                             Real            eps_rel,

Src/LinearSolvers/MLMG/AMReX_MLCellABecLap.H
@@ -59,9 +59,9 @@ public:
     virtual MultiFab const* getACoeffs (int amrlev, int mglev) const = 0;
     virtual Array<MultiFab const*,AMREX_SPACEDIM> getBCoeffs (int amrlev, int mglev) const = 0;
 
-    virtual void applyInhomogNeumannTerm (int amrlev, MultiFab& rhs) const final override;
+    virtual void applyInhomogNeumannTerm (int amrlev, Any& rhs) const final override;
 
-    virtual void applyOverset (int amlev, MultiFab& rhs) const override;
+    virtual void applyOverset (int amlev, Any& rhs) const override;
 
 #if defined(AMREX_USE_HYPRE) && (AMREX_SPACEDIM > 1)
     virtual std::unique_ptr<Hypre> makeHypre (Hypre::Interface hypre_interface) const override;

commit 7ede2a285482680382990eb8b417c3580cd31b87

Fix a new bug introduced in #2858

Src/Base/AMReX_Any.H
@@ -48,11 +48,25 @@ public:
 
     //! Returns a reference to the contained object.
     template <typename MF>
-    MF& get () { return dynamic_cast<innards<MF>&>(*m_ptr).m_mf; }
+    MF& get () {
+        if (auto p0 = dynamic_cast<innards<MF>*>(m_ptr.get())) {
+            return p0->m_mf;
+        } else {
+            return dynamic_cast<innards<MF&>&>(*m_ptr).m_mf;
+        }
+    }
 
     //! Returns a const reference to the contained object.
     template <typename MF>
-    MF const& get () const { return dynamic_cast<innards<MF> const&>(*m_ptr).m_mf; }
+    MF const& get () const {
+        if (auto p0 = dynamic_cast<innards<MF>*>(m_ptr.get())) {
+            return p0->m_mf;
+        } else if (auto p1 = dynamic_cast<innards<MF&>*>(m_ptr.get())) {
+            return p1->m_mf;
+        } else {
+            return dynamic_cast<innards<MF const&> const&>(*m_ptr).m_mf;
+        }
+    }
 
     template <typename MF>
     bool is () const { return m_ptr->Type() == typeid(MF); }

Src/LinearSolvers/MLMG/AMReX_MLMG.cpp
@@ -1152,7 +1152,7 @@ MLMG::compResidual (const Vector<MultiFab*>& a_res, const Vector<MultiFab*>& a_s
     {
         if (cf_strategy == CFStrategy::ghostnodes || a_sol[alev]->nGrowVect() == ng_sol)
         {
-            sol[alev] = linop.AnyMakeAlias(a_sol[alev]);
+            sol[alev] = linop.AnyMakeAlias(*a_sol[alev]);
             sol_is_alias[alev] = true;
         }
         else