6f5256f56045f92d29b0dfaa07ff24bf2d8128a6
Axel Huebl <axel.huebl@plasma.ninja>
2022-08-09 15:52:12 -0600

    Add: `MultiFab::sum_unique`
    This provides a new method to sum values in a `MultiFab`.
    For non-cell-centered data, `MultiFab::sum` double counts box
    boundary values that are owned by multiple boxes. This provides
    a function that does not double count these and provides a
    quick way to get only the sum of physically unique values.

--- Src/Base/AMReX_MultiFab.cpp
+++ Src/Base/AMReX_MultiFab.cpp
@@ -5,6 +5,7 @@
 #include <AMReX_BLProfiler.H>
 #include <AMReX_iMultiFab.H>
 #include <AMReX_FabArrayUtility.H>
+#include <AMReX_REAL.H>
 
 #ifdef AMREX_MEM_PROFILING
 #include <AMReX_MemProfiler.H>
@@ -1582,6 +1583,53 @@ MultiFab::sum (int comp, bool local) const
     if (!local) {
         ParallelAllReduce::Sum(sm, ParallelContext::CommunicatorSub());
     }
+
+    return sm;
+}
+
+Real
+MultiFab::sum_unique (int comp,
+                      const Periodicity& period) const
+{
+    BL_PROFILE("MultiFab::sum_unique()");
+
+    using namespace amrex::literals;
+
+    // Owner is the grid with the lowest grid number containing the data
+    std::unique_ptr<iMultiFab> owner_mask = OwnerMask(period);
+
+    Real sm = Real(0.0);
+#ifdef AMREX_USE_GPU
+    if (Gpu::inLaunchRegion()) {
+        auto const& ma = this->const_arrays();
+        auto const& msk = owner_mask->const_array();
+        sm = ParReduce(TypeList<ReduceOpSum>{}, TypeList<Real>{}, *this, IntVect(0),
+        [=] AMREX_GPU_DEVICE (int box_no, int i, int j, int k) noexcept
+                       -> GpuTuple<Real>
+        {
+            return msk(i,j,k,comp) ? ma[box_no](i,j,k,comp) : 0.0_rt;
+        });
+    } else
+#endif
+    {
+#ifdef AMREX_USE_OMP
+#pragma omp parallel if (!system::regtest_reduction) reduction(+:sm)
+#endif
+        for (MFIter mfi(*this,true); mfi.isValid(); ++mfi)
+        {
+            Box const& bx = mfi.tilebox();
+            Array4<Real const> const& a = this->const_array(mfi);
+            Array4<int const> const& msk = owner_mask->const_array(mfi);
+            Real tmp = 0.0_rt;
+            AMREX_LOOP_3D(bx, i, j, k,
+            {
+                tmp += msk(i,j,k,comp) ? a(i,j,k,comp) : 0.0_rt;
+            });
+            sm += tmp; // Do it this way so that it does not break regression tests.
+        }
+    }
+
+    ParallelAllReduce::Sum(sm, ParallelContext::CommunicatorSub());
 
     return sm;
 }
