75c018593613e76ed26c58a76dd2af29806c6910
Andrew Myers <atmyers2@gmail.com>
2022-11-08 11:25:13 -0800

    pacify clang

--- Tests/Particles/CheckpointRestart/main.cpp
+++ Tests/Particles/CheckpointRestart/main.cpp
@@ -118,9 +118,10 @@ void test ()
     int num_particles = nppc * AMREX_D_TERM(ncells, * ncells, * ncells);
     bool serialize = false;
     int iseed = 451;
-    MyPC::ParticleInitData pdata = {1.0, 2.0, 3.0, 4.0, 5, 6.0,
-                                    7.0, 8.0, 9.0, 10.0, 11.0,
-                                    12.0, 13.0, 14, 15, 16};
+    MyPC::ParticleInitData pdata = {{1.0, 2.0, 3.0, 4.0},
+                                    {5},
+                                    {6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0},
+                                    {14, 15, 16}};
 
     if (nparticlefile > 0) {
         amrex::Print() << "Init particles ..." << std::endl;
