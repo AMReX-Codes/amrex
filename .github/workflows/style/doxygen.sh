a42a3b268cf2634fa625b021863d743f33abc4f5
Axel Huebl <axel.huebl@plasma.ninja>
2022-12-09 14:43:23 -0800

    CI Script: Fix Comment

--- .github/workflows/style/doxygen.sh
+++ .github/workflows/style/doxygen.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 #
-# Copyright 2020 Axel Huebl
+# Copyright 2022 The AMReX Community
 #
+# Author: Axel Huebl
 # License: BSD-3-Clause-LBNL
-
-# search recursive inside a folder if a file contains tabs
-#
-# @result 0 if no files are found, else 1
 #
 
 set -eu -o pipefail
