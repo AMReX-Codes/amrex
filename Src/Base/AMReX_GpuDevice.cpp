AMReX-Codes/amrex

commit 38572ef1ff1da8fe89482183f66bf5ba6b91813d

Gpu stream thread safety (#1064)
* Make gpu streams omp cpu thread safe.  Add OpenMP namespace for convenience.

* use the new OpenMP:: functions

* fix typo

Src/Base/AMReX_GpuDevice.cpp
@@ -58,7 +58,7 @@ int  Device::max_blocks_per_launch = 640;
 
 std::array<gpuStream_t,Device::max_gpu_streams> Device::gpu_streams;
 gpuStream_t                                     Device::gpu_default_stream;
-gpuStream_t                                     Device::gpu_stream;
+Vector<gpuStream_t>                             Device::gpu_stream;
 gpuDeviceProp_t                                 Device::device_prop;
 
 constexpr int                                   Device::warp_size;
@@ -359,7 +359,7 @@ Device::Finalize ()
         delete s.queue;
         s.queue = nullptr;
     }
-    gpu_stream.queue = nullptr;
+    gpu_stream.clear();
     delete gpu_default_stream.queue;
     gpu_default_stream.queue = nullptr;
 #endif
@@ -473,7 +473,7 @@ Device::initialize_gpu ()
     }
 #endif
 
-    gpu_stream = gpu_default_stream;
+    gpu_stream.resize(OpenMP::get_max_threads(), gpu_default_stream);
 
     ParmParse pp("device");
 
@@ -539,13 +539,13 @@ Device::setStreamIndex (const int idx) noexcept
 {
 #ifdef AMREX_USE_GPU
     if (idx < 0) {
-        gpu_stream = gpu_default_stream;
+        gpu_stream[OpenMP::get_thread_num()] = gpu_default_stream;
 
 #ifdef AMREX_USE_ACC
         amrex_set_acc_stream(acc_async_sync);
 #endif
     } else {
-        gpu_stream = gpu_streams[idx % max_gpu_streams];
+        gpu_stream[OpenMP::get_thread_num()] = gpu_streams[idx % max_gpu_streams];
 
 #ifdef AMREX_USE_ACC
         amrex_set_acc_stream(idx % max_gpu_streams);
@@ -558,16 +558,16 @@ Device::setStreamIndex (const int idx) noexcept
 gpuStream_t
 Device::resetStream () noexcept
 {
-    gpuStream_t r = gpu_stream;
-    gpu_stream = gpu_default_stream;
+    gpuStream_t r = gpu_stream[OpenMP::get_thread_num()];
+    gpu_stream[OpenMP::get_thread_num()] = gpu_default_stream;
     return r;
 }
 
 gpuStream_t
 Device::setStream (gpuStream_t s) noexcept
 {
-    gpuStream_t r = gpu_stream;
-    gpu_stream = s;
+    gpuStream_t r = gpu_stream[OpenMP::get_thread_num()];
+    gpu_stream[OpenMP::get_thread_num()] = s;
     return r;
 }
 #endif
@@ -599,8 +599,8 @@ Device::streamSynchronize () noexcept
         amrex::Abort(std::string("streamSynchronize: ")+ex.what()+"!!!!!");
     }
 #else
-    AMREX_HIP_OR_CUDA( AMREX_HIP_SAFE_CALL(hipStreamSynchronize(gpu_stream));,
-                       AMREX_CUDA_SAFE_CALL(cudaStreamSynchronize(gpu_stream)); )
+    AMREX_HIP_OR_CUDA( AMREX_HIP_SAFE_CALL(hipStreamSynchronize(gpu_stream[OpenMP::get_thread_num()]));,
+                       AMREX_CUDA_SAFE_CALL(cudaStreamSynchronize(gpu_stream[OpenMP::get_thread_num()])); )
 #endif
 }
 

Src/Base/AMReX_BoxArray.cpp
@@ -10,9 +10,7 @@
 #include <AMReX_MemProfiler.H>
 #endif
 
-#ifdef _OPENMP
-#include <omp.h>
-#endif
+#include <AMReX_OpenMP.H>
 
 namespace amrex {
 
@@ -1023,10 +1021,8 @@ BoxArray::minimalBox () const
 #pragma omp parallel
 #endif
 	    {
-#ifndef _OPENMP
-		int tid = 0;
-#else
-		int tid = omp_get_thread_num();
+                int tid = OpenMP::get_thread_num();
+#ifdef _OPENMP
 #pragma omp for
 #endif
 		for (int i = 0; i < N; ++i) {
@@ -1075,10 +1071,8 @@ BoxArray::minimalBox (Long& npts_avg_box) const
 #pragma omp parallel reduction(+:npts_tot)
 #endif
             {
-#ifndef _OPENMP
-                int tid = 0;
-#else
-                int tid = omp_get_thread_num();
+                int tid = OpenMP::get_thread_num();
+#ifdef _OPENMP
 #pragma omp for
 #endif
                 for (int i = 0; i < N; ++i) {

Src/Base/AMReX_Geometry.cpp
@@ -9,9 +9,7 @@
 #include <AMReX_Utility.H>
 #include <AMReX_SPACE.H>
 
-#ifdef _OPENMP
-#include <omp.h>
-#endif
+#include <AMReX_OpenMP.H>
 
 namespace amrex {
 
@@ -119,9 +117,7 @@ Geometry::Setup (const RealBox* rb, int coord, int const* isper) noexcept
 
     if (gg->ok) return;
 
-#ifdef _OPENMP
-    BL_ASSERT(!omp_in_parallel());
-#endif
+    BL_ASSERT(!OpenMP::in_parallel());
 
     ParmParse pp("geometry");
 

Src/Base/AMReX_GpuDevice.H
@@ -9,6 +9,8 @@
 #include <AMReX_GpuTypes.H>
 #include <AMReX_GpuError.H>
 #include <AMReX_GpuControl.H>
+#include <AMReX_OpenMP.H>
+#include <AMReX_Vector.H>
 
 namespace amrex {
 
@@ -45,17 +47,17 @@ public:
     static void Finalize ();
 
 #if defined(AMREX_USE_GPU)
-    static gpuStream_t gpuStream () noexcept { return gpu_stream; }
+    static gpuStream_t gpuStream () noexcept { return gpu_stream[OpenMP::get_thread_num()]; }
     static gpuStream_t nullStream () noexcept { return gpu_default_stream; }
 #ifdef AMREX_USE_CUDA
     // for backward compatibility
-    static cudaStream_t cudaStream () noexcept { return gpu_stream; }
+    static cudaStream_t cudaStream () noexcept { return gpu_stream[OpenMP::get_thread_num()]; }
 #endif
 #ifdef AMREX_USE_DPCPP
     static sycl::queue& nullQueue () noexcept { return *(gpu_default_stream.queue); }
-    static sycl::queue& streamQueue () noexcept { return *(gpu_stream.queue); }
+    static sycl::queue& streamQueue () noexcept { return *(gpu_stream[OpenMP::get_thread_num()].queue); }
     static sycl::queue& streamQueue (int i) noexcept { return *(gpu_streams[i].queue); }
-    static bool onNullStream () noexcept { return gpu_stream == gpu_default_stream; }
+    static bool onNullStream () noexcept { return gpu_stream[OpenMP::get_thread_num()] == gpu_default_stream; }
     static bool onNullStream (gpuStream_t stream) noexcept { return stream == gpu_default_stream; }
 #endif
 #endif
@@ -149,7 +151,7 @@ private:
 
     static std::array<gpuStream_t,max_gpu_streams> gpu_streams;
     static gpuStream_t gpu_default_stream;
-    static gpuStream_t gpu_stream;
+    static Vector<gpuStream_t> gpu_stream;
     static gpuDeviceProp_t device_prop;
     static int max_blocks_per_launch;
 

Src/Base/AMReX_MFIter.cpp
@@ -2,6 +2,7 @@
 #include <AMReX_MFIter.H>
 #include <AMReX_FabArray.H>
 #include <AMReX_FArrayBox.H>
+#include <AMReX_OpenMP.H>
 
 namespace amrex {
 
@@ -144,11 +145,7 @@ MFIter::MFIter (const BoxArray& ba, const DistributionMapping& dm, const MFItInf
     tile_size(info.tilesize),
     flags(info.do_tiling ? Tiling : 0),
     streams(info.num_streams),
-#ifdef _OPENMP
-    dynamic(info.dynamic && (omp_get_num_threads() > 1)),
-#else
-    dynamic(false),
-#endif
+    dynamic(info.dynamic && (OpenMP::get_num_threads() > 1)),
     device_sync(info.device_sync),
     index_map(nullptr),
     local_index_map(nullptr),
@@ -180,11 +177,7 @@ MFIter::MFIter (const FabArrayBase& fabarray_, const MFItInfo& info)
     tile_size(info.tilesize),
     flags(info.do_tiling ? Tiling : 0),
     streams(info.num_streams),
-#ifdef _OPENMP
-    dynamic(info.dynamic && (omp_get_num_threads() > 1)),
-#else
-    dynamic(false),
-#endif
+    dynamic(info.dynamic && (OpenMP::get_num_threads() > 1)),
     device_sync(info.device_sync),
     index_map(nullptr),
     local_index_map(nullptr),
@@ -650,13 +643,8 @@ MFGhostIter::Initialize ()
     }
 #endif
 
-    int tid = 0;
-    int nthreads = 1;
-#ifdef _OPENMP
-    nthreads = omp_get_num_threads();
-    if (nthreads > 1)
-	tid = omp_get_thread_num();
-#endif
+    int tid = OpenMP::get_thread_num();
+    int nthreads = OpenMP::get_num_threads();
 
     int npes = nworkers*nthreads;
     int pid = rit*nthreads+tid;

Src/Base/AMReX_MemPool.cpp
@@ -1,7 +1,3 @@
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
 #include <iostream>
 #include <limits>
 #include <algorithm>
@@ -13,6 +9,7 @@
 #include <AMReX_CArena.H>
 #include <AMReX_MemPool.H>
 #include <AMReX_Vector.H>
+#include <AMReX_OpenMP.H>
 
 #ifdef AMREX_MEM_PROFILING
 #include <AMReX_MemProfiler.H>
@@ -48,12 +45,7 @@ void amrex_mempool_init ()
         ParmParse pp("fab");
 	pp.query("init_snan", init_snan);
 
-	int nthreads = 1;
-
-#ifdef _OPENMP
-	nthreads = omp_get_max_threads();
-#endif
-
+	int nthreads = OpenMP::get_max_threads();
 
 #ifdef USE_PERILLA_PTHREADS
 #ifdef _OPENMP
@@ -106,11 +98,7 @@ void amrex_mempool_finalize ()
 
 void* amrex_mempool_alloc (size_t nbytes)
 {
-  int tid=0;
-
-#ifdef _OPENMP
-  tid = omp_get_thread_num();
-#endif
+  int tid = OpenMP::get_thread_num();
 
 #ifdef USE_PERILLA_PTHREADS
 #ifdef _OPENMP
@@ -124,11 +112,7 @@ void* amrex_mempool_alloc (size_t nbytes)
 
 void amrex_mempool_free (void* p) 
 {
-  int tid=0;
-
-#ifdef _OPENMP
-  tid = omp_get_thread_num();
-#endif
+  int tid = OpenMP::get_thread_num();
 
 #ifdef USE_PERILLA_PTHREADS
 #ifdef _OPENMP

Src/Base/AMReX_OpenMP.H
@@ -1,0 +1,31 @@
+#ifndef AMREX_OPENMP_H_
+#define AMREX_OPENMP_H_
+
+#ifdef _OPENMP
+#include <omp.h>
+
+namespace amrex {
+namespace OpenMP {
+
+    inline int get_num_threads () { return omp_get_num_threads(); }
+    inline int get_max_threads () { return omp_get_max_threads(); }
+    inline int get_thread_num  () { return omp_get_thread_num();  }
+    inline int in_parallel     () { return omp_in_parallel();     }
+
+}}
+
+#else
+
+namespace amrex {
+namespace OpenMP {
+
+    constexpr int get_num_threads () { return 1; }
+    constexpr int get_max_threads () { return 1; }
+    constexpr int get_thread_num  () { return 0; }
+    constexpr int in_parallel     () { return false; }
+
+}}
+
+#endif
+
+#endif

Src/Base/AMReX_Random.cpp
@@ -8,13 +8,10 @@
 #include <AMReX_BlockMutex.H>
 #include <AMReX_GpuLaunch.H>
 #include <AMReX_GpuDevice.H>
+#include <AMReX_OpenMP.H>
 
 #ifdef AMREX_USE_HIP
 #include <hiprand.hpp>
-#endif
-
-#ifdef _OPENMP
-#include <omp.h>
 #endif
 
 #ifdef AMREX_USE_HIP
@@ -53,23 +50,17 @@ namespace
 void
 amrex::InitRandom (amrex::ULong seed, int nprocs)
 {
-#ifdef _OPENMP
-    nthreads = omp_get_max_threads();
-#else
-    nthreads = 1;
-#endif
+    nthreads = OpenMP::get_max_threads();
     generators.resize(nthreads);
 
 #ifdef _OPENMP
 #pragma omp parallel
+#endif
     {
-        int tid = omp_get_thread_num();
+        int tid = OpenMP::get_thread_num();
         amrex::ULong init_seed = seed + tid*nprocs;
         generators[tid].seed(init_seed);
     }
-#else
-    generators[0].seed(seed);
-#endif
 
 #ifdef AMREX_USE_GPU
     DeallocateRandomSeedDevArray();
@@ -143,13 +134,8 @@ amrex::RandomNormal (amrex::Real mean, amrex::Real stddev)
 
 #else
 
-#ifdef _OPENMP
-    int tid = omp_get_thread_num();
-#else
-    int tid = 0;
-#endif
-
     std::normal_distribution<amrex::Real> distribution(mean, stddev);
+    int tid = OpenMP::get_thread_num();
     rand = distribution(generators[tid]);
 
 #endif
@@ -189,14 +175,10 @@ amrex::Random ()
 
 #else     // on the host
 
-#ifdef _OPENMP
-    int tid = omp_get_thread_num();
-#else
-    int tid = 0;
-#endif
-
     std::uniform_real_distribution<amrex::Real> distribution(0.0, 1.0);
+    int tid = OpenMP::get_thread_num();
     rand = distribution(generators[tid]);
+
 #endif
 
     return rand;
@@ -230,13 +212,8 @@ amrex::RandomPoisson (amrex::Real lambda)
 
 #else
 
-#ifdef _OPENMP
-    const auto tid = omp_get_thread_num();
-#else
-    const auto tid = 0;
-#endif
-
     std::poisson_distribution<unsigned int> distribution(lambda);
+    int tid = OpenMP::get_thread_num();
     rand = distribution(generators[tid]);
 
 #endif
@@ -273,12 +250,8 @@ amrex::Random_int (unsigned int n)
 
 #else // on the host
 
-#ifdef _OPENMP
-    int tid = omp_get_thread_num();
-#else
-    int tid = 0;
-#endif
     std::uniform_int_distribution<unsigned int> distribution(0, n-1);
+    int tid = OpenMP::get_thread_num();
     return distribution(generators[tid]);
 
 #endif
@@ -287,12 +260,8 @@ amrex::Random_int (unsigned int n)
 AMREX_GPU_HOST amrex::ULong
 amrex::Random_long (amrex::ULong n)
 {
-#ifdef _OPENMP
-    int tid = omp_get_thread_num();
-#else
-    int tid = 0;
-#endif
     std::uniform_int_distribution<amrex::ULong> distribution(0, n-1);
+    int tid = OpenMP::get_thread_num();
     return distribution(generators[tid]);
 }
 

commit 86e767c7119155582585feb05a15484d79d141f8

Fix bug in Device::Finalize: Forgot to clear streams
We forgot to clear the non-owning gpu_stream vector.  They still held the
old streams after we destroyed the streams in Device::Finalize.  This bug
affects codes that call amrex::Initialize and amrex::Finalize repeatedly.

Note that we have the non-owning gpu stream vector for OMP CPU thread
safety.

The bug was introduced in #1064.

Src/Base/AMReX_GpuDevice.cpp
@@ -385,12 +385,6 @@ Device::Finalize ()
 #ifdef AMREX_USE_GPU
     Device::profilerStop();
 
-    for (int i = 0; i < max_gpu_streams; ++i)
-    {
-        AMREX_HIP_OR_CUDA( AMREX_HIP_SAFE_CALL( hipStreamDestroy(gpu_stream_pool[i]));,
-                          AMREX_CUDA_SAFE_CALL(cudaStreamDestroy(gpu_stream_pool[i])); );
-    }
-
 #ifdef AMREX_USE_DPCPP
     sycl_context.reset();
     sycl_device.reset();
@@ -398,8 +392,15 @@ Device::Finalize ()
         delete s.queue;
         s.queue = nullptr;
     }
-    gpu_stream.clear();
+#else
+    for (int i = 0; i < max_gpu_streams; ++i)
+    {
+        AMREX_HIP_OR_CUDA( AMREX_HIP_SAFE_CALL( hipStreamDestroy(gpu_stream_pool[i]));,
+                          AMREX_CUDA_SAFE_CALL(cudaStreamDestroy(gpu_stream_pool[i])); );
+    }
 #endif
+
+    gpu_stream.clear();
 
 #ifdef AMREX_USE_ACC
     amrex_finalize_acc();