97b6d7171dad853a9caf23219f11d18fce24b82e
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-06 15:14:44 -0700

    CI: Retry Apt Repos
    Since Intel Repos are often out-of-sync with each others
    for apt packages provided (checksum errors), we try to
    pause and retry up to five times to get them installed.
    
    Also set apt retries to 3 for `apt update`.

--- .github/workflows/dependencies/dependencies_mac.sh
+++ .github/workflows/dependencies/dependencies_mac.sh
@@ -7,6 +7,11 @@
 
 set -eu -o pipefail
 
+# `man apt.conf`:
+#   Number of retries to perform. If this is non-zero APT will retry
+#   failed files the given number of times.
+echo 'Acquire::Retries "3";' | sudo tee /etc/apt/apt.conf.d/80-retries
+
 brew update
 brew install gfortran || true
 brew install libomp || true
