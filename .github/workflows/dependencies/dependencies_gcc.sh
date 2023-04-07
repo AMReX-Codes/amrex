2f68f00d5f56d03dc4c9930f688c89c5621c33b6
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-06 19:30:39 -0700

    CI: Retry Apt Repos (#3242)
    Since Intel Repos are often out-of-sync with each others for apt
    packages provided (checksum errors), we try to pause and retry up to
    five times to get them installed.
    
    Also set apt retries to 3 for `apt update`.

--- .github/workflows/dependencies/dependencies_gcc.sh
+++ .github/workflows/dependencies/dependencies_gcc.sh
@@ -7,6 +7,11 @@
 
 set -eu -o pipefail
 
+# `man apt.conf`:
+#   Number of retries to perform. If this is non-zero APT will retry
+#   failed files the given number of times.
+echo 'Acquire::Retries "3";' | sudo tee /etc/apt/apt.conf.d/80-retries
+
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 
