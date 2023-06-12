aec8ab0d584202e335ed9ca1a5e078bdaafcea1a
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-06-12 15:56:41 -0700

    Remove unused options

--- .github/workflows/dependencies/dependencies_codeplay.sh
+++ .github/workflows/dependencies/dependencies_codeplay.sh
@@ -6,6 +6,6 @@
 
 set -eu -o pipefail
 
-curl -o oneapi_nvidia.sh -LJO "https://developer.codeplay.com/api/v1/products/download?product=oneapi&variant=nvidia&filters[]=linux&aat=$1"
+curl -o oneapi_nvidia.sh -L "https://developer.codeplay.com/api/v1/products/download?product=oneapi&variant=nvidia&filters[]=linux&aat=$1"
 chmod +x oneapi_nvidia.sh
 sudo ./oneapi_nvidia.sh --yes
