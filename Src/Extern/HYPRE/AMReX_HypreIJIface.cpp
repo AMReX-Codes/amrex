3f3867ce008ddcc0d40a6617ee9c524ceaf43dc2
Paul Mullowney <Paul.Mullowney@nrel.gov>
2023-06-22 11:17:48 -0400

    Adding more hooks for ILU smoothers

--- Src/Extern/HYPRE/AMReX_HypreIJIface.cpp
+++ Src/Extern/HYPRE/AMReX_HypreIJIface.cpp
@@ -316,6 +316,10 @@ void HypreIJIface::boomeramg_precond_configure (const std::string& prefix)
             hpp("bamg_ilu_type", HYPRE_BoomerAMGSetILUType);
             hpp("bamg_ilu_level", HYPRE_BoomerAMGSetILULevel);
             hpp("bamg_ilu_max_iter", HYPRE_BoomerAMGSetILUMaxIter);
+            hpp("bamg_ilu_reordering_type", HYPRE_BoomerAMGSetILULocalReordering);
+            hpp("bamg_ilu_tri_solve", HYPRE_BoomerAMGSetILUTriSolve);
+            hpp("bamg_ilu_lower_jacobi_iters", HYPRE_BoomerAMGSetILULowerJacobiIters);
+            hpp("bamg_ilu_upper_jacobi_iters", HYPRE_BoomerAMGSetILUUpperJacobiIters);
         }
         else if (smooth_type == 7) { // Pilut
             hpp("bamg_smooth_num_sweeps", HYPRE_BoomerAMGSetSmoothNumSweeps);
