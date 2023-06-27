8a352bf881a367b17690ef144bf2b69f6ef70478
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2023-06-27 12:48:44 +0100

    Further simplify LLVM/CUDA dependencies file

--- .github/workflows/dependencies/dependencies_llvm_cuda11_clang15.sh
+++ .github/workflows/dependencies/dependencies_llvm_cuda11_clang15.sh
@@ -8,20 +8,12 @@ set -eu -o pipefail
 
 sudo apt-get -qqq update
 sudo apt-get install -y \
-    build-essential     \
-    ca-certificates     \
     libunwind-15        \
     libunwind-15-dev    \
     clang-15            \
     libc++-15-dev       \
     libc++abi-15-dev    \
     libc++1-15          \
-    libc++abi1-15       \
-    cmake               \
-    gnupg               \
-    libopenmpi-dev      \
-    openmpi-bin         \
-    pkg-config          \
-    wget
+    libc++abi1-15
 
 .github/workflows/dependencies/dependencies_nvcc.sh 11.7
