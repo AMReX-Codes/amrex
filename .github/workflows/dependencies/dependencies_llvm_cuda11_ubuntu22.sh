c961fd9bb3cdf284e36128f86cff4d8eb5341bab
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2023-06-27 11:58:43 +0100

    Use new unified nvcc dependencies file in existing workflows

--- .github/workflows/dependencies/dependencies_llvm_cuda11_ubuntu22.sh
+++ .github/workflows/dependencies/dependencies_llvm_cuda11_ubuntu22.sh
@@ -24,15 +24,4 @@ sudo apt-get install -y \
     pkg-config          \
     wget
 
-curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update
-sudo apt-get install -y \
-    cuda-command-line-tools-11-7 \
-    cuda-compiler-11-7           \
-    cuda-cupti-dev-11-7          \
-    cuda-minimal-build-11-7      \
-    cuda-nvml-dev-11-7           \
-    cuda-nvtx-11-7               \
-    libcurand-dev-11-7
-sudo ln -s cuda-11.7 /usr/local/cuda
+.github/workflows/dependencies/dependencies_nvcc.sh 11.7
