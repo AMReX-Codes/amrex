2ff27084128d92ed40e30692c04ddfe0abe91e14
atmyers <atmyers2@gmail.com>
2023-06-28 15:25:40 -0700

    add sanity checks

--- Src/Base/AMReX_PODVector.cpp
+++ Src/Base/AMReX_PODVector.cpp
@@ -9,5 +9,13 @@ namespace amrex::VectorGrowthStrategy
     void Initialize () {
         ParmParse pp("amrex");
         pp.queryAdd("vector_growth_factor", growth_factor);
+
+        // sanity checks
+        auto eps = std::numeric_limits<Real>::epsilon();
+        auto huge = 1000._rt;  // huge enough for our purposes...
+        AMREX_ALWAYS_ASSERT_WITH_MESSAGE(growth_factor - 1.0_rt >= eps,
+            "User-specified vector growth factor is too small.");
+        AMREX_ALWAYS_ASSERT_WITH_MESSAGE(growth_factor < huge,
+            "User-specified vector growth factor is too large.");
     }
 }
