6a4df9ba26f1fb1357ecfbba54b888b65d49e21f
atmyers <atmyers2@gmail.com>
2023-06-30 15:00:10 -0700

    add function for setting growth factor

--- Src/Base/AMReX_PODVector.cpp
+++ Src/Base/AMReX_PODVector.cpp
@@ -6,28 +6,40 @@ namespace amrex::VectorGrowthStrategy
 {
     Real growth_factor = 1.5_rt;
 
+    // clamp user input to reasonable values
+    constexpr Real min_factor = 1.001_rt;
+    constexpr Real max_factor = 4._rt;
+
+    namespace detail
+    {
+        void ValidateUserInput() {
+            if (growth_factor < min_factor) {
+                if (Verbose()) {
+                    amrex::Print() << "Warning: user-provided vector growth factor is too small."
+                                   << " Clamping to " << min_factor << ". \n";
+                }
+                growth_factor = min_factor;
+            }
+
+            if (growth_factor > max_factor) {
+                if (Verbose()) {
+                    amrex::Print() << "Warning: user-provided vector growth factor is too large."
+                                   << " Clamping to " << max_factor << ". \n";
+                }
+                growth_factor = max_factor;
+            }
+        }
+    }
+
     void Initialize () {
         ParmParse pp("amrex");
         pp.queryAdd("vector_growth_factor", growth_factor);
 
-        // clamp user input to reasonable values
-        constexpr Real min_factor = 1.05_rt;
-        constexpr Real max_factor = 4._rt;
+        detail::ValidateUserInput();
+    }
 
-        if (growth_factor < min_factor) {
-            if (Verbose()) {
-                amrex::Print() << "Warning: user-provided vector growth factor is too small."
-                               << " Clamping to " << min_factor << ". \n";
-            }
-            growth_factor = min_factor;
-        }
-
-        if (growth_factor > max_factor) {
-            if (Verbose()) {
-                amrex::Print() << "Warning: user-provided vector growth factor is too large."
-                               << " Clamping to " << max_factor << ". \n";
-            }
-            growth_factor = max_factor;
-        }
+    void SetGrowthFactor (Real a_factor) {
+        growth_factor = a_factor;
+        detail::ValidateUserInput();
     }
 }
