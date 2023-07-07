AMReX-Codes/amrex

commit ca706580b5fd28296608a18ef7ab29a9149b4438

Multi-Dim Buildsystem Support (#3309)
## Summary

This updates the CMake build system to allow to build multiple AMReX
libraries *at once*, by building multiple, uniquely-named
`(lib)amrex_[1|2|3]d.[so|a|dll|lib]` artifacts. Developers can request
this by specifying a list of dims instead of a single one during
configure time, e.g., `-DAMReX_SPACEDIM="1;2;3"`.

Providing this functionality will significantly reduce deployment
burdens for:
- conda packages
- spack packages
- Python modules & packages
- probably Julia packages (Johannes, blink twice when you read this)
- HPC modules

because all dimension-related variants can be shipped _at once_ and _do
not produce multiple, conflicting packages_ anymore.

Apps can use the same mechanism to pick up the dimensional variants and
build multiple artifacts. (This is something we already do, but cannot
generalize to package mangers just yet.)

### Backward Compatibility

To stay backwards compatible, the *last* (or traditionally, *only*)
passed dimensionality will define a few legacy artifacts:
- [x] the `amrex::amrex`/`amrex` CMake targets
- [x] the `libamrex.[so|a]`/`amrex.[dll|lib]` artifact (symlink)
- [x] the `AMReX_Config.H` file: defaults to the last dim in
`AMReX_SPACEDIM` and defines `AMREX_SPACEDIM` via a public CLI define
now

## Additional background

I recently wrote an analysis piece lamenting that [compile time software
variants that define exclusive
features](https://bssw.io/blog_posts/rethinking-software-variants) are a
productivity and usability challenge. One particular challenge we face
in the shipping of WarpX and pyAMReX is that users (and developers)
would like to seamlessly switch between the dims (1-3D) of our codes.
Switching between compile-time and runtime description for such
elementary workflows can and should be avoided.

Generally, this problem can be solved with more general interfaces in

Tools/CMake/AMReXInstallHelpers.cmake
@@ -62,6 +62,22 @@ function (install_amrex_targets)
           NAMESPACE AMReX::
           DESTINATION lib/cmake/AMReX )
 
+       #
+       # alias: last element will be legacy target
+       #
+       list(LENGTH AMReX_SPACEDIM list_len)
+       math(EXPR list_last "${list_len} - 1")
+       list(GET AMReX_SPACEDIM ${list_last} AMReX_SPACEDIM_LAST)
+
+       # legacy symlink for: libamrex.[so|a] / amrex.[dll.lib]
+       #   escape spaces for generated cmake_install.cmake file
+       file(TO_CMAKE_PATH "${CMAKE_INSTALL_PREFIX}/lib" ABS_INSTALL_LIB_DIR)
+       install(CODE "file(CREATE_LINK
+           $<TARGET_FILE_NAME:amrex_${AMReX_SPACEDIM_LAST}d>
+           \"${ABS_INSTALL_LIB_DIR}/$<TARGET_FILE_PREFIX:amrex_${AMReX_SPACEDIM_LAST}d>amrex$<TARGET_FILE_SUFFIX:amrex_${AMReX_SPACEDIM_LAST}d>\"
+           COPY_ON_ERROR SYMBOLIC)"
+       )
+
        # Install fortran modules if Fortran is enabled
        get_property(_lang GLOBAL PROPERTY ENABLED_LANGUAGES)
        if ("Fortran" IN_LIST _lang AND "amrex" IN_LIST _targets)

commit 72e37c14dbfdbdcc65e42cb7fc2e6df101085984

Fix Fortran module installation
It was broken in #3309, in which multi-D support was added.

Tools/CMake/AMReXInstallHelpers.cmake
@@ -80,8 +80,8 @@ function (install_amrex_targets)
 
        # Install fortran modules if Fortran is enabled
        get_property(_lang GLOBAL PROPERTY ENABLED_LANGUAGES)
-       if ("Fortran" IN_LIST _lang AND "amrex" IN_LIST _targets)
-          get_target_property(_mod_dir amrex Fortran_MODULE_DIRECTORY )
+       if ("Fortran" IN_LIST _lang AND "amrex_${AMReX_SPACEDIM_LAST}d" IN_LIST _targets)
+          get_target_property(_mod_dir "amrex_${AMReX_SPACEDIM_LAST}d" Fortran_MODULE_DIRECTORY )
           install( DIRECTORY ${_mod_dir}/ DESTINATION include ) # Trailing backslash is crucial here!
        endif ()
 