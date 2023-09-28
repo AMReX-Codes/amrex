1bfac24ebcd68fa48474fa31dcd91651cc33aa19
Edward Basso <edwardebasso@gmail.com>
2023-09-28 16:49:59 -0700

    Introduce IndexTypeSet class

--- Src/Base/AMReX_IndexType.cpp
+++ Src/Base/AMReX_IndexType.cpp
@@ -49,4 +49,98 @@ operator>> (std::istream& is,
     return is;
 }
 
+IndexTypeSet::IndexTypeSet (unsigned long a_iTypeSet) : m_EncodedLong(a_iTypeSet) {};
+
+IndexTypeSet::IndexTypeSet (Vector<IndexType> a_IxTypes)
+{
+    define(a_IxTypes);
+}
+
+IndexTypeSet::IndexTypeSet (Vector<IntVect> a_IntVects)
+{
+    define(ConvertToIxTypes(a_IntVects));
+}
+
+void
+IndexTypeSet::define(Vector<IndexType> a_IxTypes)
+{
+    m_n = a_IxTypes.size();
+    m_EncodedLong = ConvertToEncodedLong(a_IxTypes);
+}
+
+Vector<IndexType>
+IndexTypeSet::IndexTypeList ()
+{
+    return ConvertToIxTypes(m_EncodedLong);
+}
+
+Vector<IntVect>
+IndexTypeSet::IntVectList ()
+{
+    return ConvertToIntVects(m_EncodedLong);
+}
+
+unsigned long
+IndexTypeSet::ConvertToEncodedLong (Vector<IndexType> a_IxTypes)
+{
+    unsigned int o_EncodedLong = 0;
+    for (IndexType ixtype : a_IxTypes) {
+        o_EncodedLong = o_EncodedLong << AMREX_SPACEDIM;
+        o_EncodedLong += ixtype.itype;
+    }
+    o_EncodedLong = o_EncodedLong << IndexTypeSet::const_NumBitsForSize;
+    o_EncodedLong += a_IxTypes.size() % (2 << IndexTypeSet::const_NumBitsForSize);
+    return o_EncodedLong;
+}
+
+unsigned long
+IndexTypeSet::ConvertToEncodedLong (Vector<IntVect> a_IntVects)
+{
+    return ConvertToEncodedLong(ConvertToIxTypes(a_IntVects));
+}
+
+Vector<IndexType>
+IndexTypeSet::ConvertToIxTypes (unsigned long a_EncodedLong)
+{
+    int tmp, size, i;
+    tmp = a_EncodedLong;
+    size = tmp % (2 << IndexTypeSet::const_NumBitsForSize);
+    tmp = tmp >> IndexTypeSet::const_NumBitsForSize;
+    Vector<IndexType> o_IxTypes(size);
+
+    for (i = size-1; i >= 0; i++) {
+        IndexType ixtype = IndexType();
+        ixtype.itype = tmp % (2 << AMREX_SPACEDIM);
+        o_IxTypes[i] = ixtype;
+        tmp = tmp >> AMREX_SPACEDIM;
+    }
+    return o_IxTypes;
+}
+
+Vector<IndexType>
+IndexTypeSet::ConvertToIxTypes (Vector<IntVect> a_IntVects)
+{
+    Vector<IndexType> o_IxTypes;
+    for (IntVect iv : a_IntVects) {
+        o_IxTypes.push_back(IndexType(iv));
+    }
+    return o_IxTypes;
+}
+
+Vector<IntVect>
+IndexTypeSet::ConvertToIntVects (unsigned long a_EncodedLong)
+{
+    return ConvertToIntVects(ConvertToIxTypes(a_EncodedLong));
+}
+
+Vector<IntVect>
+IndexTypeSet::ConvertToIntVects (Vector<IndexType> a_IxTypes)
+{
+    Vector<IntVect> o_IntVects;
+    for (IndexType ixtype : a_IxTypes) {
+        o_IntVects.push_back(ixtype.toIntVect());
+    }
+    return o_IntVects;
+}
+
 }
