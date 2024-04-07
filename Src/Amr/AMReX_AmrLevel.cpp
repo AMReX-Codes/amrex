fc74e2738a8b35af72b6fd8d72c476f1d74e3c32
Max Katz <maxpkatz@gmail.com>
2024-04-07 15:56:37 -0400

    Fix scope of f_lev

--- Src/Amr/AMReX_AmrLevel.cpp
+++ Src/Amr/AMReX_AmrLevel.cpp
@@ -179,6 +179,8 @@ AmrLevel::writePlotFile (const std::string& dir,
     // if the State_Type is ::Interval, this will get t^{n+1/2} instead of t^n
     Real cur_time = state[0].curTime();
 
+    int f_lev = std::min(parent->plotMaxLevel(), parent->finestLevel());
+
     if (level == 0 && ParallelDescriptor::IOProcessor())
     {
         //
@@ -218,7 +220,6 @@ AmrLevel::writePlotFile (const std::string& dir,
 
         os << AMREX_SPACEDIM << '\n';
         os << parent->cumTime() << '\n';
-        int f_lev = std::min(parent->plotMaxLevel(), parent->finestLevel());
         os << f_lev << '\n';
         for (i = 0; i < AMREX_SPACEDIM; i++) {
             os << Geom().ProbLo(i) << ' ';
@@ -290,7 +291,7 @@ AmrLevel::writePlotFile (const std::string& dir,
             RealBox gridloc = RealBox(grids[i],geom.CellSize(),geom.ProbLo());
             for (n = 0; n < AMREX_SPACEDIM; n++) {
                 os << gridloc.lo(n) << ' ' << gridloc.hi(n) << '\n';
-}
+	    }
         }
         //
         // The full relative pathname of the MultiFabs at this level.
