e64ffef57a7608d1d60f9abe738cc634e9c1272e
Weiqun Zhang <WeiqunZhang@lbl.gov>
2024-10-11 14:51:26 -0500

    SmallMatrix: Support 1-based indexing (#4188)
    To avoid confusion, operations involving both 0 and 1-base indexing are
    not allowed.

--- Tests/SmallMatrix/main.cpp
+++ Tests/SmallMatrix/main.cpp
@@ -12,6 +12,7 @@ int main (int argc, char* argv[])
                   Order::F == Order::ColumnMajor);
 
     amrex::Initialize(argc, argv);
+    // 0-based indexing
     {
         SmallMatrix<Real,3,4> m34{};
         for (int j = 0; j < 4; ++j) {
@@ -137,6 +138,136 @@ int main (int argc, char* argv[])
         SmallMatrix<int,2,3> a{{+1, +2, +3},
                                {+7, +8, +9}};
         SmallMatrix<int,2,3> b;
+        b.setVal(-1);
+        AMREX_ALWAYS_ASSERT(a.dot(b) == -30);
+    }
+
+    // 1-based indexing
+    {
+        SmallMatrix<Real,3,4,Order::F,1> m34{};
+        for (int j = 1; j <= 4; ++j) {
+            for (int i = 1; i <= 3; ++i) {
+                AMREX_ALWAYS_ASSERT(m34(i,j) == 0.0_rt);
+            }
+        }
+    }
+    {
+        SmallVector<Real,3,1> cv{};
+        SmallRowVector<Real,3,1> rv{};
+        SmallVector<int,3,1> cv2{1,2,3};
+        SmallRowVector<int,3,1> rv2{0,10,20};
+        SmallVector<int,5,1> cv3{0,1,2};
+        for (int j = 0; j < 3; ++j) {
+            AMREX_ALWAYS_ASSERT(cv(j+1) == 0.0_rt &&
+                                rv(j+1) == 0.0_rt &&
+                                cv2(j+1) == j+1 &&
+                                rv2(j+1) == j*10 &&
+                                cv3(j+1) == j);
+        }
+        AMREX_ALWAYS_ASSERT(cv3(4) == 0 && cv3(5) == 0);
+    }
+    {
+        SmallMatrix<int,3,4,Order::F,1> m34{{0,3,6,9},
+                                            {1,4,7,10},
+                                            {2,5,8,11}};
+        int v = 0;
+        for (int j = 1; j <= 4; ++j) {
+            for (int i = 1; i <= 3; ++i) {
+                AMREX_ALWAYS_ASSERT(m34(i,j) == v++);
+            }
+        }
+        std::cout << m34;
+    }
+    {
+        SmallMatrix<int,3,4,Order::C,1> m34{{0,1,2,3},
+                                            {4,5,6,7},
+                                            {8,9,10,11}};
+        int v = 0;
+        for (int i = 1; i <= 3; ++i) {
+            for (int j = 1; j <= 4; ++j) {
+                AMREX_ALWAYS_ASSERT(m34(i,j) == v++);
+            }
+        }
+    }
+    {
+        auto v3 = SmallVector<double,3,1>::Zero();
+        v3[1] = 1.;
+        v3(2) = 2.;
+        v3[3] = 3.;
+        auto m33 = SmallMatrix<double,3,3,Order::F,1>::Identity();
+        auto r = m33*v3;
+        AMREX_ALWAYS_ASSERT(almostEqual(r[1],v3[1]) &&
+                            almostEqual(r[2],v3[2]) &&
+                            almostEqual(r[3],v3[3]));
+    }
+    {
+        SmallMatrix<int,4,3,Order::C,1> A{{1, 0, 1},
+                                          {2, 1, 1},
+                                          {0, 1, 1},
+                                          {1, 1, 2}};
+        SmallMatrix<int,3,3,Order::C,1> B{{1, 2, 1},
+                                          {2, 3, 1},
+                                          {4, 2, 2}};
+        SmallMatrix<int,3,1,Order::C,1> C{10, 8, 6};
+        auto ABC = A*B*C;
+        AMREX_ALWAYS_ASSERT(ABC(1,1) == 100 &&
+                            ABC(2,1) == 182 &&
+                            ABC(3,1) == 118 &&
+                            ABC(4,1) == 218);
+    }
+    {
+        SmallMatrix<int,3,4,Order::F,1> A{{1, 2, 0, 1},
+                                          {0, 1, 1, 1},
+                                          {1, 1, 1, 2}};
+        SmallMatrix<int,3,3,Order::F,1> B{{1, 2, 4},
+                                          {2, 3, 2},
+                                          {1, 1, 2}};
+        SmallMatrix<int,1,3,Order::F,1> C{10, 8, 6};
+        auto ABC = A.transpose()*B.transposeInPlace()*C.transpose();
+        AMREX_ALWAYS_ASSERT(ABC(1,1) == 100 &&
+                            ABC(2,1) == 182 &&
+                            ABC(3,1) == 118 &&
+                            ABC(4,1) == 218);
+    }
+    {
+        SmallMatrix<int, 3, 4, Order::F, 1> m;
+        m.setVal(2);
+        using M = decltype(m);
+        AMREX_ALWAYS_ASSERT(m.product() == Math::powi<M::row_size*M::column_size>(2));
+        AMREX_ALWAYS_ASSERT(m.sum() == 2*m.row_size*m.column_size);
+    }
+    {
+        SmallMatrix<double, 5, 5, Order::F, 1> m{{1.0, 3.4, 4.5, 5.6, 6.7},
+                                                 {1.3, 2.0, 4.5, 5.6, 6.7},
+                                                 {1.3, 1.0, 3.0, 5.6, 6.7},
+                                                 {1.3, 1.4, 4.5, 4.0, 6.7},
+                                                 {1.3, 1.0, 4.5, 5.6, 5.0}};
+        AMREX_ALWAYS_ASSERT(m.trace() == double(1+2+3+4+5));
+    }
+    {
+        SmallMatrix<int,2,3,Order::F,1> a{{+1, +2, +3},
+                                          {+7, +8, +9}};
+        SmallMatrix<int,2,3,Order::F,1> b{{-1, -2, -3},
+                                          {-7, -8, -9}};
+        auto c = a*2 + 2*b;
+        for (auto const& x : c) {
+            AMREX_ALWAYS_ASSERT(x == 0);
+        }
+    }
+    {
+        SmallMatrix<int,2,3,Order::F,1> a{{+1, +2, +3},
+                                          {+7, +8, +9}};
+        SmallMatrix<int,2,3,Order::F,1> b{{-1, -2, -3},
+                                          {-7, -8, -9}};
+        auto c = -a - b;
+        for (auto const& x : c) {
+            AMREX_ALWAYS_ASSERT(x == 0);
+        }
+    }
+    {
+        SmallMatrix<int,2,3,Order::F,1> a{{+1, +2, +3},
+                                          {+7, +8, +9}};
+        SmallMatrix<int,2,3,Order::F,1> b;
         b.setVal(-1);
         AMREX_ALWAYS_ASSERT(a.dot(b) == -30);
     }
