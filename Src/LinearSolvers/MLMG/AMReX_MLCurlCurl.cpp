146079e2248618c1c51c50bb4f1b260457d4ff6c
RevathiJambunathan <revanathan@gmail.com>
2024-11-21 14:17:09 -0800

    add 1D CurlCurl - separate smooth1D kernel since 1D solution reduces to a simple result

--- Src/LinearSolvers/MLMG/AMReX_MLCurlCurl.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLCurlCurl.cpp
@@ -78,7 +78,7 @@ void MLCurlCurl::setBeta (const Vector<Array<MultiFab const*,3>>& a_bcoefs)
                                    *m_bcoefs[amrlev][mglev  ][idim], ratio);
                 m_bcoefs[amrlev][mglev][idim]->FillBoundary(m_geom[amrlev][mglev].periodicity());
             }
-#if (AMREX_SPACEDIM == 2)
+#if (AMREX_SPACEDIM < 3)
             if (m_bcoefs[amrlev][mglev][2] == nullptr) {
                 m_bcoefs[amrlev][mglev][2] = std::make_unique<MultiFab>
                     (amrex::convert(m_grids[amrlev][mglev], m_etype[2]),
@@ -86,6 +86,15 @@ void MLCurlCurl::setBeta (const Vector<Array<MultiFab const*,3>>& a_bcoefs)
             }
             average_down_nodal(*m_bcoefs[amrlev][mglev-1][2],
                                *m_bcoefs[amrlev][mglev  ][2], ratio);
+#endif
+#if (AMREX_SPACEDIM == 1)
+            if (m_bcoefs[amrlev][mglev][1] == nullptr) {
+                m_bcoefs[amrlev][mglev][1] = std::make_unique<MultiFab>
+                    (amrex::convert(m_grids[amrlev][mglev], m_etype[1]),
+                     m_dmap[amrlev][mglev], 1, 0);
+            }
+            average_down_nodal(*m_bcoefs[amrlev][mglev-1][1],
+                               *m_bcoefs[amrlev][mglev  ][1], ratio);
 #endif
         }
     }
@@ -316,6 +325,15 @@ void MLCurlCurl::smooth (int amrlev, int mglev, MF& sol, const MF& rhs,
 
     applyBC(amrlev, mglev, const_cast<MF&>(rhs), CurlCurlStateType::b);
 
+#if (AMREX_SPACEDIM == 1)
+    for (int color = 0; color < 2; ++color) {
+        if (!skip_fillboundary) {
+            applyBC(amrlev, mglev, sol, CurlCurlStateType::x);
+        }
+        skip_fillboundary = false;
+        smooth1D(amrlev, mglev, sol, rhs, color);
+    }
+#else
     for (int color = 0; color < 4; ++color) {
         if (!skip_fillboundary) {
             applyBC(amrlev, mglev, sol, CurlCurlStateType::x);
@@ -323,11 +341,42 @@ void MLCurlCurl::smooth (int amrlev, int mglev, MF& sol, const MF& rhs,
         skip_fillboundary = false;
         smooth4(amrlev, mglev, sol, rhs, color);
     }
+#endif
+}
+
+void MLCurlCurl::smooth1D (int amrlev, int mglev, MF& sol, MF const& rhs,
+                 int color) const
+{
+    auto const& ex = sol[0].arrays();
+    auto const& ey = sol[1].arrays();
+    auto const& ez = sol[2].arrays();
+    auto const& rhsx = rhs[0].const_arrays();
+    auto const& rhsy = rhs[1].const_arrays();
+    auto const& rhsz = rhs[2].const_arrays();
+
+    auto beta = m_beta;
+
+    auto dinfo = getDirichletInfo(amrlev,mglev);
+    auto adxinv = this->m_geom[amrlev][mglev].InvCellSizeArray();
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+        adxinv[idim] *= std::sqrt(m_alpha);
+    }
+
+    MultiFab nmf(amrex::convert(rhs[0].boxArray(),IntVect(1)),
+                 rhs[0].DistributionMap(), 1, 0, MFInfo().SetAlloc(false));
+    ParallelFor( nmf, [=] AMREX_GPU_DEVICE(int bno, int i, int j, int k)
+    {
+        mlcurlcurl_1D(i,j,k,ex[bno],ey[bno],ez[bno],
+                      rhsx[bno],rhsy[bno],rhsz[bno],
+                      beta,adxinv,color,dinfo);
+    });
+    Gpu::streamSynchronize();
 }
 
 void MLCurlCurl::smooth4 (int amrlev, int mglev, MF& sol, MF const& rhs,
                           int color) const
 {
+#if (AMREX_SPACEDIM > 1)
     auto const& ex = sol[0].arrays();
     auto const& ey = sol[1].arrays();
     auto const& ez = sol[2].arrays();
@@ -385,6 +434,7 @@ void MLCurlCurl::smooth4 (int amrlev, int mglev, MF& sol, MF const& rhs,
         }
     }
     Gpu::streamSynchronize();
+#endif
 }
 
 void MLCurlCurl::solutionResidual (int amrlev, MF& resid, MF& x, const MF& b,
@@ -453,6 +503,7 @@ void MLCurlCurl::compresid (int amrlev, int mglev, MF& resid, MF const& b) const
 
 void MLCurlCurl::prepareForSolve ()
 {
+#if (AMREX_SPACEDIM > 1)
     if (m_bcoefs[0][0][0] == nullptr) {
         for (int amrlev = 0;  amrlev < m_num_amr_levels; ++amrlev) {
             for (int mglev = 0; mglev < m_num_mg_levels[amrlev]; ++mglev) {
@@ -536,6 +587,7 @@ void MLCurlCurl::prepareForSolve ()
             }
         }
     }
+#endif
 }
 
 Real MLCurlCurl::xdoty (int amrlev, int mglev, const MF& x, const MF& y,
@@ -633,6 +685,10 @@ void MLCurlCurl::applyBC (int amrlev, int mglev, MF& in, CurlCurlStateType type)
 #if (AMREX_SPACEDIM == 2)
     if (CurlCurlStateType::b == type) {
         nmfs = 2; // no need to applyBC on Ez
+    }
+#elif (AMREX_SPACEDIM == 1)
+    if (CurlCurlStateType::b == type) {
+        nmfs = 1; // no need to applyBC on Ez
     }
 #endif
     Vector<MultiFab*> mfs(nmfs);
@@ -798,6 +854,10 @@ CurlCurlDirichletInfo MLCurlCurl::getDirichletInfo (int amrlev, int mglev) const
         if (idim == 2) {
             return std::numeric_limits<int>::lowest();
         }
+#elif (AMREX_SPACEDIM == 1)
+        if (idim > 1) {
+            return std::numeric_limits<int>::lowest();
+        }
 #endif
 
         if (face == 0) {
@@ -830,6 +890,10 @@ CurlCurlSymmetryInfo MLCurlCurl::getSymmetryInfo (int amrlev, int mglev) const
     {
 #if (AMREX_SPACEDIM == 2)
         if (idim == 2) {
+            return std::numeric_limits<int>::lowest();
+        }
+#elif (AMREX_SPACEDIM == 1)
+        if (idim > 1) {
             return std::numeric_limits<int>::lowest();
         }
 #endif
