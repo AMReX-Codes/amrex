58236fdec3c70b751412438b184b854281f552fc
Benjamin Wibking <benjamin.wibking@anu.edu.au>
2025-09-26 03:17:00 +1000

    remove instrumentation from async memcpy

--- Src/Base/AMReX_GpuDevice.cpp
+++ Src/Base/AMReX_GpuDevice.cpp
@@ -731,10 +731,7 @@ Device::startGraphRecording(bool first_iter, void* h_ptr, void* d_ptr, size_t sz
         AMREX_CUDA_SAFE_CALL(cudaStreamBeginCapture(graph_stream, cudaStreamCaptureModeGlobal));
 #endif
 
-        bool const guard_active = Gpu::syncLaunchGuardActive();
-        if (guard_active) { Gpu::synchronize(); }
         AMREX_CUDA_SAFE_CALL(cudaMemcpyAsync(d_ptr, h_ptr, sz, cudaMemcpyHostToDevice, graph_stream));
-        if (guard_active) { Gpu::synchronize(); }
         AMREX_CUDA_SAFE_CALL(cudaEventRecord(memcpy_event, graph_stream));
 
         // Note: Main graph stream fixed at 0, so i starts at 1.
