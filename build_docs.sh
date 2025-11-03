0a734ba13c61dd6625ac2e210addbef04fecb419
atmyers <atmyers2@gmail.com>
2025-11-03 14:47:36 -0800

    Cosmetic changes

--- build_docs.sh
+++ build_docs.sh
@@ -12,21 +12,20 @@ if grep -q "warning:" doxygen.out; then
 fi
 cd ../..
 
+# copy doxygen to target location
 mkdir build
 cd build
 mkdir docs_html docs_xml
-
-# copy doxygen to target location
 mkdir -p docs_html/doxygen
 cp -rp ../Docs/Doxygen/html/* docs_html/doxygen/
 mkdir -p docs_xml/doxygen
 cp -rp ../Docs/Doxygen/xml/* docs_xml/doxygen/
 # add tagfile to allow other docs to interlink with amrex
 cp ../Docs/Doxygen/amrex-doxygen-web.tag.xml docs_xml/doxygen/.
+cd ..
 
 # Build sphinx
-cd ../Docs/sphinx_documentation
-
+cd Docs/sphinx_documentation
 echo "Build the Sphinx documentation for Amrex."
 make PYTHON="python3" latexpdf
 mv build/latex/amrex.pdf source/
@@ -34,7 +33,6 @@ make clean
 make SPHINXOPTS='-v -W --keep-going' PYTHON="python3" html
 cd ../../
 
-cd build
-
 # copy sphinx to target location
+cd build
 cp -rp ../Docs/sphinx_documentation/build/html/* docs_html/
