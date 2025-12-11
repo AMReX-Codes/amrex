8786b29283683f46cac4652b615c667847e6d3f4
Weiqun Zhang <weiqunzhang@lbl.gov>
2025-12-11 09:09:08 -0800

    Need to set rowoffsets[nrows] because its size is nrows+1

--- Tests/Algebra/SpMV/main.cpp
+++ Tests/Algebra/SpMV/main.cpp
@@ -29,32 +29,31 @@ int main(int argc, char *argv[]) {
     //     s_n + 2.0 * s_{n+1} = (n + 1) + 2 * (n + 2)
     //
     // independent on each processor
-    ParallelFor(nrows - 1, [=] AMREX_GPU_DEVICE(Long lrow) {
+    ParallelFor(nrows, [=] AMREX_GPU_DEVICE(Long lrow) {
       auto row = lrow + ib; // global row index
 
-      rhs[lrow + 1] = static_cast<Real>((lrow + 1.0) + 2.0 * (lrow + 2.0));
-      phi[lrow + 1] = static_cast<Real>(lrow + 2);
+      if (lrow == 0) {
+        rhs[0] = Real(1.0);
+        phi[0] = Real(1.0);
 
-      matRowOffsets[lrow + 1] = static_cast<Long>(2 * (lrow + 1));
-      matCols[2 * (lrow + 1)] = row + 1;
-      matCols[2 * (lrow + 1) + 1] = row;
-      matVals[2 * (lrow + 1)] = static_cast<Real>(2.0);
-      matVals[2 * (lrow + 1) + 1] = static_cast<Real>(1.0);
+        matRowOffsets[0] = 0;
+        matRowOffsets[nrows] = 2*nrows;
+        matCols[0] = ib;
+        matCols[1] = ib + 1;
+        matVals[0] = Real(1.0);
+        matVals[1] = Real(0.0);
+      } else {
+        rhs[lrow] = static_cast<Real>(lrow + 2 * (lrow + 1));
+        phi[lrow] = static_cast<Real>(lrow + 1);
+
+        matRowOffsets[lrow] = static_cast<Long>(2 * lrow);
+        matCols[2*lrow    ] = row;
+        matCols[2*lrow + 1] = row - 1;
+        matVals[2*lrow    ] = static_cast<Real>(2.0);
+        matVals[2*lrow + 1] = static_cast<Real>(1.0);
+      }
     });
 
-    // boundary condition: s_0 = 1.0
-    ParallelFor(1, [=] AMREX_GPU_DEVICE(Long) {
-      // global row index
-
-      rhs[0] = 1.0;
-      phi[0] = 1.0;
-
-      matRowOffsets[0] = 0;
-      matCols[0] = ib;
-      matCols[1] = ib + 1;
-      matVals[0] = 1.0;
-      matVals[1] = 0.0;
-    });
 
     auto eps = (sizeof(Real) == 4) ? Real(1.e-5) : Real(1.e-12);
     amrex::SpMV(xvec, mat, exact);
