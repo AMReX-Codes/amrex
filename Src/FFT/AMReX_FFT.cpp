b01c5db25b09d5bc19a593ffcdf8c61e1d2a0f8d
Weiqun Zhang <WeiqunZhang@lbl.gov>
2026-01-05 11:42:24 -0800

    Apply suggestions from code review
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- Src/FFT/AMReX_FFT.cpp
+++ Src/FFT/AMReX_FFT.cpp
@@ -20,7 +20,7 @@ void Initialize ()
     {
 #if defined(AMREX_USE_HIP)
         AMREX_ROCFFT_SAFE_CALL(rocfft_setup());
-#elif !defined(AMREX_USE_GPU) && defined(AMREX_USE_OMP)
+#elif !defined(AMREX_USE_GPU) && defined(AMREX_USE_OMP) && defined(AMREX_USE_FFT)
         fftw_init_threads();
         fftwf_init_threads();
         fftw_plan_with_nthreads(amrex::OpenMP::get_max_threads());
@@ -41,7 +41,7 @@ void Finalize ()
 
 #if defined(AMREX_USE_HIP)
         AMREX_ROCFFT_SAFE_CALL(rocfft_cleanup());
-#elif !defined(AMREX_USE_GPU) && defined(AMREX_USE_OMP)
+#elif !defined(AMREX_USE_GPU) && defined(AMREX_USE_OMP) && defined(AMREX_USE_FFT)
         fftw_cleanup_threads();
         fftwf_cleanup_threads();
 #endif
