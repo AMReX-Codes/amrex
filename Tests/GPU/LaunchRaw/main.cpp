09d75ee02b27714d27f6ff93a0b2c6153436ae6b
Alexander Sinn <alexander.sinn@desy.de>
2026-03-11 13:43:57 +0100

    update test and see if it is compiled

--- Tests/GPU/LaunchRaw/main.cpp
+++ Tests/GPU/LaunchRaw/main.cpp
@@ -9,8 +9,13 @@ using namespace amrex;
 
 void test3d () {
 
-    constexpr int num_threads = 256;
-    IntVectND<3> num_blocks {31, 23, 11};
+    const IntVectND<3> num_blocks {31, 23, 11};
+#ifdef AMREX_USE_GPU
+    static constexpr IntVectND<3> blockdim {2, 8, 16};
+#else
+    static constexpr IntVectND<3> blockdim {1, 1, 1};
+#endif
+    static constexpr int num_threads = blockdim[0] * blockdim[1] * blockdim[2];
 
     Gpu::DeviceVector<int> vect(num_threads * num_blocks[0] * num_blocks[1] * num_blocks[2], -999);
 
@@ -29,12 +34,12 @@ void test3d () {
     LaunchRaw<num_threads>(num_blocks,
         [=](auto lh){
             auto block = lh.blockIdxND();
-            auto thread = lh.template threadIdxND<2, 8, 16>();
+            auto thread = lh.template threadIdxND<blockdim[0], blockdim[1], blockdim[2]>();
             auto tmp = data[
                 (block[0] + block[1] * num_blocks[0] +
                 block[2] * num_blocks[0] * num_blocks[1]) * num_threads +
-                thread[2] + thread[1] * 16 +
-                thread[0] * 16 * 8
+                thread[2] + thread[1] * blockdim[2] +
+                thread[0] * blockdim[2] * blockdim[1]
             ];
             lh.synctheads();
             data[lh.blockIdx1D() * lh.blockDim1D() + lh.threadIdx1D()] = tmp;
@@ -43,11 +48,12 @@ void test3d () {
     LaunchRaw<num_threads, int>(num_blocks, num_threads,
         [=](auto lh){
             auto smem = lh.shared_memory();
-            auto thread1 = lh.template threadIdxND<16, 8, 2>();
-            auto locid1 = thread1[2] + thread1[1] * 2 + thread1[0] * 2 * 8;
-            auto thread2 = lh.template threadIdxND<16, 2, 8>();
-            auto locid2 = thread2[0] + thread2[2] * 16 + thread2[1] * 16 * 8;
-
+            auto thread1 = lh.template threadIdxND<blockdim[2], blockdim[1], blockdim[0]>();
+            auto locid1 = thread1[2] + thread1[1] * blockdim[0] +
+                          thread1[0] * blockdim[0] * blockdim[1];
+            auto thread2 = lh.template threadIdxND<blockdim[2], blockdim[0], blockdim[1]>();
+            auto locid2 = thread2[0] + thread2[2] * blockdim[2] +
+                          thread2[1] * blockdim[2] * blockdim[1];
             smem[lh.threadIdx1D()] = data[lh.blockIdx1D() * lh.blockDim1D() + locid1];
             lh.syncthreads();
             data[lh.blockIdx1D() * lh.blockDim1D() + locid2] = smem[locid2];
@@ -65,7 +71,7 @@ int main (int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
     {
-        test3d();
+        test3d()
 
         amrex::Print() << "Passed! \n";
     }
