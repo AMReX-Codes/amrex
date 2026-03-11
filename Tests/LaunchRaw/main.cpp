f29aef1eab8664fdc56c6ab58dcda828af9eb1ce
Alexander Sinn <alexander.sinn@desy.de>
2026-03-11 16:38:25 +0100

    add 1d and 2d tests

--- Tests/LaunchRaw/main.cpp
+++ Tests/LaunchRaw/main.cpp
@@ -7,6 +7,117 @@
 
 using namespace amrex;
 
+void test1d () {
+
+    const IntVectND<1> num_blocks {31};
+#ifdef AMREX_USE_GPU
+    static constexpr IntVectND<1> blockdim {256};
+#else
+    static constexpr IntVectND<1> blockdim {1};
+#endif
+    static constexpr int num_threads = blockdim[0];
+
+    Gpu::DeviceVector<int> vect(num_threads * num_blocks[0], -999);
+
+    auto * data = vect.dataPtr();
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            data[lh.globalIdx1D()] = lh.blockIdx1D();
+        });
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            data[lh.blockIdx1D() * lh.blockDim1D() + lh.threadIdx1D()] += lh.threadIdx1D();
+        });
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            auto block = lh.blockIdxND();
+            auto thread = lh.template threadIdxND<blockdim[0]>();
+            auto tmp = data[
+                block[0] * num_threads + thread[0]
+            ];
+            lh.syncthreads();
+            data[lh.blockIdx1D() * lh.blockDim1D() + lh.threadIdx1D()] = tmp;
+        });
+
+    LaunchRaw<num_threads, int>(num_blocks, num_threads,
+        [=](auto lh){
+            auto smem = lh.shared_memory();
+            auto thread = lh.template threadIdxND<blockdim[0]>();
+            auto locid = thread[0];
+            smem[lh.threadIdx1D()] = data[lh.blockIdx1D() * lh.blockDim1D() + locid];
+            lh.syncthreads();
+            data[lh.blockIdx1D() * lh.blockDim1D() + locid] = smem[locid];
+        });
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            data[lh.globalIdx1D()] =
+                data[lh.globalIdx1D()] == static_cast<int>(lh.blockIdx1D() + lh.threadIdx1D());
+        });
+
+    AMREX_ALWAYS_ASSERT(Reduce::Sum(vect.size(), data, 0) == static_cast<int>(vect.size()));
+}
+
+void test2d () {
+
+    const IntVectND<2> num_blocks {31, 23};
+#ifdef AMREX_USE_GPU
+    static constexpr IntVectND<2> blockdim {8, 32};
+#else
+    static constexpr IntVectND<2> blockdim {1, 1};
+#endif
+    static constexpr int num_threads = blockdim[0] * blockdim[1];
+
+    Gpu::DeviceVector<int> vect(num_threads * num_blocks[0] * num_blocks[1], -999);
+
+    auto * data = vect.dataPtr();
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            data[lh.globalIdx1D()] = lh.blockIdx1D();
+        });
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            data[lh.blockIdx1D() * lh.blockDim1D() + lh.threadIdx1D()] += lh.threadIdx1D();
+        });
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            auto block = lh.blockIdxND();
+            auto thread = lh.template threadIdxND<blockdim[0], blockdim[1]>();
+            auto tmp = data[
+                (block[0] + block[1] * num_blocks[0]) * num_threads +
+                thread[1] + thread[0] * blockdim[1]
+            ];
+            lh.syncthreads();
+            data[lh.blockIdx1D() * lh.blockDim1D() + lh.threadIdx1D()] = tmp;
+        });
+
+    LaunchRaw<num_threads, int>(num_blocks, num_threads,
+        [=](auto lh){
+            auto smem = lh.shared_memory();
+            auto thread1 = lh.template threadIdxND<blockdim[1], blockdim[0]>();
+            auto locid1 = thread1[1] + thread1[0] * blockdim[0];
+            auto thread2 = lh.template threadIdxND<blockdim[0], blockdim[1]>();
+            auto locid2 = thread2[1] + thread2[0] * blockdim[1];
+            smem[lh.threadIdx1D()] = data[lh.blockIdx1D() * lh.blockDim1D() + locid1];
+            lh.syncthreads();
+            data[lh.blockIdx1D() * lh.blockDim1D() + locid2] = smem[locid2];
+        });
+
+    LaunchRaw<num_threads>(num_blocks,
+        [=](auto lh){
+            data[lh.globalIdx1D()] =
+                data[lh.globalIdx1D()] == static_cast<int>(lh.blockIdx1D() + lh.threadIdx1D());
+        });
+
+    AMREX_ALWAYS_ASSERT(Reduce::Sum(vect.size(), data, 0) == static_cast<int>(vect.size()));
+}
+
 void test3d () {
 
     const IntVectND<3> num_blocks {31, 23, 11};
@@ -19,7 +130,7 @@ void test3d () {
 
     Gpu::DeviceVector<int> vect(num_threads * num_blocks[0] * num_blocks[1] * num_blocks[2], -999);
 
-    auto data = vect.dataPtr();
+    auto * data = vect.dataPtr();
 
     LaunchRaw<num_threads>(num_blocks,
         [=](auto lh){
@@ -70,8 +181,12 @@ void test3d () {
 
 int main (int argc, char* argv[])
 {
-    amrex::Initialize(argc,argv);
+    amrex::Initialize(argc, argv);
     {
+        test1d();
+
+        test2d();
+
         test3d();
 
         amrex::Print() << "Passed! \n";
