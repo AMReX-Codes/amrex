e972eeb48c1d47072c60b353212d080ee7e07fc5
Andrew Myers <atmyers2@gmail.com>
2026-03-11 11:48:50 -0700

    reduce size of binary file

--- Tests/Particles/InitFromBinary/main.cpp
+++ Tests/Particles/InitFromBinary/main.cpp
@@ -6,7 +6,7 @@ using namespace amrex;
 
 void test_init_binary ()
 {
-    // The binary file 32.nyx has NP=32768, DM=3, NX=4
+    // The binary file 8.nyx has NP=512, DM=3, NX=4
     // with particle positions in [0, 8]^3
     constexpr int NStructReal = 4;
     constexpr int NStructInt  = 0;
@@ -20,28 +20,28 @@ void test_init_binary ()
     }
 
     IntVect domain_lo(AMREX_D_DECL(0, 0, 0));
-    IntVect domain_hi(AMREX_D_DECL(31, 31, 31));
+    IntVect domain_hi(AMREX_D_DECL(7, 7, 7));
     const Box domain(domain_lo, domain_hi);
 
     int is_per[] = {AMREX_D_DECL(1, 1, 1)};
     Geometry geom(domain, &real_box, CoordSys::cartesian, is_per);
 
     BoxArray ba(domain);
-    ba.maxSize(16);
+    ba.maxSize(4);
 
     DistributionMapping dmap(ba);
 
     using MyPC = ParticleContainer<NStructReal, NStructInt, NArrayReal, NArrayInt>;
     MyPC pc(geom, dmap, ba);
 
-    pc.InitFromBinaryFile("32.nyx", NStructReal);
+    pc.InitFromBinaryFile("8.nyx", NStructReal);
 
     Long np = pc.TotalNumberOfParticles();
     amrex::Print() << "Total number of particles: " << np << "\n";
-    AMREX_ALWAYS_ASSERT(np == 32768);
+    AMREX_ALWAYS_ASSERT(np == 512);
 
-    // Particle masses in this file are 1.0, 2.0, ..., 32768.0
-    // so the total mass should be 32768 * 32769 / 2 = 536887296
+    // Particle masses in this file are 1.0, 2.0, ..., 512.0
+    // so the total mass should be 512 * 513 / 2 = 131328
     using PType = MyPC::SuperParticleType;
     Real total_mass = amrex::ReduceSum(pc,
                                        [=] AMREX_GPU_HOST_DEVICE (const PType& p) -> Real
@@ -52,7 +52,7 @@ void test_init_binary ()
     ParallelDescriptor::ReduceRealSum(total_mass);
 
     amrex::Print() << "Total particle mass: " << total_mass << "\n";
-    AMREX_ALWAYS_ASSERT(total_mass == 536887296.0);
+    AMREX_ALWAYS_ASSERT(total_mass == 131328.0);
 }
 
 int main (int argc, char* argv[])
