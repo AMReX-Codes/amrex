aa87aadb1cca1f571d1b5a57b30aa40fe94bbc03
Nils Schild <nils.schild@ipp.mpg.de>
2026-04-08 14:07:05 +0200

    Create symbolic link during install

--- Tools/CMake/AMReXInstallHelpers.cmake
+++ Tools/CMake/AMReXInstallHelpers.cmake
@@ -71,11 +71,13 @@ function (install_amrex_targets)
 
        # legacy symlink for: libamrex.[so|a] / amrex.[dll.lib]
        #   escape spaces for generated cmake_install.cmake file
-       file(TO_CMAKE_PATH "${CMAKE_INSTALL_PREFIX}/lib" ABS_INSTALL_LIB_DIR)
-       install(CODE "file(CREATE_LINK
-           $<TARGET_FILE_NAME:amrex_${AMReX_SPACEDIM_LAST}d>
-           \"${ABS_INSTALL_LIB_DIR}/$<TARGET_FILE_PREFIX:amrex_${AMReX_SPACEDIM_LAST}d>amrex$<TARGET_FILE_SUFFIX:amrex_${AMReX_SPACEDIM_LAST}d>\"
-           COPY_ON_ERROR SYMBOLIC)"
+       install(CODE "
+           file(TO_CMAKE_PATH \"\${CMAKE_INSTALL_PREFIX}/lib\" ABS_INSTALL_LIB_DIR)
+
+           file(CREATE_LINK
+                $<TARGET_FILE_NAME:amrex_${AMReX_SPACEDIM_LAST}d>
+                \"\${ABS_INSTALL_LIB_DIR}/$<TARGET_FILE_PREFIX:amrex_${AMReX_SPACEDIM_LAST}d>amrex$<TARGET_FILE_SUFFIX:amrex_${AMReX_SPACEDIM_LAST}d>\"
+                COPY_ON_ERROR SYMBOLIC)"
        )
 
        # Install fortran modules if Fortran is enabled
