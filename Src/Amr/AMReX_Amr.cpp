1634cf9bd55795f35a8855cb11ac13051c0e1549
Weiqun Zhang <weiqunzhang@lbl.gov>
2026-04-10 18:35:45 -0700

    Amr::regrid used wrong `n_cycle` index for new levels

--- Src/Amr/AMReX_Amr.cpp
+++ Src/Amr/AMReX_Amr.cpp
@@ -2732,7 +2732,7 @@ Amr::regrid (int  lbase,
             a->init();
             amr_level[lev].reset(a);
             if (lev > 0) {
-                level_steps[lev] = level_steps[lev-1] * n_cycle[lev-1];
+                level_steps[lev] = level_steps[lev-1] * n_cycle[lev];
             }
             this->SetBoxArray(lev, amr_level[lev]->boxArray());
             this->SetDistributionMap(lev, amr_level[lev]->DistributionMap());
