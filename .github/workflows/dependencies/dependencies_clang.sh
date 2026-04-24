9f4686a10f435a337d070f340a331d374fa5a1aa
Weiqun Zhang <weiqunzhang@lbl.gov>
2026-04-24 08:57:14 -0700

    Update Clang+CUDA CI

--- .github/workflows/dependencies/dependencies_clang.sh
+++ .github/workflows/dependencies/dependencies_clang.sh
@@ -7,6 +7,27 @@
 
 set -eu -o pipefail
 
+clang_version=$1
+install_libcxx=${2:-false}
+
+packages=(
+    build-essential
+    libfftw3-dev
+    gfortran
+    clang-"${clang_version}"
+)
+
+case "${install_libcxx}" in
+    1|true|TRUE|yes|YES|on|ON)
+        packages+=(
+            libc++-"${clang_version}"-dev
+            libc++abi-"${clang_version}"-dev
+            libc++1-"${clang_version}"
+            libc++abi1-"${clang_version}"
+        )
+        ;;
+esac
+
 # `man apt.conf`:
 #   Number of retries to perform. If this is non-zero APT will retry
 #   failed files the given number of times.
@@ -14,8 +35,4 @@ echo 'Acquire::Retries "3";' | sudo tee /etc/apt/apt.conf.d/80-retries
 
 sudo apt-get update
 
-sudo apt-get install -y --no-install-recommends \
-    build-essential      \
-    libfftw3-dev         \
-    gfortran             \
-    clang-$1
+sudo apt-get install -y --no-install-recommends "${packages[@]}"
