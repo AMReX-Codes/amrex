bf8310cd6fec3fb20105334672b8367ca7cf6382
Andrew Myers <atmyers2@gmail.com>
2026-05-12 12:27:41 -0700

    pacify CI

--- Tests/Base/RealBox/main.cpp
+++ Tests/Base/RealBox/main.cpp
@@ -28,12 +28,12 @@ void test_contains_closed_interval ()
     AMREX_ALWAYS_ASSERT(box.contains(lo_outside, delta));
     AMREX_ALWAYS_ASSERT(box.contains(hi_outside, delta));
 
-    XDim3 lo_dim3{AMREX_D_DECL(.x = lo_arr[0], .y = lo_arr[1], .z = lo_arr[2])};
-    XDim3 hi_dim3{AMREX_D_DECL(.x = hi_arr[0], .y = hi_arr[1], .z = hi_arr[2])};
+    XDim3 lo_dim3{AMREX_D_DECL(.x = Real(-2.0), .y = Real(-1.0), .z = Real(-0.5))};
+    XDim3 hi_dim3{AMREX_D_DECL(.x = Real( 3.0), .y = Real( 4.0), .z = Real( 5.5))};
     XDim3 lo_dim3_outside{
-        AMREX_D_DECL(.x = lo_arr[0] - delta, .y = lo_arr[1], .z = lo_arr[2])};
+        AMREX_D_DECL(.x = Real(-2.0) - delta, .y = Real(-1.0), .z = Real(-0.5))};
     XDim3 hi_dim3_outside{
-        AMREX_D_DECL(.x = hi_arr[0] + delta, .y = hi_arr[1], .z = hi_arr[2])};
+        AMREX_D_DECL(.x = Real( 3.0) + delta, .y = Real( 4.0), .z = Real( 5.5))};
 
     AMREX_ALWAYS_ASSERT(box.contains(lo_dim3));
     AMREX_ALWAYS_ASSERT(box.contains(hi_dim3));
