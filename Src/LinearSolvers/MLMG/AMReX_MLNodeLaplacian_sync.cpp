1d89f7b3f059f305aa4d50bfa15f412fb4d5a33c
Marc Day <Marcus.Day@sintef.no>
2026-06-18 16:54:08 +0200

    Rephrase constexpr stuff

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian_sync.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian_sync.cpp
@@ -330,20 +330,20 @@ MLNodeLaplacian::compSyncResidualCoarse (MultiFab& sync_resid, const MultiFab& a
                             {
                                 if (ibx.contains(IntVect(AMREX_D_DECL(i,j,k))) && cccmsk(i,j,k)) {
                                     sigmaarr(i,j,k) = sigmaarr_orig(i,j,k);
-#if (AMREX_SPACEDIM >= 2)
-                                    if (is_aniso) { syarr_w(i,j,k) = syarr_orig(i,j,k); }
-#endif
-#if (AMREX_SPACEDIM == 3)
-                                    if (is_aniso) { szarr_w(i,j,k) = szarr_orig(i,j,k); }
-#endif
+									if constexpr (AMREX_SPACEDIM >= 2) {
+									  if (is_aniso) { syarr_w(i,j,k) = syarr_orig(i,j,k); }
+									}
+									if constexpr (AMREX_SPACEDIM == 3) {
+									  if (is_aniso) { szarr_w(i,j,k) = szarr_orig(i,j,k); }
+									}
                                 } else {
                                     sigmaarr(i,j,k) = 0.0;
-#if (AMREX_SPACEDIM >= 2)
-                                    if (is_aniso) { syarr_w(i,j,k) = 0.0; }
-#endif
-#if (AMREX_SPACEDIM == 3)
-                                    if (is_aniso) { szarr_w(i,j,k) = 0.0; }
-#endif
+									if constexpr (AMREX_SPACEDIM >= 2) {
+									  if (is_aniso) { syarr_w(i,j,k) = 0.0; }
+									}
+									if constexpr (AMREX_SPACEDIM == 3) {
+									  if (is_aniso) { szarr_w(i,j,k) = 0.0; }
+									}
                                 }
                             });
                         } else {
@@ -669,20 +669,20 @@ MLNodeLaplacian::compSyncResidualFine (MultiFab& sync_resid, const MultiFab& phi
                         {
                             if (ovlp2.contains(IntVect(AMREX_D_DECL(i,j,k)))) {
                                 sigmaarr(i,j,k) = sigmaarr_orig(i,j,k);
-#if (AMREX_SPACEDIM >= 2)
-                                if (is_aniso) { syarr_w(i,j,k) = syarr_orig(i,j,k); }
-#endif
-#if (AMREX_SPACEDIM == 3)
-                                if (is_aniso) { szarr_w(i,j,k) = szarr_orig(i,j,k); }
-#endif
+								if constexpr (AMREX_SPACEDIM >= 2) {
+								  if (is_aniso) { syarr_w(i,j,k) = syarr_orig(i,j,k); }
+								}
+								if constexpr (AMREX_SPACEDIM == 3) {
+								  if (is_aniso) { szarr_w(i,j,k) = szarr_orig(i,j,k); }
+								}
                             } else {
                                 sigmaarr(i,j,k) = 0.0;
-#if (AMREX_SPACEDIM >= 2)
-                                if (is_aniso) { syarr_w(i,j,k) = 0.0; }
-#endif
-#if (AMREX_SPACEDIM == 3)
-                                if (is_aniso) { szarr_w(i,j,k) = 0.0; }
-#endif
+								if constexpr (AMREX_SPACEDIM >= 2) {
+								  if (is_aniso) { syarr_w(i,j,k) = 0.0; }
+								}
+								if constexpr (AMREX_SPACEDIM == 3) {
+									if (is_aniso) { szarr_w(i,j,k) = 0.0; }
+								}
                             }
                         });
                     } else {
