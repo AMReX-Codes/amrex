b5b314ff903640192e58908da2d91a5e2ee7d08a
Weiqun Zhang <WeiqunZhang@lbl.gov>
2026-09-19 08:18:43 -0700

    FFT: fix R2C on degenerate domains with a sub-object (#5928)
    When SubHelper reduces a degenerate domain, m_rx is never defined.
    scalingFactor and getLocalDomain now delegate to m_r2c_sub, and
    setLocalDomain/setLocalSpectralDomain use m_info.batch_size instead of
    reading m_rx's uninitialized component count. scalingFactor also lets
    oned_mode win over twod_mode, matching how the plans are made.
    Tests/FFT/R2C and Tests/FFT/RawPtr now cover degenerate domains.
    
    Fixes #5791.

--- Tests/FFT/RawPtr/main.cpp
+++ Tests/FFT/RawPtr/main.cpp
@@ -77,6 +77,81 @@ int main (int argc, char* argv[])
         AMREX_ALWAYS_ASSERT(error < eps);
     }
 
+#if (AMREX_SPACEDIM > 1)
+    // Degenerate domains.  SubHelper reorders the axes internally, so the
+    // local domains must be obtained from the solver rather than computed here.
+    {
+        amrex::Vector<std::array<int,AMREX_SPACEDIM>> degenerate_domains
+#if (AMREX_SPACEDIM == 2)
+            {{1,32}, {32,1}};
+#else
+            {{1,32,16}, {32,1,16}, {32,16,1}};
+#endif
+
+        for (auto const& ds : degenerate_domains) {
+            amrex::FFT::R2C<RT,amrex::FFT::Direction::both> r2c(ds);
+
+            auto const& [local_start, local_size] = r2c.getLocalDomain();
+            auto const& [local_start_sp, local_size_sp] = r2c.getLocalSpectralDomain();
+            amrex::ignore_unused(local_start, local_start_sp);
+
+            auto nr = AMREX_D_TERM(std::size_t(local_size[0]),
+                                  *std::size_t(local_size[1]),
+                                  *std::size_t(local_size[2]));
+
+            // The local domains must add up to the whole domain.
+            auto ntot = amrex::Long(nr);
+            amrex::ParallelDescriptor::ReduceLongSum(ntot);
+            AMREX_ALWAYS_ASSERT(ntot == AMREX_D_TERM(amrex::Long(ds[0]),
+                                                    *amrex::Long(ds[1]),
+                                                    *amrex::Long(ds[2])));
+
+            r2c.setLocalDomain(local_start, local_size);
+            r2c.setLocalSpectralDomain(local_start_sp, local_size_sp);
+
+            auto* pr = (RT*)amrex::The_Arena()->alloc(sizeof(RT)*std::max(nr,std::size_t(1)));
+            amrex::ParallelFor(nr, [=] AMREX_GPU_DEVICE (std::size_t i)
+            {
+                pr[i] = std::sin(RT(i));
+            });
+
+            auto nc = AMREX_D_TERM(std::size_t(local_size_sp[0]),
+                                  *std::size_t(local_size_sp[1]),
+                                  *std::size_t(local_size_sp[2]));
+            auto* pc = (CT*)amrex::The_Arena()->alloc(sizeof(CT)*std::max(nc,std::size_t(1)));
+
+            r2c.forward(pr, pc);
+
+            auto scaling = r2c.scalingFactor();
+            amrex::ParallelFor(nc, [=] AMREX_GPU_DEVICE (std::size_t i)
+            {
+                pc[i] *= scaling;
+            });
+
+            r2c.backward(pc, pr);
+
+            auto error = amrex::Reduce::Max<RT>
+                (nr, [=] AMREX_GPU_DEVICE (std::size_t i)
+                         {
+                             return std::abs(pr[i]-std::sin(RT(i)));
+                         });
+            amrex::ParallelDescriptor::ReduceRealMax(error);
+            amrex::Print() << "  Degenerate domain " << AMREX_D_TERM(ds[0], << "x" << ds[1],
+                                                                    << "x" << ds[2])
+                           << ", expected to be close to zero: " << error << "\n";
+#ifdef AMREX_USE_FLOAT
+            auto eps = 3.e-6F;
+#else
+            auto eps = 1.e-13;
+#endif
+            AMREX_ALWAYS_ASSERT(error < eps);
+
+            amrex::The_Arena()->free(pr);
+            amrex::The_Arena()->free(pc);
+        }
+    }
+#endif
+
     int nbatch = 3;
     {
         amrex::FFT::Info info{};
